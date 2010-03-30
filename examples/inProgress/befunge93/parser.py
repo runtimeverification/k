3946a7ab7104165e8077fbf5ec29cdf50386dc69
Chucky Ellison <cmelliso@gmail.com>
2010-03-30 00:17:52 +0000

    simplistic and incomplete befunge parser
    git-svn-id: http://k-framework.googlecode.com/svn/k-maude/trunk@116 82a86efa-2583-11df-8ea0-49ba94de29ab

---
+++ examples/inProgress/befunge93/parser.py
@@ -0,0 +1,22 @@
+# 
+# usage: cat <bf-program>.bf | python parser.py > <bf-program>.k
+# -----------------------------------------------------------------------------
+
+import sys
+
+s = sys.stdin.read()
+x = 0
+y = 0
+print "injectM( __(.Map,(.Map "
+for line in s.splitlines():
+	x = 0
+	for c in line:
+		if c == '\\':
+			c = '\\\\'
+		if c == '"':
+			c = '\"'
+		print "\tcoord(inject(%(x)d)(.List{K}),, inject(%(y)d)(.List{K})) |-> inject(\"%(c)s\")(.List{K})" % {'x': x, 'y': y, 'c': c}
+		#print x, y, c
+		x += 1
+	y += 1
+print ")))"
