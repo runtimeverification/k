f2e1c818033e080ffcdd923cd1bf2110eb170697
Chucky Ellison <cmelliso@gmail.com>
2010-04-27 21:18:57 +0000

    oops :)
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@289 82a86efa-2583-11df-8ea0-49ba94de29ab

---
+++ tools/make-helper.mk
@@ -0,0 +1,35 @@
+################################
+# You shouldn't need to change anything below here.
+# The things below are set based on what you set above
+
+ALL_MAUDE_FILES = $(wildcard *.maude) $(wildcard */*.maude) $(wildcard */*/*.maude)
+MAUDE_FILES = $(filter-out %-compiled.maude,$(ALL_MAUDE_FILES))
+TOOL_DIR_FILES = $(wildcard $(TOOL_DIR)/*)
+COMPILED_FILE = $(MAIN_FILE)-compiled.maude
+LANGUAGE_FILE = $(MAIN_FILE).maude
+
+# phony tells make which targets aren't real files
+.PHONY: all test-% test force build
+
+# the top target, so the default target
+# compiles the definition, then runs all of the tests
+all: build 
+
+build: $(COMPILED_FILE)
+
+# this just builds the $(COMPILED_FILE) by running $(KCOMPILE)
+$(COMPILED_FILE): $(LANGUAGE_FILE) $(TOOL_DIR_FILES) $(MAUDE_FILES) Makefile
+	$(KCOMPILE) $(LANGUAGE_FILE) $(LANGUAGE_NAME)
+
+# to satisfy the target "test", it needs to satisfy the targets "test-a test-b test-c" for a b c \in $(TESTS)
+test: $(COMPILED_FILE) $(addprefix test-,$(TESTS))
+
+# this is how to satisfy the target "test-%" for some %.  It requires %.maude to exist.  It then runs it through maude
+test-%: %.maude
+	echo q | maude $<
+
+# used to force targets to run
+force: ;
+	
+clean:
+	rm -f *-compiled.maude kcompile_*
