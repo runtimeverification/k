46011dfa321e6a317cff737e35baa11366f1dac8
Traian Șerbănuță <Traian.Serbanuta@gmail.com>
2010-05-29 20:51:14 +0000

    k2latex improved with style selection.  run latexify.sh -h for details
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@412 82a86efa-2583-11df-8ea0-49ba94de29ab

--- tools/latexify.sh
+++ tools/latexify.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 MAUDE="maude"
 function usage {
-  echo "usage: $0 <source_file>[.maude] [<module_name>]
+  echo "usage: $0 [-s <style>] <source_file>[.maude] [<module_name>]
   
   If <module_name> is not specified, is asumed to be allcaps(<source_file>).
   <source_file> should ensure that a module <module_name> is loaded.
   Output is printed in <module_name>.tex and, if lucky, in <module_name>.PDF
+
+  The optional parameter style can alter the style used for typesetting.  
+  Currently, there are two styles options available, bb, for typesetting 
+  via tikz graphical cells, and mm, for typesetting using the simple 
+  mathematical notation.  the default option is bb.
+
+  If a file <source_file>.sty is provided by the user, it will be included in
+  the generated tex file after the above mentioned style, and thus could be
+  used to alter the default typesetting macros.
   "
 }
 
@@ -22,6 +31,12 @@ MAUDE="$MAUDE -no-banner -batch -no-wrap"
 echo "q" | $MAUDE >/dev/null
 RUNNER=`which "$0"`
 KBASE=`dirname "$RUNNER"`/..
+STYLE="bb"
+if [ "$1" = "-s" ]; then
+  STYLE="$2"
+  shift 2
+fi
+
 FILE=${1/.*/}
 if [[ $# -eq  2 ]]; then
   LANG="$2"
@@ -30,6 +45,15 @@ else
 fi
 
 
+echo "\\documentclass[landscape]{article}
+\\usepackage{import}
+\\import{$KBASE/tools/}{k2latex.$STYLE.sty}
+" > $LANG.tex
+
+if [ -f "$FILE.sty" ]
+then
+  echo "\\input{$FILE.sty}" >>$LANG.tex
+fi
 echo "
 load \"$KBASE/tools/k-to-latex\"
 set show advisories off .
@@ -40,7 +64,7 @@ select LATEX-PRINT-LOOP .
 loop latex-print .
 (print $LANG .)
 q
-" | maude -no-banner -no-wrap $1 | grep -A 10000 "documentclass" | sed '$d' >$LANG.tex
+" | maude -no-banner -no-wrap $1 | sed -n '/^\\begin{document}/,/\\end{document}/p'  >>$LANG.tex
 echo "LaTeX file generated.  now compiling through pdflatex"
 echo "Q" | pdflatex -interaction=errorstopmode $LANG.tex |tail -5
 
