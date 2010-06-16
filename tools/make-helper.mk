f8be87a52860d8ac08d0963f7ff43731d9cd4764
Traian Șerbănuță <Traian.Serbanuta@gmail.com>
2010-06-16 14:43:03 +0000

    updated make process so it know about .k and .m
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@567 82a86efa-2583-11df-8ea0-49ba94de29ab

--- tools/make-helper.mk
+++ tools/make-helper.mk
@@ -2,11 +2,11 @@
 # You shouldn't need to change anything below here.
 # The things below are set based on what you set above
 
-ALL_MAUDE_FILES = $(wildcard *.maude) $(wildcard */*.maude) $(wildcard */*/*.maude)
+ALL_MAUDE_FILES = $(wildcard *.k) $(wildcard *.k) $(wildcard */*.k) $(wildcard */*/*.k) $(wildcard *.kmaude) $(wildcard *.kmaude) $(wildcard */*.kmaude) $(wildcard */*/*.kmaude) $(wildcard *.maude) $(wildcard *.maude) $(wildcard */*.maude) $(wildcard */*/*.maude)
 MAUDE_FILES = $(filter-out %-compiled.maude,$(ALL_MAUDE_FILES))
 TOOL_DIR_FILES = $(wildcard $(TOOL_DIR)/*)
 COMPILED_FILE = $(MAIN_FILE)-compiled.maude
-LANGUAGE_FILE = $(or $(shell if [ -e $(MAIN_FILE).kmaude ]; then echo $(MAIN_FILE).kmaude; fi), $(shell if [ -e $(MAIN_FILE).maude ]; then echo $(MAIN_FILE).maude; fi))
+LANGUAGE_FILE = $(or $(shell if [ -e $(MAIN_FILE).k ]; then echo $(MAIN_FILE).k; fi), $(or $(shell if [ -e $(MAIN_FILE).kmaude ]; then echo $(MAIN_FILE).kmaude; fi), $(shell if [ -e $(MAIN_FILE).maude ]; then echo $(MAIN_FILE).maude; fi)))
 
 # phony tells make which targets aren't real files
 .PHONY: all test-% test force build
@@ -25,7 +25,7 @@ $(COMPILED_FILE): $(LANGUAGE_FILE) $(TOOL_DIR_FILES) $(MAUDE_FILES) Makefile
 test: $(COMPILED_FILE) $(addprefix test-,$(TESTS))
 
 # this is how to satisfy the target "test-%" for some %.  It requires %.maude to exist.  It then runs it through maude
-test-%: %.maude
+test-%: %
 	echo q | maude $<
 
 # used to force targets to run
