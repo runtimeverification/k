eb05a5d0a5dee7ea1b0b362c93ffffcaee8b4183
Andrei Ștefănescu <andreistef@gmail.com>
2010-06-21 22:01:40 +0000

    matching logic pl-builtins inclusion problem
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@615 82a86efa-2583-11df-8ea0-49ba94de29ab

--- examples/ml-imp/pre.sh
+++ examples/ml-imp/pre.sh
@@ -7,13 +7,10 @@ main=unwrapBuiltinsMain
 filepath=${1%/*}
 kmaude_file=${1##*/}
 filename=${kmaude_file%%.*}
-maude_file=${filename}".maude"
-syntax_file=${filename}"-syntax.maude"
-semantics_file=${filename}"-semantics.maude"
 compiled_file=${filename}"-compiled.maude"
 ml_file="ml-"${filename}".maude"
 cwd=`pwd`
 
 ${k_toolsdir}/kompile.pl $1
-python format.py ${compiled_file} | (cd ${antlr_rootdir} && make -f ${makefile} ${target} MAIN=${main}) | sed "1d" | sed "s/subsort Int\+\+ < Builtins \./\0\nsubsort Int\+\+ < KResult \.\nsubsort Id < K \./g" >${cwd}/${ml_file}
+(cd ${antlr_rootdir} && make -f ${makefile} ${target} MAIN=${main}) <${compiled_file} | sed "1d" | sed "s/subsort Int++ < Builtins \./\0\nsubsort Int++ < KResult \.\nsubsort Id < K \./g" >${cwd}/${ml_file}
 
