593262e7e58c5684617fa134372721d7a7d35a52
Andrei Ștefănescu <andreistef@gmail.com>
2010-06-23 21:43:12 +0000

    matching logic add fol= files
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@624 82a86efa-2583-11df-8ea0-49ba94de29ab

--- examples/ml-imp/pre.sh
+++ examples/ml-imp/pre.sh
@@ -5,12 +5,19 @@ makefile=Makefile.antlr
 target=run
 main=unwrapBuiltinsMain
 filepath=${1%/*}
-kmaude_file=${1##*/}
-filename=${kmaude_file%%.*}
+kmaude_main_file=${1##*/}
+filename=${kmaude_main_file%%.*}
+syntax_file=${filename}"-syntax.maude"
+semantics_file=${filename}"-semantics.maude"
+main_file=${filename}".maude"
 compiled_file=${filename}"-compiled.maude"
 ml_file="ml-"${filename}".maude"
 cwd=`pwd`
 
-${k_toolsdir}/kompile.pl $1
-(cd ${antlr_rootdir} && make -f ${makefile} ${target} MAIN=${main}) <${compiled_file} | sed "1d" | sed "s/subsort Int++ < Builtins \./\0\nsubsort Int++ < KResult \.\nsubsort Id < K \./g" >${cwd}/${ml_file}
+#${k_toolsdir}/kompile.pl -m $1
+${k_toolsdir}/kompile.pl -m ${filename}
+sed -i "s/\: : Int/: : IntSort/g" ${syntax_file}
+sed -i "s/\: : Int/: : IntSort/g" ${semantics_file}
+${k_toolsdir}/kompile.pl -c ${filename}
+(cd ${antlr_rootdir} && make -f ${makefile} ${target} MAIN=${main}) <${compiled_file} | sed "1,3d" | sed "/sorts/a\subsort Id < KProper ." >${cwd}/${ml_file}
 
