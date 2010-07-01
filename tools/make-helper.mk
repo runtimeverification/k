c92e301f3abf653bccc0cfc82784e2872c459710
Traian Șerbănuță <Traian.Serbanuta@gmail.com>
2010-07-01 14:13:11 +0000

    added make latex (which builds only the latex) and make pdf (which also runs it through pdflatex) options.  to use it in your language, you need to specify in your makefile LANGUAGE_MODULES to contain all modules which you want to be latexified, in that order.  also you can set the environment variable LATEX_STYLE to your preferred style (now accepting mm-for simple mathematical notation and bb-for soffisticated bubbles, defaulting to bb). I have tested it for imp, chalenge, and agent.
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@644 82a86efa-2583-11df-8ea0-49ba94de29ab

--- tools/make-helper.mk
+++ tools/make-helper.mk
@@ -2,10 +2,13 @@
 # You shouldn't need to change anything below here.
 # The things below are set based on what you set above
 
-ALL_MAUDE_FILES = $(wildcard *.k) $(wildcard *.k) $(wildcard */*.k) $(wildcard */*/*.k) $(wildcard *.kmaude) $(wildcard *.kmaude) $(wildcard */*.kmaude) $(wildcard */*/*.kmaude) $(wildcard *.maude) $(wildcard *.maude) $(wildcard */*.maude) $(wildcard */*/*.maude)
+ALL_MAUDE_FILES = $(wildcard *.k) $(wildcard *.k) $(wildcard */*.k) $(wildcard */*/*.k) $(wildcard *.m) $(wildcard *.m) $(wildcard */*.m) $(wildcard */*/*.m)  $(wildcard *.kmaude) $(wildcard *.kmaude) $(wildcard */*.kmaude) $(wildcard */*/*.kmaude) $(wildcard *.maude) $(wildcard *.maude) $(wildcard */*.maude) $(wildcard */*/*.maude)
 MAUDE_FILES = $(filter-out %-compiled.maude,$(ALL_MAUDE_FILES))
 TOOL_DIR_FILES = $(wildcard $(TOOL_DIR)/*)
 COMPILED_FILE = $(MAIN_FILE)-compiled.maude
+LATEX_FILE = $(MAIN_FILE).tex
+PDF_FILE = $(MAIN_FILE).pdf
+LATEX_STYLE ?= bb
 LANGUAGE_FILE = $(or $(shell if [ -e $(MAIN_FILE).k ]; then echo $(MAIN_FILE).k; fi), $(or $(shell if [ -e $(MAIN_FILE).kmaude ]; then echo $(MAIN_FILE).kmaude; fi), $(shell if [ -e $(MAIN_FILE).maude ]; then echo $(MAIN_FILE).maude; fi)))
 
 # phony tells make which targets aren't real files
@@ -21,6 +24,17 @@ build: $(COMPILED_FILE)
 $(COMPILED_FILE): $(LANGUAGE_FILE) $(TOOL_DIR_FILES) $(MAUDE_FILES) Makefile
 	$(KCOMPILE) $(LANGUAGE_FILE) -l $(LANGUAGE_NAME)
 
+# this should build the latex
+latex: $(LATEX_FILE)
+
+$(LATEX_FILE):  $(LANGUAGE_FILE) $(TOOL_DIR_FILES) $(MAUDE_FILES) Makefile
+	$(KCOMPILE) $(MAIN_FILE) -style $(LATEX_STYLE) -latex $(LANGUAGE_MODULES)
+
+pdf: $(PDF_FILE)
+	
+$(PDF_FILE): $(LATEX_FILE) 
+	pdflatex $(MAIN_FILE)
+
 # to satisfy the target "test", it needs to satisfy the targets "test-a test-b test-c" for a b c \in $(TESTS)
 test: $(COMPILED_FILE) $(addprefix test-,$(TESTS))
 
@@ -32,4 +46,4 @@ test-%: %
 force: ;
 	
 clean:
-	rm -f *-compiled.maude kcompile_*
+	rm -f $(MAIN_FILE)-compiled.maude kompile_* $(MAIN_FILE).aux $(MAIN_FILE).log $(MAIN_FILE).pdf $(MAIN_FILE).tex
