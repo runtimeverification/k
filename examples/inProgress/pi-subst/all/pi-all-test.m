3de36cf3696ffc2c40548663b06759ea4011cffa
Traian Șerbănuță <Traian.Serbanuta@gmail.com>
2010-07-11 04:43:34 +0000

    small updates to pi-calculus.  also, added a version (pi-subst) which assumes alpha-conversion and a built-in substitution (and thus, is not executable :-)
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@751 82a86efa-2583-11df-8ea0-49ba94de29ab

---
+++ examples/inProgress/pi-subst/all/pi-all-test.m
@@ -0,0 +1,8 @@
+load pi-all-compiled
+
+search run('p1) =>! B:Bag .
+show search graph  .
+
+
+rew run('p1) .
+
---
+++ examples/inProgress/pi-subst/pi-test.m
@@ -0,0 +1,6 @@
+load pi-compiled
+
+rew run('p1) .
+
+search run('p1) =>! B:Bag .
+show search graph  .
