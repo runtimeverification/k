e98a3566b37df0a93e30159dc494710d6d182ae6
Traian Șerbănuță <Traian.Serbanuta@gmail.com>
2010-08-01 00:53:11 +0000

    make make png use the same path
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@906 82a86efa-2583-11df-8ea0-49ba94de29ab

--- tools/make-helper.mk
+++ tools/make-helper.mk
@@ -7,9 +7,11 @@ MAUDE_FILES = $(filter-out %-compiled.maude,$(ALL_MAUDE_FILES))
 TOOL_DIR_FILES = $(wildcard $(TOOL_DIR)/*)
 COMPILED_FILE = $(MAIN_FILE)-compiled.maude
 LATEX_FILE = $(MAIN_FILE).tex
+DVI_FILE = $(MAIN_FILE).DVI
 PDF_FILE = $(MAIN_FILE).pdf
 CROP_PDF_FILE = $(MAIN_FILE)-crop.pdf
-PNG_FILES = $(MAIN_FILE)1.png
+EPS_FILES = $(MAIN_FILE)-ps-001.epsi
+PNG_FILES = $(EPS_FILES).png
 LATEX_STYLE ?= mm
 LANGUAGE_FILE = $(or $(shell if [ -e $(MAIN_FILE).k ]; then echo $(MAIN_FILE).k; fi), $(or $(shell if [ -e $(MAIN_FILE).kmaude ]; then echo $(MAIN_FILE).kmaude; fi), $(shell if [ -e $(MAIN_FILE).maude ]; then echo $(MAIN_FILE).maude; fi)))
 
@@ -36,23 +38,20 @@ pdf: $(PDF_FILE)
 	
 png: $(PNG_FILES)
 
-$(PNG_FILES): $(LATEX_FILE)
+$(PNG_FILES): $(EPS_FILES)
+	find . -name "$(MAIN_FILE)-ps-[0-9][0-9][0-9].epsi" -exec gs -q -dNOPAUSE -dEPSCrop -dBATCH -sDEVICE=pngalpha -r150 -sOutputFile={}.png {} \;
+
+$(DVI_FILE): $(LATEX_FILE)
 	latex $(MAIN_FILE)
-	rm -f $(MAIN_FILE)-picture-*.png
-	rm -f $(MAIN_FILE)-picture-*.eps
-	dvips -E -i -D 150 $(MAIN_FILE) -o $(MAIN_FILE)-picture-
-	find . -iname "$(MAIN_FILE)-picture-*" -exec mv {} {}.eps \;
-	find . -iname "$(MAIN_FILE)-picture-*.eps" -exec gs -q -dNOPAUSE -dEPSCrop -dBATCH -sDEVICE=pngalpha -r150 -sOutputFile={}.png {} \;
-	rm $(MAIN_FILE)-picture-*.eps
+
+$(EPS_FILES): $(DVI_FILE)
+	dvips -T 20in,9in -i $(MAIN_FILE) -o $(MAIN_FILE)-ps-
+	find . -name "$(MAIN_FILE)-ps-[0-9][0-9][0-9]" -exec ps2epsi {} \;
 
 crop-pdf: ${CROP_PDF_FILE}
 	
-$(CROP_PDF_FILE): $(LATEX_FILE)
-	latex $(MAIN_FILE)
-	dvips -T 20in,9in -i $(MAIN_FILE) -o kompile_ps_
-	find . -iname "kompile_ps_*" -exec ps2epsi {} \;
-	gs -q -dNOPAUSE -dEPSCrop -dBATCH -sDEVICE=pdfwrite -sOutputFile=$(CROP_PDF_FILE) `ls kompile_ps_*.epsi`
-	rm kompile_ps_*
+$(CROP_PDF_FILE): $(EPS_FILES)
+	gs -q -dNOPAUSE -dEPSCrop -dBATCH -sDEVICE=pdfwrite -sOutputFile=$(CROP_PDF_FILE) `ls $(MAIN_FILE)-ps-[0-9][0-9][0-9].epsi`
 
 $(PDF_FILE): $(CROP_PDF_FILE) 
 	nice-pdf.sh $(MAIN_FILE)
@@ -68,4 +67,4 @@ test-%: %
 force: ;
 	
 clean:
-	rm -f $(MAIN_FILE)-compiled.maude kompile_* $(MAIN_FILE).aux $(MAIN_FILE).log $(MAIN_FILE).pdf $(MAIN_FILE).ps $(MAIN_FILE).dvi *.png $(MAIN_FILE).tex $(CROP_PDF_FILE)
+	rm -f $(MAIN_FILE)-compiled.maude kompile_* $(MAIN_FILE).aux $(MAIN_FILE).log $(MAIN_FILE).pdf $(MAIN_FILE)-ps-* $(MAIN_FILE).dvi *.png $(MAIN_FILE).tex $(CROP_PDF_FILE)
