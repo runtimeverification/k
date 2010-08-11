1ce0e5a4d3136026affd21211d8d93b16351f847
Dorel Lucanu <Dorel.Lucanu@gmail.com>
2010-08-11 20:47:49 +0000

    solved a ubunto-related problem with the default size of dvips
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@971 82a86efa-2583-11df-8ea0-49ba94de29ab

--- tools/nice-pdf.sh
+++ tools/nice-pdf.sh
@@ -9,7 +9,7 @@ PH=$(( H + 1))
 echo "Re-running latex with approximative height $PH inches" 
 cat $1.tex | sed "s/\\\\begin{document}/\\\\geometry{papersize={1200mm,${PH}in},textheight=${H}in,textwidth=1180mm}\\\\pagestyle{empty}\\\\begin{document}\\\\noindent\\\\hspace{-2px}\\\\rule{1px}{1px}/ ; s/\\\\end{document}/\\\\par\\\\noindent\\\\hspace{-2px}\\\\rule{1px}{1px}\\\\end{document}/ ; s/\\\\newpage/\\\\bigskip/g" |latex --jobname=$1 |grep wide
 echo "Converting to postscript"
-dvips $MAIN_FILE -o $MAIN_FILE.ps 2>/dev/null
+dvips -T 1200mm,${PH}in $MAIN_FILE -o $MAIN_FILE.ps 2>/dev/null
 echo "Converting to eps"
 ps2eps -f -q -P -H -l $MAIN_FILE.ps
 echo "Converting to pdf"
