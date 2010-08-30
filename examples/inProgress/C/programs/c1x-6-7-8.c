d888ec46ae91a1047b8449eaab53f4eeadb4873d
Chucky Ellison <cmelliso@gmail.com>
2010-08-30 23:38:24 +0000

    starting new work on casting
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@1009 82a86efa-2583-11df-8ea0-49ba94de29ab

--- examples/inProgress/C/programs/c1x-6-7-8.c
+++ examples/inProgress/C/programs/c1x-6-7-8.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 int sec26(void){
 	int y1[4][3] = {
 		{1, 3, 5},
@@ -7,6 +9,7 @@ int sec26(void){
 	int y2[4][3] = {1, 3, 5, 2, 4, 6, 3, 5, 7};
 	for (int i = 0; i < 4; i = i + 1){
 		for (int j = 0; j < 3; j = j + 1){
+			printf("y1[%d][%d]=%d, y2[%d][%d]=%d\n", i, j, ((int*)y1)[i * 3 + j], i, j, ((int*)y2)[i * 3 + j]);
 			if (y1[i][j] != y2[i][j]) {
 				return 1;
 			}
