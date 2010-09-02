81d638b58638c21381a8fccbdbf2052b6925e08e
Chucky Ellison <cmelliso@gmail.com>
2010-09-02 00:07:46 +0000

    redoing the single inits to make sure i cover all the cases
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@1032 82a86efa-2583-11df-8ea0-49ba94de29ab

---
+++ examples/inProgress/C/programs/differentInits.c
@@ -0,0 +1,10 @@
+int main(void){
+	int x1 = 0;
+	int x2 = {0};
+	char x3[] = "hi";
+	char x4[2] = "hi";
+	unsigned char x5[] = "hi";
+	// int y[] = {"hi"};
+	// struct {int a, int b, int c[2]; int d} z = {[b] = 2, 3, 4, 5};
+	return 0;
+}
