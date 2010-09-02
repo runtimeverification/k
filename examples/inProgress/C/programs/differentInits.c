fac5113855ccc0bffa33244a2e329e72d3a70fe3
Chucky Ellison <cmelliso@gmail.com>
2010-09-02 16:00:43 +0000

    i think i have all cases now except initializing an array with something other than a string
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@1033 82a86efa-2583-11df-8ea0-49ba94de29ab

--- examples/inProgress/C/programs/differentInits.c
+++ examples/inProgress/C/programs/differentInits.c
@@ -4,7 +4,12 @@ int main(void){
 	char x3[] = "hi";
 	char x4[2] = "hi";
 	unsigned char x5[] = "hi";
-	// int y[] = {"hi"};
+	char x6[] = {"hi"};
+	char x7[2] = {"hi"};
+	
+	char x8[3] = {1};
+	int x9[3] = {1, 2, 3};
+	int x10[] = {5, 6, 7};
 	// struct {int a, int b, int c[2]; int d} z = {[b] = 2, 3, 4, 5};
 	return 0;
 }
