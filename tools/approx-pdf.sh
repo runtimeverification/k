1f5a950c08bf0a839d844a2b09f725515eae2dfe
Traian Șerbănuță <Traian.Serbanuta@gmail.com>
2010-09-15 05:00:50 +0000

    make pdf now seems to work on k
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@1131 82a86efa-2583-11df-8ea0-49ba94de29ab

--- tools/approx-pdf.sh
+++ tools/approx-pdf.sh
@@ -1,5 +1,5 @@
 echo "Running LaTeX to approximate height"
-cat $1.tex | sed 's/\\begin{document}/\\geometry{portrait,papersize={1200mm,11in},textwidth=1180mm}\\pagestyle{empty}\\begin{document}/ ; s/\\newpage/\\bigskip/g' | latex --jobname=$1 >/dev/null 
+cat $1.tex | sed 's/^\\documentclass\[landscape\]/\\documentclass/ ; s/\\begin{document}/\\geometry{papersize={1200mm,11in},textwidth=1180mm}\\pagestyle{empty}\\begin{document}/ ; s/\\newpage/\\bigskip/g' | latex --jobname=$1 >/dev/null 
 if [ "$?" -ne 0 ] ; then exit 0 ; fi
 PAGES=$(grep -o "[0-9]\+ pages\?, " $1.log | grep -o "[0-9]*")
 echo "We have $PAGES page(s)"
--- tools/nice-pdf.sh
+++ tools/nice-pdf.sh
@@ -10,7 +10,7 @@ fi
 H=$(( 9 * PAGES))
 PH=$(( H + 1))
 echo "Re-running latex with approximative height $PH inches" 
-cat $1.tex | sed "s/\\\\begin{document}/\\\\geometry{portrait,papersize={1200mm,${PH}in},textheight=${H}in,textwidth=1180mm}\\\\pagestyle{empty}\\\\begin{document}\\\\noindent\\\\hspace{-2px}\\\\rule{1px}{1px}/ ; s/\\\\end{document}/\\\\par\\\\noindent\\\\hspace{-2px}\\\\rule{1px}{1px}\\\\end{document}/ ; s/\\\\newpage/\\\\bigskip/g" |latex --jobname=$1 |grep wide
+cat $1.tex | sed "s/^\\documentclass\[landscape\]/\\documentclass/ ; s/\\\\begin{document}/\\\\geometry{papersize={1200mm,${PH}in},textheight=${H}in,textwidth=1180mm}\\\\pagestyle{empty}\\\\begin{document}\\\\noindent\\\\hspace{-2px}\\\\rule{1px}{1px}/ ; s/\\\\end{document}/\\\\par\\\\noindent\\\\hspace{-2px}\\\\rule{1px}{1px}\\\\end{document}/ ; s/\\\\newpage/\\\\bigskip/g" |latex --jobname=$1 |grep wide
 echo "Converting to postscript"
 dvips -T 1200mm,${PH}in $MAIN_FILE -o $MAIN_FILE.ps 2>/dev/null
 echo "Converting to eps"
