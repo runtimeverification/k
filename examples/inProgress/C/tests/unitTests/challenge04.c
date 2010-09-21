371c4302f858bba8319bfbe1d1ac8fb7631caf6f
Chucky Ellison <cmelliso@gmail.com>
2010-09-21 19:39:45 +0000

    fixed bug with array arguments
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@1169 82a86efa-2583-11df-8ea0-49ba94de29ab

--- examples/inProgress/C/tests/unitTests/challenge04.c
+++ examples/inProgress/C/tests/unitTests/challenge04.c
@@ -1,9 +1,13 @@
 struct t {int a[2];} s;
 void f(struct t s) { s.a[1] = 3; }
-void g(int a[2]) { a[1] = 5; }
+void g(int a[2]) { printf("inner %p\n", a); a[1] = 5; }
 int main() {
 	int x = (f(s), s.a[1]);
+	printf("%d\n", x);
+	printf("outer %p\n", s.a); 
 	int y = (g(s.a), s.a[1]);
+	printf("%d\n", y);
+	printf("%d\n", x + y);
 	return x + y;
 }
 // should return 5, not 8
