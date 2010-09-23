a54e7cb3971c10a5a8967dedf0cb1b5e62c7244a
Traian Șerbănuță <Traian.Serbanuta@gmail.com>
2010-09-23 15:50:01 +0000

    standard kernelc
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@1180 82a86efa-2583-11df-8ea0-49ba94de29ab

---
+++ examples/kernelC/kernelc-test.m
@@ -0,0 +1,11 @@
+load kernelc-compiled
+
+rew run('pSum) .
+rew run('pCollatz) .
+rew run('pAllocatePrintFree) .
+rew run('pAllocatePrintReversePrintFree) .
+rew run('pStrTest) .
+rew run('pStrErr) .
+rew run('pAllocateBadPrint) .
+rew run('pBufferOverflow) .
+rew run('pSimpTest) .
