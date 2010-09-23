c69ed60806c3d771eecbf8017e2720fb307625ce
Chucky Ellison <cmelliso@gmail.com>
2010-09-23 20:49:19 +0000

    significant additions to twofile
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@1193 82a86efa-2583-11df-8ea0-49ba94de29ab

--- examples/inProgress/C/programs/twoFile1.c
+++ examples/inProgress/C/programs/twoFile1.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 extern int f(int x, int y);
 int z = 5;
+static int w = 7;
+int q = 3;
+int c = 32;
 int main(void){
+	int q = 4;
 	printf("1z = %d\n", z);
-	printf("f(2, 3) should be 7\n");
+	printf("1w = %d\n", w);
+	printf("1q = %d\n", q);
 	printf("f(2, 3)==%d\n", f(2, 3));
+	printf("f(2, 3)==%d\n", f(2, 3));
+	printf("f(2, 3) should be 7\n");
 	return 0;
 }
--- examples/inProgress/C/programs/twoFile2.c
+++ examples/inProgress/C/programs/twoFile2.c
@@ -1,5 +1,13 @@
 int z;
+static int w = 13;
+static int r = 15;
 int f (int x, int y) {
-	printf("2z = %d\n", z);
+	static int r;
+	r++;
+	extern int c;
+	printf("  2z = %d\n", z);
+	printf("  2w = %d\n", w);
+	printf("  2r = %d\n", r);
+	printf("  2c = %d\n", c);
 	return x * 2 + y;
 }
