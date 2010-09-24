d73688892c74f3d320f4e1382716658062b811e0
Chucky Ellison <cmelliso@gmail.com>
2010-09-24 22:43:14 +0000

    covering nearly every case.  still need to handle the difference between all extern decls and all but one
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@1200 82a86efa-2583-11df-8ea0-49ba94de29ab

--- examples/inProgress/C/programs/twoFile1.c
+++ examples/inProgress/C/programs/twoFile1.c
@@ -5,13 +5,18 @@ static int w = 7;
 int q = 3;
 int c = 32;
 int r = 77;
+extern int s;
+typedef int myvar;
 // i think there will be a problem with constant expressions using other internal globals
 int main(void){
 	int q = 4;
+	myvar v = 15; 
 	printf("1z = %d\n", z);
 	printf("1w = %d\n", w);
 	printf("1q = %d\n", q);
 	printf("1r = %d\n", r);
+	printf("1v = %d\n", v);
+	printf("1s = %d\n", s);
 	printf("f(2, 3)==%d\n", f(2, 3));
 	printf("f(2, 3)==%d\n", f(2, 3));
 	printf("f(2, 3) should be 7\n");
--- examples/inProgress/C/programs/twoFile2.c
+++ examples/inProgress/C/programs/twoFile2.c
@@ -2,13 +2,16 @@
 int z;
 static int w = 13;
 static int r = 15;
+typedef double myvar;
 int f (int x, int y) {
 	static int r;
 	r++;
 	extern int c;
+	myvar v = 2.3;
 	printf("  2z = %d\n", z);
 	printf("  2w = %d\n", w);
 	printf("  2r = %d\n", r);
 	printf("  2c = %d\n", c);
+	printf("  2v = %d\n", (int)(v*1000));
 	return x * 2 + y;
 }
