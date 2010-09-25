e14476e1215b5f2fbd9032fe6625859fc6f8ee3f
Chucky Ellison <cmelliso@gmail.com>
2010-09-25 00:53:19 +0000

    need to think about locals and make sure constant expressions in global initializations are using the right objects
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@1202 82a86efa-2583-11df-8ea0-49ba94de29ab

--- examples/inProgress/C/programs/twoFile1.c
+++ examples/inProgress/C/programs/twoFile1.c
@@ -8,7 +8,7 @@ int r = 77;
 typedef int myvar;
 // i think there will be a problem with constant expressions using other internal globals
 // also look for problems with structs having the same name in two files, etc
-
+static int zz;
 static int g(int x) {
 	return x;
 }
@@ -16,12 +16,14 @@ static int g(int x) {
 int main(void){
 	int q = 4;
 	myvar v = 15; 
+	printf("1zza = %d\n", zz);
+	static int zz = 5;
 	printf("1z = %d\n", z);
 	printf("1w = %d\n", w);
 	printf("1q = %d\n", q);
 	printf("1r = %d\n", r);
 	printf("1v = %d\n", v);
-	//printf("1s = %d\n", s);
+	printf("1zzb = %d\n", zz);
 	printf("1g(5) = %d\n", g(5));
 	printf("f(2, 3)==%d\n", f(2, 3));
 	printf("f(2, 3)==%d\n", f(2, 3));
