ec742250d643b762d513332066f5bb55e3759b9e
Chucky Ellison <cmelliso@gmail.com>
2010-09-26 22:44:42 +0000

    still can't really handle static.  need to remember all this and the linkage when internal and not just external
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@1211 82a86efa-2583-11df-8ea0-49ba94de29ab

--- examples/inProgress/C/programs/testStatic.c
+++ examples/inProgress/C/programs/testStatic.c
@@ -1,8 +1,13 @@
 int x = 17;
+int y = 5;
 int f(void){
 	static int x;
 	x++;
-	{static int x; x = 12;}
+	{
+		static int x; 
+		x = 12;
+	}
+	
 	return x;
 }
 
@@ -14,6 +19,14 @@ int g(void){
 
 int main(void){
 	int x = 5;
+	printf("01y=%d\n", y);
+	int y = 6;
+	printf("02y=%d\n", y);
+	{
+		extern int y;
+		printf("03y=%d\n", y);
+	}
+	printf("04y=%d\n", y);
 	f();f();
 	g();g();g();
 	return f() + g();
