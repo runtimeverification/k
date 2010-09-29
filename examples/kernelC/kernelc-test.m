d285b54791cac33a1975bf72a12457ab05628b79
Traian Șerbănuță <Traian.Serbanuta@gmail.com>
2010-09-29 18:19:31 +0000

    kernelc looks like C
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@1253 82a86efa-2583-11df-8ea0-49ba94de29ab

--- examples/kernelC/kernelc-test.m
+++ examples/kernelC/kernelc-test.m
@@ -1,33 +1,35 @@
----(
 rew run('pSum) .
 rew run('pCollatz) .
-rew run('pAllocatePrintFree) .
-rew run('pAllocatePrintReversePrintFree) .
+rew run('pBufferOverflow) .
 rew run('pStrTest) .
 rew run('pStrErr) .
+rew run('pAllocatePrintFree) .
+rew run('pAllocatePrintReversePrintFree) .
 rew run('pAllocateBadPrint) .
-rew run('pBufferOverflow) .
 rew run('pSimpTest) .
 rew run('pSeqQuickSort, (Int 7(.List{K}),,Int 5(.List{K}),,Int 9(.List{K}),,Int 1(.List{K}),,Int 3(.List{K}),,Int 8(.List{K}),,Int 4(.List{K}),,Int 10(.List{K}),, Int 0(.List{K}))) .
 rew run('pConcQuickSort, (Int 7(.List{K}),,Int 5(.List{K}),,Int 9(.List{K}),,Int 1(.List{K}),,Int 3(.List{K}),,Int 8(.List{K}),,Int 4(.List{K}),,Int 10(.List{K}),, Int 0(.List{K}))) .
 
-search[1] run('pConcQuickSort, (Int 3(.List{K}),,Int 5(.List{K}),,Int 9(.List{K}),,Int 1(.List{K}),,Int 3(.List{K}),,Int 8(.List{K}),,Int 4(.List{K}),,Int 10(.List{K}),, Int 0(.List{K}))) =>*  < race > B:Bag </ race > .
+---search[1] run('pConcQuickSort, (Int 7(.List{K}),,Int 5(.List{K}),,Int 9(.List{K}),,Int 1(.List{K}),,Int 3(.List{K}),,Int 8(.List{K}),,Int 4(.List{K}),,Int 10(.List{K}),, Int 0(.List{K}))) =>*  < raceDetected > B:Bag </ raceDetected > .
 rew run('pRace1) .
+search[1] run('pRace1) =>*   < raceDetected > B:Bag </ raceDetected > .
 
 rew run('pAccount1) .
-rew run('pAccount2) .
-search[1] run('pAccount2) =>* < race > B:Bag </ race > .
+search[1] run('pAccount2) =>* < raceDetected > B:Bag </ raceDetected > .
 
----)
+rew run('pAccount2) .
+search[1] run('pAccount2) =>* < raceDetected > B:Bag </ raceDetected > .
+
 frew run('pAccount3) .
-search[1] run('pAccount3) =>* < race > B:Bag </ race > .
+search[1] run('pAccount3) =>* < raceDetected > B:Bag </ raceDetected > .
 search[1] run('pAccount3) =>! < T > B:Bag </ T > .
 frew run('pAccount4) .
-search[1] run('pAccount4) =>* < race > B:Bag </ race > .
+search[1] run('pAccount4) =>* < raceDetected > B:Bag </ raceDetected > .
 search[1] run('pAccount4) =>! < T > B:Bag </ T > .
 
 frew run('pAccount5) .
-search[1] run('pAccount5) =>* < race > B:Bag </ race > .
+search[1] run('pAccount5) =>* < raceDetected > B:Bag </ raceDetected > .
 search[1] run('pAccount5) =>! < T > B:Bag </ T > .
 ---show search graph .
 ---frew run('pRace2) .
+---)
