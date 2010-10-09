842bbb9412a68701ae0f52e309a0231db5cbb32e
Andrei Arusoai <Andrei.Arusoaie@gmail.com>
2010-10-09 09:25:27 +0000

    make latex, pdf, eps.. works now with kompile -latex, -pdf, -eps...
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@1314 82a86efa-2583-11df-8ea0-49ba94de29ab

--- tools/make-helper.mk
+++ tools/make-helper.mk
@@ -6,12 +6,12 @@ ALL_MAUDE_FILES = $(wildcard *.k) $(wildcard *.k) $(wildcard */*.k) $(wildcard *
 MAUDE_FILES = $(filter-out %-compiled.maude,$(ALL_MAUDE_FILES))
 TOOL_DIR_FILES = $(wildcard $(TOOL_DIR)/*)
 COMPILED_FILE = $(MAIN_FILE)-compiled.maude
-LATEX_FILE = $(MAIN_FILE).tex
-DVI_FILE = $(MAIN_FILE).DVI
-PDF_FILE = $(MAIN_FILE).pdf
-CROP_PDF_FILE = $(MAIN_FILE)-crop.pdf
-EPS_FILES = $(MAIN_FILE)-ps-001.eps
-PNG_FILES = $(EPS_FILES).png
+# LATEX_FILE = $(MAIN_FILE).tex
+# DVI_FILE = $(MAIN_FILE).DVI
+# PDF_FILE = $(MAIN_FILE).pdf
+# CROP_PDF_FILE = $(MAIN_FILE)-crop.pdf
+# EPS_FILES = $(MAIN_FILE)-ps-001.eps
+# PNG_FILES = $(EPS_FILES).png
 LATEX_STYLE ?= mm
 LANGUAGE_FILE = $(or $(shell if [ -e $(MAIN_FILE).k ]; then echo $(MAIN_FILE).k; fi), $(or $(shell if [ -e $(MAIN_FILE).kmaude ]; then echo $(MAIN_FILE).kmaude; fi), $(shell if [ -e $(MAIN_FILE).maude ]; then echo $(MAIN_FILE).maude; fi)))
 
@@ -29,32 +29,29 @@ $(COMPILED_FILE): $(LANGUAGE_FILE) $(TOOL_DIR_FILES) $(MAUDE_FILES) Makefile
 	$(KCOMPILE) $(LANGUAGE_FILE) -l $(LANGUAGE_NAME)
 
 # this should build the latex
-latex: $(LATEX_FILE)
-
-$(LATEX_FILE):  $(LANGUAGE_FILE) $(TOOL_DIR_FILES) $(MAUDE_FILES) Makefile
+latex: $(LANGUAGE_FILE) $(TOOL_DIR_FILES) $(MAUDE_FILES) Makefile
 	$(KCOMPILE) $(LANGUAGE_FILE) -l $(LANGUAGE_NAME) -style $(LATEX_STYLE) -latex $(LANGUAGE_MODULES)
 
-pdf: $(PDF_FILE)
-	
-png: $(PNG_FILES)
+# this should build the pdf
+pdf:  $(LANGUAGE_FILE) $(TOOL_DIR_FILES) $(MAUDE_FILES) Makefile
+	$(KCOMPILE) $(LANGUAGE_FILE) -l $(LANGUAGE_NAME) -style $(LATEX_STYLE) -pdf $(LANGUAGE_MODULES)
 
-$(PNG_FILES): $(CROP_PDF_FILE)
-	find . -name "$(MAIN_FILE)-ps-[0-9][0-9][0-9].eps" -exec gs -q -dNOPAUSE -dEPSCrop -dBATCH -sDEVICE=pngalpha -r150 -sOutputFile={}.png {} \;
+# this should build the png
+png:  $(LANGUAGE_FILE) $(TOOL_DIR_FILES) $(MAUDE_FILES) Makefile
+	$(KCOMPILE) $(LANGUAGE_FILE) -l $(LANGUAGE_NAME) -style $(LATEX_STYLE) -png $(LANGUAGE_MODULES)
 
-$(DVI_FILE): $(LATEX_FILE)
-	latex $(MAIN_FILE)
+# this should build the eps
+eps:  $(LANGUAGE_FILE) $(TOOL_DIR_FILES) $(MAUDE_FILES) Makefile
+	$(KCOMPILE) $(LANGUAGE_FILE) -l $(LANGUAGE_NAME) -style $(LATEX_STYLE) -eps $(LANGUAGE_MODULES)
 
-$(EPS_FILES): $(DVI_FILE)
-	dvips -T 1189mm,297mm -i $(MAIN_FILE) -o $(MAIN_FILE)-ps-
-	find . -name "$(MAIN_FILE)-ps-[0-9][0-9][0-9]" -exec ps2eps -f -q -P -H {} \;
+# this should build the ps
+ps:  $(LANGUAGE_FILE) $(TOOL_DIR_FILES) $(MAUDE_FILES) Makefile
+	$(KCOMPILE) $(LANGUAGE_FILE) -l $(LANGUAGE_NAME) -style $(LATEX_STYLE) -ps $(LANGUAGE_MODULES)
 
-crop-pdf: ${CROP_PDF_FILE}
-	
-$(CROP_PDF_FILE): $(LATEX_FILE)
-	$(TOOL_DIR)/nice-crop-pdf.sh $(MAIN_FILE)
+# this should build the crop-pdf
+crop-pdf:  $(LANGUAGE_FILE) $(TOOL_DIR_FILES) $(MAUDE_FILES) Makefile
+	$(KCOMPILE) $(LANGUAGE_FILE) -l $(LANGUAGE_NAME) -style $(LATEX_STYLE) -crop $(LANGUAGE_MODULES)
 
-$(PDF_FILE): $(LATEX_FILE) 
-	$(TOOL_DIR)/nice-pdf.sh $(MAIN_FILE)
 
 # to satisfy the target "test", it needs to satisfy the targets "test-a test-b test-c" for a b c \in $(TESTS)
 test: $(COMPILED_FILE) $(addprefix test-,$(TESTS))
