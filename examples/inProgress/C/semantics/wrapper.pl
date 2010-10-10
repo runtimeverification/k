9d4ed092d7745eacb7e9dbd3d7354d3c04ff2fe4
Chucky Ellison <cmelliso@gmail.com>
2010-10-10 02:00:59 +0000

    typing arrows, and printing out top k and type if core dump
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@1318 82a86efa-2583-11df-8ea0-49ba94de29ab

--- examples/inProgress/C/semantics/wrapper.pl
+++ examples/inProgress/C/semantics/wrapper.pl
@@ -6,8 +6,11 @@ my $haveResult = 0;
 my $buffer = "";
 my $kCell = "";
 my $typeCell = "";
+my $ignoreThis = 0;
 while (my $line = <STDIN>) {
-	$buffer .= $line;
+	if (!$ignoreThis){
+		$buffer .= $line;
+	}
 	chomp($line);
 	$line =~ s/[\000-\037]\[(\d|;)+m//g; # remove ansi colors
 	#print "$line\n";
@@ -29,22 +32,43 @@ while (my $line = <STDIN>) {
 			printf "FAILURE\n";
 		}
 	} elsif ($state eq "success"){
-		if ($line =~ m/< (input|\(input\)\.CellLabel) > .* <\/ \1 >/){
+		if ($line =~ m/< input > .* <\/ input >/){
 			$reduced = 1;
-		} elsif ($line =~ m/(< (k|\(k\)\.CellLabel) > .* <\/ \2 >)/){
+		} elsif ($line =~ m/< callStack > List\(/){
+			$ignoreThis = 1;
+		} elsif ($line =~ m/<\/ callStack >/){
+			$ignoreThis = 0;
+		} elsif ($line =~ m/< blockStack > List\(/){
+			$ignoreThis = 1;
+		} elsif ($line =~ m/<\/ blockStack >/){
+			$ignoreThis = 0;
+		} elsif ($line =~ m/< genv >/){
+			$ignoreThis = 1;
+		} elsif ($line =~ m/<\/ genv >/){
+			$ignoreThis = 0;
+		} elsif ($line =~ m/< gtypes >/){
+			$ignoreThis = 1;
+		} elsif ($line =~ m/<\/ gtypes >/){
+			$ignoreThis = 0;
+		} elsif ($line =~ m/< loopStack >/){
+			$ignoreThis = 1;
+		} elsif ($line =~ m/<\/ loopStack >/){
+			$ignoreThis = 0;
+		# } elsif ($line =~ m/(< (k|\(k\)\.CellLabel) > .* <\/ \2 >)/){
+		} elsif ($line =~ m/(< k > .*)$/){
 			$kCell = $1;
-		} elsif ($line =~ m/(< (type|\(type\)\.CellLabel) > .* <\/ \2 >)/){
+		} elsif ($line =~ m/(< type > .*)$/){
 			$typeCell = $1;
-		}  elsif ($line =~ m/< (output|\(output\)\.CellLabel) > String "(.*)"\(\.List{K}\) <\/ \1 >/){
-			my $output = $2;
+		} elsif ($line =~ m/< output > String "(.*)"\(\.List{K}\) <\/ output >/){
+			my $output = $1;
 			$output =~ s/\%/\%\%/g;
 			$output =~ s/\\\\/\\\\\\\\/g;
 			print substr(`printf "x$output"`, 1);
-		} elsif ($line =~ m/< (resultValue|\(resultValue\)\.CellLabel) > \('tv\)\.KResultLabel\(kList\("wklist_"\)\(BaseValue (-?\d+)\(\.List\{K\}\)\),,BaseType int\(\.List\{K\}\)\) <\/ \1 >/){
+		} elsif ($line =~ m/< resultValue > \('tv\)\.KResultLabel\(kList\("wklist_"\)\(BaseValue (-?\d+)\(\.List\{K\}\)\),,BaseType int\(\.List\{K\}\)\) <\/ resultValue >/){
 			$haveResult = 1;
-			$retval = $2;
+			$retval = $1;
 		}
-	} 
+	}
 	
 	if ($state eq "failure"){
 		print "$line\n";
@@ -59,14 +83,14 @@ if ($reduced == 0||$haveResult == 0) {
 	# $kCell =~ s/Id ([^\)\(,]+)/Id\(\1\)/g;
 	# $kCell =~ s/\(([^\)]*)\)\.(?:KProperLabel|KResultLabel)(\)| )/\1\2/g;
 	#$kCell =~ s/\("_\*_"\)\.KProperLabel\(([^,\(\)]+),,([^,\(\)]+)\)/\1 * \2/g;
-	my @lines = split(/ ~> /, $kCell);
-	@lines = @lines[0..5];
-	$kCell = join(" ~>\n", @lines);
+	# my @lines = split(/ ~> /, $kCell);
+	# @lines = @lines[0..5];
+	# $kCell = join(" ~>\n", @lines);
 	print "\n$kCell\n";
 	
-	@lines = split(/ ~> /, $typeCell);
-	@lines = @lines[0..2];
-	$typeCell = join(" ~>\n", @lines);
+	# my @lines = split(/ ~> /, $typeCell);
+	# @lines = @lines[0..2];
+	# $typeCell = join(" ~>\n", @lines);
 	print "\n$typeCell\n";
 }
 exit $retval;
