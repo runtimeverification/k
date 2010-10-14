d85794bd6a3af5ee6eddc3978f023872dacbfef4
Traian Șerbănuță <Traian.Serbanuta@gmail.com>
2010-10-14 03:07:10 +0000

    added a relaxed memory model for kernelc
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@1338 82a86efa-2583-11df-8ea0-49ba94de29ab

--- examples/kernelC/kernelc-test.m
+++ examples/kernelC/kernelc-test.m
@@ -3,32 +3,13 @@ rew run('pCollatz) .
 rew run('pBufferOverflow, pBufferOverflow.in1) .
 rew run('pBufferOverflow, pBufferOverflow.in2) .
 rew run('pBufferOverflow, pBufferOverflow.in3) .
----(
-rew run('pStrTest) .
-rew run('pStrErr) .
-rew run('pAllocatePrintFree) .
-rew run('pAllocatePrintReversePrintFree) .
-rew run('pAllocateBadPrint) .
-rew run('pSimpTest) .
----)
 rew run('pSeqQuickSort, pSeqQuickSort.in) .
 rew run('pConcQuickSort, pSeqQuickSort.in) .
 ---search[1] run('pConcQuickSort, pSeqQuickSort.in) =>*  < raceDetected > B:Bag </ raceDetected > .
----(
-rew run('pRace1) .
----search[1] run('pRace1) =>*   < raceDetected > B:Bag </ raceDetected > .
-rew run('pAccount1) .
----search[1] run('pAccount2) =>* < raceDetected > B:Bag </ raceDetected > .
----)
 rew run('pAccount2) .
 ---search[1] run('pAccount2) =>* < raceDetected > B:Bag </ raceDetected > .
 ---search[1] run('pAccount4) =>* < raceDetected > B:Bag </ raceDetected > .
 ---search[1] run('pAccount4) =>! < T > B:Bag </ T > .
-eof
 
-frew run('pAccount5) .
----search[1] run('pAccount5) =>* < raceDetected > B:Bag </ raceDetected > .
----search[1] run('pAccount5) =>! < T > B:Bag </ T > .
----show search graph .
----frew run('pRace2) .
----)
+rew run('pPeterson.c) .
+search run('pPeterson.c) =>! B:Bag .
