6696534cb7208e48ec22d2527e1b2bce63506de8
Andrei Arusoai <Andrei.Arusoaie@gmail.com>
2010-10-25 10:28:53 +0000

    -crop uses preview package
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@1424 82a86efa-2583-11df-8ea0-49ba94de29ab

--- tools/kompile.pl
+++ tools/kompile.pl
@@ -753,16 +753,13 @@ sub make_pdf
 
     # Generate tex
     my $status = system("pdflatex $language_file_name-pdf.tex > out");
-    print "Failed to run latex. Exit status $status.\n" if (($status >>= 8) != 0);
+    print "Failed to run pdflatex. Exit status $status.\n" if (($status >>= 8) != 0);
 
     rename ("$language_file_name-pdf.pdf", "$language_file_name.pdf");
     
     # delete auxialiary files if not verbose
     unlink("$language_file_name-pdf.tex") if !$verbose;
     unlink("$language_file_name-pdf.aux") if !$verbose;
-#    unlink("$language_file_name-pdf.dvi") if !$verbose;
-#    unlink("$language_file_name-pdf.eps") if !$verbose;
-#    unlink("$language_file_name-pdf.ps") if !$verbose;
     unlink("$language_file_name-pdf.log") if !$verbose;
     unlink("out");
 }
@@ -909,78 +906,29 @@ sub make_crop
 {
     latexify("crop", @crop_modules);
 
-    # Find number of pages
-    my $pages = run_latex("$language_file_name-crop");
-    my $h = 9 * $pages;
-    my $ph = $h + 1;
-
     # modify page and save it
-     my $latex_out = get_file_content("$language_file_name-crop.tex");
-     my $settings = "\\geometry{papersize={1200mm,".$ph."in},textheight=".$h."in,textwidth=1180mm}\\pagestyle{empty}\\begin{document}\\noindent\\hspace{-2px}\\rule{1px}{1px}";
-     $latex_out =~ s/\\begin{document}/$settings/;
-     $latex_out =~ s/\\end{document}/\\par\\noindent\\hspace{-2px}\\rule{1px}{1px}\\end{document}/;
-     $latex_out =~ s/\\newpage/\\par\\noindent\\hspace{-2px}\\rule{1px}{1px}\\newpage\\noindent\\hspace{-2px}\\rule{1px}{1px}/g;
-    
-     open FILE,">", "$language_file_name-crop.tex" or die "Cannot create $language_file_name-crop.tex\n";
-     print FILE $latex_out;
-     close FILE;
-    # Generate tex
-    my $status = system("latex -interaction=nonstopmode $language_file_name-crop.tex > out");
-    print "Failed to run latex. Exit status $status.\n" if (($status >>= 8) != 0);
+    my $latex_out = get_file_content("$language_file_name-crop.tex");
 
+    # initial settings
+    $latex_out =~ s/\\usepackage{import}/\\usepackage{import}\n\\usepackage[active,tightpage,pdftex]{preview}\n\\setlength\\PreviewBorder{5pt}%/;
+    $latex_out =~ s/\\begin{document}/\\PreviewEnvironment{module}\n\\begin{document}\n\\pagestyle{empty}/;
+    open FILE,">", "$language_file_name-crop.tex" or die "Cannot create $language_file_name-crop.tex\n";
+    print FILE $latex_out;
+    close FILE;    
     
-    # Generate postscript
-    $status = system("dvips $language_file_name-crop -i -o in  -o $language_file_name-crop- 2>/dev/null");
-    print "Failed to generate ps. Exit status $status.\n" if (($status >>= 8) != 0);
-
-    # Generate eps
-    opendir(DIR, ".");
-    my @all_files = readdir(DIR);
-    closedir(DIR);
-    foreach (@all_files)
-    {
-	if (/$language_file_name-crop-[0-9][0-9][0-9]/)
-	{
-	    $status = system("ps2eps -f -q -P -H -l $_");
-	    print "Failed to generate eps for $_. Exit status $status.\n" if (($status >>= 8) != 0);
-	}
-    }
-    
-    
-    # refresh files from . folder
-    opendir(DIR, ".");
-    @all_files = readdir(DIR);
-    closedir(DIR);
-    my @eps_files = ();
-    foreach (@all_files)
-    {
-	if (/$language_file_name-crop-[0-9][0-9][0-9].eps/)
-	{
-	    push(@eps_files, $_);
-	}
-    }
-    # Generate pdf
-    $status = system("gs -q -dNOPAUSE -dEPSCrop -dBATCH -sDEVICE=pdfwrite -sOutputFile=$language_file_name-crop.pdf @eps_files");
-    print "Failed to generate pdf. Exit status $status.\n" if (($status >>= 8) != 0);
+    my $status = system("pdflatex $language_file_name-crop.tex > out");
+    print "Failed to run pdflatex. Exit status $status.\n" if (($status >>= 8) != 0);
     
     # print message
     print "Generated $language_file_name-crop.pdf which contains modules: @crop_modules\n";
     
     # delete auxialiary files if not verbose
-    unlink("$language_file_name-crop.tex") if !$verbose;
+#    unlink("$language_file_name-crop.tex") if !$verbose;
     unlink("$language_file_name-crop.aux") if !$verbose;
     unlink("$language_file_name-crop.dvi") if !$verbose;
     unlink("$language_file_name-crop.log") if !$verbose;
     unlink("out");
     
-    foreach (@all_files)
-    {
-	if (/$language_file_name-crop-[0-9][0-9][0-9]/)
-	{
-	    unlink($_) if !$verbose;
-	}
-    }
-
 }
 
 # Next routine compiles the language definition in $language_file_name
