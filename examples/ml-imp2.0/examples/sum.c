5e302db9ab6eefd5c30a0de8d62f67a229ea495b
Elena Naum <naum.elena@gmail.com>
2010-10-26 22:18:15 +0000

    git-svn-id: http://k-framework.googlecode.com/svn/trunk@1461 82a86efa-2583-11df-8ea0-49ba94de29ab

--- examples/ml-imp2.0/examples/sum.c
+++ examples/ml-imp2.0/examples/sum.c
@@ -1,18 +1,19 @@
-/*@ var ?x ?y ?p : ?Int */
+/*@ var ?s ?n : ?Int */
+/*@ var n0 : FreeInt */
 /*@ var ?B ?C : ?Seq */
 /*@ var A : FreeSeq */
 /*@ var ?rho ?H : ?MapItem */
 
 int sum(int n)
 /*@ pre < config > < env > n |-> n0 </ env > < heap > (.).Map </ heap > < form > @(n0 >=Int 0) </ form > </ config > */
-/*@ post < config > < env > ?rho </ env > < heap > (.).Map </ heap > < form > returns ?s /\ @(?n >=Int 0) /\ @(n0 >=Int 0) </ form > </ config > */
+/*@ post < config > < env > ?rho </ env > < heap > (.).Map </ heap > < form > returns ?s /\ @(?n ==Int 0) /\ @(n0 >=Int 0) </ form > </ config > */
 {
   int s;
   s = 0;
-/*@ invariant <config> 
-              <env> n |-> ?n s |-> (((n0 +Int (-Int ?n)) *Int (n0 +Int ?n +Int 1)) /Int 2) </env>
-              <heap> .Heap </heap> 
-              <form> @(?n >=Int 0) /\ @(n0 >=Int 0) </form> </config> */
+/*@ invariant < config > 
+              < env > n |-> ?n s |-> (((n0 +Int (-Int ?n)) *Int (n0 +Int ?n +Int 1)) /Int 2) </ env >
+              < heap > (.).Map </ heap > 
+              < form > @(?n >=Int 0) /\ @(n0 >=Int 0) </ form > </ config > */
   while (n > 0) {
       s = s + n ;
       n = n - 1 ;
@@ -27,9 +28,7 @@ void main()
 /*@ pre < config > < env > (.).Map </ env > < heap > (.).Map </ heap > < form > TrueFormula </ form > </ config > */
 /*@ post < config > < env > ?rho </ env > < heap > ?H </ heap > < form > TrueFormula </ form > </ config > */
 {
-  int n;
-  n = 5;
-  int summ;
-  summ = sum(n);
+  int s;
+  s = sum(5);
 }
 
