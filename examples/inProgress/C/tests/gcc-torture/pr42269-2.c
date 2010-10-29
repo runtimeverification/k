25b99fab129e5ba5bd97afba0e9f86b494401125
Chucky Ellison <cmelliso@gmail.com>
2010-10-29 00:25:07 +0000

    tons of new passing gcc tests :)
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@1531 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: 8d8c65738ad65819407139a09c9aa6243420d5ca

---
+++ examples/inProgress/C/tests/gcc-torture/pr42269-2.c
@@ -0,0 +1,16 @@
+#include "fsl-header.h"
+/* Make sure that language + abi extensions in passing S interoperate.  */
+
+static long long __attribute__((noinline))
+foo (unsigned short s)
+{
+  return (short) s;
+}
+
+unsigned short s = 0xFFFF;
+
+int
+main (void)
+{
+  return foo (s) + 1 != 0;
+}
