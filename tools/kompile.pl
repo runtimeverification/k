fc29cca07288e46893b9d49d0c9211115a0d0707
Andrei Arusoai <Andrei.Arusoaie@gmail.com>
2010-11-03 19:24:37 +0000

    dots replacement suspended temporary
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@1563 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: 49cb74d820688e6bdc8ea5aaa6501d7c3f31c0e3

--- tools/kompile.pl
+++ tools/kompile.pl
@@ -1086,7 +1086,7 @@ sub maudify_file {
     local $/=undef; open FILE,"<",$file or die "Cannot open $file\n"; local $_ = <FILE>; close FILE;
 
 # Replacinf dots    
-    $_ = replace_dots($_);
+    # $_ = replace_dots($_);
 
 # Getting rid of comments, maintaining the line numbers of the remaining code
     s/($comment)/
