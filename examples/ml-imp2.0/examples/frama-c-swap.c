b31ea1f26a44f57fc58d76ee0f41c63fafd7b66b
Elena Naum <naum.elena@gmail.com>
2010-11-11 19:15:04 +0000

    git-svn-id: http://k-framework.googlecode.com/svn/trunk@1633 82a86efa-2583-11df-8ea0-49ba94de29ab
    Former-commit-id: ea2334d5dfdfbea6b66ff6f45fa106bd9905ee52

--- examples/ml-imp2.0/examples/frama-c-swap.c
+++ examples/ml-imp2.0/examples/frama-c-swap.c
@@ -1,9 +1,20 @@
-#include >stdlib.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 struct nodeList {
   int val;
   struct nodeList *next;
 };
+
+struct nodeList* swap(struct nodeList* x)
+{
+  struct nodeList* p;
+  p = x;
+  x = x->next;
+  p->next = x->next;
+  x->next = p;
+  return x;
+}
 
 struct nodeList* create(int n)
 {
@@ -21,44 +32,87 @@ struct nodeList* create(int n)
   return x;
 }
 
-struct nodeList* swap(struct nodeList* x)
-/*@ pre < config > 
-        < env > x |-> ?x </ env > 
-        < heap > list(?x)([?v1] @ [?v2] @ A) H </ heap > 
-        < form > ~(?x === 0) </ form > C </ config > */
-/*@ post < config > 
-         < env > ?rho </ env > 
-         < heap > list(?x)([?v2] @ [?v1] @ A) H </ heap > 
-         < form > returns ?x </ form > C </ config > */
+
+void destroy(struct nodeList* x)
 {
-  struct nodeList* p;
-    p = x;
-    x = x->next;
-    p->next = x->next;
-    x->next = p;
+  struct nodeList *y;
+  while(x != 0)
+  {
+    y = x->next;
+    free(x);
+    x = y;
+  }
+}
+
+struct nodeList* print(struct nodeList* x)
+/*@ pre < config > 
+             < env > x |-> x0 </ env > 
+             < heap > list(x0)(A) H </ heap > 
+             < form > TrueFormula </ form > C </ config > */
+/*@ post < config > 
+             < env > ?rho </ env > 
+             < heap > list(x0)(A) H </ heap > 
+             < form > returns x0 </ form > C </ config > */
+{
+  struct nodeList* smth;
+  smth = x;
+/*@ invariant < config > 
+             < env > x |-> x0  smth |-> ?s </ env > 
+             < heap > lseg(x0,?s)(?A) list(?s)(?A') H </ heap > 
+             < form > A === ?A @ ?A' </ form > C </ config > */
+  while(smth != 0)
+  {
+    printf("%d ", smth->val);
+    smth = smth->next;
+  }
+  printf("\n");
   return x;
 }
 
 /*@ verify */
 int main()
-/*@ pre < config > < env > (.).Map </ env > < heap > (.).Map </ heap > < form > TrueFormula </ form > </ config > */
-/*@ post < config > < env > ?rho </ env > < heap > ?H </ heap > < form > TrueFormula </ form > </ config > */
 {
   struct nodeList *x;
-  x = create(4);
-/*@ assert < config > 
-        < env > x |-> ?x y |-> ?x </ env > 
-        < heap > list(?x)([1,2,3,4]) </ heap > 
-        < form > ~(?x === 0) </ form > </ config > */
+  struct nodeList *y;
+  x = create(5);
+  // /*@ assert < config > 
+             // < env > x |-> ?x  y |-> ?x </ env > 
+             // < heap > list(?x)([1, 2, 3, 4, 5]) </ heap > 
+             // < form > TrueFormula </ form > </ config > */
+  print(x);
   x = swap(x);
+  print(x);
+  /*@ assert < config >
+             < env > x |-> ?x  y |-> ?y </ env >
+             < heap > list(?x)([2, 1, 3, 4, 5]) </ heap >
+             < form > TrueFormula </ form > </ config > */
+  destroy(x);
+  
+  x = create(5);
+  // /*@ assert < config > 
+             // < env > x |-> ?x  y |-> ?x </ env > 
+             // < heap > list(?x)([!v1] @ [!v2] @ !A) </ heap > 
+             // < form > TrueFormula </ form > </ config > */
+  print(x);
+  x = swap(x);
+  print(x);
+  /*@ assert < config >
+             < env > x |-> ?x  y |-> ?y </ env >
+             < heap > list(?x)([!v2] @ [!v1] @ !A) </ heap >
+             < form > TrueFormula </ form > </ config > */
+  destroy(x);
+  
   return 0;
 }
 
 
-/*@ var ?x ?v1 ?v2 : ?Int */
-/*@ var A : FreeSeq */
-/*@ var ?A : ?Seq */
-/*@ var !A : !Seq */
+/*@ var ?x ?y ?p ?i ?v ?s : ?Int */
+/*@ var x0 : FreeInt */
+/*@ var !v1 !v2 : !Int */
+/*@ var ?B ?C ?A1 ?A2 ?A ?A' : ?Seq */
+/*@ var !A !A1 !A2 : !Seq */
+/*@ var A B : FreeSeq */
 /*@ var ?rho ?H : ?MapItem */
+/*@ var !rho !H : !MapItem */
 /*@ var H : FreeMapItem */
 /*@ var C : FreeBagItem */
