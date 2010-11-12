f203ba21724f0bb7cd44e8c0d7cb215dee1f1329
Andrei Ștefănescu <andreistef@gmail.com>
2010-11-12 04:51:59 +0000

    ml: bubble-sort-list.c commit problem?
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@1654 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: b9c9234c91ee04d94acaea1d027351f3474a8b00

--- examples/ml-imp2.0/examples/bubble-sort-list.c
+++ examples/ml-imp2.0/examples/bubble-sort-list.c
@@ -6,6 +6,74 @@ struct nodeList {
   struct nodeList *next;
 };
 
+
+struct nodeList* bubble(struct nodeList* x)
+/*@ pre < config >
+        < env > x |-> x0 </ env >
+        < heap > list(x0)(L) H </ heap >
+        < form > TrueFormula </ form > C 
+        </ config > */
+/*@ post < config >
+         < env > ?rho </ env >
+         < heap > list(x0)(?L) H </ heap >
+         < form > returns x0 /\ (seq2mset(L) === seq2mset(?L)) /\ isSorted(?L) </ form > C 
+         </ config > */
+{
+  int change;
+  int tmp;
+  struct nodeList* p;
+  struct nodeList* r;
+  if (x != 0)
+  {
+    change = 1;
+    /*@ invariant < config >
+                  < env >
+                    x |-> x0 p |-> ?p r |-> ?r change |-> ?c tmp |-> ?t
+                  </ env >
+                  < heap > list(x0)(?L) H </ heap >
+                  < form >
+                    seq2mset(L) === seq2mset(?L) /\
+                    (?c === 0 /\ isSorted(?L) \/ ?c === 1)
+                  </ form > C </ config > */
+    while(change)
+    {
+      change = 0;
+      p = x;
+      r = x->next;
+      /*@ invariant < config >
+                      < env >
+                        x |-> x0 p |-> ?p r |-> ?r change |-> ?c tmp |-> ?t
+                      </ env >
+                      < heap >
+                        lseg(x0,?p)(?L1) 
+                        ?p |-> ?v : (nodeList . val)  
+                        (?p +Int 1) |-> ?r : (nodeList . next)
+                        list(?r)(?L2)
+                        H
+                      </ heap >
+                      < form >
+                        seq2mset(L) === seq2mset(?L1 @ [?v] @ ?L2) /\
+                        (?c === 0 /\ isSorted(?L1 @ [?v]) \/ ?c === 1)
+                      </ form > C </ config > */
+      while (r != 0)
+      {
+        if (p->val > r->val)
+        {
+          change = 1;
+          tmp = p->val;
+          p->val = r->val;
+          r->val = tmp;
+          /* assert (.).Bag */
+        }
+        p = r;
+        r = r->next;
+      }
+    }
+  }
+  return x;
+}
+
+
 struct nodeList* create(int n)
 {
   struct nodeList *x;
@@ -22,93 +90,6 @@ struct nodeList* create(int n)
   return x;
 }
 
-struct nodeList* bubble(struct nodeList* x)
-/*@ pre < config >
-        < env > x |-> x0 </ env >
-        < heap > list(x0)(L) H </ heap >
-        < form > ~(x0 === 0) </ form > C 
-        </ config > */
-/*@ post < config >
-         < env > ?rho </ env >
-         < heap > list(x0)(?L) H </ heap >
-         < form > (seq2mset(L) === seq2mset(?L)) /\ isSorted(?L) </ form > C 
-         </ config > */
-{
-  int change;
-  int tmp;
-  struct nodeList* p;
-  struct nodeList* r;
-  if (x != 0)
-  {
-    change = 1;
-/*@ invariant < config >
-        < env > x |-> x0 p |-> ?p r |-> ?r change |-> ?c tmp |-> ?t </ env >
-        < heap > list(x0)(?L) H </ heap >
-        < form >  ~(x0 === 0) /\
-                  (seq2mset(L) === seq2mset(?L)) /\
-                  (?c === 0 /\ isSorted(?L) \/ ?c === 1)
-        </ form > C 
-        </ config > */
-    while(change == 1)
-    {
-      change = 0;
-      p = x;
-      r = x->next;
-      
-/*@ invariant < config >
-        < env > x |-> x0 p |-> ?p r |-> ?r change |-> ?c tmp |-> ?t </ env >
-        < heap > lseg(x0,?p)(?L1) 
-                 ?p |-> ?v : (nodeList . val)  
-                 (?p +Int 1) |-> ?r : (nodeList . next)
-                 list(?r)(?L2)
-                 H </ heap >
-        < form >  ~(x0 === 0) /\
-                  (seq2mset(L) === seq2mset(?L)) /\
-                  ?L === (?L1 @ [?v] @ ?L2) /\
-                  (?c === 0 /\ isSorted(?L1 @ [?v]) \/ ?c === 1)
-        </ form > C 
-        </ config > */
-      while (r != 0)
-      {
-        if (p->val > r->val)
-        {
-          change = 1;
-          tmp = p->val;
-          p->val = r->val;
-          r->val = tmp;
-        }
-        p = r;
-        r = r->next;
-      }
-    }
-  }
-  return x;
-}
-
-struct nodeList* print(struct nodeList* x)
-/*@ pre < config > 
-             < env > x |-> x0 </ env > 
-             < heap > list(x0)(A) H </ heap > 
-             < form > TrueFormula </ form > C </ config > */
-/*@ post < config > 
-             < env > ?rho </ env > 
-             < heap > list(x0)(A) H </ heap > 
-             < form > returns x0 </ form > C </ config > */
-{
-  struct nodeList* smth;
-  smth = x;
-/*@ invariant < config > 
-             < env > x |-> x0  smth |-> ?s </ env > 
-             < heap > lseg(x0,?s)(?A) list(?s)(?A') H </ heap > 
-             < form > A === ?A @ ?A' </ form > C </ config > */
-  while(smth != 0)
-  {
-    printf("%d ", smth->val);
-    smth = smth->next;
-  }
-  printf("\n");
-  return x;
-}
 
 void destroy(struct nodeList* x)
 {
@@ -121,21 +102,18 @@ void destroy(struct nodeList* x)
   }
 }
 
+
 /*@ verify */
 int main()
 {
   struct nodeList *x;
-  struct nodeList *y;
   x = create(5);
   /*@ assert < config >
-             < env > x |-> ?x  y |-> ?y </ env >
+             < env > x |-> ?x </ env >
              < heap > list(?x)([1,2,3,4,5]) </ heap >
              < form > TrueFormula </ form > </ config > */
-  x->val = 10;
-  print(x);
   x = bubble(x);
-  print(x);
-  destroy(x);
+  //destroy(x);
   return 0;
 }
 
@@ -144,7 +122,6 @@ int main()
 /*@ var x0 : FreeInt */
 /*@ var ?L ?L1 ?L2 : ?Seq */
 /*@ var L : FreeSeq */
-/*@ var ?rho ?H : ?MapItem */
-/*@ var !rho !H : !MapItem */
+/*@ var ?rho : ?MapItem */
 /*@ var H : FreeMapItem */
 /*@ var C : FreeBagItem */
