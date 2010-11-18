cbc8ee391916f2a66f45e6094fcc1b79edf3d5b5
Elena Naum <naum.elena@gmail.com>
2010-11-18 01:50:36 +0000

    int *p; raises problems, working on fixing them
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@1701 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: d041482e630840e254e94259d915fb09e7c7b353

--- examples/ml-imp2.0/examples/enqueue.c
+++ examples/ml-imp2.0/examples/enqueue.c
@@ -15,7 +15,7 @@ struct nodeQueue* enqueue(struct nodeQueue *x, int val)
 /*@ pre   < config > 
           < env > x |-> x0 val |-> val0 </ env > 
           < heap > queue(x0)(A) H </ heap >
-          < form > TrueFormula </ form > C </ config > */
+          < form > ~(x0 === 0) </ form > C </ config > */
 /*@ post  < config > 
           < env > ?rho </ env > 
           < heap > queue(x0)(A @ [val0]) H </ heap >
@@ -24,32 +24,39 @@ struct nodeQueue* enqueue(struct nodeQueue *x, int val)
   struct nodeList* n;
   n = (struct nodeList*)malloc(sizeof(struct nodeList));
   n->val = val;
-  n->next = x;
-  
-  if (x != 0)
+  n->next = 0;
+  x->tail = x->tail;
+  x->tail->next = x->tail->next;
+
+  if (x->head != 0)
   {
-    if (x->head != 0)
-    {
-      x->tail->next = n ;
-    }
-    else 
-    {
-      x->head = n ;
-    }
-    x->tail = n;
+    x->tail->next = n ;
   }
+  else 
+  {
+    x->head = n ;
+  }
+  x->tail = n;
+
   return x;
 }
 
 int main()
 {
-  struct nodeQueue *x;
-  struct nodeList *hd;
-  struct nodeList *tl;
-  x = (struct nodeQueue*)malloc(sizeof(struct nodeQueue));
-  hd = (struct nodeList*)malloc(sizeof(struct nodeList));
-  tl = (struct nodeList*)malloc(sizeof(struct nodeList));
-  
+  struct nodeQueue* x;
+	struct nodeList *f;
+	struct nodeList* l;
+
+	f = (struct nodeList*)malloc(sizeof(struct nodeList));
+	l = (struct nodeList*)malloc(sizeof(struct nodeList));
+	x = (struct nodeQueue*)malloc(sizeof(struct nodeQueue));
+	f->val = 5;
+	f->next = l;
+	l->val=6;
+	l->next=0;
+	x->head=f;
+	x->tail=l;
+	x = enqueue(x,10);
   return 0;
 }
 
