42c10b17bc6cdca41a41c2b4b26c110eee723be7
Michael Ilseman <michael.ilseman@gmail.com>
2010-12-06 22:04:21 +0000

    case-t
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@1844 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: ab79de6892c18a189a5f78386aca90be8cb9bedb

--- examples/inProgress/kaskell/k-definition/tests/case-t.hs
+++ examples/inProgress/kaskell/k-definition/tests/case-t.hs
@@ -2,12 +2,6 @@ module CaseT where
   test1lhs = case () of { () | p <- e0 -> e; _ -> e' }
   test1rhs = case e0 of { p -> e; _ -> e' }
 
-
 {-
-  Parse as
-
-
-
-  After transformation
-
--}
+  error: Pattern Guards not enabled
+ -}
