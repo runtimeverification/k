6637c1fed1152d8586887a476c4beb2dc106c6ff
Andrei Ștefănescu <andreistef@gmail.com>
2011-02-16 01:37:13 +0000

    ml: io example updated
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@2239 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: c6ad687608e335de1ec53882911d8547a2cf927c

--- examples/matchC/demo/io3.c
+++ examples/matchC/demo/io3.c
@@ -8,14 +8,11 @@ struct listNode {
 };
 
 
-void readWrite()
-//@ pre  <in> [n0] @ A @ I </in><out> O </out> /\ len(A) = n0
+void readWrite(int n)
+//@ pre  <in> A @ I </in><out> O </out> /\ len(A) = n
 //@ post <in> I </in><out> O @ A </out>
 {
   int t;
-  int n;
-
-  scanf("%d", &n);
 
   /*@ invariant <in> ?B @ I </in><out> O @ ?A </out>
                 /\ n >= 0 /\ len(?B) = n /\ A = ?A @ ?B */
@@ -27,21 +24,18 @@ void readWrite()
 }
 
 
-void readWriteBuffer()
-//@ pre  <in> [n0] @ A @ I </in><out> O </out> /\ len(A) = n0
+void readWriteBuffer(int n)
+//@ pre  <in> A @ I </in><out> O </out> /\ len(A) = n
 //@ post <in> I </in><out> O @ rev(A) </out>
 {
   int i;
-  int n;
   struct listNode *x;
   struct listNode *y;
-
-  scanf("%d", &n);
 
   i = 0;
   x = 0;
   /*@ invariant <in> ?B @ I </in><heap> list(x)(?A), !H </heap>
-                /\ n = n0 /\ i <= n /\ len(?B) = n - i /\ A = rev(?A) @ ?B */
+                /\ i <= n /\ len(?B) = n - i /\ A = rev(?A) @ ?B */
   while (i < n) {
     y = x;
     x = (struct listNode*) malloc(sizeof(struct listNode));
@@ -52,25 +46,28 @@ void readWriteBuffer()
 
   i = 0;
   /*@ invariant <out> O @ ?A </out><heap> list(x)(?B), !H </heap>
-                /\ n = n0 /\ i <= n /\ len(?B) = n - i /\ A = rev(?A @ ?B) */
-  while (i < n) {
+                /\ A = rev(?A @ ?B) */
+  while (x) {
     y = x->next;
     printf("%d ",x->val);
     free(x);
     x = y;
-    i += 1;
   }
 }
 
 
 int main()
 {
+  int n;
   /*@ assume <in> [5, 1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 10] </in>
              <out> epsilon </out> */
-  readWrite();
+
+  scanf("%d", &n);
+  readWrite(n);
   //@ assert <in> [5, 6, 7, 8, 9, 10] </in><out> [1, 2, 3, 4, 5] </out>
 
-  readWriteBuffer();
+  scanf("%d", &n);
+  readWriteBuffer(n);
   //@ assert <in> epsilon </in><out> [1, 2, 3, 4, 5, 10, 9, 8, 7, 6] </out>
 
   return 0;
