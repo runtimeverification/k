050055ae0c99e3a07793c6386932b821a27f0b1f
Andrei Ștefănescu <andreistef@gmail.com>
2011-02-17 02:54:34 +0000

    ml: demo almost final
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@2256 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: 6186b56fbde530175cd9deac7bfb9971876d5aa2

---
+++ examples/matchC/bin/maude_runner.py
@@ -0,0 +1,62 @@
+import sys
+import subprocess
+import time
+
+### 
+maude_output_prefix = "maude-output"
+maude_timer_prefix = "-timer"
+maude_rewrites_prefix = "-rewrites"
+tmp_out_filename=".tmp_out_file"
+
+black_color  = "\033[1;30m"
+red_color    = "\033[1;31m"
+green_color  = "\033[1;32m"
+yellow_color = "\033[1;33m"
+blue_color   = "\033[1;34m"
+purple_color = "\033[1;35m"
+cyan_color   = "\033[1;36m"
+white_color  = "\033[1;37m"
+no_color     = "\033[0m"
+###
+
+
+if len(sys.argv) != 2: sys.exit("maude_runner: invalid number of arguments")
+
+tmp_out_file=open(".tmp_out_file", "w")
+
+maude_args = "unbuffer maude -no-banner -no-wrap -no-ansi-color " + sys.argv[1]
+maude = subprocess.Popen(maude_args, shell=True, stdout=subprocess.PIPE)
+
+
+print "Loading Maude...",
+start = time.time()
+while True:
+  maude_output_line = maude.stdout.readline()
+  if maude_output_line.startswith("Bye"): break
+
+  if maude_output_line.startswith(maude_output_prefix):
+    tmp_out_file.write(">>>" + maude_output_line)
+    maude_output_line = maude_output_line[len(maude_output_prefix):]
+    maude_output_line = maude_output_line.rstrip("\n")
+
+    tmp_out_file.write(">>>" + maude_output_line)
+    if maude_output_line.startswith(maude_timer_prefix):
+      maude_output_line = maude_output_line[len(maude_timer_prefix):]
+      end = time.time()
+      elapsed = yellow_color + "%.3f" % round(end - start, 3) + "s" + no_color
+      start = end
+      print maude_output_line + "[" + elapsed + "]"
+    elif maude_output_line.startswith(maude_rewrites_prefix):
+      maude_output_line = maude_output_line[len(maude_rewrites_prefix):]
+      end = time.time()
+      elapsed = yellow_color + "%.3f" % round(end - start, 3) + "s" + no_color
+      print maude_output_line + "[" + elapsed + ",",
+    else:
+      print maude_output_line,
+  else:
+    if maude_output_line.startswith("rewrites"):
+      rewrites = cyan_color + maude_output_line.split()[1] + no_color
+      print rewrites + " rewrites]"
+    else:
+      tmp_out_file.write(maude_output_line)
+
