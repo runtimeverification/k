be256954d54a5ce966a971329e1b06561624e059
Chucky Ellison <cmelliso@gmail.com>
2011-02-23 06:56:23 +0000

    added test for issue 173 and added a timeout to the individal tests.  The timeout is only based on cpu seconds, so if it doesn't hang busy, we might not terminate the tests...
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@2321 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: c2e6da239940b4c997977d3384eac0cc7cb2db0c

---
+++ newTests/issue173/test.m
@@ -0,0 +1,5 @@
+load example-compiled
+rew 'b(.List{K}) . ---@ test2 'f(.List{K})
+rew 'c(.List{K}) . ---@ test3 'f(.List{K})
+rew 'd(.List{K}) . ---@ test4 'f(.List{K})
+rew 'a(.List{K}) . ---@ test1 'f(.List{K})
