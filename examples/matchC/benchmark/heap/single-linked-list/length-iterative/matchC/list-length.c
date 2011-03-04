f280da1f7171692d86c4995e8df634f0c1bb8938
Elena Naum <naum.elena@gmail.com>
2011-03-04 09:53:28 +0000

    length-iterative
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@2385 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: 6c65d84dea3a29519f759ab8c98cd074346ae6b4

--- examples/matchC/benchmark/heap/single-linked-list/length-iterative/matchC/list-length.c
+++ examples/matchC/benchmark/heap/single-linked-list/length-iterative/matchC/list-length.c
@@ -1,54 +1,91 @@
 #include <stdlib.h>
+#include <stdio.h>
 
-struct nodeList {
+
+struct listNode {
   int val;
-  struct nodeList *next;
+  struct listNode *next;
 };
 
-int length(struct nodeList* a)
-/*@ pre < config > 
-        < env > a |-> a0 </ env >
-        < heap > list(a0)(A) H </ heap > 
-        < form > TrueFormula </ form > </ config > */
-/*@ post < config > 
-         < env > ?rho </ env >
-         < heap > ?H </ heap > 
-         < form > ?l === len(A) /\ returns ?l </ form > </ config > */
+int length(struct listNode* x)
+//@ pre  <heap> list(x)(A), H </heap> /\ x = x0
+//@ post <heap> list(x0)(A), H </heap> /\ returns(len(A))
 {
   int l;
-  struct nodeList* x;
-  x = a;
+  
   l = 0;
-/*@ invariant < config > 
-              < env > a |-> a0  x |-> ?x l |-> ?l </ env >
-              < heap > lseg(a0,?x)(?A)  list(?x)(?X) H </ heap >
-              < form > (?A @ ?X) === A /\ ?l === len(?A) </ form >
-              </ config > */
-  while (x != 0) {
-        x = x->next ;
-        l = l + 1 ;
-    }
+  /*@ invariant <heap> lseg(x0,x)(?A1), list(x)(?A2), H </heap>
+                /\ A = ?A1 @ ?A2 /\ l = len(?A1) */
+  while (x) {
+    l += 1;
+    x = x->next ;
+  }
+
   return l;
 }
 
-int main()
-/*@ pre < config > < env > (.).Map </ env > < heap > (.).Map </ heap > < form > TrueFormula </ form > </ config > */
-/*@ post < config > < env > ?rho </ env > < heap > ?H </ heap > < form > TrueFormula </ form > </ config > */
+struct listNode* create(int n)
 {
-  int l;
-  struct nodeList* x;
-  x = (struct nodeList*)malloc(sizeof(struct nodeList));
-  x->val = 5;
-  x->next = 0;
-  l = length(x);
-  printf("%d ", l);
-  printf("\n");
+  struct listNode *x;
+  struct listNode *y;
+  x = 0;
+  while (n)
+  {
+    y = x;
+    x = (struct listNode*)malloc(sizeof(struct listNode));
+    x->val = n;
+    x->next = y;
+    n -= 1;
+  }
+  return x;
+}
+
+void destroy(struct listNode* x)
+//@ pre  <heap> list(x)(?A), H </heap>
+//@ post <heap> H </heap>
+{
+  struct listNode *y;
+
+  //@ invariant <heap> list(x)(?A), H </heap>
+  while(x)
+  {
+    y = x->next;
+    free(x);
+    x = y;
+  }
+}
+
+void print(struct listNode* x)
+//@ pre  <heap>  list(x)(A), H </heap><out> B </out> /\ x = x0
+//@ post <heap> list(x0)(A), H </heap><out> B @ A </out>
+{
+  /*@ invariant <heap> lseg(x0,x)(?A1), list(x)(?A2), H </heap>
+                <out> B @ ?A1 </out> /\ A = ?A1 @ ?A2 */
+  while(x)
+  {
+    printf("%d ",x->val);
+    x = x->next;
+  }
+  printf("\n"); 
+}
+
+
+int main()
+{
+  struct listNode *x;
+  int n;
+
+  x = create(5);
+  //@ assert <heap> list(x)([1, 2, 3, 4, 5]) </heap>
+  n = length(x);
+  //@ assert <heap> list(x)([1, 2, 3, 4, 5]) </heap>
+  destroy(x);
+  //@ assert <heap> . </heap>
   return 0;
 }
 
-/*@ var ?x ?l : ?Int */
-/*@ var a0 : FreeInt */
-/*@ var ?A ?X : ?Seq */
-/*@ var A : FreeSeq */
-/*@ var ?rho ?H : ?MapItem */
-/*@ var H : FreeMapItem */
+
+//@ var n : Int
+//@ var A, B, C : Seq
+//@ var H : MapItem
+
