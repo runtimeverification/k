048bfcd9756f31155bacb2b1b12756b21a7fd5b1
Elena Naum <naum.elena@gmail.com>
2011-03-04 09:23:10 +0000

    append
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@2379 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: 0e7a70dfb87abe53eb75f9ef02fc4307ef9cdbda

--- examples/matchC/benchmark/heap/single-linked-list/append/matchC/list-append.c
+++ examples/matchC/benchmark/heap/single-linked-list/append/matchC/list-append.c
@@ -1,154 +1,102 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-struct nodeList {
+
+struct listNode {
   int val;
-  struct nodeList *next;
+  struct listNode *next;
 };
 
-
-struct nodeList* append(struct nodeList *x, struct nodeList *y)  
-/*@ pre  < config > < env > x |-> ?x  y |-> ?y  </ env >
-                    < heap > list(?x)(A) list(?y)(B) H </ heap >
-                    < form > TrueFormula </ form > C </ config > */
-/*@ post < config > < env >  ?rho </ env >
-                    < heap > list(?x)(A @ B) H </ heap > 
-                    < form > returns ?x </ form > C </ config > */
+struct listNode* append(struct listNode *x, struct listNode *y)
+//@ pre  <heap> list(x)(A), list(y)(B), H </heap>
+//@ post <heap> list(?x)(A @ B), H </heap> /\ returns(?x)
 {
-  struct nodeList *p;
-  struct nodeList *i;
-  if (x == 0) x = y;
-  else
+  struct listNode *p;
+  if (x == 0)
+   return y;
+
+  p = x;
+  /*@ invariant <heap> lseg(x, p)(?A1), list(p)(?A2), !H </heap> 
+                /\ A = ?A1 @ ?A2 /\ ~(p = 0) /\ y = !y */
+  while (p->next)
+    p = p->next;
+  p->next = y;
+
+  return x;
+}
+
+struct listNode* create(int n)
+{
+  struct listNode *x;
+  struct listNode *y;
+  x = 0;
+  while (n)
   {
-    p = x ;
-    i = x->next; 
-    /*@ invariant < config > < env > x |-> ?x  i |-> ?i  p |-> ?p  !rho </ env >
-                             < heap >
-                               lseg(?x , ?p)(?A1) 
-                               ?p |-> ?v : (nodeList . val)
-                               (?p +Int 1) |-> ?i :  (nodeList . next)
-                                list(?i)(?A2)  
-                               !H
-                             </ heap > 
-                             < form > (?A1 @ [?v] @ ?A2) === A </ form > 
-                             C </ config > */
-    while (i != 0)
-    {
-        p = i ;
-        i = i->next;
-    }
-    p->next = y ;
+    y = x;
+    x = (struct listNode*)malloc(sizeof(struct listNode));
+    x->val = n;
+    x->next = y;
+    n -= 1;
   }
   return x;
 }
 
+void destroy(struct listNode* x)
+//@ pre  <heap> list(x)(?A), H </heap>
+//@ post <heap> H </heap>
+{
+  struct listNode *y;
+
+  //@ invariant <heap> list(x)(?A), H </heap>
+  while(x)
+  {
+    y = x->next;
+    free(x);
+    x = y;
+  }
+}
+
+
+void print(struct listNode* x)
+//@ pre  <heap>  list(x)(A), H </heap><out> B </out> /\ x = x0
+//@ post <heap> list(x0)(A), H </heap><out> B @ A </out>
+{
+  /*@ invariant <heap> lseg(x0,x)(?A1), list(x)(?A2), H </heap>
+                <out> B @ ?A1 </out> /\ A = ?A1 @ ?A2 */
+  while(x)
+  {
+    printf("%d ",x->val);
+    x = x->next;
+  }
+  printf("\n"); 
+}
+
+
 int main()
 {
-  struct nodeList *x;
-  struct nodeList *y;
-  struct nodeList *z;
-  x = (struct nodeList*)malloc(sizeof(struct nodeList));
-  x->val = 7;
-  x->next = 0;
-  y = (struct nodeList*)malloc(sizeof(struct nodeList));
-  y->val = 6;
-  y->next = x;
-  x = y;
-  y = (struct nodeList*)malloc(sizeof(struct nodeList));
-  y->val = 5;
-  y->next = x;
-  x = y;
-  /*@ assert < config > 
-             < env > x |-> ?x  y |-> ?x  z |-> ?z </ env > 
-             < heap > list(?x)([5, 6, 7]) </ heap > 
-             < form > TrueFormula </ form > </ config > */
-  printf("x: %d %d %d\n",x->val, x->next->val, x->next->next->val);
-  z = (struct nodeList*)malloc(sizeof(struct nodeList));
-  z->val = 5;
-  z->next = 0;
-  y = (struct nodeList*)malloc(sizeof(struct nodeList));
-  y->val = 3;
-  y->next = z;
-  z = y;
-  y = (struct nodeList*)malloc(sizeof(struct nodeList));
-  y->val = 1;
-  y->next = z;
-  z = y;
-  printf("z: %d %d %d\n",z->val, z->next->val, z->next->next->val);
-  /*@ assert < config > 
-             < env > x |-> ?x  z |-> ?z  y |-> ?z </ env > 
-             < heap > list(?x)([5, 6, 7]) list(?z)([1, 3, 5]) </ heap > 
-             < form > TrueFormula </ form > </ config > */
-  x = append(x,z);
-  printf("x: %d %d %d %d %d %d\n",x->val, x->next->val, x->next->next->val, x->next->next->next->val, x->next->next->next->next->val, x->next->next->next->next->next->val);
-  /*@ assert < config > 
-             < env > x |-> ?x  z |-> ?z  y |-> ?z </ env > 
-             < heap > list(?x)([5, 6, 7, 1, 3, 5]) </ heap > 
-             < form > TrueFormula </ form > </ config > */
-  y = x;
-  x = x->next;
-  free(y);
-  y = x;
-  x = x->next;
-  free(y);
-  y = x;
-  x = x->next;
-  free(y);
-  y = x;
-  x = x->next;
-  free(y);
-  y = x;
-  x = x->next;
-  free(y);
-  y = x;
-  x = x->next;
-  free(y);
-  /*@ assert < config > < env > x |-> ?x  y |-> ?y  z |-> ?z </ env >
-                        < heap > (.).Map </ heap >
-                        < form > TrueFormula </ form > </ config > */
-  x = (struct nodeList*)malloc(sizeof(struct nodeList));
-  x->val = 7;
-  x->next = 0;
-  y = (struct nodeList*)malloc(sizeof(struct nodeList));
-  y->val = 6;
-  y->next = x;
-  x = y;
-  y = (struct nodeList*)malloc(sizeof(struct nodeList));
-  y->val = 5;
-  y->next = x;
-  x = y;
-  /*@ assert < config > 
-             < env > x |-> ?x y |-> ?x z |-> ?z </ env > 
-             < heap > list(?x)(!A1) </ heap > 
-             < form > TrueFormula </ form > </ config > */
-  z = (struct nodeList*)malloc(sizeof(struct nodeList));
-  z->val = 5;
-  z->next = 0;
-  y = (struct nodeList*)malloc(sizeof(struct nodeList));
-  y->val = 3;
-  y->next = z;
-  z = y;
-  y = (struct nodeList*)malloc(sizeof(struct nodeList));
-  y->val = 1;
-  y->next = z;
-  z = y;
-  /*@ assert < config > 
-             < env > x |-> ?x z |-> ?z y |-> ?y </ env > 
-             < heap > list(?x)(!A1) list(?z)(!A2) </ heap > 
-             < form > TrueFormula </ form > </ config > */
-  x = append(x,z);
-  /*@ assert < config > 
-             < env > x |-> ?x z |-> ?z y |-> ?y </ env > 
-             < heap > list(?x)(!A1 @ !A2) </ heap > 
-             < form > TrueFormula </ form > </ config > */
+  struct listNode *x;
+  struct listNode *y;
+
+  x = create(4);
+  printf("x: ");
+  print(x);
+  //@ assert <heap> list(x)(!A1) </heap>
+  y = create(3);
+  printf("y: ");
+  print(y);
+  //@ assert <heap> list(x)(!A1), list(y)(!A2) </heap>
+  x = append(x, y);
+  printf("append(x, y): ");
+  print(x);
+  //@ assert <heap> list(x)(!A1 @ !A2) </heap>
+  destroy(x);
+  //@ assert <heap> . </heap>
+  
   return 0;
 }
 
-/*@ var ?x ?y ?p ?i ?v ?z : ?Int */
-/*@ var ?A1 ?A2 : ?Seq */
-/*@ var !A1 !A2 : !Seq */
-/*@ var A B : FreeSeq */
-/*@ var ?rho ?H : ?MapItem */
-/*@ var !rho !H : !MapItem */
-/*@ var H : FreeMapItem */
-/*@ var C : FreeBagItem */
+
+//@ var n : Int
+//@ var A, B, C : Seq
+//@ var H : MapItem
+
