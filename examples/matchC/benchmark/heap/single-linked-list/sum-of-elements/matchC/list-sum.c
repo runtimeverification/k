567320117a92d3b695db6584ee9a404b0429f408
Elena Naum <naum.elena@gmail.com>
2011-03-09 14:40:28 +0000

    update
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@2400 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: 35fd2ce939a751874cf1638512d8bd989817ad17

--- examples/matchC/benchmark/heap/single-linked-list/sum-of-elements/matchC/list-sum.c
+++ examples/matchC/benchmark/heap/single-linked-list/sum-of-elements/matchC/list-sum.c
@@ -1,17 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-struct nodeList {
+
+struct listNode {
   int val;
-  struct nodeList *next;
+  struct listNode *next;
 };
 
-int summ(struct nodeList* a)
+int summ(struct listNode* a)
 //@ pre  <heap> list(a)(A), H </heap> /\ a = a0
 //@ post <heap> list(a0)(A), H </heap> /\ returns(thesum(A))
 {
   int s;
-  struct nodeList* x;
+  struct listNode* x;
   x = a;
   s = 0;
 //@ invariant <heap> lseg(a0,x)(?A), list(x)(?X), H </heap> /\ (?A @ ?X) = A /\ (s = thesum(?A))
@@ -25,20 +26,20 @@ int summ(struct nodeList* a)
 int main()
 {
   int s;
-  struct nodeList* x;
-  struct nodeList* y;
-  x = (struct nodeList*)malloc(sizeof(struct nodeList));
+  struct listNode* x;
+  struct listNode* y;
+  x = (struct listNode*)malloc(sizeof(struct listNode));
   x->val = 5;
   x->next = 0;
-  y = (struct nodeList*)malloc(sizeof(struct nodeList));
+  y = (struct listNode*)malloc(sizeof(struct listNode));
   y->val = 4;
   y->next = x;
   x = y;
-  y = (struct nodeList*)malloc(sizeof(struct nodeList));
+  y = (struct listNode*)malloc(sizeof(struct listNode));
   y->val = 3;
   y->next = x;
   x = y;
-  // s = summ(x);
+  s = summ(x);
   printf("%d\n", s);
   // assert <out> [content] </out>
   return 0;
