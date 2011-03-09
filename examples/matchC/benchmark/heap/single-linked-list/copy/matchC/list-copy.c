48b1cb7947ca63bf66c5b9db999238f88dfa09a8
Elena Naum <naum.elena@gmail.com>
2011-03-09 14:38:43 +0000

    list copy
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@2399 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: 3b4d45a63e4e1e3c415e1dd92e61111e9c30482a

--- examples/matchC/benchmark/heap/single-linked-list/copy/matchC/list-copy.c
+++ examples/matchC/benchmark/heap/single-linked-list/copy/matchC/list-copy.c
@@ -1,62 +1,104 @@
 #include <stdlib.h>
 
 
-struct nodeList {
+struct listNode {
   int val;
-  struct nodeList *next;
+  struct listNode *next;
 };
 
 
-struct nodeList *copy(struct nodeList *x)
-/*@ pre  < config > < env > x |-> x0 </ env >
-                    < heap > list(x0)(A) H </ heap >
-                    < form > TrueFormula </ form > C </ config > */
-/*@ post < config > < env > ?rho </ env >
-                    < heap > list(x0)(A) list(?y)(A) H </ heap >
-                    < form > returns ?y </ form > C </ config > */
+struct listNode *copy(struct listNode *x)
+// pre  <heap> list(x)(A), H </heap> /\ x = x0
+// post <heap> list(x0)(A), list(y)(A), H </heap> /\ returns(y)
 {
-  struct nodeList* y;
-  struct nodeList* iterx;
-  struct nodeList* itery;
-  struct nodeList* newnode;
+  struct listNode* y;
+  struct listNode* ix;
+  struct listNode* iy;
+  struct listNode* newnode;
 
   if (x == 0)
     return 0;
 
-  y = (struct nodeList *)malloc(sizeof(struct nodeList));
+  y = (struct listNode *)malloc(sizeof(struct listNode));
   y->val = x->val;
   y->next = 0;
-  iterx = x->next;
-  itery = y;
-  /*@ invariant < config > < env >
-                             iterx |-> ?ix itery |-> ?iy newnode |-> ?n
-                             x |-> x0 y |-> ?y 
-                           </ env >
-                           < heap >
-                             lseg(x0,?ix)(?A @ [?v]) list(?ix)(?B)
-                             lseg(?y,?iy)(?A)
-                             ?iy |-> ?v : (nodeList . val)
-                             ?iy +Int 1 |-> 0 : (nodeList . next)
-                             H
-                           </ heap >
-                           < form > A === (?A @ [?v] @ ?B) </ form >
-                           C </ config > */
-  while(iterx) {
-    newnode = (struct nodeList *)malloc(sizeof(struct nodeList));
-    newnode->val = iterx->val;
+  ix = x->next;
+  iy = y;
+// invariant <heap> lseg(x0,ix)(?A @ [?v]), list(ix)(?B), lseg(y,iy)(?A), iy |-> ?v : (id("listNode").id("val")), iy +Int 1 |-> 0 : (id("listNode").id("next")), H </heap> /\ A = (?A @ [?v] @ ?B)
+  while(ix) {
+    newnode = (struct listNode *)malloc(sizeof(struct listNode));
+    newnode->val = ix->val;
     newnode->next = 0;
-    itery->next = newnode;
-    iterx = iterx->next;
-    itery = newnode;
+    iy->next = newnode;
+    ix = ix->next;
+    iy = newnode;
   }
 
   return y;
 }
 
-/*@ var ?y ?ix ?iy ?v ?n : ?Int */
-/*@ var x0 : FreeInt */
-/*@ var ?A ?B ?C : ?Seq */
-/*@ var A : FreeSeq */
-/*@ var ?rho : ?MapItem */
-/*@ var H : FreeMapItem */
-/*@ var C : FreeBagItem */
+struct listNode* create(int n)
+{
+  struct listNode *x;
+  struct listNode *y;
+  x = 0;
+  while (n)
+  {
+    y = x;
+    x = (struct listNode*)malloc(sizeof(struct listNode));
+    x->val = n;
+    x->next = y;
+    n -= 1;
+  }
+  return x;
+}
+
+void destroy(struct listNode* x)
+//@ pre  <heap> list(x)(?A), H </heap>
+//@ post <heap> H </heap>
+{
+  struct listNode *y;
+
+  //@ invariant <heap> list(x)(?A), H </heap>
+  while(x)
+  {
+    y = x->next;
+    free(x);
+    x = y;
+  }
+}
+
+
+void print(struct listNode* x)
+//@ pre  <heap>  list(x)(A), H </heap><out> B </out> /\ x = x0
+//@ post <heap> list(x0)(A), H </heap><out> B @ A </out>
+{
+  /*@ invariant <heap> lseg(x0,x)(?A1), list(x)(?A2), H </heap>
+                <out> B @ ?A1 </out> /\ A = ?A1 @ ?A2 */
+  while(x)
+  {
+    printf("%d ",x->val);
+    x = x->next;
+  }
+  printf("\n"); 
+}
+
+
+int main()
+{
+  struct listNode *x;
+  struct listNode *y;
+
+  x = create(5);
+  print(x);
+  y = copy(x);
+  print(y);
+  destroy(x);
+  destroy(y);
+  return 0;
+}
+
+
+//@ var n : Int
+//@ var A, B, C : Seq
+//@ var H : MapItem
