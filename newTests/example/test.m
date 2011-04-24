bb429e95a2eea08830cd689e9587134490ae136a
Chucky Ellison <cmelliso@gmail.com>
2011-04-24 22:32:33 +0000

    fixed example test again
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@2825 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: 284824bb65acfd94e62a15631f6ec29d9c8c8fb7 [formerly 7da6a2be077c94b0ae05ad584282a2d6367516e2]
    Former-commit-id: c952e3b199855b9e6d1d0455e17fdb3bee07d656

--- newTests/example/test.m
+++ newTests/example/test.m
@@ -1,3 +1,3 @@
 load example-compiled
-rew test1(.List{K}) . ---@ test1 'SomeLabel(.List{K})
-rew test2(.List{K}) . ---@ test2 'SomeLabel(.List{K})
+rew 'test1(.List{K}) . ---@ test1 'SomeLabel(.List{K})
+rew 'test2(.List{K}) . ---@ test2 'SomeLabel(.List{K})
