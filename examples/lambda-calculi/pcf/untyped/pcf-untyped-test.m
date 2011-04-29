06245afea1fcdcfe454493ed12545c618eb61fca
David Lazar <david.lazar@gmail.com>
2011-04-29 06:49:05 +0000

    Use getResult in tests.
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@2899 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: c6e3976dc7f4a9b974a46c0a2f3ca255b41611cf [formerly 062747ae863e609317869460b9c695a1640cb9d2]
    Former-commit-id: 7aea3a22df99419eee1e61066efc656ec9982c9b

--- examples/lambda-calculi/pcf/untyped/pcf-untyped-test.m
+++ examples/lambda-calculi/pcf/untyped/pcf-untyped-test.m
@@ -1,50 +1,50 @@
 load pcf-untyped-compiled
 
-rewrite [[ 'pgm001 ]] .
-rewrite [[ 'pgm002 ]] .
-rewrite [[ 'pgm003 ]] .
-rewrite [[ 'pgm004 ]] .
-rewrite [[ 'pgm005 ]] .
-rewrite [[ 'pgm006 ]] .
-rewrite [[ 'pgm007 ]] .
-rewrite [[ 'pgm008 ]] .
-rewrite [[ 'pgm009 ]] .
-rewrite [[ 'pgm010 ]] .
-rewrite [[ 'pgm011 ]] .
---- rewrite [[ 'pgm012 ]] . --- slow
-rewrite [[ 'pgm013 ]] .
-rewrite [[ 'pgm014 ]] .
-rewrite [[ 'pgm015 ]] .
-rewrite [[ 'pgm016 ]] .
-rewrite [[ 'pgm017 ]] .
---- rewrite [[ 'pgm018 ]] . --- slow
-rewrite [[ 'pgm019 ]] .
-rewrite [[ 'pgm020 ]] .
-rewrite [[ 'pgm021 ]] .
-rewrite [[ 'pgm022 ]] .
-rewrite [[ 'pgm023 ]] .
-rewrite [[ 'pgm024 ]] .
-rewrite [[ 'pgm025 ]] .
-rewrite [[ 'pgm026 ]] .
-rewrite [[ 'pgm027 ]] .
-rewrite [[ 'pgm028 ]] .
-rewrite [[ 'pgm029 ]] .
-rewrite [[ 'pgm030 ]] .
-rewrite [[ 'pgm031 ]] .
-rewrite [[ 'pgm032 ]] .
-rewrite [[ 'pgm033 ]] .
-rewrite [[ 'pgm034 ]] .
-rewrite [[ 'pgm035 ]] .
-rewrite [[ 'pgm036 ]] .
-rewrite [[ 'pgm037 ]] .
-rewrite [[ 'pgm038 ]] .
-rewrite [[ 'pgm039 ]] .
-rewrite [[ 'pgm040 ]] .
-rewrite [[ 'pgm041 ]] .
-rewrite [[ 'pgm042 ]] .
---- rewrite [[ 'pgm043 ]] . --- should not terminate
-rewrite [[ 'pgm044 ]] .
-rewrite [[ 'pgm045 ]] .
-rewrite [[ 'pgm046 ]] .
-rewrite [[ 'pgm047 ]] .
---- rewrite [[ 'pgm048 ]] . --- should not terminate
+rewrite getResult [[ 'pgm001 ]] .
+rewrite getResult [[ 'pgm002 ]] .
+rewrite getResult [[ 'pgm003 ]] .
+rewrite getResult [[ 'pgm004 ]] .
+rewrite getResult [[ 'pgm005 ]] .
+rewrite getResult [[ 'pgm006 ]] .
+rewrite getResult [[ 'pgm007 ]] .
+rewrite getResult [[ 'pgm008 ]] .
+rewrite getResult [[ 'pgm009 ]] .
+rewrite getResult [[ 'pgm010 ]] .
+rewrite getResult [[ 'pgm011 ]] .
+--- rewrite getResult [[ 'pgm012 ]] . --- slow
+rewrite getResult [[ 'pgm013 ]] .
+rewrite getResult [[ 'pgm014 ]] .
+rewrite getResult [[ 'pgm015 ]] .
+rewrite getResult [[ 'pgm016 ]] .
+rewrite getResult [[ 'pgm017 ]] .
+--- rewrite getResult [[ 'pgm018 ]] . --- slow
+rewrite getResult [[ 'pgm019 ]] .
+rewrite getResult [[ 'pgm020 ]] .
+rewrite getResult [[ 'pgm021 ]] .
+rewrite getResult [[ 'pgm022 ]] .
+rewrite getResult [[ 'pgm023 ]] .
+rewrite getResult [[ 'pgm024 ]] .
+rewrite getResult [[ 'pgm025 ]] .
+rewrite getResult [[ 'pgm026 ]] .
+rewrite getResult [[ 'pgm027 ]] .
+rewrite getResult [[ 'pgm028 ]] .
+rewrite getResult [[ 'pgm029 ]] .
+rewrite getResult [[ 'pgm030 ]] .
+rewrite getResult [[ 'pgm031 ]] .
+rewrite getResult [[ 'pgm032 ]] .
+rewrite getResult [[ 'pgm033 ]] .
+rewrite getResult [[ 'pgm034 ]] .
+rewrite getResult [[ 'pgm035 ]] .
+rewrite getResult [[ 'pgm036 ]] .
+rewrite getResult [[ 'pgm037 ]] .
+rewrite getResult [[ 'pgm038 ]] .
+rewrite getResult [[ 'pgm039 ]] .
+rewrite getResult [[ 'pgm040 ]] .
+rewrite getResult [[ 'pgm041 ]] .
+rewrite getResult [[ 'pgm042 ]] .
+--- rewrite getResult [[ 'pgm043 ]] . --- should not terminate
+rewrite getResult [[ 'pgm044 ]] .
+rewrite getResult [[ 'pgm045 ]] .
+rewrite getResult [[ 'pgm046 ]] .
+rewrite getResult [[ 'pgm047 ]] .
+--- rewrite getResult [[ 'pgm048 ]] . --- should not terminate
