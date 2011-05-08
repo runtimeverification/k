b5f536ba93cad67cb572191140c8bf6e6d29552e
David Lazar <david.lazar@gmail.com>
2011-05-08 09:19:27 +0000

    Add tests of parsing infix operators.
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@2997 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: 2561dd2fda51f4b05a589bfc075dbd5506637e00 [formerly 216b0e7b9a1ee8267ac266fe409f5b6101fdc25b]
    Former-commit-id: 0ec95299b0cad7c538f55f37b80f3422fcd440a5

--- examples/inProgress/kaskell/tools/language-k/tests/HUnit.hs
+++ examples/inProgress/kaskell/tools/language-k/tests/HUnit.hs
@@ -23,6 +23,9 @@ tests = TestList
     [ test001 `shouldParseToS` test001R
     , test002 `shouldParseToS` test002R
     , test003 `shouldParseToS` test003R
+    , test004 `shouldParseToS` test004R
+    , test005 `shouldParseToS` test005R
+    , test006 `shouldParseToS` test006R
     -- Attoparsec tests
     , test001 `shouldParseToB` test001R
     , test002 `shouldParseToB` test002R
@@ -54,6 +57,9 @@ assertAttoparsec input expected = do
 test001, test001R :: (IsString a) => a
 test002, test002R :: (IsString a) => a
 test003, test003R :: (IsString a) => a
+test004, test004R :: (IsString a) => a
+test005, test005R :: (IsString a) => a
+test006, test006R :: (IsString a) => a
 
 test001  = "'NegApp_('Lit_('Int_(Int 42(.List{K}))))"
 test001R = "NegApp (Lit (Int (42)))"
@@ -63,3 +69,12 @@ test002R = "Lit (String (\"hello\"))"
 
 test003  = "'Let__('BDecls_('`(:`)__('FunBind_('`(:`)__('Match______('SrcLoc___(String \"unknown.hs\"(.List{K}),,Int 0(.List{K}),,Int 0(.List{K})),,'Ident_(String \"ok0\"(.List{K})),,'`(:`)__('PVar_('Ident_(String \"x\"(.List{K}))),,'`[`](.List{K})),,'Nothing(.List{K}),,'UnGuardedRhs_('Var_('UnQual_('Ident_(String \"foo\"(.List{K}))))),,'BDecls_('`[`](.List{K}))),,'`(:`)__('Match______('SrcLoc___(String \"unknown.hs\"(.List{K}),,Int 0(.List{K}),,Int 0(.List{K})),,'Ident_(String \"ok0\"(.List{K})),,'`(:`)__('PWildCard(.List{K}),,'`[`](.List{K})),,'Nothing(.List{K}),,'UnGuardedRhs_('App__('Var_('UnQual_('Ident_(String \"fail\"(.List{K})))),,'Lit_('String_(String \"pattern fail\"(.List{K}))))),,'BDecls_('`[`](.List{K}))),,'`[`](.List{K})))),,'`[`](.List{K}))),,'InfixApp___('Var_('UnQual_('Ident_(String \"xs\"(.List{K})))),,'QVarOp_('UnQual_('Symbol_(String \">>=\"(.List{K})))),,'Var_('UnQual_('Ident_(String \"ok0\"(.List{K}))))))"
 test003R = "Let (BDecls ((:) (FunBind ((:) (Match (SrcLoc (\"unknown.hs\") (0) (0)) (Ident (\"ok0\")) ((:) (PVar (Ident (\"x\"))) ([])) (Nothing) (UnGuardedRhs (Var (UnQual (Ident (\"foo\"))))) (BDecls ([]))) ((:) (Match (SrcLoc (\"unknown.hs\") (0) (0)) (Ident (\"ok0\")) ((:) (PWildCard) ([])) (Nothing) (UnGuardedRhs (App (Var (UnQual (Ident (\"fail\")))) (Lit (String (\"pattern fail\"))))) (BDecls ([]))) ([])))) ([]))) (InfixApp (Var (UnQual (Ident (\"xs\")))) (QVarOp (UnQual (Symbol (\">>=\")))) (Var (UnQual (Ident (\"ok0\")))))"
+
+test004  = "'forall_._(Id t(.List{K}),,'_->_(Id t(.List{K}),,'_->_(Id t(.List{K}),,Id t(.List{K}))))"
+test004R = "forall t . (t -> (t -> t))"
+
+test005  = "'forall_._(Id t1(.List{K}),,'forall_._(Id t2(.List{K}),,'_->_('_->_(Id t1(.List{K}),,Id t2(.List{K})),,'forall_._(Id t3(.List{K}),,'_->_('_->_(Id t2(.List{K}),,Id t3(.List{K})),,'_->_(Id t1(.List{K}),,Id t3(.List{K})))))))"
+test005R = "forall t1 . (forall t2 . ((t1 -> t2) -> (forall t3 . ((t2 -> t3) -> (t1 -> t3)))))"
+
+test006  = "'_->_('int(.List{K}),,'int(.List{K}))"
+test006R = "int -> int"
