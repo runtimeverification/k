e79e43b8c8618b61a91a3934455a50cff5ebad92
Traian Șerbănuță <Traian.Serbanuta@gmail.com>
2011-05-14 13:18:18 +0000

    sepaated escargot to reduce running time
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@3220 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: 48f18d781554f279fe9c7dc9b861b3ebdfaab356 [formerly 076c42f8ec03b1de07546580f7a97ec8b0b75719]
    Former-commit-id: b50cdf521fa930566232ca34d2f87fe63545e903

---
+++ examples/algorithms/sudoku/2-with-counters/sudoku-test-escargot.m
@@ -0,0 +1,7 @@
+load sudoku-compiled.maude
+---rew easy .
+---rew escargot1 .
+---q
+search [1] escargot1 =>! < T > Bs:Bag  </ T > .
+--- next takes about 4 minutes
+search [1] escargot2 =>! < T > Bs:Bag  </ T > .
--- examples/algorithms/sudoku/2-with-counters/sudoku-test.m
+++ examples/algorithms/sudoku/2-with-counters/sudoku-test.m
@@ -3,6 +3,6 @@ load sudoku-compiled.maude
 ---rew escargot1 .
 ---q
 search [1] easy      =>! < T > Bs:Bag  </ T > .
-search [1] escargot1 =>! < T > Bs:Bag  </ T > .
+---search [1] escargot1 =>! < T > Bs:Bag  </ T > .
 --- next takes about 4 minutes
-search [1] escargot2 =>! < T > Bs:Bag  </ T > .
+---search [1] escargot2 =>! < T > Bs:Bag  </ T > .
