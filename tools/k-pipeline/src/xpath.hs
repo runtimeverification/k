46aa207541fdec162bc4815f6024a49bd8b77d08
David Lazar <david.lazar@gmail.com>
2011-05-18 08:13:13 +0000

    Add new xpath utility.
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@3334 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: 078cadae8c293ff00f89362e872165bbc736c7ae [formerly b9b4fcab22894d2167f8e34d47fcb9717cf1389d]
    Former-commit-id: 1a9a83317720bbdcc87165f2d14f5d973e8fb3ce

---
+++ tools/k-pipeline/src/xpath.hs
@@ -0,0 +1,28 @@
+-- Simple XPath CLI utility.
+module Main where
+
+import Control.Monad (when)
+import System.Environment
+import System.Exit
+import Text.XML.HXT.Core hiding (when)
+import Text.XML.HXT.XPath
+
+main :: IO ()
+main = do
+    args <- getArgs
+
+    when (null args) $ do
+        putStrLn "One argument required (the xpath query)."
+        exitFailure
+
+    let query = head args
+
+    rs <- runX $
+        readDocument [] ""
+        >>>
+        getXPathTrees query
+        >>>
+        -- Is this the right thing to do?
+        xshow this
+
+    mapM_ putStrLn rs
