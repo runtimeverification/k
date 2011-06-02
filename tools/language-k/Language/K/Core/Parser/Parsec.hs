d6026c2f9b192e9806d86e12b4f97dea97f72250
David Lazar <david.lazar@gmail.com>
2011-06-02 06:33:04 +0000

    Rewrite the K Core Parsec parser to use the new syntax description in language-k.
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@3439 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: b622b9003fb7325d34407fdb34e5fd962d30ff32 [formerly 120f090d9f76c7e6122c364155fc80cd9dbdaaa7]
    Former-commit-id: 36dfbf8b71f4a10f4d92ea12f2ed467a2aa38995

--- tools/language-k/Language/K/Core/Parser/Parsec.hs
+++ tools/language-k/Language/K/Core/Parser/Parsec.hs
@@ -9,56 +9,42 @@
 -- Stability   :  experimental
 -- Portability :  unknown
 --
--- A small, incomplete Parsec parser for low-level K.
+-- A Parsec parser for K Core.
 -----------------------------------------------------------------------------
 
 module Language.K.Core.Parser.Parsec where
 
 import Control.Applicative ((<$>))
-import Data.Char (isAlphaNum)
 import Text.Parsec
 
+import Language.K.Core.Syntax
 import Internal.Lexer
 
-parseKterm :: String -> Either ParseError String
-parseKterm = parse (spaces >> kterm) ""
+parseK :: String -> Either ParseError K
+parseK = parse kterm ""
 
 -- | Reduce clutter while still keeping the types generic.
 type Parser a = (Stream s m Char) => ParsecT s u m a
 
 -- | Parse a K term
-kterm :: Parser String
-kterm = kempty <|> kapp
-
--- | Parse the K identity element
-kempty :: Parser String
-kempty = string ".List{K}"
+kterm :: Parser K
+kterm = kapp
 
 -- | Parse a K application: KLabel(K1,,K2)
-kapp :: Parser String
+kapp :: Parser K
 kapp = do
     kl <- klabel
-    argv <- parens (kterm `sepBy` (symbol ",,"))
-    return . unwords $ zipSyntax kl argv
+    argv <- parens klist
+    return $ KApp kl argv
 
--- | Combine a KLabel and a list of arguments to form the original
--- abstract syntax.
-zipSyntax (Syntax s : xs) as = s : zipSyntax xs as
-zipSyntax (Arg : xs) (a : as)
-    -- somewhat hackish way to reduce parentheses in output
-    -- TODO: this breaks test cases
-    -- TODO: will this make parsing harder in other places?
-    --  Perhaps this feature should be configurable.
-    | all isAlphaNum a = a : zipSyntax xs as
-    | otherwise = ("(" ++ a ++ ")") : zipSyntax xs as
-zipSyntax _ _ = []
+klist :: Parser [K]
+klist = kempty <|> (kterm `sepBy` (symbol ",,"))
+
+-- | Parse the K identity element
+kempty :: Parser [K]
+kempty = string ".List{K}" >> return []
 
 {- KLabels -}
-
-data KLabelPart = Syntax String | Arg
-    deriving (Eq, Show)
-
-type KLabel = [KLabelPart]
 
 -- | Parse a KLabel
 klabel :: Parser KLabel
@@ -66,45 +52,52 @@ klabel = genklabel <|> kbuiltin
 
 -- | Parse generated K label: 'Foo___
 genklabel :: Parser KLabel
-genklabel = char '\'' >> many1 klabelpart
+genklabel = KLabel <$> (char '\'' >> many1 klabelpart)
 
 -- | Parse part of a K label (an '_' arg or syntax)
 klabelpart :: Parser KLabelPart
-klabelpart = syntax <|> arg
+klabelpart = syntax <|> hole
     where syntax = Syntax <$> maudeIdentifier
-          arg = char '_' >> return Arg
+          hole = char '_' >> return Hole
 
 -- | Parse a K builtin
 kbuiltin :: Parser KLabel
-kbuiltin = (:[]) . Syntax <$> (try kid <|> knum <|> kstring <|> kbool)
+kbuiltin = try kid <|> knat <|> kint <|> kstring <|> kbool
 
 -- | Parse an Id builtin: Id x
-kid :: Parser String
+kid :: Parser KLabel
 kid = do
     symbol "Id"
     id <- many1 alphaNum
-    return id
+    return (KId id)
 
--- | Parse an Number builtin: Int 42 or Nat 42
-knum :: Parser String
-knum = do
-    symbol "Int" <|> symbol "Nat"
+-- | Parse a Nat builtin: Nat 42
+knat :: Parser KLabel
+knat = do
+    symbol "Nat"
     i <- integer
-    return (show i)
+    return (KNat i)
+
+-- | Parse an Int builtin: Int 42
+kint :: Parser KLabel
+kint = do
+    symbol "Int"
+    i <- integer
+    return (KInt i)
 
 -- | Parse a String builtin: String "hello"
-kstring :: Parser String
+kstring :: Parser KLabel
 kstring = do
     symbol "String"
     s <- stringLiteral
-    return (show s)
+    return (KString s)
 
 -- | Parse a Bool builtin: Bool true or Bool false
-kbool :: Parser String
+kbool :: Parser KLabel
 kbool = do
     symbol "Bool"
-    s <- string "true" <|> string "false"
-    return s
+    b <- (symbol "true" >> return True) <|> (string "false" >> return False)
+    return (KBool b)
 
 {- Maude identifiers -}
 
