658c49865b36bd73058b93baee2c4cb0281760da
Traian Șerbănuță <Traian.Serbanuta@gmail.com>
2011-06-22 08:44:12 +0000

    removed an impp test
    git-svn-id: http://k-framework.googlecode.com/svn/branches/syntax-first@3547 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: 80c333ff3e17a0fdca0ebf716c2eca8d850f61fe [formerly f2383475984675f1fa24e85863913489932f213f]
    Former-commit-id: 62cff2e0249acaa5a41cedbdd8ea1896d55a3648

--- examples/languages/research/imppp/imppp-test.m
+++ examples/languages/research/imppp/imppp-test.m
@@ -6,4 +6,5 @@ rew run('p7) .
 search[2] run('p13) =>! B:Bag .
 rew run('p15,Int 10(.List{K})) .
 rew run('p16,Int 10(.List{K})) .
-search run('p17,Int 3(.List{K})) =>! B:Bag .
+--- search run('p17,Int 3(.List{K})) =>! B:Bag .
+--- commented out because it is apparently blowing out of memeory
