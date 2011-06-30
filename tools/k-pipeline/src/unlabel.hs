7d3beaf4af2fa74c9645468130d50d733cb29c6a
David Lazar <david.lazar@gmail.com>
2011-06-30 23:29:46 +0000

    Update unlabel to work with the new language-k 0.2.0 API.
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@3605 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: 85690c3f2638a145fe13c41e6554854fda1d85fe [formerly 392437cafacd1442d5332c06ecff7230e411245b]
    Former-commit-id: 52616f1345825f96f8c33eba4b562745bb724425

--- tools/k-pipeline/src/unlabel.hs
+++ tools/k-pipeline/src/unlabel.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Language.K.Core.Parser
+import Language.K.Core.Pretty.RaisedMode
 
 main :: IO ()
-main = interact ((++ "\n") . either show id . parseKterm)
+main = interact ((++ "\n") . either show prettyPrint . parseK)
