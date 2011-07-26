d168c3e6f6b861b5776307b54d04c69818f5184b
Traian Șerbănuță <Traian.Serbanuta@gmail.com>
2011-07-26 07:07:42 +0000

    seqstrict test written to satisfy Chucky's script
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@3825 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: abd8fd7e3ddcddc8dcfec774e9650e8c4edb9a86 [formerly 7862469a21fe35b414bc52b500f7070ebc4652ad]
    Former-commit-id: 085266152b50edd4182fc50cc1b3f5b4c9dab63f

--- tests/issues/seqstrict/test.m
+++ tests/issues/seqstrict/test.m
@@ -1,3 +1,3 @@
 load example-compiled
-search start =>! bad .
-rew in META-LEVEL : metaSearch(upModule('ISSUE,false), 'start.Bag, 'bad.Bag, nil, '!, unbounded, 0) . ---@ test1 failure
+select META-LEVEL .
+rew metaSearch(upModule('ISSUE, false), 'start.Bag, 'bad.Bag, nil, '!, unbounded, 0) . ---@ test1 failure
