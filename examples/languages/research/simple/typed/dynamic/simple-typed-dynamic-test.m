317ff2977ce5b0278f337d52c9003f4ebb9fcd22
Traian Șerbănuță <Traian.Serbanuta@gmail.com>
2011-07-27 05:01:35 +0000

    adding undefined to SIMPLE-UNTYPED and more
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@3844 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: ee0bd3c7467eb5866fcfc70d102f2d51cb078342 [formerly 17b10fdd301b004ffa39f8f88c4f7877beed9262]
    Former-commit-id: 003e297d6259e3183aa231f4bfc9f68ff068c12f

--- examples/languages/research/simple/typed/dynamic/simple-typed-dynamic-test.m
+++ examples/languages/research/simple/typed/dynamic/simple-typed-dynamic-test.m
@@ -1,6 +1,6 @@
 load simple-typed-dynamic-compiled.maude
 rew run('pFactorial) .
 rew run('pCollatz) .
-rew run('pSorting) .
+rew run('pSorting, (ListItem(Int(7)(.List{K})) ListItem(Int(23)(.List{K})) ListItem(Int(654)(.List{K})) ListItem(Int(656)(.List{K})) ListItem(Int(54)(.List{K})) ListItem(Int(456)(.List{K})) ListItem(Int(436345647643567)(.List{K})) ListItem(Int(354654364356)(.List{K})) ListItem(Int(4357643576)(.List{K})))) .
 
 q
