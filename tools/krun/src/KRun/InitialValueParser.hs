3725cea888a9871872b908e97339c9142689c117
David Lazar <david.lazar@gmail.com>
2011-09-26 17:14:03 +0000

    Add parser for initial configuration values: ListItem(1) -> ListItem(# 1(.List{K}))
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@4172 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: f1338bd5f847b41817328f1407caaa0438407a7e [formerly 7d12a288c48a67f210f2fdc8bb44836a09d04d3f]
    Former-commit-id: db21b9aa267447e476b751e9eff16006f4f211e2

---
+++ tools/krun/src/KRun/InitialValueParser.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE OverloadedStrings #-}
+module KRun.InitialValueParser where
+
+import Control.Applicative
+import qualified Data.Map as Map
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Attoparsec.Text
+import KRun.Types
+
+parseKeyVals :: [Text] -> Map Text Kast
+parseKeyVals txts = do
+    keyVals <- mapM (parseOnly keyVal) txts
+    return $ Map.fromList $ map (\(k, v) -> (k, Kast v)) keyVals
+
+keyVal = do
+    key <- takeWhile1 (/= '=')
+    char '='
+    val <- listOrBag
+    return (key, val)
+
+listOrBag = do
+    items <- many1 $ do
+        skipSpace
+        i <- listOrBagItem
+        skipSpace
+        return i
+    return $ T.intercalate " " items
+
+-- TODO: assuming lists/bags of numbers for now.
+-- Strings or chars with ')' in them will break.
+listOrBagItem = do
+    ctor <- string "ListItem" <|> string "BagItem"
+    char '('
+    item <- takeWhile1 (/= ')')
+    char ')'
+    return $ T.concat [ctor, "(# ", item, "(.List{K}))"]
