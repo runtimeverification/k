cedf04ad8db9dd0dab7922f5b8f7dc1531571374
David Lazar <david.lazar@gmail.com>
2011-10-21 00:04:43 +0000

    Pretty-print list items and bag items as ListItem(x) and BagItem(x).
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@4407 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: a247bcb2a343448bb772ca7243bd728b74bb355c [formerly 82e86553d8b14591efbc9d1c8659dc613a68031a]
    Former-commit-id: df225bd5cdd3ec00a0fcceedc239ce5d9004fdb2

--- tools/language-k/Language/K/Core/NewPretty.hs
+++ tools/language-k/Language/K/Core/NewPretty.hs
@@ -20,11 +20,12 @@ ppKLabel (KString s) = text (show s)
 ppKBag (KBag []) = char '.'
 ppKBag (KBag bs) = vsep $ map ppBagItem bs
 
+ppBagItem (BagItem k) = text "BagItem(" <> ppK k <> char ')'
 ppBagItem (CellItem label content) =
     hang 2 (ppStartTag label <$> (ppCellContent content)) <$> ppEndTag label
 
 ppKList (KList []) = char '.'
-ppKList (KList ks) = vsep $ map ppK ks
+ppKList (KList ks) = vsep $ map (\k -> text "ListItem(" <> ppK k <> char ')') ks
 
 ppKMap (KMap m) = vcat . map ppMapItem . toList $ m
 
