c2096d7afb172d38348655069caf772a25ff32ee
David Lazar <david.lazar@gmail.com>
2012-01-07 05:05:54 +0000

    Add Language.K.Core which exports the modules under it.
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@5640 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: 8da1abb0e3c7414e0abad8cd6ca268f5eacb39d1 [formerly ed34e6bef49d221e275a4287d625452bf629a167]
    Former-commit-id: 95c616813de29b854febd4f6638e5ed4eb17772c

---
+++ tools/language-k/src/Language/K/Core.hs
@@ -0,0 +1,9 @@
+module Language.K.Core
+    ( module Language.K.Core.Syntax
+    , module Language.K.Core.Parser
+    , module Language.K.Core.Prettier
+    ) where
+
+import Language.K.Core.Syntax
+import Language.K.Core.Parser
+import Language.K.Core.Prettier
