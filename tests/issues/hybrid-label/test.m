ca1f4cae82a491d2e5a6d087f44c16a1804bdbe0
Traian Șerbănuță <Traian.Serbanuta@gmail.com>
2012-03-15 21:56:08 +0000

    Fix for issue 319.
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@6186 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: 870306b5beb9381532273f503266f6e78ee5ad6e [formerly 442b17f3f6e7e8782bf9491c32812cc6d1db105a]
    Former-commit-id: 23daebc4ac059d6ac71cea705ca482720537ef04

--- tests/issues/hybrid-label/test.m
+++
@@ -1,3 +0,0 @@
-load example-compiled
-rew < k > 'test1(.List{K}) </ k > . ---@ test1 'done(.List{K})
-rew < k > 'test2(.List{K}) </ k > . ---@ test2 'done(.List{K})
---
+++ tests/regression/issue319/test.m
@@ -0,0 +1,3 @@
+load example-compiled
+rew 'test1(.List{K}) . ---@ test1 'done(.List{K})
+rew 'test2(.List{K}) . ---@ test2 'done(.List{K})
