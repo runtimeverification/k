2f70eb3ac68b77b14a8cfdb98c6fd9c32a83f8e1
Andrei Arusoai <Andrei.Arusoaie@gmail.com>
2012-04-10 11:17:03 +0000

    Fixing K3 maudifier.
    git-svn-id: http://k-framework.googlecode.com/svn/branches/k3@6415 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: fb82a2ca4f52b8ffa2190b07676c6bb3513d71d1 [formerly 16a63b1fde1996051687c346a75025044242517a]
    Former-commit-id: 60e87808f67a7d04046823787f1e3e99206754e4

--- javasources/Kil2Maude/src/generated/Constants.java
+++ javasources/Kil2Maude/src/generated/Constants.java
@@ -5,11 +5,11 @@ package generated;
 
 /**
  * @author andrei.arusoaie
- * 
+ *
  */
 public class Constants {
-
-	// Tags constants
+	
+	//Tags constants
 	public static final String MODULE_module_TAG = "module";
 	public static final String IMPORTS_imports_TAG = "imports";
 	public static final String MODULEIMPORT_moduleImport_TAG = "moduleImport";
@@ -20,46 +20,48 @@ public class Constants {
 	public static final String TERMINAL_terminal_TAG = "terminal";
 	public static final String ANNOS_annos_TAG = "annos";
 	public static final String TAG_tag_TAG = "tag";
-	public static final String CONFIG_config_TAG = "config";
-	public static final String BAG_Bag_TAG = "Bag";
-	public static final String CELL_cell_TAG = "cell";
-	public static final String MAP_Map_TAG = "Map";
-	public static final String EMPTY_empty_TAG = "empty";
 	public static final String USERLIST_userlist_TAG = "userlist";
 	public static final String RULE_rule_TAG = "rule";
 	public static final String BODY_body_TAG = "body";
 	public static final String REWRITE_rewrite_TAG = "rewrite";
 	public static final String LEFT_left_TAG = "left";
-	public static final String KAPP_KApp_TAG = "KApp";
-	public static final String LABEL_label_TAG = "label";
-	public static final String CONST_const_TAG = "const";
-	public static final String LISTOFK_ListOfK_TAG = "ListOfK";
-	public static final String RIGHT_right_TAG = "right";
-	public static final String DEFINE_define_TAG = "define";
 	public static final String TERM_term_TAG = "term";
-	public static final String LIST_List_TAG = "List";
-	public static final String LISTITEM_ListItem_TAG = "ListItem";
 	public static final String VAR_var_TAG = "var";
+	public static final String RIGHT_right_TAG = "right";
+	public static final String EMPTY_empty_TAG = "empty";
+	public static final String CONST_const_TAG = "const";
+	public static final String CONFIG_config_TAG = "config";
+	public static final String BAG_Bag_TAG = "Bag";
+	public static final String CELL_cell_TAG = "cell";
+	public static final String KSEQUENCE_KSequence_TAG = "KSequence";
+	public static final String LIST_List_TAG = "List";
+	public static final String MAP_Map_TAG = "Map";
+	public static final String SET_Set_TAG = "Set";
+	public static final String BUILTINOP_builtinOp_TAG = "builtinOp";
+	public static final String LISTOFK_ListOfK_TAG = "ListOfK";
 	public static final String MAPITEM_MapItem_TAG = "MapItem";
 	public static final String KEY_key_TAG = "key";
 	public static final String VALUE_value_TAG = "value";
+	public static final String CONTEXT_context_TAG = "context";
+	public static final String HOLE_hole_TAG = "hole";
 	public static final String COND_cond_TAG = "cond";
-	public static final String BUILTINOP_builtinOp_TAG = "builtinOp";
-	public static final String SET_Set_TAG = "Set";
-
-	// Attributes constants
+	public static final String LISTITEM_ListItem_TAG = "ListItem";
+	public static final String SETITEM_SetItem_TAG = "SetItem";
+	public static final String KAPP_KApp_TAG = "KApp";
+	public static final String LABEL_label_TAG = "label";
+	
+	//Attributes constants
 	public static final String VALUE_value_ATTR = "value";
 	public static final String LOC_loc_ATTR = "loc";
 	public static final String KEY_key_ATTR = "key";
 	public static final String TYPE_type_ATTR = "type";
 	public static final String ASSOC_assoc_ATTR = "assoc";
+	public static final String SEPARATOR_separator_ATTR = "separator";
+	public static final String NAME_name_ATTR = "name";
 	public static final String SORT_sort_ATTR = "sort";
+	public static final String CONS_cons_ATTR = "cons";
 	public static final String COLOR_color_ATTR = "color";
 	public static final String LABEL_label_ATTR = "label";
 	public static final String MULTIPLICITY_multiplicity_ATTR = "multiplicity";
-	public static final String SEPARATOR_separator_ATTR = "separator";
-	public static final String NAME_name_ATTR = "name";
 	public static final String ELLIPSES_ellipses_ATTR = "ellipses";
-	public static final String CONS_cons_ATTR = "cons";
-
 }
