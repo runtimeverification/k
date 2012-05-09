ec645db39157d7e21a20138bc26e7848e7792845
Andrei Arusoai <Andrei.Arusoaie@gmail.com>
2012-05-09 06:24:55 +0000

    Minor.
    git-svn-id: http://k-framework.googlecode.com/svn/branches/k3@6648 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: bf9404767c7e373e7c3add5998821710d665886d [formerly 49da20661e82a776be6a52eb2221dd8dbccdf67e]
    Former-commit-id: 8f7cd2de04732f4af593ea87414b7888ab9fe179

--- javasources/JavaClasses/tests/ro/uaic/info/fmse/tests/KLabelTest.java
+++ javasources/JavaClasses/tests/ro/uaic/info/fmse/tests/KLabelTest.java
@@ -9,8 +9,6 @@ import org.w3c.dom.Document;
 
 import ro.uaic.info.fmse.k.Production;
 import ro.uaic.info.fmse.loader.DefinitionHelper;
-import ro.uaic.info.fmse.loader.JavaClassesFactory;
-import ro.uaic.info.fmse.parsing.ASTNode;
 import ro.uaic.info.fmse.utils.file.FileUtil;
 import ro.uaic.info.fmse.utils.xml.XML;
 
@@ -20,7 +18,7 @@ public class KLabelTest {
 	public void testGetTerm() {
 		String file = "c:/work/k3/javasources/K3Syntax/test/imp/.k/def.xml";
 		Document doc = XML.getDocument(FileUtil.readFileAsString(file));
-		ASTNode out = JavaClassesFactory.getTerm(doc.getDocumentElement());
+		// ASTNode out = JavaClassesFactory.getTerm(doc.getDocumentElement());
 		// System.out.println(out.toMaude());
 
 		assertTrue(doc != null);
