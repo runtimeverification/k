1319399e957dfc3c07a88210a272c4f33a8fd6e2
Andrei Arusoai <Andrei.Arusoaie@gmail.com>
2012-05-16 09:16:28 +0000

    Lists with the same separator do not produce warnings anymore.
    git-svn-id: http://k-framework.googlecode.com/svn/branches/k3@6693 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: f0af2cc7ec3632b1fdd30794d58c1c126307e5c9 [formerly 78a3359f4081b0248abb4345e7cea0cbc45aed26]
    Former-commit-id: 0d4e43b01fd2f23b47f941275fb44295a196c5dc

--- javasources/JavaClasses/src/ro/uaic/info/fmse/lists/EmptyListsVisitor.java
+++ javasources/JavaClasses/src/ro/uaic/info/fmse/lists/EmptyListsVisitor.java
@@ -8,6 +8,7 @@ import ro.uaic.info.fmse.k.Production;
 import ro.uaic.info.fmse.k.Sort;
 import ro.uaic.info.fmse.k.Term;
 import ro.uaic.info.fmse.k.TermCons;
+import ro.uaic.info.fmse.k.UserList;
 import ro.uaic.info.fmse.loader.Constants;
 import ro.uaic.info.fmse.loader.DefinitionHelper;
 import ro.uaic.info.fmse.parsing.ASTNode;
@@ -18,67 +19,92 @@ public class EmptyListsVisitor extends Visitor {
 
 	@Override
 	public ASTNode visit(ASTNode astNode) {
-		
+
 		// traverse
 		astNode.all(this);
 
-		if(astNode instanceof TermCons)
-		{
+		if (astNode instanceof TermCons) {
 			TermCons tc = (TermCons) astNode;
-			Production production = DefinitionHelper.conses.get("\"" + tc.getCons() + "\"");
-//			System.out.println("TermCons: " + tc);
-//			System.out.println("Production: " + production);
-			
-			int i = 0, j = 0; 
-			while(!(i >= production.getItems().size() || j >= tc.getContents().size()))
-			{
-//				System.out.println("Compare: ");
-//				System.out.println("\tPItem: " + production.getItems().get(i));
-//				System.out.println("\tTItem: " + tc.getContents().get(j));
-				if (production.getItems().get(i) instanceof Sort)
-				{
-				// if the sort of the j-th term is a list sort
+			Production production = DefinitionHelper.conses.get("\""
+					+ tc.getCons() + "\"");
+			// System.out.println("TermCons: " + tc);
+			// System.out.println("Production: " + production);
+
+			int i = 0, j = 0;
+			while (!(i >= production.getItems().size() || j >= tc.getContents()
+					.size())) {
+				// System.out.println("Compare: ");
+				// System.out.println("\tPItem: " +
+				// production.getItems().get(i));
+				// System.out.println("\tTItem: " + tc.getContents().get(j));
+				if (production.getItems().get(i) instanceof Sort) {
+					// if the sort of the j-th term is a list sort
 					String psort = production.getItems().get(i).toString();
 					String tsort = tc.getContents().get(j).getSort();
-					
-					if (!psort.equals(tsort))
-					{
-						if (isListSort(psort) && subsort(tsort, psort))
-						{
+
+					if (!psort.equals(tsort)) {
+						if (isListSort(psort) && subsort(tsort, psort)) {
 							List<Term> genContents = new LinkedList<Term>();
 							genContents.add(tc.getContents().get(j));
-							genContents.add(new Empty("generated", "generated", psort));
-							
-							tc.getContents().set(j, new TermCons("generated", "generated", psort, getListCons(psort), genContents));
-							System.out.println("Adding cons at " + tc.getLocation());
+							genContents.add(new Empty("generated", "generated",
+									psort));
+
+							tc.getContents().set(
+									j,
+									new TermCons("generated", "generated",
+											psort, getListCons(psort),
+											genContents));
+//							System.out.println("Adding cons at "
+//									+ tc.getLocation());
 						}
-						
-						if (!subsort(tsort, psort) && isListSort(psort))
-							Error.silentReport("Cannot infer list terminator for term:" + tc + " at location " + tc.getLocation() + " in file: " + tc.getFilename() + "\n    Expected sort: " + psort + " found sort: " + tsort);
+
+						if (!subsort(tsort, psort) && isListSort(psort)) {
+							boolean avoid = false;
+
+							if (isListSort(psort) && isListSort(tsort)) {
+								UserList ps = (UserList) (DefinitionHelper.listConses
+										.get(psort).getItems().get(0));
+								UserList ts = (UserList) (DefinitionHelper.listConses
+										.get(tsort).getItems().get(0));
+
+								if (ps.getSeparator().equals(ts.getSeparator()))
+									avoid = true;
+							}
+
+							if (!avoid)
+								Error.silentReport("Cannot infer list terminator for term:"
+										+ tc
+										+ " at location "
+										+ tc.getLocation()
+										+ " in file: "
+										+ tc.getFilename()
+										+ "\n    Expected sort: "
+										+ psort
+										+ " found sort: " + tsort);
+						}
 					}
 					j++;
 				}
 				i++;
 			}
-			
-//			System.out.println("\n\n");
+
+			// System.out.println("\n\n");
 		}
-		
+
 		return astNode;
 	}
-	
+
 	private String getListCons(String psort) {
 		Production p = DefinitionHelper.listConses.get(psort);
 		String cons = p.getAttributes().get(Constants.CONS_cons_ATTR);
-		return cons.substring(1, cons.length()-1);
+		return cons.substring(1, cons.length() - 1);
 	}
 
 	private boolean subsort(String tsort, String psort) {
 		return DefinitionHelper.isSubsorted(psort, tsort);
 	}
 
-	public boolean isListSort(String sort)
-	{
+	public boolean isListSort(String sort) {
 		return DefinitionHelper.listConses.containsKey(sort);
 	}
 }
