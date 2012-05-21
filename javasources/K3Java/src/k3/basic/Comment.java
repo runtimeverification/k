3fd1ef9eaa0882b5d5ff7380665ecbaeb3ba0dbf
Radu Mereuta <headness13@gmail.com>
2012-05-21 17:48:19 +0000

    K3 - modified the intra module comment tag.
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@6737 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: 4ecace7cb1bfe7d4b79d02b2b3320c5b712fc5c0 [formerly 7b3952f7feaa883e26690f1acccee65fed2af258]
    Former-commit-id: e69565be1a33fd1563f3becb18d81c543b18fef6

--- javasources/K3Java/src/k3/basic/Comment.java
+++ javasources/K3Java/src/k3/basic/Comment.java
@@ -1,11 +1,11 @@
 package k3.basic;
 
-import k.utils.Error;
 import k.utils.StringUtil;
 import k.utils.Tag;
-import k.utils.XmlLoader;
 
-import org.w3c.dom.*;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
 
 public class Comment extends Sentence {
 	private String content;
@@ -21,31 +21,14 @@ public class Comment extends Sentence {
 		super(node);
 
 		NamedNodeMap attr = node.getAttributes();
-		Node item = attr.getNamedItem(Tag.location);
-		location = item.getNodeValue();
-		xmlTerm = node;
+		Element elm = node.getOwnerDocument().createElement("moduleComment");
+		NamedNodeMap nmp = node.getAttributes();
+		for (int i = 0; i < nmp.getLength(); i++)
+			elm.setAttribute(nmp.item(i).getNodeName(), nmp.item(i).getNodeValue());
+		xmlTerm = elm;
 
-		item = attr.getNamedItem(Tag.value);
+		Node item = attr.getNamedItem(Tag.value);
 		content = StringUtil.unescape(item.getNodeValue());
-	}
-
-	public void parse() {
-		try {
-			String parsed = k3parser.KParser.ParseKRuleString(content);
-			Document doc = XmlLoader.getXMLDoc(parsed);
-
-			Node old = xmlTerm;
-			xmlTerm = doc.getFirstChild().getFirstChild().getNextSibling();
-			XmlLoader.updateLocation(xmlTerm, XmlLoader.getLocNumber(location, 0), XmlLoader.getLocNumber(location, 1));
-			XmlLoader.addFilename(xmlTerm, filename);
-			XmlLoader.reportErrors(doc);
-
-			old.getParentNode().replaceChild(old.getOwnerDocument().importNode(xmlTerm, true), old);
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			Error.report("Cannot parse rule: " + e.getLocalizedMessage() + " at: " + this.location + " in file: " + this.filename);
-		}
 	}
 
 	public String getContent() {
