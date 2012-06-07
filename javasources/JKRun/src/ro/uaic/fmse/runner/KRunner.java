895fe7ed485d9f9b0a30070ff372178c1c082a75
Andrei Arusoai <Andrei.Arusoaie@gmail.com>
2012-06-07 15:19:37 +0000

    Debugging KRunner.
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@6926 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: d9271f19f08a3252508131b901445418e8cd6cd0 [formerly 8d9f79cfe934354a10bb6e4584c58d9659fe6d47]
    Former-commit-id: 42bc8477f8ae79a2da3d65664788f1b81002e76a

--- javasources/JKRun/src/ro/uaic/fmse/runner/KRunner.java
+++ javasources/JKRun/src/ro/uaic/fmse/runner/KRunner.java
@@ -111,7 +111,8 @@ public class KRunner {
 		_maudeCommandFileName = KPaths.windowfyPath(_maudeCommandFileName);
 		String commandTemplate = "load {0}\n" + "mod KRUNNER is including {1} .\n" + "eq #TCPPORT = {2,number,#} .\n" + "endm\n" + "load {3}\n";
 		String command = MessageFormat.format(commandTemplate, _maudeFileName, _maudeModule, _port, _maudeCommandFileName);
-		MaudeTask maude = new MaudeTask(command, _outputFileName, _errorFileName, _logger);
+System.out.println("CMD:" + command);
+			MaudeTask maude = new MaudeTask(command, _outputFileName, _errorFileName, _logger);
 
 		maude.start();
 		_logger.info("Maude started");
