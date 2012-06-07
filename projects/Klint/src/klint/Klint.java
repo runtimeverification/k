b88ddf4ec992555604e303ac2b385e824daf025e
Felipe Tanus <fotanus@gmail.com>
2012-06-07 05:17:04 +0000

    Reused parsing method from KompileFrontEnd on Klint
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@6897 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: f093f6402cc5ffaae0d56984a3878aa15a53c29d [formerly 2c0d00e2d38ae6bf81a122bf2cd6d8da9d2864f0]
    Former-commit-id: 884b3e56a613933c7e907d405ea2db4902f7e13a

--- projects/Klint/src/klint/Klint.java
+++ projects/Klint/src/klint/Klint.java
@@ -7,9 +7,17 @@ package klint;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import k.utils.FileUtil;
+import k.utils.ResourceExtractor;
+import k.utils.Sdf2Table;
+import k.utils.XmlLoader;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import ro.uaic.info.fmse.k.Definition;
+import ro.uaic.info.fmse.loader.AmbFilter;
+import ro.uaic.info.fmse.loader.CollectSubsortsVisitor;
 import ro.uaic.info.fmse.pp.Preprocessor;
 
 /**
@@ -24,24 +32,24 @@ private static HashMap parameters = new HashMap();
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-		if(processParameters(args)) {
-			String fileName = (String) parameters.get("sourceFile");
-			File sourceFile = new File(fileName).getCanonicalFile();
+		try {
+			if(processParameters(args)) {
 
-			k3.basic.Definition def = new k3.basic.Definition();
-			def.slurp(sourceFile, true);
-			def.setMainFile(sourceFile);
-			def.addConsToProductions();
-			def.parseRules();
+				String fileName = (String) parameters.get("sourceFile");
+				File sourceFile = new File(fileName).getCanonicalFile();
 
-			Preprocessor preprocessor = new Preprocessor();
-			Document preprocessedDef = preprocessor.run(def.getDefAsXML());
-			Definition javaDef = new Definition((Element) preprocessedDef.getFirstChild());
-			System.out.println(javaDef.toString());
-
-		} else {
-			System.exit(1);
-		}
+				File dotk = new File(sourceFile.getParent() + "/.k");
+				dotk.mkdirs();
+				String lang = sourceFile.getName().replaceFirst("\\.[a-zA-Z]+$", "").toUpperCase();
+							
+				Definition javaDef = parseDefinition(lang, sourceFile, dotk);
+				System.out.println(javaDef.toString());
+			} else {
+				System.exit(1);
+			}
+		} catch (Exception ex) {
+				Logger.getLogger(Klint.class.getName()).log(Level.SEVERE, null, ex);
+		}		
     }
 
 	/**
@@ -76,5 +84,99 @@ private static HashMap parameters = new HashMap();
 	 */
 	private static void printUsage() {
 		System.out.println("klin file.k [options]");
+	}
+	  
+	
+	/**
+	 * This method is a base copy from javasources/K3Java/src/k3/basic/Definition.java
+	 * I strongly think this should be somewhere else since it parses a file to a AST.
+	 * @param mainModule
+	 * @param canonicalFiletest.k
+	 * @param dotk
+	 * @return The AST
+	 * @throws IOException
+	 * @throws Exception 
+	 */
+	private static Definition parseDefinition(String mainModule, File canonicalFile, File dotk) throws IOException, Exception {
+		// ------------------------------------- basic parsing
+		k3.basic.Definition def = new k3.basic.Definition();
+		def.slurp(canonicalFile, true);
+		def.setMainFile(canonicalFile);
+		def.setMainModule(mainModule);
+		def.addConsToProductions();
+		
+		// ------------------------------------- generate files
+		ResourceExtractor.ExtractAllSDF(dotk);
+
+		ResourceExtractor.ExtractProgramSDF(dotk);
+
+		// ------------------------------------- generate parser TBL
+		// cache the TBL if the sdf file is the same
+		String oldSdf = "";
+		if (new File(dotk.getAbsolutePath() + "/pgm/Program.sdf").exists()) {
+			oldSdf = FileUtil.getFileContent(dotk.getAbsolutePath() + "/pgm/Program.sdf");
+		}
+		FileUtil.saveInFile(dotk.getAbsolutePath() + "/pgm/Program.sdf", def.getSDFForPrograms());
+
+		String newSdf = FileUtil.getFileContent(dotk.getAbsolutePath() + "/pgm/Program.sdf");
+
+
+		if (!oldSdf.equals(newSdf)) {
+			Sdf2Table.run_sdf2table(new File(dotk.getAbsoluteFile() + "/pgm"), "Program");
+		}
+
+		// generate a copy for the definition and modify it to generate the intermediate data
+		k3.basic.Definition def2 = def.clone();// (Definition) Cloner.copy(def);
+		def2.makeConsLists();
+
+		FileUtil.saveInFile(dotk.getAbsolutePath() + "/Integration.sbs", def2.getSubsortingAsStrategoTerms());
+		FileUtil.saveInFile(dotk.getAbsolutePath() + "/Integration.cons", def2.getConsAsStrategoTerms());
+		FileUtil.saveInFile(dotk.getAbsolutePath() + "/Integration.ditto", def2.getDittosAsStrategoTerm());
+
+		def2.replaceDittoCons();
+
+		// ------------------------------------- generate parser TBL
+		// cache the TBL if the sdf file is the same
+		oldSdf = "";
+		if (new File(dotk.getAbsolutePath() + "/def/Integration.sdf").exists()) {
+			oldSdf = FileUtil.getFileContent(dotk.getAbsolutePath() + "/def/Integration.sdf");
+		}
+		FileUtil.saveInFile(dotk.getAbsolutePath() + "/def/Integration.sdf", def2.getSDFForDefinition());
+		newSdf = FileUtil.getFileContent(dotk.getAbsolutePath() + "/def/Integration.sdf");
+
+		if (!oldSdf.equals(newSdf)) {
+			Sdf2Table.run_sdf2table(new File(dotk.getAbsoluteFile() + "/def"), "K3Disamb");
+		}
+
+
+		// ------------------------------------- import files in Stratego
+		k3parser.KParser.ImportSbs(dotk.getAbsolutePath() + "/Integration.sbs");
+		k3parser.KParser.ImportDitto(dotk.getAbsolutePath() + "/Integration.ditto");
+		k3parser.KParser.ImportCons(dotk.getAbsolutePath() + "/Integration.cons");
+		k3parser.KParser.ImportTbl(dotk.getAbsolutePath() + "/def/K3Disamb.tbl");
+
+
+
+		// ------------------------------------- parse configs
+		FileUtil.saveInFile(dotk.getAbsolutePath() + "/Integration.cells", def.getCellsFromConfigAsStrategoTerm());
+		k3parser.KParser.ImportCells(dotk.getAbsolutePath() + "/Integration.cells");
+
+
+
+		// ----------------------------------- parse rules
+		def.parseRules();
+
+		// ----------------------------------- preprocessiong steps
+		Preprocessor preprocessor = new Preprocessor();
+		Document preprocessedDef = preprocessor.run(def.getDefAsXML());
+
+		XmlLoader.writeXmlFile(preprocessedDef, dotk.getAbsolutePath() + "/def.xml");
+
+
+		ro.uaic.info.fmse.k.Definition javaDef = new ro.uaic.info.fmse.k.Definition((Element) preprocessedDef.getFirstChild());
+
+		javaDef = (ro.uaic.info.fmse.k.Definition) javaDef.accept(new AmbFilter());
+		javaDef.accept(new CollectSubsortsVisitor());
+		return javaDef;
 	}
 }
