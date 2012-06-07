b58ef89686091660e7e03095570602f4677e0787
Andrei Arusoai <Andrei.Arusoaie@gmail.com>
2012-06-07 05:31:33 +0000

    First checkout, then build.
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@6898 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: 8b19c52fc5c770b31a4d6f6557dc646901e25e62 [formerly d8f3055a35efe11e41e54411b45bd522e2855f1c]
    Former-commit-id: c12a767feb4528ef1718de13730dcf5f8fc39102

--- tests/tools/Jenkins/tests/AllTests.java
+++ tests/tools/Jenkins/tests/AllTests.java
@@ -4,7 +4,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 
 @RunWith(Suite.class)
-@SuiteClasses({ Build.class, Checkout.class, Kompile.class })
+@SuiteClasses({ Checkout.class, Build.class, Kompile.class })
 public class AllTests {
 
 }
