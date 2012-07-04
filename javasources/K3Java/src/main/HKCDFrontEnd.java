ac5755ca4f72382ff351d37009676440e1b36ef7
Dmitry Dzhus <lapoussique@gmail.com>
2012-07-04 00:47:34 +0000

    HKCD stub
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@7171 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: ae574d3a4d3bc73cfe482eaf5e3b309cd0f74460 [formerly 093845adec84d55ef0bc2742bdbd4ca58bba54e1]
    Former-commit-id: d333eabb9093ca3bb3cc0b3be41004c948aba8dd

---
+++ javasources/K3Java/src/main/HKCDFrontEnd.java
@@ -0,0 +1,120 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import k.utils.DefinitionLoader;
+import k.utils.FileUtil;
+import k.utils.KPaths;
+import k.utils.Stopwatch;
+import k.utils.XmlLoader;
+import k3.basic.Definition;
+
+import org.apache.commons.cli.CommandLine;
+
+import ro.uaic.info.fmse.errorsystem.KException;
+import ro.uaic.info.fmse.errorsystem.KException.ExceptionType;
+import ro.uaic.info.fmse.errorsystem.KException.KExceptionGroup;
+import ro.uaic.info.fmse.general.GlobalSettings;
+import ro.uaic.info.fmse.latex.LatexFilter;
+import ro.uaic.info.fmse.latex.LatexFilter;
+
+
+/// Haskell K Compiler dump tool frontend
+///
+/// @todo .def loading routine is the same as in kompile — refactor it
+/// as well
+public class HKCDFrontEnd {
+
+	public static void hkcd(String[] args) {
+		Stopwatch sw = new Stopwatch();
+		KompileOptionsParser op = new KompileOptionsParser();
+
+		CommandLine cmd = op.parse(args);
+
+		// options: help
+		if (cmd.hasOption("help")) {
+			k.utils.Error.helpExit(op.getHelp(), op.getOptions());
+		}
+
+		// // set verbose
+		// if (cmd.hasOption("verbose")) {
+		//	GlobalSettings.verbose = true;
+		// }
+
+		String def = null;
+		if (cmd.hasOption("def"))
+			def = cmd.getOptionValue("def");
+		else {
+			String[] restArgs = cmd.getArgs();
+			if (restArgs.length < 1)
+				k.utils.Error.report("You have to provide a file in order to compile!.");
+			else
+				def = restArgs[0];
+		}
+
+		File mainFile = new File(def);
+		GlobalSettings.mainFile = mainFile;
+		GlobalSettings.mainFileWithNoExtension = mainFile.getAbsolutePath().replaceFirst("\\.k$", "").replaceFirst("\\.xml$", "");
+		if (!mainFile.exists()) {
+			File errorFile = mainFile;
+			mainFile = new File(def + ".k");
+			if (!mainFile.exists())
+				GlobalSettings.kem.register(new KException(ExceptionType.ERROR, KExceptionGroup.CRITICAL, "File: " + errorFile.getName() + "(.k) not found.", errorFile.getAbsolutePath(), "File system.", 0));
+		}
+
+		String lang = null;
+		if (cmd.hasOption("lang"))
+			lang = cmd.getOptionValue("lang");
+		else
+			lang = mainFile.getName().replaceFirst("\\.[a-zA-Z]+$", "").toUpperCase();
+
+		hkcd(mainFile, lang);
+
+		if (GlobalSettings.verbose)
+			sw.printTotal("Total           = ");
+		GlobalSettings.kem.print(GlobalSettings.level);
+	}
+
+
+	public static String hkcd(File mainFile, String mainModule) {
+		try {
+			// for now just use this file as main argument
+			File canonicalFile = mainFile.getCanonicalFile();
+
+			String fileSep = System.getProperty("file.separator");
+			File dotk = new File(canonicalFile.getParent() + fileSep + ".k");
+			dotk.mkdirs();
+
+			GlobalSettings.latex = true;
+
+			ro.uaic.info.fmse.k.Definition javaDef = k.utils.DefinitionLoader.loadDefinition(mainFile, mainModule, GlobalSettings.verbose);
+
+			Stopwatch sw = new Stopwatch();
+
+			//HaskellDumpFilter lf = new HaskellDumpFilter();
+			//javaDef.accept(hdf);
+
+			//String dumped = hdf.getResult();
+			String dumped = "";
+
+			FileUtil.saveInFile(dotk.getAbsolutePath() + "/def.hkcd", dumped);
+
+			FileUtil.saveInFile(FileUtil.stripExtension(canonicalFile.getAbsolutePath()) + ".hkcd", dumped);
+
+			if (GlobalSettings.verbose) {
+				sw.printIntermediate("HKCD         = ");
+			}
+
+			return dumped;
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
