7236eed7bb2a674f32efaa382e030fab8d502684
Dmitry Dzhus <lapoussique@gmail.com>
2012-07-07 20:12:23 +0000

    Try using xstream for HKC Dump Filter
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@7254 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: 1fd2809600496dc7581eb01184520fa7e44e2d22 [formerly 85e7cab2a4f3d39204703ce8504ff6aee38a6cb5]
    Former-commit-id: 7e167f6171fbf3a30f73a43c309e83ad82eaf705

--- javasources/JavaClasses/src/ro/uaic/info/fmse/hkcd/HaskellDumpFilter.java
+++ javasources/JavaClasses/src/ro/uaic/info/fmse/hkcd/HaskellDumpFilter.java
@@ -3,13 +3,16 @@ package ro.uaic.info.fmse.hkcd;
 import ro.uaic.info.fmse.k.*;
 import ro.uaic.info.fmse.visitors.BasicVisitor;
 
+import com.thoughtworks.xstream.XStream;
+
 public class HaskellDumpFilter extends BasicVisitor {
 	String endl = System.getProperty("line.separator");
+	XStream xstream = new XStream();
 	private String result = "";
 
 	@Override
 	public void visit(Rewrite r) {
-		result += r.toString() + endl;
+		result += xstream.toXML(r);
 	}
 
 	public String getResult() {
