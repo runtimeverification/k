fa7b1a00f9366d48c095ea686bf171cd339c4143
Dmitry Dzhus <lapoussique@gmail.com>
2012-07-07 20:26:11 +0000

    Split parsePgm into separate class; kast updated
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@7255 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: f9d45eaf6469abc9980070c73d4f34c259a556aa [formerly 09d8d794b77ab8497a13774bf5b9aa3dd42ae38e]
    Former-commit-id: 3341ef1f1fb29ad649763ffbf7b549309e1687fb

---
+++ javasources/K3Java/src/k/utils/ProgramLoader.java
@@ -0,0 +1,85 @@
+package k.utils;
+
+import java.io.File;
+import java.io.IOException;
+
+import k3.basic.Definition;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import ro.uaic.info.fmse.k.ASTNode;
+import ro.uaic.info.fmse.loader.CollectConsesVisitor;
+import ro.uaic.info.fmse.loader.JavaClassesFactory;
+import ro.uaic.info.fmse.loader.UpdateReferencesVisitor;
+import ro.uaic.info.fmse.transitions.labelify.KAppModifier;
+
+import com.thoughtworks.xstream.XStream;
+
+public class ProgramLoader {
+	public static void parsePgm(File mainFile, File defFile, Boolean verbose) {
+		try {
+			// compile a definition here
+			Stopwatch sw = new Stopwatch();
+
+			// for now just use this file as main argument
+			File f = mainFile.getCanonicalFile();
+			File dotk = new File(defFile.getCanonicalFile().getParent() + "/.k");
+
+			dotk.mkdirs();
+			File tbl = new File(dotk.getCanonicalPath() + "/pgm/Program.tbl");
+
+			// ------------------------------------- import files in Stratego
+			k3parser.KParser.ImportTblPgm(tbl.getAbsolutePath());
+			if (verbose)
+				sw.printIntermediate("Importing Files = ");
+
+			try {
+				String content = FileUtil.getFileContent(f.getAbsolutePath());
+
+				String parsed = k3parser.KParser.ParseProgramString(content);
+				Document doc = XmlLoader.getXMLDoc(parsed);
+
+				XmlLoader.addFilename(doc.getFirstChild(), mainFile.getAbsolutePath());
+				XmlLoader.reportErrors(doc);
+				XmlLoader.writeXmlFile(doc, dotk.getAbsolutePath() + "/pgm.xml");
+
+				if (verbose) {
+					sw.printIntermediate("Parsing Program = ");
+				}
+
+				String definition = FileUtil.getFileContent(dotk.getAbsolutePath() + "/def.xml");
+
+				Document defDoc = ro.uaic.info.fmse.utils.xml.XML.getDocument(definition);
+				ASTNode outDef = JavaClassesFactory.getTerm(defDoc.getDocumentElement());
+				outDef.accept(new UpdateReferencesVisitor());
+				outDef.accept(new CollectConsesVisitor());
+
+				ASTNode out = JavaClassesFactory.getTerm((Element) doc.getDocumentElement().getFirstChild().getNextSibling());
+
+				out = out.accept(new KAppModifier());
+				String kast = out.toMaude();
+
+				System.out.println(kast);
+
+				String ast;
+				ast = "load ../" + defFile.getName().substring(0, defFile.getName().length() - 2) + "-compiled.maude\n";
+				ast += "set show command off .\n erewrite #eval(__((_|->_((# \"$PGM\"(.List{K})) , (\n\n";
+				ast += kast;
+				ast += "\n\n))),(.).Map))  .\n quit\n";
+
+				FileUtil.saveInFile(dotk.getAbsolutePath() + "/pgm.maude", ast);
+
+				if (verbose) {
+					sw.printIntermediate("Maudify Program = ");
+					sw.printTotal("Total           = ");
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				k.utils.Error.report("Cannot parse program: " + e.getLocalizedMessage());
+			}
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+	}
+}
--- javasources/K3Java/src/main/KastFrontEnd.java
+++ javasources/K3Java/src/main/KastFrontEnd.java
@@ -3,6 +3,7 @@ package main;
 import java.io.File;
 import java.io.IOException;
 
+import k.utils.ProgramLoader;
 import k.utils.FileUtil;
 import k.utils.Stopwatch;
 import k.utils.XmlLoader;
@@ -12,11 +13,6 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 import ro.uaic.info.fmse.general.GlobalSettings;
-import ro.uaic.info.fmse.k.ASTNode;
-import ro.uaic.info.fmse.loader.CollectConsesVisitor;
-import ro.uaic.info.fmse.loader.JavaClassesFactory;
-import ro.uaic.info.fmse.loader.UpdateReferencesVisitor;
-import ro.uaic.info.fmse.transitions.labelify.KAppModifier;
 
 public class KastFrontEnd {
 
@@ -86,74 +82,8 @@ public class KastFrontEnd {
 				e.printStackTrace();
 			}
 		}
-		parsePgm(mainFile, def);
+		k.utils.ProgramLoader.parsePgm(mainFile, def, GlobalSettings.verbose);
 		if (GlobalSettings.verbose)
 			sw.printTotal("Total           = ");
-	}
-
-	public static void parsePgm(File mainFile, File defFile) {
-		try {
-			// compile a definition here
-			Stopwatch sw = new Stopwatch();
-
-			// for now just use this file as main argument
-			File f = mainFile.getCanonicalFile();
-			File dotk = new File(defFile.getCanonicalFile().getParent() + "/.k");
-
-			dotk.mkdirs();
-			File tbl = new File(dotk.getCanonicalPath() + "/pgm/Program.tbl");
-
-			// ------------------------------------- import files in Stratego
-			k3parser.KParser.ImportTblPgm(tbl.getAbsolutePath());
-			if (GlobalSettings.verbose)
-				sw.printIntermediate("Importing Files = ");
-
-			try {
-				String content = FileUtil.getFileContent(f.getAbsolutePath());
-
-				String parsed = k3parser.KParser.ParseProgramString(content);
-				Document doc = XmlLoader.getXMLDoc(parsed);
-
-				XmlLoader.addFilename(doc.getFirstChild(), mainFile.getAbsolutePath());
-				XmlLoader.reportErrors(doc);
-				XmlLoader.writeXmlFile(doc, dotk.getAbsolutePath() + "/pgm.xml");
-
-				if (GlobalSettings.verbose) {
-					sw.printIntermediate("Parsing Program = ");
-				}
-
-				String definition = FileUtil.getFileContent(dotk.getAbsolutePath() + "/def.xml");
-
-				Document defDoc = ro.uaic.info.fmse.utils.xml.XML.getDocument(definition);
-				ASTNode outDef = JavaClassesFactory.getTerm(defDoc.getDocumentElement());
-				outDef.accept(new UpdateReferencesVisitor());
-				outDef.accept(new CollectConsesVisitor());
-
-				ASTNode out = JavaClassesFactory.getTerm((Element) doc.getDocumentElement().getFirstChild().getNextSibling());
-
-				out = out.accept(new KAppModifier());
-				String kast = out.toMaude();
-
-				System.out.println(kast);
-
-				String ast;
-				ast = "load ../" + defFile.getName().substring(0, defFile.getName().length() - 2) + "-compiled.maude\n";
-				ast += "set show command off .\n erewrite #eval(__((_|->_((# \"$PGM\"(.List{K})) , (\n\n";
-				ast += kast;
-				ast += "\n\n))),(.).Map))  .\n quit\n";
-
-				FileUtil.saveInFile(dotk.getAbsolutePath() + "/pgm.maude", ast);
-
-				if (GlobalSettings.verbose) {
-					sw.printIntermediate("Maudify Program = ");
-					sw.printTotal("Total           = ");
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-				k.utils.Error.report("Cannot parse program: " + e.getLocalizedMessage());
-			}
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
 	}
 }
