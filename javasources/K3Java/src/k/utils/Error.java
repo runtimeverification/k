352e3f55d21a30632c0da15b59012f6cbbba3af2
Dmitry Dzhus <lapoussique@gmail.com>
2012-08-01 18:58:20 +0000

    Overloaded helpExit to allow using non-"help" program name in usage message
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@7428 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: c411503b52a4921a3658b120c8e859b5bcd580eb [formerly 1e0b9af40f952863d4f4010cb8a8d523afe16970]
    Former-commit-id: 437d88a1e42da769845462606d16745a93d16c99

--- javasources/K3Java/src/k/utils/Error.java
+++ javasources/K3Java/src/k/utils/Error.java
@@ -19,8 +19,12 @@ public class Error {
 	}
 
 	public static void helpExit(HelpFormatter help, Options options) {
+		helpExit(help, "help", options);
+	}
+
+	public static void helpExit(HelpFormatter help, String cmdLineSyntax, Options options) {
 		System.out.println("\n");
-		help.printHelp("help", "\nCompiles the K definitions given as arguments.\n\n", options, "\n", true);
+		help.printHelp(cmdLineSyntax, "\nCompiles the K definitions given as arguments.\n\n", options, "\n", true);
 		System.out.println("\n");
 		System.exit(1);
 	}
