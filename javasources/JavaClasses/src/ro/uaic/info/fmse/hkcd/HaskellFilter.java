49e9e316c2fcb99ab09c15f1bd3a82e0d006fb9e
Dmitry Dzhus <lapoussique@gmail.com>
2012-08-14 23:41:55 +0000

    HaskellFilter: dump empties of basic sorts as simply [].
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@7485 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: e2c24a1b37c4b0f84c039c55ad7752a8fc4780c4 [formerly 3b55788990910b327f90adce6f1c0a1e270f1125]
    Former-commit-id: 6b48c89ecaac068390c6444ac063b93915d89909

--- javasources/JavaClasses/src/ro/uaic/info/fmse/hkcd/HaskellFilter.java
+++ javasources/JavaClasses/src/ro/uaic/info/fmse/hkcd/HaskellFilter.java
@@ -3,7 +3,11 @@ package ro.uaic.info.fmse.hkcd;
 import ro.uaic.info.fmse.k.*;
 import ro.uaic.info.fmse.loader.DefinitionHelper;
 import ro.uaic.info.fmse.visitors.BasicVisitor;
+import ro.uaic.info.fmse.transitions.maude.MaudeHelper;
 
+/**
+ * Translate AST into Haskell data constructors.
+ */
 public class HaskellFilter extends BasicVisitor {
 	String endl = System.getProperty("line.separator");
 	protected String result = "";
@@ -15,11 +19,13 @@ public class HaskellFilter extends BasicVisitor {
 		result += "KApp (";
 
 		// Perhaps we'd want to simply serialize some set of
-		// classes into Haskell. This cannot be done with
-		// existing set of Java classes for K since in Haskell
-		// we have different constructors for various types of
-		// constants, while in Java this information is stored
-		// in the field of Constant class.
+		// classes into Haskell. However, this cannot be done
+		// with existing set of Java classes for K since in
+		// Haskell we have different constructors for various
+		// types of constants, while in Java this information
+		// is stored in the field of Constant class.
+		// Implementation for such classes would basically be
+		// the same as this code.
 		if (s.equals("#Int")) {
 			result += "KInt (" + v + ")";
 		} else if (s.equals("#String")) {
@@ -30,11 +36,19 @@ public class HaskellFilter extends BasicVisitor {
 			result += "KBool " + HaskellUtil.capitalizeFirst(v);
 		}
 
+		// We take a shortcut here instead of processing a new
+		// Empty of sort "List{K}".
 		result += ") []";
 	}
 
 	public void visit(Empty e) {
-		result += "KApp (KLabel \"'." + e.getSort() + "\") []";
+		String s = e.getSort();
+		
+		if (!MaudeHelper.basicSorts.contains(s)) {
+			result += "KApp (KLabel \"'." + s + "\") []";
+		}
+		else
+			result += "[]";
 	}
 
 	public String getResult() {
