54df7b1323e50d4ada23ef76be5716a5cbc207a0
Andrei Arusoai <Andrei.Arusoaie@gmail.com>
2012-08-24 09:40:16 +0000

    Forgot to add some sources.
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@7608 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: f8488dd6374eadfbac1e883b913a12b828ae2b40 [formerly 03bbfb7542b578012cb72d91a2a6b9d7fdf12db2]
    Former-commit-id: e42733c32fd1e960e54cab2fcdcdb1c3c8951cc2

---
+++ javasources/IOServer/src/jfkbits/Atom.java
@@ -0,0 +1,20 @@
+package jfkbits;
+ 
+import jfkbits.LispParser.Expr;
+ 
+public class Atom implements Expr
+{
+    String name;
+    public String toString()
+    {
+        return name;
+    }
+    public Atom(String text)
+    {
+        name = text;
+    }
+ 
+    public String getKIF() {
+    	return "";
+    }
+}
