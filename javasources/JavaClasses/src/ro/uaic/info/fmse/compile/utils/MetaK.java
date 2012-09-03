ba90fa5420c20e7bf7468e457265eca4513bb9a1
Traian Șerbănuță <Traian.Serbanuta@gmail.com>
2012-09-03 11:11:03 +0000

    Fixing...
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@7722 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: 359fbff7c0f6c9261dde8887269a55146e548a0a [formerly 5870a3866b753541e9fe6815ad7f183c952403d3]
    Former-commit-id: 247936709b416134412bb437d339e3af8b58c3c1

--- javasources/JavaClasses/src/ro/uaic/info/fmse/compile/utils/MetaK.java
+++ javasources/JavaClasses/src/ro/uaic/info/fmse/compile/utils/MetaK.java
@@ -12,45 +12,44 @@ public class MetaK {
 	public static String nextIdModules[] = {
 		"SUBSTITUTION",
 	};
-	
+
 	public static String kModules[] = {
-	    "K-CONDITION-SEARCH", 
-	    "K-CONFIG",
+		"K-CONDITION-SEARCH", 
+		"K-CONFIG",
 		"K-CONTEXTS", 
-	    "K-LATEX",
-	    "K-OPEN-CELLS",
-	    "K-POLYMORPHIC-VARIABLES",  
+		"K-LATEX",
+		"K-OPEN-CELLS",
+		"K-POLYMORPHIC-VARIABLES",  
 		"K-PROPER", 
-	    "K-QUOTED-LABELS",
+		"K-QUOTED-LABELS",
 		"K-RESULT",
-	    "K-RULES", 
+		"K-RULES", 
 		"K-SENTENCE", 
 		"K-STRICNESS",
 		"K-TECHNIQUE",
 		"K-WHERE",  
-	    "K-WRAPPERS", 
-	    "K-WRAPPERS-LABELS", 
-		};
-	
+		"K-WRAPPERS", 
+		"K-WRAPPERS-LABELS", 
+	};
+
 	public static boolean isKModule(String key) {
 		return (Arrays.binarySearch(kModules, key) >= 0);		
 	}
-	
+
 	public static boolean isNextIdModule(String key) {
 		return (Arrays.binarySearch(nextIdModules, key) >= 0);		
 	}
-	
+
 	public static boolean isBuiltinModule(String key) {
 		return key.startsWith("#");
 	}
-	
+
 	public static Set<Variable> getVariables(Visitable node) {
 		final Set<Variable> result = new HashSet<Variable>();
 		node.accept(new BasicVisitor() {
 			@Override
 			public void visit(Variable node) {
 				result.add(node);
-				super.visit(node);
 			}
 		});
 		return result;
