601912a9b74683064542d94bfb551e0ece905b7e
Ștefan Ciobâcă <stefan.ciobaca@gmail.com>
2012-09-20 11:10:13 +0000

    fixed radu's command line argument parsing :D
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@7829 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: 76aca94c62cbe6196ae3b348ae249b18248c11e8 [formerly 172fbf3bf94fc32047bb26a32c91144660c344ec]
    Former-commit-id: ace65aba038230455b1a507385cd40294e0117ec

--- javasources/KTool/src/org/kframework/main/Main.java
+++ javasources/KTool/src/org/kframework/main/Main.java
@@ -9,19 +9,22 @@ public class Main {
 	 *            - the running arguments for the K3 tool. First argument must be one of the following: kompile|kast|krun.
 	 */
 	public static void main(String[] args) {
-		String[] args2 = new String[args.length - 1];
-		for (int i = 0; i < args.length - 1; i++)
-			args2[i] = args[i + 1];
-
-		if (args[0].equals("-kompile")) {
-			KompileFrontEnd.kompile(args2);
-		} else if (args[0].equals("-kast")) {
-			KastFrontEnd.kast(args2);
-		} else if (args[0].equals("-hkcd")) {
-			HKCDFrontEnd.hkcd(args2);
-		} else if (args[0].equals("-krun")) {
-			org.kframework.krun.Main.execute_Krun(args2);
-		} else
-			Error.report("The first arguemnt of K3 must be one of the following: -kompile|-kast|-krun");
+		if (args.length >= 1) {
+			String[] args2 = new String[args.length - 1];
+			for (int i = 0; i < args.length - 1; i++)
+				args2[i] = args[i + 1];
+			if (args[0].equals("-kompile")) {
+				KompileFrontEnd.kompile(args2);
+			} else if (args[0].equals("-kast")) {
+				KastFrontEnd.kast(args2);
+			} else if (args[0].equals("-hkcd")) {
+				HKCDFrontEnd.hkcd(args2);
+			} else if (args[0].equals("-krun")) {
+				org.kframework.krun.Main.execute_Krun(args2);
+			} else {
+				Error.report("The first argument of K3 not recognized. Try -kompile or -kast or -krun.");
+			}
+		}
+		Error.report("There must be a first argument to K3: try -kompile or -kast or -krun.");
 	}
 }
