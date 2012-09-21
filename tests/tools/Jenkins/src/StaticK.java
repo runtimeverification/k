344bed8159fbfacf735f3d4573afaf395d885439
Andrei Arusoai <Andrei.Arusoaie@gmail.com>
2012-09-21 08:43:31 +0000

    Try to fix Jenkins.
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@7844 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: a583f8ae11db08e82a384c2da4d30429cdd8e091 [formerly b77d4fcf9bf961e8dcde7b357565e4ed6132b60f]
    Former-commit-id: 016866657f395634e615ef50b4333e2d61f976b1

--- tests/tools/Jenkins/src/StaticK.java
+++ tests/tools/Jenkins/src/StaticK.java
@@ -36,8 +36,7 @@ public class StaticK {
 	{
 		int poolSize = 1;
 		int cores = Runtime.getRuntime().availableProcessors();
-		int pS = cores / 4;
-		pS = 1;
+		int pS = cores - cores / 4;
 		if (pS > poolSize)
 			return pS;
 		
