c74d4cdcc18abb70a8185feba48b58e122f61a2e
Andrei Arusoai <Andrei.Arusoaie@gmail.com>
2012-10-16 14:30:33 +0000

    Do not run programs in tutorial.
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@8115 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: 3318ae9713d37933ead99cb300f4e5ae63cc39f0 [formerly 5a048858e808bda85b926df24e74d1f1b3b35823]
    Former-commit-id: 55ce4ee910a0f648568b978117128d2d969e526f

--- tests/tools/Jenkins/tests/RunPrograms.java
+++ tests/tools/Jenkins/tests/RunPrograms.java
@@ -30,13 +30,16 @@ public class RunPrograms {
 		List<List<Example>> all = new ArrayList<List<Example>>();
 
 		for (String tag : StaticK.tags) {
-			List<Example> examples = StaticK.getExamples(configuration,
-					StaticK.k3Jar, tag, StaticK.kbasedir);
-			for (Example example : examples) {
-				example.runPrograms = true;
-				tasks.add(Executors.callable(example));
+			if (!tag.equals("tutorial"))
+			{
+				List<Example> examples = StaticK.getExamples(configuration,
+						StaticK.k3Jar, tag, StaticK.kbasedir);
+				for (Example example : examples) {
+					example.runPrograms = true;
+					tasks.add(Executors.callable(example));
+				}
+				all.add(examples);
 			}
-			all.add(examples);
 		}
 
 		// running
@@ -45,7 +48,8 @@ public class RunPrograms {
 		es.invokeAll(tasks);
 
 		// report
-		for (List<Example> examples : all)
+		for (List<Example> examples : all){
+			if (!StaticK.tags.get(all.indexOf(examples)).equals("tutorial"))
 			for (Example example : examples) {
 				Report report = StaticK.reports.get(example
 						.getJenkinsSuiteName());
@@ -55,13 +59,15 @@ public class RunPrograms {
 				report.save();
 				StaticK.reports.put(example.getJenkinsSuiteName(), report);
 			}
-
-		for (List<Example> examples : all)
-			for (Example example : examples) {
+		}
+		for (List<Example> examples : all){
+			if (!StaticK.tags.get(all.indexOf(examples)).equals("tutorial"))
+				for (Example example : examples) {
 				for (Program program : example.programs) {
 					assertTrue(program.isCorrect());
 				}
 			}
+		}
 		System.out.println("\nDone.");
 	}
 
