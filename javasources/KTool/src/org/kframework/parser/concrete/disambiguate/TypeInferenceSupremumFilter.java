99301440bba100bb1668ef0c4f963a1923f3a369
Radu Mereuta <headness13@gmail.com>
2012-10-24 13:14:55 +0000

    K3 - improved disambiguation filter. Disambiguate only if one term extend the other.
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@8248 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: 4841eacb38cb8eb3001cb282db7536ba33a57a7d [formerly c8cb639fcbd337c8f13281d491afa558404c14bb]
    Former-commit-id: 90547f3195071be3beb3fe038730a0dc76cb91fa

--- javasources/KTool/src/org/kframework/parser/concrete/disambiguate/TypeInferenceSupremumFilter.java
+++ javasources/KTool/src/org/kframework/parser/concrete/disambiguate/TypeInferenceSupremumFilter.java
@@ -4,11 +4,15 @@ import java.util.ArrayList;
 
 import org.kframework.kil.ASTNode;
 import org.kframework.kil.Ambiguity;
+import org.kframework.kil.ProductionItem;
+import org.kframework.kil.ProductionItem.ProductionType;
+import org.kframework.kil.Sort;
 import org.kframework.kil.Term;
+import org.kframework.kil.TermCons;
+import org.kframework.kil.UserList;
 import org.kframework.kil.loader.DefinitionHelper;
 import org.kframework.kil.visitors.BasicTransformer;
 import org.kframework.kil.visitors.exceptions.TransformerException;
-
 
 public class TypeInferenceSupremumFilter extends BasicTransformer {
 
@@ -24,10 +28,11 @@ public class TypeInferenceSupremumFilter extends BasicTransformer {
 		for (Term trm1 : amb.getContents()) {
 			boolean topSort = true;
 			for (Term trm2 : amb.getContents()) {
-				if (DefinitionHelper.isSubsorted(trm2.getSort(), trm1.getSort())) {
-					topSort = false;
-					break;
-				}
+				if (termsAlike(trm1, trm2))
+					if (DefinitionHelper.isSubsorted(trm2.getSort(), trm1.getSort())) {
+						topSort = false;
+						break;
+					}
 			}
 			if (topSort)
 				terms.add(trm1);
@@ -39,5 +44,67 @@ public class TypeInferenceSupremumFilter extends BasicTransformer {
 			amb.setContents(terms);
 
 		return super.transform(amb);
+	}
+
+	/**
+	 * Returns true if the terms are of the same kind (Variable, TermCons...) If they are TermCons, then all the Items must be alike also.
+	 * 
+	 * @param trm1
+	 *            First term to compare.
+	 * @param trm2
+	 *            Second term to compare.
+	 * @return true or false weather they are alike.
+	 */
+	private static boolean termsAlike(Term trm1, Term trm2) {
+		if (!trm1.getClass().equals(trm2.getClass()))
+			return false;
+
+		if (trm1 instanceof TermCons) {
+			TermCons term1 = (TermCons) trm1;
+			TermCons term2 = (TermCons) trm2;
+
+			if (term1.getProduction().getItems().size() != term2.getProduction().getItems().size())
+				return false;
+
+			// check to see if every non terminal is subsorted-eq to the other term.
+			// add 1 to left each time Sort1 < Sort2 and add 1 to right vice versa
+			// if at least one of them is 0 at the end return true.
+			int left = 0, right = 0;
+			for (int i = 0; i < term1.getProduction().getItems().size(); i++) {
+				ProductionItem itm1 = term1.getProduction().getItems().get(i);
+				ProductionItem itm2 = term2.getProduction().getItems().get(i);
+
+				if (itm1.getType() == ProductionType.TERMINAL && !itm1.equals(itm2))
+					return false;
+				else if (itm1.getType() == ProductionType.USERLIST) {
+					if (itm2.getType() != ProductionType.USERLIST)
+						return false;
+					UserList ul1 = (UserList) itm1;
+					UserList ul2 = (UserList) itm2;
+
+					if (DefinitionHelper.isSubsortedEq(ul1.getSort(), ul2.getSort()))
+						left++;
+					else if (DefinitionHelper.isSubsortedEq(ul2.getSort(), ul1.getSort()))
+						right++;
+					else
+						return false;
+				} else {
+					// only sort possible now.
+					Sort srt1 = (Sort) itm1;
+					Sort srt2 = (Sort) itm2;
+
+					if (DefinitionHelper.isSubsortedEq(srt1.getName(), srt2.getName()))
+						left++;
+					else if (DefinitionHelper.isSubsortedEq(srt2.getName(), srt1.getName()))
+						right++;
+					else
+						return false;
+				}
+			}
+			if (right == 0 || left == 0)
+				return true;
+		}
+
+		return true;
 	}
 }
