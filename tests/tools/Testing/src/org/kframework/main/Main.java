2e58f964a3d78ca32cc85e59f76523aae8f86481
Andrei Arusoai <Andrei.Arusoaie@gmail.com>
2012-10-30 13:00:20 +0000

    Run ant.
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@8374 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: b4f9d706606b73659cc81a39ec6d423626d5343c [formerly a24ce95cb8c96f1a661a1ae1d166fc442a8d7522]
    Former-commit-id: 902a37e416cff72a1a3b505a347be45c31fd55a3

--- tests/tools/Testing/src/org/kframework/main/Main.java
+++ tests/tools/Testing/src/org/kframework/main/Main.java
@@ -31,42 +31,42 @@ public class Main {
 
 		// a little bit hack-ish but it works until somebody complains
 		System.out.println("UDIR: " + System.getProperty("user.dir"));
-		if (System.getProperty("user.dir").contains("jenkins"))
-		{
+		if (System.getProperty("user.dir").contains("jenkins")) {
 			// first copy the k-framework artifacts
 			try {
-				copyFolder(new File("/var/lib/jenkins/workspace/k-framework"), new File("k"));
+				copyFolder(new File("/var/lib/jenkins/workspace/k-framework"),
+						new File("k"));
 			} catch (IOException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
-			
+
 			// setup the new path
 			Configuration.HOME_DIR = "/var/lib/jenkins/workspace/k-framework-tests/k";
-			
+
 			// build K
-			Task build = new Task(new String[]{"cd", "/var/lib/jenkins/workspace/k-framework-tests/k", ";", "ant"}, "");
-			build.start();
 			try {
-				build.join();
-			} catch (InterruptedException e) {
+				ProcessBuilder pb = new ProcessBuilder("ant");
+				Process process = pb.start();
+				int exit = process.waitFor();
+				String out = Task.readString(process.getInputStream());
+				String err = Task.readString(process.getErrorStream());
+				System.out.println(out);
+				System.out.println(err);
+				if (exit != 0)
+					System.exit(exit);
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			
-			System.out.println(build.getStdout());
-			System.out.println(build.getStderr());
-			if (build.getExit() != 0)
-				System.exit(2);
 		}
-		
-		// execute the right scripts 
+
+		// execute the right scripts
 		String os = System.getProperty("os.name").toLowerCase();
-		if (os.contains("win"))
-		{
+		if (os.contains("win")) {
 			Configuration.kompile = Configuration.kompile + ".bat";
 			Configuration.krun = Configuration.kompile + ".bat";
 		}
-		
+
 		List<Test> alltests = new LinkedList<Test>();
 
 		// load config
@@ -130,7 +130,7 @@ public class Main {
 						+ " failed.");
 		}
 		System.out.println();
-		
+
 		// console display
 		System.out.println("Pdf compilation failed for: ");
 		for (Entry<Test, Task> entry : pdfDefinitions.entrySet()) {
@@ -186,50 +186,46 @@ public class Main {
 		}
 
 	}
-	
-	public static void copyFolder(File src, File dest)
-	    	throws IOException{
-	 
-			if (src.getName().startsWith("."))
-				return;
-		
-	    	if(src.isDirectory()){
-	 
-	    		//if directory not exists, create it
-	    		if(!dest.exists()){
-	    		   dest.mkdir();
-//	    		   System.out.println("Directory copied from " 
-//	                              + src + "  to " + dest);
-	    		}
-	 
-	    		//list all the directory contents
-	    		String files[] = src.list();
-	 
-	    		for (String file : files) {
-	    		   //construct the src and dest file structure
-	    		   File srcFile = new File(src, file);
-	    		   File destFile = new File(dest, file);
-	    		   //recursive copy
-	    		   copyFolder(srcFile,destFile);
-	    		}
-	 
-	    	}else{
-	    		//if file, then copy it
-	    		//Use bytes stream to support all file types
-	    		InputStream in = new FileInputStream(src);
-	    	        OutputStream out = new FileOutputStream(dest); 
-	 
-	    	        byte[] buffer = new byte[1024];
-	 
-	    	        int length;
-	    	        //copy the file content in bytes 
-	    	        while ((length = in.read(buffer)) > 0){
-	    	    	   out.write(buffer, 0, length);
-	    	        }
-	 
-	    	        in.close();
-	    	        out.close();
-//	    	        System.out.println("File copied from " + src + " to " + dest);
-	    	}
+
+	public static void copyFolder(File src, File dest) throws IOException {
+
+		if (src.getName().startsWith("."))
+			return;
+
+		if (src.isDirectory()) {
+
+			// if directory not exists, create it
+			if (!dest.exists()) {
+				dest.mkdir();
+			}
+
+			// list all the directory contents
+			String files[] = src.list();
+
+			for (String file : files) {
+				// construct the src and dest file structure
+				File srcFile = new File(src, file);
+				File destFile = new File(dest, file);
+				// recursive copy
+				copyFolder(srcFile, destFile);
+			}
+
+		} else {
+			// if file, then copy it
+			// Use bytes stream to support all file types
+			InputStream in = new FileInputStream(src);
+			OutputStream out = new FileOutputStream(dest);
+
+			byte[] buffer = new byte[1024];
+
+			int length;
+			// copy the file content in bytes
+			while ((length = in.read(buffer)) > 0) {
+				out.write(buffer, 0, length);
+			}
+
+			in.close();
+			out.close();
+		}
 	}
 }
