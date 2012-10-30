f851786de2721003ccdb96288cfebe0601ab8403
Andrei Arusoai <Andrei.Arusoaie@gmail.com>
2012-10-30 12:41:23 +0000

    Manual copy of k-framework dir.
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@8369 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: 392868fa538dbee1351587bb65af6a588dbe6bb9 [formerly 5feae98b68a62be825cb8a6aff6c4be79940115f]
    Former-commit-id: 6719c2e013697bc82574066fedb65e057405e1cc

--- tests/tools/Testing/src/org/kframework/main/Main.java
+++ tests/tools/Testing/src/org/kframework/main/Main.java
@@ -1,7 +1,11 @@
 package org.kframework.main;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -37,6 +41,8 @@ public class Main {
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
+
+			copyFolder(new File("/var/lib/jenkins/workspace/k-framework"), new File("k"));
 			
 			// setup the new path
 			Configuration.HOME_DIR = "/var/lib/jenkins/workspace/k-framework-tests/k";
@@ -183,4 +189,46 @@ public class Main {
 		}
 
 	}
+	
+	public static void copyFolder(File src, File dest)
+	    	throws IOException{
+	 
+	    	if(src.isDirectory()){
+	 
+	    		//if directory not exists, create it
+	    		if(!dest.exists()){
+	    		   dest.mkdir();
+	    		   System.out.println("Directory copied from " 
+	                              + src + "  to " + dest);
+	    		}
+	 
+	    		//list all the directory contents
+	    		String files[] = src.list();
+	 
+	    		for (String file : files) {
+	    		   //construct the src and dest file structure
+	    		   File srcFile = new File(src, file);
+	    		   File destFile = new File(dest, file);
+	    		   //recursive copy
+	    		   copyFolder(srcFile,destFile);
+	    		}
+	 
+	    	}else{
+	    		//if file, then copy it
+	    		//Use bytes stream to support all file types
+	    		InputStream in = new FileInputStream(src);
+	    	        OutputStream out = new FileOutputStream(dest); 
+	 
+	    	        byte[] buffer = new byte[1024];
+	 
+	    	        int length;
+	    	        //copy the file content in bytes 
+	    	        while ((length = in.read(buffer)) > 0){
+	    	    	   out.write(buffer, 0, length);
+	    	        }
+	 
+	    	        in.close();
+	    	        out.close();
+	    	        System.out.println("File copied from " + src + " to " + dest);
+	    	}
 }
