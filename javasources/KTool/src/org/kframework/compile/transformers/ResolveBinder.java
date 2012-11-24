9674a93a4ec86ae2c9c19aea95bf9c0e8a1a622b
Andrei Ștefănescu <andreistef@gmail.com>
2012-11-24 22:32:18 +0000

    git-svn-id: http://k-framework.googlecode.com/svn/trunk@8763 82a86efa-2583-11df-8ea0-49ba94de29ab
    Former-commit-id: 9288cc4f43efcf0d2f704aa89261c19cac7e2af8 [formerly 1e62ab67f0b9175fc751b46b565c57bf8dd243be]
    Former-commit-id: c7eeb2dfa685d88d33d3b264648decf84f41a0de

--- javasources/KTool/src/org/kframework/compile/transformers/ResolveBinder.java
+++ javasources/KTool/src/org/kframework/compile/transformers/ResolveBinder.java
@@ -7,25 +7,24 @@ import org.kframework.kil.visitors.CopyOnWriteTransformer;
 import org.kframework.kil.visitors.exceptions.TransformerException;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
 public class ResolveBinder extends CopyOnWriteTransformer {
 
-	private static final Constant BINDER_PREDICATE
-			= new Constant("KLabel", AddPredicates.predicate("Binder"));
-	//    private static final Constant BINDER_STAR_PREDICATE
-//            = new Constant("KLabel", AddPredicates.predicate("BinderStar"));
-	private static final Constant BOUNDED_PREDICATE
-			= new Constant("KLabel", AddPredicates.predicate("Bound"));
-	private static final Constant BOUNDING_PREDICATE
-			= new Constant("KLabel", AddPredicates.predicate("Bounding"));
+  private static final Constant BINDER_PREDICATE 
+    = new Constant("KLabel", AddPredicates.predicate("Binder"));
+  private static final Constant BOUNDED_PREDICATE
+    = new Constant("KLabel", AddPredicates.predicate("Bound"));
+  private static final Constant BOUNDING_PREDICATE
+    = new Constant("KLabel", AddPredicates.predicate("Bounding"));
 
-	private static final String REGEX
-			//= "\\s*(\\d+)(\\*?)\\s*-\\>\\s*(\\d+)\\s*(,\\s*(\\d+)(\\*?)\\s*-\\>\\s*(\\d+)\\s*)*";
-			= "\\s*(\\d+)(\\*?)\\s*-\\>\\s*(\\d+)\\s*(,?)";
+  private static final String REGEX
+    = "\\s*(\\d+)(\\s*-\\>\\s*(\\d+))?\\s*(,?)";
 
 	public ResolveBinder() {
 		super("Resolve binder");
@@ -34,103 +33,91 @@ public class ResolveBinder extends CopyOnWriteTransformer {
 	@Override
 	public ASTNode transform(Module node) throws TransformerException {
 		List<Production> prods = GetSyntaxByTag.applyVisitor(node, "binder");
-		if (prods.isEmpty()) return node;
+		if (prods.isEmpty())
+      return node;
+
 		List<ModuleItem> items = new ArrayList<ModuleItem>(node.getItems());
 		node = node.shallowCopy();
-		for (Production prod : prods) {
-			String bindInfo = prod.getAttributes().get("binder");
-			if (bindInfo == null)
-				bindInfo = "";
-			if (bindInfo == "") {
-				bindInfo = "1->" + prod.getArity();
-			}
-			Pattern p = Pattern.compile(REGEX);
-			Matcher m = p.matcher(bindInfo);
-
-			int bndIdx = -1;
-			Boolean isStar = false;
-			while (m.regionStart() < m.regionEnd()) {
-				if (!m.lookingAt()) {
-					System.err.println("[error:] could not parse binder attribute \"" + bindInfo.substring(m.regionStart(), m.regionEnd()) + "\"");
-					System.exit(1);
-				}
-				if (m.end() < m.regionEnd()) {
-					if (!m.group(4).equals(",")) {
-						System.err.println("[error:] expecting ',' after \"" + m.group() + "\"");
-						System.exit(1);
-					}
-				} else {
-					if (!m.group(4).equals("")) {
-						System.err.println("[error:] unexpected ',' after \"" + m.group() + "\"");
-						System.exit(1);
-					}
-				}
-
-				if (bndIdx != -1) {
-					if (bndIdx != Integer.parseInt(m.group(1))) {
-						System.err.println("[error:] multiple bounded arguments: " + bndIdx + "," + m.group(1));
-						System.exit(1);
-					}
-					if (isStar != m.group(2).equals("*")) {
-						System.err.println("[error:] argument " + bndIdx + " is declared as variable and collection");
-						System.exit(1);
-					}
-				} else {
-					bndIdx = Integer.parseInt(m.group(1));
-					if (bndIdx == 0 || bndIdx > prod.getArity())  {
-						System.err.println("[error:] argument index out of bounds: " + bndIdx);
-						System.exit(1);
-					}
-					isStar = m.group(2).equals("*");
-				}
-
-				int termIdx = Integer.parseInt(m.group(3));
-
-				Constant klblCt = new Constant("KLabel", prod.getKLabel());
-				Term kapp = new KApp(new KInjectedLabel(klblCt), Empty.ListOfK);
-				ListOfK list = new ListOfK();
-				list.getContents().add(kapp);
-				list.getContents().add(new Constant("Int", Integer.toString(termIdx)));
-				Rule rule = new Rule(
-						new KApp(new Constant("KLabel", "isBounding"), list),
-						Constant.TRUE);
-				rule.getAttributes().getContents().add(new Attribute("anywhere", ""));
-				items.add(rule);
-
-				m.region(m.end(), m.regionEnd());
-			}
-
-			Rule rule = new Rule(
-					new KApp(new Constant("KLabel", "isBinder"), MetaK.getTerm(prod)),
-					Constant.TRUE);
-			rule.getAttributes().getContents().add(new Attribute("anywhere", ""));
-			items.add(rule);
-
-//      if (isStar) {
-//        rule = new Rule(
-//            new KApp(new Constant("KLabel", "isStarBinder"), MetaK.getTerm(prod)),
-//            Constant.TRUE);
-//        rule.getAttributes().getContents().add(new Attribute("anywhere", ""));
-//			  items.add(rule);
-//      }
-
-			Constant klblCt = new Constant("KLabel", prod.getKLabel());
-			Term kapp = new KApp(new KInjectedLabel(klblCt), Empty.ListOfK);
-			ListOfK list = new ListOfK();
-			list.getContents().add(kapp);
-			list.getContents().add(new Constant("Int", Integer.toString(bndIdx)));
-			Term term = Constant.TRUE;
-			if (isStar) {
-				term = Constant.STAR;
-			}
-			rule = new Rule(
-					new KApp(new Constant("KLabel", "isBound"), list),
-					term);
-			rule.getAttributes().getContents().add(new Attribute("anywhere", ""));
-			items.add(rule);
-
-		}
 		node.setItems(items);
+
+		for (Production prod : prods) {
+      String bindInfo = prod.getAttributes().get("binder");
+      if (bindInfo == null || bindInfo.equals(""))
+        bindInfo = "1->" + prod.getArity();
+      Pattern p = Pattern.compile(REGEX);
+      Matcher m = p.matcher(bindInfo);
+      Map<Integer, Integer> bndMap = new HashMap<Integer, Integer>();
+
+      while (m.regionStart() < m.regionEnd()) {
+        if (!m.lookingAt()) {
+          System.err.println("[error:] could not parse binder attribute \"" + bindInfo.substring(m.regionStart(), m.regionEnd()) + "\"");
+          System.exit(1);
+        }
+        if (m.end() < m.regionEnd()) {
+          if (!m.group(4).equals(",")) {
+            System.err.println("[error:] expecting ',' at the end \"" + m.group() + "\"");
+            System.exit(1);
+          }
+        } else {
+          if (!m.group(4).equals("")) {
+            System.err.println("[error:] unexpected ',' at the end \"" + m.group() + "\"");
+            System.exit(1);
+          }
+        }
+
+        int bndIdx = Integer.parseInt(m.group(1));
+        if (m.group(3) == null) {
+          for (int idx = 1; idx <= prod.getArity(); idx++) {
+            if (idx != bndIdx)
+              bndMap.put(bndIdx, idx);
+          }
+        } else {
+          bndMap.put(bndIdx, Integer.parseInt(m.group(3)));
+        }
+
+        m.region(m.end(), m.regionEnd());
+      } 
+
+      Rule rule = new Rule(
+          new KApp(BINDER_PREDICATE, MetaK.getTerm(prod)),
+          Constant.TRUE);
+      rule.getAttributes().getContents().add(new Attribute("anywhere", ""));
+			items.add(rule);
+
+      Constant klblCt = new Constant("KLabel", prod.getKLabel());
+      Term klblK = new KApp(new KInjectedLabel(klblCt), Empty.ListOfK);
+
+      for (int bndIdx : bndMap.keySet()) {
+        ListOfK list = new ListOfK();
+        list.getContents().add(klblK);
+        list.getContents().add(new Constant("Int", Integer.toString(bndIdx)));
+        rule = new Rule(new KApp(BOUNDED_PREDICATE, list), Constant.TRUE);
+			  rule.getAttributes().getContents().add(new Attribute("anywhere", ""));
+			  items.add(rule);
+      }
+
+      for (int bodyIdx : bndMap.values()) {
+        ListOfK list = new ListOfK();
+        list.getContents().add(klblK);
+        list.getContents().add(new Constant("Int", Integer.toString(bodyIdx)));
+        rule = new Rule(new KApp(BOUNDING_PREDICATE, list), Constant.TRUE);
+			  rule.getAttributes().getContents().add(new Attribute("anywhere", ""));
+			  items.add(rule);
+      }
+
+/*
+if (bndIdx == 0 || bndIdx > prod.getArity())  {
+          System.err.println("[error:] argument index out of bounds: " + bndIdx);
+          System.exit(1);
+        }
+
+if (bndMap.containsKey(bndIndex)) {
+            System.err.println("[error:] " + bndIdx );
+            System.exit(1);
+          }
+*/
+		}
+
 		return node;
 	}
 }
