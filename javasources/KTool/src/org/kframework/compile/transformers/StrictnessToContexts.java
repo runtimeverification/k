f8c312456ac97198b6a96595d3dc503d897b5f1f
Cansu Erdogan <csnerd@gmail.com>
2012-11-26 13:37:36 +0000

    git-svn-id: http://k-framework.googlecode.com/svn/trunk@8780 82a86efa-2583-11df-8ea0-49ba94de29ab
    Former-commit-id: d7a8fc1c7248f836a727eaa618fbcdff31e6a7c3 [formerly 40895118233a3b6561e0feaba1cd3549ba3d8a6e]
    Former-commit-id: e1f091657e610c339e25221c33f77e5930166568

--- javasources/KTool/src/org/kframework/compile/transformers/StrictnessToContexts.java
+++ javasources/KTool/src/org/kframework/compile/transformers/StrictnessToContexts.java
@@ -1,16 +1,28 @@
 package org.kframework.compile.transformers;
 
-import org.kframework.compile.utils.GetSyntaxByTag;
-import org.kframework.compile.utils.MetaK;
-import org.kframework.kil.*;
-import org.kframework.kil.visitors.CopyOnWriteTransformer;
-import org.kframework.kil.visitors.exceptions.TransformerException;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.StringTokenizer;
+
+import org.kframework.compile.utils.GetSyntaxByTag;
+import org.kframework.compile.utils.MetaK;
+import org.kframework.kil.ASTNode;
+import org.kframework.kil.Attributes;
+import org.kframework.kil.Context;
+import org.kframework.kil.Hole;
+import org.kframework.kil.Module;
+import org.kframework.kil.ModuleItem;
+import org.kframework.kil.Production;
+import org.kframework.kil.TermCons;
+import org.kframework.kil.visitors.CopyOnWriteTransformer;
+import org.kframework.kil.visitors.exceptions.TransformerException;
+import org.kframework.utils.errorsystem.KException;
+import org.kframework.utils.errorsystem.KException.ExceptionType;
+import org.kframework.utils.errorsystem.KException.KExceptionGroup;
+import org.kframework.utils.general.GlobalSettings;
 
 public class StrictnessToContexts extends CopyOnWriteTransformer {
+
+	private List<ModuleItem> items;
 
 	public StrictnessToContexts() {
 		super("Strict Ops To Context");
@@ -25,171 +37,222 @@ public class StrictnessToContexts extends CopyOnWriteTransformer {
 //			System.out.println(contexts.get(i));
 //		}
 
-
 		//collect productions that are strict or seqstrict
 		List<Production> prods = GetSyntaxByTag.applyVisitor(node, "strict");
 		prods.addAll(GetSyntaxByTag.applyVisitor(node, "seqstrict"));
 
+//		System.out.println("------------------");	
+
 		if (prods.isEmpty()) return node;
-		List<ModuleItem> items = new ArrayList<ModuleItem>(node.getItems());
+		items = new ArrayList<ModuleItem>(node.getItems());
 		node = node.shallowCopy();
 
-//		System.out.println();		
+		String arg, attr = "";
 
 		for (Production prod : prods) {
-			if (prod.containsAttribute("strict")){
-				String arg = prod.getAttribute("strict");
-//				System.out.println("strict: " + arg);
 
-				//strict in only one argument
-				if(!arg.equalsIgnoreCase("") && arg.indexOf(",") == -1){
-					int argi = Integer.parseInt(arg);
+			//			System.out.println(prod.toString());
 
-					int size = ((TermCons) MetaK.getTerm(prod)).getContents().size(); 
+			Attributes attributes = prod.getAttributes();
 
-//					System.out.println("Contents: " + ((TermCons) MetaK.getTerm(prod)).getContents() + "\nsize: " + size);
+			if (attributes.containsKey("strict")){
+				attr = "strict";
+			}else if(attributes.containsKey("seqstrict")){
+				attr = "seqstrict";
+			}else continue;
 
-					int co = 0;
+			arg = attributes.get(attr);
+			//			System.out.println(attr + ": " + arg);
 
-					TermCons tc = (TermCons) MetaK.getTerm(prod);
+			//strict in only one argument
+			if(!arg.equalsIgnoreCase("") && arg.indexOf(",") == -1){
+				strictInOne(attr, prod, arg);
 
-					while(co < size){
-						if(co == argi-1){
-							tc.getContents().set(co, new Hole("K"));
-						}
-						//						else{
-						//							tc.getContents().set(co, MetaK.getFreshVar(tc.getContents().get(co).getSort()));
-						//						}
-						co++;
-					}
+				//strict in all arguments
+			}else if(arg.equalsIgnoreCase("")){
+				strictInAll(attr, prod);
 
-					Context ctx = new Context();
-					ctx.setBody(tc);
-
-//					System.out.println(ctx.toString());
-
-					items.add(ctx);
-
-					//strict in all arguments
-				}else if(arg.equalsIgnoreCase("")){
-
-					int size = ((TermCons) MetaK.getTerm(prod)).getContents().size(); 
-
-//					System.out.println("Contents: " + ((TermCons) MetaK.getTerm(prod)).getContents() + "\nsize: " + size);
-
-					int co = 0, i = 0;
-
-					while(co < size){
-						TermCons tc = (TermCons) MetaK.getTerm(prod);
-
-						while(i < size){
-
-							if(i == co)
-								tc.getContents().set(i, new Hole("K"));
-							//							else
-							//								tc.getContents().set(i, MetaK.getFreshVar(tc.getContents().get(i).getSort()));
-
-							i++;
-
-						}
-
-						Context ctx = new Context();
-						ctx.setBody(tc);
-
-//						System.out.println(ctx.toString());
-
-						items.add(ctx);
-
-						i = 0;
-						co++;
-					}
-
-
-					//strict in more than one arguments
-				}else{
-
-					StringTokenizer st = new StringTokenizer(arg, ", ");
-
-					String[] s = new String[st.countTokens()];
-					int j = 0;
-
-					while(st.hasMoreTokens()){s[j] = st.nextToken(); j++;}
-
-					int size = ((TermCons) MetaK.getTerm(prod)).getContents().size(); 
-
-//					System.out.println("Contents: " + ((TermCons) MetaK.getTerm(prod)).getContents() + "\nsize: " + size);
-
-					int co = 0, i = 0;
-
-					while(co < s.length){
-						TermCons tc = (TermCons) MetaK.getTerm(prod);
-
-						while(i < size){
-
-							if(i == Integer.parseInt(s[co])-1)
-								tc.getContents().set(i, new Hole("K"));
-							//							else
-							//								tc.getContents().set(i, MetaK.getFreshVar(tc.getContents().get(i).getSort()));
-
-							i++;
-						}
-
-						Context ctx = new Context();
-						ctx.setBody(tc);
-
-//						System.out.println(ctx.toString());
-
-						items.add(ctx);
-
-						co++;
-						i = 0;
-					}
-				}
-
-				prod.getAttributes().remove("strict");
+				//strict in more than one arguments
+			}else{
+				strictInMore(attr, prod, arg);
 			}
 
-			if (prod.containsAttribute("seqstrict")){
-//				System.out.println("seqstrict: " + attributes.get("seqstrict"));
-
-				int size = ((TermCons) MetaK.getTerm(prod)).getContents().size(); 
-
-//				System.out.println("Contents: " + ((TermCons) MetaK.getTerm(prod)).getContents() + "\nsize: " + size);
-
-				int co = 0, i = 0;
-
-				while(co < size){
-					TermCons tc = (TermCons) MetaK.getTerm(prod);
-
-					while(i < size){
-
-						if(i == co)
-							tc.getContents().set(i, new Hole("K"));
-						//						else
-						//							tc.getContents().set(i, MetaK.getFreshVar(tc.getContents().get(i).getSort()));
-
-						i++;
-
-					}
-
-					Context ctx = new Context();
-					ctx.setBody(tc);
-
-//					System.out.println(ctx.toString());
-
-					items.add(ctx);
-
-					i = 0;
-					co++;
-				}
-
-				prod.getAttributes().remove("seqstrict");
-			}
-
+			prod.getAttributes().remove(attr);
 		}
 
-		node.setItems(items);
+		node.setItems(items);	
+
+//		contexts = node.getItems();
+//
+//		for (int i = 0; i < contexts.size(); i++) {
+//			System.out.println(contexts.get(i));
+//		}
 
 		return node;
+	}
+
+
+	private void strictInMore(String attr, Production prod, String arg) {
+		//		ArrayList<Integer> seqargs = new ArrayList<Integer>();
+
+		int size = ((TermCons) MetaK.getTerm(prod)).getContents().size(); 
+
+		//		System.out.println("Contents: " + ((TermCons) MetaK.getTerm(prod)).getContents() + "\nsize: " + size);
+
+		//case: (1,) or (,)
+		if(arg.endsWith(",")){
+			String msg ="Make sure the given argument is correct (" + arg + ")";
+			GlobalSettings.kem.register(new KException(ExceptionType.ERROR, KExceptionGroup.COMPILER, msg, getName(), prod.getFilename(), prod.getLocation()));
+		}
+
+		String[] s = arg.split("[,]+");
+
+		for (int i = 0; i < s.length; i++) {
+			s[i] = s[i].trim();
+
+			//			System.out.println("trim" + s[i] + "trim");
+
+			//case: (,2)
+			if(s[i].equalsIgnoreCase("")){
+				String msg ="Wrong argument (" + arg + ") given to strict attribute";
+				GlobalSettings.kem.register(new KException(ExceptionType.ERROR, KExceptionGroup.COMPILER, msg, getName(), prod.getFilename(), prod.getLocation()));
+			}
+
+			//case: strict(1,3) when there are only 2 terms
+			if(Integer.parseInt(s[i]) > size){
+				String msg ="Production does not have this (" + Integer.parseInt(s[i]) + ") many terms";
+				GlobalSettings.kem.register(new KException(ExceptionType.ERROR, KExceptionGroup.COMPILER, msg, getName(), prod.getFilename(), prod.getLocation()));
+			}
+
+			//			if(attr.equalsIgnoreCase("seqstrict"))
+			//				seqargs.add(Integer.parseInt(s[i]));
+		}
+
+		//		for (int i = 0; i < s.length; i++) {
+		//			System.out.println("s"+i+": " + s[i]);
+		//		}
+
+		int co = 0, i = 0;
+
+		while(co < s.length){
+			TermCons tc = (TermCons) MetaK.getTerm(prod);
+
+			while(i < size){
+
+				if(i == Integer.parseInt(s[co])-1){
+					//					System.out.println("i: " + i + " s[co]: " + s[co]);
+					tc.getContents().set(i, new Hole("K"));
+
+					if(attr.equalsIgnoreCase("seqstrict") && co > 0){
+						for (int j = 0; j < co; j++) {
+							//							tc.getContents().set(seqargs.get(j), MetaK.getFreshVar("KResult"));
+							tc.getContents().get(Integer.parseInt(s[j])-1).setSort("KResult");
+						}
+					}
+				}else
+					tc.getContents().set(i, MetaK.getFreshVar(tc.getContents().get(i).getSort()));
+
+
+				i++;
+			}
+
+			Context ctx = new Context();
+			ctx.setBody(tc);
+
+			//			System.out.println(ctx.toString());
+
+			items.add(ctx);
+
+			co++;
+			i = 0;
+		}
+	}
+
+	private void strictInOne(String attr, Production prod, String arg) {
+
+		int argi = 0;
+
+		try{
+			argi = Integer.parseInt(arg);
+		}catch(NumberFormatException nfe){
+
+			//case: (1 2)
+			String msg = "Wrong argument (" + arg + ") given to strict attribute. Make sure arguments are separated by a comma.";
+			GlobalSettings.kem.register(new KException(ExceptionType.ERROR, KExceptionGroup.COMPILER, msg, getName(), prod.getFilename(), prod.getLocation()));
+		}
+
+		int size = ((TermCons) MetaK.getTerm(prod)).getContents().size(); 
+
+		//case: strict(3) when there are only 2 terms
+		if(argi > size){
+			String msg ="Production does not have this (" + argi + ") many terms";
+			GlobalSettings.kem.register(new KException(ExceptionType.ERROR, KExceptionGroup.COMPILER, msg, getName(), prod.getFilename(), prod.getLocation()));
+		}
+
+		//		System.out.println("Contents: " + ((TermCons) MetaK.getTerm(prod)).getContents() + "\nsize: " + size);
+
+		int co = 0;
+
+		TermCons tc = (TermCons) MetaK.getTerm(prod);
+
+		while(co < size){
+			if(co == argi-1){
+				tc.getContents().set(co, new Hole("K"));
+			}
+			else{
+				tc.getContents().set(co, MetaK.getFreshVar(tc.getContents().get(co).getSort()));
+			}
+			co++;
+		}
+
+		Context ctx = new Context();
+		ctx.setBody(tc);
+
+		//		System.out.println(ctx.toString());
+
+		items.add(ctx);
+	}
+
+
+	private void strictInAll(String attr, Production prod){
+
+		int size = ((TermCons) MetaK.getTerm(prod)).getContents().size(); 
+
+		//		System.out.println("Contents: " + ((TermCons) MetaK.getTerm(prod)).getContents() + "\nsize: " + size);
+
+		int co = 0, i = 0;
+
+		while(co < size){
+			TermCons tc = (TermCons) MetaK.getTerm(prod);
+
+			while(i < size){
+
+				if(i == co){
+					tc.getContents().set(i, new Hole("K"));
+
+					if(attr.equalsIgnoreCase("seqstrict")){
+
+						for (int j = 0; j < i; j++) {
+							//								tc.getContents().set(seqargs.get(j), MetaK.getFreshVar("KResult"));
+							tc.getContents().get(j).setSort("KResult");
+						}
+					}
+				}else
+					tc.getContents().set(i, MetaK.getFreshVar(tc.getContents().get(i).getSort()));
+
+				i++;
+
+			}
+
+			Context ctx = new Context();
+			ctx.setBody(tc);
+
+			//			System.out.println(ctx.toString());
+
+			items.add(ctx);
+
+			i = 0;
+			co++;
+		}
 	}
 }
