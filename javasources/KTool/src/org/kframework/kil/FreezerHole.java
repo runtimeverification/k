4dba072fef3fb98123e46ff33c7a34a6d5159018
Ștefan Ciobâcă <stefan.ciobaca@gmail.com>
2012-12-14 13:29:35 +0000

    added FreezerHole class
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@9055 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: cf7fa5a08e2f98a98b64ac03996f15afd0397833 [formerly 907c15ffa10928381c378085f50d3711642595a0]
    Former-commit-id: 8394e780bd3077f9e68c63e220b2409a65544b32

---
+++ javasources/KTool/src/org/kframework/kil/FreezerHole.java
@@ -0,0 +1,35 @@
+package org.kframework.kil;
+
+import org.kframework.kil.visitors.Transformer;
+import org.kframework.kil.visitors.Visitor;
+import org.kframework.kil.visitors.exceptions.TransformerException;
+import org.w3c.dom.Element;
+
+public class FreezerHole extends Term {
+	private int index;
+	
+	public FreezerHole(int index) {
+		super("K");
+		this.index = index;
+	}
+	
+	public FreezerHole(Element element) {
+		// TODO: for Radu
+		super(element);
+	}
+
+	@Override
+	public void accept(Visitor visitor) {
+		visitor.visit(this);
+	}
+
+	@Override
+	public ASTNode accept(Transformer visitor) throws TransformerException {
+		return visitor.transform(this);
+	}
+
+	@Override
+	public Term shallowCopy() {
+		return new FreezerHole(this.index);
+	}
+}
