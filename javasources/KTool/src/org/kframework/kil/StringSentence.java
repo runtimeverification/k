fb605e7837b86b14678cc5b950bc6c0b94072b1e
Patrick Meredith <pmeredit@gmail.com>
2013-01-22 13:54:31 +0000

    fixed build break
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@9241 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: 44af9a50bb569e0a240c46b4b348d6c4286adb1f [formerly 2bc44750622e480c730fd969fe711e516fb59604]
    Former-commit-id: 6f948443f0820b77cf048705b4100a1aa7dbe873

--- javasources/KTool/src/org/kframework/kil/StringSentence.java
+++ javasources/KTool/src/org/kframework/kil/StringSentence.java
@@ -2,7 +2,6 @@ package org.kframework.kil;
 
 import org.kframework.kil.loader.Constants;
 import org.kframework.kil.visitors.Transformer;
-import org.kframework.kil.matchers.Matcher;
 import org.kframework.kil.visitors.Visitor;
 import org.kframework.kil.visitors.exceptions.TransformerException;
 import org.kframework.utils.StringUtil;
@@ -45,12 +44,6 @@ public class StringSentence extends ModuleItem {
 	public ASTNode accept(Transformer visitor) throws TransformerException {
 		return visitor.transform(this);
 	}
-
-  @Override
-  public void accept(Matcher matcher, ASTNode toMatch){
-    matcher.match(this, toMatch);
-  }
-
 
 	public String getContent() {
 		return content;
--- javasources/KTool/src/org/kframework/kil/Syntax.java
+++ javasources/KTool/src/org/kframework/kil/Syntax.java
@@ -4,7 +4,6 @@ import org.kframework.compile.utils.MetaK;
 import org.kframework.kil.loader.Constants;
 import org.kframework.kil.loader.JavaClassesFactory;
 import org.kframework.kil.visitors.Transformer;
-import org.kframework.kil.matchers.Matcher;
 import org.kframework.kil.visitors.Visitor;
 import org.kframework.kil.visitors.exceptions.TransformerException;
 import org.kframework.utils.xml.XML;
@@ -119,12 +118,6 @@ public class Syntax extends ModuleItem {
 	public ASTNode accept(Transformer visitor) throws TransformerException {
 		return visitor.transform(this);
 	}
-
-  @Override
-  public void accept(Matcher matcher, ASTNode toMatch){
-    matcher.match(this, toMatch);
-  }
-
 
 	@Override
 	public boolean equals(Object obj) {
