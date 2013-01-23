71938e7a7ce5501fb01ca982af6095cb447c2335
Patrick Meredith <pmeredit@gmail.com>
2013-01-23 18:16:35 +0000

    rewriter works
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@9258 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: e8cad5417c6c6d26b1670ac5c1458fe06f9d64e3 [formerly 6f2c176cb3684b9ff48c075b7e4a4b99c0f6f514]
    Former-commit-id: eb11153286e1b61c62ef4356bb255f909687eb64

--- javasources/KTool/src/org/kframework/kil/rewriter/SimpleRewriter.java
+++ javasources/KTool/src/org/kframework/kil/rewriter/SimpleRewriter.java
@@ -1,5 +1,12 @@
 package org.kframework.kil.rewriter;
 
+//unit test imports
+import org.kframework.kil.Constant;
+import org.kframework.kil.KApp;
+import org.kframework.kil.KList;
+import org.kframework.kil.Variable;
+//end test imports
+
 import org.kframework.kil.Rewrite;
 import org.kframework.kil.Term;
 
@@ -10,6 +17,8 @@ import org.kframework.kil.matchers.SimpleMatcher;
 import org.kframework.kil.visitors.exceptions.TransformerException;
 
 import org.kframework.compile.utils.Substitution;
+
+
 
 /** 
  * This class implements Term Rewriting based on the
@@ -81,10 +90,56 @@ public class SimpleRewriter {
   static public Term rewriteN(RewriteSet trs, Term t, int n){
     Term out = t;
     Term temp = t;
-    for(int i = 0; (i < n) && (temp != null); ++i){
+    for(int i = 0; (i <= n) && (temp != null); ++i){
       out = temp;
       temp = rewrite(trs, out);
     } 
     return out;
+  }
+
+  public static void main(String[] args){
+    KList patternGuts = new KList();
+    KList termGuts = new KList();
+    KList subtermGuts = new KList();
+    KList rhsGuts = new KList();
+    patternGuts.add(new Variable("x", "KLabel"));
+    patternGuts.add(new Variable("y", "KLabel"));
+    patternGuts.add(new Variable("z", "K"));
+    patternGuts.add(new Variable("x", "KLabel"));
+    subtermGuts.add(Constant.KLABEL("d"));
+    subtermGuts.add(Constant.KLABEL("e"));
+    KApp subterm = new KApp(Constant.KLABEL("bar"), subtermGuts);
+    termGuts.add(Constant.KLABEL("a"));
+    termGuts.add(Constant.KLABEL("b"));
+    termGuts.add(subterm);
+    termGuts.add(Constant.KLABEL("a"));
+    rhsGuts.add(Constant.KLABEL("42"));
+    rhsGuts.add(new Variable("x", "KLabel"));
+    rhsGuts.add(new Variable("z", "K"));
+    KApp pattern = new KApp(Constant.KLABEL("foo"), patternGuts);
+    KApp term = new KApp(Constant.KLABEL("foo"), termGuts);
+    KApp rhs = new KApp(Constant.KLABEL("car"), rhsGuts);
+    System.out.println(pattern);
+    System.out.println(term);
+    Matcher m = new SimpleMatcher();
+    pattern.accept(m, term);
+    System.out.println(m.getSubstitution());
+    System.out.println("==================");
+    RewriteSet trs = new RewriteSet();
+    trs.add(new Rewrite(pattern, rhs));
+    System.out.println(trs);
+    System.out.println("==============>");
+    System.out.println(rewrite(trs, term));
+    System.out.println("==================");
+    //add another rewrite that matches the rhs of the previous rewrite
+    //and just rewrites to variable z
+    trs.add(new Rewrite(rhs, new Variable("z", "K")));
+    //add another rewrite that matches z from above (which is java variable subterm)
+    trs.add(new Rewrite(subterm, Constant.KLABEL("DONE")));
+    System.out.println(trs);
+    System.out.println("rewrite 1: " + rewriteN(trs,term,1)); //should print same as last time
+    System.out.println("rewrite 2: " + rewriteN(trs,term,2)); //should print bar(d ,, e)
+    System.out.println("rewrite 3: " + rewriteN(trs,term,3)); //should print DONE
+    System.out.println("rewrite normal form: " + rewriteToNormalForm(trs,term)); //should print DONE
   }
 }
