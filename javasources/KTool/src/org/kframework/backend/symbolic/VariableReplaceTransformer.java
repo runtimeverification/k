e3468ae811994773ffca9a718a8205e862c74b45
Andrei Arusoai <Andrei.Arusoaie@gmail.com>
2013-02-27 15:31:00 +0000

    Smart linearising.
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@9481 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: 7e93cbd839e64447bcd98845ea211e0754c47b7f [formerly bf10234637516f01c0a562e06b44bbfc29435151]
    Former-commit-id: da589972572ca2291d9bfcd105c5f03625bf8d71

--- javasources/KTool/src/org/kframework/backend/symbolic/VariableReplaceTransformer.java
+++ javasources/KTool/src/org/kframework/backend/symbolic/VariableReplaceTransformer.java
@@ -1,7 +1,9 @@
 package org.kframework.backend.symbolic;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.kframework.compile.utils.MetaK;
 import org.kframework.kil.ASTNode;
@@ -12,21 +14,26 @@ import org.kframework.kil.visitors.exceptions.TransformerException;
 public class VariableReplaceTransformer extends BasicTransformer {
 
 	private Map<Variable, Variable> generatedVariables;
+	private Set<String> vars;
 
-	
 	public VariableReplaceTransformer(String name) {
 		super("Replace Variables");
 		generatedVariables = new HashMap<Variable, Variable>();
+		vars = new HashSet<String>();
 	}
 
-	
 	@Override
 	public ASTNode transform(Variable node) throws TransformerException {
-		Variable newVar = MetaK.getFreshVar(node.getSort());
-		generatedVariables.put(newVar, node);
+		Variable newVar = node;
+		if (vars.contains(node.getName())) {
+			newVar = MetaK.getFreshVar(node.getSort());
+			generatedVariables.put(newVar, node);
+		}
+
+		vars.add(node.getName());
 		return newVar;
 	}
-	
+
 	public Map<Variable, Variable> getGeneratedVariables() {
 		return generatedVariables;
 	}
