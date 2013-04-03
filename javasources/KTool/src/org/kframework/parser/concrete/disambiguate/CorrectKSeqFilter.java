6579b9d91af8084c03ebe9d882826c4fefef5ac6
Radu Mereuta <headness13@gmail.com>
2013-04-03 20:49:53 +0000

    Kompile - improved KSequence and rewrite disambiguation. Part 2.
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@9753 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: daacf2d2bdbc7b1a8dde674c0d8090d4dcd6874d [formerly 16e6e77f3d8bd014c5c7bab70fd1fb078dbc9f5c]
    Former-commit-id: f9067bbb55f6e99aad09825e54a038198ad264bf

--- javasources/KTool/src/org/kframework/parser/concrete/disambiguate/CorrectKSeqFilter.java
+++ javasources/KTool/src/org/kframework/parser/concrete/disambiguate/CorrectKSeqFilter.java
@@ -1,10 +1,10 @@
 package org.kframework.parser.concrete.disambiguate;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.kframework.kil.ASTNode;
 import org.kframework.kil.Ambiguity;
-import org.kframework.kil.KList;
 import org.kframework.kil.KSequence;
 import org.kframework.kil.ProductionItem.ProductionType;
 import org.kframework.kil.Term;
@@ -22,6 +22,22 @@ public class CorrectKSeqFilter extends BasicTransformer {
 
 	public CorrectKSeqFilter() {
 		super("Correct K sequences");
+	}
+
+	public ASTNode transform(Ambiguity amb) throws TransformerException {
+		List<Term> children = new ArrayList<Term>();
+		for (Term t : amb.getContents()) {
+			if (t instanceof KSequence) {
+				children.add(t);
+			}
+		}
+
+		if (children.size() == 0 || children.size() == amb.getContents().size())
+			return super.transform(amb);
+		if (children.size() == 1)
+			return children.get(0).accept(this);
+		amb.setContents(children);
+		return super.transform(amb);
 	}
 
 	@Override
--- javasources/KTool/src/org/kframework/parser/concrete/disambiguate/CorrectRewritePriorityFilter.java
+++ javasources/KTool/src/org/kframework/parser/concrete/disambiguate/CorrectRewritePriorityFilter.java
@@ -1,6 +1,7 @@
 package org.kframework.parser.concrete.disambiguate;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.kframework.kil.ASTNode;
 import org.kframework.kil.Ambiguity;
@@ -24,6 +25,22 @@ public class CorrectRewritePriorityFilter extends BasicTransformer {
 
 	public CorrectRewritePriorityFilter() {
 		super("Correct Rewrite priority");
+	}
+
+	public ASTNode transform(Ambiguity amb) throws TransformerException {
+		List<Term> children = new ArrayList<Term>();
+		for (Term t : amb.getContents()) {
+			if (t instanceof Rewrite) {
+				children.add(t);
+			}
+		}
+
+		if (children.size() == 0 || children.size() == amb.getContents().size())
+			return super.transform(amb);
+		if (children.size() == 1)
+			return children.get(0).accept(this);
+		amb.setContents(children);
+		return super.transform(amb);
 	}
 
 	@Override
