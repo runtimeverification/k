9f65a9a36065c367e18cb69da76194ba2b2aec27
Radu Mereuta <headness13@gmail.com>
2013-05-21 15:53:07 +0000

    K3 - fixing eclipse and windows executable after /lib refactoring.
    git-svn-id: http://k-framework.googlecode.com/svn/branches/v3.2@10096 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: dcd7b1d8b2bdd0268053810dbdf6e97ed39e7d0f [formerly 68f7b3c8cfcaa87f501040bddcb69d87287c8a62]
    Former-commit-id: dd31fb8fb5491c26fbb291dcbc614cf5b749d572

--- dist/bin/kompile.bat
+++ dist/bin/kompile.bat
@@ -1,2 +1,2 @@
 @ECHO off
-java -ea -ss8m -Xms64m -Xmx1G -jar "%~dp0java\k3.jar" -kompile %*
+java -ea -ss8m -Xms64m -Xmx1G -jar "%~dp0..\lib\java\k3.jar" -kompile %*
--- dist/bin/krun.bat
+++ dist/bin/krun.bat
@@ -1,2 +1,2 @@
 @ECHO off
-java -jar -Xmx1024m "%~dp0java\k3.jar" -krun %*
+java -jar -Xmx1024m "%~dp0..\lib\java\k3.jar" -krun %*
