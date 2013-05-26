e217b47c753c1930c8eff6e718afe841fb6d2036
Andrei Ștefănescu <andreistef@gmail.com>
2013-05-26 01:40:58 +0000

    ... works with new data structure builtins
    git-svn-id: http://k-framework.googlecode.com/svn/branches/v3.2@10206 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: 8b2ae8202044f71cefd01c24f327693c39624479 [formerly e4f6d6f08211d81ba0e737a7d1bbf1376df61712]
    Former-commit-id: 76840cf8d73b6e9d885dd43bd65b8856236544f4

--- javasources/KTool/src/org/kframework/compile/transformers/ResolveOpenCells.java
+++ javasources/KTool/src/org/kframework/compile/transformers/ResolveOpenCells.java
@@ -25,37 +25,66 @@ public class ResolveOpenCells extends CopyOnWriteTransformer {
 		Ellipses ellipses = node.getEllipses();
 		if (ellipses == Ellipses.NONE) 
 			return node;
+
 		node = node.shallowCopy();
 		node.setCellAttributes(new HashMap<String, String>(node.getCellAttributes()));
 		node.setEllipses(Ellipses.NONE);
-		KSort sort = KSort.getKSort(node.getContents().getSort()).mainSort();
-		Collection col;
+
+        DataStructureSort dataStructureSort = context.dataStructureSortOf(context
+                .getConfigurationStructureMap().get(node).cell
+                .getCellAttributes().get(Cell.SORT_ATTRIBUTE));
+        if (dataStructureSort != null) {
+            /* data structure sort */
+            if (ellipses == Ellipses.BOTH && !dataStructureSort.type().equals(KSorts.LIST)) {
+                ellipses = Ellipses.RIGHT;
+            }
+
+            Term content = node.getContents();
+            if (ellipses == Ellipses.BOTH || ellipses == Ellipses.LEFT) {
+                content = KApp.of(
+                        KLabelConstant.of(dataStructureSort.constructorLabel()),
+                        Variable.getFreshVar(dataStructureSort.name()),
+                        content);
+            }
+            if (ellipses == Ellipses.BOTH || ellipses == Ellipses.RIGHT) {
+                content = KApp.of(
+                        KLabelConstant.of(dataStructureSort.constructorLabel()),
+                        content,
+                        Variable.getFreshVar(dataStructureSort.name()));
+            }
+
+            node.setContents(content);
+            return node;
+        }
+
+        KSort kind = KSort.getKSort(node.getContents().getSort()).mainSort();
+        Collection col;
 		if (node.getContents() instanceof Collection) {
 			col = (Collection) node.getContents().shallowCopy();
 			col.setContents(new ArrayList<Term>(col.getContents()));
 		} else {
-			col = MetaK.createCollection(node.getContents(), sort);
+			col = MetaK.createCollection(node.getContents(), kind);
 			if (col == null) {
 				GlobalSettings.kem.register(new KException(ExceptionType.ERROR, 
 						KExceptionGroup.COMPILER, 
-						"Expecting a collection item here but got " + node.getContents() + " which is of sort " + sort, getName(), 
+						"Expecting a collection item here but got " + node.getContents() + " which is of sort " + kind, getName(),
 						node.getFilename(), node.getLocation()));
 								
 			}
 		}
-
-        String sortName = sort.toString();
 		node.setContents(col);
-		if (ellipses == Ellipses.BOTH && sort != KSort.K && sort != KSort.List) {
+
+		if (ellipses == Ellipses.BOTH && kind != KSort.K && kind != KSort.List) {
 			ellipses = Ellipses.RIGHT;
 		}
         if (ellipses == Ellipses.BOTH || ellipses == Ellipses.LEFT) {
-			col.getContents().add(0, Variable.getFreshVar(sortName));
+			col.getContents().add(0, Variable.getFreshVar(kind.toString()));
 		}
 		if (ellipses == Ellipses.BOTH || ellipses == Ellipses.RIGHT) {
-			col.getContents().add(Variable.getFreshVar(sortName));
+			col.getContents().add(Variable.getFreshVar(kind.toString()));
 		}
-		return node;
+
+        return node;
 	}
 
 	@Override
