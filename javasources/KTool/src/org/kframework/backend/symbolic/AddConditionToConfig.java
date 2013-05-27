1c47baccbb43f5c7ed1c3ed30b20bc819b9efb82
Andrei Arusoai <Andrei.Arusoaie@gmail.com>
2013-05-27 07:01:52 +0000

    Add PC variable in <path-condition> cell.
    git-svn-id: http://k-framework.googlecode.com/svn/branches/v3.2@10210 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: d847be8abde1b36879acb06ad72147656c70271a [formerly 0b42d7cd440a564d44c40d6a42f4905a0455043b]
    Former-commit-id: 35f71f67aaef940b7a105de6332ce792b6a56048

--- javasources/KTool/src/org/kframework/backend/symbolic/AddConditionToConfig.java
+++ javasources/KTool/src/org/kframework/backend/symbolic/AddConditionToConfig.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.kframework.compile.utils.MetaK;
 import org.kframework.kil.ASTNode;
 import org.kframework.kil.Bag;
-import org.kframework.kil.BoolBuiltin;
 import org.kframework.kil.Cell;
 import org.kframework.kil.Cell.Ellipses;
 import org.kframework.kil.Configuration;
@@ -18,6 +17,7 @@ import org.kframework.kil.Sort;
 import org.kframework.kil.Syntax;
 import org.kframework.kil.Term;
 import org.kframework.kil.Terminal;
+import org.kframework.kil.Variable;
 import org.kframework.kil.loader.Context;
 import org.kframework.kil.visitors.CopyOnWriteTransformer;
 import org.kframework.kil.visitors.exceptions.TransformerException;
@@ -47,8 +47,8 @@ public class AddConditionToConfig extends CopyOnWriteTransformer {
         Cell cell = new Cell();
         cell.setLabel(MetaK.Constants.pathCondition);
         cell.setEllipses(Ellipses.NONE);
-        cell.setContents(BoolBuiltin.TRUE);
-
+        cell.setContents(new Variable("$PC", "Bool"));
+        
         // append the path condition cell as subcell of generated top cell
         Term body = node.getBody();
         if (body instanceof Cell) {
