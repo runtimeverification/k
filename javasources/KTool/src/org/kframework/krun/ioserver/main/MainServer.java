ed56e69753238fa5edf0cbc8def5cd30af3234f8
Dwight Guth <dwight.guth@gmail.com>
2013-06-17 21:27:36 +0000

    add some more stuff for invoking kio directly so that it can successfully invoke #parse
    git-svn-id: http://k-framework.googlecode.com/svn/branches/v3.2@10315 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: ba1b96555276b46304111030037d27c6baaad9d8 [formerly 1ad2abcd4cbce28501e6bfd490b1a24c40ef754e]
    Former-commit-id: a3d52ca4f04e8bfe51247f893f392d41d346fb2a

--- javasources/KTool/src/org/kframework/krun/ioserver/main/MainServer.java
+++ javasources/KTool/src/org/kframework/krun/ioserver/main/MainServer.java
@@ -1,8 +1,15 @@
 package org.kframework.krun.ioserver.main;
 
+import org.kframework.compile.FlattenModules;
+import org.kframework.compile.transformers.AddTopCellConfig;
+import org.kframework.kil.Definition;
 import org.kframework.kil.loader.Context;
+import org.kframework.krun.K;
 import org.kframework.krun.ioserver.resources.ResourceSystem;
+import org.kframework.utils.BinaryLoader;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.logging.Logger;
 
 public class MainServer implements Runnable {
@@ -48,6 +55,14 @@ public class MainServer implements Runnable {
 		
 	public static void main(String[] args) throws Exception {
 		Context context = new Context();
+		context.kompiled = new File(args[1]);
+		Definition javaDef = (Definition) BinaryLoader.fromBinary(
+			new FileInputStream(context.kompiled.getCanonicalPath() + "/defx.bin"));
+		javaDef = new FlattenModules(context).compile(javaDef, null);
+		javaDef = (Definition) javaDef.accept(new AddTopCellConfig(context));
+		// This is essential for generating maude
+		javaDef.preprocess(context);
+        K.parser = args[2];
 		Logger logger = java.util.logging.Logger.getLogger("KRunner");
 		logger.setUseParentHandlers(false);
 		MainServer ms = new MainServer(Integer.parseInt(args[0]), logger, context);
