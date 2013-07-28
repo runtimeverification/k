ff002dbb89831ade96fa44823d26537618f06dec
Dwight Guth <dwight.guth@gmail.com>
2013-07-28 14:44:01 +0000

    fix #noIO bug
    git-svn-id: http://k-framework.googlecode.com/svn/trunk@10497 82a86efa-2583-11df-8ea0-49ba94de29ab
    
    Former-commit-id: 919c2065e9a7bc88b18d91d1d2cacd65ab5373a7 [formerly b53e0a8f09cef0fb777d1e6a3d552c1c98f60710]
    Former-commit-id: e5b45fcb89ecc060b4bf1a8492e2b63524f604a3

--- javasources/KTool/src/org/kframework/krun/Main.java
+++ javasources/KTool/src/org/kframework/krun/Main.java
@@ -242,7 +242,7 @@ public class Main {
 			stdin = "";
 		}
 		if (stdin != null) {
-			output.put("$noIO", new BackendTerm("ListItem", "ListItem(#noIO)"));
+			output.put("$noIO", new BackendTerm("ListItem", "ListItem('#noIO(.KList))"));
 			output.put("$stdin", new BackendTerm("K", "# \"" + stdin
 					+ "\\n\"(.KList)"));
 		} else {
