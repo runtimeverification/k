607f21d5f6364fc75322a7215f4c8bdc06feb9e6
Traian Florin Serbanuta <traian.serbanuta@gmail.com>
2013-09-02 16:10:47 +0300

    Extracted a generic external process server class.
    Former-commit-id: 7cf80db334427a412612522c9de8a1b5c7b2b710 [formerly a739a6c90fa4b5e4acde25d60cd9fd4d3ce44e05]
    Former-commit-id: f0fe5a09985e35d0a48829e270da070eafc8a813

---
+++ javasources/KTool/src/org/kframework/parser/utils/ExternalProcessServer.java
@@ -0,0 +1,112 @@
+package org.kframework.parser.utils;
+
+import org.kframework.utils.file.KPaths;
+import org.kframework.utils.general.GlobalSettings;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.lang.ProcessBuilder.Redirect;
+
+public class ExternalProcessServer {
+    // Generic server process
+    private final String executable;
+    private Process server;
+    private DataOutputStream output;
+    private DataInputStream input;
+
+    private final boolean DEBUG = false;
+
+    // A flag signaling whether this is the first time the server process is instantiated.
+    private boolean firstTime;
+
+    public ExternalProcessServer(String executable) {
+        this.executable = executable;
+
+        server = null;
+        output = null;
+        input = null;
+        firstTime = true;
+    }
+
+    /** Start the server process */
+    public void init() throws IOException {
+        if (!firstTime) return;
+        File f = null;
+        String basePath = KPaths.getKBase(false);
+
+        if (GlobalSettings.isUnixOS()) {
+            f = new File(basePath + "/lib/native/linux/" + executable);
+            f.setExecutable(true, false);
+        }
+        if (GlobalSettings.isWindowsOS()) {
+            f = new File(basePath + "/lib/native/cygwin/" + executable + ".exe");
+        }
+        if (GlobalSettings.isMacOS()) {
+            f = new File(basePath + "/lib/native/macosx/" + executable);
+            f.setExecutable(true, false);
+        }
+
+        ProcessBuilder pb = new ProcessBuilder(f.getAbsolutePath());
+        pb.redirectError(Redirect.INHERIT);
+        server = pb.start();
+        output = new DataOutputStream(server.getOutputStream());
+        input = new DataInputStream(server.getInputStream());
+
+        Thread childWatcher = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int exitCode;
+                try {
+                    exitCode = server.waitFor();
+                    System.err.println(executable + " server exited with code " + exitCode);
+                } catch (InterruptedException e) {
+                    System.err.println("Interrupted while waiting for " + executable + " server");
+                }
+            }
+        });
+        childWatcher.setDaemon(true);
+        childWatcher.start();
+
+        firstTime = false;
+    }
+
+    /**
+     * Send the low-order bytes of the string to the process.
+     * @param str
+     * @throws java.io.IOException
+     */
+    public void sendString(String str) throws IOException {
+        output.writeInt(str.length());
+        if (DEBUG) {
+            System.err.println("sent length "+str.length());
+        }
+        output.writeBytes(str);
+        if (DEBUG) {
+            System.err.println("sent string "+str);
+        }
+    }
+
+    /**
+     * Receive a response from the process
+     * @return
+     * @throws java.io.IOException
+     */
+    public byte[] readBytes() throws IOException {
+        int len = input.readInt();
+        if (DEBUG) {
+            System.err.println("read length "+len);
+        }
+        byte[] bytes = new byte[len];
+        input.readFully(bytes);
+        if (DEBUG) {
+            System.err.println("got bytes");
+        }
+        return bytes;
+    }
+
+    public void flushOutput() throws IOException {
+        output.flush();
+    }
+}
--- javasources/KTool/src/org/kframework/parser/utils/SglrServer.java
+++ javasources/KTool/src/org/kframework/parser/utils/SglrServer.java
@@ -1,100 +1,15 @@
 package org.kframework.parser.utils;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
 import java.io.IOException;
-import java.lang.ProcessBuilder.Redirect;
-
-import org.kframework.utils.file.KPaths;
-import org.kframework.utils.general.GlobalSettings;
 
 public final class SglrServer {
-    // the sglr-server process
-    private static Process server = null;
-    private static DataOutputStream output = null;
-    private static DataInputStream input = null;
+    private static ExternalProcessServer sglrProcess = null;
 
-    private static final boolean DEBUG = false;
-    
-    // A flag signaling whether this is the first time the parser is instantiated.
-    private static boolean firstTime = true;
-
-    /** Start the server process */
     private static void init() throws IOException {
-        File f = null;
-        String basePath = KPaths.getKBase(false);
-
-        if (GlobalSettings.isUnixOS()) {
-            f = new File(basePath + "/lib/native/linux/sglr-server");
-            f.setExecutable(true, false);
-        }
-        if (GlobalSettings.isWindowsOS()) {
-            f = new File(basePath + "/lib/native/cygwin/sglr-server.exe");
-        }
-        if (GlobalSettings.isMacOS()) {
-            f = new File(basePath + "/lib/native/macosx/sglr-server");
-            f.setExecutable(true, false);
-        }
-        
-        ProcessBuilder pb = new ProcessBuilder(f.getAbsolutePath());
-        pb.redirectError(Redirect.INHERIT);
-        server = pb.start();
-        output = new DataOutputStream(server.getOutputStream());
-        input = new DataInputStream(server.getInputStream());
-        
-        Thread childWatcher = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                int exitCode;               
-                try {
-                    exitCode = server.waitFor();
-                    System.err.println("SGLR server exited with code "+exitCode);
-                } catch (InterruptedException e) {
-                    System.err.println("Interrupted while waiting for SLGR server");
-                }                
-            }
-        });
-        childWatcher.setDaemon(true);
-        childWatcher.start();
-        
-        firstTime = false;
-    }
-    
-    /**
-     * Send the low-order bytes of the string to the process.
-     * @param str
-     * @throws IOException
-     */
-    private static void sendString(String str) throws IOException {
-        output.writeInt(str.length());
-        if (DEBUG) {
-            System.err.println("sent length "+str.length());
-        }
-        output.writeBytes(str);
-        if (DEBUG) {
-            System.err.println("sent string "+str);
-        }
+        sglrProcess = new ExternalProcessServer("sglr-server");
+        sglrProcess.init();
     }
 
-    /**
-     * Receive a response from the process
-     * @return
-     * @throws IOException
-     */
-    private static byte[] readBytes() throws IOException {
-        int len = input.readInt();
-        if (DEBUG) {
-            System.err.println("read length "+len);
-        }
-        byte[] bytes = new byte[len];
-        input.readFully(bytes);
-        if (DEBUG) {
-            System.err.println("got bytes");
-        }
-        return bytes;
-    }
-    
     /**
      * The main parsing function that accesses the C parser in native way.
      * The library is loaded only once and it is persistent.
@@ -106,15 +21,13 @@ public final class SglrServer {
      */
     public static synchronized byte[] parseString(String parseTablePath, String input, String startSymbol, String inputFileName) {
         try {
-            if (firstTime) {
-                init();
-            }
-            sendString(parseTablePath);
-            sendString(input);
-            sendString(startSymbol);
-            sendString(inputFileName);
-            output.flush();
-            return readBytes();
+            if (sglrProcess == null) init();
+            sglrProcess.sendString(parseTablePath);
+            sglrProcess.sendString(input);
+            sglrProcess.sendString(startSymbol);
+            sglrProcess.sendString(inputFileName);
+            sglrProcess.flushOutput();
+            return sglrProcess.readBytes();
         } catch (IOException e) {
             e.printStackTrace();
             return null;
