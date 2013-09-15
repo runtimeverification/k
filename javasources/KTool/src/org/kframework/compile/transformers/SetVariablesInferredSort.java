8b783df6794f91b3ebacbbddc56a736d72a60dd0
Andrei Stefanescu <andreistef@gmail.com>
2013-09-15 17:53:15 -0500

    the java rewrite engine now uses sort inference (offered by Radu); imp and imp++ are updated to take advantage of it
    Former-commit-id: debb43c5b6c0e454062eb14e755c60c4703bbcea [formerly 090a43be274266ce99941e0e592855986545378b]
    Former-commit-id: 51fe0baabd8b23ff7c76221f5954bc50c5bfab06

---
+++ javasources/KTool/src/org/kframework/compile/transformers/SetVariablesInferredSort.java
@@ -0,0 +1,26 @@
+package org.kframework.compile.transformers;
+
+import org.kframework.kil.Variable;
+import org.kframework.kil.loader.Context;
+import org.kframework.kil.visitors.CopyOnWriteTransformer;
+
+/**
+ * Sets the sort of each {@code Variable} node in the AST to the sort produced by the type
+ * inferred by {@link org.kframework.parser.concrete.disambiguate.CollectExpectedVariablesVisitor}.
+ *
+ * @see org.kframework.parser.concrete.disambiguate.CollectExpectedVariablesVisitor
+ *
+ * @author AndreiS
+ */
+public class SetVariablesInferredSort extends CopyOnWriteTransformer {
+
+    public SetVariablesInferredSort(Context context) {
+        super("Set the sort of each variable to the inferred sort", context);
+    }
+
+    @Override
+    public Variable transform(Variable variable) {
+        return new Variable(variable.getName(), variable.getExpectedSort());
+    }
+
+}
