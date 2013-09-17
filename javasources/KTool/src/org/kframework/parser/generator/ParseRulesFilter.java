589e49e44425477424458702d4b69b8c216ba23c
Michael D. Adams <http://michaeldadams.org>
2013-09-17 00:50:05 -0500

    Fix bug where attributes of attributes were checked instead of the value of attributes.
    Former-commit-id: 391ece7a1972f1ec2b75e780e2cc6b03b8442f42 [formerly c74e16176c75896886e713e195aaf98cc17ab71d]
    Former-commit-id: 51ea174cd5333c57d1f7f0cbda39f0fc06dd580f

--- javasources/KTool/src/org/kframework/parser/generator/ParseRulesFilter.java
+++ javasources/KTool/src/org/kframework/parser/generator/ParseRulesFilter.java
@@ -109,7 +109,7 @@ public class ParseRulesFilter extends BasicTransformer {
 					((Sentence) config).setAttributes(ss.getAttributes());
 				} else {
 					String parsed = null;
-					if (ss.getAttributes().containsAttribute("kore")) {
+					if (ss.containsAttribute("kore")) {
 						Stopwatch sww = new Stopwatch();
 						parsed = org.kframework.parser.concrete.KParser.ParseKoreString(ss.getContent());
 						if (GlobalSettings.verbose)
