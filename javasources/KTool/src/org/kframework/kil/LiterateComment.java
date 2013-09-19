c1922c67d9fca590a6b6a637108aaf0dcc65bb43
Michael D. Adams <http://michaeldadams.org>
2013-09-19 14:38:27 -0500

    Better pretty printing for literate comments.
    Former-commit-id: 8ecf2e329082125f9aaa73fa6b4546848c245216 [formerly b66b5086f850bad14164ba32ab8baa368a1420ec]
    Former-commit-id: 79cc8e072af237d313dd38ca5fef812c644e2c21

--- javasources/KTool/src/org/kframework/kil/LiterateComment.java
+++ javasources/KTool/src/org/kframework/kil/LiterateComment.java
@@ -2,8 +2,13 @@ package org.kframework.kil;
 
 public interface LiterateComment {
 	public enum LiterateCommentType {
-		LATEX, PREAMBLE, COMMON
-	};
+		LATEX, PREAMBLE, COMMON;
+		public String toString() {
+			if (this == LATEX) { return "@"; }
+			else if (this == PREAMBLE) { return "!"; }
+			else { assert this == COMMON; return ""; }
+		}
+	}
 
 	public LiterateCommentType getType();
 }
