517924667685c1db62b843d128979872f6179d28
Daejun Park <daejunpark@gmail.com>
2013-10-08 02:19:07 -0500

    CLEANUP_OPTIONS: Hack for backward compatibility of 'ktest --config'
    Former-commit-id: ec3de9726223f8cbdcdafdc82ddfce5d8d63b803 [formerly 3adfc609b0c483548952e94c49db0a060984af1d]
    Former-commit-id: a78250b63653c7765dbbee0352fd89b5f9a983e9

--- javasources/KTool/src/org/kframework/ktest/KTestOptionsParser.java
+++ javasources/KTool/src/org/kframework/ktest/KTestOptionsParser.java
@@ -54,6 +54,8 @@ public class KTestOptionsParser {
 		addOptionS(OptionBuilder.withLongOpt(Configuration.DIRECTORY_OPTION).hasArg().withArgName("dir").withDescription("A root directory where K definitions reside. Valid only in batch mode.").create("d"));
 		addOptionS(OptionBuilder.withLongOpt(Configuration.REPORT_OPTION).withDescription("Generate a junit-like report.").create());
 		addOptionS(OptionBuilder.withLongOpt(Configuration.PROCESSES_OPTION).hasArg().withArgName("num").withDescription("The maximum number of threads.").create());
+
+		addOptionE(OptionBuilder.withLongOpt("config").withDescription("Just for backward compatibility.").create());
 	}
 	
 	// parse the command line arguments
