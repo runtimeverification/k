5230091ff8585a6ccbf793d1e59dd61e37f4532f
denis-bogdanas <denis.bogdanas@gmail.com>
2013-10-21 18:22:05 +0300

    issue #91. Changed default vm options to: -Xms64m -Xmx1024m -Xss32m for all tools, all platforms. Putting -Xss to 64m is too much: OutOfMemoryError on Windows 32 bit with 2GB RAM.
    Former-commit-id: b00e68d4f15f4bf65b550614aee8a90557c8722c [formerly 3236b80d594d03dff8826301a0ad1f137164c7a7]
    Former-commit-id: 4342b56b96795098582aaa1695d7c77a7ea24097

--- dist/bin/krun.bat
+++ dist/bin/krun.bat
@@ -2,6 +2,6 @@
 set PATH=%PATH%;%~dp0..\lib\native\cygwin;%~dp0..\lib\native\cygwin\x64
 SETLOCAL ENABLEEXTENSIONS
 IF ERRORLEVEL 1 ECHO Unable to enable extensions
-IF NOT DEFINED K_OPTS SET K_OPTS=-Xss64m -Xmx1024m -Xss8m
+IF NOT DEFINED K_OPTS SET K_OPTS=-Xms64m -Xmx1024m -Xss32m
 java -ea -jar %K_OPTS% "%~dp0..\lib\java\k3.jar" -krun %*
 ENDLOCAL
