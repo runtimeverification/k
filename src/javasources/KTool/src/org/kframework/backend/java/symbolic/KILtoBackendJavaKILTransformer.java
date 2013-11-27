392112d00bd7d3b9e7ca24a0b1260513899b40e8
Yilong Li <yli147@illinois.edu>
2013-11-27 01:15:55 -0600

    Java backend: fixed a bug(?) in transforming the requires part of a rule from KIL to Java backend KIL
    Former-commit-id: 0ec1f70c780e7e85cdd0bdaa90219cb541608d4f [formerly 0058ef3feb247aaf1f28f60a4cf5001adaa994ef]
    Former-commit-id: c452d512534437965c6885bc663091098e3b6784

--- src/javasources/KTool/src/org/kframework/backend/java/symbolic/KILtoBackendJavaKILTransformer.java
+++ src/javasources/KTool/src/org/kframework/backend/java/symbolic/KILtoBackendJavaKILTransformer.java
@@ -502,10 +502,7 @@ public class KILtoBackendJavaKILTransformer extends CopyOnWriteTransformer {
         //TODO: Deal with Ensures
         if (node.getRequires() != null) {
             Term term = (Term) node.getRequires().accept(this);
-            if (term instanceof KItem &&
-                // TODO(YilongL): hasn't the compiler already transformed '_andBool_ to '#andBool?
-                    (((KItem) term).kLabel().toString().equals("'_andBool_") ||
-                     (((KItem) term).kLabel().toString().equals("'#andBool") && K.do_testgen) )) {
+            if (term instanceof KItem && ((KItem) term).kLabel().toString().equals("'#andBool")) {
                 for (Term item : ((KItem) term).kList().getItems()) {
                     if (item instanceof KItem && ((KItem) item).kLabel().toString().equals("'fresh(_)")) {
                         freshVariables.add((Variable) ((KItem) item).kList().get(0));
