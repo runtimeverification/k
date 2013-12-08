ca83b03e32ef44c2c8fdad74fbf104e6d9a15daf
osa1 <omeragacan@gmail.com>
2013-12-08 15:14:30 +0200

    some refactoring in main class and kagreg
    Former-commit-id: ad8ed1c1228fad62a81c8a4beab4dd8ac24a0ef0 [formerly 36d24cc8d8e1c1a190e55fd0481b0e8ee992207a]
    Former-commit-id: 35bd7ca2c3e6bf65e5d85095fbb86c3f9ab4fe1f

--- src/javasources/KTool/src/org/kframework/kagreg/KagregFrontEnd.java
+++ src/javasources/KTool/src/org/kframework/kagreg/KagregFrontEnd.java
@@ -13,6 +13,7 @@ import org.kframework.kil.ConfigurationNotUnique;
 import org.kframework.kil.Definition;
 import org.kframework.kil.Import;
 import org.kframework.kil.loader.Context;
+import org.kframework.kil.visitors.exceptions.TransformerException;
 import org.kframework.parser.DefinitionLoader;
 import org.kframework.parser.concrete.KParser;
 import org.kframework.utils.Error;
@@ -29,7 +30,7 @@ import org.kframework.utils.general.GlobalSettings;
  */
 public class KagregFrontEnd {
 
-	public static void kagreg(String[] args) throws IOException, Exception {
+	public static void kagreg(String[] args) throws IOException, TransformerException {
 		if (args.length != 2) {
 			Error.report("There must be exactly two K definitions as arguments to kagreg.");
 		}
--- src/javasources/KTool/src/org/kframework/main/Main.java
+++ src/javasources/KTool/src/org/kframework/main/Main.java
@@ -1,14 +1,13 @@
 package org.kframework.main;
 
-import java.awt.GraphicsEnvironment;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.Arrays;
 
+import org.kframework.kil.visitors.exceptions.TransformerException;
 import org.kframework.utils.Error;
 import org.kframework.utils.Stopwatch;
 import org.kframework.utils.file.KPaths;
-import org.kframework.utils.general.GlobalSettings;
 
 public class Main {
 
@@ -24,9 +23,7 @@ public class Main {
 			Field fieldSysPath = ClassLoader.class.getDeclaredField("sys_paths");
 			fieldSysPath.setAccessible(true);
 			fieldSysPath.set(null, null);
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (NoSuchFieldException e) {
+		} catch (IllegalAccessException | NoSuchFieldException e) {
 			e.printStackTrace();
 		}
 	}
@@ -34,51 +31,62 @@ public class Main {
 	/**
 	 * @param args
 	 *            - the running arguments for the K3 tool. First argument must be one of the following: kompile|kast|krun.
-	 * @throws Exception when loadDefinition fails
+	 * @throws TransformerException when loadDefinition fails
 	 * @throws IOException when loadDefinition fails 
 	 */
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException, TransformerException {
         Stopwatch.init();
 		setJavaLibraryPath();
 
 		if (args.length >= 1) {
             String[] args2 = Arrays.copyOfRange(args, 1, args.length);
-			if (args[0].equals("-kompile")) {
-				org.kframework.kompile.KompileFrontEnd.main(args2);
-			} else if (args[0].equals("-kagreg")) {
-				org.kframework.kagreg.KagregFrontEnd.kagreg(args2);
-			} else if (args[0].equals("-kcheck")) {
-				org.kframework.kcheck.KCheckFrontEnd.kcheck(args2);
-			} else if (args[0].equals("-ktest")) {
-				org.kframework.ktest.KTest.main(args2);
-			} else if (args[0].equals("-kast")) {
-				org.kframework.kast.KastFrontEnd.kast(args2);
-			} else if (args[0].equals("-krun")) {
-			    // unable to load commandlineOptions since it loads K class 
-			    // K loads Color which sets headless field in GraphicsEnvironment 
-			    // and since this can be set only once we can not reset it by java.awt.headless
-			    for (String s : args){
-			        if(s.contains("debug-gui")){
-			            System.setProperty("java.awt.headless", "false");
-			            //force headless filed to be instantiated
-			            java.awt.GraphicsEnvironment.isHeadless() ;
-			            break;
-			        }
-			    }
-			    org.kframework.krun.Main.execute_Krun(args2);
-			} else if (args[0].equals("-kpp")) {
-				org.kframework.main.Kpp.codeClean(args2);
-			} else if (args[0].equals("-ioserver")) {
-				try {
-					org.kframework.krun.ioserver.main.MainServer.main(args2);
-				} catch (Exception e) {
-					Error.report("IO server threw exception");
-				}
-			} else if (args[0].equals("-kpretty")) {
-				org.kframework.main.KPretty.main(args2);
-            } else {
-				Error.report("The first argument of K3 not recognized. Try -kompile, -kast, -krun or -kpp.");
-			}
+            switch (args[0]) {
+                case "-kompile":
+                    org.kframework.kompile.KompileFrontEnd.main(args2);
+                    break;
+                case "-kagreg":
+                    org.kframework.kagreg.KagregFrontEnd.kagreg(args2);
+                    break;
+                case "-kcheck":
+                    org.kframework.kcheck.KCheckFrontEnd.kcheck(args2);
+                    break;
+                case "-ktest":
+                    org.kframework.ktest.KTest.main(args2);
+                    break;
+                case "-kast":
+                    org.kframework.kast.KastFrontEnd.kast(args2);
+                    break;
+                case "-krun":
+                    // unable to load commandlineOptions since it loads K class
+                    // K loads Color which sets headless field in GraphicsEnvironment
+                    // and since this can be set only once we can not reset it by java.awt.headless
+                    for (String s : args) {
+                        if (s.contains("debug-gui")) {
+                            System.setProperty("java.awt.headless", "false");
+                            //force headless filed to be instantiated
+                            java.awt.GraphicsEnvironment.isHeadless();
+                            break;
+                        }
+                    }
+                    org.kframework.krun.Main.execute_Krun(args2);
+                    break;
+                case "-kpp":
+                    Kpp.codeClean(args2);
+                    break;
+                case "-ioserver":
+                    try {
+                        org.kframework.krun.ioserver.main.MainServer.main(args2);
+                    } catch (Exception e) {
+                        Error.report("IO server threw exception");
+                    }
+                    break;
+                case "-kpretty":
+                    KPretty.main(args2);
+                    break;
+                default:
+                    Error.report("The first argument of K3 not recognized. Try -kompile, -kast, -krun or -kpp.");
+                    break;
+            }
 		} else
 			Error.report("There must be a first argument to K3: try -kompile, -kast, -krun or -kpp.");
 	}
