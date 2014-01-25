96907943328fc2ad9b52b13f08c1c1abd2c5f556
Radu Mereuta <headness13@gmail.com>
2014-01-25 15:50:27 -0600

    Progress on unifying builtins. Things started to parse now.
    Former-commit-id: ee0eb75b99e31482c9171ec3795a966dc2ceb50c [formerly f2d7409347f5421545f37eae14f4cab14456d34e]
    Former-commit-id: 4aa9524c23e43d6bf29f64684922a7ec043f58bc

--- src/javasources/KTool/src/org/kframework/backend/java/symbolic/JavaSymbolicBackend.java
+++ src/javasources/KTool/src/org/kframework/backend/java/symbolic/JavaSymbolicBackend.java
@@ -159,7 +159,7 @@ public class JavaSymbolicBackend extends BasicBackend {
         steps.add(new ResolveRewrite(context));
 
         /* data structure related stuff */
-        steps.add(new CompileToBuiltins(context));
+       // steps.add(new CompileToBuiltins(context));
         steps.add(new CompileDataStructures(context));
         steps.add(new Cell2DataStructure(context));
         steps.add(new DataStructureToLookupUpdate(context));
