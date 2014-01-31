482eb52be832ea23fed17a16c20d892c4e1d6eb9
Radu Mereuta <headness13@gmail.com>
2014-01-31 11:00:26 -0600

    Preparing some more test templates.
    Former-commit-id: 8596df1023f7ed2fbd26ab80b5be6d085a542356 [formerly 42978b9322f41a1c05910705cf02442ea49482cd]
    Former-commit-id: 4a5ad2f15ce63ebf877a8c19d28b620ec3b78b48

--- src/javasources/KTool/test/org/kframework/parser/concrete2/ParserTest.java
+++ src/javasources/KTool/test/org/kframework/parser/concrete2/ParserTest.java
@@ -8,7 +8,7 @@ public class ParserTest extends TestCase {
 
     }
 
-    public void testParse() throws Exception {
+    public void testEmptyGrammar() throws Exception {
         Grammar.NonTerminalId ntistart = new Grammar.NonTerminalId("StartNT");
         Grammar.StateId stistart = new Grammar.StateId("StartState");
         Grammar.StateId stiend = new Grammar.StateId("EndState");
@@ -23,5 +23,29 @@ public class ParserTest extends TestCase {
         ParseState ps = new ParseState("");
         Parser parser = new Parser(ps);
         parser.parse(nt1, 0);
+    }
+
+    public void testSingleToken() throws Exception {
+
+    }
+
+    public void testSequenceOfTokens() throws Exception {
+
+    }
+
+    public void testDisjunctionOfTokens() throws Exception {
+
+    }
+
+    public void testListOfTokens() throws Exception {
+
+    }
+
+    public void testNestedNonTerminals() throws Exception {
+
+    }
+
+    public void testArithmeticLanguage() throws Exception {
+
     }
 }
