a17fe7b974cb24e9e5257e386b02c2de46dfa021
qangel <liyili2@illinois.edu>
2014-01-31 12:24:02 -0600

    kore
    Former-commit-id: 1ec99a542430d798b5ccc2c34fd1f22e01e010f4 [formerly c363adc1e190951ec4bf5629e8d5d82db1f03036]
    Former-commit-id: 10201a8ed2e823db7cf92811ecda1ca66ad33674

--- src/javasources/KTool/src/org/kframework/backend/kore/KilTransformer.java
+++ src/javasources/KTool/src/org/kframework/backend/kore/KilTransformer.java
@@ -1,5 +1,6 @@
 package org.kframework.backend.kore;
 
+import org.kframework.compile.transformers.FlattenSyntax;
 import org.kframework.kil.ASTNode;
 import org.kframework.kil.loader.Context;
 import org.kframework.kil.visitors.exceptions.TransformerException;
@@ -8,30 +9,31 @@ import org.kframework.krun.ColorSetting;
 
 public class KilTransformer {
 	
-	ToBuiltinTransformer builtinTrans;
-	ToKAppTransformer kappTrans;
+	//ToBuiltinTransformer builtinTrans;
+	FlattenSyntax kappTrans;
 	KoreFilter koreTrans;
 	
 	public KilTransformer(Context context){
 		
 	    koreTrans = new KoreFilter(context);
-	    builtinTrans = new ToBuiltinTransformer(context);
-	    kappTrans = new ToKAppTransformer(context);
+	    //builtinTrans = new ToBuiltinTransformer(context);
+	    kappTrans = new FlattenSyntax(context);
 
 	}
 	
 	public KilTransformer(boolean inConfiguration, ColorSetting color, org.kframework.kil.loader.Context context){
 		
 	    koreTrans = new KoreFilter(inConfiguration, color, context);
-	    builtinTrans = new ToBuiltinTransformer(context);
-	    kappTrans = new ToKAppTransformer(context);
+	    //builtinTrans = new ToBuiltinTransformer(context);
+	    kappTrans = new FlattenSyntax(context);
 
 	}
 	
 	public String kilToKore(ASTNode node){
 
 	    try {
-			node.accept(builtinTrans).accept(kappTrans).accept(koreTrans);
+			//node.accept(builtinTrans).accept(kappTrans).accept(koreTrans);
+			node.accept(kappTrans).accept(koreTrans);
 		} catch (TransformerException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
