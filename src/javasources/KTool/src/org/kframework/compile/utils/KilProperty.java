801bf539123a6824e54632c9747da7834a81baf2
Dwight Guth <dwight.guth@runtimeverification.com>
2014-02-25 15:49:48 -0800

    add javadocs
    Former-commit-id: 97b1d8a52a50da8c409196282abae50d02a1c03d [formerly 0133bc631ae5c2da51548602c5d05fa0d835b6e1]
    Former-commit-id: 8744db2629296c4309f3731041d98f0b1d19043c

--- src/javasources/KTool/src/org/kframework/compile/utils/KilProperty.java
+++ src/javasources/KTool/src/org/kframework/compile/utils/KilProperty.java
@@ -5,16 +5,28 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Represents a contract asserting that a particular AST has a particular
+ * property (e.g. does not contain any concrete syntax, does not contain any contexts, etc).
+ */
 public enum KilProperty {
     NO_CONCRETE_SYNTAX;
     //TODO: add more
 
+    /**
+     * Used to annotate code which takes an AST as input which will not work on
+     * terms without a particular KilProperty.
+     */
     @Retention(RetentionPolicy.RUNTIME)
     @Target(ElementType.TYPE)
     public static @interface DependsOn {
 	    KilProperty value();
     }
 
+    /**
+     * Used to annotate code which outputs an AST which is guaranteed to ensure
+     * a particular KilProperty.
+     */
     @Retention(RetentionPolicy.RUNTIME)
     @Target(ElementType.TYPE)
     public static @interface Provides {
