a58c47eb313e26774abb31fc74c0a297160e984c
Dwight Guth <dwight.guth@runtimeverification.com>
2014-02-25 15:13:04 -0800

    on second thought, this is better
    Former-commit-id: 7bb814ac0c2e0baf995bf25c172dadf6f346cc7d [formerly f8108a5f9bab4da2500ee945f9bff3d4fc0f00c3]
    Former-commit-id: f005d68e43de5e8a5f29211d02c5b893a23d0c7c

---
+++ src/javasources/KTool/src/org/kframework/compile/utils/KilProperty.java
@@ -0,0 +1,23 @@
+package org.kframework.compile.utils;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+public enum KilProperty {
+    NO_CONCRETE_SYNTAX
+    //TODO: add more
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.TYPE)
+    public @interface DependsOn {
+	    KilProperty value();
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.TYPE)
+    public @interface Provides {
+	    KilProperty value();
+    }
+}
