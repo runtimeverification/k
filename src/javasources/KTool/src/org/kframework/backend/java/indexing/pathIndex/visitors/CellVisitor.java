696a34c5340a93cbceb4256501131fc004dc727c
Owolabi Legunsen <owolabileg@gmail.com>
2014-02-26 13:21:46 -0600

    indexing: added a new visitor class for traversing terms at run time for indexing purposes.
    Former-commit-id: 466ce9cd57ae9f558aede0fb1064e3177dd54ae8 [formerly bd5657a990e18873a6389fd0e81ec08f69b42ba7]
    Former-commit-id: 251a8f702482580a04fb0e76d6591ff7b37b86d4

---
+++ src/javasources/KTool/src/org/kframework/backend/java/indexing/pathIndex/visitors/CellVisitor.java
@@ -0,0 +1,59 @@
+package org.kframework.backend.java.indexing.pathIndex.visitors;
+
+import org.kframework.backend.java.kil.Cell;
+import org.kframework.backend.java.kil.Kind;
+import org.kframework.backend.java.symbolic.BottomUpVisitor;
+import org.kframework.kil.loader.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This visitor class is used to traverse a term. It does the following in a single traversal:
+ *  (1) collect position strings from its kCell(s)
+ *  (2) selects i/o cells by name (this is a shortcoming of this approach as it specializes the
+ *      indexing technique to the, possibly user-defined cell names.)
+ * Author: OwolabiL
+ * Date: 2/26/14
+ * Time: 12:56 PM
+ */
+class CellVisitor extends BottomUpVisitor {
+    private Context context;
+    private Cell inCell;
+    private Cell outCell;
+
+    private List<String> kCellPStings = new ArrayList<>();
+
+    CellVisitor(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public void visit(Cell cell) {
+        if (cell.getLabel().equals("k")) {
+            // get the pString from each k cell using a new visitor each time,
+            // but accumulate the pStrings
+            TermVisitor visitor = new TermVisitor(this.context);
+            cell.getContent().accept(visitor);
+            kCellPStings.addAll(visitor.getpStrings());
+        } else if (cell.getLabel().equals("out")) {
+            outCell = cell;
+        } else if (cell.getLabel().equals("in")) {
+            inCell = cell;
+        } else if (cell.contentKind() == Kind.CELL_COLLECTION) {
+            super.visit(cell);
+        }
+    }
+
+    List<String> getkCellPStings() {
+        return kCellPStings;
+    }
+
+    Cell getInCell() {
+        return inCell;
+    }
+
+    Cell getOutCell() {
+        return outCell;
+    }
+}
