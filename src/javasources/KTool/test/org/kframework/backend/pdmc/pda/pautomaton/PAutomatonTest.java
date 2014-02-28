0e133c7b9369a01d6ece073fa9db09995aed2b62
Traian Florin Serbanuta <traian.serbanuta@gmail.com>
2014-02-28 11:50:48 +0200

    Added test for getPath
    Former-commit-id: 07314b497a20b40954528c4ee626716fa65213cb [formerly f9e5b539f4145b1ca7978bc4ef71f2435d746155]
    Former-commit-id: 5f6dc0a28684e3c3e4e7e07e1b0f520a96c1da05

--- src/javasources/KTool/test/org/kframework/backend/pdmc/pda/pautomaton/PAutomatonTest.java
+++ src/javasources/KTool/test/org/kframework/backend/pdmc/pda/pautomaton/PAutomatonTest.java
@@ -2,6 +2,9 @@ package org.kframework.backend.pdmc.pda.pautomaton;
 
 import junit.framework.Assert;
 import org.junit.Test;
+import org.kframework.backend.pdmc.automaton.Transition;
+
+import java.util.Collection;
 
 /**
  * @author Traian
@@ -33,5 +36,24 @@ public class PAutomatonTest {
         String automaton = auto.toString();
         PAutomaton auto1 = PAutomaton.of(automaton);
         Assert.assertEquals(automaton,auto1.toString());
+    }
+
+
+    @Test
+    public void testGetPath() throws Exception {
+        String aut =
+                "p b r;\n" +
+                "q a r;\n" +
+                "r b p;\n" +
+                "r a q;\n" +
+                "q a t;\n" +
+                "p;\n" +
+                "t;\n";
+        PAutomaton auto = PAutomaton.of(aut);
+        PAutomatonState p = PAutomatonState.ofString("p");
+        PAutomatonState t = PAutomatonState.ofString("t");
+        Collection<Transition> path = auto.getPath(p, t);
+        System.err.println("One shortest path from p to t is: " + path);
+        org.junit.Assert.assertEquals("Path from p to t should have size 3", 3, path.size());
     }
 }
