10ff4bad2ffb5af6252ac06a6b4146dde8df3c53
Radu Mereuta <headness13@gmail.com>
2014-03-19 11:23:31 -0500

    Fixing an issue with Tokens creating ambiguities with variables.
    Former-commit-id: 22c4753cc736bb85ed3e766cd1c6f3b9e2d61c79 [formerly 20b2845c11d81c8e3797811953fd3ec816471814]
    Former-commit-id: 1e04b875f735f581f34db6eff12cff8b2d2769ca

--- src/javasources/KTool/src/org/kframework/compile/transformers/RemoveBrackets.java
+++ src/javasources/KTool/src/org/kframework/compile/transformers/RemoveBrackets.java
@@ -2,6 +2,11 @@ package org.kframework.compile.transformers;
 
 import org.kframework.kil.ASTNode;
 import org.kframework.kil.Bracket;
+import org.kframework.kil.GenericToken;
+import org.kframework.kil.Production;
+import org.kframework.kil.Term;
+import org.kframework.kil.TermCons;
+import org.kframework.kil.Terminal;
 import org.kframework.kil.loader.Context;
 import org.kframework.kil.visitors.CopyOnWriteTransformer;
 import org.kframework.kil.visitors.exceptions.TransformerException;
@@ -13,6 +18,28 @@ public class RemoveBrackets extends CopyOnWriteTransformer {
 
     public RemoveBrackets(Context context) {
         super("Remove brackets", context);
+    }
+
+    /**
+     * Fixing an issue where syntax like Id ::= "Main" should be automatically be transformed
+     * into constants (#token). I'm letting it parse as klabel at first so I don't modify the SDF parser
+     * which is unstable on modifications. After it parses I use the RemoveBrackets filter to do the post
+     * processing that transforms the TermCons element into a constant. Putting it here ensures me that
+     * the filter will be applied in every parser call.
+     */
+    @Override
+    public ASTNode transform(TermCons tc) throws TransformerException {
+        if (context.getTokenSorts().contains(tc.getSort())) {
+            Production p = (Production) tc.getProduction();
+            if (p.getItems().size() == 1 && p.getItems().get(0) instanceof Terminal) {
+                Terminal t = (Terminal) p.getItems().get(0);
+                Term trm = GenericToken.kAppOf(p.getSort(), t.getTerminal());
+                trm.setFilename(tc.getFilename());
+                trm.setLocation(tc.getLocation());
+                return trm;
+            }
+        }
+        return super.transform(tc);
     }
 
     @Override
