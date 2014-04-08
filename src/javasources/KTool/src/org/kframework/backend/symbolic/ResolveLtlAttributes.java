c8dce877f8f6946e9ef2e2cb3c4c0ae0ff5737a3
andreiarusoaie <andrei.arusoaie@gmail.com>
2014-04-08 23:42:10 +0300

    Fixing Dwight's comments.
    Former-commit-id: 4f4c551b73f5144b3b2e6ad38c12df15d7ab11b9 [formerly a1018950994217d8e23e769d1ae7561878d0dd37]
    Former-commit-id: 7b06308fd222639cd5a7a69ec1fc1385ca0db2e7

--- src/javasources/KTool/src/org/kframework/backend/symbolic/ResolveLtlAttributes.java
+++ src/javasources/KTool/src/org/kframework/backend/symbolic/ResolveLtlAttributes.java
@@ -7,7 +7,6 @@ import org.kframework.kil.BoolBuiltin;
 import org.kframework.kil.KApp;
 import org.kframework.kil.KInjectedLabel;
 import org.kframework.kil.KLabelConstant;
-import org.kframework.kil.KList;
 import org.kframework.kil.Rewrite;
 import org.kframework.kil.Rule;
 import org.kframework.kil.StringBuiltin;
@@ -44,120 +43,96 @@ import java.util.List;
  */
 public class ResolveLtlAttributes extends CopyOnWriteTransformer {
 
+    // the 'ltl' attribute used to identify the LTL satisfaction rules
     private static final String LTL = "ltl";
-    private static final String LTL_SAT = "'_|=Ltl_";
+
+    // the LTL satisfaction relation
+    public static final String LTL_SAT = "'_|=Ltl_";
 
     public ResolveLtlAttributes(Context context) {
         super("Resolve LTL attributes", context);
     }
 
     @Override
-    public ASTNode transform(Rule node) throws TransformerException {
-
-        if (node.getAttributes().containsKey(LTL)) {
-            Term body = node.getBody();
+    public ASTNode transform(Rule rule) throws TransformerException {
+        if (rule.getAttributes().containsKey(LTL)) {
+            Term body = rule.getBody();
             if (body instanceof Rewrite) {
+                // get the rule's side condition
+                Term requires = rule.getRequires();
 
-                // split the rule content into components
-                Rewrite rewrite = (Rewrite) body;
-                Term rewriteLHS = rewrite.getLeft();
-                Term rewriteRHS = rewrite.getRight();
-                Term requires = node.getRequires();
-                Term ensures = node.getEnsures();
+                // search the LTL state
+                SearchLTLState searchLTLState = new SearchLTLState(context);
+                rule.accept(searchLTLState);
 
-
-                // process the left hand side
-                if (rewriteLHS instanceof KApp) {
-                    // extract the left hand side top KApp
-                    KApp rewriteLHSBody = (KApp) rewriteLHS;
-                    if (rewriteLHSBody.getLabel() instanceof KLabelConstant) {
-                        // get left hand side KLabel
-                        KLabelConstant rewriteLHSLabel = (KLabelConstant) rewriteLHSBody.getLabel();
-
-                        // check if the left hand side KLabel is '|=Ltl
-                        if (rewriteLHSLabel.getLabel().equals(LTL_SAT)) {
-                            // Prepare to extract the components of the '|=Ltl KApp body which
-                            // should ALWAYS have a variable representing the program state in the
-                            // left hand side and a predicate in the right hand side:
-                            //     B |=Ltl predicate
-                            KList rewriteLHSContents = (KList) rewriteLHSBody.getChild();
-
-                            Term expectedVariable = rewriteLHSContents.getContents().get(0);
-                            // extract the actual Variable instance from the AST
-                            expectedVariable = ((KApp) expectedVariable).getLabel();
-                            expectedVariable = ((KInjectedLabel) expectedVariable).getTerm();
-
-                            // If expectedVariable is not a Variable, then throw an error message.
-                            if (!(expectedVariable instanceof Variable)) {
-                                GlobalSettings.kem.register(new KException(
-                                        KException.ExceptionType.ERROR,
-                                        KException.KExceptionGroup.CRITICAL,
-                                        "the 'ltl' attribute can be used only for rules " +
-                                                "having only a variable in the left hand side of "
-                                                + LTL_SAT,
-                                        node.getFilename(),
-                                        node.getLocation()));
-                            } else {
-                                // process the variable: wrap it with generatedTop and add the path condition
-                                Variable phi = Variable.getFreshVar("K");
-                                Term ltlState = WrapVariableWithTopCell.wrapPCAndVarWithGeneratedTop(phi, expectedVariable);
-
-                                // process the predicate: traverse it and replace all the
-                                // occurrences of expectedVariable with the newly constructed ltlState
-                                String ltlVarStateName = ((Variable) expectedVariable).getName();
-                                Term ltlPredicate = rewriteLHSContents.getContents().get(1);
-                                ltlPredicate = (Term) ltlPredicate.accept(new WrapVariableWithTopCell(context, phi, ltlVarStateName));
-
-                                // process the right hand side of the rule
-                                rewriteRHS = (Term) rewriteRHS.accept(new WrapVariableWithTopCell(context, phi, ltlVarStateName));
-
-                                // process the rule's conditions
-                                if (requires != null) {
-                                    requires = (Term) requires.accept(new WrapVariableWithTopCell(context, phi, ltlVarStateName));
-                                }
-                                if (ensures != null) {
-                                    ensures = (Term) ensures.accept(new WrapVariableWithTopCell(context, phi, ltlVarStateName));
-                                }
-
-                                // check if the path condition implies the rule condition
-                                // step 1: filter the condition and separate the smtValid part
-                                //         of it (operations which can be sent to the
-                                //         SMT solver) from the smtInvalid clauses.
-                                //         For the latter add predicates in condition.
-                                ConditionTransformer ct = new ConditionTransformer(context);
-                                Term smtInvalidCondition = (Term) requires.accept(ct);
-                                List<Term> filtered = ct.getFilteredTerms();
-                                filtered.add(BoolBuiltin.TRUE);
-                                Term smtValidCondition = AddPathCondition.andBool(filtered);
-                                Term predicates = AddPathCondition.andBool(ct.getGeneratedPredicates());
-
-                                // step 2: check the implication using the SMT solver using 'checkSat;
-                                // Note that we use smtValid instead of the rule condition because
-                                // predicates are not important when checking the implication.
-                                Term conditionNegation = KApp.of(KLabelConstant.NOTBOOL_KLABEL, smtValidCondition);
-                                Term implication = KApp.of(KLabelConstant.BOOL_ANDBOOL_KLABEL, phi, conditionNegation);
-                                KApp unsat = StringBuiltin.kAppOf("unsat");
-                                KApp checkSat = KApp.of(KLabelConstant.of("'checkSat", context), implication);
-                                Term equalsUnsat = KApp.of(KLabelConstant.KEQ_KLABEL, checkSat, unsat);
-                                // append the implication and the predicates to condition
-                                requires = KApp.of(KLabelConstant.BOOL_ANDBOOL_KLABEL, smtInvalidCondition, equalsUnsat);
-                                requires = KApp.of(KLabelConstant.BOOL_ANDBOOL_KLABEL, predicates, requires);
-
-                                // reconstruct the rule and return
-                                ltlState = KApp.of(new KInjectedLabel(ltlState), KList.EMPTY);
-                                rewriteLHS = KApp.of(KLabelConstant.of(rewriteLHSLabel.getLabel()), ltlState, ltlPredicate);
-                                Rule newRule = new Rule(rewriteLHS, rewriteRHS, context);
-                                newRule.setRequires(requires);
-                                newRule.setEnsures(ensures);
-                                return newRule;
-                            }
-                        }
-                    }
+                // The LTL state should ALWAYS be a variable.
+                // Otherwise, an error message is thrown.
+                Term expectedVariable = searchLTLState.getLtlState();
+                // extract the actual instance from the AST
+                try {
+                    expectedVariable = ((KApp) expectedVariable).getLabel();
+                    expectedVariable = ((KInjectedLabel) expectedVariable).getTerm();
+                } catch (ClassCastException e) {
+                    GlobalSettings.kem.register(new KException(
+                            KException.ExceptionType.ERROR,
+                            KException.KExceptionGroup.CRITICAL,
+                            "could not extract the term representing the " +
+                                    "LTL state from left hand side of the rule",
+                            rule.getFilename(),
+                            rule.getLocation()));
                 }
+                // If expectedVariable is not a Variable, then throw an error message.
+                if (!(expectedVariable instanceof Variable)) {
+                    GlobalSettings.kem.register(new KException(
+                            KException.ExceptionType.ERROR,
+                            KException.KExceptionGroup.CRITICAL,
+                            "the 'ltl' attribute can be used only for rules " +
+                                    "having only a variable in the left hand side of "
+                                    + LTL_SAT,
+                            rule.getFilename(),
+                            rule.getLocation()));
+                } else {
+                    // create a variable representing the path condition
+                    Variable phi = Variable.getFreshVar("K");
 
+                    // wrap the LTL state variable and the path condition phi with
+                    // <generatedTop> cell
+                    String ltlVarStateName = ((Variable) expectedVariable).getName();
+                    WrapVariableWithTopCell wrapper = new WrapVariableWithTopCell(context, phi, ltlVarStateName);
+                    rule = (Rule) rule.accept(wrapper);
+
+                    // check if the path condition implies the rule condition
+                    // step 1: filter the condition and separate the smtValid part
+                    //         of it (operations which can be sent to the
+                    //         SMT solver) from the smtInvalid clauses.
+                    //         For the latter add predicates in condition.
+                    ConditionTransformer ct = new ConditionTransformer(context);
+                    Term smtInvalidCondition = (Term) requires.accept(ct);
+                    List<Term> filtered = ct.getFilteredTerms();
+                    filtered.add(BoolBuiltin.TRUE);
+                    Term smtValidCondition = AddPathCondition.andBool(filtered);
+                    Term predicates = AddPathCondition.andBool(ct.getGeneratedPredicates());
+
+                    // step 2: check the implication using the SMT solver using 'checkSat;
+                    // Note that we use smtValid instead of the rule condition because
+                    // predicates are not important when checking the implication.
+                    Term conditionNegation = KApp.of(KLabelConstant.NOTBOOL_KLABEL, smtValidCondition);
+                    Term implication = KApp.of(KLabelConstant.BOOL_ANDBOOL_KLABEL, phi, conditionNegation);
+                    KApp unsat = StringBuiltin.kAppOf("unsat");
+                    KApp checkSat = KApp.of(KLabelConstant.of("'checkSat", context), implication);
+                    Term equalsUnsat = KApp.of(KLabelConstant.KEQ_KLABEL, checkSat, unsat);
+                    // append the implication and the predicates to condition
+                    requires = KApp.of(KLabelConstant.BOOL_ANDBOOL_KLABEL, smtInvalidCondition, equalsUnsat);
+                    requires = KApp.of(KLabelConstant.BOOL_ANDBOOL_KLABEL, predicates, requires);
+
+                    // add the new side condition of the rule and return
+                    Rule newRule = rule.shallowCopy();
+                    newRule.setRequires(requires);
+                    return newRule;
+                }
             }
         }
 
-        return super.transform(node);
+        return super.transform(rule);
     }
 }
