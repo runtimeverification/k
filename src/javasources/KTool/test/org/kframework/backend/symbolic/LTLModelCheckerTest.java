2f585450151de41190f467cbee0dbeca46322ac3
andreiarusoaie <andrei.arusoaie@gmail.com>
2014-04-10 23:12:32 +0300

    Modified junit test to use term equality instead of string equality.
    Former-commit-id: 58d4375b1c5ef4d0cc2b314a6e8b29649c6df4e8 [formerly 4bcbbb0b0b094c2645847801b2582eb06989c808]
    Former-commit-id: 50e874b90d1f807352848b0eee2802e581476bde

--- src/javasources/KTool/test/org/kframework/backend/symbolic/LTLModelCheckerTest.java
+++ src/javasources/KTool/test/org/kframework/backend/symbolic/LTLModelCheckerTest.java
@@ -18,8 +18,6 @@ import org.kframework.kil.loader.Context;
 import org.kframework.kil.visitors.exceptions.TransformerException;
 import org.mockito.Mockito;
 
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -43,46 +41,73 @@ public class LTLModelCheckerTest {
     @Test
     public void testSymbolicLTLStateRuleTransformer() {
 
-        String inputRule = "rule '_|=Ltl_(# B:Bag (),, eq(X:Id ,, I:Int )) => #token(\"#Bool\", \"true\")() requires '_=K_(val(# B:Bag (),, X:Id ),, I:Int ) [ ltl(null),  anywhere(null)]\n";
-        String outputRule = "rule '_|=Ltl_(# <generatedTop> B:Bag <path-condition> GeneratedFreshVar0:K  </path-condition>  </generatedTop> (),, eq(X:Id ,, I:Int )) => #token(\"#Bool\", \"true\")() requires '#andBool('_=K_(val(# <generatedTop> B:Bag <path-condition> GeneratedFreshVar0:K  </path-condition>  </generatedTop> (),, X:Id ),, I:Int ),, #token(\"#Bool\", \"true\")(),, '_==K_('checkSat('_andBool_(GeneratedFreshVar0:K ,, 'notBool_('_andBool_(#token(\"#Bool\", \"true\")(),, #token(\"#Bool\", \"true\")())))),, #token(\"#String\", \"\\\"unsat\\\"\")())) [ ltl(null),  anywhere(null)]\n";
-
-        // create input the rule
+        // create the input rule
         // LHS
-        Variable LTLState = new Variable("B", KSorts.BAG);
-        KApp LTLStateKApp = KApp.of(new KInjectedLabel(LTLState), KList.EMPTY);
+        Variable inputLTLState = new Variable("B", KSorts.BAG);
+        KApp inputLTLStateKApp = KApp.of(new KInjectedLabel(inputLTLState), KList.EMPTY);
         Variable X = new Variable("X", "Id");
         Variable I = new Variable("I", "Int");
         KApp predicate = KApp.of(KLabelConstant.of("eq"), X, I);
-        Term lhs = KApp.of(KLabelConstant.of(ResolveLtlAttributes.LTL_SAT), LTLStateKApp, predicate);
+        Term inputLhs = KApp.of(KLabelConstant.of(ResolveLtlAttributes.LTL_SAT), inputLTLStateKApp, predicate);
 
         // RHS
-        Term rhs = BoolBuiltin.TRUE;
+        Term inputRhs = BoolBuiltin.TRUE;
 
         // requires
-        KApp val = KApp.of(KLabelConstant.of("val"), LTLStateKApp, X.shallowCopy());
-        Term requires = KApp.of(KLabelConstant.KEQ, val, I.shallowCopy());
+        KApp val = KApp.of(KLabelConstant.of("val"), inputLTLStateKApp, X.shallowCopy());
+        Term inputRequires = KApp.of(KLabelConstant.KEQ, val, I.shallowCopy());
 
         // attributes
-        Attributes attrs = new Attributes();
-        attrs.set("ltl", null);
-        attrs.set("anywhere", null);
+        Attributes inputAttrs = new Attributes();
+        inputAttrs.set("ltl", null);
+        inputAttrs.set("anywhere", null);
 
         // rule
-        Rule rule = new Rule(lhs, rhs, context);
-        rule.setRequires(requires);
+        Rule rule = new Rule(inputLhs, inputRhs, context);
+        rule.setRequires(inputRequires);
         rule.setEnsures(null);
-        rule.setAttributes(attrs);
+        rule.setAttributes(inputAttrs);
 
-        // test input rule
-        Assert.assertEquals(rule.toString().trim(), inputRule.trim());
+
+        // create the output rule
+        // LHS
+        Variable phi = new Variable("GeneratedFreshVar0", "K");
+        Term outputLTLState = WrapVariableWithTopCell.wrapPCAndVarWithGeneratedTop(phi, inputLTLState.shallowCopy());
+        KApp outputLTLStateKApp = KApp.of(new KInjectedLabel(outputLTLState), KList.EMPTY);
+        Term outpuLhs = KApp.of(KLabelConstant.of(ResolveLtlAttributes.LTL_SAT), outputLTLStateKApp, predicate.shallowCopy());
+
+        // RHS
+        Term outputRhs = BoolBuiltin.TRUE;
+
+        // requires
+        KApp outputVal = KApp.of(KLabelConstant.of("val"), outputLTLStateKApp, X.shallowCopy());
+        KApp defaultTrue = KApp.of(KLabelConstant.BOOL_ANDBOOL_KLABEL, BoolBuiltin.TRUE, BoolBuiltin.TRUE);
+        Term outputCheckSat = ResolveLtlAttributes.createSMTImplicationTerm(phi, defaultTrue, context);
+        Term valEq = KApp.of(KLabelConstant.KEQ, outputVal, I.shallowCopy());
+        Term outputRequires = KApp.of(KLabelConstant.ANDBOOL_KLABEL, valEq, BoolBuiltin.TRUE, outputCheckSat);
+
+        // attributes
+        Attributes outputAttrs = new Attributes();
+        outputAttrs.set("ltl", null);
+        outputAttrs.set("anywhere", null);
+
+        // rule
+        Rule outputRule = new Rule(outpuLhs, outputRhs, context);
+        outputRule.setRequires(outputRequires);
+        outputRule.setEnsures(null);
+        outputRule.setAttributes(outputAttrs);
+
 
         // test ResolverLTLAttributes transformer
         ResolveLtlAttributes resolveLtlAttributes = new ResolveLtlAttributes(context);
         try {
             rule = (Rule) rule.accept(resolveLtlAttributes);
-            Assert.assertEquals(rule.toString().trim(), outputRule.trim());
+            Assert.assertEquals(rule.getBody(), outputRule.getBody());
+            Assert.assertEquals(rule.getRequires(), outputRule.getRequires());
+            Assert.assertEquals(rule.getEnsures(), outputRule.getEnsures());
         } catch (TransformerException e) {
             Assert.fail();
         }
     }
+
 }
