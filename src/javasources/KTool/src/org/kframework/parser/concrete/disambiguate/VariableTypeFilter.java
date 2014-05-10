0d236d42981c297d93dec682f69e1070ee2e54c7
Radu Mereuta <headness13@gmail.com>
2014-05-10 12:34:23 -0500

    Fixing user typedness for variables being global (per rule).
    Conflicts:
    
    	src/javasources/KTool/src/org/kframework/parser/concrete/disambiguate/VariableTypeFilter.java
    
    
    Former-commit-id: 0720efbe1fb791e4a9dd7fe5d4380b8217ea86f0 [formerly 300347c88f3a4769cd69ec96fdc4142b855fd2fe]
    Former-commit-id: 043dcd19c8eecdace5930b64c0e11bcd84bac400

--- src/javasources/KTool/src/org/kframework/parser/concrete/disambiguate/VariableTypeFilter.java
+++ src/javasources/KTool/src/org/kframework/parser/concrete/disambiguate/VariableTypeFilter.java
@@ -1,4 +1,3 @@
-// Copyright (c) 2012-2014 K Team. All Rights Reserved.
 package org.kframework.parser.concrete.disambiguate;
 
 import org.kframework.kil.ASTNode;
@@ -38,7 +37,7 @@ public class VariableTypeFilter extends ParseForestTransformer {
                 newV.setSort(correctVar.getSort());
             newV.setExpectedSort(correctVar.getExpectedSort());
             newV.setSyntactic(correctVar.isSyntactic());
-            newV.setUserTyped(correctVar.isUserTyped());
+            newV.setUserTyped(r.isUserTyped());
             return newV;
         }
         String msg = "Variable " + r.getName() + " is contextually expected to have sort " + r.getExpectedSort();
