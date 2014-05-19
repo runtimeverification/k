f8caf40b8849974c3e840e0165fd13ece5910228
Andrei Stefanescu <andreistef@gmail.com>
2014-05-19 10:12:58 -0500

    fixed bug in I/O builtins when returning K
    Former-commit-id: afa62143a4b44c632626b72a7ab345ff746e0db7 [formerly fef14e05673c6c343da8ec7d0e7b2fd520db2ba2]
    Former-commit-id: 4400175dbdf41d4305739734aceca970e0d79bc2

--- src/javasources/KTool/src/org/kframework/backend/java/kil/KLabelInjection.java
+++ src/javasources/KTool/src/org/kframework/backend/java/kil/KLabelInjection.java
@@ -1,3 +1,5 @@
+// Copyright (c) 2013-2014 K Team. All Rights Reserved.
+
 package org.kframework.backend.java.kil;
 
 import org.kframework.backend.java.symbolic.Matcher;
@@ -7,12 +9,20 @@ import org.kframework.backend.java.symbolic.Visitor;
 import org.kframework.kil.ASTNode;
 
 
+
 /**
  * An injection KLabel.
  *
  * @author AndreiS
  */
 public class KLabelInjection extends KLabel {
+
+    /**
+     * Returns an injection of the term into a {@link org.kframework.backend.java.kil.KItem}.
+     */
+    public static KItem injectionOf(Term term, TermContext context) {
+        return new KItem(new KLabelInjection(term), new KList(), context);
+    }
 
     private final Term term;
 
