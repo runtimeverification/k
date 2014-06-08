70fb82baf15b7b9d59aea754d7cceac4eff0c9c0
Cosmin Radoi <cosmin.radoi@gmail.com>
2014-06-08 14:19:39 -0500

    remove KLabelConstant dependence on TermContext
    Former-commit-id: a5fcab4229649cbfe672e8d949899cb72704fb42 [formerly b127c9918b46f72b8577b2937f68d93cad8aca77]
    Former-commit-id: 4579df5d4af53f692e2daf31b932656deaea312f

--- src/javasources/KTool/src/org/kframework/backend/java/symbolic/BuiltinFunction.java
+++ src/javasources/KTool/src/org/kframework/backend/java/symbolic/BuiltinFunction.java
@@ -25,7 +25,6 @@ import java.util.Properties;
 
 import com.google.common.collect.ImmutableSet;
 
-
 /**
  * Utility class that handles the builtin (hooked) operations and their Java
  * implementations.
@@ -44,12 +43,8 @@ public class BuiltinFunction {
      * @see org.kframework.backend.java.symbolic.KILtoBackendJavaKILTransformer#evaluateDefinition(org.kframework.backend.java.kil.Definition)
      * @see org.kframework.backend.java.symbolic.KILtoBackendJavaKILTransformer#evaluateRule(org.kframework.backend.java.kil.Rule, org.kframework.backend.java.kil.Definition)
      */
-    private static final ImmutableSet<String> hookMetaModules = ImmutableSet.of(
-            "#META-K",
-            "MetaK",
-            "Visitor",
-            "#IO",
-            "#FRESH");
+    private static final ImmutableSet<String> hookMetaModules = ImmutableSet.of("#META-K",
+            "MetaK", "Visitor", "#IO", "#FRESH");
 
     /**
      * Map of {@link KLabelConstant} representation of builtin (hooked) operations to
@@ -59,73 +54,68 @@ public class BuiltinFunction {
 
     public static void init(Definition definition) {
         /* initialize {@code table} */
-            String separator = System.getProperty("file.separator");
-            String path = KPaths.getKBase(false) + separator + "include" + separator + "java";
-            Properties properties = new Properties();
+        String separator = System.getProperty("file.separator");
+        String path = KPaths.getKBase(false) + separator + "include" + separator + "java";
+        Properties properties = new Properties();
 
-            String propertyFile = path + separator + hookPropertiesFileName;
-            try {
-                FileUtil.loadProperties(properties, propertyFile);
-            } catch (IOException e) {
-                if (definition.context().globalOptions.debug) {
-                    e.printStackTrace();
-                }
-                GlobalSettings.kem.register(new KException(ExceptionType.ERROR, KExceptionGroup.INTERNAL, "Could not read from " + propertyFile));
+        String propertyFile = path + separator + hookPropertiesFileName;
+        try {
+            FileUtil.loadProperties(properties, propertyFile);
+        } catch (IOException e) {
+            if (definition.context().globalOptions.debug) {
+                e.printStackTrace();
             }
+            GlobalSettings.kem.register(new KException(ExceptionType.ERROR,
+                    KExceptionGroup.INTERNAL, "Could not read from " + propertyFile));
+        }
 
-            for (String label : definition.context().labels.keySet()) {
-                for (Production production : definition.context().productionsOf(label)) {
-                    if (production.getKLabel().equals(label)    // make sure the label is a Klabel
-                            && production.containsAttribute(Attribute.HOOK_KEY)) {
-                        String hookAttribute = production.getAttribute(Attribute.HOOK_KEY);
-                        String hookPrefix = hookAttribute.substring(0, hookAttribute.indexOf(":"));
-                        /*
-                         * exclude hook from evaluation during compilation if the hook is dynamic
-                         * in nature (is related to I/O or to meta properties).
-                         * */
-                        if (K.do_kompilation && hookMetaModules.contains(hookPrefix)) {
-                            continue;
-                        }
+        for (String label : definition.context().labels.keySet()) {
+            for (Production production : definition.context().productionsOf(label)) {
+                if (production.getKLabel().equals(label) // make sure the label is a Klabel
+                        && production.containsAttribute(Attribute.HOOK_KEY)) {
+                    String hookAttribute = production.getAttribute(Attribute.HOOK_KEY);
+                    String hookPrefix = hookAttribute.substring(0, hookAttribute.indexOf(":"));
+                    /*
+                     * exclude hook from evaluation during compilation if the hook is dynamic
+                     * in nature (is related to I/O or to meta properties).
+                     * */
+                    if (K.do_kompilation && hookMetaModules.contains(hookPrefix)) {
+                        continue;
+                    }
 
-                        String hook = properties.getProperty(hookAttribute);
-                        if (hook == null) {
-                            GlobalSettings.kem.register(new KException(
-                                    KException.ExceptionType.HIDDENWARNING,
-                                    KException.KExceptionGroup.CRITICAL,
-                                    "missing entry in " + hookPropertiesFileName
-                                            + " for hook " + hookAttribute,
-                                    production.getFilename(),
-                                    production.getLocation()));
-                            continue;
-                        }
+                    String hook = properties.getProperty(hookAttribute);
+                    if (hook == null) {
+                        GlobalSettings.kem.register(new KException(
+                                KException.ExceptionType.HIDDENWARNING,
+                                KException.KExceptionGroup.CRITICAL, "missing entry in "
+                                        + hookPropertiesFileName + " for hook " + hookAttribute,
+                                production.getFilename(), production.getLocation()));
+                        continue;
+                    }
 
-                        try {
-                            String className = hook.substring(0, hook.lastIndexOf('.'));
-                            String methodName = hook.substring(hook.lastIndexOf('.') + 1);
-                            Class<?> c = Class.forName(className);
-                            for (Method method : c.getDeclaredMethods()) {
-                                if (method.getName().equals(methodName)) {
-                                    table.put(
-                                            KLabelConstant.of(label, TermContext.of(definition)),
-                                            method);
-                                    break;
-                                }
+                    try {
+                        String className = hook.substring(0, hook.lastIndexOf('.'));
+                        String methodName = hook.substring(hook.lastIndexOf('.') + 1);
+                        Class<?> c = Class.forName(className);
+                        for (Method method : c.getDeclaredMethods()) {
+                            if (method.getName().equals(methodName)) {
+                                table.put(KLabelConstant.of(label, definition), method);
+                                break;
                             }
-                        } catch (ClassNotFoundException | SecurityException e) {
-                            if (definition.context().globalOptions.debug) {
-                                e.printStackTrace();
-                            }
-                            GlobalSettings.kem.register(new KException(
-                                    KException.ExceptionType.WARNING,
-                                    KException.KExceptionGroup.CRITICAL,
-                                    "missing implementation for hook " + hookAttribute + ":\n"
-                                            + hook,
-                                    production.getFilename(),
-                                    production.getLocation()));
                         }
+                    } catch (ClassNotFoundException | SecurityException e) {
+                        if (definition.context().globalOptions.debug) {
+                            e.printStackTrace();
+                        }
+                        GlobalSettings.kem.register(new KException(
+                                KException.ExceptionType.WARNING,
+                                KException.KExceptionGroup.CRITICAL,
+                                "missing implementation for hook " + hookAttribute + ":\n" + hook,
+                                production.getFilename(), production.getLocation()));
                     }
                 }
             }
+        }
     }
 
     /**
@@ -141,16 +131,16 @@ public class BuiltinFunction {
      * @throws IllegalAccessException
      * @throws IllegalArgumentException
      */
-    public static Term invoke(TermContext context, KLabelConstant label, Term ... arguments)
+    public static Term invoke(TermContext context, KLabelConstant label, Term... arguments)
             throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-        Object[] args =  Arrays.copyOf(arguments, arguments.length + 1, Object[].class);
-        args[arguments.length] =  context;
+        Object[] args = Arrays.copyOf(arguments, arguments.length + 1, Object[].class);
+        args[arguments.length] = context;
         // TODO(YilongL): is reflection/exception really the best way to
         // deal with builtin functions? builtin functions are supposed to be
         // super-fast...
         Method method = table.get(label);
         Term t = (Term) method.invoke(null, args);
-        return t; 
+        return t;
     }
 
     /**
