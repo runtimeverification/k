5efd5ec4fa21417b4e5addba86482bac5e5471e6
Yilong Li <yli147@illinois.edu>
2014-06-24 12:46:13 -0500

    implemented a faster deep cloner
    Former-commit-id: c2918dcfeef95cf64570f1eb8cbe04b8abc5925f [formerly 28c4529380b2c7189aabb14e5e649d3cb98e6ffc]
    Former-commit-id: fb46cba799d342dfa28f2d25c64118d70c31c5fa

--- src/javasources/KTool/src/org/kframework/backend/java/symbolic/DeepCloner.java
+++ src/javasources/KTool/src/org/kframework/backend/java/symbolic/DeepCloner.java
@@ -1,6 +1,8 @@
 // Copyright (c) 2014 K Team. All Rights Reserved.
 package org.kframework.backend.java.symbolic;
 
+import java.util.Map;
+
 import org.kframework.backend.java.kil.Immutable;
 import org.kframework.backend.java.kil.Term;
 
@@ -14,7 +16,7 @@ import com.rits.cloning.Cloner;
  */
 public class DeepCloner {
     
-    private static final Cloner cloner = new Cloner();
+    private static final Cloner cloner = new FastTermCloner();
     
     static {
         cloner.dontCloneInstanceOf(Immutable.class);
@@ -23,5 +25,22 @@ public class DeepCloner {
     public static Term clone(Term term) {
         return cloner.deepClone(term);
     }
-       
+    
+    /**
+     * Besides {@link Immutable} terms, avoid cloning terms that are possibly
+     * mutable but actually immutable at run-time. E.g., a {@code KItem} is
+     * possibly but rarely mutable in practice.
+     */
+    private static class FastTermCloner extends Cloner {
+
+        @Override
+        protected Object fastClone(final Object o, final Map<Object, Object> clones) throws IllegalAccessException {
+            if (o instanceof Term && !((Term) o).hasCell()) {
+                return o;
+            } else {
+                return super.fastClone(o, clones);
+            }
+        }
+    }
+
 }
