208dc3d24f1ba3dcf24dffe615b58a8ff6b79253
Dwight Guth <dwightguth@runtimeverification.com>
2014-07-15 15:18:10 -0500

    fix checkstyle
    Former-commit-id: 2cf0844d3c2e7df9c3c109a06ff6cea7b233426d [formerly 45b1def3169cf1dbc5060283bb4c958e72e9a586]
    Former-commit-id: 44d7e472e151fffd9355a39b765d4254b1c49cc3

--- src/javasources/KTool/src/org/kframework/backend/java/symbolic/JavaExecutionOptions.java
+++ src/javasources/KTool/src/org/kframework/backend/java/symbolic/JavaExecutionOptions.java
@@ -8,11 +8,11 @@ import com.beust.jcommander.Parameter;
 
 public final class JavaExecutionOptions {
     @Parameter(names="--generate-tests", description="Test programs will be generated along with "
-	+ "normal search.")
+        + "normal search.")
     public boolean generateTests = false;
     
     @Parameter(names="--deterministic-functions", description="Throw assertion failure during "
-	+ "execution in the java backend if function definitions are not deterministic.")
+        + "execution in the java backend if function definitions are not deterministic.")
     public boolean deterministicFunctions = false;
     
     public boolean concreteExecution() {
@@ -27,7 +27,7 @@ public final class JavaExecutionOptions {
     public boolean indexingStats = false;
     
     @Parameter(names="--pattern-matching", description="Use pattern-matching rather than "
-	+ "unification to drive rewriting in the Java backend.")
+        + "unification to drive rewriting in the Java backend.")
     public boolean patternMatching = false;
 }
 
