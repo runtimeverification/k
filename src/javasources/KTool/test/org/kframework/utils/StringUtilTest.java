8a577ef626e06986ffac5d15837ad33382135d00
Radu Mereuta <headness13@gmail.com>
2014-07-25 19:52:16 -0500

    Added a test for enquoteKString also.
    Former-commit-id: 9facb2f368b7c925e2774bf88babde8cd9ea315e [formerly 4b1a90417d3c82d8660ea4ab1e64774ae5cea5e7]
    Former-commit-id: a7e03af19e7c3961ed8c2a5e6db98dac31450d34

--- src/javasources/KTool/test/org/kframework/utils/StringUtilTest.java
+++ src/javasources/KTool/test/org/kframework/utils/StringUtilTest.java
@@ -31,4 +31,20 @@ public class StringUtilTest {
             // good
         }
     }
+
+    @Test
+    public void testKBijection() {
+        char[] all = new char[257];
+        for (int i = 0; i < 256; i++)
+            all[i] = (char) i;
+        all[256] = 0xffff;
+        String str = String.valueOf(all);
+        String enquoted = StringUtil.enquoteKString(str);
+        String backToAll = StringUtil.unquoteKString(enquoted);
+        char[] all2 = backToAll.toCharArray();
+
+        Assert.assertEquals("Different sizes after running quote unquote.", all.length , all2.length);
+        for (int i = 0; i < 257; i++)
+            Assert.assertEquals("Different values at position: " + i, all[i] , all2[i]);
+    }
 }
