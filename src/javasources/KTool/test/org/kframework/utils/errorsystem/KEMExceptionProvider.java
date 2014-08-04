636d4e88c2e353fd853efa89afb12e07178b0ab0
Dwight Guth <dwightguth@runtimeverification.com>
2014-08-04 11:31:44 -0500

    add test case
    Former-commit-id: 359ab44020996c2f61ced15750849c7611d4bf3e [formerly a97c16d6c75422b8d65e3ae4172074f72b0020ba]
    Former-commit-id: 50545fee5dc368777ccab38e51862fa09a02e47d

---
+++ src/javasources/KTool/test/org/kframework/utils/errorsystem/KEMExceptionProvider.java
@@ -0,0 +1,14 @@
+package org.kframework.utils.errorsystem;
+
+import org.kframework.main.GlobalOptions;
+
+import com.google.inject.Provider;
+
+public class KEMExceptionProvider<T> implements Provider<T> {
+
+    @Override
+    public T get() {
+        new KExceptionManager(new GlobalOptions()).registerCriticalError("foo");
+        throw new AssertionError("unreachable");
+    }
+}
