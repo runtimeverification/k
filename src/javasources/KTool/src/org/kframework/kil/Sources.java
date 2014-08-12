b915ce944f98ace6350bd6619f7621ae1ebec756
Dwight Guth <dwightguth@runtimeverification.com>
2014-08-12 10:34:53 -0500

    add file I forgot
    Former-commit-id: 160411fa2e97fa3a6ab55e5a4d935633dbc6273b [formerly a108ad0cd90d16638b5d79a399a6745994ffa9b9]
    Former-commit-id: 1ca68a872a192d61b7c6ee8f3e8d43ed8b2e3cca

---
+++ src/javasources/KTool/src/org/kframework/kil/Sources.java
@@ -0,0 +1,24 @@
+package org.kframework.kil;
+
+import java.io.File;
+
+public class Sources {
+
+    private Sources() {}
+
+    public static Source generatedBy(Class<?> sourceClass) {
+        return new GeneratedSource(sourceClass);
+    }
+
+    public static Source fromFile(File filename) {
+        return new FileSource(filename);
+    }
+
+    public static Source fromFile(String filename) {
+        return new FileSource(new File(filename));
+    }
+
+    public static Source fromCommandLine(String optionName) {
+        return new CommandLineSource(optionName);
+    }
+}
