c7cc740a277922a4109e27236f89c902a4a564eb
Andrei Stefanescu <andreistef@gmail.com>
2014-08-16 10:59:10 -0500

    added proof files for bst insert delete and find
    Former-commit-id: 46a70598a990925e0064fd1dfe42969119f23ec0 [formerly 18e0eec067360ad7b8950669cc2cebe7ca59272f]
    Former-commit-id: d71500a15c5ec5c622354d7c06230a9fb9c70315

---
+++ samples/kernelc/tests/binary_search_tree/find.c
@@ -0,0 +1,23 @@
+/*
+ * Function that searches a binary search tree for a node with a certain value.
+ */
+#include <stdlib.h>
+
+struct treeNode {
+  int value;
+  struct treeNode *left;
+  struct treeNode *right;
+};
+
+int find(int v, struct treeNode *t)
+{
+  if (t == NULL)
+    return 0;
+  else if (v == t->value)
+    return 1;
+  else if (v < t->value)
+    return find(v, t->left);
+  else
+    return find(v, t->right);
+}
+
--- samples/kernelc/tests/binary_search_tree/insert.c
+++ samples/kernelc/tests/binary_search_tree/insert.c
@@ -2,12 +2,10 @@
  * Function that inserts a new value into a binary search tree.
  */
 
-
 #include <stdlib.h>
 
-
 struct treeNode {
-  int val;
+  int value;
   struct treeNode *left;
   struct treeNode *right;
 };
@@ -17,7 +15,7 @@ struct treeNode* new_node(int v)
 {
   struct treeNode *node;
   node = (struct treeNode *) malloc(sizeof(struct treeNode));
-  node->val = v;
+  node->value = v;
   node->left = NULL;
   node->right = NULL;
   return node;
@@ -25,20 +23,15 @@ struct treeNode* new_node(int v)
 
 
 struct treeNode* insert(int v, struct treeNode *t)
-/*@ rule <k> $ => return ?t; ...</k>
-         <heap>... tree(t)(T) => tree(?t)(?T) ...</heap>
-    if isBst(T) /\ isBst(?T) /\ tree2mset(?T) = tree2mset(T) U {v} */
 {
   if (t == NULL)
     return new_node(v);
 
-  if (v < t->val)
+  if (v < t->value)
     t->left = insert(v, t->left);
-  else
+  else if (v > t->value) 
     t->right = insert(v, t->right);
 
   return t;
 }
 
-
-//@ var T : Tree
