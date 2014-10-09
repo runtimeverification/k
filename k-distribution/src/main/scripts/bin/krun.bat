d4540fbfbe13647056841adb9b0ddb61a5c58055
Dwight Guth <dwightguth@runtimeverification.com>
2014-10-09 10:09:59 -0500

    fix batch scripts
    Former-commit-id: cfc7e8745350df2df471a8e29730ea47f299d484 [formerly a4d36c0079eb8b8ab4453e7dd2c135087fbf9b5f]
    Former-commit-id: 88438a57c8ae4ec3fdc12f6b59856813630c0680

--- k-distribution/src/main/scripts/bin/krun.bat
+++ k-distribution/src/main/scripts/bin/krun.bat
@@ -1,8 +1,2 @@
 @ECHO off
-set PATH=%PATH%;"%~dp0..\lib\native"
-set CYGWIN=nodosfilewarning
-SETLOCAL ENABLEEXTENSIONS
-IF ERRORLEVEL 1 ECHO Unable to enable extensions
-IF NOT DEFINED K_OPTS SET K_OPTS=-Xms64m -Xmx1024m -Xss32m -XX:+TieredCompilation
-java  -Djava.awt.headless=true -ea %K_OPTS% -cp "%~dp0..\lib\java\*" org.kframework.main.Main -krun %*
-ENDLOCAL
+call "%~dp0..\lib\k.bat" -krun %*
---
+++ k-distribution/src/main/scripts/lib/setenv.bat
@@ -0,0 +1,10 @@
+@ECHO off
+for /f "delims=" %%i in ('java -version 2>&1') do set VERSION=%%i
+if not %version:64-Bit=% == %version% (
+  set ARCH=64
+) else (
+  set ARCH=32
+)
+
+set PATH=%PATH%;"%~dp0\native\windows";"%~dp0\native\windows%ARCH%";"%~dp0\native\%ARCH%"
+set CYGWIN=nodosfilewarning
