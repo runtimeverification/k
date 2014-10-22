c823d92a40295689f7eddd942f2fe986d77e03b8
Yilong Li <yli147@illinois.edu>
2014-10-22 15:23:41 -0500

    fixed operations andThenBool and orElseBool
    Former-commit-id: fe261276cbccaf4e56b7110f6e49b060a482f92d [formerly f9db8811c35f3e2d271e6b414210a10f0c81294e]
    Former-commit-id: 73dd628c6829c819cefe9933a17551269995d1f6

--- java-backend/src/main/java/org/kframework/backend/java/builtins/BuiltinBoolOperations.java
+++ java-backend/src/main/java/org/kframework/backend/java/builtins/BuiltinBoolOperations.java
@@ -2,6 +2,7 @@
 package org.kframework.backend.java.builtins;
 
 
+import org.kframework.backend.java.kil.Term;
 import org.kframework.backend.java.kil.TermContext;
 
 /**
@@ -19,8 +20,32 @@ public class BuiltinBoolOperations {
         return BoolToken.of(term1.booleanValue() && term2.booleanValue());
     }
 
+    public static Term andThen(Term term1, Term term2, TermContext context) {
+        if (term1 instanceof BoolToken) {
+            BoolToken boolToken1 = (BoolToken) term1;
+            return boolToken1.booleanValue() ? term2 : BoolToken.FALSE;
+        } else if (term2 instanceof BoolToken) {
+            BoolToken boolToken2 = (BoolToken) term2;
+            return boolToken2.booleanValue() ? term1 : BoolToken.FALSE;
+        } else {
+            return null;
+        }
+    }
+
     public static BoolToken or(BoolToken term1, BoolToken term2, TermContext context) {
         return BoolToken.of(term1.booleanValue() || term2.booleanValue());
+    }
+
+    public static Term orElse(Term term1, Term term2, TermContext context) {
+        if (term1 instanceof BoolToken) {
+            BoolToken boolToken1 = (BoolToken) term1;
+            return boolToken1.booleanValue() ? BoolToken.TRUE : term2;
+        } else if (term2 instanceof BoolToken) {
+            BoolToken boolToken2 = (BoolToken) term2;
+            return boolToken2.booleanValue() ? BoolToken.TRUE : term1;
+        } else {
+            return null;
+        }
     }
 
     public static BoolToken xor(BoolToken term1, BoolToken term2, TermContext context) {
