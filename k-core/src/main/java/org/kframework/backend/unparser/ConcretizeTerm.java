5d00cad547178bad061fd147ae8c69843967fb53
Dwight Guth <dwightguth@runtimeverification.com>
2014-10-24 13:53:43 -0500

    fix copyright
    Former-commit-id: 1263f3275aec777b917b94d62c5fb2df220096bf [formerly 227bc0d9a0a85d7295ca7585c14c1f00fbecd94a]
    Former-commit-id: 18b528db1cef5af3cd00f3d299b3b0f54cba5dd9

--- k-core/src/main/java/org/kframework/backend/unparser/ConcretizeTerm.java
+++ k-core/src/main/java/org/kframework/backend/unparser/ConcretizeTerm.java
@@ -1,3 +1,4 @@
+// Copyright (c) 2013-2014 K Team. All Rights Reserved.
 package org.kframework.backend.unparser;
 
 import java.util.HashMap;
--- k-core/src/main/java/org/kframework/parser/TermLoader.java
+++ k-core/src/main/java/org/kframework/parser/TermLoader.java
@@ -1,3 +1,4 @@
+// Copyright (c) 2012-2014 K Team. All Rights Reserved.
 package org.kframework.parser;
 
 import java.util.List;
