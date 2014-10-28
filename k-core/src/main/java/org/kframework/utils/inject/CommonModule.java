0958bf4fa5c5da2f361f84595a4258dacaabb3e2
Dwight Guth <dwightguth@runtimeverification.com>
2014-10-28 14:50:40 -0500

    install FileSystemModule
    Former-commit-id: 203bc22ab3686545922298bc0ac2675faaf99412 [formerly 226446cfe3d9c946131aa7a7eaeabdafc0364c1e]
    Former-commit-id: 63e1f6cfa64c08a8aba98804c2a4de4af02ebcc8

--- k-core/src/main/java/org/kframework/utils/inject/CommonModule.java
+++ k-core/src/main/java/org/kframework/utils/inject/CommonModule.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.kframework.main.Tool;
 import org.kframework.utils.file.Environment;
+import org.kframework.utils.file.FileSystemModule;
 import org.kframework.utils.file.FileUtil;
 import org.kframework.utils.file.TempDir;
 import org.kframework.utils.file.WorkingDir;
@@ -17,7 +18,9 @@ import com.google.inject.Singleton;
 public class CommonModule extends AbstractModule {
 
     @Override
-    protected void configure() {}
+    protected void configure() {
+        install(new FileSystemModule(new File("."), System.getenv()));
+    }
 
     @Provides @TempDir @Singleton
     File tempDir(@WorkingDir File workingDir, Tool tool) {
