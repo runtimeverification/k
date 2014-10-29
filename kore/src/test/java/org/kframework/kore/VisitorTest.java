7029bcaf6eee83ed368606cf24e83fd9af8343e6
Cosmin Radoi <cosmin.radoi@gmail.com>
2014-10-29 17:32:21 -0500

    clean Java interface for KORE inner, and examples
    Former-commit-id: d71b2ff0c36c8a158040c135df6d9627afde58c9 [formerly 7ab373e1b5a0fa7e6c64bf45cbc53f6ccb80a87c]
    Former-commit-id: c31a1a7584d069597b68c15f5c51e2f85ddbf14f

---
+++ kore/src/test/java/org/kframework/kore/VisitorTest.java
@@ -0,0 +1,40 @@
+package org.kframework.kore;
+
+import org.junit.Test;
+import static org.kframework.kore.Interface.*;
+
+public class VisitorTest {
+	class FooTransformer extends AbstractKORETransformer<K> {
+
+		@Override
+		public K transform(KApply k) {
+			return k.copy();
+		}
+
+		@Override
+		public K transform(KRewrite k) {
+			return k.copy();
+		}
+
+		@Override
+		public K transform(KToken k) {
+			return KVariable("T");
+		}
+
+		@Override
+		public K transform(KVariable k) {
+			return k.copy();
+		}
+	}
+
+	@Test
+	public void testFoo() {
+		FooTransformer fooTransformer = new FooTransformer();
+
+		KToken someToken = KToken(Sort("foo"), KString("bla"));
+
+		K transformed = fooTransformer.transform(someToken);
+
+		System.out.println(transformed);
+	}
+}
