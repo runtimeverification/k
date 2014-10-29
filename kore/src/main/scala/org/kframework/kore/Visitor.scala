f26493cb72ae9e6f51296fcc84580bdfe1e410ae
Cosmin Radoi <cosmin.radoi@gmail.com>
2014-10-29 17:36:32 -0500

    and the kore transformers
    Former-commit-id: 92c232e74be703a7ae3b486604ae86be734233ab [formerly 872e5e8cba73f234f2012ada62671debc0113b6b]
    Former-commit-id: a32f63e12b24cfa4a44971d02fa9ddf212d2cc58

---
+++ kore/src/main/scala/org/kframework/kore/Visitor.scala
@@ -0,0 +1,30 @@
+package org.kframework.kore
+
+import scala.collection.immutable.Nil
+
+trait KORETransformer[T] {
+  def transform(k: K): T = (k: @annotation.switch) match {
+    case k: KORE => k match {
+      case k: KApply => transform(k)
+      case k: KRewrite => transform(k)
+      case k: KToken => transform(k)
+      case k: KVariable => transform(k)
+    }
+  }
+
+  def transform(k: KApply): T
+  def transform(k: KRewrite): T
+  def transform(k: KToken): T
+  def transform(k: KVariable): T
+}
+
+trait KOREVisitor extends KORETransformer[Nothing] {
+  def visit(k: K) {
+    transform(null: K)
+  }
+}
+
+/* Java interfaces */
+
+abstract class AbstractKORETransformer[T] extends KORETransformer[T]
+abstract class AbstractKOREVisitor extends KOREVisitor
