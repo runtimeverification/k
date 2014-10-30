8ae088ee88a05f87e800430e5db1087dc00deb76
Cosmin Radoi <cosmin.radoi@gmail.com>
2014-10-30 15:44:10 -0500

    style fixes
    Former-commit-id: a2a4394637dcc311939e1387a0d01a34971610fc [formerly 4d1d9f839c6211fa68bd2ff7def7bbe78b288687]
    Former-commit-id: c5041ff6b952d017d90061f04f69fd33cc292afe

--- kore/src/test/java/org/kframework/kore/VisitorTest.java
+++ kore/src/test/java/org/kframework/kore/VisitorTest.java
@@ -6,37 +6,37 @@ import org.junit.Test;
 import static org.kframework.kore.Interface.*;
 
 public class VisitorTest {
-	class FooTransformer extends AbstractKORETransformer<K> {
+  class FooTransformer extends AbstractKORETransformer<K> {
 
-		@Override
-		public K transform(KApply k) {
-			return k.copy();
-		}
+    @Override
+    public K transform(KApply k) {
+      return k.copy();
+    }
 
-		@Override
-		public K transform(KRewrite k) {
-			return k.copy();
-		}
+    @Override
+    public K transform(KRewrite k) {
+      return k.copy();
+    }
 
-		@Override
-		public K transform(KToken k) {
-			return KVariable("T");
-		}
+    @Override
+    public K transform(KToken k) {
+      return KVariable("T");
+    }
 
-		@Override
-		public K transform(KVariable k) {
-			return k.copy();
-		}
-	}
+    @Override
+    public K transform(KVariable k) {
+      return k.copy();
+    }
+  }
 
-	@Test
-	public void testFoo() {
-		FooTransformer fooTransformer = new FooTransformer();
+  @Test
+  public void testFoo() {
+    FooTransformer fooTransformer = new FooTransformer();
 
-		KToken someToken = KToken(Sort("foo"), KString("bla"));
+    KToken someToken = KToken(Sort("foo"), KString("bla"));
 
-		K transformed = fooTransformer.transform(someToken);
+    K transformed = fooTransformer.transform(someToken);
 
-		System.out.println(transformed);
-	}
+    System.out.println(transformed);
+  }
 }
