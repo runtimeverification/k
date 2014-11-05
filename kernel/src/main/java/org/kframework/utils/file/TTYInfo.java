ddad619a6f058576cabcd258fa6d510a980d975b
Dwight Guth <dwightguth@runtimeverification.com>
2014-11-05 17:07:59 -0600

    fix javadoc
    Former-commit-id: dd6f86626f66c59c67ff2c8876a19cca254fd794 [formerly 4854a5d4557605cd0abbd051e3dbd6b29bf1cf10]
    Former-commit-id: 10a389476b9fb13a033490d09db0c267446cb45b

--- kernel/src/main/java/org/kframework/utils/file/TTYInfo.java
+++ kernel/src/main/java/org/kframework/utils/file/TTYInfo.java
@@ -1,6 +1,14 @@
 // Copyright (c) 2014 K Team. All Rights Reserved.
 package org.kframework.utils.file;
 
+/**
+ * Encapsulates information about whether stdin, stdout, and stderr are
+ * connected to terminals. Essentially, each boolean in this class is
+ * true if and only if the corresponding stream is one for which
+ * ultimately the isatty function returns true.
+ * @author dwightguth
+ *
+ */
 public class TTYInfo {
 
     public final boolean stdin, stdout, stderr;
