1f589e657c3b519412a3be65c77cdd5e5721cb42
Dwight Guth <dwightguth@runtimeverification.com>
2014-11-07 17:52:12 -0600

    fix yilong's comment
    Former-commit-id: 192aeac257ce00d938de980539af70d85bbe4e83 [formerly 7c400b043b934d009a0c9d1dd031e565da627c93]
    Former-commit-id: b94f569b84b37fd98ffc98eb0a4f6c2fd78702eb

--- kernel/src/main/java/org/kframework/kompile/KompileOptions.java
+++ kernel/src/main/java/org/kframework/kompile/KompileOptions.java
@@ -52,7 +52,7 @@ public final class KompileOptions implements Serializable {
     @Parameter(names={"--directory", "-d"}, description="Path to the directory in which the output resides. An output can be either a kompiled K definition or a document which depends on the type of backend. The default is the current directory.")
     public String directory = ".";
 
-    @Parameter(names="--backend", description="Choose a backend. <backend> is one of [pdf|coq|java]. Each creates the kompiled K definition.")
+    @Parameter(names="--backend", description="Choose a backend. <backend> is one of [maude|coq|java]. Each creates the kompiled K definition.")
     public String backend = Backends.MAUDE;
 
     @Parameter(names="--doc-style", description="Specify a style option for the package 'k.sty' (when '--backend [pdf|latex]' is used) or path to an alternative .css file (when '--backend html' is used).")
