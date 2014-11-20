3406f7b60a8c6222f44cecf9e95efb6d38b9110f
Dwight Guth <dwightguth@runtimeverification.com>
2014-11-20 10:40:17 -0600

    add isNormal function + use it to exit quickly from unification
    Former-commit-id: f0e8ffa844635066d00bcb710ea2d10c2abe4e64 [formerly 9d09926b1d97726e75e702b8f18260ac322e3050]
    Former-commit-id: 6c99540edaa22596657503a9609d889eb3466fa6

---
+++ java-backend/src/main/java/org/kframework/backend/java/symbolic/IncrementalCollector.java
@@ -0,0 +1,69 @@
+// Copyright (c) 2013-2014 K Team. All Rights Reserved.
+package org.kframework.backend.java.symbolic;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+
+import org.kframework.backend.java.kil.JavaSymbolicObject;
+
+
+/**
+ * A visitor which computes the set of variables in a term.
+ *
+ * @author AndreiS
+ */
+public class IncrementalCollector<T> extends PrePostVisitor {
+
+    private Set<T> set = new HashSet<>();
+    private final Stack<Set<T>> setStack = new Stack<>();
+    private final BiConsumer<Set<T>, JavaSymbolicObject> setValue;
+    private final Function<JavaSymbolicObject, Set<T>> getValue;
+
+    public IncrementalCollector(
+            BiConsumer<Set<T>, JavaSymbolicObject> setValue,
+            Function<JavaSymbolicObject, Set<T>> getValue,
+            LocalVisitor computeLocal) {
+        getPreVisitor().addVisitor(new PreVariableVisitor());
+        getPostVisitor().addVisitor(computeLocal);
+        getPostVisitor().addVisitor(new PostVariableVisitor());
+        this.setValue = setValue;
+        this.getValue = getValue;
+    }
+
+    private class PreVariableVisitor extends LocalVisitor {
+        @Override
+        public void visit(JavaSymbolicObject term) {
+            Set<T> termSet = getValue.apply(term);
+            if (termSet != null) {
+                proceed = false;
+                set.addAll(termSet);
+            } else {
+                setStack.push(set);
+                set = new HashSet<>();
+                setValue.accept(set, term);
+            }
+        }
+    }
+
+    private class PostVariableVisitor extends LocalVisitor {
+        @Override
+        public void visit(JavaSymbolicObject term) {
+            set = setStack.pop();
+            set.addAll(getValue.apply(term));
+        }
+    }
+
+    /**
+     * Gets the computed set of values in the term which accepts this
+     * visitor.
+     *
+     * @return the set of result values in that term
+     */
+    public Set<T> getResultSet() {
+        return set;
+    }
+
+}
