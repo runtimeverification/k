10dfd822b75bd75bd7e5a1ec93b691ee4719768b
Dwight Guth <dwightguth@runtimeverification.com>
2014-12-03 11:24:38 -0600

    fix binder
    Former-commit-id: eb3c33f72b7c82ae3519b600414d33505682f744 [formerly 612e460a9186d418713344d63acc22bf1d3e1297]
    Former-commit-id: f8c4cf8994864c0bfb28b4e2b4385ce97e7acec4

--- kernel/src/main/java/org/kframework/compile/transformers/RemovePreincludedRules.java
+++ kernel/src/main/java/org/kframework/compile/transformers/RemovePreincludedRules.java
@@ -2,6 +2,7 @@
 package org.kframework.compile.transformers;
 
 import org.kframework.kil.ASTNode;
+import org.kframework.kil.GeneratedSource;
 import org.kframework.kil.Rule;
 import org.kframework.kil.loader.Context;
 import org.kframework.kil.visitors.CopyOnWriteTransformer;
@@ -18,6 +19,9 @@ public class RemovePreincludedRules extends CopyOnWriteTransformer {
 
     @Override
     public ASTNode visit(Rule node, Void _void)  {
+        if (node.getSource() instanceof GeneratedSource && ((GeneratedSource)node.getSource()).getSourceClass() == ResolveBinder.class) {
+            return null;
+        }
         return node;
     }
 }
