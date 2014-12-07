a35eeb41cc38d69e993ff35123ac70f7d6413bcd
Cosmin Radoi <cosmin.radoi@gmail.com>
2014-12-07 13:31:25 -0600

    true, working, multiple dispatch using reflection
    Former-commit-id: ed1c9b1ac0dfcbe437676b5d30ea32bbc0b86b45 [formerly 10637f2d357b313570aef6dfd2a43a6d75a8c740]
    Former-commit-id: 3832532b207275e53b8c329a56e1d2ac8ec84777

--- kore/src/main/scala/org/kframework/tiny/Reflection.scala
+++ kore/src/main/scala/org/kframework/tiny/Reflection.scala
@@ -1,6 +1,7 @@
 package org.kframework.tiny
 
 import scala.reflect.ManifestFactory
+import collection.JavaConverters._
 
 object Reflection {
   import scala.reflect.runtime.universe._
@@ -15,14 +16,21 @@ object Reflection {
     moduleMirror.instance
   }
 
-  def isSubclassOf(a: Class[_], b: Class[_]) = {
-    !b.isAssignableFrom(a)
+  val unbox = Map[Class[_], Class[_]](
+    classOf[Integer] -> Integer.TYPE).withDefault { x => x }
+
+  def <(a: Class[_], b: Class[_]) = {
+    unbox(b).isAssignableFrom(unbox(a))
+  }
+
+  def deconstruct(o: Any): TraversableOnce[Any] = o match {
+    case o: Iterable[_] => o.iterator
+    case o: Product => o.productIterator
+    case o: java.lang.Iterable[_] => o.asScala.iterator
+    case o: Iterator[_] => o
   }
 
   def construct(className: String, args: Seq[Any]) = {
-    println(Class.forName(className).getConstructors map { _.getParameterTypes.mkString(",") } mkString "\n")
-    println(args map { _.getClass })
-
     val workingConstructors = Class.forName(className).getConstructors filter {
       case c =>
         c.getParameterCount == args.size
@@ -49,40 +57,68 @@ object Reflection {
     val typeSignature = instanceMirror.symbol.typeSignature
     val termSymbol = typeSignature member methodTermName
 
+    def valueFor(name: String, p: Symbol, i: Int, argsSoFar: List[Any]): Option[Any] = {
+      val defarg = typeSignature member newTermName(s"$name$$default$$${i + 1}")
+      if (defarg != NoSymbol) {
+        Some((instanceMirror reflectMethod defarg.asMethod)(argsSoFar: _*))
+      } else
+        None
+      //        throw new IllegalArgumentException(name + " " + p + " " + i + defarg.toString)
+    }
+
     val methodSymbolAlternatives = termSymbol.asTerm.alternatives
 
-    val methodSymbol = (if (methodSymbolAlternatives.size == 1)
-      methodSymbolAlternatives.head
-    else {
-      methodSymbolAlternatives.head // TODO: fix this for methods with true overloading
-    }).asMethod
+    val symbolsWithArgs = methodSymbolAlternatives
+      .map { _.asMethod }
+      .filter { _.paramLists.size >= givenArgsLists.size }
+      .filter {
+        !_.paramLists.zipAll(givenArgsLists, null, Seq()).exists({
+          case (params, givenArgs) => params.size < givenArgs.size
+        })
+      }
+      .map { methodSymbol =>
+        val paramsWithGivenArgsLists = methodSymbol.paramLists
+          .zipAll(givenArgsLists, null, Seq())
+          .map { case (params, args) => params.zipAll(args map { Option(_) }, null, None) }
 
-    def valueFor(name: String, p: Symbol, i: Int, argsSoFar: List[Any]): Any = {
-      val defarg = typeSignature member newTermName(s"$name$$default$$${i + 1}")
-      if (defarg != NoSymbol)
-        (instanceMirror reflectMethod defarg.asMethod)(argsSoFar: _*)
-      else
-        throw new IllegalArgumentException(name + " " + p + " " + i + defarg.toString)
-    }
-    val paramsWithGivenArgsLists = methodSymbol.paramLists
-      .zipAll(givenArgsLists, null, Seq())
-      .map { case (params, args) => params.zipAll(args map { Option(_) }, null, None) }
-
-    val paramsWithGivenArgsAndIndex = paramsWithGivenArgsLists.foldLeft((0, List[List[((Symbol, Option[Any]), Int)]]())) {
-      case ((index, newPList), pList) => (index + pList.size, newPList :+
-        (pList.zipWithIndex map {
-          case (v, i) => (v, i + index)
-        }))
-    }
-
-    val args = paramsWithGivenArgsAndIndex._2.foldLeft(List[Any]()) {
-      case (argsSoFar, args) =>
-        val argsWithDefaults = args map {
-          case ((_, Some(v)), _) => v
-          case x @ ((sym, None), index) => valueFor(methodName, sym, index, argsSoFar)
+        val paramsWithGivenArgsAndIndex = paramsWithGivenArgsLists.foldLeft((0, List[List[((Symbol, Option[Any]), Int)]]())) {
+          case ((index, newPList), pList) => (index + pList.size, newPList :+
+            (pList.zipWithIndex map {
+              case (v, i) => (v, i + index)
+            }))
         }
-        argsSoFar ++ argsWithDefaults
-    }
+
+        val args = paramsWithGivenArgsAndIndex._2.foldLeft(Option(List[Any]())) {
+          case (Some(argsSoFar), args) =>
+            val argsWithDefaults = args
+              .map {
+                case ((sym, Some(v)), _) => Some(v)
+                case x @ ((sym, None), index) => valueFor(methodName, sym, index, argsSoFar)
+              }
+            if (argsWithDefaults.contains(None))
+              None
+            else
+              Some(argsSoFar ++ (argsWithDefaults map { _.get }))
+
+          case (None, args) => None
+        }
+        (methodSymbol, args)
+      }
+      .collect {
+        case (sym, Some(args)) => (sym, args)
+      }
+      .filter {
+        case (sym, args) =>
+          val paramTypes = sym.asMethod.paramLists.flatten
+            .map { _.typeSignature.erasure.typeSymbol.asClass }
+            .map { m.runtimeClass(_) }
+          val argsTypes = args map { _.getClass }
+          !(paramTypes.zip(argsTypes) exists {
+            case (paramType, argType) => ! <(argType, paramType)
+          })
+      }
+
+    val (methodSymbol, args) = symbolsWithArgs.head
 
     val methodMirror = instanceMirror.reflectMethod(methodSymbol)
 
