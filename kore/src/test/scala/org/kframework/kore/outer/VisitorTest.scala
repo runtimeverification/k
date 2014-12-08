531ed250b80e7c69c5cc9dd5e4b04f141cef629e
Cosmin Radoi <cosmin.radoi@gmail.com>
2014-12-07 19:10:07 -0600

    fix
    Former-commit-id: 42caa073c48cfefe463208cd37dfb4c9a31fdf16 [formerly 13c5434de1fff1bc82671577f6e5ec3e1eb97343]
    Former-commit-id: 71f49cc46075bce098e855d5e0995e5767cf7c07

--- kore/src/test/scala/org/kframework/kore/outer/VisitorTest.scala
+++ kore/src/test/scala/org/kframework/kore/outer/VisitorTest.scala
@@ -7,23 +7,6 @@ import org.kframework.tiny.Reflection
 import org.junit.Assert
 import java.lang.reflect.Method
 
-abstract class AbstractVisitor {
-  import scala.reflect.runtime.universe.MethodMirror
-  val cache = collection.mutable.Map[Class[_], MethodMirror]()
-  //  val cache = collection.mutable.Map[Class[_], Method]()
-
-  def apply(o: Any) {
-    val methodMirror = cache.getOrElseUpdate(o.getClass, {
-      val types = Reflection.typesForArgs(Seq(Seq(o)))
-      val (methodSymbol, _) = Reflection.findMethod(this, "visit", types)
-      Reflection.mirrorForMethod(this, methodSymbol)
-    })
-    methodMirror(o)
-    Reflection.deconstruct(o) foreach apply
-  }
-  def visit(x: AnyRef) {}
-}
-
 trait Foo {
   def accept(x: DoubleDispatchVisitor)
 }
