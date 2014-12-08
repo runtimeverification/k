5263662e577b4caa22a3e4f92d1d2c323976f9a0
Cosmin Radoi <cosmin.radoi@gmail.com>
2014-12-08 11:57:25 -0600

    sketch of pattern-matching outer visitor
    Former-commit-id: f8659c09c4cfd51fc407299c7922580fbbc6e96a [formerly 3a02badc87915ae0e433b75b33d3dd29f891f310]
    Former-commit-id: db9f03624abfca7eb3997e7399dcc00e2e0e00af

---
+++ kore/src/main/scala/org/kframework/kore/outer/Visitor.scala
@@ -0,0 +1,35 @@
+// Copyright (c) 2014 K Team. All Rights Reserved.
+
+package org.kframework.kore.outer
+
+import org.kframework.kore.K
+
+trait Transformer extends Function1[Definition, Definition] {
+  import org.kframework.kore.outer;
+
+  def apply(d: Definition): Definition = outer.Definition(d.requires map apply, d.modules map apply)
+  def apply(r: Require): Require = r
+  def apply(m: Module): Module = outer.Module(m.name, m.sentences map apply, m.att)
+  def apply(s: Sentence): Sentence = s match {
+    case i: Import => apply(i)
+    case c: Context => apply(c)
+    case r: Rule => apply(r)
+    case mc: ModuleComment => apply(mc)
+    case sp: SyntaxPriority => apply(sp)
+    case sa: SyntaxAssociativity => apply(sa)
+    case ss: SyntaxSort => apply(ss)
+    case sp: SyntaxProduction => apply(sp)
+  }
+  def apply(c: Context): Context = outer.Context(apply(c.body), apply(c.requires), c.att)
+  def apply(i: Import): Import
+  def apply(r: Rule): Rule
+  def apply(c: ModuleComment): ModuleComment
+  def apply(s: SyntaxPriority): SyntaxPriority
+  def apply(s: SyntaxAssociativity): SyntaxAssociativity
+  def apply(s: SyntaxSort): SyntaxSort
+  def apply(s: SyntaxProduction): SyntaxProduction
+  def apply(n: NonTerminal): NonTerminal
+  def apply(r: Terminal): Terminal
+  def apply(r: RegexTerminal): RegexTerminal
+  def apply(k: K): K
+}
