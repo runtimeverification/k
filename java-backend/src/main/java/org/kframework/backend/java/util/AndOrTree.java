3ae1379ea9b1e6ecf423afd0eb6d0c2f79152e6e
Dwight Guth <dwightguth@runtimeverification.com>
2014-12-09 13:51:12 -0600

    draft of work on disjunctions
    Former-commit-id: e995bf1031fffcf942e01cfc6f6e9e18a308c8ce [formerly 4f39e2b82f4d83eefaca075e06a958e19f4089bf]
    Former-commit-id: 055d5bc0221f21f3d7a115129e9e745905ac81a5

---
+++ java-backend/src/main/java/org/kframework/backend/java/util/AndOrTree.java
@@ -0,0 +1,104 @@
+package org.kframework.backend.java.util;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.commons.collections4.ListUtils;
+
+import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+
+public class AndOrTree<T> {
+    public static enum NodeType {
+        AND,
+        OR,
+        LEAF
+    }
+
+    private final NodeType nodeType;
+    private final T leaf;
+    private final ImmutableList<AndOrTree<T>> children;
+
+    public ImmutableList<AndOrTree<T>> getChildren() {
+        return children;
+    }
+
+    public NodeType getNodeType() {
+        return nodeType;
+    }
+
+    public T getLeaf() {
+        return leaf;
+    }
+
+    public AndOrTree(T leaf) {
+        this.nodeType = NodeType.LEAF;
+        this.leaf = leaf;
+        this.children = null;
+    }
+
+    @SafeVarargs
+    public AndOrTree(boolean and, AndOrTree<T>... children) {
+        this.nodeType = and ? NodeType.AND : NodeType.OR;
+        this.leaf = null;
+        this.children = ImmutableList.copyOf(children);
+    }
+
+    public AndOrTree(boolean and, Iterable<AndOrTree<T>> children) {
+        this.nodeType = and ? NodeType.AND : NodeType.OR;
+        this.leaf = null;
+        this.children = ImmutableList.copyOf(children);
+    }
+
+    public List<List<T>> sumOfProducts() {
+        if (nodeType == NodeType.LEAF) {
+            return Collections.singletonList(Collections.singletonList(leaf));
+        } else if (nodeType == NodeType.OR) {
+            List<List<T>> result = Lists.newArrayList();
+            for (AndOrTree<T> child : children) {
+                //(ab + cd) + (ef + gh) = ab + cd + ef + gh
+                result.addAll(child.sumOfProducts());
+            }
+            return result;
+        } else if (nodeType == NodeType.AND) {
+            List<AndOrTree<T>> copyOfChildren = Lists.newArrayList(children);
+            AndOrTree<T> head = children.get(0);
+            copyOfChildren.remove(head);
+            if (copyOfChildren.size() == 1) {
+                return sumOfProducts(head, copyOfChildren.get(0));
+            }
+            AndOrTree<T> tail = new AndOrTree<>(true, copyOfChildren);
+            return sumOfProducts(head, tail);
+        } else {
+            throw new AssertionError("unexpected node type");
+        }
+    }
+
+    private List<List<T>> sumOfProducts(AndOrTree<T> head, AndOrTree<T> tail) {
+        List<List<T>> headSumOfProducts = head.sumOfProducts();
+        List<List<T>> tailSumOfProducts = tail.sumOfProducts();
+        //(ab + cd)(ef + gh) = abef + abgh + cdef + cdgh
+        List<List<T>> result = Lists.newArrayList();
+        for (List<T> product1 : headSumOfProducts) {
+            for (List<T> product2 : tailSumOfProducts) {
+                result.add(ListUtils.union(product1, product2));
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        switch (nodeType) {
+        case LEAF:
+            return leaf.toString();
+        case AND:
+            return "(" + Joiner.on(" /\\ ").join(children) + ")";
+        case OR:
+            return "(" + Joiner.on(" \\/ ").join(children) + ")";
+        default:
+            throw new AssertionError("unexpected node type");
+        }
+    }
+}
---
+++ java-backend/src/test/java/org/kframework/backend/java/util/AndOrTreeTest.java
@@ -0,0 +1,68 @@
+package org.kframework.backend.java.util;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.junit.Test;
+
+public class AndOrTreeTest {
+
+    @Test
+    public void testLeaf() {
+        AndOrTree<Integer> tree1 = new AndOrTree<>(0);
+        assertEquals(Collections.singletonList(Collections.singletonList(0)), tree1.sumOfProducts());
+    }
+
+    @Test
+    public void testAnd() {
+        AndOrTree<Integer> tree2 = new AndOrTree<>(true, new AndOrTree<>(0), new AndOrTree<>(1));
+        assertEquals(Collections.singletonList(Arrays.asList(0, 1)), tree2.sumOfProducts());
+        AndOrTree<Integer> tree3 = new AndOrTree<>(true, tree2, tree2);
+        assertEquals(Collections.singletonList(Arrays.asList(0, 1, 0, 1)), tree3.sumOfProducts());
+    }
+
+    @Test
+    public void testOr() {
+        AndOrTree<Integer> tree4 = new AndOrTree<>(false, new AndOrTree<>(0), new AndOrTree<>(1));
+        assertEquals(Arrays.asList(Collections.singletonList(0), Collections.singletonList(1)),
+                tree4.sumOfProducts());
+        AndOrTree<Integer> tree5 = new AndOrTree<>(false, new AndOrTree<>(2), new AndOrTree<>(3));
+        AndOrTree<Integer> tree6 = new AndOrTree<>(false, tree4, tree5);
+        assertEquals(Arrays.asList(Collections.singletonList(0), Collections.singletonList(1),
+                Collections.singletonList(2), Collections.singletonList(3)), tree6.sumOfProducts());
+    }
+
+    @Test
+    public void testAndInsideOr() {
+        AndOrTree<Integer> tree2 = new AndOrTree<>(true, new AndOrTree<>(0), new AndOrTree<>(1));
+        AndOrTree<Integer> tree7 = new AndOrTree<>(true, new AndOrTree<>(2), new AndOrTree<>(3));
+        AndOrTree<Integer> tree8 = new AndOrTree<>(false, tree2, tree7);
+        assertEquals(Arrays.asList(Arrays.asList(0, 1), Arrays.asList(2, 3)), tree8.sumOfProducts());
+    }
+
+    @Test
+    public void testOrInsideAnd() {
+        AndOrTree<Integer> tree4 = new AndOrTree<>(false, new AndOrTree<>(0), new AndOrTree<>(1));
+        AndOrTree<Integer> tree8 = new AndOrTree<>(true, tree4, tree4);
+        assertEquals(Arrays.asList(Arrays.asList(0, 0), Arrays.asList(0, 1), Arrays.asList(1, 0),
+                Arrays.asList(1, 1)), tree8.sumOfProducts());
+    }
+
+    @Test
+    public void testComplex() {
+        AndOrTree<Integer> tree9 = new AndOrTree<>(true,
+                new AndOrTree<>(0),
+                new AndOrTree<>(true,
+                        new AndOrTree<>(1),
+                        new AndOrTree<>(2),
+                        new AndOrTree<>(false,
+                                new AndOrTree<>(true,
+                                        new AndOrTree<>(3),
+                                        new AndOrTree<>(4)),
+                                new AndOrTree<>(5))));
+        assertEquals(Arrays.asList(Arrays.asList(0, 1, 2, 3, 4), Arrays.asList(0, 1, 2, 5)), tree9.sumOfProducts());
+    }
+
+}
