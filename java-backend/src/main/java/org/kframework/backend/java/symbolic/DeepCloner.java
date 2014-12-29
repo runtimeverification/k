a0f12652778978514b89e1e6ddf2a5e9451db3ca
Dwight Guth <dwightguth@runtimeverification.com>
2014-12-29 17:21:02 -0600

    fix bug in cloning
    Former-commit-id: 1750bf13eb23e09c2a4d29d2df3b9a2cfea1240b [formerly 711784f6eac20c1563d02783c1310449fbf53dea]
    Former-commit-id: c415fb2e9722f306a6e05fe415bedf91e133a089

--- java-backend/src/main/java/org/kframework/backend/java/symbolic/DeepCloner.java
+++ java-backend/src/main/java/org/kframework/backend/java/symbolic/DeepCloner.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.kframework.backend.java.kil.Immutable;
 import org.kframework.backend.java.kil.Term;
+import org.kframework.backend.java.kil.TermContext;
 import org.kframework.backend.java.util.Profiler;
 
 import com.rits.cloning.Cloner;
@@ -21,6 +22,7 @@ public class DeepCloner {
 
     static {
         cloner.dontCloneInstanceOf(Immutable.class);
+        cloner.dontCloneInstanceOf(TermContext.class);
     }
 
     public static Term clone(Term term) {
@@ -39,7 +41,7 @@ public class DeepCloner {
 
         @Override
         protected Object fastClone(final Object o, final Map<Object, Object> clones) throws IllegalAccessException {
-            if (o instanceof Term && !((Term) o).isMutable()) {
+            if ((o instanceof Term && !((Term) o).isMutable())) {
                 return o;
             } else {
                 return super.fastClone(o, clones);
