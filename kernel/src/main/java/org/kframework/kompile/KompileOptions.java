402ef300354923d18a70734f29e5da67b3824cde
Dwight Guth <dwightguth@runtimeverification.com>
2015-01-08 16:07:56 -0600

    change default output to directory of source file instead of working directory
    Former-commit-id: 7d58483255a142189261345cfbdffbd9dfea1280 [formerly 6b177a584892ad983982bb616b0e20b7ba6f3c92]
    Former-commit-id: 68c40f57ed65c069ec6f3b9ec58855ae851083af

--- kernel/src/main/java/org/kframework/kompile/KompileOptions.java
+++ kernel/src/main/java/org/kframework/kompile/KompileOptions.java
@@ -50,7 +50,7 @@ public class KompileOptions implements Serializable {
      * Directory in which the compiled definition should be put.
      */
     @Parameter(names={"--directory", "-d"}, description="Path to the directory in which the output resides. An output can be either a kompiled K definition or a document which depends on the type of backend. The default is the current directory.")
-    public String directory = ".";
+    public String directory;
 
     @Parameter(names="--backend", description="Choose a backend. <backend> is one of [coq|java]. Each creates the kompiled K definition.")
     public String backend = Backends.JAVA;
