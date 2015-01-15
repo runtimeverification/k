cfa8e28c65328595a50b028f5ba9737df5c920f8
Cosmin Radoi <cosmin.radoi@gmail.com>
2015-01-15 17:12:18 -0600

    new parser works with imp, kast bubbles (to merge)
    Former-commit-id: 2e20742817f1e42710f45e5c83afc4394572ab8b [formerly 62edf5896a0b7126cfc1ab471d5cf41b9c6501fc]
    Former-commit-id: 5d73d954d825300d6c6074805bc8a8d10daab291

--- k-distribution/src/test/java/org/kframework/tiny/TinyTest.java
+++ k-distribution/src/test/java/org/kframework/tiny/TinyTest.java
@@ -3,41 +3,51 @@ package org.kframework.tiny;
 import java.io.File;
 import java.io.IOException;
 
-import org.apache.commons.io.FileUtils;
 import org.junit.Test;
-import org.kframework.kil.Definition;
-import org.kframework.kil.Sources;
-import org.kframework.kore.convertors.BubbleParsing;
+import org.kframework.kore.convertors.BaseTest.DefintionWithContext;
 import org.kframework.kore.convertors.KILtoKORE;
-import org.kframework.kore.outer.Module;
-import org.kframework.parser.outer.Outer;
+import org.kframework.kore.convertors.SDFCompilerTest;
 
-public class TinyTest {
-    @Test public void imp() {
-        String mainModule = "IMP";
-        Definition d = parseUsingOuter(
-                new File("src/test/resources/tiny-tests/imp.k"),
-                mainModule);
-        KILtoKORE kilToKore = new KILtoKORE(null);
-        org.kframework.kore.outer.Definition koreDef = kilToKore.apply(d);
+public class TinyTest extends SDFCompilerTest {
+    private static final String impMainModule = "IMP";
 
-        BubbleParsing bubbleParsing = new BubbleParsing();
-        Module koreModule = bubbleParsing.parseBubbles(koreDef.getModule(mainModule).get());
+    private static final File impDefinitionFile = new File("tutorial/1_k/2_imp/lesson_1/imp.k");
 
-        System.out.println(koreModule.toString());
+
+    @Test public void imp() throws IOException {
+        DefintionWithContext defWithContext = parse(impDefinitionFile.getAbsoluteFile(), impMainModule, true);
+        KILtoKORE kilToKore = new KILtoKORE(defWithContext.context);
+        org.kframework.kore.outer.Definition koreDef = kilToKore.apply(defWithContext.definition);
+
+        System.out.println(koreDef);
     }
 
-    private Definition parseUsingOuter(File file, String mainModule) {
-        Definition def = new Definition();
-        String definitionText;
-        try {
-            definitionText = FileUtils.readFileToString(file);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        def.setItems(Outer.parse(Sources.generatedBy(TinyTest.class), definitionText, null));
-        def.setMainModule(mainModule);
-        def.setMainSyntaxModule(mainModule);
-        return def;
-    }
+//    private static final File impSimpleDefinitionFile = new File("src/test/resources/tiny-tests/imp.k");
+//    @Test public void impOnNewParser() {
+//        String mainModule = impMainModule;
+//        Definition d = parseUsingOuter(
+//                impSimpleDefinitionFile,
+//                mainModule);
+//        KILtoKORE kilToKore = new KILtoKORE(null);
+//        org.kframework.kore.outer.Definition koreDef = kilToKore.apply(d);
+//
+//        BubbleParsing bubbleParsing = new BubbleParsing();
+//        Module koreModule = bubbleParsing.parseBubbles(koreDef.getModule(mainModule).get());
+//
+//        System.out.println(koreModule.toString());
+//    }
+//
+//    private Definition parseUsingOuter(File file, String mainModule) {
+//        Definition def = new Definition();
+//        String definitionText;
+//        try {
+//            definitionText = FileUtils.readFileToString(file);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//        def.setItems(Outer.parse(Sources.generatedBy(TinyTest.class), definitionText, null));
+//        def.setMainModule(mainModule);
+//        def.setMainSyntaxModule(mainModule);
+//        return def;
+//    }
 }
