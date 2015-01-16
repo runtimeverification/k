777958d20566bd69e406ad76f9b547398eacced8
Manasvi <msaxena2@illinois.edu>
2015-01-16 21:31:52 +0530

    modified gneneric Transition
    Former-commit-id: 93780f26c2ba06a5cdc684da482abc1154e7abd8 [formerly 7d40accc5b82626b50544c493abc2b70cf87133b]
    Former-commit-id: ddcbe729aef01516c1e3b1b2ad8e0be483ee12c1

--- kernel/src/main/java/org/kframework/krun/GenericTransition.java
+++ kernel/src/main/java/org/kframework/krun/GenericTransition.java
@@ -1,7 +1,71 @@
 package org.kframework.krun;
 
+import org.kframework.kil.ASTNode;
+import org.kframework.kil.Term;
+import org.kframework.kil.Variable;
+import org.kframework.krun.api.Transition;
+import sun.net.www.content.text.Generic;
+
+import java.util.Map;
+
 /**
- * Created by manasvi on 1/16/15.
+ * Generic Transition for Rules that have already been converted to generic Kil
  */
-public class GenericTransition {
+public class GenericTransition extends Transition {
+
+    public GenericTransition(TransitionType type, String label, ASTNode rule,
+                             Map<Variable, Term> substitution,
+                             String readString) {
+        super(type, label, rule, substitution, readString);
+    }
+    @Override
+    public ASTNode getRule() {
+        if (rule.isPresent()) {
+            return rule.get();
+        }
+        return null;
+    }
+
+    @Override
+    public int hashCode() {
+        return rule.get().hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof GenericTransition)) {
+            return false;
+        }
+        GenericTransition obj1 = (GenericTransition) obj;
+        return rule.equals(obj1.getRule());
+    }
+
+    @Override
+    public Map<Variable, Term> getSubstitution() {
+        return substitution.get();
+    }
+
+    public static GenericTransition rule(ASTNode rule) {
+        return new GenericTransition(TransitionType.RULE, null, rule, null, null);
+    }
+
+    public static GenericTransition label(String label) {
+        return new GenericTransition(TransitionType.LABEL, label, null, null, null);
+    }
+
+    public static GenericTransition unlabelled() {
+        return new GenericTransition(TransitionType.UNLABELLED, null, null, null, null);
+    }
+
+    public static GenericTransition deadlock() {
+        return new GenericTransition(TransitionType.DEADLOCK, null, null, null, null);
+    }
+
+    public static GenericTransition reduce() {
+        return new GenericTransition(TransitionType.REDUCE, null, null, null, null);
+    }
+
+    public static GenericTransition stdin(String readString) {
+        return new GenericTransition(TransitionType.STDIN, null, null, null, readString);
+    }
 }
