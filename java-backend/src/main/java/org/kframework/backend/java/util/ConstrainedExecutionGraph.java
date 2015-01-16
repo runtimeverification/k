2de21b16ff48ac1c99a9f5c3df094ecd6490daf4
Manasvi <msaxena2@illinois.edu>
2015-01-16 19:16:45 +0530

    changes to the execution graph class, updated with the constrained Transition:
    Former-commit-id: b61322df2023f8c7501d572eb6c7a9268343a1f1 [formerly a62a42a75e906b9ddd2b94f5130cf7a93a2d7ae2]
    Former-commit-id: 764fd0fc40ccd4e6b94a1242172334eba1c943b7

--- java-backend/src/main/java/org/kframework/backend/java/util/ConstrainedExecutionGraph.java
+++ java-backend/src/main/java/org/kframework/backend/java/util/ConstrainedExecutionGraph.java
@@ -2,17 +2,12 @@
 package org.kframework.backend.java.util;
 
 import edu.uci.ics.jung.graph.DirectedOrderedSparseMultigraph;
-import org.apache.commons.lang3.tuple.Pair;
 import org.kframework.backend.java.kil.ConstrainedTerm;
-import org.kframework.backend.java.kil.Rule;
-import org.kframework.backend.java.kil.Term;
-import org.kframework.backend.java.kil.Variable;
 
-import java.util.Map;
 
 /**
  * Backend specific execution graph.
  */
 public class ConstrainedExecutionGraph extends DirectedOrderedSparseMultigraph<ConstrainedTerm,
-        Pair<Rule, Map<Variable, Term>>> {
+        ConstrainedTransition> {
 }
