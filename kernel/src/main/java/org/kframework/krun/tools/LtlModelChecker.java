dd3cc5ed8488395c1d18c3943c203bb540e614a9
Traian Florin Serbanuta <traian.serbanuta@gmail.com>
2015-01-30 13:10:51 +0000

    LTLMC API changes. Printing ProofResults.
    Former-commit-id: 63cf6691a59d8be86dd1a8d1c26057141689a1ee [formerly e778c49800e20d2a7b632898d5f1459b3acc85c1]
    Former-commit-id: 343be6b4e385c2aed0263f4101edab9b0c732630

--- kernel/src/main/java/org/kframework/krun/tools/LtlModelChecker.java
+++ kernel/src/main/java/org/kframework/krun/tools/LtlModelChecker.java
@@ -35,7 +35,7 @@ public interface LtlModelChecker {
     @return An object containing both metadata about krun's execution, and a graph containing
     the LTL counterexample if model-checking failed (null if it succeeded)
     */
-    public abstract KRunProofResult<KRunGraph> modelCheck(Term formula, Term cfg) throws KRunExecutionException;
+    public abstract KRunProofResult<KRunGraph> modelCheck(String formula, Term cfg) throws KRunExecutionException;
 
     public static class Tool implements Transformation<Void, KRunResult> {
 
@@ -44,7 +44,6 @@ public interface LtlModelChecker {
         private final Context context;
         private final Stopwatch sw;
         private final LtlModelChecker modelChecker;
-        private final ProgramLoader loader;
         private final FileUtil files;
 
         @Inject
@@ -54,14 +53,12 @@ public interface LtlModelChecker {
                 @Main Context context,
                 Stopwatch sw,
                 @Main LtlModelChecker modelChecker,
-                ProgramLoader loader,
                 @Main FileUtil files) {
             this.options = options;
             this.initialConfiguration = initialConfiguration;
             this.context = context;
             this.sw = sw;
             this.modelChecker = modelChecker;
-            this.loader = loader;
             this.files = files;
         }
 
@@ -69,10 +66,8 @@ public interface LtlModelChecker {
         public KRunProofResult<KRunGraph> run(Void v, Attributes a) {
             a.add(Context.class, context);
             try {
-                Term formula = (Term) loader.loadPgmAst(ltlmc(), Sources.fromCommandLine("parameters"),
-                        Sort.of("LtlFormula"), context);
                 KRunProofResult<KRunGraph> result = modelChecker.modelCheck(
-                                formula,
+                                ltlmc(),
                                 initialConfiguration.get());
                 sw.printIntermediate("Model checking total");
                 return result;
