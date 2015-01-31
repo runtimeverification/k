cbb3c6d346754f249c6e0ce9bb4b1b11d428cd80
Cosmin Radoi <cosmin.radoi@gmail.com>
2015-01-30 22:22:24 -0600

    refactor
    Former-commit-id: 014b67878f12a28d6681d59b2268f59032d20c6a [formerly 4e1c7b21ee24b9c17c10fc9395f0f76c4c9fd606]
    Former-commit-id: 66a72dba476957110176781aa4d5e1c578e4d974

--- kore/src/main/scala/org/kframework/tiny/adt.scala
+++ kore/src/main/scala/org/kframework/tiny/adt.scala
@@ -25,11 +25,11 @@ trait KCollection extends kore.KCollection with Collection[K] with K {
   type This <: KCollection
   override def size = super[Collection].size
   def copy(att: Att) = label.apply(iterable.toSeq, att)
-  override def iterable: Iterable[K] = children
+  def iterable: Iterable[K] = children
   def children: Iterable[K]
   def label: KCollectionLabel[This]
-  override def newBuilder(): mutable.Builder[K, This] = label.newBuilder(att)
-  override def items: java.util.List[kore.K] = iterable.toList.asInstanceOf[List[kore.K]].asJava
+  def newBuilder(): mutable.Builder[K, This] = label.newBuilder(att)
+  def items: java.util.List[kore.K] = iterable.toList.asInstanceOf[List[kore.K]].asJava
 }
 
 trait KApp extends kore.KApply with KCollection {
@@ -48,29 +48,29 @@ trait ProductOfKs extends KCollection with Product {
 case class KVar(name: String, att: Att = Att()) extends kore.KVariable
 
 case class KTok(sort: Sort, s: String, att: Att = Att()) extends kore.KToken with K with Label[KTok] {
-  override type This = KTok
-  override def label: Label[KTok] = this
-  override def name: String = s + ":" + sort
+  type This = KTok
+  def label: Label[KTok] = this
+  def name: String = s + ":" + sort
   def apply(l: Seq[K], att: Att): This = { assert(l.size == 0); KTok(sort, s, att) }
-  override def copy(att: Att): This = KTok(sort, s, att)
+  def copy(att: Att): This = KTok(sort, s, att)
 }
 
 class RegularKApp(val label: RegularKAppLabel, val children: Seq[K], val att: Att = Att()) extends KApp {
-  override type This = RegularKApp
+  type This = RegularKApp
 }
 
 class AssocKApp(val label: AssocKAppLabel, val children: Seq[K], val att: Att = Att()) extends KApp {
-  override type This = AssocKApp
+  type This = AssocKApp
 }
 
 class KSeq private(val children: Seq[K], val att: Att) extends kore.KSequence with K with KCollection {
-  override type This = KSeq
-  override def label: KSeq.type = KSeq
+  type This = KSeq
+  def label: KSeq.type = KSeq
 }
 
 case class KRewrite(val left: K, val right: K, val att: Att) extends kore.KRewrite with ProductOfKs {
-  override type This = KRewrite
-  override def label: KCollectionLabel[This] = KRewrite
+  type This = KRewrite
+  def label: KCollectionLabel[This] = KRewrite
 }
 
 case class InjectedLabel(klabel: Label[_ <: K], att: Att) extends kore.InjectedKLabel
@@ -92,10 +92,10 @@ trait KCollectionLabel[This <: K] extends Label[This] {
 }
 
 object KRewrite extends KCollectionLabel[KRewrite] {
-  override def newBuilder(att: Att): mutable.Builder[K, KRewrite] =
+  def newBuilder(att: Att): mutable.Builder[K, KRewrite] =
     ListBuffer() mapResult { case List(left, right) => new KRewrite(left, right, att) }
-  override def att: Att = Att()
-  override def name: String = "=>"
+  def att: Att = Att()
+  def name: String = "=>"
 }
 
 case class RegularKAppLabel(name: String, att: Att) extends KCollectionLabel[RegularKApp] {
@@ -109,7 +109,7 @@ case class AssocKAppLabel(name: String, att: Att) extends KCollectionLabel[Assoc
 }
 
 object KSeq extends KCollectionLabel[KSeq] {
-  override def att: Att = Att()
-  override def name: String = "~>"
+  def att: Att = Att()
+  def name: String = "~>"
   def newBuilder(att: Att): mutable.Builder[K, KSeq] = new AssocBuilder[K, List[K], KSeq](ListBuffer()).mapResult { new KSeq(_, att) }
 }
