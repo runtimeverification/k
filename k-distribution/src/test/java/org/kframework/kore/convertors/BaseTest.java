8363e771c9bba408c31acc381c62e37d308ee78d
Cosmin Radoi <cosmin.radoi@gmail.com>
2015-03-02 14:08:30 -0600

    Update BaseTest.java
    Former-commit-id: fd4763f054365c7ec36b2bdf48b7b36310a1d22b [formerly 46cb0da8ee78308081404773cc8f202a86276715]
    Former-commit-id: b6452dfdac36fb6f734d09b4ad32cddc171c9753

--- k-distribution/src/test/java/org/kframework/kore/convertors/BaseTest.java
+++ k-distribution/src/test/java/org/kframework/kore/convertors/BaseTest.java
@@ -49,7 +49,7 @@ public abstract class BaseTest extends SDFCompilerTest {
 
     private File testResource(String baseName) {
         return new File(new File("k-distribution/src/test/resources" + baseName)
-                .getAbsoluteFile().toString().replaceAll("k-distribution.k-distribution", "k-distribution"));
+                .getAbsoluteFile().toString().replace("k-distribution" + File.separator + "k-distribution", "k-distribution"));
         // a bit of a hack to get around not having a clear working directory
         // Eclipse runs tests within k/k-distribution, IntelliJ within /k
     }
