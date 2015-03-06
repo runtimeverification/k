af4f570a090975a94e263088a81c1a5f7f9329df
Brandon Moore <brandon.3.moore@gmail.com>
2015-03-06 14:54:49 -0600

    configuration concretization (mostly Brandon)
    Former-commit-id: be86c63fd2aa8c2e02b0904bb13854144246118f [formerly d5a83a25e77e935ba95c2c2d3320dcaf572d1e5d]
    Former-commit-id: ebf7fc7eed3f0b4b10f31bf713842b7dcd4e5278

--- kernel/src/test/java/org/kframework/kore/compile/CompilerTest.java
+++ kernel/src/test/java/org/kframework/kore/compile/CompilerTest.java
@@ -1,28 +1,25 @@
 package org.kframework.kore.compile;
 
 
-import com.google.common.base.Function;
-import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.ListMultimap;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Multimap;
-import com.google.common.collect.Multimaps;
 import org.junit.Assert;
 import org.junit.Test;
 import org.kframework.kore.*;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.kframework.kore.KORE.*;
-import static org.kframework.Collections.*;
 
 public class CompilerTest {
 
@@ -30,24 +27,31 @@ public class CompilerTest {
 
         Map<KLabel, Integer> levels = new HashMap();
         Map<KLabel, KLabel> parents = new HashMap();
+        Multimap<KLabel, KLabel> children = HashMultimap.create();
+        public enum Multiplicity { ONE, OPTIONAL, STAR };
+        Map<KLabel, Multiplicity> multiplicities = new HashMap();
+
+        private void addCell(String parent, String child, int level, Multiplicity m) {
+            if (parent != null) {
+                parents.put(KLabel(child), KLabel(parent));
+                children.put(KLabel(parent), KLabel(child));
+            }
+            levels.put(KLabel(child), level);
+            multiplicities.put(KLabel(child), m);
+        }
 
         Configuration() {
-            levels.put(KLabel("<k>"), 3);
-            levels.put(KLabel("<env>"), 3);
-            levels.put(KLabel("<t>"), 2);
-            levels.put(KLabel("<ts>"), 1);
-            levels.put(KLabel("<state>"), 1);
-            levels.put(KLabel("<T>"), 0);
-
-            parents.put(KLabel("<k>"), KLabel("<t>"));
-            parents.put(KLabel("<env>"), KLabel("<t>"));
-            parents.put(KLabel("<t>"), KLabel("<ts>"));
-            parents.put(KLabel("<ts>"), KLabel("<T>"));
-            parents.put(KLabel("<state>"), KLabel("<T>"));
+            addCell(null, "<T>",0,Multiplicity.ONE);
+            addCell("<T>","<ts>",1,Multiplicity.ONE);
+            addCell("<T>","<state>",1,Multiplicity.ONE);
+            addCell("<ts>","<t>",2,Multiplicity.STAR);
+            addCell("<ts>","<scheduler>",2,Multiplicity.ONE);
+            addCell("<t>","<k>",3,Multiplicity.ONE);
+            addCell("<t>","<env>",3,Multiplicity.ONE);
         }
 
         Comparator<KLabel> comparator() {
-            return (k1, k2) -> levels.get(k2) - levels.get(k1);
+            return Comparator.comparing(levels::get);
         }
 
         int getLevel(KLabel k) {
@@ -57,53 +61,58 @@ public class CompilerTest {
         KLabel getParent(KLabel k) {
             return parents.get(k);
         }
+
+        Multiplicity getMultiplicity(KLabel k) {
+            return multiplicities.get(k);
+        }
+
+        boolean isLeafCell(KLabel k) {
+            return !parents.values().contains(k);
+        }
     }
 
     final Configuration cfg = new Configuration();
 
+    List<KApply> makeParents(KLabel parent, List<KApply> children) {
+        Map<KLabel, Long> counts =
+           children.stream().map(KApply::klabel).filter(l -> cfg.getMultiplicity(l) != Configuration.Multiplicity.STAR)
+                .collect(Collectors.groupingBy(k -> k, Collectors.counting()));
+        if (counts.values().stream().anyMatch(repetitions -> repetitions > 1)) {
+            if (counts.size() > 1) {
+                throw new IllegalArgumentException("Ambiguous completion");
+            } else {
+                return children.stream().map(k -> KApply(parent,k)).collect(Collectors.toList());
+            }
+        } else {
+            return Lists.newArrayList(KApply(parent, KList(children)));
+        }
+    }
+
     K concretize(K k) {
-        if (k instanceof KApply) {
+        if (!(k instanceof KApply)) {
+            return k;
+        } else {
             KApply app = (KApply)k;
             KLabel target = app.klabel();
-            TreeMap<Integer, List<K>>
-            ListMultimap<Integer, K> levels = Multimaps.newListMultimap(new TreeMap<Integer, >)
-            List<K> finishedChildren = new ArrayList<K>();
-            int maxLevel = 0;
-            for (K child : app.klist().items()) {
-                if (child instanceof KApply) {
-                    KLabel parent = cfg.getParent(((KApply) child).klabel());
-                    if (parent != null && !parent.equals(target)) {
-                        int level = cfg.getLevel(((KApply) child).klabel());
-                        if (level > maxLevel) { maxLevel = level; }
-                        levels.put(level, child);
-                    }
-                }
-                finishedChildren.add(child);
+            if (cfg.isLeafCell(target)) {
+                return k;
             }
-            while (maxLevel )
-            levels.keys().
-            Multimaps.index(k -> cfg.get)
-            List<K> nonCellChildren = app.klist().stream()
-                    .filter()
-            Stream<K> children = ((KApply) k).klist().stream();
-            KLabel klabel = ((KApply) k).klabel();
-            TreeSet<K> toProcess = new TreeSet<>();
-            Set<K> processed = new HashSet<>();
-
-            children.forEach(k -> {
-                if (k instanceof KApply)
-                    if (cfg.getParent(((KApply) k).klabel()).equals(klabel))
-                        else
-
-                    else
-
-            });
-
-            List<K> sortedChildren = children.sorted(Comparator.comparing(kk -> ((KApply) kk).klabel(), cfg.comparator())).collect(Collectors.toList());
-
-            sortedChildren.filter();
+            List<KApply> children = app.klist().stream().map(kk -> (KApply)kk).collect(Collectors.toList());
+            int targetLevel = cfg.getLevel(target)+1;
+            TreeMap<Integer, List<KApply>> levels =
+                    new TreeMap(children.stream().collect(Collectors.groupingBy(kk -> cfg.getLevel(kk.klabel()))));
+            while (levels.lastKey() > targetLevel) {
+                List<KApply> level = levels.remove(levels.lastKey());
+                for (Map.Entry<KLabel,List<KApply>> e : level.stream().collect(Collectors.groupingBy(kk -> cfg.getParent(kk.klabel()))).entrySet()) {
+                    KLabel parent = e.getKey();
+                    List<KApply> newCells = makeParents(parent, e.getValue());
+                    levels.compute(cfg.getLevel(parent),
+                            (kk, v) -> { if (v == null) { return newCells; } else {v.addAll(newCells); return v;}});
+                }
+            }
+            List<KApply> wrappedChildren = levels.remove(levels.lastKey());
+            return KApply(target, KList(wrappedChildren));
         }
-        return k;
     }
 
     @Test
@@ -124,6 +133,32 @@ public class CompilerTest {
     public void testTwoCellsCompletion() {
         K term = cell("<ts>", cell("<k>", intToToken(2)));
         K expected = cell("<ts>", cell("<t>", cell("<k>", intToToken(2))));
+        Assert.assertEquals(expected, concretize(term));
+    }
+
+    @Test public void testMultiplicitySeparate() {
+        K term = cell("<ts>", cell("<k>", intToToken(1)), cell("<k>", intToToken(2)));
+        K expected = cell("<ts>", cell("<t>", cell("<k>", intToToken(1))),
+                                  cell("<t>", cell("<k>", intToToken(2))));
+        Assert.assertEquals(expected, concretize(term));
+    }
+    @Test public void testMultiplicityShared() {
+        K term = cell("<ts>", cell("<k>", intToToken(1)), cell("<env>", intToToken(2)));
+        K expected = cell("<ts>", cell("<t>", cell("<k>", intToToken(1)), cell("<env>", intToToken(2))));
+        Assert.assertEquals(expected, concretize(term));
+    }
+    @Test(expected = IllegalArgumentException.class) public void testAmbiguityError() {
+        K term = cell("<ts>", cell("<k>", intToToken(1)), cell("<k>", intToToken(2)), cell("<env>", intToToken(2)));
+        concretize(term);
+    }
+    @Test public void testDeep2() {
+        Assert.assertEquals(Lists.newArrayList(cell("<ts>", cell("<t>", intToToken(1)), cell("<t>", intToToken(2)))),
+                makeParents(KLabel("<ts>"), Lists.newArrayList(cell("<t>", intToToken(1)), cell("<t>", intToToken(2)))));
+    }
+    @Test public void testDeep() {
+        K term = cell("<T>", cell("<k>", intToToken(1)), cell("<k>", intToToken(2)));
+        K expected = cell("<T>", cell("<ts>", cell("<t>", cell("<k>", intToToken(1))),
+                cell("<t>", cell("<k>", intToToken(2)))));
         Assert.assertEquals(expected, concretize(term));
     }
 
