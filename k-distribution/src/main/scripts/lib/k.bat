bc1684cb33975e23ccc7459875fe148b4569c309
Dwight Guth <dwightguth@runtimeverification.com>
2015-03-13 12:50:13 -0500

    try to fix batch file
    Former-commit-id: 74e9776f6c9e0d57e8c23f7c0a404c91b9411043 [formerly ec00b285c993dee3f79f049f658ca9ff5ce2dd7b]
    Former-commit-id: 5fea46216e7fcbaf9b82658ea5958971a8c34d04

--- k-distribution/src/main/scripts/lib/k.bat
+++ k-distribution/src/main/scripts/lib/k.bat
@@ -4,6 +4,6 @@ IF ERRORLEVEL 1 ECHO Unable to enable extensions
 call "%~dp0\setenv.bat"
 IF NOT DEFINED K_OPTS SET K_OPTS=-Xms64m -Xmx1024m -Xss32m -XX:+TieredCompilation
 ng.exe ng-version > NUL 2>&1
-IF ERRORLEVEL 0 ng.exe org.kframework.main.Main %*
-ELSE java  -Djava.awt.headless=true -ea %K_OPTS% -cp "%~dp0..\lib\java\*" org.kframework.main.Main %*
+IF ERRORLEVEL 0 ( ng.exe org.kframework.main.Main %* )
+ELSE ( java  -Djava.awt.headless=true -ea %K_OPTS% -cp "%~dp0..\lib\java\*" org.kframework.main.Main %* )
 ENDLOCAL
