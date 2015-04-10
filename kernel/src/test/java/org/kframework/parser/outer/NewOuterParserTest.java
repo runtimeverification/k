fc5439e8317ba7892bfebd81f1827fcfa2a7ab47
Radu Mereuta <headness13@gmail.com>
2015-04-10 14:18:26 -0500

    Removing duplicated modules from kore and e-kore.
    Former-commit-id: f7b209ed5bfbb8a4bfefce8675ab0c12505576ab [formerly 75c25a2f6b3e38c68cc689940146daf22fbabdf5]
    Former-commit-id: f758a790623e0785cc0a4fccd060d24a48ff1e6d

--- kernel/src/test/java/org/kframework/parser/outer/NewOuterParserTest.java
+++ kernel/src/test/java/org/kframework/parser/outer/NewOuterParserTest.java
@@ -5,6 +5,7 @@ package org.kframework.parser.outer;
 import org.junit.Test;
 import org.kframework.kore.K;
 import org.kframework.parser.concrete2kore.ParserUtils;
+import org.kframework.utils.file.JarInfo;
 
 import java.io.File;
 
@@ -12,14 +13,14 @@ import java.io.File;
  * Tests the K definition of the outer K syntax.
  */
 public class NewOuterParserTest {
+    public static final File BUILTIN_DIRECTORY = JarInfo.getKIncludeDir().resolve("builtin").toFile();
 
     @Test
     public void testKOREOuter() throws Exception {
         CharSequence theTextToParse = "module FOO syntax Exp ::= Exp [stag(as(d)f)] rule ab cd [rtag(.::KList)] endmodule";
         String mainModule = "KORE";
         String startSymbol = "KDefinition";
-        File definitionFile = new File(NewOuterParserTest.class.getResource
-                ("/e-kore.k").toURI()).getAbsoluteFile();
+        File definitionFile = new File(BUILTIN_DIRECTORY.toString() + "/e-kore.k");
 
         K kBody = ParserUtils.parseWithFile(theTextToParse, mainModule, startSymbol, definitionFile);
         //System.out.println(kBody);
