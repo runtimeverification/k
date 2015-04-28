c321893900b33e10ef5be689b0a3f535d231eec5
Dwight Guth <dwightguth@runtimeverification.com>
2015-04-28 16:16:16 -0500

    address Cosmin's comment
    Former-commit-id: 88c379996499160af77a2a575c971a94041abeff [formerly cb785a59402fdd07a33a8ce71768d10c2611d2f9]
    Former-commit-id: 9468a8fc82c8bd8cce3fc82491ee044788a88cc0

--- kernel/src/main/java/org/kframework/utils/errorsystem/KExceptionManager.java
+++ kernel/src/main/java/org/kframework/utils/errorsystem/KExceptionManager.java
@@ -22,7 +22,7 @@ import java.util.List;
 
 @RequestScoped
 public class KExceptionManager {
-    private final List<KException> exceptions = new ArrayList<KException>();
+    private final List<KException> exceptions = Collections.synchronizedList(new ArrayList<>());
 
     private final GlobalOptions options;
 
@@ -144,15 +144,11 @@ public class KExceptionManager {
     }
 
     public void addKException(KException kex) {
-        synchronized (exceptions) {
-            exceptions.add(kex);
-        }
+        exceptions.add(kex);
     }
 
     public void addAllKException(Collection<KException> kex) {
-        synchronized (exceptions) {
-            exceptions.addAll(kex);
-        }
+        exceptions.addAll(kex);
     }
 
     public void registerCompilerWarning(String message) {
@@ -225,31 +221,27 @@ public class KExceptionManager {
     private void registerInternal(KException exception) {
         if (!options.warnings.includesExceptionType(exception.type))
             return;
-        synchronized (exceptions) {
-            exceptions.add(exception);
-            if (exception.type == ExceptionType.ERROR || options.warnings2errors) {
-                throw new KEMException(exception);
-            }
+        exceptions.add(exception);
+        if (exception.type == ExceptionType.ERROR || options.warnings2errors) {
+            throw new KEMException(exception);
         }
     }
 
     public void print() {
-        synchronized (exceptions) {
-            Collections.sort(exceptions, new Comparator<KException>() {
-                @Override
-                public int compare(KException arg0, KException arg1) {
-                    return arg0.toString(options.verbose).compareTo(arg1.toString(options.verbose));
-                }
-            });
-            KException last = null;
-            for (KException e : exceptions) {
-                if (last != null && last.toString(options.verbose).equals(e.toString(options.verbose))) {
-                    continue;
-                }
-                printStackTrace(e);
-                System.err.println(StringUtil.splitLines(e.toString(options.verbose)));
-                last = e;
+        Collections.sort(exceptions, new Comparator<KException>() {
+            @Override
+            public int compare(KException arg0, KException arg1) {
+                return arg0.toString(options.verbose).compareTo(arg1.toString(options.verbose));
             }
+        });
+        KException last = null;
+        for (KException e : exceptions) {
+            if (last != null && last.toString(options.verbose).equals(e.toString(options.verbose))) {
+                continue;
+            }
+            printStackTrace(e);
+            System.err.println(StringUtil.splitLines(e.toString(options.verbose)));
+            last = e;
         }
     }
 
@@ -272,9 +264,7 @@ public class KExceptionManager {
         }
 
         public void register(KExceptionManager kem) {
-            synchronized (kem.getExceptions()) {
-                kem.getExceptions().add(exception);
-            }
+            kem.getExceptions().add(exception);
         }
     }
 
