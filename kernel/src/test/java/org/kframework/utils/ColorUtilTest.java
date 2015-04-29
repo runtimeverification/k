87de6c85aeb2facd5ef956edf2593ebb165dbc2f
Dwight Guth <dwightguth@runtimeverification.com>
2015-04-29 13:49:07 -0500

    fix one last issue
    Former-commit-id: 2632077b38d7d180296a17300f9ecdcdd731a1ed [formerly c7f0671c8be852920184903965ddc18f39aa25d1]
    Former-commit-id: c4ef195a005f404d247ae7430f8d8037a9940952

--- kernel/src/test/java/org/kframework/utils/ColorUtilTest.java
+++ kernel/src/test/java/org/kframework/utils/ColorUtilTest.java
@@ -10,6 +10,8 @@ public class ColorUtilTest {
 
     @Test
     public void testGetColor() {
+        // set java.awt.headless so that we don't activate the awt bug (see issue #982)
+        System.setProperty("java.awt.headless", "true");
         assertEquals("", ColorUtil.RgbToAnsi("red", ColorSetting.OFF, "black"));
         assertEquals("\u001b[31m", ColorUtil.RgbToAnsi("red", ColorSetting.ON, "black"));
         assertEquals("", ColorUtil.RgbToAnsi("red", ColorSetting.OFF, "red"));
