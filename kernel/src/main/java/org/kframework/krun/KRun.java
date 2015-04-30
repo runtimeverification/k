3ae0621cc06dfdabdb27f4c3e9f35c8f34576373
Cosmin Radoi <cosmin.radoi@gmail.com>
2015-04-30 14:24:57 -0500

    basic KORE-based execution in krun
    Former-commit-id: da030162bbe3352bf6ea88a4c23330d15649c495 [formerly 4a8074937337b1655b2db7b88788681257a04d33]
    Former-commit-id: d555ec8f4dc3ab67fbfd9be14838059fe3ad324f

--- kernel/src/main/java/org/kframework/krun/KRun.java
+++ kernel/src/main/java/org/kframework/krun/KRun.java
@@ -1,14 +1,22 @@
 // Copyright (c) 2015 K Team. All Rights Reserved.
 package org.kframework.krun;
 
+import com.google.common.io.Files;
+import org.kframework.attributes.Source;
 import org.kframework.kil.Attributes;
 import org.kframework.kompile.CompiledDefinition;
+import org.kframework.kore.K;
+import org.kframework.tiny.Rewriter;
 import org.kframework.transformation.Transformation;
 import org.kframework.utils.errorsystem.KExceptionManager;
 import org.kframework.utils.file.FileUtil;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
 /**
- * Created by dwightguth on 4/30/15.
+ * The KORE-based KRun
  */
 public class KRun implements Transformation<Void, Void> {
 
@@ -21,8 +29,25 @@ public class KRun implements Transformation<Void, Void> {
     }
 
     public int run(CompiledDefinition compiledDef, KRunOptions options) {
-        System.err.println("TODO(cos): krun");
-        return 0;
+        try {
+            String pgmFileName = options.configurationCreation.pgm();
+            if (!options.configurationCreation.term()) {
+                KExceptionManager.criticalError("Unsupported options: term=false");
+            }
+
+            String pgm = Files.toString(new File(pgmFileName), Charset.defaultCharset());
+            K program = compiledDef.getProgramParser().apply(pgm, Source.apply(pgm));
+
+            Rewriter rewriter = new Rewriter(compiledDef.executionModule());
+
+            K result = rewriter.execute(program);
+
+            System.err.println("result:" + result);
+            return 0;
+        } catch (Exception e) {
+            KExceptionManager.criticalError(e.getMessage(), e);
+            return 1;
+        }
     }
 
     @Override
