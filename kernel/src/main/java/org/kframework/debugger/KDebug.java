2e5f2c6a56abc10feb258f8d8b98ed4351ef4d3c
Manasvi <msaxena2@illinois.edu>
2015-06-12 13:27:23 -0500

    added the main debugger interface
    Former-commit-id: c4d888b7acd26d8a518c303fe1a5628c102708f7 [formerly 8e9e0d26eeb09547d521dfae8c298350e5b09d10]
    Former-commit-id: d80ae5b780203578b81f8750f2d9cb6aa8cc9ce2

---
+++ kernel/src/main/java/org/kframework/debugger/KDebug.java
@@ -0,0 +1,14 @@
+package org.kframework.debugger;
+
+/**
+ * The Debugger Interface, represents the
+ * Debugger Api. Any Interaction to the Debugger
+ * happens through the methods described in this
+ * interface.
+ */
+public interface KDebug {
+
+    public
+
+
+}
