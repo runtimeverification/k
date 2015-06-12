6ce7cb96b9ef7d84540cbb7d04d2addda48015d1
Manasvi <msaxena2@illinois.edu>
2015-06-12 13:54:53 -0500

    beginning work on the Kore debugger API implementation
    Former-commit-id: 583d2b33689acc66ef310a00f1a421c5471f4da9 [formerly 5df32b6308b455514d9487f7512e86b6e5759913]
    Former-commit-id: dbe2184de79a76e20d9e7a4eb68156f5f34a9d4c

--- kernel/src/main/java/org/kframework/debugger/KDebug.java
+++ kernel/src/main/java/org/kframework/debugger/KDebug.java
@@ -13,6 +13,7 @@ public interface KDebug {
      */
     public int step(int steps);
 
-    
+
+
 
 }
---
+++ kernel/src/main/java/org/kframework/debugger/KoreKDebug.java
@@ -0,0 +1,14 @@
+// Copyright (c) 2015 K Team. All Rights Reserved.
+package org.kframework.debugger;
+
+/**
+ * Kore Based Debugger Implementation.
+ */
+public class KoreKDebug implements KDebug {
+
+    @Override
+    public int step(int steps) {
+        return 0;
+    }
+
+}
