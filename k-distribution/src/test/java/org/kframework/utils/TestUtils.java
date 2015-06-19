9924b3980c9197da43ca5d494d492ce6177079b7
Manasvi <msaxena2@illinois.edu>
2015-06-19 15:49:35 -0500

    more test related refactoring
    Former-commit-id: 10f7632f689d6490f5f3c1f0d28156b0ef6d9c00 [formerly f293d96b2d7d49b313e32b48273364d4f678af08]
    Former-commit-id: 16719374d281383a8377faff84d33a643b8a5223

--- k-distribution/src/test/java/org/kframework/utils/TestUtils.java
+++ k-distribution/src/test/java/org/kframework/utils/TestUtils.java
@@ -1,14 +1,18 @@
 package org.kframework.utils;
 
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 import org.kframework.Rewriter;
 import org.kframework.attributes.Source;
+import org.kframework.backend.java.symbolic.InitializeRewriter;
 import org.kframework.builtin.Sorts;
+import org.kframework.definition.Module;
 import org.kframework.kompile.CompiledDefinition;
 import org.kframework.kompile.Kompile;
 import org.kframework.kompile.KompileOptions;
 import org.kframework.kore.K;
 import org.kframework.kore.convertors.TstTinyOnKORE_IT;
-import org.kframework.krun.KRun;
 import org.kframework.main.GlobalOptions;
 import org.kframework.utils.errorsystem.KExceptionManager;
 import org.kframework.utils.file.FileUtil;
@@ -18,20 +22,33 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.function.BiFunction;
 
+
+
 /**
  * Created by Manasvi on 6/19/15.
  *
- * Common Utilities shared across Tests.
+ * Create this object to use for Tests.
+ *
+ * Contains utilities used across Tests.
  */
 public class TestUtils {
 
-    private static CompiledDefinition compiledDef;
-    protected static File testResource(String baseName) throws URISyntaxException {
+    private CompiledDefinition compiledDef;
+    private AbstractModule guiceModule;
+
+
+
+    public TestUtils(CompiledDefinition compiledDef, AbstractModule guiceModule) {
+        this.compiledDef = compiledDef;
+        this.guiceModule = guiceModule;
+    }
+
+    protected File testResource(String baseName) throws URISyntaxException {
         return new File(TstTinyOnKORE_IT.class.getResource(baseName).toURI());
     }
 
 
-    public static K getProgram(String fileName, String program, Source source) throws IOException, URISyntaxException{
+    public K getProgram(String fileName, String program, Source source) throws IOException, URISyntaxException{
         File definitionFile = testResource(fileName);
         KExceptionManager kem = new KExceptionManager(new GlobalOptions());
 
@@ -46,7 +63,14 @@ public class TestUtils {
         }
     }
 
-    public static Rewriter getRewriter() {
+    public Rewriter getRewriter() {
+        Injector injector = Guice.createInjector(guiceModule);
+        InitializeRewriter init = injector.getInstance(InitializeRewriter.class);
+        return init.apply(compiledDef.executionModule());
+    }
+
+    public Module getUnparsingModule() {
+        return compiledDef.getExtensionModule(Module("UNPARSING", Set(compiledDef.executionModule(), compiledDef.syntaxModule(), compiledDef.getParsedDefinition().getModule("K-SORT-LATTICE").get(), compiledDef.getParsedDefinition().getModule("KSEQ-SYMBOLIC").get()), Set(), Att()));
 
     }
 
