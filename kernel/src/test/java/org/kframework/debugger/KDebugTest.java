36d63fbbd2c2e6d33749c680980a5f081912d6e3
Manasvi <msaxena2@illinois.edu>
2015-06-22 10:46:12 -0500

    small changes
    Former-commit-id: a33bebcf29fa665a629bb8f596034b5f7e9b1d55 [formerly e56817d04a455e4dcbbb447213987cbd8b22a2c4]
    Former-commit-id: 132486599e1e08e247a09da733a488d36774343f

--- kernel/src/test/java/org/kframework/debugger/KDebugTest.java
+++ kernel/src/test/java/org/kframework/debugger/KDebugTest.java
@@ -1,26 +1,77 @@
-package test.java.org.kframework.debugger;
+package org.kframework.debugger;
 
+import com.google.inject.Guice;
 import com.google.inject.Injector;
-import com.google.inject.Module;
-import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Test;
-import org.kframework.krun.KRunFrontEnd;
+import org.junit.rules.TestName;
+import org.kframework.attributes.Source;
+import org.kframework.backend.java.symbolic.JavaSymbolicCommonModule;
+import org.kframework.backend.java.symbolic.Stage;
+import org.kframework.builtin.Sorts;
+import org.kframework.kompile.CompiledDefinition;
+import org.kframework.kompile.Kompile;
+import org.kframework.kompile.KompileOptions;
+import org.kframework.kore.K;
+import org.kframework.krun.api.io.FileSystem;
+import org.kframework.krun.ioserver.filesystem.portable.PortableFileSystem;
+import org.kframework.main.GlobalOptions;
+import org.kframework.utils.errorsystem.KExceptionManager;
+import org.kframework.utils.file.FileUtil;
+import org.kframework.utils.inject.DefinitionScoped;
+import org.kframework.utils.inject.RequestScoped;
+import org.kframework.utils.inject.SimpleScope;
+import org.kframework.utils.options.SMTOptions;
+import org.kframework.testingutils.*;
 
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 
 /**
  * Created by Manasvi on 6/19/15.
- *
+ * <p>
  * Test File for the Debugger Interface Implementation
  */
 public class KDebugTest {
 
-    @BeforeClass
-    public static void setup() {
-        List<Module> modules = KRunFrontEnd.getDefinitionSpecificModules();
+    private static TestUtils testUtils;
+
+    @org.junit.Rule
+    public TestName name = new TestName();
+
+    protected File testResource(String baseName) throws URISyntaxException {
+        return new File(KDebugTest.class.getResource(baseName).toURI());
     }
 
+    @BeforeClass
+    public void setup() throws IOException, URISyntaxException {
+        String filename = "/convertor-tests/" + name.getMethodName() + ".k";
 
+        File definitionFile = testResource(filename);
+        KExceptionManager kem = new KExceptionManager(new GlobalOptions());
 
+        try {
+            CompiledDefinition compiledDef = new Kompile(new KompileOptions(), FileUtil.testFileUtil(), kem, false).run(definitionFile, "IMP", "IMP-SYNTAX", Sorts.K());
+            SimpleScope requestScope = new SimpleScope();
+            Injector injector = Guice.createInjector(new JavaSymbolicCommonModule() {
+                @Override
+                protected void configure() {
+                    super.configure();
+                    bind(GlobalOptions.class).toInstance(new GlobalOptions());
+                    bind(SMTOptions.class).toInstance(new SMTOptions());
+                    bind(Stage.class).toInstance(Stage.REWRITING);
+                    bind(FileSystem.class).to(PortableFileSystem.class);
+                    bind(FileUtil.class).toInstance(FileUtil.testFileUtil());
+
+                    bindScope(RequestScoped.class, requestScope);
+                    bindScope(DefinitionScoped.class, requestScope);
+                }
+            });
+
+        } finally {
+
+        }
+
+    }
 }
--- kernel/src/test/java/org/kframework/testingutils/TestUtils.java
+++ kernel/src/test/java/org/kframework/testingutils/TestUtils.java
@@ -1,23 +1,15 @@
-package org.kframework.utils;
+package org.kframework.testingutils;
 
-import com.google.inject.AbstractModule;
-import com.google.inject.Guice;
 import com.google.inject.Injector;
 import org.kframework.Rewriter;
 import org.kframework.attributes.Source;
 import org.kframework.backend.java.symbolic.InitializeRewriter;
-import org.kframework.builtin.Sorts;
 import org.kframework.definition.Module;
 import org.kframework.kompile.CompiledDefinition;
-import org.kframework.kompile.Kompile;
-import org.kframework.kompile.KompileOptions;
 import org.kframework.kore.K;
-import org.kframework.kore.convertors.TstTinyOnKORE_IT;
 import org.kframework.main.GlobalOptions;
 import org.kframework.utils.errorsystem.KExceptionManager;
-import org.kframework.utils.file.FileUtil;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.function.BiFunction;
@@ -32,29 +24,22 @@ import static org.kframework.definition.Constructors.*;
  *
  * Contains utilities used across Tests.
  */
+
 public class TestUtils {
 
     private CompiledDefinition compiledDef;
-    private AbstractModule guiceModule;
+    private Injector injector;
+    private KExceptionManager kem;
 
-
-
-    public TestUtils(CompiledDefinition compiledDef, AbstractModule guiceModule) {
+    public TestUtils(CompiledDefinition compiledDef, Injector injector, KExceptionManager kem) {
         this.compiledDef = compiledDef;
-        this.guiceModule = guiceModule;
+        this.injector = injector;
+        this.kem = kem;
     }
 
-    protected File testResource(String baseName) throws URISyntaxException {
-        return new File(TstTinyOnKORE_IT.class.getResource(baseName).toURI());
-    }
-
-
-    public K getProgram(String fileName, String program, Source source) throws IOException, URISyntaxException{
-        File definitionFile = testResource(fileName);
+    public K getParsed(String program, Source source) throws IOException, URISyntaxException{
         KExceptionManager kem = new KExceptionManager(new GlobalOptions());
-
         try {
-            compiledDef = new Kompile(new KompileOptions(), FileUtil.testFileUtil(), kem, false).run(definitionFile, "IMP", "IMP-SYNTAX", Sorts.K());
             BiFunction<String, Source, K> programParser = compiledDef.getProgramParser(kem);
             K parsedProgram = programParser.apply(program, source);
             return parsedProgram;
@@ -65,7 +50,6 @@ public class TestUtils {
     }
 
     public Rewriter getRewriter() {
-        Injector injector = Guice.createInjector(guiceModule);
         InitializeRewriter init = injector.getInstance(InitializeRewriter.class);
         return init.apply(compiledDef.executionModule());
     }
