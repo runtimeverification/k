8dc63c09b1651c862118aabf837125d14be4dcc0
Manasvi <msaxena2@illinois.edu>
2015-07-07 18:07:37 -0500

    small fixes needed for compilation
    Former-commit-id: bef78acc85dc6d6dfead2fde6480c804b09ee4b7 [formerly 797245f322f242557d1d0a3e59088360bf0fdbf6]
    Former-commit-id: 9933c7c624d9e2202cd5d6dd14189e91bdb369d2

--- java-backend/src/main/java/org/kframework/backend/java/symbolic/SymbolicRewriter.java
+++ java-backend/src/main/java/org/kframework/backend/java/symbolic/SymbolicRewriter.java
@@ -350,6 +350,8 @@ public class SymbolicRewriter {
      */
     public List<Substitution<Variable,Term>> search(
             Term initialTerm,
+            Term targetTerm,
+            List<Rule> rules,
             Rule pattern,
             int bound,
             int depth,
