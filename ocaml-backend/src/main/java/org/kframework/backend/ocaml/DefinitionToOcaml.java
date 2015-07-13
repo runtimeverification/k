280a620807966e27097599de05341810c914da05
Dwight Guth <dwightguth@runtimeverification.com>
2015-07-13 10:28:13 -0500

    fix bug in STRING module
    Former-commit-id: cfdb1cbc6fcf1eb22e052aeabac8045e092eb048 [formerly d697bf96ce56ef9cced9bc0496c6e6423205fe89]
    Former-commit-id: a452fc934f405d7a00fe8d55f740f7c00cba7048

--- ocaml-backend/src/main/java/org/kframework/backend/ocaml/DefinitionToOcaml.java
+++ ocaml-backend/src/main/java/org/kframework/backend/ocaml/DefinitionToOcaml.java
@@ -87,8 +87,8 @@ public class DefinitionToOcaml implements Serializable {
         this.globalOptions = globalOptions;
         this.kompileOptions = kompileOptions;
     }
-    public static final boolean ocamlopt = false;
-    public static final boolean fastCompilation = true;
+    public static final boolean ocamlopt = true;
+    public static final boolean fastCompilation = false;
     public static final Pattern identChar = Pattern.compile("[A-Za-z0-9_]");
 
     public static final String kType = "t = kitem list\n" +
