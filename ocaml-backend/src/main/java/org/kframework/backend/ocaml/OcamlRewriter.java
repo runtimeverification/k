110991fb1112954dfd59969b03e7a492ea2ef552
Manasvi <msaxena2@illinois.edu>
2015-07-14 17:33:03 -0500

    fixed some bugs
    Former-commit-id: d980819a08d2624eb1efa24657841adc787bd832 [formerly 4af98f75e42b71766f8297368e690e4412191de9]
    Former-commit-id: e24243e31dcc066975ee5c45f8913d2dc5bd728c

--- ocaml-backend/src/main/java/org/kframework/backend/ocaml/OcamlRewriter.java
+++ ocaml-backend/src/main/java/org/kframework/backend/ocaml/OcamlRewriter.java
@@ -53,6 +53,11 @@ public class OcamlRewriter implements Function<Module, Rewriter> {
         }
         return new Rewriter() {
             @Override
+            public List<? extends Map<? extends KVariable, ? extends K>> search(K initialConfig, Optional<Integer> depth, Optional<Integer> bound, Rule pattern) {
+                return null;
+            }
+
+            @Override
             public K execute(K k, Optional<Integer> depth) {
                 String ocaml = converter.execute(k, depth.orElse(-1), files.resolveTemp("run.out").getAbsolutePath());
                 files.saveToTemp("pgm.ml", ocaml);
@@ -65,6 +70,23 @@ public class OcamlRewriter implements Function<Module, Rewriter> {
                 String ocaml = converter.match(k, rule, files.resolveTemp("run.out").getAbsolutePath());
                 files.saveToTemp("match.ml", ocaml);
                 String output = compileAndExecOcaml("match.ml");
+                String[] lines = output.split("\n");
+                int count = Integer.parseInt(lines[0]);
+                int line = 1;
+                List<Map<KVariable, K>> list = new ArrayList<>();
+                for (int i = 0; i < count; i++) {
+                    Map<KVariable, K> map = new HashMap<>();
+                    list.add(map);
+                    for (; line < lines.length; line += 2) {
+                        if (lines[line].equals("|")) {
+                            line++;
+                            break;
+                        }
+                        KVariable key = KVariable(lines[line]);
+                        K value = parseOcamlOutput(lines[line + 1]);
+                        map.put(key, value);
+                    }
+                }
                 return parseOcamlSearchOutput(output);
             }
 
@@ -93,7 +115,7 @@ public class OcamlRewriter implements Function<Module, Rewriter> {
                     break;
                 }
                 KVariable key = KVariable(lines[line]);
-                K value = parseOcamlOutput(lines[line+1]);
+                K value = parseOcamlOutput(lines[line + 1]);
                 map.put(key, value);
             }
         }
@@ -120,15 +142,18 @@ public class OcamlRewriter implements Function<Module, Rewriter> {
                         "-I", "+gmp", "-I", files.resolveKompiled(".").getAbsolutePath(),
                         name);
             }
+
             Process p = pb.directory(files.resolveTemp("."))
                     .redirectError(files.resolveTemp("compile.err"))
                     .redirectOutput(files.resolveTemp("compile.out"))
                     .start();
             int exit = p.waitFor();
+
             if (exit != 0) {
                 System.err.println(files.loadFromTemp("compile.err"));
                 throw KEMException.criticalError("Failed to compile program to ocaml. See output for error information.");
             }
+
             Process p2 = files.getProcessBuilder()
                     .command(files.resolveTemp("a.out").getAbsolutePath())
                     .start();
@@ -150,7 +175,8 @@ public class OcamlRewriter implements Function<Module, Rewriter> {
                     while ((count = System.in.read(buffer)) > 0) {
                         p2.getOutputStream().write(buffer, 0, count);
                     }
-                } catch (IOException | InterruptedException e) {}
+                } catch (IOException | InterruptedException e) {
+                }
             });
             Thread out = new Thread(() -> {
                 int count;
@@ -159,7 +185,8 @@ public class OcamlRewriter implements Function<Module, Rewriter> {
                     while ((count = p2.getInputStream().read(buffer)) >= 0) {
                         System.out.write(buffer, 0, count);
                     }
-                } catch (IOException e) {}
+                } catch (IOException e) {
+                }
             });
             Thread err = new Thread(() -> {
                 int count;
@@ -168,7 +195,8 @@ public class OcamlRewriter implements Function<Module, Rewriter> {
                     while ((count = p2.getErrorStream().read(buffer)) >= 0) {
                         System.err.write(buffer, 0, count);
                     }
-                } catch (IOException e) {}
+                } catch (IOException e) {
+                }
             });
             in.start();
             out.start();
