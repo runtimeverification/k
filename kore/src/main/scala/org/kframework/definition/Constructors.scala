d2ca91914dc32ec53054726b3e1d2dcd0456d30b
Brandon Moore <brandon.3.moore@gmail.com>
2015-08-17 22:45:29 -0500

    Simplify follow regex construction
    This changes Terminal to take the follow pattern as a list of strings
    rather than a single regular expression.
    This avoids excessive stack use in the RegExp parser, and also
    quadratic time appending together the regular expression.
    
    
    Former-commit-id: 3aa9cb0dc04e4f587f09960dd72721f3ad02e7a3 [formerly efddbe16646f16abc4916b1a24419baac70eb0fb]
    Former-commit-id: 1c87288e4b522c20e42f502bbb7c2a5d17f3c12e

--- kore/src/main/scala/org/kframework/definition/Constructors.scala
+++ kore/src/main/scala/org/kframework/definition/Constructors.scala
@@ -28,8 +28,7 @@ object Constructors {
   def Production(klabel: String, sort: Sort, items: Seq[ProductionItem]) = definition.Production(klabel, sort, items)
   def Production(klabel: String, sort: Sort, items: Seq[ProductionItem], att: attributes.Att) = definition.Production(klabel, sort, items, att)
 
-  def Terminal(s: String) = definition.Terminal(s, "#")
-  def Terminal(s: String, followRegexString: String) = definition.Terminal(s, followRegexString)
+  def Terminal(s: String) = definition.Terminal(s, Seq())
   def NonTerminal(sort: Sort) = definition.NonTerminal(sort)
   def RegexTerminal(regexString: String) = definition.RegexTerminal("#", regexString, "#")
   def RegexTerminal(precedeRegexString: String, regexString: String, followRegexString: String) = definition.RegexTerminal(precedeRegexString, regexString, followRegexString)
--- kore/src/test/scala/org/kframework/meta/TestMeta.scala
+++ kore/src/test/scala/org/kframework/meta/TestMeta.scala
@@ -31,11 +31,11 @@ class TestMeta {
     if (x != y) Assert.assertEquals(x.toString, y.toString)
   }
 
-  val m = Module("TEST", Set(), Set(Production("Foo", Sort("Foo"), Seq(Terminal("Bar", "#")))))
+  val m = Module("TEST", Set(), Set(Production("Foo", Sort("Foo"), Seq(Terminal("Bar", Seq())))))
   val d = Definition(m, m, Set(m))
 
   val metamodule = 'Module("TEST", 'Set(),
-    'Set('Production("Foo", 'Sort("Foo"), 'List('Terminal("Bar", "#")))))
+    'Set('Production("Foo", 'Sort("Foo"), 'List('Terminal("Bar")))))
 
   val metad = 'Definition(metamodule, metamodule,
     'Set(metamodule),
@@ -104,7 +104,7 @@ class TestMeta {
       assertEquals(prod, actual)
     }
     {
-      val prod = Production(Sort("Exp"), Seq(NonTerminal(Sort("Int")), Terminal("+", "#"), RegexTerminal("#", "[a-z]", "#")), Att())
+      val prod = Production(Sort("Exp"), Seq(NonTerminal(Sort("Int")), Terminal("+", Seq()), RegexTerminal("#", "[a-z]", "#")), Att())
       val prod2 = Production(Sort("Exp"), Seq(), Att().add("originalProd", prod))
       assertEquals(prod, prod2.att.get("originalProd").get)
     }
