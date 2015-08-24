743fb7085c9f7c2eea8a122e33d63fc534488c00
Dwight Guth <dwightguth@runtimeverification.com>
2015-08-24 10:17:30 -0500

    better random interface
    Former-commit-id: 0a0761a081428f2c82e3c5cf59444fd2eeba433e [formerly 8078df79057bebd7e70a1e2b94a608e2f8cd3325]
    Former-commit-id: 154c2ca0e615a4efef2e0b2c3fb0de253068f466

--- k-distribution/include/ocaml/prelude.ml
+++ k-distribution/include/ocaml/prelude.ml
@@ -481,11 +481,11 @@ struct
   let hook_min c lbl sort config ff = match c with
       [Int a], [Int b] -> [Int (Z.min a b)]
     | _ -> raise Not_implemented
-  let hook_random c lbl sort config ff = match c with
-      [Int seed] -> Random.init (Z.to_int seed);
-          let i64 = Random.int64 (Int64.max_int) in
-          [Int (Z.of_int64 i64)]
+  let hook_rand c lbl sort config ff = match c with
+      [Int max] -> let mpz = Gmp.Z.unrandomm Gmp.RNG.default max in
+          [Int (to_zarith mpz)]
     | _ -> raise Not_implemented
+  let hook_srand c lbl sort config ff = raise Not_implemented
   let hook_ediv c lbl sort config ff = raise Not_implemented
   let hook_emod c lbl sort config ff = raise Not_implemented
 end
