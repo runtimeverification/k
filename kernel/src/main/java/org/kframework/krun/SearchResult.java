9b6067c80cff9e67edb7cf886023841e1d51564c
Manasvi <msaxena2@illinois.edu>
2015-09-04 20:09:56 -0500

    some small changes
    Former-commit-id: 19007ef18334156855a83cbbfb407d9ee5c289b8 [formerly 7698405fcf19fee98d23213eade10099fd96e1c8]
    Former-commit-id: 941033dcaebb84061cfc243b10adac296f818edf

--- kernel/src/main/java/org/kframework/krun/SearchResult.java
+++ kernel/src/main/java/org/kframework/krun/SearchResult.java
@@ -10,16 +10,16 @@ import java.util.Map;
 /**
  * Created by manasvi on 9/4/15.
  * Object Containing the results of a search operation.
- *
  */
 public class SearchResult {
-    private final List<? extends Map<? extends KVariable, ? extends K>> searchList;
+    private List<? extends Map<? extends KVariable, ? extends K>> searchList;
 
     public SearchResult(List<? extends Map<? extends KVariable, ? extends K>> searchList) {
         this.searchList = searchList;
     }
 
     public List<? extends Map<? extends KVariable, ? extends K>> getSearchList() {
-        return Collections.unmodifiableList(searchList);
+        return searchList == null ? null : Collections.unmodifiableList(searchList);
+
     }
 }
