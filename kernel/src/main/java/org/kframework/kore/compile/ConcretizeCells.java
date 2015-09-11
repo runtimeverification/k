54eabd504481efdae60d25a97f669dfc51a1257a
Cosmin Radoi <cosmin.radoi@gmail.com>
2015-09-10 21:01:36 -0500

    split K cell wrapping from adding the top cell
    Former-commit-id: cfa0702d81aef2d12ceff3660d1ac61f0a50e759 [formerly 002e4cff8f2a17650c14bd49e6ac23a834d4e4f9]
    Former-commit-id: 7d0fdd94b9cc3d77524f233fad4be4d490f2bd8c

--- kernel/src/main/java/org/kframework/kore/compile/ConcretizeCells.java
+++ kernel/src/main/java/org/kframework/kore/compile/ConcretizeCells.java
@@ -3,8 +3,6 @@ package org.kframework.kore.compile;
 
 import org.kframework.compile.ConfigurationInfo;
 import org.kframework.compile.LabelInfo;
-import org.kframework.definition.Module;
-import org.kframework.definition.ModuleTransformer;
 import org.kframework.definition.Sentence;
 import org.kframework.utils.errorsystem.KExceptionManager;
 
@@ -18,7 +16,7 @@ import org.kframework.utils.errorsystem.KExceptionManager;
  * strict accordance with their declared fixed-arity productions.
  *
  * This is a simple composition of the
- * {@link AddImplicitCells}, {@link AddParentCells},
+ * {@link AddTopCellToRules}, {@link AddParentCells},
  * {@link CloseCells}, and {@link SortCells} passes,
  * see their documentation for details on the transformations.
  */
@@ -26,17 +24,17 @@ public class ConcretizeCells {
     final ConfigurationInfo configurationInfo;
     final LabelInfo labelInfo;
     final SortInfo sortInfo;
-
-    final AddImplicitCells addImplicitCells;
+    
     final AddParentCells addParentCells;
     final CloseCells closeCells;
     final SortCells sortCells;
+    private final AddTopCellToRules addRootCell;
 
     public ConcretizeCells(ConfigurationInfo configurationInfo, LabelInfo labelInfo, SortInfo sortInfo, KExceptionManager kem) {
         this.configurationInfo = configurationInfo;
         this.labelInfo = labelInfo;
         this.sortInfo = sortInfo;
-        addImplicitCells = new AddImplicitCells(configurationInfo, labelInfo);
+        addRootCell = new AddTopCellToRules(configurationInfo, labelInfo);
         addParentCells = new AddParentCells(configurationInfo, labelInfo);
         closeCells = new CloseCells(configurationInfo, sortInfo, labelInfo);
         sortCells = new SortCells(configurationInfo, labelInfo, kem);
@@ -46,6 +44,6 @@ public class ConcretizeCells {
         return sortCells.sortCells(
                 closeCells.close(
                  addParentCells.concretize(
-                  addImplicitCells.addImplicitCells(s))));
+                         addRootCell.addImplicitCells(s))));
     }
 }
