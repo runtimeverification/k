e6b2a4761ddb214b94944619272644c59c24707e
Andrei Stefanescu <andreistef@gmail.com>
2015-09-17 00:42:46 -0500

    added kernelc to ktest
    Former-commit-id: ac9a0f84f7bc33d98aa1a62279ab8b970131a7a0 [formerly c0bca9f3bc69ac95909b0f78c4c1235666aea576]
    Former-commit-id: 0b763b979faf3c20da13719378fa5c1c98ae83f3

--- java-backend/src/main/java/org/kframework/backend/java/symbolic/SymbolicRewriter.java
+++ java-backend/src/main/java/org/kframework/backend/java/symbolic/SymbolicRewriter.java
@@ -514,31 +514,35 @@ public class SymbolicRewriter {
                     continue;
                 }
 
-//                // TODO(YilongL): the `get(0)` seems hacky
-//                Term leftKContent = term.term().getCellContentsByName(CellLabel.K).get(0);
-//                Variable leftFrame = KSequence.getFrame(leftKContent);
-//                if (leftFrame != null) {
-//                    KSequence.Builder builder = KSequence.builder();
-//                    KSequence.getElements(leftKContent).stream().forEach(builder::concatenate);
-//                    leftKContent = builder.build();
-//                }
-//                Term rightKContent = targetTerm.term().getCellContentsByName(CellLabel.K).get(0);
-//                Variable rightFrame = KSequence.getFrame(rightKContent);
-//                if (rightFrame != null) {
-//                    KSequence.Builder builder = KSequence.builder();
-//                    KSequence.getElements(rightKContent).stream().forEach(builder::concatenate);
-//                    rightKContent = builder.build();
-//                }
-//                if (leftFrame != null && rightFrame != null && leftFrame.equals(rightFrame)) {
-//                    BoolToken matchable = MetaK.matchable(
-//                            leftKContent,
-//                            rightKContent,
-//                            term.termContext());
-//                    if (matchable != null && matchable.booleanValue()) {
-//                        proofResults.add(term);
-//                        continue;
-//                    }
-//                }
+                List<Term> leftKContents = term.term().getCellContentsByName(CellLabel.K);
+                List<Term> rightKContents = targetTerm.term().getCellContentsByName(CellLabel.K);
+                // TODO(YilongL): the `get(0)` seems hacky
+                if (leftKContents.size() == 1 && rightKContents.size() == 1) {
+                    Term leftKContent = leftKContents.get(0);
+                    Variable leftFrame = KSequence.getFrame(leftKContent);
+                    if (leftFrame != null) {
+                        KSequence.Builder builder = KSequence.builder();
+                        KSequence.getElements(leftKContent).stream().forEach(builder::concatenate);
+                        leftKContent = builder.build();
+                    }
+                    Term rightKContent = rightKContents.get(0);
+                    Variable rightFrame = KSequence.getFrame(rightKContent);
+                    if (rightFrame != null) {
+                        KSequence.Builder builder = KSequence.builder();
+                        KSequence.getElements(rightKContent).stream().forEach(builder::concatenate);
+                        rightKContent = builder.build();
+                    }
+                    if (leftFrame != null && rightFrame != null && leftFrame.equals(rightFrame)) {
+                        BoolToken matchable = MetaK.matchable(
+                                leftKContent,
+                                rightKContent,
+                                term.termContext());
+                        if (matchable != null && matchable.booleanValue()) {
+                            proofResults.add(term);
+                            continue;
+                        }
+                    }
+                }
 
                 if (guarded) {
                     ConstrainedTerm result = applyRule(term, rules);
