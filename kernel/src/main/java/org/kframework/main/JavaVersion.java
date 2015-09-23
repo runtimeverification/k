fdde9ad8949650499e7f304734a9ff5d61ea357f
Dwight Guth <dwightguth@runtimeverification.com>
2015-09-23 18:15:08 -0500

    restructure boot scripts to avoid calls to `java -version` when nailgun is running; it is slow

---
+++ kernel/src/main/java/org/kframework/main/JavaVersion.java
@@ -0,0 +1,12 @@
+package org.kframework.main;
+
+/**
+ * Created by dwightguth on 9/23/15.
+ */
+public class JavaVersion {
+
+    public static void main(String[] args) {
+        System.err.println("java version \"" + System.getProperty("java.version") + "\"");
+        System.err.println(System.getProperty("sun.arch.data.model") + "-Bit");
+    }
+}
