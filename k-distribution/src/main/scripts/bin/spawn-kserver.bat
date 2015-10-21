2c4186bfe07bfd1c1a9c7fb583a584d6e97ad89e
Dwight Guth <dwightguth@runtimeverification.com>
2015-10-21 11:21:11 -0500

    add another path to spawn-kserver.bat

--- k-distribution/src/main/scripts/bin/spawn-kserver.bat
+++ k-distribution/src/main/scripts/bin/spawn-kserver.bat
@@ -1,6 +1,6 @@
 @ECHO off
 SETLOCAL ENABLEEXTENSIONS
 IF ERRORLEVEL 1 ECHO Unable to enable extensions
-set PATH=%PATH%;C:\RV-Match\msys\usr\bin
+set PATH=%PATH%;C:\RV-Match\msys\usr\bin;C:\RV-Match\msys\mingw32\bin
 start wscript "%~dp0..\lib\invisible.vbs" "%~dp0\kserver.bat" 
 exit
