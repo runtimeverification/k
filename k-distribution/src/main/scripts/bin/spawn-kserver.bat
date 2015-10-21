be69fde56331c8a3eb97a814e2a108909acb616a
Dwight Guth <dwightguth@runtimeverification.com>
2015-10-21 11:17:36 -0500

    fix spawn-kserver.bat for new rv-match

--- k-distribution/src/main/scripts/bin/spawn-kserver.bat
+++ k-distribution/src/main/scripts/bin/spawn-kserver.bat
@@ -1,6 +1,6 @@
 @ECHO off
 SETLOCAL ENABLEEXTENSIONS
 IF ERRORLEVEL 1 ECHO Unable to enable extensions
-set PATH=%PATH%;C:\cygwin\bin;C:\cygwin\usr\bin;C:\cygwin\usr\local\bin
+set PATH=%PATH%;C:\RV-Match\msys\usr\bin
 start wscript "%~dp0..\lib\invisible.vbs" "%~dp0\kserver.bat" 
 exit
