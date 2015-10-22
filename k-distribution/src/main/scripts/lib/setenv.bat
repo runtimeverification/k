d742f7bfb43c5767fe6dc8100262f38cfbc33b2d
Dwight Guth <dwightguth@runtimeverification.com>
2015-10-22 18:09:14 -0500

    fix k scripts on windows to run faster

--- k-distribution/src/main/scripts/lib/setenv.bat
+++ k-distribution/src/main/scripts/lib/setenv.bat
@@ -1,10 +1,6 @@
 @ECHO off
-for /f "delims=" %%i in ('java -version 2^>^&1') do set VERSION=%%i
-if not "%version:64-Bit=%" == "%version%" (
-  set ARCH=64
-) else (
-  set ARCH=32
-)
+set PATH=%~dp0..\lib\native\windows;%PATH%
+call "%~dp0..\lib\checkJava.bat" :: sets ARCH and JAVA
 
-set PATH=%PATH%;%~dp0\..\lib\native\windows;%~dp0\..\lib\native\windows%ARCH%;%~dp0\..\lib\native\%ARCH%
+set PATH=%~dp0..\lib\native\windows%ARCH%;%~dp0..\lib\native\%ARCH%;%PATH%
 set CYGWIN=nodosfilewarning
