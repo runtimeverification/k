8d9f8aeb75df6a36dae382bbe0ebfb22d948ddcd
Brandon Moore <brandon.3.moore@gmail.com>
2015-11-12 22:54:26 -0600

    Revert "Simplify step-counting loop in SymbolicRewriter"
    This reverts commit 8d0f9d390c0b5c4e49fc1d96e3deafcf02a98ae5 [formerly cb890acddd5cc542bf30e234027558a1671df490] [formerly a8a792102d5ca0fb751782cca708a574682c6414 [formerly ad24fc34012d56e8fc1def19b38d2a9beaba05ff]].
    
    
    Former-commit-id: ddfff4aeb22043c342d94e50ea98f1b7008113eb [formerly 8b33b56856ed4254008b087d265178ae810cb9b1]
    Former-commit-id: 5cd032831a8cfd25aa65806b4c1959706220433b

--- java-backend/src/main/java/org/kframework/backend/java/symbolic/SymbolicRewriter.java
+++ java-backend/src/main/java/org/kframework/backend/java/symbolic/SymbolicRewriter.java
@@ -50,15 +50,25 @@ public class SymbolicRewriter {
 
     public KRunState rewrite(ConstrainedTerm constrainedTerm, int bound) {
         stopwatch.start();
-        int step = 0;
-        List<ConstrainedTerm> results;
-        while (step != bound &&
-                !(results = computeRewriteStep(constrainedTerm, step+1, true)).isEmpty()) {
+        KRunState finalState = null;
+        int step = 1;
+        if (bound == 0) {
+            finalState = new JavaKRunState(constrainedTerm, counter, Optional.of(0));
+        }
+        while (step <= bound || bound < 0) {
             /* get the first solution */
-            constrainedTerm = results.get(0);
+            List<ConstrainedTerm> results = computeRewriteStep(constrainedTerm, step, true);
+            if (!results.isEmpty()) {
+                constrainedTerm = results.get(0);
+                if (step == bound) {
+                    finalState = new JavaKRunState(constrainedTerm, counter, Optional.of(step));
+                }
+            } else {
+                finalState = new JavaKRunState(constrainedTerm, counter, Optional.of(step - 1));
+                break;
+            }
             step++;
         }
-        KRunState finalState = new JavaKRunState(constrainedTerm, counter, Optional.of(step));
 
         stopwatch.stop();
         if (constrainedTerm.termContext().global().krunOptions.experimental.statistics) {
