16c98b5ab056df6612d540f54bd9335146077c2b
Cosmin Radoi <cosmin.radoi@gmail.com>
2015-12-07 17:32:38 -0600

    extra utilities
    Former-commit-id: 52c7dd105e1df07c12abf0a23a5a7ada01a81b96 [formerly e2540957a3534ca1f7a7ad4e7cb6a38b2cfdd9d5]
    Former-commit-id: c92c75d885e40579040217cf60519ed222f7eb40

---
+++ kore/src/main/java/org/kframework/kore/FindK.java
@@ -0,0 +1,19 @@
+package org.kframework.kore;
+
+import org.kframework.Collections;
+import scala.collection.Set;
+
+/**
+ * Finds all patterns described by the visitor
+ */
+public class FindK extends AbstractFoldK<Set<K>> {
+    @Override
+    public Set<K> unit() {
+        return Collections.<K>Set();
+    }
+
+    @Override
+    public Set<K> merge(Set<K> a, Set<K> b) {
+        return Collections.or(a, b);
+    }
+}
