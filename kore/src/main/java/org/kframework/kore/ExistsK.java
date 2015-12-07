415135004ff2a235d9cd117a8f951b83776fea7b
Cosmin Radoi <cosmin.radoi@gmail.com>
2015-12-07 17:32:38 -0600

    extra utilities
    Former-commit-id: 189d91adc719814fb8c87c5369541714873e7247 [formerly 8ac7c77e06509497f7418f58dc5e86ce998e5b2c]
    Former-commit-id: 9b001dd65248bd77c7f0788bdbc1b55d704b312f

---
+++ kore/src/main/java/org/kframework/kore/ExistsK.java
@@ -0,0 +1,16 @@
+package org.kframework.kore;
+
+/**
+ * Checks whether particular K pattern given as a visitor exists.
+ */
+public class ExistsK extends AbstractFoldK<Boolean> {
+    @Override
+    public Boolean unit() {
+        return false;
+    }
+
+    @Override
+    public Boolean merge(Boolean a, Boolean b) {
+        return a || b;
+    }
+}
