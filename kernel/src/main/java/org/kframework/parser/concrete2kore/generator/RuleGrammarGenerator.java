8f98185503b50df9588da394bc0984eb0779c76d
Cosmin Radoi <cosmin.radoi@gmail.com>
2015-12-10 19:12:13 -0600

    fix bug we've introduced
    Former-commit-id: 9eedd81b0f8dd3ecb3f03c22686e89366667ac0a [formerly da5104e4846615d6c6b8dc5d561c0375dc209cf1]
    Former-commit-id: 5a46c959e3c7f7a1e346d95fb18aa833d11c3e32

--- kernel/src/main/java/org/kframework/parser/concrete2kore/generator/RuleGrammarGenerator.java
+++ kernel/src/main/java/org/kframework/parser/concrete2kore/generator/RuleGrammarGenerator.java
@@ -269,18 +269,18 @@ public class RuleGrammarGenerator {
             for (UserList ul : UserList.getLists(prods3)) {
                 org.kframework.definition.Production prod1, prod2, prod3, prod4, prod5;
 
-                Att newAtts = ul.attrs.remove("userList").add("klabel", ul.klabel);
+                Att newAtts = ul.attrs.remove("userList");
                 // Es#Terminator ::= "" [klabel('.Es)]
                 prod1 = Production(ul.terminatorKLabel, Sort(ul.sort + "#Terminator"), Seq(Terminal("")),
-                        newAtts.add(Constants.ORIGINAL_PRD, ul.pTerminator));
+                        newAtts.add("klabel", ul.terminatorKLabel).add(Constants.ORIGINAL_PRD, ul.pTerminator));
                 // Ne#Es ::= E "," Ne#Es [klabel('_,_)]
                 prod2 = Production(ul.klabel, Sort("Ne#" + ul.sort),
                         Seq(NonTerminal(Sort(ul.childSort)), Terminal(ul.separator), NonTerminal(Sort("Ne#" + ul.sort))),
-                        newAtts.add(Constants.ORIGINAL_PRD, ul.pList));
+                        newAtts.add("klabel", ul.klabel).add(Constants.ORIGINAL_PRD, ul.pList));
                 // Ne#Es ::= E Es#Terminator [klabel('_,_)]
                 prod3 = Production(ul.klabel, Sort("Ne#" + ul.sort),
                         Seq(NonTerminal(Sort(ul.childSort)), NonTerminal(Sort(ul.sort + "#Terminator"))),
-                        newAtts.add(Constants.ORIGINAL_PRD, ul.pList));
+                        newAtts.add("klabel", ul.klabel).add(Constants.ORIGINAL_PRD, ul.pList));
                 // Es ::= Ne#Es
                 prod4 = Production(Sort(ul.sort), Seq(NonTerminal(Sort("Ne#" + ul.sort))));
                 // Es ::= Es#Terminator // if the list is *
