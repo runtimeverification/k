6f355aeb58328266318ac4b7f1261743a6fc5e59
Andrei Stefanescu <andreistef@gmail.com>
2015-12-27 01:17:26 +0200

    add toString to BitSet
    Former-commit-id: 81f9a687215b5eb3f2043f28e38fb5d0054dcec6 [formerly 2a7505c50cbf401f2ce2447f4e6d3302067cf4cc]
    Former-commit-id: 454111af6a3ed2a6fe696cb1f4ad6b66586bbba8

--- kernel/src/main/java/org/kframework/utils/FourWordBitSet.java
+++ kernel/src/main/java/org/kframework/utils/FourWordBitSet.java
@@ -2,6 +2,8 @@
 
 package org.kframework.utils;
 
+import java.util.stream.Collectors;
+
 /**
  * {@link BitSet} implementation backed by four words. Faster than using the JDK {@link java.util.BitSet}.
  */
@@ -199,6 +201,11 @@ public class FourWordBitSet implements BitSet<FourWordBitSet> {
     @Override
     public FourWordBitSet clone() {
         return new FourWordBitSet(word0, word1, word2, word3);
+    }
+
+    @Override
+    public String toString() {
+        return "{ " +  stream().mapToObj(i -> Integer.toString(i)).collect(Collectors.joining(", ")) + "}";
     }
 
 }
