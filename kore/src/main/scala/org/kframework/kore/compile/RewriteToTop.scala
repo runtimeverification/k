730ae0aefe9913054ca6488236dcd7d5c313df8e
Andrei Stefanescu <andreistef@gmail.com>
2015-12-30 17:05:07 +0200

    fix bubbling of rewrite out of cells
    Former-commit-id: b1d8a57590183e4c45b78e2a6b5f5fb27025c2be [formerly 150a25fb925d8843630242b257fe26f1398fe960]
    Former-commit-id: 2c764df19f578118a395781abdb64a27b40c9b5e

--- kore/src/main/scala/org/kframework/kore/compile/RewriteToTop.scala
+++ kore/src/main/scala/org/kframework/kore/compile/RewriteToTop.scala
@@ -30,13 +30,26 @@ object RewriteToTop {
 
   def bubbleRewriteToTopInsideCells(k: K): K = k match {
     case kapp: KApply =>
-      if (kapp.klabel.name.startsWith("<") && kapp.klabel.name.endsWith(">"))
-        KApply(kapp.klabel, immutable(kapp.klist.items) map bubbleRewriteToTopInsideCells, kapp.att)
+      if (isCell(kapp) && nonCell(kapp.items.get(0)))
+        KApply(kapp.klabel, ADT.KRewrite(toLeft(kapp.items.get(0)), toRight(kapp.items.get(0)), kapp.att))
       else
-        makeRewriteIfNeeded(k)
-    case _ => makeRewriteIfNeeded(k)
+        KApply(kapp.klabel, immutable(kapp.klist.items) map bubbleRewriteToTopInsideCells, kapp.att)
+    case _ => k
   }
 
-  private def makeRewriteIfNeeded(k: K): K = if (toLeft(k) != toRight(k)) ADT.KRewrite(toLeft(k), toRight(k)) else k
+
+  def nonCell(k: K): Boolean = k match {
+    case kapp: KApply => if (!isCell(kapp)) {
+      immutable(kapp.klist.items) map nonCell forall { b => b }
+    } else {
+      false
+    }
+    case rw: KRewrite => nonCell(rw.left) && nonCell(rw.right)
+    case _ => true
+  }
+
+  private def isCell(kapp: KApply): Boolean = {
+    kapp.klabel.name.startsWith("<") && kapp.klabel.name.endsWith(">")
+  }
 
 }
