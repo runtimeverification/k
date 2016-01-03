7617864f666646c7b48593d25c62cc945071639b
Daejun Park <daejunpark@gmail.com>
2016-01-03 10:50:25 +0900

    [em] no comparison for source location
    Former-commit-id: 66837b483e0114e348010dc6747aad067839954b [formerly 83f53e99411659549a0d7ee43ae6b19a939fbf85]
    Former-commit-id: aa32894cd5b78eac038a1bec0277856881c97b27

--- k-distribution/tests/regression/error-messages/run.sh
+++ k-distribution/tests/regression/error-messages/run.sh
@@ -2,7 +2,5 @@
 
 for i in */*.k.out; do
   echo "// $i"
-  d=`dirname "$i"`
-  o=`basename "$i"`
-  ( cd "$d"; kompile "${o%.out}" 2>&1 | diff - "$o" )
+  kompile "${i%.out}" 2>&1 | grep -v 'Location\|Source' | diff - "$i"
 done >>x.k
