0f39d78a31639080d91296cd294063352a5d02a2
Radu Mereuta <headness13@gmail.com>
2016-01-16 14:57:41 +0200

    Fixing rewrite and KSeq filters to handle more cases.
    Former-commit-id: 4d37e78b215e68fc07b7c637fd6b927bfe398abe [formerly d44995a6872889ea5ce71c4d2b833c069296f002]
    Former-commit-id: 0ffb6bd9837a19338da101e7b7e1fe23789cc2cb

--- kernel/src/main/java/org/kframework/parser/concrete2kore/disambiguation/CorrectKSeqPriorityVisitor.java
+++ kernel/src/main/java/org/kframework/parser/concrete2kore/disambiguation/CorrectKSeqPriorityVisitor.java
@@ -2,7 +2,9 @@
 package org.kframework.parser.concrete2kore.disambiguation;
 
 import com.google.common.collect.Sets;
+import org.kframework.Collections;
 import org.kframework.definition.NonTerminal;
+import org.kframework.parser.Ambiguity;
 import org.kframework.parser.SetsTransformerWithErrors;
 import org.kframework.parser.Term;
 import org.kframework.parser.TermCons;
@@ -16,6 +18,7 @@ import scala.util.Right;
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.kframework.Collections.*;
 
 /**
  * Make sure that KSequence binds greedy (has least priority after rewrite).
@@ -23,6 +26,7 @@ import java.util.Set;
 public class CorrectKSeqPriorityVisitor extends SetsTransformerWithErrors<ParseFailedException> {
 
     private final static Set<String> exceptions;
+
     static {
         exceptions = new HashSet<>();
         exceptions.add("#ruleRequires");
@@ -31,6 +35,20 @@ public class CorrectKSeqPriorityVisitor extends SetsTransformerWithErrors<ParseF
         exceptions.add("#KRewrite");
         exceptions.add("#KSequence");
         exceptions.add("#KList");
+    }
+
+    @Override
+    public Either<java.util.Set<ParseFailedException>, Term> apply(Ambiguity amb) {
+        // if the ambiguity has KSeq at the top, prefer them, and eliminate the rest
+        scala.collection.Set<Term> rewrites = amb.items().stream().filter(o ->
+                o instanceof TermCons &&
+                        ((TermCons) o).production().klabel().isDefined() &&
+                        ((TermCons) o).production().klabel().get().name().equals("#KSequence")).collect(Collections.toSet());
+        if (rewrites.size() == 0 || rewrites.size() == amb.items().size())
+            return super.apply(amb);
+        if (rewrites.size() == 1)
+            return Right.apply(rewrites.head());
+        return super.apply(Ambiguity.apply(mutable(rewrites)));
     }
 
     @Override
@@ -60,6 +78,7 @@ public class CorrectKSeqPriorityVisitor extends SetsTransformerWithErrors<ParseF
 
     private static class PriorityVisitor2 extends SetsTransformerWithErrors<ParseFailedException> {
         private final TermCons parent;
+
         public PriorityVisitor2(TermCons parent) {
             this.parent = parent;
         }
