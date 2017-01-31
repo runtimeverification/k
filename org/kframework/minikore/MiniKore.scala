c2acbaf4eee5bcf9831a6ff500795bdeb701e6f7
Daejun Park <daejunpark@gmail.com>
2017-01-31 00:08:57 -0600

    wip

--- org/kframework/minikore/MiniKore.scala
+++ org/kframework/minikore/MiniKore.scala
@@ -20,26 +20,33 @@ object MiniKore {
   case class Axiom(pattern: Pattern, att: Attributes) extends Sentence
 
   sealed trait Pattern extends i.Pattern
-  case class Variable(name: String, sort: String) extends Pattern
-  case class Application(label: String, args: Seq[Pattern]) extends Pattern
-  case class DomainValue(label: String, value: String) extends Pattern
+  case class Variable(name: String, sort: String) extends Pattern with i.Variable
+  case class Application(label: String, args: Seq[Pattern]) extends Pattern with i.Application
+  case class DomainValue(label: String, value: String) extends Pattern with i.DomainValue
   //
-  case class True() extends Pattern
-  case class False() extends Pattern
+  case class True() extends Pattern with i.True
+  case class False() extends Pattern with i.False
   //
   case class And(p: Pattern, q: Pattern) extends Pattern with i.And
-  case class Or(p: Pattern, q: Pattern) extends Pattern
+  case class Or(p: Pattern, q: Pattern) extends Pattern with i.Or
   case class Not(p: Pattern) extends Pattern
   case class Implies(p: Pattern, q: Pattern) extends Pattern
-  case class Exists(v: Variable, p: Pattern) extends Pattern
+  case class Exists(v: Variable, p: Pattern) extends Pattern with i.Exists
   case class ForAll(v: Variable, p: Pattern) extends Pattern
   //
   case class Next(p: Pattern) extends Pattern
   case class Rewrite(p: Pattern, q: Pattern) extends Pattern
   case class Equal(p: Pattern, q: Pattern) extends Pattern
 
-
-  object And extends i.AndConstructor {
-    def apply(p: i.Pattern, q: i.Pattern): i.Pattern = new And(p.asInstanceOf[Pattern], q.asInstanceOf[Pattern])
+  object Constructor extends i.Constructor[Pattern, Variable] {
+    override def Variable(name: String, sort: String) = MiniKore.Variable(name, sort)
+    override def Application(label: String, args: Seq[Pattern]) = MiniKore.Application(label, args)
+    override def DomainValue(label: String, value: String) = MiniKore.DomainValue(label, value)
+    override def True() = MiniKore.True()
+    override def False() = MiniKore.False()
+    override def And(p: Pattern, q: Pattern) = MiniKore.And(p,q)
+    override def Or(p: Pattern, q: Pattern) = MiniKore.Or(p,q)
+    override def Exists(v: Variable, p: Pattern) = MiniKore.Exists(v,p)
   }
+
 }
--- org/kframework/minikore/MiniKoreInterface.scala
+++ org/kframework/minikore/MiniKoreInterface.scala
@@ -1,21 +1,52 @@
 package org.kframework.minikore
 
-/**
-  * Created by daejunpark on 1/26/17.
-  */
 object MiniKoreInterface {
 
   trait Pattern
+
+  trait Variable extends Pattern {
+    def name: String
+    def sort: String
+  }
+
+  trait Application extends Pattern {
+    def label: String
+    def args: Seq[Pattern]
+  }
+
+  trait DomainValue extends Pattern {
+    def label: String
+    def value: String
+  }
+
+  trait True extends Pattern
+
+  trait False extends Pattern
 
   trait And extends Pattern {
     def p: Pattern
     def q: Pattern
   }
 
-  //trait PatternConstructor
+  trait Or extends Pattern {
+    def p: Pattern
+    def q: Pattern
+  }
 
-  trait AndConstructor /*extends PatternConstructor*/ {
-    def apply(p: Pattern, q: Pattern): Pattern
+  trait Exists extends Pattern {
+    def v: Variable
+    def p: Pattern
+  }
+
+  trait Constructor[P <: Pattern, V <: Variable] {
+    def Variable(name: String, sort: String): P
+    def Application(label: String, args: Seq[P]): P
+    def DomainValue(label: String, value: String): P
+    def True(): P
+    def False(): P
+    def And(p: P, q: P): P
+    def Or(p: P, q: P): P
+    def Exists(v: V, p: P): P
   }
 
 }
