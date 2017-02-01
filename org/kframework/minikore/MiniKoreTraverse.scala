93f8e18f5bf9ce8cbc0c19d4098b139d1765c340
Daejun Park <daejunpark@gmail.com>
2017-01-31 21:39:44 -0600

    wip

--- org/kframework/minikore/MiniKoreTraverse.scala
+++ org/kframework/minikore/MiniKoreTraverse.scala
@@ -9,26 +9,6 @@ abstract class MiniKoreTraverse {
 
   def c[P <: i.Pattern, V <: i.Variable]: i.Constructor[P,V]
 
-//  def pre(p: i.Pattern): i.Pattern
-//  def post(p: i.Pattern): i.Pattern
-//  def postv(p: i.Variable): i.Variable
-//  def map2(p: i.Pattern): i.Pattern = pre(p) match {
-//    case p:i.Variable => postv(c.Variable(p.name, p.sort))
-//    case p:i.Application => post(c.Application(p.label, p.args.map(map)))
-//    case p:i.DomainValue => post(c.DomainValue(p.label, p.value))
-//    case p:i.True => post(c.True())
-//    case p:i.False => post(c.False())
-//    case p:i.And => post(c.And(map(p.p), map(p.q)))
-//    case p:i.Or => post(c.Or(map(p.p), map(p.q)))
-//    case p:i.Not => post(c.Not(map(p.p)))
-//    case p:i.Implies => post(c.Implies(map(p.p), map(p.q)))
-//    case p:i.Exists => post(c.Exists(postv(p.v), map(p.p)))
-//    case p:i.ForAll => post(c.ForAll(postv(p.v), map(p.p)))
-//    case p:i.Next => post(c.Next(map(p.p)))
-//    case p:i.Rewrite => post(c.Rewrite(map(p.p), map(p.q)))
-//    case p:i.Equal => post(c.Equal(map(p.p), map(p.q)))
-//  }
-
   def map(f: i.Pattern => i.Pattern)(p: i.Pattern): i.Pattern = p match {
     case p:i.Variable    => f(c.Variable(p.name, p.sort))
     case p:i.Application => f(c.Application(p.label, p.args.map(map(f))))
@@ -46,27 +26,22 @@ abstract class MiniKoreTraverse {
     case p:i.Equal       => f(c.Equal   (map(f)(p.p), map(f)(p.q)))
   }
 
-  def fi(p: i.Pattern): Unit
-  def iter(p: i.Pattern): Unit = p match {
-    case p:i.Variable    => fi(p)
-    case p:i.Application => fi(p); p.args.foreach(iter)
-    case p:i.DomainValue => fi(p)
-    case p:i.True        => fi(p)
-    case p:i.False       => fi(p)
-    case p:i.And         => fi(p); iter(p.p); iter(p.q)
-    case p:i.Or          => fi(p); iter(p.p); iter(p.q)
-    case p:i.Not         => fi(p); iter(p.p)
-    case p:i.Implies     => fi(p); iter(p.p); iter(p.q)
-    case p:i.Exists      => fi(p); iter(p.v); iter(p.p)
-    case p:i.ForAll      => fi(p); iter(p.v); iter(p.p)
-    case p:i.Next        => fi(p); iter(p.p)
-    case p:i.Rewrite     => fi(p); iter(p.p); iter(p.q)
-    case p:i.Equal       => fi(p); iter(p.p); iter(p.q)
+  def iter(f: i.Pattern => i.Pattern)(p: i.Pattern): Unit = p match {
+    case p:i.Variable    => f(p)
+    case p:i.Application => f(p); p.args.foreach(iter(f))
+    case p:i.DomainValue => f(p)
+    case p:i.True        => f(p)
+    case p:i.False       => f(p)
+    case p:i.And         => f(p); iter(f)(p.p); iter(f)(p.q)
+    case p:i.Or          => f(p); iter(f)(p.p); iter(f)(p.q)
+    case p:i.Not         => f(p); iter(f)(p.p)
+    case p:i.Implies     => f(p); iter(f)(p.p); iter(f)(p.q)
+    case p:i.Exists      => f(p); iter(f)(p.v); iter(f)(p.p)
+    case p:i.ForAll      => f(p); iter(f)(p.v); iter(f)(p.p)
+    case p:i.Next        => f(p); iter(f)(p.p)
+    case p:i.Rewrite     => f(p); iter(f)(p.p); iter(f)(p.q)
+    case p:i.Equal       => f(p); iter(f)(p.p); iter(f)(p.q)
   }
-
-//  def apply(v: i.Variable): i.Variable = {
-//    case _:i.Variable =>
-//  }
 
   def size(p: i.Pattern): Int = p match {
     case p:i.Leaf => 1
