881412b9739483a8028e8a2e031ced5313523bb9
Daejun Park <daejunpark@gmail.com>
2017-02-01 21:26:27 -0600

    fold

--- org/kframework/minikore/MiniKoreTraverse.scala
+++ org/kframework/minikore/MiniKoreTraverse.scala
@@ -35,17 +35,29 @@ object MiniKoreTraverse {
     case p:i.NodeV[_]    => f(p.constructor(p.v, p.p))
   }
 
-//  def fold[P1 <: i.Pattern, P2 <: i.Pattern, V1 <: i.Variable, V2 <: i.Variable]
-//      (f: P1 => P2, v: V1 => V2, name: String => String, sort: String => String, label: String => String, value: String => String)
-//      (p: P1): P2 = p match {
-//    case p:i.Variable    => f(p.constructor(name(p.name), sort(p.sort)))
-//    case p:i.Application => f(p.constructor(p.label, p.args.map(map(f))))
-//    case p:i.DomainValue => f(p.constructor(p.label, p.value))
-//    case p:i.Node0[_]    => f(p.constructor())
-//    case p:i.Node1[_]    => f(p.constructor(p.p))
-//    case p:i.Node2[_]    => f(p.constructor(p.p, p.q))
-//    case p:i.NodeV[_]    => f(p.constructor(p.v, p.p))
-//  }
+  def fold
+      (c: i.Constructor)
+      (fp: i.Pattern => i.Pattern, fv: i.Variable => i.Variable,
+       fn: String => String, fs: String => String, fl: String => String, fval: String => String)
+      (p: i.Pattern): i.Pattern = {
+    def loop(p: i.Pattern): i.Pattern = fold(c)(fp,fv,fn,fs,fl,fval)(p)
+    p match {
+      case p:i.Variable    => fv(c.Variable(fn(p.name), fs(p.sort)))
+      case p:i.Application => fp(c.Application(fl(p.label), p.args.map(loop)))
+      case p:i.DomainValue => fp(c.DomainValue(fl(p.label), fval(p.value)))
+      case p:i.True        => fp(c.True())
+      case p:i.False       => fp(c.False())
+      case p:i.And         => fp(c.And(loop(p.p), loop(p.q)))
+      case p:i.Or          => fp(c.Or(loop(p.p), loop(p.q)))
+      case p:i.Not         => fp(c.Not(loop(p.p)))
+      case p:i.Implies     => fp(c.Implies(loop(p.p), loop(p.q)))
+      case p:i.Exists      => fp(c.Exists(fv(p.v), loop(p.p)))
+      case p:i.ForAll      => fp(c.ForAll(fv(p.v), loop(p.p)))
+      case p:i.Next        => fp(c.Next(loop(p.p)))
+      case p:i.Rewrite     => fp(c.Rewrite(loop(p.p), loop(p.q)))
+      case p:i.Equal       => fp(c.Equal(loop(p.p), loop(p.q)))
+    }
+  }
 
   def subst(m: Map[i.Variable, i.Pattern])(p: i.Pattern): i.Pattern = {
     def fresh(x: i.Variable): i.Variable = {
