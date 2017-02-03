f979345d7607eb695eef078e34a60d9903234ea5
Manasvi Saxena <msaxena2@illinois.edu>
2017-02-02 18:49:30 -0600

    changes to interface

--- org/kframework/minikore/MiniKore.scala
+++ org/kframework/minikore/MiniKore.scala
@@ -5,108 +5,97 @@ import org.kframework.minikore.{MiniKoreInterface => i}
 import scala.collection._
 
 /** Algebraic data type of MiniKore. */
-object MiniKore extends i.Constructor {
+object MiniKore {
+
+  //Default Implementation
 
   type Attributes = Seq[Pattern]
 
   case class Definition(modules: Seq[Module], att: Attributes)
+
   case class Module(name: String, sentences: Seq[Sentence], att: Attributes)
 
   sealed trait Sentence
+
   case class Import(name: String, att: Attributes) extends Sentence
+
   case class SortDeclaration(sort: String, att: Attributes) extends Sentence
+
   case class SymbolDeclaration(sort: String, label: String, args: Seq[String], att: Attributes) extends Sentence
+
   case class Rule(pattern: Pattern, att: Attributes) extends Sentence
+
   case class Axiom(pattern: Pattern, att: Attributes) extends Sentence
 
   sealed trait Pattern extends i.Pattern
-  case class Variable(name: String, sort: String) extends Pattern with i.Variable {
-    override def constructor = Variable
-  }
+
+  case class Variable(name: String, sort: String) extends Pattern with i.Variable
+
   case class Application(label: String, args: Seq[Pattern]) extends Pattern with i.Application {
-    override def constructor = Application
-  }
-  case class DomainValue(label: String, value: String) extends Pattern with i.DomainValue {
-    override def constructor = DomainValue
-  }
-  //
-  case class True() extends Pattern with i.True {
-    override def constructor = True
-  }
-  case class False() extends Pattern with i.False {
-    override def constructor = False
-  }
-  //
-  case class And    (p: Pattern , q: Pattern) extends Pattern with i.And {
-    override def constructor = And
-  }
-  case class Or     (p: Pattern , q: Pattern) extends Pattern with i.Or {
-    override def constructor = Or
-  }
-  case class Not    (p: Pattern             ) extends Pattern with i.Not {
-    override def constructor = Not
-  }
-  case class Implies(p: Pattern , q: Pattern) extends Pattern with i.Implies {
-    override def constructor = Implies
-  }
-  case class Exists (v: Variable, p: Pattern) extends Pattern with i.Exists {
-    override def constructor = Exists
-  }
-  case class ForAll (v: Variable, p: Pattern) extends Pattern with i.ForAll {
-    override def constructor = ForAll
-  }
-  case class Next   (p: Pattern             ) extends Pattern with i.Next {
-    override def constructor = Next
-  }
-  case class Rewrite(p: Pattern , q: Pattern) extends Pattern with i.Rewrite {
-    override def constructor = Rewrite
-  }
-  case class Equal  (p: Pattern , q: Pattern) extends Pattern with i.Equal {
-    override def constructor = Equal
+    override def children: Seq[Pattern] = args
+
+    override def childrenAsSeq: Seq[Pattern] = children
   }
 
-  // implementation of i.Constructor
+  case class DomainValue(label: String, value: String) extends Pattern with i.DomainValue
 
-  object Variable    extends i.VariableConstructor {
-  //override def apply(name: String, sort: String): i.Variable
+  case class True() extends Pattern with i.True
+
+  case class False() extends Pattern with i.False
+
+  case class And(p: Pattern, q: Pattern) extends Pattern with i.And {
+    override def children: (Pattern, Pattern) = (p, q)
+
+    override def childrenAsSeq: Seq[Pattern] = Seq(p, q)
   }
-  object Application extends i.ApplicationConstructor {
-    override def apply(label: String, args: Seq[i.Pattern]): i.Application = Application(label, args.asInstanceOf[Seq[Pattern]])
+
+  case class Or(p: Pattern, q: Pattern) extends Pattern with i.Or {
+    override def children: (Pattern, Pattern) = (p, q)
+
+    override def childrenAsSeq: Seq[Pattern] = Seq(p, q)
   }
-  object DomainValue extends i.DomainValueConstructor {
-  //override def apply(label: String, value: String): i.DomainValue
+
+  case class Not(p: Pattern) extends Pattern with i.Not {
+    override def children: Pattern = p
+
+    override def childrenAsSeq: Seq[Pattern] = Seq(p)
   }
-  object True        extends i.Node0Constructor[i.True] {
-  //override def apply(): i.True
+
+  case class Implies(p: Pattern, q: Pattern) extends Pattern with i.Implies {
+    override def children: (Pattern, Pattern) = (p, q)
+
+    override def childrenAsSeq: Seq[Pattern] = Seq(p, q)
   }
-  object False       extends i.Node0Constructor[i.False] {
-  //override def apply(): i.False
+
+  case class Exists(v: Variable, p: Pattern) extends Pattern with i.Exists {
+    override def children: (Variable, Pattern) = (v, p)
+
+    override def childrenAsSeq: Seq[Pattern] = Seq(v, p)
+
   }
-  object And         extends i.Node2Constructor[i.And] {
-    override def apply(p: i.Pattern, q: i.Pattern): i.And = And(p.asInstanceOf[Pattern], q.asInstanceOf[Pattern])
+
+  case class ForAll(v: Variable, p: Pattern) extends Pattern with i.ForAll {
+    override def children: (Variable, Pattern) = (v, p)
+
+    override def childrenAsSeq: Seq[Pattern] = Seq(v, p)
   }
-  object Or          extends i.Node2Constructor[i.Or] {
-    override def apply(p: i.Pattern, q: i.Pattern): i.Or = Or(p.asInstanceOf[Pattern], q.asInstanceOf[Pattern])
+
+  case class Next(p: Pattern) extends Pattern with i.Next {
+    override def children: Pattern = p
+
+    override def childrenAsSeq: Seq[Pattern] = Seq(p)
   }
-  object Not         extends i.Node1Constructor[i.Not] {
-    override def apply(p: i.Pattern): i.Not = Not(p.asInstanceOf[Pattern])
+
+  case class Rewrite(p: Pattern, q: Pattern) extends Pattern with i.Rewrite {
+    override def children: (Pattern, Pattern) = (p, q)
+
+    override def childrenAsSeq: Seq[Pattern] = Seq(p, q)
   }
-  object Implies     extends i.Node2Constructor[i.Implies] {
-    override def apply(p: i.Pattern, q: i.Pattern): i.Implies = Implies(p.asInstanceOf[Pattern], q.asInstanceOf[Pattern])
+
+  case class Equals(p: Pattern, q: Pattern) extends Pattern with i.Equals {
+    override def children: (Pattern, Pattern) = (p, q)
+
+    override def childrenAsSeq: Seq[Pattern] = Seq(p, q)
   }
-  object Exists      extends i.NodeVConstructor[i.Exists] {
-    override def apply(v: i.Variable, p: i.Pattern): i.Exists = Exists(v.asInstanceOf[Variable], p.asInstanceOf[Pattern])
-  }
-  object ForAll      extends i.NodeVConstructor[i.ForAll] {
-    override def apply(v: i.Variable, p: i.Pattern): i.ForAll = ForAll(v.asInstanceOf[Variable], p.asInstanceOf[Pattern])
-  }
-  object Next        extends i.Node1Constructor[i.Next] {
-    override def apply(p: i.Pattern): i.Next = Next(p.asInstanceOf[Pattern])
-  }
-  object Rewrite     extends i.Node2Constructor[i.Rewrite] {
-    override def apply(p: i.Pattern, q: i.Pattern): i.Rewrite = Rewrite(p.asInstanceOf[Pattern], q.asInstanceOf[Pattern])
-  }
-  object Equal       extends i.Node2Constructor[i.Equal] {
-    override def apply(p: i.Pattern, q: i.Pattern): i.Equal = Equal(p.asInstanceOf[Pattern], q.asInstanceOf[Pattern])
-  }
+
 }
--- org/kframework/minikore/MiniKoreInterface.scala
+++ org/kframework/minikore/MiniKoreInterface.scala
@@ -11,15 +11,23 @@ object MiniKoreInterface {
     def apply: A => T
 
     def children: C
+
+    def childrenAsSeq: Seq[Pattern]
   }
 
   // A Pattern Such as True/False takes no arguments, hence types of Children is None, Constructor's arguments is None.
-  sealed trait Node0[T] extends Node[T, None.type, None.type]
+  sealed trait Node0[T] extends Node[T, None.type, None.type] {
+    override def children = None
+
+    override def childrenAsSeq = Seq.empty[Pattern]
+  }
 
   // A Pattern such as Not, takes 1 argument, and the type of the children is the same.
   sealed trait Node1[T, C <: Pattern] extends Node[T, C, C]
 
+
   sealed trait Node2[T, C1 <: Pattern, C2 <: Pattern] extends Node[T, (C1, C2), (C1, C2)]
+
 
   // An Application Node takes a Label Type, and a Children type. The type of the constructor is a composite type.
   sealed trait NodeApply[T, Label, Children] extends Node[T, Children, (Label, Children)]
