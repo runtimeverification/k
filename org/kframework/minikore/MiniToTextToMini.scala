07f4548180e2cfbe5bc3299c24d04975265f5aac
Manasvi Saxena <msaxena2@illinois.edu>
2017-02-19 11:11:19 -0600

    made parser parametric over builder

--- org/kframework/minikore/MiniToTextToMini.scala
+++ org/kframework/minikore/MiniToTextToMini.scala
@@ -4,14 +4,19 @@ import org.apache.commons.io.FileUtils
 
 import org.kframework.minikore.MiniKore.Definition
 
+import org.kframework.minikore.Builders
+
 // TODO(Daejun): drop this file
 
 object MiniToTextToMini {
+
+  def defaultImplemntation: Builders = DefaultBuilders.build
+
   def apply(d: Definition): Definition = {
     val text = MiniToText.apply(d)
     val file = new java.io.File("/tmp/x")
     FileUtils.writeStringToFile(file, text)
-    val d2 = new TextToMini().parse(file)
+    val d2 = new TextToMini(defaultImplemntation).parse(file)
     val text2 = MiniToText.apply(d2)
     assert(d == d2)
     d2
