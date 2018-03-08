dcfd4abb8604e7d7e8f97195c28bcc4fa6d1289a
Traian Florin Serbanuta <traian.serbanuta@gmail.com>
2018-03-09 00:58:29 +0200

    Escaping back ticks and backslashes in Kast

--- kore/src/main/scala/org/kframework/unparser/Unparse.scala
+++ kore/src/main/scala/org/kframework/unparser/Unparse.scala
@@ -30,11 +30,14 @@ object ToKast {
         && l.name != "#token" && l.name != "#klabel") {
       l.name
     } else if (inParen) {
-      " `"+l.name+'`'
+      " `"+ escapeBackTicksAndSlashes(l.name) +'`'
     } else {
-      '`'+l.name+'`'
+      '`'+ escapeBackTicksAndSlashes(l.name) +'`'
     }
   }
+
+  def escapeBackTicksAndSlashes(str: String) : String =
+    str.foldRight[List[Char]] (Nil) { (c, r) => if (c == '`' || c == '\\') '\\' :: c :: r else c :: r }.mkString
 
   /**
    * Recursive worker function for printing KAST terms.
