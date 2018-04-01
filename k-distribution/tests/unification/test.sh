803c7b4b560216b8fe8c5d1f082b928a98e1be1b
Everett Hildenbrandt <hildenb2@illinois.edu>
2018-04-01 00:11:15 -0600

    k-distribution/tests/unification: adding tests for `#matches` function

--- k-distribution/tests/unification/test.sh
+++ k-distribution/tests/unification/test.sh
@@ -4,7 +4,7 @@ set -e
 
 KOMPILE=../../target/release/k/bin/kompile
 KRUN=../../target/release/k/bin/krun
-TESTS=(basic)
+TESTS="basic match"
 
 tmp_out=$(mktemp output.XXXXX)
 trap "rm -rf $tmp_out" INT TERM EXIT
@@ -14,5 +14,5 @@ $KOMPILE --backend java test.k
 for test in $TESTS; do
     echo "== running: $test.test"
     $KRUN $test.test > $tmp_out
-    git diff --no-index $test.out $tmp_out
+    git --no-pager diff --no-index $test.out $tmp_out
 done
