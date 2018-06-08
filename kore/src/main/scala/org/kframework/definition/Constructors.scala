be282cc9851532892ee3e4d5215050318ded60a4
dwightguth <dwight.guth@runtimeverification.com>
2018-06-08 12:14:05 -0500

    Fix regression in java backend about sort of poly productions (#95)
    * fix bug in sort computation in java backend
    
    * add regression test for poly sort in java backend

--- kore/src/main/scala/org/kframework/definition/Constructors.scala
+++ kore/src/main/scala/org/kframework/definition/Constructors.scala
@@ -31,6 +31,8 @@ object Constructors {
   def Production(sort: Sort, items: Seq[ProductionItem], att: attributes.Att) = definition.Production(sort, items, att)
   def Production(klabel: KLabel, sort: Sort, items: Seq[ProductionItem]) = definition.Production(klabel, sort, items)
   def Production(klabel: KLabel, sort: Sort, items: Seq[ProductionItem], att: attributes.Att) = definition.Production(klabel, sort, items, att)
+  def Production(klabel: Option[KLabel], sort: Sort, items: Seq[ProductionItem]) = definition.Production(klabel, sort, items, Att.empty)
+  def Production(klabel: Option[KLabel], sort: Sort, items: Seq[ProductionItem], att: attributes.Att) = definition.Production(klabel, sort, items, att)
 
   def Terminal(s: String) = definition.Terminal(s)
   def NonTerminal(sort: Sort) = definition.NonTerminal(sort, None)
