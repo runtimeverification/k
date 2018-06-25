a51d3b30224a45215d26f8098aeaccb58cea389c
Everett Hildenbrandt <hildenb2@illinois.edu>
2018-06-25 15:58:55 -0600

    kernel/.../{KProve,KProveOptions}: option `--debugg` to log proof information

--- kernel/src/main/java/org/kframework/kprove/KProve.java
+++ kernel/src/main/java/org/kframework/kprove/KProve.java
@@ -3,6 +3,7 @@ package org.kframework.kprove;
 
 import com.google.inject.Inject;
 import org.apache.commons.io.FilenameUtils;
+import org.kframework.Debugg;
 import org.kframework.attributes.Att;
 import org.kframework.compile.*;
 import org.kframework.definition.*;
@@ -56,7 +57,16 @@ public class KProve {
         Rewriter rewriter = rewriterGenerator.apply(compiled._1().mainModule());
         Module specModule = compiled._2();
 
-        K results = rewriter.prove(specModule);
+        Debugg.init(files, specModule, compiled._1().getModule("LANGUAGE-PARSING").get(), kprint, options.debugg);
+        Debugg.log("spec " + options.specFile(files).getAbsolutePath());
+        K results;
+        try {
+            results = rewriter.prove(specModule);
+        } catch (Exception e) {
+            Debugg.log(Debugg.LogEvent.CRASH);
+            Debugg.close();
+            throw e;
+        }
         int exit;
         if (results instanceof KApply) {
             KApply kapp = (KApply) results;
@@ -65,6 +75,7 @@ public class KProve {
             exit = 1;
         }
         kprint.prettyPrint(compiled._1(), compiled._1().getModule("LANGUAGE-PARSING").get(), compiledDefinition.kompileOptions, s -> kprint.outputFile(s), results);
+        Debugg.close();
         return exit;
     }
 
