1555322666acb3c33a6b9fccfd3c94d2b38c7cb8
Dwight Guth <dwight.guth@runtimeverification.com>
2018-09-24 17:31:26 -0500

    fix fresh constants for kore

--- kernel/src/main/java/org/kframework/compile/ResolveFreshConstants.java
+++ kernel/src/main/java/org/kframework/compile/ResolveFreshConstants.java
@@ -1,24 +1,40 @@
 // Copyright (c) 2015-2018 K Team. All Rights Reserved.
 package org.kframework.compile;
 
+import org.kframework.attributes.Att;
 import org.kframework.builtin.BooleanUtils;
+import org.kframework.builtin.KLabels;
 import org.kframework.builtin.Sorts;
 import org.kframework.definition.Context;
 import org.kframework.definition.Definition;
 import org.kframework.definition.Module;
+import org.kframework.definition.NonTerminal;
+import org.kframework.definition.Production;
+import org.kframework.definition.ProductionItem;
 import org.kframework.definition.Rule;
 import org.kframework.definition.Sentence;
 import org.kframework.kore.K;
 import org.kframework.kore.KApply;
+import org.kframework.kore.KLabel;
+import org.kframework.kore.KToken;
 import org.kframework.kore.KVariable;
 import org.kframework.kore.Sort;
+import org.kframework.kore.TransformK;
 import org.kframework.kore.VisitK;
+import org.kframework.parser.concrete2kore.ParseInModule;
+import org.kframework.parser.concrete2kore.generator.RuleGrammarGenerator;
 import org.kframework.utils.StringUtil;
 import org.kframework.utils.errorsystem.KEMException;
 import scala.collection.Set;
+import scala.Option;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.kframework.Collections.*;
 import static org.kframework.definition.Constructors.*;
@@ -27,10 +43,14 @@ import static org.kframework.kore.KORE.*;
 public class ResolveFreshConstants {
 
     private final Definition def;
+    private final boolean kore;
+    private Module m;
     private java.util.Set<KVariable> freshVars = new HashSet<>();
+    private Map<KVariable, Integer> offsets = new HashMap<>();
 
     private void reset() {
         freshVars.clear();
+        offsets.clear();
     }
 
     private Rule resolve(Rule rule) {
@@ -38,6 +58,31 @@ public class ResolveFreshConstants {
         analyze(rule.body());
         analyze(rule.requires());
         analyze(rule.ensures());
+        finishAnalysis();
+        if (kore) {
+            Rule withFresh = Rule(
+                    addFreshCell(transform(rule.body())),
+                    transform(rule.requires()),
+                    transform(rule.ensures()),
+                    rule.att());
+            if (rule.att().contains("initializer")) {
+                K left = RewriteToTop.toLeft(withFresh.body());
+                if (left instanceof KApply) {
+                    KApply kapp = (KApply) left;
+                    if (kapp.klabel().equals(KLabels.INIT_GENERATED_TOP_CELL)) {
+                        KApply right = (KApply)RewriteToTop.toRight(withFresh.body());
+                        List<K> items = new ArrayList<>(right.items());
+                        items.add(KApply(KLabels.INIT_GENERATED_COUNTER_CELL));
+                        return Rule(
+                                KRewrite(left, KApply(right.klabel(), immutable(items))),
+                                withFresh.requires(),
+                                withFresh.ensures(),
+                                withFresh.att());
+                    }
+                }
+            }
+            return withFresh;
+        }
         return Rule(
                 rule.body(),
                 addSideCondition(rule.requires()),
@@ -55,6 +100,13 @@ public class ResolveFreshConstants {
                 super.apply(k);
             }
         }.apply(term);
+    }
+
+    private void finishAnalysis() {
+        int i = 0;
+        for (KVariable v : freshVars) {
+            offsets.put(v, i++);
+        }
     }
 
     private K addSideCondition(K requires) {
@@ -77,14 +129,68 @@ public class ResolveFreshConstants {
         }
     }
 
+    private static KVariable FRESH = KVariable("!Fresh", Att.empty().add(Sort.class, Sorts.Int()));
+
+    private K transform(K term) {
+        return new TransformK() {
+            @Override
+            public K apply(KVariable k) {
+                if (freshVars.contains(k)) {
+                    Optional<Sort> s = k.att().getOptional(Sort.class);
+                    if (!s.isPresent()) {
+                        throw KEMException.compilerError("Fresh constant used without a declared sort.", k);
+                    }
+                    Option<KLabel> lbl = m.freshFunctionFor().get(s.get());
+                    if (!lbl.isDefined()) {
+                        throw KEMException.compilerError("No fresh generator defined for sort " + s, k);
+                    }
+                    return KApply(lbl.get(), KApply(KLabel("_+Int_"), FRESH, KToken(offsets.get(k).toString(), Sorts.Int())));
+                }
+                return super.apply(k);
+            }
+        }.apply(term);
+    }
+
+    private K addFreshCell(K body) {
+        if (freshVars.size() == 0) {
+            return body;
+        }
+        KApply cellTerm = IncompleteCellUtils.make(KLabels.GENERATED_COUNTER_CELL, false, KRewrite(FRESH, KApply(KLabel("_+Int_"), FRESH, KToken(Integer.toString(freshVars.size()), Sorts.Int()))), false);
+        return KApply(KLabels.CELLS, body, cellTerm);
+    }
+
     private Context resolve(Context context) {
         reset();
         analyze(context.body());
         analyze(context.requires());
+        finishAnalysis();
+        if (kore) {
+            return new Context(
+                    addFreshCell(transform(context.body())),
+                    transform(context.requires()),
+                    context.att());
+        }
         return new Context(
                 context.body(),
                 addSideCondition(context.requires()),
                 context.att());
+    }
+
+    private Production resolve(Production prod) {
+        if (kore && prod.klabel().isDefined() && prod.klabel().get() == KLabels.GENERATED_TOP_CELL) {
+            List<ProductionItem> pis = stream(prod.items()).collect(Collectors.toCollection(ArrayList::new));
+            int idx = 0;
+            int i = 0;
+            for (ProductionItem pi : pis) {
+                if (pi instanceof NonTerminal) {
+                    idx = i;
+                }
+                i++;
+            }
+            pis.add(idx, NonTerminal(Sorts.GeneratedCounterCell()));
+            return Production(prod.klabel().get(), prod.sort(), immutable(pis), prod.att());
+        }
+        return prod;
     }
 
     private Sentence resolve(Sentence s) {
@@ -92,21 +198,48 @@ public class ResolveFreshConstants {
             return resolve((Rule) s);
         } else if (s instanceof Context) {
             return resolve((Context) s);
-        } else {
-            return s;
+        } else if (s instanceof Production) {
+            return resolve((Production) s);
         }
+        return s;
     }
 
-    public ResolveFreshConstants(Definition def) {
+    public ResolveFreshConstants(Definition def, boolean kore) {
         this.def = def;
+        this.kore = kore;
     }
 
     public Module resolve(Module m) {
+        this.m = m;
         Set<Sentence> sentences = map(this::resolve, m.localSentences());
         if (sentences.equals(m.localSentences())) {
             return m;
         }
-        return Module(m.name(), add(def.getModule("K-REFLECTION").get(), m.imports()), sentences, m.att());
+        KToken counterCellLabel = KToken("generatedCounter", Sort("#CellName"));
+        KApply freshCell = KApply(KLabel("#configCell"), counterCellLabel, KApply(KLabel("#cellPropertyListTerminator")), KToken("0", Sorts.Int()), counterCellLabel);
+        if (m.equals(def.mainModule()) && kore) {
+            if (!m.definedKLabels().contains(KLabels.GENERATED_TOP_CELL)) {
+                RuleGrammarGenerator gen = new RuleGrammarGenerator(def);
+                ParseInModule mod = RuleGrammarGenerator.getCombinedGrammar(gen.getConfigGrammar(m), true);
+                ConfigurationInfoFromModule configInfo = new ConfigurationInfoFromModule(m);
+                Sort topCellSort = configInfo.topCell();
+                KLabel topCellLabel = configInfo.getCellLabel(topCellSort);
+                Production prod = m.productionsFor().apply(topCellLabel).head();
+                KToken cellName = KToken(prod.att().get("cellName"), Sort("#CellName"));
+
+                KToken topCellToken = KToken(KLabels.GENERATED_TOP_CELL_NAME, Sort("#CellName"));
+                K generatedTop = KApply(KLabel("#configCell"), topCellToken, KApply(KLabel("#cellPropertyListTerminator")), KApply(KLabels.CELLS, KApply(KLabel("#externalCell"), cellName), freshCell), topCellToken);
+                Set<Sentence> newSentences = GenerateSentencesFromConfigDecl.gen(generatedTop, BooleanUtils.TRUE, Att.empty(), mod.getExtensionModule());
+                sentences = (Set<Sentence>) sentences.$bar(newSentences);
+            }
+        }
+        if (kore && m.localKLabels().contains(KLabels.GENERATED_TOP_CELL)) {
+            RuleGrammarGenerator gen = new RuleGrammarGenerator(def);
+            ParseInModule mod = RuleGrammarGenerator.getCombinedGrammar(gen.getConfigGrammar(m), true);
+            Set<Sentence> newSentences = GenerateSentencesFromConfigDecl.gen(freshCell, BooleanUtils.TRUE, Att.empty(), mod.getExtensionModule());
+            sentences = (Set<Sentence>) sentences.$bar(newSentences);
+        }
+        return Module(m.name(), kore ? m.imports() : add(def.getModule("K-REFLECTION").get(), m.imports()), sentences, m.att());
     }
 }
 
