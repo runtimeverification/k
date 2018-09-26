f67a853f1a41198bcf0da1dddd10d648c2877980
Denis Bogdanas <denis.bogdanas@gmail.com>
2018-09-26 19:38:17 +0300

    Java backend: minor optimization: avoid multiple conversion to toString() later.

--- java-backend/src/main/java/org/kframework/backend/java/symbolic/SMTOperations.java
+++ java-backend/src/main/java/org/kframework/backend/java/symbolic/SMTOperations.java
@@ -46,7 +46,7 @@ public class SMTOperations {
             constraint.globalContext().profiler.queryBuildTimer.start();
             CharSequence query;
             try {
-                query = KILtoSMTLib.translateConstraint(constraint);
+                query = KILtoSMTLib.translateConstraint(constraint).toString();
             } finally {
                 constraint.globalContext().profiler.queryBuildTimer.stop();
             }
@@ -73,7 +73,7 @@ public class SMTOperations {
                 left.globalContext().profiler.queryBuildTimer.start();
                 CharSequence query;
                 try {
-                    query = KILtoSMTLib.translateImplication(left, right, existentialQuantVars);
+                    query = KILtoSMTLib.translateImplication(left, right, existentialQuantVars).toString();
                 } finally {
                     left.globalContext().profiler.queryBuildTimer.stop();
                 }
