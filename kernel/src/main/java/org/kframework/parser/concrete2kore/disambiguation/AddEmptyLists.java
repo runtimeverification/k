681b03455035d59362956ed7a9fc2a25c6fbf65f
Dwight Guth <dwight.guth@runtimeverification.com>
2018-11-05 11:01:06 -0600

    fix bug where list terminator is not least sort

--- kernel/src/main/java/org/kframework/parser/concrete2kore/disambiguation/AddEmptyLists.java
+++ kernel/src/main/java/org/kframework/parser/concrete2kore/disambiguation/AddEmptyLists.java
@@ -105,7 +105,13 @@ public class AddEmptyLists extends SetsGeneralTransformer<ParseFailedException, 
                         return new Tuple2<>(Left.apply(Sets.newHashSet(new ParseFailedException(ex))), warnings);
                     }
                     UserList ul = lists.get(least.iterator().next()).get(0);
-                    TermCons terminator = TermCons.apply(ConsPStack.empty(), ul.pTerminator, child.location(), child.source());
+                    Set<Sort> leastTerm = subsorts.minimal(stream(listSorts).filter(s -> subsorts.lessThanEq(s, expectedSort)).collect(Collectors.toList()));
+                    if (leastTerm.size() != 1) {
+                        KException ex = new KException(KException.ExceptionType.ERROR, KException.KExceptionGroup.INNER_PARSER, "List terminator for overloaded term does not have a least sort. Possible sorts: " + leastTerm, tc.source().orElse(null), tc.location().orElse(null));
+                        return new Tuple2<>(Left.apply(Sets.newHashSet(new ParseFailedException(ex))), warnings);
+                    }
+                    UserList ulTerm = lists.get(leastTerm.iterator().next()).get(0);
+                    TermCons terminator = TermCons.apply(ConsPStack.empty(), ulTerm.pTerminator, child.location(), child.source());
                     // TermCons with PStack requires the elements to be in the reverse order
                     TermCons newTc = TermCons.apply(ConsPStack.from(Arrays.asList(terminator, child)), ul.pList, child.location(), child.source());
                     newItems.add(newTc);
