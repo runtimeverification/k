afdf6bf554ef5783f5e9d08694e1a52c624849b7
Jan Tusil <jenda.tusil@gmail.com>
2018-12-05 14:00:43 -0600

    local kserver

--- kernel/src/main/java/org/kframework/kserver/KServerOptions.java
+++ kernel/src/main/java/org/kframework/kserver/KServerOptions.java
@@ -16,7 +16,7 @@ public class KServerOptions {
     @Parameter(names={"--port", "-p"}, description="The port to start the server on.")
     public int port = 2113;
 
-    @Parameter(names={"--socket"}, description="The socket to start the server on.")
+    @Parameter(names={"--socket"}, description="The directory to put the unix domain socket in.")
     public String socket = null;
 
 }
