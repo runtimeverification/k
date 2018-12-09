19aac668ffb36191310c5d32c73ed0f7c78b8ae5
Denis Bogdanas <denis.bogdanas@gmail.com>
2018-12-09 10:49:45 +0200

    Logging: improved Z3 result message

--- java-backend/src/main/java/org/kframework/backend/java/util/FormulaContext.java
+++ java-backend/src/main/java/org/kframework/backend/java/util/FormulaContext.java
@@ -59,27 +59,23 @@ public class FormulaContext {
     }
 
     public void printImplication(ConjunctiveFormula left, ConjunctiveFormula right, Boolean proved, boolean cached) {
+        String cachedMsg = cached ? " (cached result)" : "";
         if (proved) {
-            System.err.format("\nZ3 Implication (%s) RHS proved:\n%s\n", kind.label, right.toStringMultiline());
+            System.err.format("\nZ3 Implication (%s) RHS proved%s:\n%s\n", kind.label, cachedMsg, right.toStringMultiline());
         } else {
-            System.err.format("\nZ3 Implication (%s) failed:\n%s\n  implies \n%s\n",
-                    kind.label, left.toStringMultiline(), right.toStringMultiline());
-        }
-        if (cached) {
-            System.err.println("cached result");
+            System.err.format("\nZ3 Implication (%s) failed%s:\n%s\n  implies \n%s\n",
+                    kind.label, cachedMsg, left.toStringMultiline(), right.toStringMultiline());
         }
         RuleSourceUtil.printRuleAndSource(rule);
         System.err.println("==================================");
     }
 
     public void printUnsat(ConjunctiveFormula formula, boolean unsat, boolean cached) {
+        String cachedMsg = cached ? " (cached result)" : "";
         if (unsat) {
-            System.err.format("\nZ3 Constraint (%s) is unsat:\n%s\n", kind.label, formula.toStringMultiline());
+            System.err.format("\nZ3 Constraint (%s) is unsat%s:\n%s\n", kind.label, cachedMsg, formula.toStringMultiline());
         } else {
-            System.err.format("\nZ3 Constraint (%s) is assumed sat:\n%s\n", kind.label, formula.toStringMultiline());
-        }
-        if (cached) {
-            System.err.println("cached result");
+            System.err.format("\nZ3 Constraint (%s) is assumed sat%s:\n%s\n", kind.label, cachedMsg, formula.toStringMultiline());
         }
         RuleSourceUtil.printRuleAndSource(rule);
         System.err.println("==================================");
