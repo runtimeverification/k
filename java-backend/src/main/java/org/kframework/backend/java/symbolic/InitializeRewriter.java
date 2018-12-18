e4ea969cf61186ecaaaca83c67e3e8c64cda2a4c
Denis Bogdanas <denis.bogdanas@gmail.com>
2018-12-18 15:10:52 +0200

    Logging: message for each rule being pre-processed.

--- java-backend/src/main/java/org/kframework/backend/java/symbolic/InitializeRewriter.java
+++ java-backend/src/main/java/org/kframework/backend/java/symbolic/InitializeRewriter.java
@@ -391,6 +391,11 @@ public class InitializeRewriter implements Function<org.kframework.definition.De
             }
 
             public org.kframework.backend.java.kil.Rule evaluateRule(org.kframework.backend.java.kil.Rule rule) {
+                if (termContext.global().javaExecutionOptions.logBasic) {
+                    System.err.println("Pre-processing rule:");
+                    RuleSourceUtil.printRuleAndSource(rule);
+                    System.err.println("==================================");
+                }
                 ConjunctiveFormula constraint = getEvaluatedConstraint(rule);
                 if (constraint.isFalseExtended()) {
                     StringBuilder sb = new StringBuilder();
