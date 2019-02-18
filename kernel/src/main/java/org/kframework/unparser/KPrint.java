9adf2417036eb2946a15c19e16ed218f60fca4a4
Martin Lundfall <martin.lundfall@protonmail.com>
2019-02-18 18:22:35 +0100

    kernel/.../KPrint: make tokenize and omit KApply instead of KToken

--- kernel/src/main/java/org/kframework/unparser/KPrint.java
+++ kernel/src/main/java/org/kframework/unparser/KPrint.java
@@ -223,7 +223,7 @@ public class KPrint {
         if (! termSort.isEmpty()) {
             finalSort = termSort.get();
         }
-        return KToken(kapp.klabel().name(), finalSort);
+        return KApply(kapp.klabel(), KToken(kapp.klabel().name(), finalSort));
     }
 
     private K tokenizeTerm(Module mod, KApply kapp) {
@@ -234,7 +234,7 @@ public class KPrint {
         if (! termSort.isEmpty()) {
             finalSort = termSort.get();
         }
-        return KToken(tokenizedTerm, finalSort);
+        return KApply(kapp.klabel(), KToken(tokenizedTerm, finalSort));
     }
 
     private static K toKASTTerm(Module mod, KApply kapp) {
