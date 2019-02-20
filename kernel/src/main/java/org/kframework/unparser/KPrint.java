7a63355ed85c90c145236d78f618ad9c0ce66623
Everett Hildenbrandt <hildenb2@illinois.edu>
2019-02-20 15:52:43 -0700

    kernel/.../KPrint: recurse term omission to subterms when appropriate

--- kernel/src/main/java/org/kframework/unparser/KPrint.java
+++ kernel/src/main/java/org/kframework/unparser/KPrint.java
@@ -166,7 +166,7 @@ public class KPrint {
                      : options.tokenizedKLabels.contains(name) ? tokenizeTerm(mod, orig)
                      : options.flattenedKLabels.contains(name) ? flattenTerm(mod, orig)
                      : options.tokastKLabels.contains(name)    ? toKASTTerm(mod, orig)
-                     : orig ;
+                     : super.apply(orig) ;
             }
         }.apply(alphaRenamed);
     }
