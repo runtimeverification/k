655f6297b92900b636fb43fe5459b38c54188152
Denis Bogdanas <denis.bogdanas@gmail.com>
2019-03-05 11:39:32 +0200

    Deleted all aux labels, no longer needed.

--- kore/src/main/java/org/kframework/builtin/KLabels.java
+++ kore/src/main/java/org/kframework/builtin/KLabels.java
@@ -64,6 +64,4 @@ public class KLabels {
     public static final KLabel MAP_KEYS = KLabel("keys");
 
     public static final String INJ = "inj";
-
-    public static final KLabel GENERATED_BOUNDARY_CELL = KLabel("<generatedBoundary>");
 }
