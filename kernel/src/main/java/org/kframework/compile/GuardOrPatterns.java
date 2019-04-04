dc648b9286db60c15e0d325a30d373347e1c9f81
dwightguth <dwight.guth@runtimeverification.com>
2019-04-04 13:06:10 -0500

    Bug fixes for LLVM Backend (#388)
    * rename !Fresh to _Fresh
    
    * fix bug in transformation of initializer
    
    * add ML-SYNTAX to BASIC-K
    
    * fix bug in addSortInjections
    
    * add sort info to #as generated by GuardOrPatterns
    
    * better fix for sort in GuardOrPatterns
    
    * fix stack overflow error
    
    * always call kem.print()
    
    * better error message in addSortInjections
    
    * temp fix for issue with assertionerror in AddSortInjections
    
    * add better diagnostic for klabel variable crash
    
    * delete old unnecessary code in computeDependencies specific to ocaml
    backend
    
    * fix NoSuchElementException
    
    * set sort of .K to K so that we can statically detect type violations in
    function return types
    
    * poly attribute on INJ_PROD
    
    * another try at INJ_PROD
    
    * fix poly att
    
    * update llvm backend submodule
    
    * update llvm backend submodule again
    
    * fix everett's comment

--- kernel/src/main/java/org/kframework/compile/GuardOrPatterns.java
+++ kernel/src/main/java/org/kframework/compile/GuardOrPatterns.java
@@ -2,7 +2,9 @@
 package org.kframework.compile;
 
 import org.kframework.builtin.KLabels;
+import org.kframework.builtin.Sorts;
 import org.kframework.definition.Context;
+import org.kframework.definition.Module;
 import org.kframework.definition.Production;
 import org.kframework.definition.Rule;
 import org.kframework.definition.Sentence;
@@ -16,44 +18,49 @@ import static org.kframework.kore.KORE.*;
 public class GuardOrPatterns {
 
     private Set<KVariable> vars = new HashSet<>();
+    private final boolean kore;
+
+    public GuardOrPatterns(boolean kore) {
+      this.kore = kore;
+    }
 
     void resetVars() {
         vars.clear();
     }
 
-    private Rule resolve(Rule rule) {
+    private Rule resolve(Module m, Rule rule) {
         resetVars();
         gatherVars(rule.body());
         gatherVars(rule.requires());
         gatherVars(rule.ensures());
         return new Rule(
-                transform(rule.body()),
-                transform(rule.requires()),
-                transform(rule.ensures()),
+                transform(rule.body(), m),
+                transform(rule.requires(), m),
+                transform(rule.ensures(), m),
                 rule.att());
     }
 
-    private Context resolve(Context context) {
+    private Context resolve(Module m, Context context) {
         resetVars();
         gatherVars(context.body());
         gatherVars(context.requires());
         return new Context(
-                transform(context.body()),
-                transform(context.requires()),
+                transform(context.body(), m),
+                transform(context.requires(), m),
                 context.att());
     }
 
-    public K resolveK(K k) {
+    public K resolveK(Module m, K k) {
         resetVars();;
         gatherVars(k);
-        return transform(k);
+        return transform(k, m);
     }
 
-    public synchronized Sentence resolve(Sentence s) {
+    public synchronized Sentence resolve(Module m, Sentence s) {
         if (s instanceof Rule) {
-            return resolve((Rule) s);
+            return resolve(m, (Rule) s);
         } else if (s instanceof Context) {
-            return resolve((Context) s);
+            return resolve(m, (Context) s);
         } else {
             return s;
         }
@@ -69,12 +76,17 @@ public class GuardOrPatterns {
         }.apply(term);
     }
 
-    K transform(K term) {
+    K transform(K term, Module m) {
         return new TransformK() {
             @Override
             public K apply(KApply k) {
               if (k.klabel().equals(KLabels.ML_OR)) {
-                return KAs(k, newDotVariable());
+                if (kore) {
+                  AddSortInjections inj = new AddSortInjections(m);
+                  return KAs(k, newDotVariable(inj.sort(k, Sorts.K())));
+                } else {
+                  return KAs(k, newDotVariable(k.att().get(Production.class).sort()));
+                }
               }
               return super.apply(k);
             }
@@ -92,10 +104,10 @@ public class GuardOrPatterns {
     }
 
     private int counter = 0;
-    KVariable newDotVariable() {
+    KVariable newDotVariable(Sort s) {
         KVariable newLabel;
         do {
-            newLabel = KVariable("_" + (counter++), Att().add("anonymous"));
+            newLabel = KVariable("_" + (counter++), Att().add("anonymous").add(Sort.class, s));
         } while (vars.contains(newLabel));
         vars.add(newLabel);
         return newLabel;
