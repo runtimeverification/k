01886d021ad3b278e4bf59c899fae663a909385b
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2019-05-26 11:53:20 -0600

    k-distribution/gen-specs.py: allow case analysis and optimizations

--- k-distribution/src/main/scripts/bin/gen-specs.py
+++ k-distribution/src/main/scripts/bin/gen-specs.py
@@ -5,6 +5,12 @@ import sys
 import re
 import configparser
 
+def write_spec_file(output_dir, spec_name, spec_defn):
+    output_file = output_dir + '/' + spec_name + '-spec.k'
+    with open(output_file, 'w') as spec_out:
+        spec_out.write(spec_defn)
+        print('Wrote spec file: ' + output_file)
+
 # TODO: for Python 3.5 or higher: z = {**x, **y}
 def merge_two_dicts(x, y):
     z = dict(x)
@@ -46,80 +52,86 @@ def gen_spec_rule(rule_template, pgm_config, rule_config, rule_name, trusted = F
     rule_spec = rule_template
     for config in [ rule_config , pgm_config ]:
         rule_spec = subst_all(rule_spec, config)
-    rule_spec = subst(rule_spec, "rulename", rule_name)
+    rule_spec = subst(rule_spec, 'rulename', rule_name)
     if trusted:
-        rule_spec += "    [trusted]"
+        rule_spec += '    [trusted]\n'
     return rule_spec
 
-def gen_spec_rules(rule_template, spec_config, rule_name_list):
+def gen_spec_rules(rule_template, spec_config, rule_name_list, trusted = False):
     pgm_config = spec_config['pgm']
     rule_spec_list = []
-    trusted_rule_names = set([])
+    lemma_names = set([])
     for rname in rule_name_list:
         rule_config = inherit_get(spec_config, rname)
-        rule_spec = gen_spec_rule(rule_template, pgm_config, rule_config, rname)
+        rule_spec = gen_spec_rule(rule_template, pgm_config, rule_config, rname, trusted = trusted)
         rule_spec_list.append(rule_spec)
-        if 'depends' in rule_config:
-            trusted_rule_names = trusted_rule_names.union(set(rule_config['depends'].split()))
-    for trname in trusted_rule_names:
+        if '_lemmas' in rule_config:
+            lemma_names = lemma_names.union(set(rule_config['_lemmas'].split()))
+    for trname in lemma_names:
         rule_spec = gen_spec_rule(rule_template, pgm_config, rule_config, trname, trusted = True)
         rule_spec_list.append(rule_spec)
-    return "\n".join(rule_spec_list)
+    return '\n'.join(rule_spec_list)
 
-def gen_spec_defn(spec_template, rule_template, spec_config, spec_tree):
-    rule_name_list_all = list(filter(lambda sec: sec.startswith(spec_tree + "-"), spec_config.sections()))
-    rule_name_list = []
+def gen_spec_defn(spec_template, rule_template, spec_config, spec_name, output_dir):
+    case_name_list_all = list(filter(lambda sec: sec.startswith(spec_name + '-'), spec_config.sections()))
+    case_name_list = []
 
-    for rname in rule_name_list_all:
-        if not any(sec_name.startswith(rname + "-") for sec_name in spec_config.sections()):
-            rule_name_list.append(rname)
+    for rname in case_name_list_all:
+        if not any(sec_name.startswith(rname + '-') for sec_name in spec_config.sections()):
+            case_name_list.append(rname)
 
-    if len(rule_name_list) == 0:
-        rule_name_list = [spec_tree]
+    rules = '\n'.join( [ gen_spec_rules(rule_template, spec_config, [spec_name])
+                       , gen_spec_rules(rule_template, spec_config, case_name_list, trusted = True)
+                       ]
+                     )
 
-    rules = gen_spec_rules(rule_template, spec_config, rule_name_list)
-
-    spec_defn_tmpl = subst(spec_template, 'module', spec_tree.upper())
+    spec_defn_tmpl = subst(spec_template, 'module', spec_name.upper())
     spec_defn      = subst(spec_defn_tmpl, 'rules', rules)
 
-    return spec_defn
+    write_spec_file(output_dir, spec_name, spec_defn)
+
+    if len(case_name_list) > 0:
+        case_rules = gen_spec_rules(rule_template, spec_config, case_name_list)
+
+        spec_case_name      = spec_name + '_cases'
+        spec_case_defn_tmpl = subst(spec_template, 'module', spec_case_name.upper())
+        spec_case_defn      = subst(spec_case_defn_tmpl, 'rules', case_rules)
+
+        write_spec_file(output_dir, spec_case_name, spec_case_defn)
 
 def usage():
     exec_name = path.basename(sys.argv[0])
-    usage_strs = [ "usage: " + sys.argv[0]
-                 , ""
-                 , "    " + exec_name + " <spec_defn_tmpl> <spec_rule_tmp> <spec_ini> <spec_tree_name>*"
-                 , ""
-                 , "        <spec_defn_tmpl>: template K definition to use."
-                 , "        <spec_rule_tmpl>: template K rule specification to use."
-                 , "        <spec_ini>:       ini format file describing tree of K proofs."
-                 , "        <spec_tree_name>: subtree of specification names to generate (defaults to pgm.specs)."
+    usage_strs = [ 'usage: ' + sys.argv[0]
+                 , ''
+                 , '    ' + exec_name + ' <spec_defn_tmpl> <spec_rule_tmp> <spec_ini> <spec_name>*'
+                 , ''
+                 , '        <spec_defn_tmpl>: template K definition to use.'
+                 , '        <spec_rule_tmpl>: template K rule specification to use.'
+                 , '        <spec_ini>:       ini format file describing tree of K proofs.'
+                 , '        <spec_name>:      name of specification to generate (defaults to pgm.specs).'
                  ]
-    print("\n".join(usage_strs))
+    print('\n'.join(usage_strs))
 
 if __name__ == '__main__':
     if len(sys.argv) < 4:
         usage()
         sys.exit(1)
 
-    spec_defn_tmpl = open(sys.argv[1], "r").read()
-    spec_rule_tmpl = open(sys.argv[2], "r").read()
+    spec_defn_tmpl = open(sys.argv[1], 'r').read()
+    spec_rule_tmpl = open(sys.argv[2], 'r').read()
     spec_ini_file  = sys.argv[3]
-    output_dir     = path.dirname(spec_ini_file)
+    output_dir     = path.abspath(path.dirname(spec_ini_file))
 
     spec_config = configparser.ConfigParser(comment_prefixes=(';'))
     spec_config.read(spec_ini_file)
 
-
     if 'pgm' not in spec_config.sections():
-        print("File " + spec_ini_file + " must have a 'pgm' section!")
+        print('File ' + spec_ini_file + ' must have a `pgm` section!')
         sys.exit(1)
 
-    spec_trees = sys.argv[4:]
-    if len(spec_trees) == 0:
-        spec_trees = spec_config['pgm']['specs'].split()
+    spec_names = sys.argv[4:]
+    if len(spec_names) == 0:
+        spec_names = spec_config['pgm']['specs'].split()
 
-    for spec_tree in spec_trees:
-        spec_defn_str = gen_spec_defn(spec_defn_tmpl, spec_rule_tmpl, spec_config, spec_tree)
-        with open(output_dir + "/" + spec_tree + "-spec.k", "w") as spec_out:
-            spec_out.write(spec_defn_str)
+    for spec_name in spec_names:
+        gen_spec_defn(spec_defn_tmpl, spec_rule_tmpl, spec_config, spec_name, output_dir)
