06dec9c19000918715fd12b9975c4e5f13dcb51a
Guy Repta <guy.repta@runtimeverification.com>
2019-06-18 16:24:02 +0000

    {JsonParser,ToJson}.java: Started implementing serialize/deserialize of Definitions

--- kernel/src/main/java/org/kframework/parser/json/JsonParser.java
+++ kernel/src/main/java/org/kframework/parser/json/JsonParser.java
@@ -1,6 +1,13 @@
 // Copyright (c) 2018-2019 K Team. All Rights Reserved.
 package org.kframework.parser.json;
 
+import org.kframework.attributes.Att;
+import org.kframework.definition.Context;
+import org.kframework.definition.Definition;
+import org.kframework.definition.Module;
+import org.kframework.definition.ModuleComment;
+import org.kframework.definition.Rule;
+import org.kframework.definition.Sentence;
 import org.kframework.kore.K;
 import org.kframework.kore.KLabel;
 import static org.kframework.kore.KORE.KLabel;
@@ -18,25 +25,173 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.io.StringReader;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
 
+import scala.collection.JavaConverters;
+
 /**
  * Parses a Json term into the KORE data structures.
  */
 public class JsonParser {
 
-    public static final String KTOKEN         = "KToken"
-                             , KAPPLY         = "KApply"
-                             , KSEQUENCE      = "KSequence"
-                             , KVARIABLE      = "KVariable"
-                             , KREWRITE       = "KRewrite"
-                             , KAS            = "KAs"
-                             , INJECTEDKLABEL = "InjectedKLabel"
+    public static final String KDEFINITION          = "KDefinition"
+                             , KATT                 = "KAtt"
+                             , KCONFIGURATION       = "KConfiguration"
+                             , KBUBBLE              = "KBubble"
+                             , KMODULE              = "KModule"
+                             , KCONTEXT             = "KContext"
+                             , KRULE                = "KRule"
+                             , KMODULECOMMENT       = "KModuleComment"
+                             , KSYNTAXPRIORITY      = "KSyntaxPriority"
+                             , KSYNTAXASSOCIATIVITY = "KSyntaxAssociativity"
+                             , KTOKEN               = "KToken"
+                             , KSYNTAXSORT          = "KSyntaxSort"
+                             , KPRODUCTION          = "KProduction"
+                             , KAPPLY               = "KApply"
+                             , KSEQUENCE            = "KSequence"
+                             , KVARIABLE            = "KVariable"
+                             , KREWRITE             = "KRewrite"
+                             , KAS                  = "KAs"
+                             , INJECTEDKLABEL       = "InjectedKLabel"
                              ;
+
+/////////////////////////////
+// Parsing Definition Json //
+/////////////////////////////
+
+    public static Definition parseDef(byte[] data) {
+        try {
+            return parseDef(new String(data, "UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            throw new AssertionError("UTF-8 encoding not supported");
+        }
+    }
+
+    public static Definition parseDef(String data) {
+        JsonReader reader = Json.createReader(new StringReader(data));
+        return parseJsonDef(reader.readObject());
+    }
+
+    public static Definition parseJsonDef(JsonObject data) {
+        try {
+            if (! (data.containsKey("format") && data.containsKey("version") && data.containsKey("term"))) {
+                throw KEMException.criticalError("Must have `format`, `version`, and `term` fields in serialized Json!");
+            }
+            if (! data.getString("format").equals("KAST")) {
+                throw KEMException.criticalError("Only can deserialize 'KAST' format Json! Found: " + data.getString("format"));
+            }
+            if (data.getInt("version") != 1) {
+                throw KEMException.criticalError("Only can deserialize KAST version '1'! Found: " + data.getInt("version"));
+            }
+            return toDef(data.getJsonObject("term"));
+        } catch (IOException e) {
+            throw KEMException.criticalError("Could not read Definition term from json", e);
+        }
+    }
+
+    public static Definition toDef(JsonObject data) throws IOException {
+        if (! data.getString("node").equals(KDEFINITION))
+            throw KEMException.criticalError("Unexpected node found in KAST Json term: " + data.getString("node"));
+
+        Module mainModule = toMod(data.getJsonObject("mainModule"));
+        JsonArray mods = data.getJsonArray("entryModules");
+        Module[] modArray = toMods(mods.size(), mods);
+        Set<Module> entryModules = new HashSet<>(Arrays.asList(modArray));
+        return new Definition(mainModule, JavaConverters.asScalaSet(entryModules), toAtt(data.getJsonObject("att")));
+    }
+
+/////////////////////////
+// Parsing Module Json //
+/////////////////////////
+
+    public static Module toMod(JsonObject data) throws IOException {
+        if (! data.getString("node").equals(KMODULE))
+            throw KEMException.criticalError("Unexpected node found in KAST Json term: " + data.getString("node"));
+
+        String name = data.getString("name");
+
+        JsonArray mods = data.getJsonArray("imports");
+        Module[] modArray = toMods(mods.size(), mods);
+        Set<Module> modImports = new HashSet<>(Arrays.asList(modArray));
+
+        JsonArray sens = data.getJsonArray("localSentences");
+        Sentence[] senArray = toSens(sens.size(), sens);
+        Set<Sentence> localSentences = new HashSet<>(Arrays.asList(senArray));
+
+        return new Module(name, JavaConverters.asScalaSet(modImports), JavaConverters.asScalaSet(localSentences), toAtt(data.getJsonObject("att")));
+    }
+
+    private static Module[] toMods(int arity, JsonArray data) throws IOException {
+        Module[] items = new Module[arity];
+        for (int i = 0; i < arity; i++) {
+            items[i] = toMod(data.getValuesAs(JsonObject.class).get(i));
+        }
+        return items;
+    }
+
+///////////////////////////
+// Parsing Sentence Json //
+///////////////////////////
+
+    public static Sentence toSen(JsonObject data) throws IOException {
+        switch(data.getString("node")) {
+            case KCONTEXT: {
+                K body = toK(data.getJsonObject("body"));
+                K requires = toK(data.getJsonObject("requires"));
+                Att att = toAtt(data.getJsonObject("att"));
+                return new Context(body, requires, att);
+            }
+            case KRULE: {
+                K body = toK(data.getJsonObject("body"));
+                K requires = toK(data.getJsonObject("requires"));
+                K ensures = toK(data.getJsonObject("ensures"));
+                Att att = toAtt(data.getJsonObject("att"));
+                return new Rule(body, requires, ensures, att);
+            }
+            case KMODULECOMMENT:{
+                String comment = data.getString("comment");
+                Att att = toAtt(data.getJsonObject("att"));
+                return new ModuleComment(comment, att);
+            }
+            case KSYNTAXPRIORITY: {
+                
+            }
+            case KSYNTAXASSOCIATIVITY: {
+                
+            }
+            default:
+                throw KEMException.criticalError("Unexpected node found in KAST Json term: " + data.getString("node"));
+        }
+    }
+
+    private static Sentence[] toSens(int arity, JsonArray data) throws IOException {
+        Sentence[] items = new Sentence[arity];
+        for (int i = 0; i < arity; i++) {
+            items[i] = toSen(data.getValuesAs(JsonObject.class).get(i));
+        }
+        return items;
+    }
+
+//////////////////////
+// Parsing Att Json //
+//////////////////////
+
+    public static Att toAtt(JsonObject data) throws IOException {
+        if (! data.getString("node").equals(KATT))
+            throw KEMException.criticalError("Unexpected node found in KAST Json term: " + data.getString("node"));
+
+        return Att.empty();
+    }
+
+////////////////////
+// Parsing K Json //
+////////////////////
 
     public static K parse(byte[] data) {
         try {
--- kernel/src/main/java/org/kframework/unparser/ToJson.java
+++ kernel/src/main/java/org/kframework/unparser/ToJson.java
@@ -1,6 +1,21 @@
 // Copyright (c) 2015-2019 K Team. All Rights Reserved.
 package org.kframework.unparser;
 
+import org.kframework.attributes.Att;
+import org.kframework.definition.Associativity;
+import org.kframework.definition.Bubble;
+import org.kframework.definition.Context;
+import org.kframework.definition.Configuration;
+import org.kframework.definition.Definition;
+import org.kframework.definition.Module;
+import org.kframework.definition.ModuleComment;
+import org.kframework.definition.Production;
+import org.kframework.definition.Rule;
+import org.kframework.definition.Sentence;
+import org.kframework.definition.SyntaxAssociativity;
+import org.kframework.definition.SyntaxPriority;
+import org.kframework.definition.SyntaxSort;
+import org.kframework.definition.Tag;
 import org.kframework.kore.InjectedKLabel;
 import org.kframework.kore.K;
 import org.kframework.kore.KApply;
@@ -30,10 +45,222 @@ import javax.json.JsonObjectBuilder;
 import javax.json.JsonWriter;
 import javax.json.JsonStructure;
 
+import scala.collection.JavaConverters;
+import scala.collection.Seq;
+import scala.collection.Set;
+import scala.Enumeration;
+
 /**
  * Writes a KAST term to the KAST Json format.
  */
 public class ToJson {
+
+    private DataOutputStream data;
+    private ToJson(DataOutputStream data) {
+        this.data = data;
+    }
+
+///////////////////////////////
+// ToJson Definition Objects //
+///////////////////////////////
+
+    public static byte[] apply(Definition def) {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try {
+            DataOutputStream data = new DataOutputStream(out);
+            JsonWriter jsonWriter = Json.createWriter(data);
+
+            JsonObjectBuilder term = Json.createObjectBuilder();
+            term.add("format", "KAST");
+            term.add("version", 1);
+            term.add("term", toJson(def));
+
+            jsonWriter.write(term.build());
+            jsonWriter.close();
+            data.close();
+        } catch (IOException e) {
+            throw KEMException.criticalError("Could not write Definition to Json", e);
+        }
+        return out.toByteArray();
+    }
+
+    public static JsonStructure toJson(Definition def) {
+        JsonObjectBuilder jdef = Json.createObjectBuilder();
+
+        JsonArrayBuilder mods = Json.createArrayBuilder();
+        for (Module m : JavaConverters.setAsJavaSet(def.modules())) {
+            mods.add(toJson(m));
+        }
+
+        jdef.add("node", JsonParser.KDEFINITION);
+        jdef.add("mainModule", def.mainModule().name());
+        jdef.add("entryModules", mods);
+        jdef.add("att", toJson(def.att()));
+
+        return jdef.build();
+    }
+
+    public static JsonStructure toJson(Att att) {
+        JsonObjectBuilder jatt = Json.createObjectBuilder();
+        jatt.add("node", JsonParser.KATT);
+        jatt.add("att", att.toString());
+
+        return jatt.build();
+    }
+
+///////////////////////////
+// ToJson Module Objects //
+///////////////////////////
+
+    public static JsonStructure toJson(Module mod) {
+        JsonObjectBuilder jmod = Json.createObjectBuilder();
+
+        jmod.add("node", JsonParser.KMODULE);
+
+        JsonArrayBuilder imports = Json.createArrayBuilder();
+        for (String s: JavaConverters.setAsJavaSet(mod.importedModuleNames())) {
+            imports.add(s);
+        }
+
+        JsonArrayBuilder sentences = Json.createArrayBuilder();
+        for (Sentence s: JavaConverters.setAsJavaSet(mod.localSentences())) {
+            sentences.add(toJson(s));
+        }
+
+        jmod.add("name", mod.name());
+        jmod.add("imports", imports);
+        jmod.add("localSentences", sentences);
+        jmod.add("att", toJson(mod.att()));
+
+        return jmod.build();
+    }
+
+/////////////////////////////
+// ToJSon Sentence Objects //
+/////////////////////////////
+
+    public static JsonStructure toJson(Sentence sen) {
+        if (sen instanceof Context)             return toJson((Context) sen);
+        if (sen instanceof Rule)                return toJson((Rule) sen);
+        if (sen instanceof ModuleComment)       return toJson((ModuleComment) sen);
+        if (sen instanceof SyntaxPriority)      return toJson((SyntaxPriority) sen);
+        if (sen instanceof SyntaxAssociativity) return toJson((SyntaxAssociativity) sen);
+        if (sen instanceof Configuration)       return toJson((Configuration) sen);
+        if (sen instanceof Bubble)              return toJson((Bubble) sen);
+        if (sen instanceof SyntaxSort)          return toJson((SyntaxSort) sen);
+        if (sen instanceof Production)          return toJson((Production) sen);
+
+        JsonObjectBuilder jsen = Json.createObjectBuilder();
+        jsen.add("node", "badsentence");
+        return jsen.build();
+    }
+
+    public static JsonStructure toJson(Context con) {
+        JsonObjectBuilder jcon = Json.createObjectBuilder();
+
+        jcon.add("node", JsonParser.KCONTEXT);
+        jcon.add("body", toJson(con.body()));
+        jcon.add("requires", toJson(con.requires()));
+        jcon.add("att", toJson(con.att()));
+
+        return jcon.build();
+    }
+
+    public static JsonStructure toJson(Rule rule) {
+        JsonObjectBuilder jrule = Json.createObjectBuilder();
+
+        jrule.add("node", JsonParser.KRULE);
+        jrule.add("body", toJson(rule.body()));
+        jrule.add("requires", toJson(rule.requires()));
+        jrule.add("ensures", toJson(rule.ensures()));
+        jrule.add("att", toJson(rule.att()));
+
+        return jrule.build();
+    }
+
+    public static JsonStructure toJson(ModuleComment mcom) {
+        JsonObjectBuilder jmcom = Json.createObjectBuilder();
+
+        jmcom.add("node", JsonParser.KMODULECOMMENT);
+        jmcom.add("comment", mcom.comment());
+        jmcom.add("att", toJson(mcom.att()));
+
+        return jmcom.build();
+    }
+
+    public static JsonStructure toJson(SyntaxPriority syn) {
+        JsonObjectBuilder jsyn = Json.createObjectBuilder();
+
+        jsyn.add("node", JsonParser.KSYNTAXPRIORITY);
+
+        Seq<Set<Tag>> priSeq = syn.priorities();
+        JsonArrayBuilder priArray = Json.createArrayBuilder();
+        for (Set<Tag> pri : JavaConverters.seqAsJavaList(priSeq)) {
+            JsonArrayBuilder tagArray = Json.createArrayBuilder();
+            for (Tag t : JavaConverters.setAsJavaSet(pri)) {
+                tagArray.add(t.name());
+            }
+            priArray.add(tagArray);
+        }
+        jsyn.add("priorities", priArray);
+
+        jsyn.add("att", toJson(syn.att()));
+
+        return jsyn.build();
+    }
+
+    public static JsonStructure toJson(SyntaxAssociativity syn) {
+        JsonObjectBuilder jsyn = Json.createObjectBuilder();
+
+        jsyn.add("node", JsonParser.KSYNTAXASSOCIATIVITY);
+        jsyn.add("assoc", syn.assoc().toString());
+
+        JsonArrayBuilder tagArray = Json.createArrayBuilder();
+        for (Tag t : JavaConverters.setAsJavaSet(syn.tags())) {
+            tagArray.add(t.name());
+        }
+        jsyn.add("tags", tagArray);
+
+        jsyn.add("att", toJson(syn.att()));
+
+        return jsyn.build();
+    }
+
+    public static JsonStructure toJson(Configuration con) {
+        JsonObjectBuilder jcon = Json.createObjectBuilder();
+
+        jcon.add("node", JsonParser.KCONFIGURATION);
+
+        return jcon.build();
+    }
+
+    public static JsonStructure toJson(Bubble bub) {
+        JsonObjectBuilder jbub = Json.createObjectBuilder();
+
+        jbub.add("node", JsonParser.KBUBBLE);
+
+        return jbub.build();
+    }
+
+    public static JsonStructure toJson(SyntaxSort sort) {
+        JsonObjectBuilder jsort = Json.createObjectBuilder();
+
+        jsort.add("node", JsonParser.KSYNTAXSORT);
+
+        return jsort.build();
+    }
+
+    public static JsonStructure toJson(Production pro) {
+        JsonObjectBuilder jpro = Json.createObjectBuilder();
+
+        jpro.add("node", JsonParser.KPRODUCTION);
+
+        return jpro.build();
+    }
+
+//////////////////////
+// ToJson K Objects //
+//////////////////////
 
     public static void apply(OutputStream out, K k) {
         try {
@@ -57,11 +284,6 @@ public class ToJson {
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         apply(out, k);
         return out.toByteArray();
-    }
-
-    private DataOutputStream data;
-    private ToJson(DataOutputStream data) {
-        this.data = data;
     }
 
     private static JsonStructure toJson(K k) {
