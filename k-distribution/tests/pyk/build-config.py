bc8f6dab3110708b1eb8d35ba69398ef022d0fb8
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2019-06-24 19:02:49 -0600

    k-distribution/tests/pyk: more thorough testing harness

--- k-distribution/tests/pyk/build-config.py
+++ k-distribution/tests/pyk/build-config.py
@@ -9,29 +9,13 @@ from util import *
 from kast import *
 from pyk  import *
 
-IMP_symbols = { 'int_;__IMP-SYNTAX'       : (lambda ds, ss : 'int ' + ds + ';\n' + ss)
-              , '_,__IMP-SYNTAX'          : assocWithUnit(' , ', '.Ids')
-              , '.List{"_,__IMP-SYNTAX"}' : constLabel('.Ids')
-              , '{}_IMP-SYNTAX'           : constLabel('{ }')
+IMP_symbols = { 'int_;_'       : (lambda ds, ss : 'int ' + ds + ';\n' + ss)
+              , '_,_'          : assocWithUnit(' , ', '.Ids')
+              , '.List{"_,_"}' : constLabel('.Ids')
+              , '{}'           : constLabel('{ }')
               }
 
 ALL_symbols = combineDicts(K_symbols, IMP_symbols)
 
-def declareVars(vList):
-    argTokens   = [KToken(v, 'Id') for v in vList]
-    asIdList    = lambda rest, v: KApply('_,__IMP-SYNTAX', [v, rest])
-    emptyIdList = KApply('.List{"_,__IMP-SYNTAX"}', [])
-    return reduce(asIdList, reversed(argTokens), emptyIdList)
-
-symbolic_configuration = KApply ( '<T>' , [ KApply ( '<k>'     , [ KVariable('K_CELL')     ] )
-                                          , KApply ( '<state>' , [ KVariable('STATE_CELL') ] )
-                                          ]
-                                )
-
-init_cells = { 'K_CELL'     : KApply('int_;__IMP-SYNTAX', [declareVars(['x', 'y']), KApply('{}_IMP-SYNTAX', [])])
-             , 'STATE_CELL' : KApply('.Map', [])
-             }
-
-instantiated_configuration = substitute(symbolic_configuration, init_cells)
-
-print(prettyPrintKast(instantiated_configuration, ALL_symbols))
+kast_term = readKastTerm(sys.argv[1])
+print(prettyPrintKast(kast_term, ALL_symbols))
