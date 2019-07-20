f58271991bbd30309a6e925bb260314fb9752283
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2019-07-20 13:32:20 -0600

    lib/pyk/__init__.py: capture stdout, pass through stdin

--- k-distribution/src/main/scripts/lib/pyk/__init__.py
+++ k-distribution/src/main/scripts/lib/pyk/__init__.py
@@ -5,29 +5,38 @@ import subprocess
 import sys
 import tempfile
 
-def notif(msg):
+def _teeProcessStdout(args, tee = True, buffer_size = 80):
+        process = subprocess.Popen(args, stdout = subprocess.PIPE, stderr = subprocess.PIPE, universal_newlines = True)
+        capture = ""
+        s = process.stdout.read(buffer_size)
+        while len(s) > 0:
+            if tee:
+                sys.stdout.write(s)
+                sys.stdout.flush()
+            capture += s
+            s = process.stdout.read(buffer_size)
+        if tee:
+            sys.stderr.write(stderr)
+            sys.stderr.flush()
+        return (process.returncode, capture, stderr)
+
+def _notif(msg):
     print()
     print(msg)
     print(''.join(['=' for c in msg]))
     sys.stdout.flush()
 
-def warning(msg):
-    notif('[WARNING] ' + msg)
+def _warning(msg):
+    _notif('[WARNING] ' + msg)
 
-def fatal(msg):
-    notif('[FATAL] ' + msg)
-    sys.exit(1)
-
-def _runK(command, definitionDir, fileName, kArgs):
-    return kProc.returnCode
+def _fatal(msg, code = 1):
+    _notif('[FATAL] ' + msg)
+    sys.exit(code)
 
 pykArgs = argparse.ArgumentParser()
 
 pykArgs.add_argument('command' , choices = ['parse', 'run', 'prove'])
-_kexec = { 'parse' : 'kast'
-         , 'run'   : 'krun'
-         , 'prove' : 'kprove'
-         }
+_kexec = { 'parse' : 'kast' , 'run' : 'krun' , 'prove' : 'kprove' }
 
 pykArgs.add_argument('-d', '--definition')
 
@@ -39,21 +48,24 @@ pykArgs.add_argument('-t', '--to',   default = 'pretty', choices = ['pretty', 'j
 
 pykArgs.add_argument('kArgs', nargs='*', help = 'Arguments to pass through to K invocation.')
 
-def __main__():
-    args = pykArgs.parse_args()
+def __main__(args):
 
-    inputFile = pykArgs.input.name
+    inputFile = args['input'].name
     if inputFile == '-':
         with tempfile.NamedTemporaryFile(mode = 'w') as tempf:
-            tempf.write(pykArgs.input.read())
+            tempf.write(args['input'].read())
             inputFile = tempf.name
 
-    kCommand = [ _kexec[pykArgs.command], '--directory', pykArgs.definition, inputFile
-               , '--input'  , pykArgs['from']
-               , '--output' , pykArgs.to
-               ] + kArgs
-    notif('Running: ' + str(kCommand))
-    kProc = subprocess.run(kCommand)
+    kCommand = [ _kexec[args['command']] , '--directory' , args['definition'] , inputFile
+               , '--input'  , args['from'] , '--output' , args['to']
+               ] + args['kArgs']
+    _notif('Running: ' + str(kCommand))
+    (returncode, stdout, stderr) = _teeProcessStdout(kCommand, tee = teeOutput)
+
+    args['output'].write(stdout)
+
+    if returnCode != 0:
+        _fatal('Non-zero exit code (' + str(returnCode) + ': ' + str(kCommand), code = returnCode)
 
 if __name__ == '__main__':
-    __main__()
+    __main__(pykArgs.parse_args())
