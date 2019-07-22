1484761fc8976cdefd753dbe30bd1ee4b4a2d6b7
Micky Abir <micky.abir@gmail.com>
2019-07-22 23:15:30 +0000

    fixed label

--- k-distribution/include/ocaml/hooks.ml
+++ k-distribution/include/ocaml/hooks.ml
@@ -299,7 +299,7 @@ struct
         | Unix.WSIGNALED n -> (128 + n)
         | Unix.WSTOPPED n -> (128 + n)
       in
-      [KApply3((parse_klabel "#systemResult(_,_,_)_K-IO"), [Int (Z.of_int exit_code)], [String (Buffer.contents buf_out)], [String (Buffer.contents buf_err)])]
+      [KApply3((parse_klabel "#systemResult"), [Int (Z.of_int exit_code)], [String (Buffer.contents buf_out)], [String (Buffer.contents buf_err)])]
     | _ -> raise Not_implemented
   let hook_mkstemp c _ _ _ _ = match c with
     | [String prefix], [String suffix] -> unix_error (fun () ->
