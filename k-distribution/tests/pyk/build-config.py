b6b531c6eb3d21138f4514f62d597e9e52a56005
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2019-09-03 10:02:12 -0600

    k-distribution/tests/pyk: KModule => KFlatModule

--- k-distribution/tests/pyk/build-config.py
+++ k-distribution/tests/pyk/build-config.py
@@ -22,7 +22,7 @@ kast_term = readKastTerm(sys.argv[1])
 if (isKRule(kast_term)):
     kast_term = minimizeRule(kast_term)
     defn_name = sys.argv[1][:-5].split("/")[1]
-    mod = KModule(defn_name.upper(), [KImport("IMP")], [kast_term])
+    mod = KFlatModule(defn_name.upper(), [KImport("IMP")], [kast_term])
     kast_term = KDefinition(defn_name, [KRequire("imp")], [mod])
 
 print(prettyPrintKast(kast_term, ALL_symbols))
