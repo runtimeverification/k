298a7e7925a220f210fcb830951d806d04fb2548
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2019-09-03 22:27:18 -0600

    kernel/{ResolveFreshConstants,ResolveFunctionWithConfig,Kompile}: uses of topCell => getRootCell

--- kernel/src/main/java/org/kframework/compile/ResolveFunctionWithConfig.java
+++ kernel/src/main/java/org/kframework/compile/ResolveFunctionWithConfig.java
@@ -62,7 +62,7 @@ public class ResolveFunctionWithConfig {
         topCell = Sorts.GeneratedTopCell();
         topCellLabel = KLabels.GENERATED_TOP_CELL;
       } else {
-        topCell = info.topCell();
+        topCell = info.getRootCell();
         topCellLabel = info.getCellLabel(topCell);
       }
       CONFIG_VAR = KVariable("_Configuration", Att().add(Sort.class, topCell).add("withConfig"));
