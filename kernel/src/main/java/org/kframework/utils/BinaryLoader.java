d599019f14b50f71031bbd8bf519f2f106c251cc
Denis Bogdanas <denis.bogdanas@gmail.com>
2019-11-01 11:49:46 +0200

    kernel/BinaryLoader: Disables JVM warning produced by FST

--- kernel/src/main/java/org/kframework/utils/BinaryLoader.java
+++ kernel/src/main/java/org/kframework/utils/BinaryLoader.java
@@ -13,12 +13,46 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectStreamException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.nio.channels.OverlappingFileLockException;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 @RequestScoped
 public class BinaryLoader {
+
+    static {
+        disableAccessWarnings();
+    }
+
+    /**
+     * Disables JVM warnings "WARNING: An illegal reflective access operation has occurred" produced by FST.
+     * <p>
+     * source: https://stackoverflow.com/a/53517025/4182868
+     */
+    @SuppressWarnings("unchecked")
+    public static void disableAccessWarnings() {
+        try {
+            Class unsafeClass = Class.forName("sun.misc.Unsafe");
+            Field field = unsafeClass.getDeclaredField("theUnsafe");
+            field.setAccessible(true);
+            Object unsafe = field.get(null);
+
+            Method putObjectVolatile =
+                    unsafeClass.getDeclaredMethod("putObjectVolatile", Object.class, long.class, Object.class);
+            Method staticFieldOffset = unsafeClass.getDeclaredMethod("staticFieldOffset", Field.class);
+
+            Class loggerClass = Class.forName("jdk.internal.module.IllegalAccessLogger");
+            Field loggerField = loggerClass.getDeclaredField("logger");
+            Long offset = (Long) staticFieldOffset.invoke(unsafe, loggerField);
+            putObjectVolatile.invoke(unsafe, loggerClass, offset, null);
+            // DISABLE EXCEPTION CHECKSTYLE
+        } catch (Exception ignored) {
+            // ENABLE EXCEPTION CHECKSTYLE
+            //Exception here caused by reflexion will just re-enable the warning.
+        }
+    }
 
     private static ReadWriteLock lock = new ReentrantReadWriteLock();
 
