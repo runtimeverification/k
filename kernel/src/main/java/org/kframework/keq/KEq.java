3a26ecd792700bcc2b4c0701161a4d0b7e4524f5
Denis Bogdanas <denis.bogdanas@gmail.com>
2019-11-08 14:22:18 +0200

    KEq: converted to injection and used other injections where possible

--- kernel/src/main/java/org/kframework/keq/KEq.java
+++ kernel/src/main/java/org/kframework/keq/KEq.java
@@ -1,47 +1,41 @@
 // Copyright (c) 2014-2019 K Team. All Rights Reserved.
 package org.kframework.keq;
 
-import org.kframework.compile.Backend;
+import com.google.inject.Inject;
 import org.kframework.definition.Definition;
 import org.kframework.definition.Module;
 import org.kframework.kompile.CompiledDefinition;
 import org.kframework.kprove.ProofDefinitionBuilder;
 import org.kframework.rewriter.Rewriter;
-import org.kframework.utils.Stopwatch;
-import org.kframework.utils.errorsystem.KExceptionManager;
 import org.kframework.utils.file.FileUtil;
+import org.kframework.utils.inject.RequestScoped;
 import scala.Tuple2;
 
 import java.util.function.Function;
 
+@RequestScoped
 public class KEq {
-    private final KExceptionManager kem;
-    private final FileUtil files;
-    private Stopwatch sw;
+    private final KEqOptions keqOptions;
 
-    public KEq(KExceptionManager kem, FileUtil files, Stopwatch sw) {
-        this.kem = kem;
-        this.files = files;
-        this.sw = sw;
+    @Inject
+    public KEq(KEqOptions keqOptions) {
+        this.keqOptions = keqOptions;
     }
 
-    public int run(
-            CompiledDefinition commonDef,
-            CompiledDefinition def1,
-            CompiledDefinition def2,
-            KEqOptions keqOptions,
-            Backend backend,
-            Function<Definition, Rewriter> commonGen,
-            Function<Definition, Rewriter> gen1,
-            Function<Definition, Rewriter> gen2) {
+    public int run(CompiledDefinition commonDef, Function<Definition, Rewriter> commonGen,
+                   Function<Definition, Rewriter> gen1,
+                   Function<Definition, Rewriter> gen2,
+                   ProofDefinitionBuilder pdb1,
+                   ProofDefinitionBuilder pdb2,
+                   FileUtil files) {
         Rewriter commonRewriter = commonGen.apply(commonDef.kompiledDefinition);
 
-        Tuple2<Definition, Module> compiled1 = new ProofDefinitionBuilder(def1, backend, files, kem, sw).build(
+        Tuple2<Definition, Module> compiled1 = pdb1.build(
                 files.resolveWorkingDirectory(keqOptions.spec1), keqOptions.defModule1, keqOptions.specModule1);
         Rewriter rewriter1 = gen1.apply(compiled1._1());
         Module spec1 = compiled1._2();
 
-        Tuple2<Definition, Module> compiled2 = new ProofDefinitionBuilder(def2, backend, files, kem, sw).build(
+        Tuple2<Definition, Module> compiled2 = pdb2.build(
                 files.resolveWorkingDirectory(keqOptions.spec2), keqOptions.defModule2, keqOptions.specModule2);
         Rewriter rewriter2 = gen2.apply(compiled2._1());
         Module spec2 = compiled2._2();
