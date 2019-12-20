2e1a14621f7c4e905e55beb51b652aa9aa0d32ab
Dwight Guth <dwight.guth@runtimeverification.com>
2019-12-20 15:08:23 +0000

    refactor associativity into java enum

--- kernel/src/main/java/org/kframework/parser/json/JsonParser.java
+++ kernel/src/main/java/org/kframework/parser/json/JsonParser.java
@@ -202,10 +202,10 @@ public class JsonParser {
             }
             case KSYNTAXASSOCIATIVITY: {
                 String assocString = data.getString("assoc");
-                scala.Enumeration.Value assoc = assocString == "Left"     ? Associativity.Left()
-                                              : assocString == "Right"    ? Associativity.Right()
-                                              : assocString == "NonAssoc" ? Associativity.NonAssoc()
-                                              : Associativity.Unspecified();
+                Associativity assoc = assocString == "Left"     ? Associativity.Left
+                                              : assocString == "Right"    ? Associativity.Right
+                                              : assocString == "NonAssoc" ? Associativity.NonAssoc
+                                              : Associativity.Unspecified;
                 scala.collection.Set<Tag> tags = toTags(data.getJsonArray("tags"));
                 Att att = toAtt(data.getJsonObject("att"));
                 return new SyntaxAssociativity(assoc, tags, att);
---
+++ kore/src/main/java/org/kframework/definition/Associativity.java
@@ -0,0 +1,6 @@
+// Copyright (c) 2014-2019 K Team. All Rights Reserved.
+package org.kframework.definition;
+
+public enum Associativity {
+  Left, Right, NonAssoc, Unspecified
+}
