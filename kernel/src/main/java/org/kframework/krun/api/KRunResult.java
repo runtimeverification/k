0630a4f743b5dc166ee2b9b52199a62b71cd389d
Dwight Guth <dwight.guth@runtimeverification.com>
2020-01-27 22:23:34 +0000

    delete unused class files

--- kernel/src/main/java/org/kframework/krun/api/KRunResult.java
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2013-2019 K Team. All Rights Reserved.
-package org.kframework.krun.api;
-
-/**
- * Marker interface for a class returned by the KRun API.
- * @author dwightguth
- *
- */
-public interface KRunResult {}
