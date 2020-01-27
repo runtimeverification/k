d009d0785369df4dd3a53042ed1ec554d67b3989
Dwight Guth <dwight.guth@runtimeverification.com>
2020-01-27 22:25:10 +0000

    delete samples

--- k-distribution/samples/java_rewrite_engine/tutorial/1_k/2_imp/testgen/testgen.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-../../../../../../bin/kompile imp.k --backend java --test-gen -v
-../../../../../../bin/krun --term while.imp --backend java --generate-tests --search -bound 100 -depth 300 --smt none --parens smart
