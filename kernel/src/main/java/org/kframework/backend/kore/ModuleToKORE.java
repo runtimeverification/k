39f35aff0a4f6dea4084a43432917e6542d62fd1
andreiburdusa <andrei.burdusa@zoho.com>
2020-04-21 11:02:29 +0300

    Revert changes from ModuleToKORE.java

--- kernel/src/main/java/org/kframework/backend/kore/ModuleToKORE.java
+++ kernel/src/main/java/org/kframework/backend/kore/ModuleToKORE.java
@@ -166,15 +166,12 @@ public class ModuleToKORE {
         translateSorts(tokenSorts, attributes, collectionSorts, sb);
 
         List<Rule> sortedRules = new ArrayList<>(JavaConverters.seqAsJavaList(module.sortedRules()));
-        if (options.backend.equals("haskell")) {
-            module.sortedProductions().toStream().filter(this::isGeneratedInKeysOp).foreach(
-                    prod -> {
-                        genMapCeilAxioms(prod, sortedRules);
-                        return prod;
-                    }
-            );
-        }
-        SetMultimap<KLabel, Rule> functionRules = HashMultimap.create();
+        module.sortedProductions().toStream().filter(this::isGeneratedInKeysOp).foreach(
+                prod -> {
+                    genMapCeilAxioms(prod, sortedRules);
+                    return prod;
+                }
+        );        SetMultimap<KLabel, Rule> functionRules = HashMultimap.create();
         for (Rule rule : sortedRules) {
             K left = RewriteToTop.toLeft(rule.body());
             if (left instanceof KApply) {
