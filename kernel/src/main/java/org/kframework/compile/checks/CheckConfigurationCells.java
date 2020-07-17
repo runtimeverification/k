a29a1e0ead3d09e503b7bbabf4b070541c3c4c2e
Dwight Guth <dwight.guth@runtimeverification.com>
2020-07-17 12:05:23 -0500

    add error for unsupported cell  bag

--- kernel/src/main/java/org/kframework/compile/checks/CheckConfigurationCells.java
+++ kernel/src/main/java/org/kframework/compile/checks/CheckConfigurationCells.java
@@ -24,9 +24,12 @@ public class CheckConfigurationCells {
 
     private final Module module;
 
-    public CheckConfigurationCells(Set<KEMException> errors, Module module) {
+    private final boolean isSymbolicKast;
+
+    public CheckConfigurationCells(Set<KEMException> errors, Module module, boolean isSymbolicKast) {
         this.errors = errors;
         this.module = module;
+        this.isSymbolicKast = isSymbolicKast;
     }
 
     public void check(Sentence s) {
@@ -49,6 +52,13 @@ public class CheckConfigurationCells {
                         }
                         cells.add(sort);
                     }
+                }
+            }
+            if (p.att().getOptional("multiplicity").orElse("").equals("*") && p.att().getOptional("type").orElse("Bag").equals("Bag")) {
+                if (!isSymbolicKast) {
+                    errors.add(KEMException.compilerError("Cell bags are not supported except on the Java backend. If you want "
+                          + "this feature, file an issue at https://github.com/kframework/k  As a workaround, you can add the attribute "
+                          + "type=\"Set\" and add a unique identifier to each element in the set.", p));
                 }
             }
         }
