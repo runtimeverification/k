eb3317b89ddba50569860387ee3e0f5f1f6a0f40
Radu Mereuta <headness13@gmail.com>
2020-10-31 01:54:11 +0200

    Remove mini kore.

--- kore/src/main/java/org/kframework/builtin/Rules.java
+++ kore/src/main/java/org/kframework/builtin/Rules.java
@@ -6,7 +6,7 @@ import static org.kframework.kore.KORE.*;
 
 public class Rules {
     public static final KRewrite STUCK_RULE = KRewrite(
-              KApply(KLabels.STRATEGY_CELL, KList(KApply(KLabels.KSEQ, KList(KApply(KLabels.DOTK, KList(), Att()), KVariable("#REST")), Att())), Att())
-            , KApply(KLabels.STRATEGY_CELL, KList(KApply(KLabels.KSEQ, KList(KApply(KLabels.STUCK, KList(), Att()), KVariable("#REST")), Att())), Att())
-            , Att());
+              KApply(KLabels.STRATEGY_CELL, KList(KApply(KLabels.KSEQ, KList(KApply(KLabels.DOTK, KList()), KVariable("#REST")))))
+            , KApply(KLabels.STRATEGY_CELL, KList(KApply(KLabels.KSEQ, KList(KApply(KLabels.STUCK, KList()), KVariable("#REST")))))
+            );
 }
