f5e7de45e8aa5a62e5078734247765e34c736869
Dwight Guth <dwight.guth@runtimeverification.com>
2020-11-04 18:53:41 -0600

    Allow user to configure max stack depth of bison parser (#1623)
    * add test
    
    * configurable YYMAXDEPTH
    
    * bypass stack overflow error
    
    * Update kernel/src/main/java/org/kframework/kast/KastOptions.java
    
    Co-authored-by: Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
    
    * Update kernel/src/main/java/org/kframework/kompile/KompileOptions.java
    
    Co-authored-by: Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
    
    Co-authored-by: Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
    Co-authored-by: rv-jenkins <admin@runtimeverification.com>

--- kernel/src/main/java/org/kframework/kompile/KompileOptions.java
+++ kernel/src/main/java/org/kframework/kompile/KompileOptions.java
@@ -143,6 +143,9 @@ public class KompileOptions implements Serializable {
         @Parameter(names="--bison-file", description="C file containing functions to link into bison parser.")
         public String bisonFile;
 
+        @Parameter(names="--bison-stack-max-depth", description="Maximum size of bison parsing stack (default: 10000).")
+        public long bisonStackMaxDepth = 10000;
+
         @Parameter(names="--transition", listConverter=StringListConverter.class, description="<string> is a whitespace-separated list of tags designating rules to become transitions.")
         public List<String> transition = Collections.singletonList(DEFAULT_TRANSITION);
 
