05a84b292c0a6c2f37c8be6d6fa66d5663fbe897
Dwight Guth <dwight.guth@runtimeverification.com>
2020-11-11 14:36:44 -0600

    allow bracket productions

--- kernel/src/main/java/org/kframework/compile/checks/CheckAtt.java
+++ kernel/src/main/java/org/kframework/compile/checks/CheckAtt.java
@@ -43,7 +43,8 @@ public class CheckAtt {
         if (!prod.sort().equals(Sorts.KItem())) {
             Att sortAtt =  m.sortAttributesFor().getOrElse(prod.sort().head(), () -> Att.empty());
             if (sortAtt.contains(Att.HOOK()) && !sortAtt.get(Att.HOOK()).equals("ARRAY.Array")) {
-                if (!prod.att().contains(Att.FUNCTION()) && !prod.att().contains("token") && !(prod.klabel().isDefined() && macros.contains(prod.klabel().get()))) {
+                if (!prod.att().contains(Att.FUNCTION()) && !prod.att().contains(Att.BRACKET()) && 
+                    !prod.att().contains("token") && !(prod.klabel().isDefined() && macros.contains(prod.klabel().get()))) {
                     if (!(prod.sort().equals(Sorts.K()) && ((prod.klabel().isDefined() && (prod.klabel().get().name().equals("#EmptyK") || prod.klabel().get().name().equals("#KSequence"))) || prod.isSubsort()))) {
                         if (!(sortAtt.contains("cellCollection") && prod.isSubsort())) {
                             errors.add(KEMException.compilerError("Cannot add new constructors to hooked sort " + prod.sort(), prod));
