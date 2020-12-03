f9dcf7a569e2b7509e3c623c286a0b131875ae56
Radu Mereuta <headness13@gmail.com>
2020-12-04 01:49:38 +0200

    Fix #1646 - error message for simplification attribute (#1671)
    * Improve error message for function checks
    
    * Remove errors to warnings from kprove.
    
    * Fix kprove spec errors
    
    * Unfix broken spec test
    
    Test error messages
    
    * Update loc info
    
    Co-authored-by: rv-jenkins <admin@runtimeverification.com>

--- kernel/src/main/java/org/kframework/compile/checks/CheckFunctions.java
+++ kernel/src/main/java/org/kframework/compile/checks/CheckFunctions.java
@@ -70,7 +70,8 @@ public class CheckFunctions {
                       || hook.equals("SET.element") || hook.equals("SET.concat") || hook.equals("SET.unit")
                       || hook.equals("MAP.element") || hook.equals("MAP.concat") || hook.equals("MAP.unit")
                       || hook.equals("BAG.element") || hook.equals("BAG.concat") || hook.equals("BAG.unit"))) {
-                  errors.add(KEMException.compilerError("Illegal function symbol on LHS of rule.", k));
+                  errors.add(KEMException.compilerError("Illegal function symbol on LHS of rule." +
+                          " Consider adding `simplification` attribute to the rule if this is intended.", k));
                 }
                 atTop = false;
                 if (hook.equals("SET.element")) return;
