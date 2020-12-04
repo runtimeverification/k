9067261784841566c9af9e6524ecd2dc9dc8a762
Thomas Tuegel <ttuegel@mailbox.org>
2020-12-04 15:29:10 -0600

    Update ./nix/update-maven.sh

--- nix/update-maven.sh
+++ nix/update-maven.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env nix-shell
-#!nix-shell ../shell.nix -i bash
+#!nix-shell ./shell.maven.nix -i bash
 
 # Run this script (from the top level of the repository) when the maven
 # project's dependencies change.
 
 # Ensure the source is loaded into the Nix store.
-# mavenix will not do this automatically because we are using nix-gitignore.
-nix-instantiate --eval -E '(import ./. {}).k.src'
+# This command will fail, but only after loading the source.
+# mavenix will not do this automatically because we it uses restrict-eval,
+# and we are using filterSource, which is disabled under restrict-eval.
+nix-build --no-out-link -E '(import ./. {}).k.src' || echo "expected error"
 
 mvnix-update -E '(import ./. {}).k'
