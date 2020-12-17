619a6d2d1eaa3e47258a3316823450e38e53e7c5
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2020-12-17 16:30:58 -0600

    Build with Nix (#1665)
    * nix: Import expressions from nixpkgs-kframework
    
    * pom.xml: fix path to haskell-backend
    
    * ktest.mak: allow overriding path to kx
    
    * README.md: Add section explaining how to use Nix

---
+++ nix/shell.maven.nix
@@ -0,0 +1,18 @@
+# This is a shell just for updating Maven.
+
+let
+  sources = import ../nix/sources.nix;
+  pinned = import sources."nixpkgs" { config = {}; overlays = []; };
+in
+
+{ pkgs ? pinned }:
+
+let
+  inherit (pkgs) mkShell;
+
+  default = import ../. { inherit pkgs; };
+  inherit (default) mavenix;
+
+in mkShell {
+  buildInputs = [ mavenix.cli ];
+}
