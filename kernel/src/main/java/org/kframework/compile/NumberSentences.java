6b5684574d5c19c09de26386be42d1e24834140e
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2021-02-11 16:49:37 -0500

    Kompile pass to add unique identifiers to rules becomes IO pure
    * kernel/{kompile,compile,backend}: make NumberSentences pass pure
    
    * kernel/kompile: write allRules.txt file after calling kompile pipeline
    
    * kernel/compile/NumberSentences: minimize imports
    
    * kernel, kore: add declared attribute for UNIQUE_ID
    
    * kernel/: correct usage of semantic UNIQUE_ID
    
    * regression-new/issue-1169: make sure we dont die at rm step
    
    * kernel/Kompile: make sure kompiled directory exists early
    
    Co-authored-by: rv-jenkins <admin@runtimeverification.com>

--- kernel/src/main/java/org/kframework/compile/NumberSentences.java
+++ kernel/src/main/java/org/kframework/compile/NumberSentences.java
@@ -4,67 +4,24 @@ package org.kframework.compile;
 import org.bouncycastle.jcajce.provider.digest.SHA3;
 import org.bouncycastle.util.encoders.Hex;
 import org.kframework.attributes.Att;
-import org.kframework.attributes.Location;
-import org.kframework.attributes.Source;
 import org.kframework.definition.Sentence;
-import org.kframework.definition.Rule;
-import org.kframework.utils.errorsystem.KEMException;
-import org.kframework.utils.file.FileUtil;
-import org.kframework.utils.file.JarInfo;
+import org.kframework.definition.RuleOrClaim;
 
-import java.util.Optional;
+public class NumberSentences {
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-
-public class NumberSentences implements AutoCloseable {
-
-    private final FileUtil files;
-    private final PrintWriter allRulesFile;
-
-    public NumberSentences(FileUtil files) {
-        this.files = files;
-        files.resolveKompiled(".").mkdirs();
-        try {
-            allRulesFile = new PrintWriter(new BufferedWriter(new FileWriter(files.resolveKompiled("allRules.txt").getAbsolutePath())));
-        } catch (IOException e) {
-            throw KEMException.internalError("Could not write list of rules to coverage document.", e);
-        }
-    }
-
-    public Sentence number(Sentence s) {
-        if (!(s instanceof Rule)) {
+    public static Sentence number(Sentence s) {
+        if (!(s instanceof RuleOrClaim)) {
             return s;
         }
         String id = ruleHash(s.withAtt(Att.empty()));
-        Optional<Source> optFile = s.att().getOptional(Source.class);
-        Optional<Location> optLine = s.att().getOptional(Location.class);
-        Optional<Location> optCol = s.att().getOptional(Location.class);
-        if (! (optFile.isPresent() && optLine.isPresent() && optCol.isPresent())) {
-            return s;
-        }
-        String file = optFile.get().source();
-        int line = optLine.get().startLine();
-        int col = optCol.get().startColumn();
-        String loc = file + ":" + line + ":" + col;
-        allRulesFile.print(id);
-        allRulesFile.print(" ");
-        allRulesFile.println(loc);
-        return s.withAtt(s.att().add("UNIQUE_ID", id));
+        return s.withAtt(s.att().add(Att.UNIQUE_ID(), id));
     }
 
-    private String ruleHash(Sentence s) {
+    private static String ruleHash(Sentence s) {
         String text = new NormalizeVariables().normalize(s).toString();
         SHA3.Digest256 sha3engine = new SHA3.Digest256();
         byte[] digest = sha3engine.digest(text.getBytes());
         String digestString = Hex.toHexString(digest);
         return digestString;
-    }
-
-    @Override
-    public void close() {
-      allRulesFile.close();
     }
 }
--- kernel/src/main/java/org/kframework/kompile/Kompile.java
+++ kernel/src/main/java/org/kframework/kompile/Kompile.java
@@ -4,6 +4,7 @@ package org.kframework.kompile;
 import com.google.inject.Inject;
 import org.kframework.Strategy;
 import org.kframework.attributes.Att;
+import org.kframework.attributes.Location;
 import org.kframework.attributes.Source;
 import org.kframework.backend.Backends;
 import org.kframework.builtin.Sorts;
@@ -40,11 +41,14 @@ import org.kframework.utils.errorsystem.KException.ExceptionType;
 import org.kframework.utils.errorsystem.KExceptionManager;
 import org.kframework.utils.file.FileUtil;
 import org.kframework.utils.file.JarInfo;
+
+import scala.collection.JavaConverters;
 import scala.Function1;
 import scala.Option;
 
 import java.io.File;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.EnumSet;
 import java.util.HashSet;
@@ -122,6 +126,8 @@ public class Kompile {
      * @return
      */
     public CompiledDefinition run(File definitionFile, String mainModuleName, String mainProgramsModuleName, Function<Definition, Definition> pipeline, Set<String> excludedModuleTags) {
+        files.resolveKompiled(".").mkdirs();
+
         if (kompileOptions.profileRules) {
             for (File f : files.resolveKompiled(".").listFiles()) {
                 if (f.getName().matches("timing[0-9]+\\.log")) {
@@ -134,11 +140,13 @@ public class Kompile {
 
         files.saveToKompiled("parsed.txt", parsedDef.toString());
         checkDefinition(parsedDef, excludedModuleTags);
+        sw.printIntermediate("Run definition checks");
 
         Definition kompiledDefinition = pipeline.apply(parsedDef);
-
         files.saveToKompiled("compiled.txt", kompiledDefinition.toString());
         sw.printIntermediate("Apply compile pipeline");
+
+        files.saveToKompiled("allRules.txt", ruleSourceMap(kompiledDefinition));
 
         if (kompileOptions.experimental.emitJson) {
             try {
@@ -178,6 +186,27 @@ public class Kompile {
         }
 
         return def;
+    }
+
+    private static String ruleSourceMap(Definition def) {
+        List<String> ruleLocs = new ArrayList<String>();
+        for (Sentence s: JavaConverters.setAsJavaSet(def.mainModule().sentences())) {
+            if (s instanceof RuleOrClaim) {
+                Optional<Source>   optFile = s.att().getOptional(Source.class);
+                Optional<Location> optLine = s.att().getOptional(Location.class);
+                Optional<Location> optCol  = s.att().getOptional(Location.class);
+                Optional<String>   optId   = s.att().getOptional(Att.UNIQUE_ID());
+                if (optFile.isPresent() && optLine.isPresent() && optCol.isPresent() && optId.isPresent()) {
+                    String file = optFile.get().source();
+                    int line    = optLine.get().startLine();
+                    int col     = optCol.get().startColumn();
+                    String loc  = file + ":" + line + ":" + col;
+                    String id   = optId.get();
+                    ruleLocs.add(id + " " + loc);
+                }
+            }
+        }
+        return String.join("\n", ruleLocs);
     }
 
     public Definition parseDefinition(File definitionFile, String mainModuleName, String mainProgramsModule, Set<String> excludedModuleTags) {
@@ -225,8 +254,7 @@ public class Kompile {
         };
         GenerateCoverage cov = new GenerateCoverage(kompileOptions.coverage, files);
         Function1<Definition, Definition> genCoverage = d -> DefinitionTransformer.fromRuleBodyTransformerWithRule((r, body) -> cov.gen(r, body, d.mainModule()), "generate coverage instrumentation").apply(d);
-        NumberSentences numSents = new NumberSentences(files);
-        DefinitionTransformer numberSentences = DefinitionTransformer.fromSentenceTransformer(numSents::number, "number sentences uniquely");
+        DefinitionTransformer numberSentences = DefinitionTransformer.fromSentenceTransformer(NumberSentences::number, "number sentences uniquely");
         Function1<Definition, Definition> resolveConfigVar = d -> DefinitionTransformer.fromSentenceTransformer(new ResolveFunctionWithConfig(d, false)::resolveConfigVar, "Adding configuration variable to lhs").apply(d);
         Function1<Definition, Definition> resolveIO = (d -> Kompile.resolveIOStreams(kem, d));
 
@@ -237,7 +265,6 @@ public class Kompile {
                 .andThen(resolveAnonVars)
                 .andThen(d -> new ResolveContexts(kompileOptions).resolve(d))
                 .andThen(numberSentences)
-                .andThen(d -> { numSents.close(); return d; })
                 .andThen(resolveHeatCoolAttribute)
                 .andThen(resolveSemanticCasts)
                 .andThen(subsortKItem)
