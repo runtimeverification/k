b906b4bdff834b6380d687add7bd4dae0c280f17
Dwight Guth <dwight.guth@runtimeverification.com>
2021-04-09 16:42:26 -0500

    add tests for FLOAT hooks

--- kernel/src/test/java/org/kframework/compile/ConstantFoldingTest.java
+++ kernel/src/test/java/org/kframework/compile/ConstantFoldingTest.java
@@ -9,6 +9,7 @@ import org.kframework.utils.errorsystem.KEMException;
 
 import java.math.BigInteger;
 import java.util.function.BiFunction;
+import java.util.function.Function;
 
 import static org.junit.Assert.*;
 import static org.kframework.kore.KORE.*;
@@ -750,5 +751,292 @@ public class ConstantFoldingTest {
   public void testIntNe() {
     assertEquals(false, cf.INT_ne(BigInteger.valueOf(1), BigInteger.valueOf(1)));
     assertEquals(true, cf.INT_ne(BigInteger.valueOf(1), BigInteger.valueOf(0)));
+  }
+
+  @Test
+  public void testPrecision() {
+    assertEquals(BigInteger.valueOf(2), cf.FLOAT_precision(FloatBuiltin.of(new BigFloat(1.0, new BinaryMathContext(2, 8)), 8)));
+    assertEquals(BigInteger.valueOf(24), cf.FLOAT_precision(FloatBuiltin.of(new BigFloat(1.0, BinaryMathContext.BINARY32), 8)));
+  }
+
+  @Test
+  public void testExponentBits() {
+    assertEquals(BigInteger.valueOf(8), cf.FLOAT_exponentBits(FloatBuiltin.of(new BigFloat(1.0, new BinaryMathContext(2, 8)), 8)));
+    assertEquals(BigInteger.valueOf(11), cf.FLOAT_exponentBits(FloatBuiltin.of(new BigFloat(1.0, BinaryMathContext.BINARY64), 11)));
+  }
+
+  @Test
+  public void testExponent() {
+    assertEquals(BigInteger.valueOf(-127), cf.FLOAT_exponent(FloatBuiltin.of(new BigFloat(0.0, BinaryMathContext.BINARY32), 8)));
+    assertEquals(BigInteger.valueOf(-127), cf.FLOAT_exponent(FloatBuiltin.of(new BigFloat(-0.0, BinaryMathContext.BINARY32), 8)));
+    assertEquals(BigInteger.valueOf(128), cf.FLOAT_exponent(FloatBuiltin.of(new BigFloat(1.0/0.0, BinaryMathContext.BINARY32), 8)));
+    assertEquals(BigInteger.valueOf(128), cf.FLOAT_exponent(FloatBuiltin.of(new BigFloat(0.0/0.0, BinaryMathContext.BINARY32), 8)));
+    assertEquals(BigInteger.valueOf(2), cf.FLOAT_exponent(FloatBuiltin.of(new BigFloat(4.0, BinaryMathContext.BINARY32), 8)));
+    assertEquals(BigInteger.valueOf(-127), cf.FLOAT_exponent(FloatBuiltin.of(BigFloat.minValue(24, BinaryMathContext.BINARY32.minExponent), 8)));
+    assertEquals(BigInteger.valueOf(-126), cf.FLOAT_exponent(FloatBuiltin.of(BigFloat.minNormal(24, BinaryMathContext.BINARY32.minExponent), 8)));
+  }
+
+  private FloatBuiltin _float(float f) {
+    return FloatBuiltin.of(new BigFloat(f, BinaryMathContext.BINARY32), 8);
+  }
+
+  private FloatBuiltin _double(double f) {
+    return FloatBuiltin.of(new BigFloat(f, BinaryMathContext.BINARY64), 11);
+  }
+
+  @Test
+  public void testSign() {
+    assertEquals(false, cf.FLOAT_sign(_float(0.0f)));
+    assertEquals(true, cf.FLOAT_sign(_float(-0.0f)));
+    assertEquals(false, cf.FLOAT_sign(_float(1.0f/0.0f)));
+    assertEquals(true, cf.FLOAT_sign(_float(-1.0f/0.0f)));
+    assertEquals(false, cf.FLOAT_sign(_float(1.0f)));
+    assertEquals(true, cf.FLOAT_sign(_float(-1.0f)));
+    assertEquals(false, cf.FLOAT_sign(_float(3.0f)));
+    assertEquals(false, cf.FLOAT_sign(_float(0.5f)));
+    assertEquals(false, cf.FLOAT_sign(_float(0.0f/0.0f)));
+  }
+
+  @Test
+  public void testIsNaN() {
+    assertEquals(false, cf.FLOAT_isNaN(_float(0.0f)));
+    assertEquals(false, cf.FLOAT_isNaN(_float(-0.0f)));
+    assertEquals(false, cf.FLOAT_isNaN(_float(1.0f/0.0f)));
+    assertEquals(true, cf.FLOAT_isNaN(_float(0.0f/0.0f)));
+  }
+
+  @Test
+  public void testFloatNeg() {
+    testUnaryOp(cf::FLOAT_neg, a -> -a);
+  }
+
+  private double[] refs = new double[] {0.0, -0.0, 1.0/0.0, -1.0/0.0, 1.0, -1.0, 3.0, 0.5, 0.0/0.0};
+
+  private void testUnaryOp(Function<FloatBuiltin, FloatBuiltin> op, Function<Double, Double> refOp) {
+    for (int i = 0; i < refs.length; i++) {
+      FloatBuiltin result = op.apply(_double(refs[i]));
+      double ref = refOp.apply(refs[i]);
+      assertEquals(ref, result.doubleValue(), Double.MIN_VALUE);
+    }
+  }
+
+  private void testBinaryOp(String operator, BiFunction<FloatBuiltin, FloatBuiltin, FloatBuiltin> op, BiFunction<Double, Double, Double> refOp) {
+    for (int i = 0; i < refs.length; i++) {
+      for (int j = 0; j < refs.length; j++) {
+        FloatBuiltin result = op.apply(_double(refs[i]), _double(refs[j]));
+        double ref = refOp.apply(refs[i], refs[j]);
+        assertEquals("Operator " + operator + "(" + refs[i] + "," + refs[j] + ") failed", ref, result.doubleValue(), Double.MIN_VALUE);
+      }
+    }
+  }
+
+  private double pow(double a, double b) {
+    if (a == 1.0) {
+      return 1.0;
+    }
+    if (a == -1.0 && Double.isInfinite(b)) {
+      return 1.0;
+    }
+    return Math.pow(a, b);
+  }
+
+  @Test
+  public void testFloatPow() {
+    testBinaryOp("pow", cf::FLOAT_pow, this::pow);
+  }
+
+  @Test
+  public void testFloatMul() {
+    testBinaryOp("*", cf::FLOAT_mul, (a, b) -> a*b);
+  }
+
+  @Test
+  public void testFloatDiv() {
+    testBinaryOp("/", cf::FLOAT_div, (a, b) -> a/b);
+  }
+
+  @Test
+  public void testFloatRem() {
+    testBinaryOp("%", cf::FLOAT_rem, (a, b) -> a%b);
+  }
+
+  @Test
+  public void testFloatAdd() {
+    testBinaryOp("+", cf::FLOAT_add, (a, b) -> a+b);
+  }
+
+  @Test
+  public void testFloatSub() {
+    testBinaryOp("-", cf::FLOAT_sub, (a, b) -> a-b);
+  }
+
+  @Test
+  public void testRoot() {
+    testUnaryOp(f -> cf.FLOAT_root(f, BigInteger.valueOf(2)), Math::sqrt);
+  }
+
+  @Test
+  public void testFloatAbs() {
+    testUnaryOp(cf::FLOAT_abs, Math::abs);
+  }
+
+  @Test
+  public void testRound() {
+    assertEquals(12.0, cf.FLOAT_round(_double(10.5), BigInteger.valueOf(2), BigInteger.valueOf(8)).doubleValue(), Double.MIN_VALUE);
+    assertEquals(8.0, cf.FLOAT_round(_double(9.5), BigInteger.valueOf(2), BigInteger.valueOf(8)).doubleValue(), Double.MIN_VALUE);
+    assertEquals(10.5f, cf.FLOAT_round(_double(10.5), BigInteger.valueOf(24), BigInteger.valueOf(8)).floatValue(), Double.MIN_VALUE);
+    assertEquals(9.5f, cf.FLOAT_round(_double(9.5), BigInteger.valueOf(24), BigInteger.valueOf(8)).floatValue(), Double.MIN_VALUE);
+  }
+
+  @Test
+  public void testFloor() {
+    assertEquals(10.0f, cf.FLOAT_floor(_float(10.5f)).floatValue(), Double.MIN_VALUE);
+  }
+
+  @Test
+  public void testCeil() {
+    assertEquals(11.0f, cf.FLOAT_ceil(_float(10.5f)).floatValue(), Double.MIN_VALUE);
+  }
+
+  @Test
+  public void testExp() {
+    testUnaryOp(cf::FLOAT_exp, Math::exp);
+  }
+
+  @Test
+  public void testLog() {
+    testUnaryOp(cf::FLOAT_log, Math::log);
+  }
+
+  @Test
+  public void testSin() {
+    testUnaryOp(cf::FLOAT_sin, Math::sin);
+  }
+
+  @Test
+  public void testCos() {
+    testUnaryOp(cf::FLOAT_cos, Math::cos);
+  }
+
+  @Test
+  public void testTan() {
+    testUnaryOp(cf::FLOAT_tan, Math::tan);
+  }
+
+  @Test
+  public void testAsin() {
+    testUnaryOp(cf::FLOAT_asin, Math::asin);
+  }
+
+  @Test
+  public void testAcos() {
+    testUnaryOp(cf::FLOAT_acos, Math::acos);
+  }
+
+  @Test
+  public void testAtan() {
+    testUnaryOp(cf::FLOAT_atan, Math::atan);
+  }
+
+  @Test
+  public void testAtan2() {
+    testBinaryOp("atan2", cf::FLOAT_atan2, Math::atan2);
+  }
+
+  @Test
+  public void testMax() {
+    testBinaryOp("max", cf::FLOAT_max, this::max);
+  }
+
+  @Test
+  public void testMin() {
+    testBinaryOp("min", cf::FLOAT_min, this::min);
+  }
+
+  private double min(double a, double b) {
+    if (a != a) {
+      return b;
+    }
+    if (b != b) {
+      return a;
+    }
+    return Math.min(a, b);
+  }
+
+  private double max(double a, double b) {
+    if (a != a) {
+      return b;
+    }
+    if (b != b) {
+      return a;
+    }
+    return Math.max(a, b);
+  }
+
+
+  @Test
+  public void testMaxValue() {
+    assertEquals(Float.MAX_VALUE, cf.FLOAT_maxValue(BigInteger.valueOf(24), BigInteger.valueOf(8)).floatValue(), Double.MIN_VALUE);
+    assertEquals(Double.MAX_VALUE, cf.FLOAT_maxValue(BigInteger.valueOf(53), BigInteger.valueOf(11)).doubleValue(), Double.MIN_VALUE);
+  }
+
+  @Test
+  public void testMinValue() {
+    assertEquals(Float.MIN_VALUE, cf.FLOAT_minValue(BigInteger.valueOf(24), BigInteger.valueOf(8)).floatValue(), Double.MIN_VALUE);
+    assertEquals(Double.MIN_VALUE, cf.FLOAT_minValue(BigInteger.valueOf(53), BigInteger.valueOf(11)).doubleValue(), Double.MIN_VALUE);
+  }
+
+  private void testComparisonOp(String operator, BiFunction<FloatBuiltin, FloatBuiltin, Boolean> op, BiFunction<Double, Double, Boolean> refOp) {
+    for (int i = 0; i < refs.length; i++) {
+      for (int j = 0; j < refs.length; j++) {
+        boolean result = op.apply(_double(refs[i]), _double(refs[j]));
+        boolean ref = refOp.apply(refs[i], refs[j]);
+        assertEquals("Operator " + operator + "(" + refs[i] + "," + refs[j] + ") failed", ref, result);
+      }
+    }
+  }
+
+  @Test
+  public void testFloatLt() {
+    testComparisonOp("<", cf::FLOAT_lt, (a, b) -> a < b);
+  }
+
+  @Test
+  public void testFloatGt() {
+    testComparisonOp(">", cf::FLOAT_gt, (a, b) -> a > b);
+  }
+
+  @Test
+  public void testFloatLe() {
+    testComparisonOp("<=", cf::FLOAT_le, (a, b) -> a <= b);
+  }
+
+  @Test
+  public void testFloatGe() {
+    testComparisonOp(">=", cf::FLOAT_ge, (a, b) -> a >= b);
+  }
+
+  @Test
+  public void testFloatEq() {
+    testComparisonOp("==", cf::FLOAT_eq, (a, b) -> a.doubleValue() == b.doubleValue());
+  }
+
+  @Test
+  public void testFloatNe() {
+    testComparisonOp("!=", cf::FLOAT_ne, (a, b) -> a.doubleValue() != b.doubleValue());
+  }
+
+  @Test
+  public void testInt2Float() {
+    assertEquals(8.0, cf.FLOAT_int2float(BigInteger.valueOf(9), BigInteger.valueOf(2), BigInteger.valueOf(8)).doubleValue(), Double.MIN_VALUE);
+    assertEquals(12.0, cf.FLOAT_int2float(BigInteger.valueOf(11), BigInteger.valueOf(2), BigInteger.valueOf(8)).doubleValue(), Double.MIN_VALUE);
+    assertEquals(8.0, cf.FLOAT_int2float(BigInteger.valueOf(10), BigInteger.valueOf(2), BigInteger.valueOf(8)).doubleValue(), Double.MIN_VALUE);
+    assertEquals(10.0, cf.FLOAT_int2float(BigInteger.valueOf(10), BigInteger.valueOf(24), BigInteger.valueOf(8)).doubleValue(), Double.MIN_VALUE);
+  }
+
+  @Test
+  public void testFloat2Int() {
+    assertEquals(BigInteger.valueOf(10), cf.FLOAT_float2int(_double(10.5)));
+    assertEquals(BigInteger.valueOf(10), cf.FLOAT_float2int(_double(9.5)));
   }
 }
