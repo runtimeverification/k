1f5bb01c7c05080154aaf8a7311384b02b0a2884
Dwight Guth <dwight.guth@runtimeverification.com>
2021-07-07 10:34:49 -0500

    add warning if user tries to create bison parser with no syntax, and fix crash

--- kore/src/main/java/org/kframework/utils/errorsystem/KException.java
+++ kore/src/main/java/org/kframework/utils/errorsystem/KException.java
@@ -124,6 +124,7 @@ public class KException implements Serializable, HasLocation {
         REMOVED_ANYWHERE,
         DEPRECATED_DIRECTORY_FLAG,
         MISSING_HOOK,
+        EMPTY_PARSER,
         FIRST_HIDDEN, // warnings below here are hidden by default
         USELESS_RULE,
         UNRESOLVED_FUNCTION_SYMBOL,
