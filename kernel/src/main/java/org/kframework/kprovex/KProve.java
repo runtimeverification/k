33374663de3389ddd548931f19771a9e9b3c8e7b
Radu Mereuta <headness13@gmail.com>
2021-07-18 18:35:11 +0300

    Duplicate code for kprovex
    and add connection scripts

---
+++ kernel/src/main/java/org/kframework/kprovex/KProve.java
@@ -0,0 +1,126 @@
+// Copyright (c) 2015-2019 K Team. All Rights Reserved.
+package org.kframework.kprovex;
+
+import com.google.inject.Inject;
+import org.kframework.RewriterResult;
+import org.kframework.attributes.Source;
+import org.kframework.definition.Definition;
+import org.kframework.definition.Module;
+import org.kframework.definition.Rule;
+import org.kframework.kompile.CompiledDefinition;
+import org.kframework.krun.KRun;
+import org.kframework.rewriter.Rewriter;
+import org.kframework.unparser.KPrint;
+import org.kframework.unparser.ToJson;
+import org.kframework.utils.BinaryLoader;
+import org.kframework.utils.errorsystem.KEMException;
+import org.kframework.utils.errorsystem.KExceptionManager;
+import org.kframework.utils.file.FileUtil;
+import scala.Tuple2;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.function.Function;
+
+
+/**
+ * Class that implements the "--prove" option.
+ */
+public class KProve {
+
+    public static final String BOUNDARY_CELL_PREFIX = "BOUND_";
+
+    private final KExceptionManager kem;
+    private final FileUtil files;
+    private final KPrint kprint;
+    private final KProveOptions kproveOptions;
+    private final CompiledDefinition compiledDefinition;
+    private final BinaryLoader loader;
+    private final ProofDefinitionBuilder proofDefinitionBuilder;
+    private final Function<Definition, Rewriter> rewriterGenerator;
+
+    @Inject
+    public KProve(KExceptionManager kem, FileUtil files, KPrint kprint, KProveOptions kproveOptions,
+                  CompiledDefinition compiledDefinition, BinaryLoader loader,
+                  ProofDefinitionBuilder proofDefinitionBuilder, Function<Definition, Rewriter> rewriterGenerator) {
+        this.kem = kem;
+        this.files = files;
+        this.kprint = kprint;
+        this.kproveOptions = kproveOptions;
+        this.compiledDefinition = compiledDefinition;
+        this.loader = loader;
+        this.proofDefinitionBuilder = proofDefinitionBuilder;
+        this.rewriterGenerator = rewriterGenerator;
+    }
+
+    public int run() {
+        if (!kproveOptions.specFile(files).exists()) {
+            throw KEMException.criticalError("Definition file doesn't exist: " +
+                    kproveOptions.specFile(files).getAbsolutePath());
+        }
+
+        Tuple2<Definition, Module> compiled = proofDefinitionBuilder
+                .build(kproveOptions.specFile(files), kproveOptions.defModule, kproveOptions.specModule, compiledDefinition.kompileOptions.readOnlyKompiledDirectory);
+
+        if (kproveOptions.saveProofDefinitionTo != null) {
+            saveFullDefinition(compiled._1());
+        }
+
+        Rewriter rewriter = rewriterGenerator.apply(compiled._1());
+        Module specModule = compiled._2();
+        Rule boundaryPattern = buildBoundaryPattern(compiledDefinition);
+
+        if (kproveOptions.emitJson) {
+            try {
+                files.saveToKompiled("prove-definition.json", new String(ToJson.apply(compiled._1()), "UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                throw KEMException.criticalError("Unsupported encoding `UTF-8` when saving JSON definition.");
+            }
+        }
+
+        RewriterResult results = rewriter.prove(specModule, boundaryPattern);
+        kprint.prettyPrint(compiled._1(), compiled._1().getModule("LANGUAGE-PARSING").get(), kprint::outputFile,
+                results.k());
+        return results.exitCode().orElse(KEMException.TERMINATED_WITH_ERRORS_EXIT_CODE);
+    }
+
+    // Saving combined verification definition to disk to be usable by other tools (e.g., kast)
+    private void saveFullDefinition(Definition fullDefinition) {
+        CompiledDefinition fullCompiledDefinition = new CompiledDefinition(
+                compiledDefinition.kompileOptions,
+                fullDefinition, fullDefinition,
+                files, kem, compiledDefinition.topCellInitializer);
+        Path proveKompiledDir = Paths.get(kproveOptions.saveProofDefinitionTo).resolve("prove-spec-kompiled");
+        try {
+            Files.createDirectories(proveKompiledDir);
+            loader.saveOrDie(proveKompiledDir.resolve("compiled.bin").toFile(), fullCompiledDefinition);
+        } catch (IOException e) {
+            throw KEMException.criticalError(
+                    "Could not create proof output directory " + proveKompiledDir.toAbsolutePath(), e);
+        }
+    }
+
+    /**
+     * A pattern that implements --boundary-cells functionality. When this pattern matches, in the resulting
+     * substitution, for each boundary cell there will be a variable starting with {@code "BOUND_"}. Other variables
+     * must be ignored.
+     *
+     * @return the rule corresponding to boundary pattern, or null if no boundary cells were set.
+     */
+    public Rule buildBoundaryPattern(CompiledDefinition compiledDefinition) {
+        if (kproveOptions.boundaryCells.isEmpty()) {
+            return null;
+        }
+        StringBuilder patternStr = new StringBuilder();
+        for (String cell : kproveOptions.boundaryCells) {
+            //for each boundary cell add a sequence of the form `<cell> VAR </cell>`
+            patternStr.append(String.format("<%2$s> %1$s%2$s </%2$s> ", BOUNDARY_CELL_PREFIX, cell));
+        }
+
+        return KRun.compilePattern(files, kem, patternStr.toString(), compiledDefinition,
+                Source.apply("<option --boundary-cells>"));
+    }
+}
---
+++ kernel/src/main/java/org/kframework/kprovex/ProofDefinitionBuilder.java
@@ -0,0 +1,110 @@
+package org.kframework.kprovex;
+
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+import org.apache.commons.io.FilenameUtils;
+import org.kframework.compile.Backend;
+import org.kframework.definition.Definition;
+import org.kframework.definition.Module;
+import org.kframework.kompile.CompiledDefinition;
+import org.kframework.kompile.Kompile;
+import org.kframework.utils.errorsystem.KEMException;
+import org.kframework.utils.file.FileUtil;
+import scala.Option;
+import scala.Tuple2;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * @author Denis Bogdanas
+ * Created on 07-Nov-19.
+ */
+public class ProofDefinitionBuilder {
+
+    private static Map<Definition, Definition>
+            cache = Collections.synchronizedMap(new LinkedHashMap<Definition, Definition>() {
+        @Override
+        protected boolean removeEldestEntry(Map.Entry entry) {
+            return size() > 10;
+        }
+    });
+
+
+    private final CompiledDefinition compiledDefinition;
+    private final Backend backend;
+    private final Kompile kompile;
+    private final FileUtil files;
+    @Inject(optional = true)
+    @Named("extraConcreteRuleLabels")
+    private List<String> extraConcreteRuleLabels = null;
+
+    @Inject
+    public ProofDefinitionBuilder(CompiledDefinition compiledDefinition, Backend backend, Kompile kompile,
+                                  FileUtil files) {
+        this.compiledDefinition = compiledDefinition;
+        this.backend = backend;
+        this.kompile = kompile;
+        this.files = files;
+    }
+
+    /**
+     * @param specFile       File containing specification rules to prove. Not part of definition.
+     * @param defModuleName  Name of main module of extended definition - that is compiled definition + extra
+     *                       modules required by proofs, usually abstractions for symbolic execution and lemmas.
+     * @param specModuleName Module containing specifications to prove
+     */
+    public Tuple2<Definition, Module> build(File specFile, String defModuleName, String specModuleName) {
+        return build(specFile, defModuleName, specModuleName, false);
+    }
+
+    public Tuple2<Definition, Module> build(File specFile, String defModuleName, String specModuleName, boolean readOnlyCache) {
+        String defModuleNameUpdated =
+                defModuleName == null ? compiledDefinition.kompiledDefinition.mainModule().name() : defModuleName;
+        String specModuleNameUpdated =
+                specModuleName == null ? FilenameUtils.getBaseName(specFile.getName()).toUpperCase() : specModuleName;
+        File absSpecFile = files.resolveWorkingDirectory(specFile).getAbsoluteFile();
+
+        Set<Module> modules = kompile.parseModules(compiledDefinition, defModuleNameUpdated, specModuleNameUpdated, absSpecFile,
+                backend.excludedModuleTags(), readOnlyCache);
+        Map<String, Module> modulesMap = modules.stream().collect(Collectors.toMap(Module::name, m -> m));
+        Definition parsedDefinition = compiledDefinition.getParsedDefinition();
+        Module specModule = getModule(specModuleNameUpdated, modulesMap, parsedDefinition);
+        specModule = kompile.proverChecks(specModule, modulesMap.get(defModuleNameUpdated));
+        kompile.structuralChecks(scala.collection.JavaConverters.asScalaSet(modules),
+                specModule, Option.empty(), backend.excludedModuleTags());
+        Module defModule = getModule(defModuleNameUpdated, modulesMap, parsedDefinition);
+        Definition rawExtendedDef = Definition.apply(defModule, parsedDefinition.entryModules(),
+                parsedDefinition.att());
+        Definition compiledExtendedDef = compileDefinition(backend, rawExtendedDef); //also resolves imports
+        compiledExtendedDef = backend.proofDefinitionNonCachedSteps(extraConcreteRuleLabels).apply(compiledExtendedDef);
+
+        specModule = backend.specificationSteps(compiledDefinition.kompiledDefinition).apply(specModule);
+
+        return Tuple2.apply(compiledExtendedDef, specModule);
+    }
+
+    private static Module getModule(String defModule, Map<String, Module> modules, Definition parsedDefinition) {
+        if (modules.containsKey(defModule))
+            return modules.get(defModule);
+        Option<Module> mod = parsedDefinition.getModule(defModule);
+        if (mod.isDefined()) {
+            return mod.get();
+        }
+        throw KEMException.criticalError("Module " + defModule + " does not exist.");
+    }
+
+    private static Definition compileDefinition(Backend backend, Definition combinedDef) {
+        Definition compiled = cache.get(combinedDef);
+        if (compiled == null) {
+            compiled = backend.steps().apply(combinedDef);
+            cache.put(combinedDef, compiled);
+        }
+        return compiled;
+    }
+}
