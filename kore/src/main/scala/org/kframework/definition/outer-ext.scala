2ad863d9909dd703dc6813b5e2a733bdb414c53c
Dwight Guth <dwight.guth@runtimeverification.com>
2021-08-17 15:00:18 -0500

    rename Import -> FlatImport

--- kore/src/main/scala/org/kframework/definition/outer-ext.scala
+++ kore/src/main/scala/org/kframework/definition/outer-ext.scala
@@ -24,12 +24,12 @@ case class Bubble(sentenceType: String, contents: String, att: Att = Att.empty) 
   override def withAtt(att: Att) = Bubble(sentenceType, contents, att)
 }
 
-case class Import(name: String, isPublic: Boolean, att: Att = Att.empty) extends HasLocation {
+case class FlatImport(name: String, isPublic: Boolean, att: Att = Att.empty) extends HasLocation {
   override def location(): Optional[Location] = att.getOptional(classOf[Location])
   override def source(): Optional[Source] = att.getOptional(classOf[Source])
 }
 
-case class FlatModule(name: String, imports: Set[Import], localSentences: Set[Sentence], @(Nonnull@param) val att: Att = Att.empty)
+case class FlatModule(name: String, imports: Set[FlatImport], localSentences: Set[Sentence], @(Nonnull@param) val att: Att = Att.empty)
   extends OuterKORE with Sorting with Serializable {
 }
 
@@ -56,7 +56,7 @@ object FlatModule {
       }
       memoization.getOrElseUpdate(m.name, {
         // transform all imports into Module
-        val f = (i: Import) => memoization.getOrElse(i.name
+        val f = (i: FlatImport) => memoization.getOrElse(i.name
           // if can't find the Module in memoization, build a new one
           , toModuleRec(allModules.find(f => f.name.equals(i.name))
               .getOrElse(throw KEMException.compilerError("Could not find module: " + i.name, i))
