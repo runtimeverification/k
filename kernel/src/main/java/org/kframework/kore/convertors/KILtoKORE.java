6188a6b2e94490e367d1bcfeb7b4dc1fdafde9f4
Dwight Guth <dwight.guth@runtimeverification.com>
2021-08-18 14:37:47 -0500

    fix kiltokore

--- kernel/src/main/java/org/kframework/kore/convertors/KILtoKORE.java
+++ kernel/src/main/java/org/kframework/kore/convertors/KILtoKORE.java
@@ -28,6 +28,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -90,7 +91,7 @@ public class KILtoKORE extends KILTransformation<Object> {
     }
 
     public org.kframework.definition.FlatImport apply(Import imp) {
-        return org.kframework.definition.FlatImport.apply(imp.getName(), imp.isPublic(), convertAttributes(imp));
+        return org.kframework.definition.FlatImport.apply(imp.getName(), imp.isPublic(), imp.getTag() == null ? immutable(Optional.empty()) : immutable(Optional.of(Tag(imp.getTag()))), convertAttributes(imp));
     }
 
     public org.kframework.definition.Definition apply(Definition d) {
