runtimeverification/k

commit 309f81b73c0c3a447e78f4fd2bb413203e519a1d

Implement -I, --md-selector, and --no-prelude in kprove, kprovex, keq, and kbmc (#2172)
* Extract -d from OuterParsingOptions

This extraction prepares OuterParsingOptions class to be included in the
following tools: kprove, kprovex, keq, and kmbc.

* KProve, KEq, KBMC: add OuterParsing options

Adding this object enables the `-I`, `--md-selector`, and `--no-prelude`
options to these tools. By doing so, each tool can configure their own
OuterParsing options rather than getting this information by de-serializing
the kompile object from the cache left by a previous invocation of the
`kompile` command.

* pyk tests: remove -I from kompile add -I to kprove

This test case utilized -I in the kompile command in order to pass the
parameter of this option to kprove. This was done through serializing
the `kompile` object during the invocation of the `kompile` command and
de-serializing the `kompile` object to access the `outerParsingOption`
during the invocation of the `kprove` command. Since the -I option has
been added to `kprove`, drop this command from the `kompile` command and
move it to the `kprove` command.

* add code review suggestions

* Compiled definition: add outerParsingOptions

* KompileFrontEnd: use injected OuterParsingOptions

* kprove-markdown: use --md-selector for testing

This test adds additional lines in a test file to test the new --md-selector
option for kprove. Before this feature was added, the default behavior was for
kprove to use k as the selector. In order to test that the --md-selector
option is functional, text has been added to the test case so that it will
cause a failure on the default md-selector.

* Kprove: use KproveOptions.outerparsing to initialize kompiledDefinition

* KompileOptions: make OuterparsingOptions transient

Co-authored-by: rv-jenkins <admin@runtimeverification.com>

kernel/src/main/java/org/kframework/kdep/KDepModule.java
@@ -5,6 +5,7 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.TypeLiteral;
 import com.google.inject.multibindings.Multibinder;
+import org.kframework.kompile.KompileOptions;
 import org.kframework.main.FrontEnd;
 import org.kframework.main.GlobalOptions;
 import org.kframework.main.Tool;
@@ -12,6 +13,7 @@ import org.kframework.utils.inject.Options;
 import org.kframework.utils.inject.OuterParsingModule;
 import org.kframework.utils.inject.RequestScoped;
 import org.kframework.utils.options.OuterParsingOptions;
+import org.kframework.utils.options.OutputDirectoryOptions;
 
 /**
  * Guice module for kdep tool. Binds the information needed to compute the kompiled directory as well as the options
@@ -36,7 +38,8 @@ public class KDepModule extends AbstractModule {
     }
 
     @Provides
-    OuterParsingOptions outerParsingOptions(KDepOptions options) {
-        return options.outerParsing;
-    }
+    OuterParsingOptions outerParsingOptions(KDepOptions options) { return options.outerParsing; }
+
+    @Provides
+    OutputDirectoryOptions outputDirectoryOptions(KompileOptions options) { return options.outputDirectory; }
 }

k-distribution/src/test/scala/org/kframework/backend/kore/KoreTest.scala
@@ -40,7 +40,7 @@ class KoreTest {
 
   def kompile(k: String): Definition = {
     val go = new GlobalOptions();
-    val compiler = new Kompile(options, go, files, kem, new Stopwatch(go), false)
+    val compiler = new Kompile(options, new OuterParsingOptions(), go, files, kem, new Stopwatch(go), false)
     val backend = new KoreBackend(options, files, kem, Tool.KOMPILE)
     files.saveToDefinitionDirectory("test.k", k)
     val defn = compiler.run(files.resolveDefinitionDirectory("test.k"), "TEST", "TEST", backend.steps, backend.excludedModuleTags)

k-distribution/tests/pyk/Makefile
@@ -36,7 +36,7 @@ $(llvm_imp_kompiled): $(llvm_imp) $(KOMPILE)
 	$(KOMPILE) --directory $(llvm_dir) --backend llvm -I $(llvm_dir) $< --emit-json --coverage
 
 $(haskell_imp_kompiled): $(haskell_imp) $(KOMPILE)
-	$(KOMPILE) --directory $(haskell_dir) --backend haskell -I $(haskell_dir) $< --emit-json
+	$(KOMPILE) --directory $(haskell_dir) --backend haskell $< --emit-json
 
 update-results:
 kompile: $(llvm_imp_kompiled) $(haskell_imp_kompiled)
@@ -75,7 +75,7 @@ $(defn_tests)/kast-tests/%.out: $(defn_tests)/kast-tests/%.gen $(llvm_imp_kompil
 	$(KAST) --directory $(llvm_dir) --output pretty --sort $(basename $(basename $*)) -m IMP $< > $@
 
 $(defn_tests)/proof-tests/%-spec.json.prove: $(defn_tests)/proof-tests/%-spec.k $(haskell_imp_kompiled)
-	$(KPROVE) --directory $(haskell_dir) $< -m IMP
+	$(KPROVE) --directory $(haskell_dir) $< -m IMP -I $(haskell_dir)
 
 ## kpyk runner tests
 

k-distribution/tests/regression-new/kprove-markdown/Makefile
@@ -3,6 +3,6 @@ EXT=set-balance
 KOMPILE_BACKEND=haskell
 KOMPILE_FLAGS=--syntax-module SET-BALANCE
 TESTDIR=.
-KPROVE_FLAGS=--def-module VERIFICATION
+KPROVE_FLAGS=--def-module VERIFICATION --md-selector 'keep&!(discard|k)'
 
 include ../../../include/kframework/ktest.mak

kernel/src/main/java/org/kframework/kbmc/KBMCModule.java
@@ -15,6 +15,7 @@ import org.kframework.utils.inject.Options;
 import org.kframework.utils.inject.RequestScoped;
 import org.kframework.utils.options.BackendOptions;
 import org.kframework.utils.options.DefinitionLoadingOptions;
+import org.kframework.utils.options.OuterParsingOptions;
 import org.kframework.utils.options.SMTOptions;
 
 public class KBMCModule extends AbstractModule {
@@ -32,6 +33,9 @@ public class KBMCModule extends AbstractModule {
 
     @Provides @RequestScoped
     GlobalOptions globalOptions(KBMCOptions options) { return options.global; }
+
+    @Provides @RequestScoped
+    OuterParsingOptions outerParsingOptions(KBMCOptions options) { return options.outer; }
 
     @Provides @RequestScoped
     PrintOptions printOptions(KBMCOptions options) {

kernel/src/main/java/org/kframework/kbmc/KBMCOptions.java
@@ -10,6 +10,7 @@ import org.kframework.utils.file.FileUtil;
 import org.kframework.utils.inject.RequestScoped;
 import org.kframework.utils.options.BackendOptions;
 import org.kframework.utils.options.DefinitionLoadingOptions;
+import org.kframework.utils.options.OuterParsingOptions;
 import org.kframework.utils.options.SMTOptions;
 
 import java.io.File;
@@ -19,6 +20,9 @@ public class KBMCOptions {
 
     @ParametersDelegate
     public transient GlobalOptions global = new GlobalOptions();
+
+    @ParametersDelegate
+    public transient OuterParsingOptions outer = new OuterParsingOptions();
 
     @ParametersDelegate
     public DefinitionLoadingOptions definitionLoading = new DefinitionLoadingOptions();

kernel/src/main/java/org/kframework/kdep/KDepOptions.java
@@ -5,6 +5,7 @@ import com.beust.jcommander.ParametersDelegate;
 import org.kframework.main.GlobalOptions;
 import org.kframework.utils.inject.RequestScoped;
 import org.kframework.utils.options.OuterParsingOptions;
+import org.kframework.utils.options.OutputDirectoryOptions;
 
 /**
  * JCommander options for kdep. Essentially, this should contain all the kompile options needed in order to decide what
@@ -19,4 +20,7 @@ public class KDepOptions {
 
     @ParametersDelegate
     public OuterParsingOptions outerParsing = new OuterParsingOptions();
+
+    @ParametersDelegate
+    public OutputDirectoryOptions outputDirectory = new OutputDirectoryOptions();
 }

kernel/src/main/java/org/kframework/keq/KEqModule.java
@@ -30,6 +30,7 @@ import org.kframework.utils.inject.RequestScoped;
 import org.kframework.utils.inject.Spec1;
 import org.kframework.utils.inject.Spec2;
 import org.kframework.utils.options.DefinitionLoadingOptions;
+import org.kframework.utils.options.OuterParsingOptions;
 import org.kframework.utils.options.SMTOptions;
 
 import java.util.List;
@@ -73,6 +74,9 @@ public class KEqModule extends AbstractModule {
     GlobalOptions globalOptions(KEqOptions options) {
         return options.global;
     }
+
+    @Provides @RequestScoped
+    OuterParsingOptions outerParsingOptions(KEqOptions options) { return options.outer; }
 
     @Provides
     SMTOptions smtOptions(KEqOptions options) { return options.smt; }

commit 75d18966514a4d8d8fcd5f4de1c5517f32a1fc0b

kdep: fix broken -d option
Commit 309f81b73c introduced a spelling error in KDepModule.java that
broke kdep's -d option. Fix this by providing the correct object
OutputDirectoryOption in the provider.

kernel/src/main/java/org/kframework/kdep/KDepModule.java
@@ -41,5 +41,5 @@ public class KDepModule extends AbstractModule {
     OuterParsingOptions outerParsingOptions(KDepOptions options) { return options.outerParsing; }
 
     @Provides
-    OutputDirectoryOptions outputDirectoryOptions(KompileOptions options) { return options.outputDirectory; }
+    OutputDirectoryOptions outputDirectoryOptions(KDepOptions options) { return options.outputDirectory; }
 }