2fe9f7b37a3e088e0f3b2d1c7ae8c7ecc3d20e73
Bruce Collie <brucecollie82@gmail.com>
2021-10-07 09:56:29 +0100

    Add option to kdep to remake dependency file
    This is an option that only makes sense for KDep; it uses the Makefile
    constructions suggested here
    (https://www.cmcrossroads.com/article/tips-and-tricks-automatic-dependency-generation-masters)
    to automatically rebuild the dependency file with a given name when any
    of the sources change.

--- kernel/src/main/java/org/kframework/kdep/KDepOptions.java
+++ kernel/src/main/java/org/kframework/kdep/KDepOptions.java
@@ -1,6 +1,7 @@
 // Copyright (c) 2015-2019 K Team. All Rights Reserved.
 package org.kframework.kdep;
 
+import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParametersDelegate;
 import org.kframework.main.GlobalOptions;
 import org.kframework.utils.inject.RequestScoped;
@@ -23,4 +24,7 @@ public class KDepOptions {
 
     @ParametersDelegate
     public OutputDirectoryOptions outputDirectory = new OutputDirectoryOptions();
+
+    @Parameter(names="--remake-depend", description="Generate an additional rule to remake the dependency file.")
+    public String alsoDependName;
 }
