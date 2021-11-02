76b79f9aec80f022ff8197579d906feec21bfb83
Erik Kaneda <erik.kaneda@runtimeverification.com>
2021-11-02 15:25:33 -0700

    Do no propagate macro labels to simplification rules
    These rules are intended for the haskell backend and should not be
    treated as macro rules.

--- kernel/src/main/java/org/kframework/compile/PropagateMacro.java
+++ kernel/src/main/java/org/kframework/compile/PropagateMacro.java
@@ -8,7 +8,9 @@ import org.kframework.kore.KLabel;
 
 /**
  * Propagate macro, macro-rec, alias, and alias-rec labels from productions to rules that only contain that klabel on the LHS
- * This prepares rules for macro expansion in ExpandMacros
+ * This prepares rules for macro expansion in ExpandMacros.
+ * There is one exception: simplification rules are meant to be used for the haskell backend and macros should not be propagated
+ * to these rules.
  */
 public class PropagateMacro {
     private final Module m;
@@ -18,7 +20,7 @@ public class PropagateMacro {
     }
 
     public Sentence propagate(Sentence s) {
-        if (s instanceof Rule && m.ruleLhsHasMacroKLabel((Rule) s)) {
+        if (s instanceof Rule && m.ruleLhsHasMacroKLabel((Rule) s) && !((Rule) s).att().contains(Att.SIMPLIFICATION())) {
             Att macroAtt = m.attributesFor().apply(m.matchKLabel((Rule) s));
             return Rule.apply(((Rule) s).body(), ((Rule) s).requires(), ((Rule) s).ensures(), s.att().add(macroAtt.getMacro().get()));
         }
