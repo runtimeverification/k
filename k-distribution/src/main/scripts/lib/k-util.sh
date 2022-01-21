36ddd14dd2355acb5de4f4cd1c8bad14862f92c9
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2022-01-21 19:14:34 +0000

    k-distribution/{k-util,k*}: factor out verbose option into k-util helper

--- k-distribution/src/main/scripts/lib/k-util.sh
+++ k-distribution/src/main/scripts/lib/k-util.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script is designed to provide a common bash library of functions for
-# scripts that are written primarily in bash. Right now, that consists of 
+# scripts that are written primarily in bash. Right now, that consists of
 # kore-print, kparse, kparse-gen, and krun. There is probably some code
 # shared among those files that could be factored out further into this
 # script, but this is an initial effort to factor out some of the most obvious
@@ -10,6 +10,41 @@
 # your script:
 #
 # source "$(dirname "$0")/../lib/kframework/k-util.sh"
+
+verbose=false
+
+k_util_usage() {
+    cat <<HERE
+  -v, --verbose            Print significant sub-commands executed
+HERE
+}
+
+args=()
+literal=false
+while [[ "$#" -gt 0 ]]
+do
+  arg="$1" ; shift
+  if ${literal}; then
+    args+=("${arg}")
+  else
+    case "${arg}" in
+      --verbose)
+      verbose=true
+      ;;
+
+      --)
+      literal=true
+      ;;
+
+      *)
+      args+=("${arg}")
+      ;;
+    esac
+  fi
+done
+if [[ "${#args[@]}" -gt 0 ]]; then
+  set -- "${args[@]}"
+fi
 
 execute () {
   (
