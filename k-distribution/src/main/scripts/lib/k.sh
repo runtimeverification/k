7ecad9e1a48eaa6eeb838d90ac706f02b3e4d115
Dwight Guth <dwight.guth@runtimeverification.com>
2022-01-21 10:46:21 -0600

    add comment

--- k-distribution/src/main/scripts/lib/k.sh
+++ k-distribution/src/main/scripts/lib/k.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+# This script is designed to provide a common bash library of functions for
+# scripts that are written primarily in bash. Right now, that consists of 
+# kore-print, kparse, kparse-gen, and krun. There is probably some code
+# shared among those files that could be factored out further into this
+# script, but this is an initial effort to factor out some of the most obvious
+# cases.
+#
+# You can use this script by adding the following line (minus the comment) to
+# your script:
+#
+# source "$(dirname "$0")/../lib/kframework/k.sh"
 
 execute () {
   (
