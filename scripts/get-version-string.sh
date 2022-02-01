c7b822b5d41748a98e0f27ae4596d9090772c6f6
Bruce Collie <brucecollie82@gmail.com>
2022-02-01 10:21:40 +0000

    Walk revlist to get last actual release tag

--- scripts/get-version-string.sh
+++ scripts/get-version-string.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-MERGE_BASE=$(git merge-base master HEAD)
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+RELEASE_TAG=$("$SCRIPTS_DIR/newest-release-tag.sh")
+
+MERGE_BASE=$(git merge-base "$RELEASE_TAG" HEAD)
 VERSION_TAG=$(git describe --contains --always "$MERGE_BASE" | sed 's/~.*//')
 
 VERSION_STRING=$VERSION_TAG
