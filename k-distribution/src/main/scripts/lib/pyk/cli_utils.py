a52c12219eb512df55ed4d483681ea3f7235b78d
Tamas Toth <tamas.toth@runtimeverification.com>
2022-02-17 10:22:24 +0100

    Add module cli_utils

---
+++ k-distribution/src/main/scripts/lib/pyk/cli_utils.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+
+def check_dir_path(path: Path) -> None:
+    if not path.exists():
+        raise ValueError(f'Directory does not exist: {path}')
+    if not path.is_dir():
+        raise ValueError(f'Path is not a directory: {path}')
+
+
+def dir_path(s: str) -> Path:
+    path = Path(s)
+    check_dir_path(path)
+    return path
+
+
+def check_file_path(path: Path) -> None:
+    if not path.exists():
+        raise ValueError(f'File does not exist: {path}')
+    if not path.is_file():
+        raise ValueError(f'Path is not a file: {path}')
+
+
+def file_path(s: str) -> Path:
+    path = Path(s)
+    check_file_path(path)
+    return path
