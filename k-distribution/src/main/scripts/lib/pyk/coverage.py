700b56c6bfb3f07a7f65101a9414615668e02494
Tamas Toth <tamas.toth@runtimeverification.com>
2022-02-17 16:54:39 +0100

    Fix linting issues in coverage

--- k-distribution/src/main/scripts/lib/pyk/coverage.py
+++ k-distribution/src/main/scripts/lib/pyk/coverage.py
@@ -29,11 +29,12 @@ def getRuleById(definition, rule_id):
                     return sentence
     fatal('Could not find rule with ID: ' + rule_id)
 
+
 def stripCoverageLogger(rule):
-    ruleBody     = rule['body']
+    ruleBody = rule['body']
     ruleRequires = rule['requires']
-    ruleEnsures  = rule['ensures']
-    ruleAtts     = rule['att']
+    ruleEnsures = rule['ensures']
+    ruleAtts = rule['att']
 
     if isKRewrite(ruleBody):
         ruleLHS = ruleBody['lhs']
@@ -42,7 +43,8 @@ def stripCoverageLogger(rule):
             ruleRHSseq = ruleRHS['args'][0]
             if isKSequence(ruleRHSseq) and len(ruleRHSseq['items']) == 2:
                 ruleBody = KRewrite(ruleLHS, ruleRHSseq['items'][1])
-    return KRule(ruleBody, requires = ruleRequires, ensures = ruleEnsures, att = ruleAtts)
+    return KRule(ruleBody, requires=ruleRequires, ensures=ruleEnsures, att=ruleAtts)
+
 
 def translateCoverage(src_all_rules, dst_all_rules, dst_definition, src_rules_list):
     """Translate the coverage data from one kompiled definition to another.
@@ -60,18 +62,18 @@ def translateCoverage(src_all_rules, dst_all_rules, dst_definition, src_rules_li
     # Load the src_rule_id -> src_source_location rule map from the src kompiled directory
     src_rule_map = {}
     for line in src_all_rules:
-        [ src_rule_hash, src_rule_loc ] = line.split(' ')
+        src_rule_hash, src_rule_loc = line.split(' ')
         src_rule_loc = src_rule_loc.split('/')[-1]
         src_rule_map[src_rule_hash.strip()] = src_rule_loc.strip()
 
     # Load the dst_rule_id -> dst_source_location rule map (and inverts it) from the dst kompiled directory
     dst_rule_map = {}
     for line in dst_all_rules:
-        [ dst_rule_hash, dst_rule_loc ] = line.split(' ')
+        dst_rule_hash, dst_rule_loc = line.split(' ')
         dst_rule_loc = dst_rule_loc.split('/')[-1]
         dst_rule_map[dst_rule_loc.strip()] = dst_rule_hash.strip()
 
-    src_rule_list = [ rule_hash.strip() for rule_hash in src_rules_list ]
+    src_rule_list = [rule_hash.strip() for rule_hash in src_rules_list]
 
     # Filter out non-functional rules from rule map (determining if they are functional via the top symbol in the rule being `<generatedTop>`)
     dst_non_function_rules = []
@@ -79,9 +81,9 @@ def translateCoverage(src_all_rules, dst_all_rules, dst_definition, src_rules_li
         for sentence in module['localSentences']:
             if isKRule(sentence):
                 ruleBody = sentence['body']
-                ruleAtt  = sentence['att']['att']
-                if    (isKApply(ruleBody)                                 and ruleBody['label']        == '<generatedTop>') \
-                   or (isKRewrite(ruleBody) and isKApply(ruleBody['lhs']) and ruleBody['lhs']['label'] == '<generatedTop>'):
+                ruleAtt = sentence['att']['att']
+                if (isKApply(ruleBody) and ruleBody['label'] == '<generatedTop>') \
+                        or (isKRewrite(ruleBody) and isKApply(ruleBody['lhs']) and ruleBody['lhs']['label'] == '<generatedTop>'):
                     if 'UNIQUE_ID' in ruleAtt:
                         dst_non_function_rules.append(ruleAtt['UNIQUE_ID'])
 
@@ -101,6 +103,7 @@ def translateCoverage(src_all_rules, dst_all_rules, dst_definition, src_rules_li
 
     return dst_rule_list
 
+
 def translateCoverageFromPaths(src_kompiled_dir, dst_kompiled_dir, src_rules_file):
     """Translate coverage information given paths to needed files.
 
@@ -114,16 +117,16 @@ def translateCoverageFromPaths(src_kompiled_dir, dst_kompiled_dir, src_rules_fil
     """
     src_all_rules = []
     with open(src_kompiled_dir + '/allRules.txt', 'r') as src_all_rules_file:
-        src_all_rules = [ line.strip() for line in src_all_rules_file ]
+        src_all_rules = [line.strip() for line in src_all_rules_file]
 
     dst_all_rules = []
     with open(dst_kompiled_dir + '/allRules.txt', 'r') as dst_all_rules_file:
-        dst_all_rules = [ line.strip() for line in dst_all_rules_file ]
+        dst_all_rules = [line.strip() for line in dst_all_rules_file]
 
     dst_definition = readKastTerm(dst_kompiled_dir + '/compiled.json')
 
     src_rules_list = []
     with open(src_rules_file, 'r') as src_rules:
-        src_rules_list = [ line.strip() for line in src_rules ]
+        src_rules_list = [line.strip() for line in src_rules]
 
     return translateCoverage(src_all_rules, dst_all_rules, dst_definition, src_rules_list)
