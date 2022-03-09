f79609e9f2e0e1d1a01810e4bba8b7915a92e9ce
Tamas Toth <tamas.toth@runtimeverification.com>
2022-03-09 16:37:42 +0100

    Apply some fixes on configuration_test.py

--- k-distribution/tests/pyk/configuration_test.py
+++ k-distribution/tests/pyk/configuration_test.py
@@ -1,17 +1,31 @@
 from abc import ABC
 from unittest import TestCase
 
-from pyk.kast import KAs, KDefinition, KRule, KSort, KSortSynonym, readKastTerm
+from pyk.kast import (
+    KApply,
+    KDefinition,
+    KSequence,
+    KVariable,
+    ktokenDots,
+    readKastTerm,
+)
+from pyk.kastManip import (
+    collapseDots,
+    removeGeneratedCells,
+    structurallyFrameKCell,
+    substitute,
+)
 
 
 class ConfigurationTest(TestCase, ABC):
-    COMPILED_JSON_PATH = 'definitions/imp-verification/haskell/imp-verification-kompiled'
+    COMPILED_JSON_PATH = 'definitions/imp-verification/haskell/imp-verification-kompiled/compiled.json'
     MODULE_NAME = 'IMP-VERIFICATION'
+
     K_CELL = KApply('<k>', [KSequence([KVariable('S1'), KVariable('_DotVar0')])])
     T_CELL = KApply('<T>', [K_CELL, KApply('<state>', [KVariable('MAP')])])
     GENERATED_COUNTER_CELL = KApply('<generatedCounter>', [KVariable('X')])
     GENERATED_TOP_CELL_1 = KApply('<generatedTop>', [T_CELL, KVariable('_GENERATED_COUNTER_PLACEHOLDER')])
-    GENERATED_TOP_CELL_1 = KApply('<generatedTop>', [T_CELL, GENERATED_COUNTER_CELL])
+    GENERATED_TOP_CELL_2 = KApply('<generatedTop>', [T_CELL, GENERATED_COUNTER_CELL])
 
     def setUp(self):
         self.definition = readKastTerm(self.COMPILED_JSON_PATH)
@@ -22,10 +36,10 @@ class StructurallyFrameKCellTest(ConfigurationTest):
 
     def test(self):
         # Given
-        config_expected = substitute(GENERATED_TOP_CELL_1, {'_DotVar0': ktokenDots})
+        config_expected = substitute(self.GENERATED_TOP_CELL_1, {'_DotVar0': ktokenDots})
 
         # When
-        config_actual = structurallyFrameKCell(GENERATED_TOP_CELL_1)
+        config_actual = structurallyFrameKCell(self.GENERATED_TOP_CELL_1)
 
         # Then
         self.assertEqual(config_actual, config_expected)
@@ -33,27 +47,27 @@ class StructurallyFrameKCellTest(ConfigurationTest):
 
 class RemoveGeneratedCounterTest(ConfigurationTest):
 
-    def first_test(self):
+    def test_first(self):
         # When
-        config_actual = removeGeneratedCells(GENERATED_TOP_CELL_1)
+        config_actual = removeGeneratedCells(self.GENERATED_TOP_CELL_1)
 
         # Then
-        self.assertEqual(config_actual, T_CELL)
+        self.assertEqual(config_actual, self.T_CELL)
 
-    def second_test(self):
+    def test_second(self):
         # When
-        config_actual = removeGeneratedCells(GENERATED_TOP_CELL_2)
+        config_actual = removeGeneratedCells(self.GENERATED_TOP_CELL_2)
 
         # Then
-        self.assertEqual(config_actual, T_CELL)
+        self.assertEqual(config_actual, self.T_CELL)
 
 
 class CollapseDotsTest(ConfigurationTest):
 
     def test(self):
         # Given
-        config_before = substitute(T_CELL, {'MAP': ktokenDots})
-        config_expected = KApply('<T>', [K_CELL, ktokenDots])
+        config_before = substitute(self.T_CELL, {'MAP': ktokenDots})
+        config_expected = KApply('<T>', [self.K_CELL, ktokenDots])
 
         # When
         config_actual = collapseDots(config_before)
