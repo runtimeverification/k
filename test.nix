b3d31646ac91f7ce3dde9b2d32cb55809dbe9613
Bruce Collie <brucecollie82@gmail.com>
2022-04-29 16:17:54 +0100

    Fix up nix

--- test.nix
+++ test.nix
@@ -41,7 +41,7 @@ stdenv.mkDerivation {
       "KOMPILE=kompile"
       "KRUN=krun"
       "KDEP=kdep"
-      "KPROVE=kprove"
+      "KPROVE=kprove-legacy"
       "KBMC=kbmc"
       "KAST=kast"
       "KPRINT=kprint"
