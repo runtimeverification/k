b7a2bac64f2fd54880dbdc94cbd244db67d9e811
Sam Balco <goodlyrottenapple@gmail.com>
2022-05-10 08:36:54 +0100

    revert changes to default.nix pertaining to haskell-packages override. can be done via k.override { haskell-backend = .. }

--- default.nix
+++ default.nix
@@ -8,7 +8,6 @@ in
 # Build an optimized release package.
 # Currently requires dependents to use LTO. Use sparingly.
 , release ? false
-, haskell-backend ? null
 }:
 
 let
@@ -16,17 +15,6 @@ let
 
   mavenix = import sources."mavenix" { inherit pkgs; };
   ttuegel = import sources."ttuegel" { inherit pkgs; };
-  haskell-backend-project = 
-    if haskell-backend != null then 
-      import haskell-backend { inherit release; }
-    else 
-      import ./haskell-backend/src/main/native/haskell-backend {
-        inherit release;
-        src = ttuegel.cleanGitSubtree {
-          src = ./.;
-          subDir = "haskell-backend/src/main/native/haskell-backend";
-        };
-      };
 
   llvm-backend-project = import ./llvm-backend/src/main/native/llvm-backend {
     inherit pkgs;
@@ -40,15 +28,21 @@ let
   inherit (llvm-backend-project) clang llvm-backend;
 
   k = callPackage ./nix/k.nix {
-    haskell-backend = haskell-backend-project.kore;
-    prelude-kore = haskell-backend-project.prelude-kore;
-    inherit llvm-backend mavenix;
+    inherit haskell-backend llvm-backend mavenix prelude-kore;
     inherit (ttuegel) cleanGit cleanSourceWith;
   };
 
+  haskell-backend-project = import ./haskell-backend/src/main/native/haskell-backend {
+    src = ttuegel.cleanGitSubtree {
+      src = ./.;
+      subDir = "haskell-backend/src/main/native/haskell-backend";
+    };
+  };
+  haskell-backend = haskell-backend-project.kore;
+  inherit (haskell-backend-project) prelude-kore;
+
   self = {
-    inherit k clang llvm-backend;
-    haskell-backend = haskell-backend-project.kore;
+    inherit k clang llvm-backend haskell-backend;
     inherit mavenix;
     inherit (pkgs) mkShell;
   };
