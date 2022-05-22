616ce43f530025cbbdda10f41e39afc3ab8646c6
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2022-05-22 02:51:56 +0000

    pyk/tests/test_kastManip: add failing test of minimizeTerm

--- pyk/src/pyk/tests/test_kastManip.py
+++ pyk/src/pyk/tests/test_kastManip.py
@@ -1,11 +1,11 @@
 from functools import partial
 from unittest import TestCase
 
-from ..kast import KApply, KRewrite, KSequence
-from ..kastManip import push_down_rewrites
+from ..kast import KApply, KRewrite, KSequence, ktokenDots
+from ..kastManip import minimizeTerm, push_down_rewrites
 
 a, b, c = (KApply(label) for label in ['a', 'b', 'c'])
-f, g, h = (partial(KApply.of, label) for label in ['f', 'g', 'h'])
+f, g, k = (partial(KApply.of, label) for label in ['f', 'g', '<k>'])
 
 
 class PushDownRewritesTest(TestCase):
@@ -20,6 +20,24 @@ class PushDownRewritesTest(TestCase):
             with self.subTest(i=i):
                 # When
                 actual = push_down_rewrites(before)
+
+                # Then
+                self.assertEqual(actual, expected)
+
+
+class MinimizeTermTest(TestCase):
+
+    def test_minimizeTerm(self):
+        # Given
+        test_data = (
+            (f(k(a)), ['<k>'], f(ktokenDots)),
+            (f(k(a)), [], f(k(a))),
+        )
+
+        for i, (before, abstract_labels, expected) in enumerate(test_data):
+            with self.subTest(i=i):
+                # When
+                actual = minimizeTerm(before, abstractLabels=abstract_labels)
 
                 # Then
                 self.assertEqual(actual, expected)
