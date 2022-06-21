97371038a1e2aa02b7f3fe8749cef3d3abda30b9
Nishant Rodrigues <nishantjr@gmail.com>
2022-06-21 09:09:32 -0500

    Use assertRaises instead of assertRaisesRegex

--- pyk/src/pyk/tests/test_utils.py
+++ pyk/src/pyk/tests/test_utils.py
@@ -6,13 +6,13 @@ from ..utils import deconstruct_short_hash
 class KCFGTestCase(TestCase):
     def test_parse_short_id(self):
         # Bad short hash: Has digits between dots
-        with self.assertRaisesRegex(ValueError, 'Bad short hash: 3\\.c62e73544\\.\\.\\.'):
+        with self.assertRaises(ValueError, msg='Bad short hash: 3.c62e73544...'):
             deconstruct_short_hash('3.c62e73544...')
 
         # Bad short hash: Has non hex digits
-        with self.assertRaisesRegex(ValueError, 'Bad short hash: 3\\.\\.\\.XXX'):
+        with self.assertRaises(ValueError, msg='Bad short hash: 3...XXX'):
             deconstruct_short_hash('3...XXX')
 
         # Bad short hash: Has more than three dots
-        with self.assertRaisesRegex(ValueError, 'Bad short hash: 3\\.\\.\\.\\.\\.adf'):
+        with self.assertRaises(ValueError, msg='Bad short hash: 3.....adf'):
             deconstruct_short_hash('3.....adf')
