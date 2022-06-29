b3a8a90ff4b49cb7baeb720dd42c2c317c837cd2
Sam Balco <goodlyrottenapple@gmail.com>
2022-06-29 11:16:48 +0100

    revert doCheck removal

--- nix/k.nix
+++ nix/k.nix
@@ -13,6 +13,9 @@ let
     # Otherwise, the jars are needlessly duplicated and bloat the cachix cache.
     copySubmodules = false;
 
+    # Cannot enable unit tests until a bug is fixed upstream (in Mavenix).
+    doCheck = false;
+
     # Set build environment variables
     #
     MAVEN_OPTS = [
