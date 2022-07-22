1e28381df97df412b493ac1bd1901e9d07e5d570
Tamás Tóth <tothtamas28@users.noreply.github.com>
2022-07-22 09:45:46 +0200

    Add module pyk.kore (#2696)
    * Add checks for identifier syntax
    
    * Add implementation stub for Kore nodes
    
    * Add final decorator to concrete AST classes
    
    * Implement validation for StrLit
    
    * Add textual serialization for Kore AST classes
    
    * Add lexer for Kore
    
    * Make StrLit a subclass of Pattern
    
    * Add function repeat_last to pyk.utils
    
    * Add parser for Kore
    
    * Rename KoreLexer.Token to KoreToken, KoreLexer.TokenType to KoreToken.Type
    
    * Extract method KoreParser._match_symbol_id
    
    * Add method KoreParser._custom_symbol_id
    
    * Define classes RoundPred and BinaryPred
    
    * Move TypeVar definitions to top in kore.parser
    
    * Extract class _LookaheadBuffer
    
    * Extract module kore.lexer
    
    * Strengthen type hint for KAst.from_dict
    
    * Add stub for Kore methods dict and from_dict
    
    * Change KAst.from_json to classmethod
    
    * Add tests for Kore.from_dict
    
    * Implement from_dict for Pattern
    
    * Fix test input
    
    * Change MLPattern.symbol into private abstract class property
    
    * Add private abstract class property _tag to class Kore
    
    * Implement method dict for Pattern subclasses
    
    * Rename test data folder
    
    * Add class ParserTest
    
    * Fix Kore lexer rule for STRING
    
    * Update TODO comments
    
    * Rename tag SortVariable to SortVar
    
    * Rename tag dv to DV
    
    * Rename JSON field resultSort to sort
    
    * Move StrLitLexer from kore.syntax to kore.lexer
    
    * Keep string literals unencoded in JSON format
    
    * Integrate parser tests from Haskell backend
    
    * Eliminate duplicated lexical checks from pyk.kore.syntax
    
    * Add method let to concrete Kore classes
    
    * Add abstract method let_attrs to class WithAttrs
    
    * Add abstract method map_pattern to class Pattern
    
    * Add method map_attrs to class WithAttrs
    
    * Remove some outdated TODOs
    
    * Extract abstract class WithSort
    
    * Eliminate code duplication in from_dict of abstract classes
    
    * Rename class SortCons to SortApp
    
    * Rename class Apply to App
    
    * Rename class StrLit to String, StrLitLexer to KoreStringLexer
    
    * Rename class ElemVar to EVar
    
    * Rename class SetVar to SVar
    
    * Rename class DomVal to DV
    
    * Change Kore._TAGS from dict to set
    
    * Reorder methods in KoreStringLexer
    
    * Add method _match_any to KoreLexer

--- pyk/src/pyk/kast.py
+++ pyk/src/pyk/kast.py
@@ -35,7 +35,7 @@ class KAst(ABC):
 
     @classmethod
     @abstractmethod
-    def from_dict(cls: Type[T], d: Dict[str, Any]) -> 'KAst':
+    def from_dict(cls: Type[T], d: Dict[str, Any]) -> T:
         node = d['node']
         return globals()[node].from_dict(d)
 
@@ -43,9 +43,9 @@ class KAst(ABC):
     def to_dict(self) -> Dict[str, Any]:
         ...
 
-    @staticmethod
-    def from_json(s: str) -> 'KAst':
-        return KAst.from_dict(json.loads(s))
+    @classmethod
+    def from_json(cls: Type[T], s: str) -> T:
+        return cls.from_dict(json.loads(s))
 
     @final
     def to_json(self) -> str:
