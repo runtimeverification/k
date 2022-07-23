545c1dfc41aaaf9ebb8422eec4fac6480bde73eb
Tamas Toth <tamas.toth@runtimeverification.com>
2022-07-23 14:14:27 +0200

    Add tests

--- pyk/src/pyk/tests/kore/test_client.py
+++ pyk/src/pyk/tests/kore/test_client.py
@@ -1,8 +1,20 @@
 from unittest import TestCase
 from unittest.mock import patch
 
-from pyk.kore.client import JsonRpcClient, KoreClient
-from pyk.kore.syntax import SortApp, Top
+from pyk.kore.client import (
+    DirectResult,
+    DirectState,
+    JsonRpcClient,
+    KoreClient,
+    StopReason,
+)
+from pyk.kore.syntax import DV, And, App, Bottom, SortApp, String, Top
+
+intSort = SortApp('IntSort')
+
+
+def intdv(n: int) -> DV:
+    return DV(intSort, String(str(n)))
 
 
 class KoreClientTest(TestCase):
@@ -24,27 +36,95 @@ class KoreClientTest(TestCase):
         MockClient.assert_called_with('localhost', 3000)
         self.assertEqual(self.client._client, self.mock)
 
-    def test(self):
+    def assumeResponse(self, response):
+        self.mock.request.return_value = response
+
+    def assertRequest(self, method, **params):
+        self.mock.request.assert_called_with(method, **params)
+
+    def test_simplify(self):
         # Given
-        top_state = {
+        pattern = And(intSort, Top(intSort), Top(intSort))
+        params = {
             'state': {
                 'format': 'KORE',
                 'version': 1,
-                'term': {
-                    'tag': 'Top',
-                    'sort': {
-                        'tag': 'SortApp',
-                        'name': 'KSort',
-                        'args': [],
-                    },
-                },
+                'term': pattern.dict,
             },
         }
-        self.mock.request.return_value = top_state
+        response = {
+            'state': {
+                'format': 'KORE',
+                'version': 1,
+                'term': Top(intSort).dict,
+            },
+        }
+        expected = Top(intSort)
+        self.assumeResponse(response)
 
         # When
-        res = self.client.simplify(Top(SortApp('KSort')))
+        actual = self.client.simplify(pattern)
 
         # Then
-        self.mock.request.assert_called_with('simplify', **top_state)
-        self.assertEqual(res, Top(SortApp('KSort')))
+        self.assertRequest('simplify', **params)
+        self.assertEqual(expected, actual)
+
+    def test_implies(self):
+        antecedent = Bottom(intSort)
+        consequent = Top(intSort)
+        params = {
+            'antecedent': {
+                'format': 'KORE',
+                'version': 1,
+                'term': antecedent.dict,
+            },
+            'consequent': {
+                'format': 'KORE',
+                'version': 1,
+                'term': consequent.dict,
+            },
+        }
+        response = {
+            'satisfiable': True,
+        }
+        expected = (True, None, None)
+        self.assumeResponse(response)
+
+        # When
+        actual = self.client.implies(antecedent, consequent)
+
+        # Then
+        self.assertRequest('implies', **params)
+        self.assertEqual(expected, actual)
+
+    def test_execute(self):
+        pattern = App('IntAdd', (), (intdv(1), intdv(1)))
+        params = {
+            'state': {
+                'format': 'KORE',
+                'version': 1,
+                'term': pattern.dict,
+            },
+        }
+        response = {
+            'states': [
+                {
+                    'state': {
+                        'format': 'KORE',
+                        'version': 1,
+                        'term': intdv(2).dict
+                    },
+                    "depth": 1,
+                }
+            ],
+            'reason': 'final-state',
+        }
+        expected = DirectResult(DirectState(intdv(2)), 1, StopReason.FINAL_STATE)
+        self.assumeResponse(response)
+
+        # When
+        actual = self.client.execute(pattern)
+
+        # Then
+        self.assertRequest('execute', **params)
+        self.assertEqual(expected, actual)
