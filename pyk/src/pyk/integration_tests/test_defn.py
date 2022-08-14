94dae30995890c0550bab186c699aa48e25156bf
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2022-08-14 17:29:49 +0000

    pyk/test_defn: reformat

--- pyk/src/pyk/integration_tests/test_defn.py
+++ pyk/src/pyk/integration_tests/test_defn.py
@@ -19,33 +19,28 @@ class DefnTest(KProveTest):
         symbol_table['.List{"_,_"}'] = constLabel('')
 
     def test_print_configuration(self):
-        # Given
 
         # fmt: off
         config = KApply('<T>', [KApply('<k>', [KApply('int_;_', [KApply('_,_', [KToken('x', 'Id'), KApply('_,_', [KToken('y', 'Id'), KApply('.List{"_,_"}')])])])]), KApply('<state>', [KApply('.Map')])])
+
+        config_expected = (
+            f'<T>\n'
+            f'  <k>\n'
+            f'    int x , y ;\n'
+            f'  </k>\n'
+            f'  <state>\n'
+            f'    .Map\n'
+            f'  </state>\n'
+            f'</T>'
+        )
         # fmt: on
 
-        config_expected = '\n'.join(
-            [
-                '<T>',
-                '  <k>',
-                '    int x , y ;',
-                '  </k>',
-                '  <state>',
-                '    .Map',
-                '  </state>',
-                '</T>',
-            ]
-        )
-
-        # When
         config_actual = self.kprove.pretty_print(config)
 
-        # Then
         self.assertEqual(config_expected, config_actual)
 
     def test_print_prove_rewrite(self):
-        # Given
+
         # fmt: off
         claim_rewrite = KRewrite( KApply('<T>', [ KApply('<k>', [KApply('_+_', [KVariable('X'), KVariable('Y')])])
                                                 , KApply('<state>', [KVariable('STATE')])
@@ -56,65 +51,57 @@ class DefnTest(KProveTest):
                                 )
         # fmt: on
 
-        minimized_claim_rewrite_expected = '\n'.join(
-            [
-                '<T>',
-                '  <k>',
-                '    ( X + Y => X +Int Y )',
-                '  </k>',
-                '  <state>',
-                '    STATE',
-                '  </state>',
-                '</T>',
-            ]
+        minimized_claim_rewrite_expected = (
+            f'<T>\n'
+            f'  <k>\n'
+            f'    ( X + Y => X +Int Y )\n'
+            f'  </k>\n'
+            f'  <state>\n'
+            f'    STATE\n'
+            f'  </state>\n'
+            f'</T>'
         )
 
-        # When
         minimized_claim_rewrite = push_down_rewrites(claim_rewrite)
         claim = KClaim(minimized_claim_rewrite)
         minimized_claim_rewrite_actual = self.kprove.pretty_print(minimized_claim_rewrite)
         result = self.kprove.prove_claim(claim, 'simple-step')
 
-        # Then
         self.assertEqual(minimized_claim_rewrite_expected, minimized_claim_rewrite_actual)
         self.assertTop(result)
 
     def test_empty_config(self):
-        # Given
         empty_config_generated_top = self.kprove.definition.empty_config(Sorts.GENERATED_TOP_CELL)
         empty_config_t = self.kprove.definition.empty_config(KSort('TCell'))
 
-        empty_config_generated_top_printed = '\n'.join(
-            [
-                '<generatedTop>',
-                '  <T>',
-                '    <k>',
-                '      K_CELL',
-                '    </k>',
-                '    <state>',
-                '      STATE_CELL',
-                '    </state>',
-                '  </T>',
-                '  <generatedCounter>',
-                '    GENERATEDCOUNTER_CELL',
-                '  </generatedCounter>',
-                '</generatedTop>',
-            ]
+        empty_config_generated_top_printed = (
+            f'<generatedTop>\n'
+            f'  <T>\n'
+            f'    <k>\n'
+            f'      K_CELL\n'
+            f'    </k>\n'
+            f'    <state>\n'
+            f'      STATE_CELL\n'
+            f'    </state>\n'
+            f'  </T>\n'
+            f'  <generatedCounter>\n'
+            f'    GENERATEDCOUNTER_CELL\n'
+            f'  </generatedCounter>\n'
+            f'</generatedTop>'
         )
 
-        empty_config_t_printed = '\n'.join(
-            [
-                '<T>',
-                '  <k>',
-                '    K_CELL',
-                '  </k>',
-                '  <state>',
-                '    STATE_CELL',
-                '  </state>',
-                '</T>',
-            ]
+        # fmt: off
+        empty_config_t_printed = (
+            f'<T>\n'
+            f'  <k>\n'
+            f'    K_CELL\n'
+            f'  </k>\n'
+            f'  <state>\n'
+            f'    STATE_CELL\n'
+            f'  </state>\n'
+            f'</T>'
         )
+        # fmt: on
 
-        # Then
         self.assertEqual(empty_config_generated_top_printed, self.kprove.pretty_print(empty_config_generated_top))
         self.assertEqual(empty_config_t_printed, self.kprove.pretty_print(empty_config_t))
