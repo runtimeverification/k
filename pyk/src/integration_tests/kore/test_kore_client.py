e129e10b5364a064053325c5ff8da51dbf78df91
Tamas Toth <tamas.toth@runtimeverification.com>
2022-09-03 13:38:09 +0200

    Add further test cases

--- pyk/src/integration_tests/kore/test_kore_client.py
+++ pyk/src/integration_tests/kore/test_kore_client.py
@@ -1,9 +1,17 @@
 from string import Template
 from typing import Any, Final, Mapping, Tuple
 
-from pyk.kore.client import BranchingResult, DepthBoundResult, ExecuteResult, ImpliesResult, State, StuckResult
+from pyk.kore.client import (
+    BranchingResult,
+    DepthBoundResult,
+    ExecuteResult,
+    ImpliesResult,
+    KoreClientError,
+    State,
+    StuckResult,
+)
 from pyk.kore.parser import KoreParser
-from pyk.kore.syntax import DV, Implies, Pattern, SortApp, String, Top
+from pyk.kore.syntax import DV, Equals, EVar, Implies, Pattern, SortApp, SortVar, String, Top
 
 from .kore_client_test import KoreClientTest
 
@@ -64,6 +72,23 @@ class SimpleKoreClientTest(KoreClientTest):
                 ImpliesResult(True, Implies(int_sort, int_dv(0), int_top), int_top, int_top),
             ),
             ('0 -> 1', int_dv(0), int_dv(1), ImpliesResult(False, Implies(int_sort, int_dv(0), int_dv(1)), None, None)),
+            (
+                'X -> 0',
+                x,
+                int_dv(0),
+                ImpliesResult(
+                    False,
+                    Implies(int_sort, x, int_dv(0)),
+                    Equals(
+                        op_sort=int_sort,
+                        sort=SortVar(name='JSONSortVariable'),
+                        left=x,
+                        right=int_dv(0),
+                    ),
+                    int_top,
+                ),
+            ),
+            ('X -> X', x, x, ImpliesResult(True, Implies(int_sort, x, x), int_top, int_top)),
         )
 
         for test_name, antecedent, consequent, expected in test_data:
@@ -74,9 +99,26 @@ class SimpleKoreClientTest(KoreClientTest):
                 # Then
                 self.assertEqual(actual, expected)
 
+    def test_implies_error(self):
+        # Given
+        test_data = (
+            ('0 -> X', int_dv(0), x),
+            ('X -> Y', x, y),
+        )
+
+        for test_name, antecedent, consequent in test_data:
+            with self.subTest(test_name):
+                with self.assertRaises(KoreClientError) as cm:
+                    # When
+                    self.client.implies(antecedent, consequent)
+
+                # Then
+                self.assertEqual(cm.exception.code, -32003)
+
 
 int_sort = SortApp('SortInt')
 int_top = Top(int_sort)
+x, y = (EVar(v, int_sort) for v in ['x', 'y'])
 
 
 def int_dv(n: int) -> DV:
