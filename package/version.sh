f8a9b39ed90388a6ee06bda1b5f1f1b586f4a1bc
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2022-09-12 18:16:43 +0000

    package/version.sh: check entire version string, not just major minor

--- package/version.sh
+++ package/version.sh
@@ -8,17 +8,14 @@ fatal() { echo "[FATAL] $@" ; exit 1 ; }
 version_file="package/version"
 
 version_bump() {
-    local version release_commit version_major version_minor version_patch new_version current_version_major current_version_minor current_version_patch
+    local version release_commit version_major version_minor version_patch new_version
     version="$1"
     version_major="$(echo ${version} | cut --delimiter '.' --field 1)"
     version_minor="$(echo ${version} | cut --delimiter '.' --field 2)"
     version_patch="$(echo ${version} | cut --delimiter '.' --field 3)"
     current_version="$(cat ${version_file})"
-    current_version_major="$(echo ${current_version} | cut --delimiter '.' --field 1)"
-    current_version_minor="$(echo ${current_version} | cut --delimiter '.' --field 2)"
-    current_version_patch="$(echo ${current_version} | cut --delimiter '.' --field 3)"
     new_version="${current_version}"
-    if [[ "${version_major}" == "${current_version_major}" ]] && [[ "${version_minor}" == "${current_version_minor}" ]]; then
+    if [[ "${version}" == "${current_version}" ]]; then
         new_version="${version_major}.${version_minor}.$((version_patch + 1))"
     fi
     echo "${new_version}" > "${version_file}"
