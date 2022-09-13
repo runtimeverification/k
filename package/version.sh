7a229529e87691ec8d3d24d1a577eade81fb913e
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2022-09-13 05:16:51 +0000

    package/version, github/bump-version: try simpler script

--- package/version.sh
+++ package/version.sh
@@ -9,15 +9,10 @@ version_file="package/version"
 
 version_bump() {
     local version release_commit version_major version_minor version_patch new_version
-    version="$1"
-    version_major="$(echo ${version} | cut --delimiter '.' --field 1)"
-    version_minor="$(echo ${version} | cut --delimiter '.' --field 2)"
-    version_patch="$(echo ${version} | cut --delimiter '.' --field 3)"
-    current_version="$(cat ${version_file})"
-    new_version="${current_version}"
-    if [[ "${version}" == "${current_version}" ]]; then
-        new_version="${version_major}.${version_minor}.$((version_patch + 1))"
-    fi
+    version_major="$(cat ${version_file} | cut --delimiter '.' --field 1)"
+    version_minor="$(cat ${version_file} | cut --delimiter '.' --field 2)"
+    version_patch="$(cat ${version_file} | cut --delimiter '.' --field 3)"
+    new_version="${version_major}.${version_minor}.$((version_patch + 1))"
     echo "${new_version}" > "${version_file}"
     notif "Version: ${new_version}"
 }
