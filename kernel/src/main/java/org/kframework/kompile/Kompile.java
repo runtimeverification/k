e07229f3935e4587eea2894ba70a46777813dffd
Radu Mereuta <headness13@gmail.com>
2022-10-14 23:12:29 +0300

    Anywhere is not supported by the haskell backend

--- kernel/src/main/java/org/kframework/kompile/Kompile.java
+++ kernel/src/main/java/org/kframework/kompile/Kompile.java
@@ -459,6 +459,12 @@ public class Kompile {
     }
 
     public void structuralChecks(scala.collection.Set<Module> modules, Module mainModule, Option<Module> kModule, Set<String> excludedModuleTags) {
+        if (kompileOptions.backend.equals(Backends.HASKELL))
+            errors.addAll(stream(modules).flatMap(m -> stream(m.localSentences()).flatMap(s -> {
+                if (s instanceof Rule && s.att().contains(Att.ANYWHERE()))
+                    return Stream.of(KEMException.compilerError(Att.ANYWHERE() + " is not supported by the " + Backends.HASKELL + " backend.", s));
+                return Stream.empty();
+            })).collect(Collectors.toSet()));
         boolean isSymbolic = excludedModuleTags.contains(Att.CONCRETE());
         boolean isKast = excludedModuleTags.contains(Att.KORE());
         CheckRHSVariables checkRHSVariables = new CheckRHSVariables(errors, !isSymbolic, kompileOptions.backend);
