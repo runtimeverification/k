d9a8605c5fa790e738c40a20742381081f8db594
Tamás Tóth <tothtamas28@users.noreply.github.com>
2022-10-25 13:45:38 +0200

    Add GraphQL server and client skeleton (https://github.com/runtimeverification/pyk/pull/64)
    Co-authored-by: devops <devops@runtimeverification.com>
    Co-authored-by: Samuel Balco <goodlyrottenapple@gmail.com>

--- pyk/src/pyk/krepl/client/__main__.py
+++ pyk/src/pyk/krepl/client/__main__.py
@@ -6,14 +6,15 @@ from . import DEFAULT_PORT, Repl
 def main() -> None:
     args = argument_parser().parse_args()
     try:
-        Repl(args.port).cmdloop()
+        Repl(args.host, args.port).cmdloop()
     except KeyboardInterrupt:
         ...
 
 
 def argument_parser() -> ArgumentParser:
     parser = ArgumentParser(description='K-REPL Client')
-    parser.add_argument('-p', '--port', type=int, default=DEFAULT_PORT, help='server port')
+    parser.add_argument('-s', '--host', type=str, default='localhost', help='server host (default: localhost)')
+    parser.add_argument('-p', '--port', type=int, default=DEFAULT_PORT, help=f'server port (default: {DEFAULT_PORT})')
     return parser
 
 
--- pyk/src/pyk/krepl/server/__main__.py
+++ pyk/src/pyk/krepl/server/__main__.py
@@ -14,17 +14,16 @@ def main() -> None:
     args = argument_parser().parse_args()
     logging.basicConfig(level=args.loglevel, format=_LOG_FORMAT)
 
-    server = KReplServer(args.port)
-
     _LOGGER.info(f'Server started at port {args.port}')
-    server.run()
+    KReplServer(args.host, args.port).run()
     _LOGGER.info('Server stopped')
 
 
 def argument_parser() -> ArgumentParser:
     parser = ArgumentParser(description='K-REPL Server')
     parser.add_argument('-l', '--loglevel', type=loglevel, default=logging.INFO, metavar='LEVEL', help='log level')
-    parser.add_argument('-p', '--port', type=int, default=DEFAULT_PORT, help='server port')
+    parser.add_argument('-s', '--host', type=str, default='localhost', help='server host (default: localhost)')
+    parser.add_argument('-p', '--port', type=int, default=DEFAULT_PORT, help=f'server port (default: {DEFAULT_PORT})')
     return parser
 
 
