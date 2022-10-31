a4b6bbe70e5f748a302b062dd6cb54e87a2ee5d6
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2022-10-31 11:32:19 -0600

    Initial implementation of kast_to_kore and kore_to_kast (https://github.com/runtimeverification/pyk/pull/73)
    Part of: https://github.com/runtimeverification/pyk/issues/76
    
    This is focused on implementing enough of `kast_to_kore` and
    `kore_to_kast` so that the new rewriter experiment with KEVM can make
    efficient queries into the LLVM backend without ever having to invoke
    `kast` binary.
    
    - Adds some tests to the `test_kore_to_kast.py` file, and updates some
    of them.
    - Add some helper facilities to `KDefinition`, including:
      - Memoize the computation of `production_for_klabel` (expensive).
      - Add a `subsorts` computation, and memoize it.
    - Add a `return_sort: KLabel -> KSort` and `argument_sorts: KLabel ->
    List[KSort]` methods, which make it easy to know the return and argument
    types of a given `KLabel`.
    - Adds initial simple conversions between KAst and Kore in `KPrint`,
    enough to handle the test inputs. Sets it up so that if conversion will
    not work with what's implemented so far, it falls back to `kast` binary.
    - Passes the `--profile` option through on `KRun.run`.
    - Adds a new `KRun.run_kore` which uses `kast_to_kore` and
    `kore_to_kast` to convert the input program rather than leaving it to
    `krun` script.
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- pyk/src/pyk/ktool/krun.py
+++ pyk/src/pyk/ktool/krun.py
@@ -7,7 +7,8 @@ from typing import Final, Iterable, Optional
 
 from ..cli_utils import check_file_path, run_process
 from ..cterm import CTerm
-from ..kast import KAst, KInner
+from ..kast import KAst, KInner, KSort
+from ..kore.parser import KoreParser
 from .kprint import KPrint
 
 _LOGGER: Final = logging.getLogger(__name__)
@@ -19,6 +20,7 @@ def _krun(
     check: bool = True,
     profile: bool = True,
     output: str = 'json',
+    expand_macros: bool = False,
     depth: Optional[int] = None,
     args: Iterable[str] = (),
 ) -> CompletedProcess:
@@ -29,6 +31,12 @@ def _krun(
 
     if depth and depth >= 0:
         args += ['--depth', str(depth)]
+
+    if profile:
+        args += ['--profile']
+
+    if not expand_macros:
+        args += ['--no-expand-macros']
 
     try:
         return run_process(krun_command + args, logger=_LOGGER, check=check, profile=profile)
@@ -50,19 +58,49 @@ class KRun(KPrint):
         with open(self.definition_dir / 'mainModule.txt', 'r') as mm:
             self.main_module = mm.read()
 
-    def run(self, init_PGM: KInner, depth: Optional[int] = None, args: Iterable[str] = ()) -> CTerm:  # noqa: N803
+    def run(
+        self, pgm: KInner, depth: Optional[int] = None, expand_macros: bool = False, args: Iterable[str] = ()
+    ) -> CTerm:
         with NamedTemporaryFile('w', dir=self.use_directory, delete=False) as ntf:
-            ntf.write(self.pretty_print(init_PGM))
+            ntf.write(self.pretty_print(pgm))
             ntf.flush()
             result = _krun(
                 self.definition_dir,
                 Path(ntf.name),
                 depth=depth,
+                expand_macros=expand_macros,
                 args=(list(args) + ['--output', 'json']),
                 profile=self._profile,
             )
             if result.returncode != 0:
                 raise RuntimeError('Non-zero exit-code from krun.')
             result_kast = KAst.from_dict(json.loads(result.stdout)['term'])
+            assert isinstance(result_kast, KInner)
+            return CTerm(result_kast)
+
+    def run_kore(
+        self,
+        pgm: KInner,
+        depth: Optional[int] = None,
+        sort: Optional[KSort] = None,
+        expand_macros: bool = False,
+        args: Iterable[str] = (),
+    ) -> CTerm:
+        kore_pgm = self.kast_to_kore(pgm, sort=sort)
+        with NamedTemporaryFile('w', dir=self.use_directory, delete=True) as ntf:
+            ntf.write(kore_pgm.text)
+            ntf.flush()
+            result = _krun(
+                self.definition_dir,
+                Path(ntf.name),
+                depth=depth,
+                expand_macros=expand_macros,
+                args=(list(args) + ['--output', 'kore', '--parser', 'cat']),
+                profile=self._profile,
+            )
+            if result.returncode != 0:
+                raise RuntimeError('Non-zero exit-code from krun.')
+            result_kore = KoreParser(result.stdout).pattern()
+            result_kast = self.kore_to_kast(result_kore)
             assert isinstance(result_kast, KInner)
             return CTerm(result_kast)
