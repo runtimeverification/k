6b415870f08cec22e0e6831330bd4b60c0a5630c
Tamás Tóth <tothtamas28@users.noreply.github.com>
2022-11-25 15:43:36 +0100

    Port some unit tests to `pytest` (#115)
    Co-authored-by: devops <devops@runtimeverification.com>

--- pyk/src/tests/kore/test_identifiers.py
+++ pyk/src/tests/kore/test_identifiers.py
@@ -1,58 +1,56 @@
-from unittest import TestCase
+import pytest
 
 from pyk.kore.syntax import check_id, check_set_var_id, check_symbol_id
 from pyk.utils import raised
 
+BASE_TEST_DATA = (
+    ('', False),
+    ('-', False),
+    ("'", False),
+    ('0', False),
+    ('@', False),
+    ('a', True),
+    ('A', True),
+    ('a0', True),
+    ('a-', True),
+    ("a'", True),
+)
 
-class IdentifierTest(TestCase):
-    BASE_TEST_DATA = (
-        ('', False),
-        ('-', False),
-        ("'", False),
-        ('0', False),
-        ('@', False),
-        ('a', True),
-        ('A', True),
-        ('a0', True),
-        ('a-', True),
-        ("a'", True),
-    )
+ID_TEST_DATA = BASE_TEST_DATA + (('sort', False),)
 
-    ID_TEST_DATA = BASE_TEST_DATA + (('sort', False),)
 
-    SYMBOL_ID_TEST_DATA = (
-        ID_TEST_DATA + tuple(('\\' + s, expected) for s, expected in BASE_TEST_DATA) + (('\\sort', True),)
-    )
+@pytest.mark.parametrize('s,expected', ID_TEST_DATA, ids=[s for s, _ in ID_TEST_DATA])
+def test_is_id(s: str, expected: bool) -> None:
+    # When
+    actual = not raised(check_id, s)
 
-    SET_VARIABLE_ID_TEST_DATA = (
-        tuple(('@' + s, expected) for s, expected in BASE_TEST_DATA)
-        + tuple((s, False) for s, _ in ID_TEST_DATA)
-        + (('@sort', True),)
-    )
+    # Then
+    assert actual == expected
 
-    def test_is_id(self) -> None:
-        for i, (s, expected) in enumerate(self.ID_TEST_DATA):
-            with self.subTest(i=i):
-                # When
-                actual = not raised(check_id, s)
 
-                # Then
-                self.assertEqual(actual, expected)
+SYMBOL_ID_TEST_DATA = ID_TEST_DATA + tuple(('\\' + s, expected) for s, expected in BASE_TEST_DATA) + (('\\sort', True),)
 
-    def test_is_symbol_id(self) -> None:
-        for i, (s, expected) in enumerate(self.SYMBOL_ID_TEST_DATA):
-            with self.subTest(i=i):
-                # When
-                actual = not raised(check_symbol_id, s)
 
-                # Then
-                self.assertEqual(actual, expected)
+@pytest.mark.parametrize('s,expected', SYMBOL_ID_TEST_DATA, ids=[s for s, _ in SYMBOL_ID_TEST_DATA])
+def test_is_symbol_id(s: str, expected: bool) -> None:
+    # When
+    actual = not raised(check_symbol_id, s)
 
-    def test_is_set_variable_id(self) -> None:
-        for i, (s, expected) in enumerate(self.SET_VARIABLE_ID_TEST_DATA):
-            with self.subTest(i=i):
-                # When
-                actual = not raised(check_set_var_id, s)
+    # Then
+    assert actual == expected
 
-                # Then
-                self.assertEqual(actual, expected)
+
+SET_VARIABLE_ID_TEST_DATA = (
+    tuple(('@' + s, expected) for s, expected in BASE_TEST_DATA)
+    + tuple((s, False) for s, _ in ID_TEST_DATA)
+    + (('@sort', True),)
+)
+
+
+@pytest.mark.parametrize('s,expected', SET_VARIABLE_ID_TEST_DATA, ids=[s for s, _ in SET_VARIABLE_ID_TEST_DATA])
+def test_is_set_variable_id(s: str, expected: bool) -> None:
+    # When
+    actual = not raised(check_set_var_id, s)
+
+    # Then
+    assert actual == expected
--- pyk/src/tests/test_kompile.py
+++ pyk/src/tests/test_kompile.py
@@ -1,38 +1,41 @@
 from pathlib import Path
-from unittest import TestCase
 
 from pyk.ktool.kompile import KompileBackend, _build_arg_list
 
 
-class BuildArgsTest(TestCase):
-    def test_all_args(self) -> None:
-        actual = _build_arg_list(
-            main_module='MAIN-MODULE',
-            syntax_module='SYNTAX-MODULE',
-            backend=KompileBackend.HASKELL,
-            output_dir=Path('path/to/kompiled'),
-            include_dirs=(Path(path) for path in ['/', '/include/lib']),
-            md_selector='k & ! nobytes & ! node',
-            hook_namespaces=['JSON', 'KRYPTO', 'BLOCKCHAIN'],
-            emit_json=True,
-            post_process='echo "hello"',
-            concrete_rules=['foo', 'bar'],
-            args=['--new-fangled-option', 'buzz'],
-        )
-        # fmt: off
-        expected = [
-            '--main-module', 'MAIN-MODULE',
-            '--syntax-module', 'SYNTAX-MODULE',
-            '--backend', 'haskell',
-            '--output-definition', 'path/to/kompiled',
-            '-I', '/',
-            '-I', '/include/lib',
-            '--md-selector', 'k & ! nobytes & ! node',
-            '--hook-namespaces', 'JSON KRYPTO BLOCKCHAIN',
-            '--emit-json',
-            '--post-process', "'echo \"hello\"'",
-            '--concrete-rules', 'foo,bar',
-            '--new-fangled-option', 'buzz'
-        ]
-        # fmt: on
-        self.assertEqual(actual, expected)
+def test_all_args() -> None:
+    # Given
+    # fmt: off
+    expected = [
+        '--main-module', 'MAIN-MODULE',
+        '--syntax-module', 'SYNTAX-MODULE',
+        '--backend', 'haskell',
+        '--output-definition', 'path/to/kompiled',
+        '-I', '/',
+        '-I', '/include/lib',
+        '--md-selector', 'k & ! nobytes & ! node',
+        '--hook-namespaces', 'JSON KRYPTO BLOCKCHAIN',
+        '--emit-json',
+        '--post-process', "'echo \"hello\"'",
+        '--concrete-rules', 'foo,bar',
+        '--new-fangled-option', 'buzz'
+    ]
+    # fmt: on
+
+    # When
+    actual = _build_arg_list(
+        main_module='MAIN-MODULE',
+        syntax_module='SYNTAX-MODULE',
+        backend=KompileBackend.HASKELL,
+        output_dir=Path('path/to/kompiled'),
+        include_dirs=(Path(path) for path in ['/', '/include/lib']),
+        md_selector='k & ! nobytes & ! node',
+        hook_namespaces=['JSON', 'KRYPTO', 'BLOCKCHAIN'],
+        emit_json=True,
+        post_process='echo "hello"',
+        concrete_rules=['foo', 'bar'],
+        args=['--new-fangled-option', 'buzz'],
+    )
+
+    # Then
+    assert actual == expected
