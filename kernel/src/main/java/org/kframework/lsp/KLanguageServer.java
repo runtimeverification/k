6d2fb70511af51595d05a1821761d62242b7fc32
Radu Mereuta <headness13@gmail.com>
2022-12-20 22:33:43 +0200

    Language Server Protocol
    Add kompile endpoint
    add lsp4j dependency

---
+++ kernel/src/main/java/org/kframework/lsp/KLanguageServer.java
@@ -0,0 +1,101 @@
+package org.kframework.lsp;
+
+
+import org.eclipse.lsp4j.ClientCapabilities;
+import org.eclipse.lsp4j.CompletionOptions;
+import org.eclipse.lsp4j.CompletionRegistrationOptions;
+import org.eclipse.lsp4j.InitializeParams;
+import org.eclipse.lsp4j.InitializeResult;
+import org.eclipse.lsp4j.InitializedParams;
+import org.eclipse.lsp4j.Registration;
+import org.eclipse.lsp4j.RegistrationParams;
+import org.eclipse.lsp4j.ServerCapabilities;
+import org.eclipse.lsp4j.TextDocumentClientCapabilities;
+import org.eclipse.lsp4j.TextDocumentSyncKind;
+import org.eclipse.lsp4j.services.LanguageClient;
+import org.eclipse.lsp4j.services.LanguageClientAware;
+import org.eclipse.lsp4j.services.LanguageServer;
+import org.eclipse.lsp4j.services.TextDocumentService;
+import org.eclipse.lsp4j.services.WorkspaceService;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * Language Server implementation for Ballerina.
+ */
+public class KLanguageServer implements LanguageServer, LanguageClientAware {
+
+    private TextDocumentService textDocumentService;
+    private WorkspaceService workspaceService;
+    private ClientCapabilities clientCapabilities;
+    LanguageClient languageClient;
+    private int shutdown = 1;
+
+    public KLanguageServer() {
+        this.textDocumentService = new KTextDocumentService(this);
+        this.workspaceService = new KWorkspaceService(this);
+    }
+
+    @Override
+    public CompletableFuture<InitializeResult> initialize(InitializeParams initializeParams) {
+        final InitializeResult response = new InitializeResult(new ServerCapabilities());
+        //Set the document synchronization capabilities to full.
+        response.getCapabilities().setTextDocumentSync(TextDocumentSyncKind.Full);
+        this.clientCapabilities = initializeParams.getCapabilities();
+
+        /* Check if dynamic registration of completion capability is allowed by the client. If so we don't register the capability.
+           Else, we register the completion capability.
+         */
+        if (!isDynamicCompletionRegistration()) {
+            response.getCapabilities().setCompletionProvider(new CompletionOptions());
+        }
+        return CompletableFuture.supplyAsync(() -> response);
+    }
+
+    @Override
+    public void initialized(InitializedParams params) {
+        //Check if dynamic completion support is allowed, if so register.
+        if (isDynamicCompletionRegistration()) {
+            CompletionRegistrationOptions completionRegistrationOptions = new CompletionRegistrationOptions();
+            Registration completionRegistration = new Registration(UUID.randomUUID().toString(),
+                    "textDocument/completion", completionRegistrationOptions);
+            languageClient.registerCapability(new RegistrationParams(List.of(completionRegistration)));
+        }
+    }
+
+    @Override
+    public CompletableFuture<Object> shutdown() {
+        shutdown = 0;
+        return CompletableFuture.supplyAsync(Object::new);
+    }
+
+    @Override
+    public void exit() {
+        System.exit(shutdown);
+    }
+
+    @Override
+    public TextDocumentService getTextDocumentService() {
+        return this.textDocumentService;
+    }
+
+    @Override
+    public WorkspaceService getWorkspaceService() {
+        return this.workspaceService;
+    }
+
+    @Override
+    public void connect(LanguageClient languageClient) {
+        this.languageClient = languageClient;
+        LSClientLogger.getInstance().initialize(this.languageClient);
+    }
+
+    private boolean isDynamicCompletionRegistration() {
+        TextDocumentClientCapabilities textDocumentCapabilities =
+                clientCapabilities.getTextDocument();
+        return textDocumentCapabilities != null && textDocumentCapabilities.getCompletion() != null
+                && Boolean.FALSE.equals(textDocumentCapabilities.getCompletion().getDynamicRegistration());
+    }
+}
---
+++ kernel/src/main/java/org/kframework/lsp/KTextDocumentService.java
@@ -0,0 +1,60 @@
+package org.kframework.lsp;
+
+import org.eclipse.lsp4j.*;
+import org.eclipse.lsp4j.jsonrpc.messages.Either;
+import org.eclipse.lsp4j.services.TextDocumentService;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * TextDocumentService implementation for K.
+ */
+public class KTextDocumentService implements TextDocumentService {
+
+    private KLanguageServer languageServer;
+    private LSClientLogger clientLogger;
+
+    public KTextDocumentService(KLanguageServer languageServer) {
+        this.languageServer = languageServer;
+        this.clientLogger = LSClientLogger.getInstance();
+    }
+
+    @Override
+    public void didOpen(DidOpenTextDocumentParams didOpenTextDocumentParams) {
+        this.clientLogger.logMessage("Operation '" + "text/didOpen" +
+                "' {fileUri: '" + didOpenTextDocumentParams.getTextDocument().getUri() + "'} opened");
+    }
+
+    @Override
+    public void didChange(DidChangeTextDocumentParams didChangeTextDocumentParams) {
+        this.clientLogger.logMessage("Operation '" + "text/didChange" +
+                "' {fileUri: '" + didChangeTextDocumentParams.getTextDocument().getUri() + "'} Changed");
+    }
+
+    @Override
+    public void didClose(DidCloseTextDocumentParams didCloseTextDocumentParams) {
+        this.clientLogger.logMessage("Operation '" + "text/didClose" +
+                "' {fileUri: '" + didCloseTextDocumentParams.getTextDocument().getUri() + "'} Closed");
+    }
+
+    @Override
+    public void didSave(DidSaveTextDocumentParams didSaveTextDocumentParams) {
+        this.clientLogger.logMessage("Operation '" + "text/didSave" +
+                "' {fileUri: '" + didSaveTextDocumentParams.getTextDocument().getUri() + "'} Saved");
+    }
+
+    @Override
+    public CompletableFuture<Either<List<CompletionItem>, CompletionList>> completion(CompletionParams position) {
+        return CompletableFuture.supplyAsync(() -> {
+            this.clientLogger.logMessage("Operation '" + "text/completion");
+            CompletionItem completionItem = new CompletionItem();
+            completionItem.setLabel("Test completion item");
+            completionItem.setInsertText("Test");
+            completionItem.setDetail("Snippet");
+            completionItem.setKind(CompletionItemKind.Snippet);
+            return Either.forLeft(Arrays.asList(completionItem));
+        });
+    }
+}
