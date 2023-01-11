042aa5bb1885da8c80645f256485c9f7c736ff29
Tamás Tóth <tothtamas28@users.noreply.github.com>
2023-01-11 15:05:32 +0100

    Add function `kore_print` (#154)
    Co-authored-by: devops <devops@runtimeverification.com>

---
+++ pyk/src/pyk/kore/tools.py
@@ -0,0 +1,33 @@
+from enum import Enum
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+from typing import Union
+
+from ..cli_utils import check_dir_path, run_process
+from .syntax import Pattern
+
+
+class PrintOutput(Enum):
+    PRETTY = 'pretty'
+    PROGRAM = 'program'
+    KAST = 'kast'
+    BINARY = 'binary'
+    JSON = 'json'
+    LATEX = 'latex'
+    KORE = 'kore'
+    NONE = 'none'
+
+
+def kore_print(pattern: Pattern, definition_dir: Union[str, Path], output: Union[str, PrintOutput]) -> str:
+    definition_dir = Path(definition_dir)
+    check_dir_path(definition_dir)
+
+    output = PrintOutput(output)
+
+    with NamedTemporaryFile(mode='w') as f:
+        f.write(pattern.text)
+        f.flush()
+
+        run_res = run_process(['kore-print', f.name, '--definition', str(definition_dir), '--output', output.value])
+
+    return run_res.stdout.strip()
