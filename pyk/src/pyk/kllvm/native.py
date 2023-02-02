1697a70663580ee5360a4496cda392dc8bd16179
Tamás Tóth <tothtamas28@users.noreply.github.com>
2023-02-02 21:03:08 +0100

    Generate module `pyk.kllvm.native._kllvm` dynamically (#169)
    ~Blocked on:
    https://github.com/runtimeverification/llvm-backend/pull/634~
    ~Blocked on: https://github.com/runtimeverification/k/pull/3132~
    ~Blocked on: https://github.com/runtimeverification/k/pull/3136~
    Fixes #119
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>
    Co-authored-by: rv-jenkins <admin@runtimeverification.com>

--- pyk/src/pyk/kllvm/native.py
+++ pyk/src/pyk/kllvm/native.py
@@ -1,12 +1,40 @@
+import atexit
+import logging
+import shutil
+import sys
 from pathlib import Path
-from typing import Final
+from tempfile import mkdtemp
+from typing import Final, Union
 
-from ..cli_utils import run_process
+from ..cli_utils import check_dir_path, run_process
 from .utils import PYTHON_EXTENSION_SUFFIX, import_from_file
+
+_LOGGER: Final = logging.getLogger(__name__)
 
 KLLVM_MODULE_NAME: Final = '_kllvm'
 KLLVM_MODULE_FILE_NAME: Final = f'{KLLVM_MODULE_NAME}{PYTHON_EXTENSION_SUFFIX}'
-KLLVM_BINDINGS_PATH = Path(run_process(['llvm-kompile', '--bindings-path'], pipe_stderr=True).stdout.strip())
-KLLVM_MODULE_DIR: Final = (KLLVM_BINDINGS_PATH / 'kllvm').resolve(strict=True)
-KLLVM_MODULE_FILE: Final = (KLLVM_MODULE_DIR / KLLVM_MODULE_FILE_NAME).resolve(strict=True)
+
+
+def compile_kllvm(target_dir: Union[str, Path], *, verbose: bool = False) -> Path:
+    target_dir = Path(target_dir).resolve()
+    check_dir_path(target_dir)
+
+    module_file = target_dir / KLLVM_MODULE_FILE_NAME
+
+    args = ['llvm-kompile', 'pythonast', '--python', sys.executable, '--', '-o', str(module_file)]
+    if verbose:
+        args.append('-v')
+
+    _LOGGER.info(f'Compiling pythonast extension: {module_file.name}')
+    run_process(args, logger=_LOGGER)
+    return module_file
+
+
+KLLVM_MODULE_DIR: Final = Path(mkdtemp()).resolve(strict=True)
+KLLVM_MODULE_FILE: Final = compile_kllvm(KLLVM_MODULE_DIR)
 _kllvm: Final = import_from_file(KLLVM_MODULE_NAME, KLLVM_MODULE_FILE)
+
+
+@atexit.register
+def _cleanup() -> None:
+    shutil.rmtree(KLLVM_MODULE_DIR, ignore_errors=True)
