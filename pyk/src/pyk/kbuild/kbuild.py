424a549adfef6e94174335715fe1476366742b57
Tamás Tóth <tothtamas28@users.noreply.github.com>
2023-02-21 15:03:34 +0100

    Add support for `resources` in `kbuild.toml` (https://github.com/runtimeverification/pyk/pull/211)
    Co-authored-by: devops <devops@runtimeverification.com>

--- pyk/src/pyk/kbuild/kbuild.py
+++ pyk/src/pyk/kbuild/kbuild.py
@@ -1,11 +1,15 @@
+import re
 import shutil
 from dataclasses import dataclass
 from functools import cached_property
 from pathlib import Path
-from typing import List, Union, final
+from re import Match
+from typing import Any, Dict, List, Union, final
 
 from ..ktool.kompile import kompile
+from ..utils import single
 from .package import Package
+from .project import Target
 from .utils import k_version, sync_files
 
 
@@ -22,18 +26,49 @@ class KBuild:
     def k_version(self) -> str:
         return k_version().text
 
-    def sync(self, package: Package) -> List[Path]:
-        return sync_files(
-            source_dir=package.project.source_dir,
-            target_dir=self.kbuild_dir / package.include_dir / package.name,
-            file_names=package.project.include_file_names,
-        )
-
     def definition_dir(self, package: Package, target_name: str) -> Path:
         return self.kbuild_dir / package.target_dir / self.k_version / target_name
 
+    def resource_dir(self, package: Package, resource_name: str) -> Path:
+        return self.kbuild_dir / package.resource_dir / resource_name
+
+    def resource_files(self, package: Package, resource_name: str) -> List[Path]:
+        return [
+            self.resource_dir(package, resource_name) / file_name
+            for file_name in package.project.resource_file_names[resource_name]
+        ]
+
+    def include_dir(self, package: Package) -> Path:
+        return self.kbuild_dir / package.include_dir
+
+    def source_dir(self, package: Package) -> Path:
+        return self.include_dir(package) / package.name
+
+    def source_files(self, package: Package) -> List[Path]:
+        return [self.source_dir(package) / file_name for file_name in package.project.source_file_names]
+
     def clean(self, package: Package, target_name: str) -> None:
         shutil.rmtree(self.definition_dir(package, target_name), ignore_errors=True)
+
+    def sync(self, package: Package) -> List[Path]:
+        res: List[Path] = []
+
+        # Sync sources
+        res += sync_files(
+            source_dir=package.project.source_dir,
+            target_dir=self.source_dir(package),
+            file_names=package.project.source_file_names,
+        )
+
+        # Sync resources
+        for resource_name in package.project.resources:
+            res += sync_files(
+                source_dir=package.project.resources[resource_name],
+                target_dir=self.resource_dir(package, resource_name),
+                file_names=package.project.resource_file_names[resource_name],
+            )
+
+        return res
 
     def kompile(self, package: Package, target_name: str) -> Path:
         for sub_package in package.sub_packages:
@@ -45,11 +80,12 @@ class KBuild:
             return output_dir
 
         target = package.project.get_target(target_name)
+        args = self.kompile_args(package, target)
         kompile(
             output_dir=output_dir,
-            include_dirs=[self.kbuild_dir / include_dir for include_dir in package.include_dirs],
-            cwd=self.kbuild_dir / package.include_dir / package.name,
-            **target.kompile_args(),
+            include_dirs=[self.include_dir(sub_package) for sub_package in package.sub_packages],
+            cwd=self.kbuild_dir,
+            **args,
         )
 
         return output_dir
@@ -64,8 +100,38 @@ class KBuild:
         input_files: List[Path] = []
         for sub_package in package.sub_packages:
             input_files.append(sub_package.project.project_file)
-            input_files.extend(self.kbuild_dir / include_file for include_file in sub_package.include_files)
+            input_files.extend(self.source_files(sub_package))
+            for resource_name in sub_package.project.resources:
+                input_files.extend(self.resource_files(sub_package, resource_name))
 
         input_timestamps = (input_file.stat().st_mtime for input_file in input_files)
         target_timestamp = timestamp.stat().st_mtime
         return all(input_timestamp < target_timestamp for input_timestamp in input_timestamps)
+
+    def kompile_args(self, package: Package, target: Target) -> Dict[str, Any]:
+        args = target.dict
+        args.pop('name')
+        args['main_file'] = self.source_dir(package) / args['main_file']
+
+        if 'ccopts' in args:
+            args['ccopts'] = [self.render_opt(package, opt) for opt in args['ccopts']]
+
+        return args
+
+    def render_opt(self, package: Package, opt: str) -> str:
+        def render(match: Match) -> str:
+            package_name = match.group('package')
+            resource_name = match.group('resource')
+
+            sub_package = single(
+                sub_package for sub_package in package.sub_packages if sub_package.name == package_name
+            )
+            resource_path = self.resource_dir(sub_package, resource_name).resolve()
+
+            if not resource_path.exists():
+                raise ValueError('Failed to resolve opt {opt}: resource path {resource_path} does not exist')
+
+            return str(resource_path)
+
+        pattern = re.compile(r'{{ *(?P<package>\S+):(?P<resource>\S+) *}}')
+        return pattern.sub(render, opt)
