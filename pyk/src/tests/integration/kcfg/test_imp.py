3a2f5316e18cb3d2332dce675c384381ed99dd28
Tamás Tóth <tothtamas28@users.noreply.github.com>
2023-03-03 12:24:00 +0100

    Update `poetry.lock` (https://github.com/runtimeverification/pyk/pull/240)
    Co-authored-by: devops <devops@runtimeverification.com>

--- pyk/src/tests/integration/kcfg/test_imp.py
+++ pyk/src/tests/integration/kcfg/test_imp.py
@@ -240,7 +240,6 @@ class TestImpProof(KCFGExploreTest):
         max_depth: int,
         terminal_rules: Iterable[str],
     ) -> None:
-
         claims = kprove.get_claims(
             Path(spec_file), spec_module_name=spec_module, claim_labels=[f'{spec_module}.{claim_id}']
         )
