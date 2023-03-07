60a7b90a179f0c8c22d5caca9d9ffe8a35b2e042
Tamás Tóth <tothtamas28@users.noreply.github.com>
2023-03-07 10:26:09 +0100

    Refactor KAST-to-KORE tests (https://github.com/runtimeverification/pyk/pull/250)
    Fixes #203
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- pyk/src/tests/integration/konvert/test_cell_map.py
+++ pyk/src/tests/integration/konvert/test_cell_map.py
@@ -6,8 +6,7 @@ import pytest
 from pyk.kast.inner import KApply, KInner, KSort, KVariable
 from pyk.konvert import kast_to_kore
 from pyk.kore.kompiled import KompiledKore
-from pyk.kore.prelude import int_dv
-from pyk.kore.syntax import DV, App, EVar, LeftAssoc, Pattern, SortApp, String
+from pyk.kore.parser import KoreParser
 from pyk.ktool.kprint import KPrint
 from pyk.prelude.kint import INT, intToken
 
@@ -17,52 +16,26 @@ BIDIRECTIONAL_TEST_DATA: Final = (
     (
         'domain-value-int',
         INT,
-        int_dv(3),
+        r'\dv{SortInt{}}("3")',
         intToken(3),
     ),
     (
         'account-cell-map',
         KSort('AccountsCell'),
-        App(
-            "Lbl'-LT-'accounts'-GT-'",
-            [],
-            [
-                App(
-                    "Lbl'Unds'AccountCellMap'Unds'",
-                    [],
-                    [
-                        App(
-                            'LblAccountCellMapItem',
-                            [],
-                            [
-                                App(
-                                    "Lbl'-LT-'id'-GT-'",
-                                    [],
-                                    [DV(SortApp('SortInt'), String('3'))],
-                                ),
-                                App(
-                                    "Lbl'-LT-'account'-GT-'",
-                                    [],
-                                    [
-                                        App(
-                                            "Lbl'-LT-'id'-GT-'",
-                                            [],
-                                            [DV(SortApp('SortInt'), String('3'))],
-                                        ),
-                                        App(
-                                            "Lbl'-LT-'balance'-GT-'",
-                                            [],
-                                            [DV(SortApp('SortInt'), String('0'))],
-                                        ),
-                                    ],
-                                ),
-                            ],
-                        ),
-                        EVar("Var'Unds'DotVar2", SortApp('SortAccountCellMap')),
-                    ],
+        r"""
+        Lbl'-LT-'accounts'-GT-'{}(
+            Lbl'Unds'AccountCellMap'Unds'{}(
+                LblAccountCellMapItem{}(
+                    Lbl'-LT-'id'-GT-'{}(\dv{SortInt{}}("3")),
+                    Lbl'-LT-'account'-GT-'{}(
+                        Lbl'-LT-'id'-GT-'{}(\dv{SortInt{}}("3")),
+                        Lbl'-LT-'balance'-GT-'{}(\dv{SortInt{}}("0"))
+                    )
                 ),
-            ],
-        ),
+                Var'Unds'DotVar2 : SortAccountCellMap{}
+            )
+        )
+        """,
         KApply(
             '<accounts>',
             [
@@ -78,30 +51,28 @@ BIDIRECTIONAL_TEST_DATA: Final = (
     ),
 )
 
-KAST_TO_KORE_TEST_DATA: Final = (
+KAST_TO_KORE_TEST_DATA: Final = BIDIRECTIONAL_TEST_DATA + (
     (
         'variable-without-sort',
         KSort('Bar'),
-        EVar('VarX', SortApp('SortBar')),
+        'VarX : SortBar{}',
         KVariable('X'),
     ),
 )
 
-KORE_TO_KAST_TEST_DATA: Final = (
+KORE_TO_KAST_TEST_DATA: Final = BIDIRECTIONAL_TEST_DATA + (
     (
         'left-assoc',
         KSort('Map'),
-        LeftAssoc(
-            App(
-                "Lbl'Unds'Map'Unds'",
-                [],
-                [
-                    EVar('VarX', SortApp('SortMap')),
-                    EVar('VarY', SortApp('SortMap')),
-                    EVar('VarZ', SortApp('SortMap')),
-                ],
+        r"""
+        \left-assoc{}(
+            Lbl'Unds'Map'Unds'{}(
+                VarX : SortMap{},
+                VarY : SortMap{},
+                VarZ : SortMap{},
             )
-        ),
+        )
+        """,
         KApply(
             '_Map_',
             [
@@ -121,27 +92,9 @@ class TestKonvertCellMap(KPrintTest):
         return KompiledKore(definition_dir)
 
     @pytest.mark.parametrize(
-        'test_id,sort,kore,kast', BIDIRECTIONAL_TEST_DATA, ids=[test_id for test_id, *_ in BIDIRECTIONAL_TEST_DATA]
-    )
-    def test_bidirectional(
-        self,
-        kprint: KPrint,
-        kompiled_kore: KompiledKore,
-        test_id: str,
-        sort: KSort,
-        kore: Pattern,
-        kast: KInner,
-    ) -> None:
-        # When
-        actual_kore = kast_to_kore(kprint.definition, kompiled_kore, kast, sort=sort)
-        actual_kast = kprint.kore_to_kast(kore)
-
-        # Then
-        assert actual_kore == kore
-        assert actual_kast == kast
-
-    @pytest.mark.parametrize(
-        'test_id,sort,kore,kast', KAST_TO_KORE_TEST_DATA, ids=[test_id for test_id, *_ in KAST_TO_KORE_TEST_DATA]
+        'test_id,sort,kore_text,kast',
+        KAST_TO_KORE_TEST_DATA,
+        ids=[test_id for test_id, *_ in KAST_TO_KORE_TEST_DATA],
     )
     def test_kast_to_kore(
         self,
@@ -149,9 +102,12 @@ class TestKonvertCellMap(KPrintTest):
         kompiled_kore: KompiledKore,
         test_id: str,
         sort: KSort,
-        kore: Pattern,
+        kore_text: str,
         kast: KInner,
     ) -> None:
+        # Given
+        kore = KoreParser(kore_text).pattern()
+
         # When
         actual_kore = kast_to_kore(kprint.definition, kompiled_kore, kast, sort=sort)
 
@@ -159,9 +115,14 @@ class TestKonvertCellMap(KPrintTest):
         assert actual_kore == kore
 
     @pytest.mark.parametrize(
-        'test_id,_sort,kore,kast', KORE_TO_KAST_TEST_DATA, ids=[test_id for test_id, *_ in KORE_TO_KAST_TEST_DATA]
+        'test_id,_sort,kore_text,kast',
+        KORE_TO_KAST_TEST_DATA,
+        ids=[test_id for test_id, *_ in KORE_TO_KAST_TEST_DATA],
     )
-    def test_kore_to_kast(self, kprint: KPrint, test_id: str, _sort: KSort, kore: Pattern, kast: KInner) -> None:
+    def test_kore_to_kast(self, kprint: KPrint, test_id: str, _sort: KSort, kore_text: str, kast: KInner) -> None:
+        # Given
+        kore = KoreParser(kore_text).pattern()
+
         # When
         actual_kast = kprint.kore_to_kast(kore)
 
--- pyk/src/tests/integration/konvert/test_simple_proofs.py
+++ pyk/src/tests/integration/konvert/test_simple_proofs.py
@@ -6,28 +6,7 @@ import pytest
 from pyk.kast.inner import KApply, KInner, KLabel, KSequence, KSort, KToken, KVariable
 from pyk.konvert import kast_to_kore
 from pyk.kore.kompiled import KompiledKore
-from pyk.kore.prelude import BOOL as KORE_BOOL
-from pyk.kore.prelude import BYTES as KORE_BYTES
-from pyk.kore.prelude import INT as KORE_INT
-from pyk.kore.prelude import bool_dv, int_dv, string_dv
-from pyk.kore.syntax import (
-    DV,
-    And,
-    App,
-    Bottom,
-    Ceil,
-    Equals,
-    EVar,
-    Exists,
-    Implies,
-    LeftAssoc,
-    Not,
-    Pattern,
-    RightAssoc,
-    SortApp,
-    String,
-    Top,
-)
+from pyk.kore.parser import KoreParser
 from pyk.ktool.kprint import KPrint
 from pyk.prelude.bytes import bytesToken
 from pyk.prelude.kbool import TRUE
@@ -41,47 +20,48 @@ BIDIRECTIONAL_TEST_DATA: Final = (
     (
         'domain-value-int',
         INT,
-        int_dv(3),
+        r'\dv{SortInt{}}("3")',
         intToken(3),
     ),
     (
         'domain-value-string',
         STRING,
-        string_dv('foobar\n'),
+        r'\dv{SortString{}}("foobar\n")',
         stringToken('foobar\n'),
     ),
     (
         'domain-value-bytes',
         KSort('Bytes'),
-        DV(KORE_BYTES, String(chr(0) + chr(60) + chr(96) + chr(245))),
+        r'\dv{SortBytes{}}("\x00<`\xf5")',
         bytesToken(chr(0) + chr(60) + chr(96) + chr(245)),
     ),
     (
         'domain-value-other',
         KSort('Foo'),
-        DV(SortApp('SortFoo'), String('hello\\n')),
+        r'\dv{SortFoo{}}("hello\\n")',
         KToken('hello\\n', 'Foo'),
     ),
     (
         'ml-top',
         KSort('GeneratedTopCell'),
-        Top(SortApp('SortGeneratedTopCell')),
+        r'\top{SortGeneratedTopCell{}}()',
         mlTop(),
     ),
     (
         'ml-bottom',
         KSort('GeneratedTopCell'),
-        Bottom(SortApp('SortGeneratedTopCell')),
+        r'\bottom{SortGeneratedTopCell{}}()',
         mlBottom(),
     ),
     (
         'ml-implies',
         KSort('GeneratedTopCell'),
-        Implies(
-            SortApp('SortGeneratedTopCell'),
-            EVar('VarX', SortApp('SortGeneratedTopCell')),
-            EVar('VarY', SortApp('SortGeneratedTopCell')),
-        ),
+        r"""
+        \implies{SortGeneratedTopCell{}}(
+            VarX : SortGeneratedTopCell{},
+            VarY : SortGeneratedTopCell{}
+        )
+        """,
         mlImplies(
             KVariable('X', sort=KSort('GeneratedTopCell')),
             KVariable('Y', sort=KSort('GeneratedTopCell')),
@@ -91,41 +71,42 @@ BIDIRECTIONAL_TEST_DATA: Final = (
     (
         'variable-with-sort',
         INT,
-        EVar('VarX', KORE_INT),
+        'VarX : SortInt{}',
         KVariable('X', sort=INT),
     ),
     (
         'variable-with-super-sort',
         KSort('Bar'),
-        App('inj', [SortApp('SortBaz'), SortApp('SortBar')], [EVar('VarX', SortApp('SortBaz'))]),
+        'inj{SortBaz{}, SortBar{}}(VarX : SortBaz{})',
         KVariable('X', sort=KSort('Baz')),
     ),
     (
         'variable-with-underscore',
         INT,
-        EVar("VarX'Unds'Y", KORE_INT),
+        "VarX'Unds'Y : SortInt{}",
         KVariable('X_Y', sort=INT),
     ),
     (
         'issue:k/2762',
         KSort('Bool'),
-        App('Lblpred1', [], [int_dv(3)]),
+        r'Lblpred1{}(\dv{SortInt{}}("3"))',
         KApply('pred1', [intToken(3)]),
     ),
     (
         'cells-conversion',
         KSort('KCell'),
-        App("Lbl'-LT-'k'-GT-'", [], [App('dotk', [], [])]),
+        "Lbl'-LT-'k'-GT-'{}(dotk{}())",
         KApply('<k>', [KSequence()]),
     ),
     (
         'constrained-term',
         KSort('KCell'),
-        And(
-            SortApp('SortKCell'),
-            App("Lbl'-LT-'k'-GT-'", [], [App('dotk', [], [])]),
-            EVar('VarX', SortApp('SortKCell')),
-        ),
+        r"""
+        \and{SortKCell{}}(
+            Lbl'-LT-'k'-GT-'{}(dotk{}()),
+            VarX : SortKCell{}
+        )
+        """,
         KApply(
             KLabel('#And', params=[KSort('KCell')]),
             [KApply('<k>', [KSequence()]), KVariable('X', sort=KSort('KCell'))],
@@ -134,12 +115,12 @@ BIDIRECTIONAL_TEST_DATA: Final = (
     (
         'ml-equals',
         KSort('GeneratedTopCell'),
-        Equals(
-            KORE_BOOL,
-            SortApp('SortGeneratedTopCell'),
-            EVar('VarX', KORE_BOOL),
-            bool_dv(True),
-        ),
+        r"""
+        \equals{SortBool{}, SortGeneratedTopCell{}}(
+            VarX : SortBool{},
+            \dv{SortBool{}}("true")
+        )
+        """,
         KApply(
             KLabel('#Equals', [KSort('Bool'), KSort('GeneratedTopCell')]),
             [KVariable('X', sort=KSort('Bool')), TRUE],
@@ -148,11 +129,7 @@ BIDIRECTIONAL_TEST_DATA: Final = (
     (
         'ml-ceil',
         KSort('GeneratedTopCell'),
-        Ceil(
-            KORE_BOOL,
-            SortApp('SortGeneratedTopCell'),
-            EVar('VarX', KORE_BOOL),
-        ),
+        r'\ceil{SortBool{}, SortGeneratedTopCell{}}(VarX : SortBool{})',
         KApply(
             KLabel('#Ceil', [KSort('Bool'), KSort('GeneratedTopCell')]),
             [KVariable('X', sort=KSort('Bool'))],
@@ -161,11 +138,7 @@ BIDIRECTIONAL_TEST_DATA: Final = (
     (
         'ml-exists',
         KSort('Bool'),
-        Exists(
-            KORE_BOOL,
-            EVar('VarX', KORE_BOOL),
-            EVar('VarX', KORE_BOOL),
-        ),
+        r'\exists{SortBool{}}(VarX : SortBool{}, VarX : SortBool{})',
         KApply(
             KLabel('#Exists', [KSort('Bool')]),
             [
@@ -177,10 +150,7 @@ BIDIRECTIONAL_TEST_DATA: Final = (
     (
         'ml-not',
         KSort('Bool'),
-        Not(
-            KORE_BOOL,
-            EVar('VarX', KORE_BOOL),
-        ),
+        r'\not{SortBool{}}(VarX : SortBool{})',
         KApply(
             KLabel('#Not', [KSort('Bool')]),
             [KVariable('X', sort=KSort('Bool'))],
@@ -189,80 +159,55 @@ BIDIRECTIONAL_TEST_DATA: Final = (
     (
         'simple-injection',
         KSort('Foo'),
-        App('Lblfoo', [], [App('inj', [SortApp('SortBaz'), SortApp('SortBar')], [App('Lblbaz', [], [])])]),
+        'Lblfoo{}(inj{SortBaz{}, SortBar{}}(Lblbaz{}()))',
         KApply('foo', [KApply('baz')]),
     ),
     (
         'cells-conversion',
         KSort('KItem'),
-        App(
-            'inj',
-            [SortApp('SortKCell'), SortApp('SortKItem')],
-            [App("Lbl'-LT-'k'-GT-'", [], [App('dotk', [], [])])],
-        ),
+        "inj{SortKCell{}, SortKItem{}}(Lbl'-LT-'k'-GT-'{}(dotk{}()))",
         KApply('<k>', [KSequence()]),
     ),
     (
         'munging-problem',
         KSort('Baz'),
-        App("Lblfoo-bar'Unds'SIMPLE-PROOFS'Unds'Baz", [], []),
+        r"Lblfoo-bar'Unds'SIMPLE-PROOFS'Unds'Baz{}()",
         KApply('foo-bar_SIMPLE-PROOFS_Baz', []),
     ),
     (
         'kseq-empty',
         KSort('K'),
-        App('dotk', [], []),
+        'dotk{}()',
         KSequence([]),
     ),
     (
         'kseq-singleton',
         KSort('K'),
-        App(
-            'kseq',
-            [],
-            [
-                App(
-                    'inj',
-                    [SortApp('SortBaz'), SortApp('SortKItem')],
-                    [App("Lblfoo-bar'Unds'SIMPLE-PROOFS'Unds'Baz", [], [])],
-                ),
-                App('dotk', (), ()),
-            ],
-        ),
+        "kseq{}(inj{SortBaz{}, SortKItem{}}(Lblfoo-bar'Unds'SIMPLE-PROOFS'Unds'Baz{}()), dotk{}())",
         KSequence([KApply('foo-bar_SIMPLE-PROOFS_Baz')]),
     ),
     (
         'kseq-two-element',
         KSort('K'),
-        App(
-            'kseq',
-            [],
-            [
-                App("Lblfoo'Unds'SIMPLE-PROOFS'Unds'KItem", [], []),
-                App(
-                    'kseq',
-                    [],
-                    [
-                        App(
-                            'inj',
-                            [SortApp('SortBaz'), SortApp('SortKItem')],
-                            [App("Lblfoo-bar'Unds'SIMPLE-PROOFS'Unds'Baz", [], [])],
-                        ),
-                        App('dotk', (), ()),
-                    ],
-                ),
-            ],
-        ),
+        """
+        kseq{}(
+            Lblfoo'Unds'SIMPLE-PROOFS'Unds'KItem{}(),
+            kseq{}(
+                inj{SortBaz{}, SortKItem{}}(Lblfoo-bar'Unds'SIMPLE-PROOFS'Unds'Baz{}()),
+                dotk{}()
+            )
+        )
+        """,
         KSequence([KApply('foo_SIMPLE-PROOFS_KItem'), KApply('foo-bar_SIMPLE-PROOFS_Baz')]),
     ),
     (
         'if-then-else',
         KSort('K'),
-        App(
-            "Lbl'Hash'if'UndsHash'then'UndsHash'else'UndsHash'fi'Unds'K-EQUAL-SYNTAX'Unds'Sort'Unds'Bool'Unds'Sort'Unds'Sort",
-            [SortApp('SortK')],
-            [EVar('VarC', KORE_BOOL), EVar('VarB1', SortApp('SortK')), EVar('VarB2', SortApp('SortK'))],
-        ),
+        r"""
+        Lbl'Hash'if'UndsHash'then'UndsHash'else'UndsHash'fi'Unds'K-EQUAL-SYNTAX'Unds'Sort'Unds'Bool'Unds'Sort'Unds'Sort{SortK{}}(
+            VarC : SortBool{}, VarB1 : SortK{}, VarB2 : SortK {}
+        )
+        """,
         KApply(
             KLabel('#if_#then_#else_#fi_K-EQUAL-SYNTAX_Sort_Bool_Sort_Sort', [KSort('K')]),
             [
@@ -274,108 +219,67 @@ BIDIRECTIONAL_TEST_DATA: Final = (
     ),
 )
 
-KAST_TO_KORE_TEST_DATA: Final = (
+KAST_TO_KORE_TEST_DATA: Final = BIDIRECTIONAL_TEST_DATA + (
     (
         'variable-without-sort',
         KSort('Bar'),
-        EVar('VarX', SortApp('SortBar')),
+        'VarX : SortBar{}',
         KVariable('X'),
     ),
     (
         'variable-different-sorts',
         KSort('BarHolder'),
-        App(
-            'Lblbarholder',
-            [],
-            [
-                App(
-                    'Lblfoo',
-                    [],
-                    [App('inj', [SortApp('SortBaz'), SortApp('SortBar')], [EVar('VarB', SortApp('SortBaz'))])],
-                ),
-                App('inj', [SortApp('SortBaz'), SortApp('SortBar')], [EVar('VarB', SortApp('SortBaz'))]),
-            ],
-        ),
+        """
+        Lblbarholder{}(
+            Lblfoo{}(inj{SortBaz{}, SortBar{}}(VarB : SortBaz{})),
+            inj{SortBaz{}, SortBar{}}(VarB : SortBaz{})
+        )
+        """,
         KApply('barholder', [KApply('foo', [KVariable('B', sort=KSort('Baz'))]), KVariable('B')]),
     ),
     (
         'variable-with-multi-sort',
         KSort('BarHolder'),
-        App(
-            'Lblbarholder2',
-            [],
-            [
-                App('inj', [SortApp('SortBaz'), SortApp('SortBar')], [EVar('VarX', SortApp('SortBaz'))]),
-                EVar('VarX', SortApp('SortBaz')),
-            ],
-        ),
+        'Lblbarholder2{}(inj{SortBaz{}, SortBar{}}(VarX : SortBaz{}), VarX : SortBaz{})',
         KApply('barholder2', [KVariable('X', sort=KSort('Baz')), KVariable('X', sort=KSort('Bar'))]),
     ),
     (
         'ml-exists-var-inference',
         KSort('Foo'),
-        Exists(
-            SortApp('SortFoo'),
-            EVar('VarX', SortApp('SortBar')),
-            App('Lblfoo', [], [EVar('VarX', SortApp('SortBar'))]),
-        ),
+        r'\exists{SortFoo{}}(VarX : SortBar{}, Lblfoo{}(VarX : SortBar{}))',
         KApply(KLabel('#Exists', [KSort('Foo')]), [KVariable('X'), KApply('foo', [KVariable('X')])]),
     ),
     (
         'ksequence-empty',
         KSort('K'),
-        App('dotk', [], []),
+        'dotk{}()',
         KSequence([]),
     ),
     (
         'ksequence-singleton-var',
         KSort('K'),
-        EVar('VarCONT', SortApp('SortK')),
+        'VarCONT : SortK{}',
         KSequence([KVariable('CONT')]),
     ),
     (
         'ksequence-duo-var-1',
         KSort('K'),
-        App(
-            'kseq',
-            (),
-            [
-                EVar('VarELEM', SortApp('SortKItem')),
-                EVar('VarCONT', SortApp('SortK')),
-            ],
-        ),
+        'kseq{}(VarELEM : SortKItem{}, VarCONT : SortK{})',
         KSequence([KVariable('ELEM'), KVariable('CONT')]),
     ),
     (
         'ksequence-duo-var-2',
         KSort('K'),
-        App(
-            'kseq',
-            (),
-            [
-                EVar('VarELEM1', SortApp('SortKItem')),
-                App('kseq', (), [EVar('VarELEM2', SortApp('SortKItem')), App('dotk', (), ())]),
-            ],
-        ),
+        'kseq{}(VarELEM1 : SortKItem{}, kseq{}(VarELEM2 : SortKItem{}, dotk{}()))',
         KSequence([KVariable('ELEM1', sort=KSort('KItem')), KVariable('ELEM2', sort=KSort('KItem'))]),
     ),
 )
 
-KORE_TO_KAST_TEST_DATA: Final = (
+KORE_TO_KAST_TEST_DATA: Final = BIDIRECTIONAL_TEST_DATA + (
     (
         'left-assoc',
         KSort('Map'),
-        LeftAssoc(
-            App(
-                "Lbl'Unds'Map'Unds'",
-                [],
-                [
-                    EVar('VarX', SortApp('SortMap')),
-                    EVar('VarY', SortApp('SortMap')),
-                    EVar('VarZ', SortApp('SortMap')),
-                ],
-            )
-        ),
+        r"\left-assoc{}(Lbl'Unds'Map'Unds'{}(VarX : SortMap{}, VarY : SortMap{}, VarZ : SortMap{}))",
         KApply(
             '_Map_',
             [
@@ -387,17 +291,7 @@ KORE_TO_KAST_TEST_DATA: Final = (
     (
         'right-assoc',
         KSort('Map'),
-        RightAssoc(
-            App(
-                "Lbl'Unds'Map'Unds'",
-                [],
-                [
-                    EVar('VarX', SortApp('SortMap')),
-                    EVar('VarY', SortApp('SortMap')),
-                    EVar('VarZ', SortApp('SortMap')),
-                ],
-            )
-        ),
+        r"\right-assoc{}(Lbl'Unds'Map'Unds'{}(VarX : SortMap{}, VarY : SortMap{}, VarZ : SortMap{}))",
         KApply(
             '_Map_',
             [
@@ -417,27 +311,9 @@ class TestKonvertSimpleProofs(KPrintTest):
         return KompiledKore(definition_dir)
 
     @pytest.mark.parametrize(
-        'test_id,sort,kore,kast', BIDIRECTIONAL_TEST_DATA, ids=[test_id for test_id, *_ in BIDIRECTIONAL_TEST_DATA]
-    )
-    def test_bidirectional(
-        self,
-        kprint: KPrint,
-        kompiled_kore: KompiledKore,
-        test_id: str,
-        sort: KSort,
-        kore: Pattern,
-        kast: KInner,
-    ) -> None:
-        # When
-        actual_kore = kast_to_kore(kprint.definition, kompiled_kore, kast, sort=sort)
-        actual_kast = kprint.kore_to_kast(kore)
-
-        # Then
-        assert actual_kore == kore
-        assert actual_kast == kast
-
-    @pytest.mark.parametrize(
-        'test_id,sort,kore,kast', KAST_TO_KORE_TEST_DATA, ids=[test_id for test_id, *_ in KAST_TO_KORE_TEST_DATA]
+        'test_id,sort,kore_text,kast',
+        KAST_TO_KORE_TEST_DATA,
+        ids=[test_id for test_id, *_ in KAST_TO_KORE_TEST_DATA],
     )
     def test_kast_to_kore(
         self,
@@ -445,9 +321,12 @@ class TestKonvertSimpleProofs(KPrintTest):
         kompiled_kore: KompiledKore,
         test_id: str,
         sort: KSort,
-        kore: Pattern,
+        kore_text: str,
         kast: KInner,
     ) -> None:
+        # Given
+        kore = KoreParser(kore_text).pattern()
+
         # When
         actual_kore = kast_to_kore(kprint.definition, kompiled_kore, kast, sort=sort)
 
@@ -455,9 +334,21 @@ class TestKonvertSimpleProofs(KPrintTest):
         assert actual_kore == kore
 
     @pytest.mark.parametrize(
-        'test_id,_sort,kore,kast', KORE_TO_KAST_TEST_DATA, ids=[test_id for test_id, *_ in KORE_TO_KAST_TEST_DATA]
+        'test_id,_sort,kore_text,kast',
+        KORE_TO_KAST_TEST_DATA,
+        ids=[test_id for test_id, *_ in KORE_TO_KAST_TEST_DATA],
     )
-    def test_kore_to_kast(self, kprint: KPrint, test_id: str, _sort: KSort, kore: Pattern, kast: KInner) -> None:
+    def test_kore_to_kast(
+        self,
+        kprint: KPrint,
+        test_id: str,
+        _sort: KSort,
+        kore_text: str,
+        kast: KInner,
+    ) -> None:
+        # Given
+        kore = KoreParser(kore_text).pattern()
+
         # When
         actual_kast = kprint.kore_to_kast(kore)
 
