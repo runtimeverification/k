747c6911d1440d7661091a8098457eb7125a22bb
Nishant Rodrigues <nishantjr@gmail.com>
2023-03-13 16:56:31 -0500

    KRun: add `--search-final` and `--no-pattern` search options (https://github.com/runtimeverification/pyk/pull/257)
    Co-authored-by: devops <devops@runtimeverification.com>
    Co-authored-by: Tamás Tóth <tothtamas28@users.noreply.github.com>

---
+++ pyk/src/tests/unit/ktool/test_krun.py
@@ -0,0 +1,65 @@
+import copy
+from collections import OrderedDict
+from pathlib import Path
+from typing import Any, Dict, List, Tuple
+
+import pytest
+
+from pyk.ktool.krun import KRunOutput, _build_arg_list
+
+# fmt: off
+required_args: Dict[str, Any] = OrderedDict([
+    ('command', 'krun'),
+    ('input_file', None),
+    ('definition_dir', None),
+    ('output', None),
+    ('parser', None),
+    ('depth', None),
+    ('pmap', None),
+    ('cmap', None),
+    ('term', False),
+    ('no_expand_macros', False),
+    ('search_final', False),
+    ('no_pattern', False),
+])
+optional_args: Dict[str, Tuple[Any, List[str]]] = {
+    'input_file':       (Path('input/path'),    ['input/path']),
+    'definition_dir':   (Path('def/path'),      ['--definition', 'def/path']),
+    'output':           (KRunOutput.JSON,       ['--output', 'json']),
+    'parser':           ('cat',                 ['--parser', 'cat']),
+    'depth':            (1234,                  ['--depth', '1234']),
+    'pmap':             ({'FOO': 'bar', 'BUZZ': 'kill'},
+                                                ['-pFOO=bar', '-pBUZZ=kill']),
+    'cmap':             ({'COO': 'car', 'FUZZ': 'bill'},
+                                                ['-cCOO=car', '-cFUZZ=bill']),
+    'term':             (True,                  ['--term']),
+    'no_expand_macros': (True,                  ['--no-expand-macros']),
+    'search_final':     (True,                  ['--search-final']),
+    'no_pattern':       (True,                  ['--no-pattern']),
+}
+# fmt: on
+
+
+def make_kwargs(test_id: str, keys: List[str]) -> Tuple[str, Dict[str, Any], List[str]]:
+    ret_kwargs = copy.deepcopy(required_args)
+    ret_expected = ['krun']
+    for key in keys:
+        arg_param, arg_expected = optional_args[key]
+        ret_kwargs[key] = arg_param
+        ret_expected += arg_expected
+    return (test_id, ret_kwargs, ret_expected)
+
+
+test_data: Tuple[Tuple[str, List[str]], ...] = (
+    ('optional-only', []),
+    ('all-args', [key for key in required_args.keys() if key != 'command']),
+    *((key, [key]) for key in required_args.keys() if key != 'command'),  # All optional arguments passed alone
+)
+
+
+@pytest.mark.parametrize('params', test_data, ids=[x[0] for x in test_data])
+def test_build_arg_list(params: Tuple[str, List[str]]) -> None:
+    test_id, kwargs, expected = make_kwargs(*params)
+    print(kwargs)
+    actual = _build_arg_list(**kwargs)
+    assert actual == expected
