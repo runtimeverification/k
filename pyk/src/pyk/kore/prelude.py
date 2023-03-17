25cea577b8c70cee41f436686ef8fe0d74315615
Tamás Tóth <tothtamas28@users.noreply.github.com>
2023-03-17 20:42:55 +0100

    Add value classes for KORE ids (https://github.com/runtimeverification/pyk/pull/274)
    By passing these value objects as parameters, KORE objects can be
    instantiated without the need to validate the same strings for lexical
    correctness every time.
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- pyk/src/pyk/kore/prelude.py
+++ pyk/src/pyk/kore/prelude.py
@@ -1,6 +1,6 @@
 from typing import Final, Optional, Tuple, Union
 
-from .syntax import DV, App, LeftAssoc, Pattern, SortApp, String
+from .syntax import DV, App, LeftAssoc, Pattern, SortApp, String, SymbolId
 
 BOOL: Final = SortApp('SortBool')
 INT: Final = SortApp('SortInt')
@@ -18,8 +18,12 @@ def dv(val: Union[bool, int, str]) -> DV:
     raise TypeError(f'Illegal type: {type(val)}')
 
 
+TRUE: Final = DV(BOOL, String('true'))
+FALSE: Final = DV(BOOL, String('false'))
+
+
 def bool_dv(val: bool) -> DV:
-    return DV(BOOL, String('true' if val else 'false'))
+    return TRUE if val else FALSE
 
 
 def int_dv(val: int) -> DV:
@@ -30,23 +34,37 @@ def string_dv(val: str) -> DV:
     return DV(STRING, String(val))
 
 
+STOP_LIST: Final = App("Lbl'Stop'List")
+LBL_LIST: Final = SymbolId("Lbl'Unds'List'Unds'")
+LBL_LIST_ITEM: Final = SymbolId('LblListItem')
+
+
 def kore_list(*args: Pattern) -> Pattern:
     if not args:
-        return App("Lbl'Stop'List")
-    return LeftAssoc(App("Lbl'Unds'List'Unds'", args=(App('LblListItem', args=(arg,)) for arg in args)))
+        return STOP_LIST
+    return LeftAssoc(App(LBL_LIST, args=(App(LBL_LIST_ITEM, args=(arg,)) for arg in args)))
+
+
+STOP_SET: Final = App("Lbl'Stop'Set")
+LBL_SET: Final = SymbolId("Lbl'Unds'Set'Unds'")
+LBL_SET_ITEM: Final = SymbolId('LblSetItem')
 
 
 def kore_set(*args: Pattern) -> Pattern:
     if not args:
-        return App("Lbl'Stop'Set")
-    return LeftAssoc(App("Lbl'Unds'Set'Unds'", args=(App('LblSetItem', args=(arg,)) for arg in args)))
+        return STOP_SET
+    return LeftAssoc(App(LBL_SET, args=(App(LBL_SET_ITEM, args=(arg,)) for arg in args)))
+
+
+STOP_MAP = App("Lbl'Stop'Map")
+LBL_MAP = SymbolId("Lbl'Unds'Map'Unds'")
+LBL_MAP_ITEM = SymbolId("Lbl'UndsPipe'-'-GT-Unds'")
 
 
 def kore_map(*args: Tuple[Pattern, Pattern], cell: Optional[str] = None) -> Pattern:
-    cell = cell or ''
-    stop_symbol = f"Lbl'Stop'{cell}Map"
-    cons_symbol = f"Lbl'Unds'{cell}Map'Unds'"
-    item_symbol = "Lbl'UndsPipe'-'-GT-Unds'" if not cell else f'Lbl{cell}MapItem'
     if not args:
-        return App(stop_symbol)
+        return App(f"Lbl'Stop'{cell}Map") if cell else STOP_MAP
+
+    cons_symbol = SymbolId(f"Lbl'Unds'{cell}Map'Unds'") if cell else LBL_MAP
+    item_symbol = SymbolId(f'Lbl{cell}MapItem') if cell else LBL_MAP_ITEM
     return LeftAssoc(App(cons_symbol, args=(App(item_symbol, args=arg) for arg in args)))
