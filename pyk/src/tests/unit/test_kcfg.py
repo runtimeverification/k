38238d43ab299bab43f2b10d2271108d1e2636cf
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2023-03-28 10:04:43 -0600

    Direct storage of split nodes (#124)
    ~Blocked on: https://github.com/runtimeverification/pyk/pull/122~
    ~Blocked on: https://github.com/runtimeverification/pyk/pull/294~
    ~Blocked on: https://github.com/runtimeverification/pyk/pull/302~
    ~Blocked on: https://github.com/runtimeverification/pyk/pull/303~
    
    This stores Split nodes as their own data-structure, rather than
    shimming them in as edges/covers. As I've been working on the summarizer
    paper, it's apparent that we need more restrictions on what a valid KCFG
    is. Explicit representation of split edges allows us to do that.
    
    **NOTE**: We _lose_ the ability to represent non-determinism properly
    here! This is because we are restricting each node to having _only one_
    outgoing Edge, Cover, or Split from it! We can recover this later with
    direct representation of non-determinism, as a multi-edge that takes
    exactly one step on each edge to the non-deterministic transitions.
    
    - Add `KCFG.Successor` class, which can be an edge or a multi-edge, and
    just has the `source` attribute. Make `KCFG.EdgeLike` a subclass of this
    class.
    - Add `KCFG.Split` class, which is a subclass of `KCFG.Successor`, and
    stores a list of `targets`. Each `target` is a `Tuple[Node, CSubst]`,
    which is basically a "reverse cover", it points at the more specialized
    node instead of the more abstract node.
    - Stricter enforcement on the structure of the `KCFG`. In particular,
    every node can have at _most_ one successor. We were allowing multiple
    0-step `KCFG.Edge` out of a given node to represent case splits. Those
    are directly represented as the `KCFG.Split` multi-edge now.
    - Adjusts the various routines to use the new `KCFG.split`
    datastructure, including:
      - `reachable_nodes`: traverses splits forward/backwards.
    - `paths_between`: almost complete rewrite now that we must support
    multi-edges.
    - `KCFG.pretty_segments` now directly detects splits instead of
    implicitly detecting them as "multiple outgoing 0-depth `KCFG.Edge` on
    the same node".
    - Updates to `KCFGExplore` routines:
    - `KCFGExplore.step` is now allowed to assume only one successor edge,
    simplifying its logic.
    - `KCFGExplore.all_path_reachability_prove` can now take advantage of
    new `KCFG.split_on_constraints`, which stores the split as a
    `KCFG.Split` node.
    - Adds a test of reachable nodes in the reverse case, that was failing
    before fixing a bug in the handling of `KCFG.Split` nodes.
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- pyk/src/tests/unit/test_kcfg.py
+++ pyk/src/tests/unit/test_kcfg.py
@@ -14,7 +14,7 @@ from pyk.utils import shorten_hash
 from .mock_kprint import MockKPrint
 
 if TYPE_CHECKING:
-    from typing import Any, Dict, Iterable, List, Optional, Tuple
+    from typing import Any, Dict, Iterable, List, Tuple
 
     from pyk.kast import KInner
 
@@ -40,18 +40,31 @@ def node(i: int) -> KCFG.Node:
 
 
 def edge(i: int, j: int) -> KCFG.Edge:
-    return KCFG.Edge(node(i), node(j), mlTop(), 0)
+    return KCFG.Edge(node(i), node(j), 1)
 
 
-def node_dicts(n: int) -> List[Dict[str, Any]]:
-    return [node(i).to_dict() for i in range(n)]
+def cover(i: int, j: int) -> KCFG.Cover:
+    csubst = term(j).match_with_constraint(term(i))
+    assert csubst is not None
+    return KCFG.Cover(node(i), node(j), csubst)
+
+
+def split(i: int, js: Iterable[int]) -> KCFG.Split:
+    split_substs = []
+    for j in js:
+        csubst = term(i).match_with_constraint(term(j))
+        assert csubst is not None
+        split_substs.append(csubst)
+    return KCFG.Split(node(i), zip(map(node, js), split_substs))
+
+
+def node_dicts(n: int, start: int = 0) -> List[Dict[str, Any]]:
+    return [node(i).to_dict() for i in range(start, n)]
 
 
 def edge_dicts(*edges: Iterable) -> List[Dict[str, Any]]:
-    def _make_edge_dict(i: int, j: int, depth: int = 0, condition: Optional[KInner] = None) -> Dict[str, Any]:
-        if condition is None:
-            condition = mlTop()
-        return {'source': nid(i), 'target': nid(j), 'condition': condition.to_dict(), 'depth': depth}
+    def _make_edge_dict(i: int, j: int, depth: int = 1) -> Dict[str, Any]:
+        return {'source': nid(i), 'target': nid(j), 'depth': depth}
 
     return [_make_edge_dict(*edge) for edge in edges]
 
@@ -63,6 +76,19 @@ def cover_dicts(*edges: Tuple[int, int]) -> List[Dict[str, Any]]:
         assert csubst is not None
         covers.append({'source': nid(i), 'target': nid(j), 'csubst': csubst.to_dict()})
     return covers
+
+
+def split_dicts(*edges: Tuple[int, Iterable[Tuple[int, KInner]]]) -> Dict[str, Any]:
+    splits = {}
+    for s, ts in edges:
+        targets = {}
+        for t, constraint in ts:
+            csubst = term(s).match_with_constraint(term(t))
+            assert csubst is not None
+            csubst = csubst.add_constraint(constraint)
+            targets[nid(t)] = csubst.to_dict()
+        splits[nid(t)] = {'source': nid(s), 'targets': targets}
+    return splits
 
 
 def test_from_dict_single_node() -> None:
@@ -194,7 +220,7 @@ def test_insert_loop_edge() -> None:
     cfg = KCFG.from_dict(d)
 
     # When
-    new_edge = cfg.create_edge(nid(0), nid(0), mlTop(), 0)
+    new_edge = cfg.create_edge(nid(0), nid(0), 1)
 
     # Then
     assert new_edge == edge(0, 0)
@@ -209,7 +235,7 @@ def test_insert_simple_edge() -> None:
     cfg = KCFG.from_dict(d)
 
     # When
-    new_edge = cfg.create_edge(nid(0), nid(1), mlTop(), 0)
+    new_edge = cfg.create_edge(nid(0), nid(1), 1)
 
     # Then
     assert new_edge == edge(0, 1)
@@ -218,14 +244,23 @@ def test_insert_simple_edge() -> None:
 
 
 def test_get_successors() -> None:
-    d = {'nodes': node_dicts(3), 'edges': edge_dicts((0, 1), (0, 2))}
+    d = {
+        'nodes': node_dicts(15),
+        'edges': edge_dicts((11, 12)),
+        'splits': split_dicts((12, [(13, mlTop()), (14, mlTop())])),
+        'covers': cover_dicts((14, 11)),
+    }
     cfg = KCFG.from_dict(d)
 
     # When
-    succs = set(cfg.edges(source_id=nid(0)))
+    edges = set(cfg.edges(source_id=nid(11)))
+    covers = set(cfg.covers(source_id=nid(14)))
+    splits = set(cfg.splits(source_id=nid(12)))
 
     # Then
-    assert succs == {edge(0, 1), edge(0, 2)}
+    assert edges == {edge(11, 12)}
+    assert covers == {cover(14, 11)}
+    assert splits == {split(12, [13, 14])}
 
 
 def test_get_predecessors() -> None:
@@ -242,37 +277,48 @@ def test_get_predecessors() -> None:
 def test_reachable_nodes() -> None:
     # Given
     d = {
-        'nodes': node_dicts(12),
-        'edges': edge_dicts((0, 1), (0, 5), (0, 11), (1, 2), (1, 3), (2, 4), (3, 4), (4, 1)),
-        'covers': cover_dicts((4, 11)),
+        'nodes': node_dicts(18),
+        'edges': edge_dicts((12, 14), (13, 14), (14, 11)),
+        'covers': cover_dicts((11, 12)),
+        'splits': split_dicts(
+            (15, [(11, mlTop()), (12, mlTop()), (16, mlTop())]), (16, [(11, mlTop()), (17, mlTop())])
+        ),
     }
     cfg = KCFG.from_dict(d)
 
     # When
-    nodes_1 = cfg.reachable_nodes(nid(1))
-    nodes_2 = cfg.reachable_nodes(nid(1), traverse_covers=True)
+    nodes_1 = cfg.reachable_nodes(nid(11))
+    nodes_2 = cfg.reachable_nodes(nid(11), traverse_covers=True)
+    nodes_3 = cfg.reachable_nodes(nid(15), traverse_covers=True)
+    nodes_4 = cfg.reachable_nodes(nid(12), traverse_covers=True, reverse=True)
 
     # Then
-    assert nodes_1 == {node(1), node(2), node(3), node(4)}
-    assert nodes_2 == {node(1), node(2), node(3), node(4), node(11)}
+    assert nodes_1 == {node(11)}
+    assert nodes_2 == {node(11), node(12), node(14)}
+    assert nodes_3 == {node(15), node(11), node(12), node(16), node(17), node(14)}
+    assert nodes_4 == {node(12), node(15), node(11), node(14), node(16), node(13)}
 
 
 def test_paths_between() -> None:
     # Given
     d = {
-        'nodes': node_dicts(4),
-        'edges': edge_dicts((0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 0)),
+        'nodes': node_dicts(18),
+        'edges': edge_dicts((12, 14), (13, 14), (14, 11)),
+        'covers': cover_dicts((11, 12)),
+        'splits': split_dicts(
+            (15, [(11, mlTop()), (12, mlTop()), (16, mlTop())]), (16, [(11, mlTop()), (17, mlTop())])
+        ),
     }
     cfg = KCFG.from_dict(d)
 
     # When
-    paths = set(cfg.paths_between(nid(0), nid(3)))
+    paths = set(cfg.paths_between(nid(15), nid(14)))
 
     # Then
     assert paths == {
-        (edge(0, 1), edge(1, 3)),
-        (edge(0, 2), edge(2, 3)),
-        (edge(0, 1), edge(1, 2), edge(2, 3)),
+        (split(15, [11]), cover(11, 12), edge(12, 14)),
+        (split(15, [12]), edge(12, 14)),
+        (split(15, [16]), split(16, [11]), cover(11, 12), edge(12, 14)),
     }
 
 
@@ -280,7 +326,6 @@ def test_resolve() -> None:
     # Given
     d = {
         'nodes': node_dicts(4),
-        'edges': edge_dicts((0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 0)),
     }
     cfg = KCFG.from_dict(d)
 
@@ -339,25 +384,31 @@ def test_aliases() -> None:
 
 
 def test_pretty_print() -> None:
+    def _x_equals(i: int) -> KInner:
+        return mlEquals(KVariable('x'), token(i))
+
     d = {
-        'init': [nid(0)],
-        'target': [nid(6)],
-        'nodes': node_dicts(13),
-        'aliases': {'foo': nid(3), 'bar': nid(3)},
-        # Each of the branching edges have given depth=0
-        # fmt: off
-        'edges': edge_dicts((0, 1, 1), (1, 2, 5), (2, 3, 1),                                        # Initial Linear segment
-                            (3, 4, 0, mlEquals(KVariable('x'), token(4))), (4, 5, 1), (5, 2, 1),    # Loops back
-                            (3, 5, 0, mlEquals(KVariable('x'), token(5))),                          # Go to previous non-terminal node not as loop
-                            (3, 6, 0, mlEquals(KVariable('x'), token(6))),                          # Terminates
-                            (3, 7, 0, mlEquals(KVariable('x'), token(7))), (7, 6, 1),               # Go to previous terminal node not as loop
-                            (3, 9, 0, mlEquals(KVariable('x'), token(9))),                          # Frontier
-                            (3, 10, 0, mlEquals(KVariable('x'), token(10))),                        # Stuck
-                            (3, 11, 0, mlEquals(KVariable('x'), token(11))), (11, 8, 1),            # Covered
-                            ),
-        # fmt: on
-        'covers': cover_dicts((8, 11)),  # Loops back
-        'expanded': [nid(i) for i in [0, 1, 2, 3, 4, 5, 7, 10, 11]],
+        'init': [nid(20)],
+        'target': [nid(16)],
+        'nodes': node_dicts(24, start=9),
+        'aliases': {'foo': nid(13), 'bar': nid(13)},
+        'edges': edge_dicts((20, 11), (11, 12, 5), (12, 13), (14, 15), (15, 12), (17, 16), (21, 18)),
+        'covers': cover_dicts((18, 21)),
+        'splits': split_dicts(
+            (
+                13,
+                [
+                    (14, _x_equals(14)),
+                    (15, _x_equals(15)),
+                    (16, _x_equals(16)),
+                    (17, _x_equals(17)),
+                    (19, _x_equals(19)),
+                    (22, _x_equals(22)),
+                    (21, _x_equals(21)),
+                ],
+            )
+        ),
+        'expanded': [nid(i) for i in [20, 11, 12, 13, 14, 15, 17, 21, 22]],
     }
     cfg = KCFG.from_dict(d)
 
@@ -366,197 +417,225 @@ def test_pretty_print() -> None:
 
     expected = (
         f'\n'
-        f'┌─ {_short_hash(0)} (init, expanded)\n'
+        f'┌─ {_short_hash(20)} (init, expanded)\n'
         f'│\n'
         f'│  (1 step)\n'
-        f'├─ {_short_hash(1)} (expanded)\n'
+        f'├─ {_short_hash(11)} (expanded)\n'
         f'│\n'
         f'│  (5 steps)\n'
-        f'├─ {_short_hash(2)} (expanded)\n'
+        f'├─ {_short_hash(12)} (expanded)\n'
         f'│\n'
         f'│  (1 step)\n'
-        f'├─ {_short_hash(3)} (expanded, @bar, @foo)\n'
+        f'├─ {_short_hash(13)} (expanded, split, @bar, @foo)\n'
         f'┃\n'
-        f'┣━━┓ constraint: #Equals ( x , 4 )\n'
+        f'┣━━┓ constraint: #Equals ( x , 14 )\n'
+        f'┃  ┃ subst: V13 <- V14\n'
         f'┃  │\n'
-        f'┃  ├─ {_short_hash(4)} (expanded)\n'
-        f'┃  │\n'
-        f'┃  │  (1 step)\n'
-        f'┃  ├─ {_short_hash(5)} (expanded)\n'
+        f'┃  ├─ {_short_hash(14)} (expanded)\n'
         f'┃  │\n'
         f'┃  │  (1 step)\n'
-        f'┃  └─ {_short_hash(2)} (expanded)\n'
+        f'┃  ├─ {_short_hash(15)} (expanded)\n'
+        f'┃  │\n'
+        f'┃  │  (1 step)\n'
+        f'┃  └─ {_short_hash(12)} (expanded)\n'
         f'┃     (looped back)\n'
         f'┃\n'
-        f'┣━━┓ constraint: #Equals ( x , 5 )\n'
+        f'┣━━┓ constraint: #Equals ( x , 15 )\n'
+        f'┃  ┃ subst: V13 <- V15\n'
         f'┃  │\n'
-        f'┃  └─ {_short_hash(5)} (expanded)\n'
+        f'┃  └─ {_short_hash(15)} (expanded)\n'
         f'┃     (continues as previously)\n'
         f'┃\n'
-        f'┣━━┓ constraint: #Equals ( x , 6 )\n'
+        f'┣━━┓ constraint: #Equals ( x , 16 )\n'
+        f'┃  ┃ subst: V13 <- V16\n'
         f'┃  │\n'
-        f'┃  └─ {_short_hash(6)} (target, leaf)\n'
+        f'┃  └─ {_short_hash(16)} (target, leaf)\n'
         f'┃\n'
-        f'┣━━┓ constraint: #Equals ( x , 7 )\n'
+        f'┣━━┓ constraint: #Equals ( x , 17 )\n'
+        f'┃  ┃ subst: V13 <- V17\n'
         f'┃  │\n'
-        f'┃  ├─ {_short_hash(7)} (expanded)\n'
+        f'┃  ├─ {_short_hash(17)} (expanded)\n'
         f'┃  │\n'
         f'┃  │  (1 step)\n'
-        f'┃  └─ {_short_hash(6)} (target, leaf)\n'
+        f'┃  └─ {_short_hash(16)} (target, leaf)\n'
         f'┃\n'
-        f'┣━━┓ constraint: #Equals ( x , 9 )\n'
+        f'┣━━┓ constraint: #Equals ( x , 19 )\n'
+        f'┃  ┃ subst: V13 <- V19\n'
         f'┃  │\n'
-        f'┃  └─ \033[1m{_short_hash(9)} (frontier, leaf)\033[0m\n'
+        f'┃  └─ \033[1m{_short_hash(19)} (frontier, leaf)\033[0m\n'
         f'┃\n'
-        f'┣━━┓ constraint: #Equals ( x , 10 )\n'
+        f'┣━━┓ constraint: #Equals ( x , 22 )\n'
+        f'┃  ┃ subst: V13 <- V22\n'
         f'┃  │\n'
-        f'┃  └─ {_short_hash(10)} (expanded, stuck, leaf)\n'
+        f'┃  └─ {_short_hash(22)} (expanded, stuck, leaf)\n'
         f'┃     (stuck)\n'
         f'┃\n'
-        f'┗━━┓ constraint: #Equals ( x , 11 )\n'
+        f'┗━━┓ constraint: #Equals ( x , 21 )\n'
+        f'   ┃ subst: V13 <- V21\n'
         f'   │\n'
-        f'   ├─ {_short_hash(11)} (expanded)\n'
+        f'   ├─ {_short_hash(21)} (expanded)\n'
         f'   │\n'
         f'   │  (1 step)\n'
-        f'   ├─ {_short_hash(8)} (leaf)\n'
+        f'   ├─ {_short_hash(18)} (leaf)\n'
         f'   │\n'
         f'   ┊  constraint: true\n'
-        f'   ┊  subst: V11 <- 8\n'
-        f'   └─ {_short_hash(11)} (expanded)\n'
+        f'   ┊  subst: V21 <- V18\n'
+        f'   └─ {_short_hash(21)} (expanded)\n'
         f'      (looped back)\n'
         f'\n'
         f'\n'
         f'Target Nodes:\n'
         f'\n'
-        f'{_short_hash(6)} (target, leaf)\n'
+        f'{_short_hash(16)} (target, leaf)\n'
         f'\n'
         f'Remaining Nodes:\n'
         f'\n'
-        f'\033[1m{_short_hash(12)} (frontier, leaf)\033[0m\n'
+        f'\033[1m{_short_hash(10)} (frontier, leaf)\033[0m\n'
+        f'\n'
+        f'\033[1m{_short_hash(9)} (frontier, leaf)\033[0m\n'
+        f'\n'
+        f'\033[1m{_short_hash(23)} (frontier, leaf)\033[0m\n'
     )
 
     expected_short_info = (
         f'\n'
-        f'┌─ {_short_hash(0)} (init, expanded)\n'
+        f'┌─ {_short_hash(20)} (init, expanded)\n'
         f'│    <top>\n'
-        f'│      0\n'
+        f'│      V20\n'
         f'│    </top>\n'
         f'│\n'
         f'│  (1 step)\n'
-        f'├─ {_short_hash(1)} (expanded)\n'
+        f'├─ {_short_hash(11)} (expanded)\n'
         f'│    <top>\n'
-        f'│      1\n'
+        f'│      V11\n'
         f'│    </top>\n'
         f'│\n'
         f'│  (5 steps)\n'
-        f'├─ {_short_hash(2)} (expanded)\n'
+        f'├─ {_short_hash(12)} (expanded)\n'
         f'│    <top>\n'
-        f'│      2\n'
+        f'│      V12\n'
         f'│    </top>\n'
         f'│\n'
         f'│  (1 step)\n'
-        f'├─ {_short_hash(3)} (expanded, @bar, @foo)\n'
+        f'├─ {_short_hash(13)} (expanded, split, @bar, @foo)\n'
         f'│    <top>\n'
-        f'│      3\n'
+        f'│      V13\n'
         f'│    </top>\n'
         f'┃\n'
-        f'┣━━┓ constraint: #Equals ( x , 4 )\n'
+        f'┣━━┓ constraint: #Equals ( x , 14 )\n'
+        f'┃  ┃ subst: V13 <- V14\n'
         f'┃  │\n'
-        f'┃  ├─ {_short_hash(4)} (expanded)\n'
+        f'┃  ├─ {_short_hash(14)} (expanded)\n'
         f'┃  │    <top>\n'
-        f'┃  │      4\n'
+        f'┃  │      V14\n'
         f'┃  │    </top>\n'
         f'┃  │\n'
         f'┃  │  (1 step)\n'
-        f'┃  ├─ {_short_hash(5)} (expanded)\n'
+        f'┃  ├─ {_short_hash(15)} (expanded)\n'
         f'┃  │    <top>\n'
-        f'┃  │      5\n'
+        f'┃  │      V15\n'
         f'┃  │    </top>\n'
         f'┃  │\n'
         f'┃  │  (1 step)\n'
-        f'┃  └─ {_short_hash(2)} (expanded)\n'
+        f'┃  └─ {_short_hash(12)} (expanded)\n'
         f'┃       <top>\n'
-        f'┃         2\n'
+        f'┃         V12\n'
         f'┃       </top>\n'
         f'┃     (looped back)\n'
         f'┃\n'
-        f'┣━━┓ constraint: #Equals ( x , 5 )\n'
+        f'┣━━┓ constraint: #Equals ( x , 15 )\n'
+        f'┃  ┃ subst: V13 <- V15\n'
         f'┃  │\n'
-        f'┃  └─ {_short_hash(5)} (expanded)\n'
+        f'┃  └─ {_short_hash(15)} (expanded)\n'
         f'┃       <top>\n'
-        f'┃         5\n'
+        f'┃         V15\n'
         f'┃       </top>\n'
         f'┃     (continues as previously)\n'
         f'┃\n'
-        f'┣━━┓ constraint: #Equals ( x , 6 )\n'
+        f'┣━━┓ constraint: #Equals ( x , 16 )\n'
+        f'┃  ┃ subst: V13 <- V16\n'
         f'┃  │\n'
-        f'┃  └─ {_short_hash(6)} (target, leaf)\n'
+        f'┃  └─ {_short_hash(16)} (target, leaf)\n'
         f'┃       <top>\n'
-        f'┃         6\n'
+        f'┃         V16\n'
         f'┃       </top>\n'
         f'┃\n'
-        f'┣━━┓ constraint: #Equals ( x , 7 )\n'
+        f'┣━━┓ constraint: #Equals ( x , 17 )\n'
+        f'┃  ┃ subst: V13 <- V17\n'
         f'┃  │\n'
-        f'┃  ├─ {_short_hash(7)} (expanded)\n'
+        f'┃  ├─ {_short_hash(17)} (expanded)\n'
         f'┃  │    <top>\n'
-        f'┃  │      7\n'
+        f'┃  │      V17\n'
         f'┃  │    </top>\n'
         f'┃  │\n'
         f'┃  │  (1 step)\n'
-        f'┃  └─ {_short_hash(6)} (target, leaf)\n'
+        f'┃  └─ {_short_hash(16)} (target, leaf)\n'
         f'┃       <top>\n'
-        f'┃         6\n'
+        f'┃         V16\n'
         f'┃       </top>\n'
         f'┃\n'
-        f'┣━━┓ constraint: #Equals ( x , 9 )\n'
+        f'┣━━┓ constraint: #Equals ( x , 19 )\n'
+        f'┃  ┃ subst: V13 <- V19\n'
         f'┃  │\n'
-        f'┃  └─ \033[1m{_short_hash(9)} (frontier, leaf)\033[0m\n'
+        f'┃  └─ \033[1m{_short_hash(19)} (frontier, leaf)\033[0m\n'
         f'┃       <top>\n'
-        f'┃         9\n'
+        f'┃         V19\n'
         f'┃       </top>\n'
         f'┃\n'
-        f'┣━━┓ constraint: #Equals ( x , 10 )\n'
+        f'┣━━┓ constraint: #Equals ( x , 22 )\n'
+        f'┃  ┃ subst: V13 <- V22\n'
         f'┃  │\n'
-        f'┃  └─ {_short_hash(10)} (expanded, stuck, leaf)\n'
+        f'┃  └─ {_short_hash(22)} (expanded, stuck, leaf)\n'
         f'┃       <top>\n'
-        f'┃         10\n'
+        f'┃         V22\n'
         f'┃       </top>\n'
         f'┃     (stuck)\n'
         f'┃\n'
-        f'┗━━┓ constraint: #Equals ( x , 11 )\n'
+        f'┗━━┓ constraint: #Equals ( x , 21 )\n'
+        f'   ┃ subst: V13 <- V21\n'
         f'   │\n'
-        f'   ├─ {_short_hash(11)} (expanded)\n'
+        f'   ├─ {_short_hash(21)} (expanded)\n'
         f'   │    <top>\n'
-        f'   │      V11\n'
+        f'   │      V21\n'
         f'   │    </top>\n'
         f'   │\n'
         f'   │  (1 step)\n'
-        f'   ├─ {_short_hash(8)} (leaf)\n'
+        f'   ├─ {_short_hash(18)} (leaf)\n'
         f'   │    <top>\n'
-        f'   │      8\n'
+        f'   │      V18\n'
         f'   │    </top>\n'
         f'   │\n'
         f'   ┊  constraint: true\n'
-        f'   ┊  subst: V11 <- 8\n'
-        f'   └─ {_short_hash(11)} (expanded)\n'
+        f'   ┊  subst: V21 <- V18\n'
+        f'   └─ {_short_hash(21)} (expanded)\n'
         f'        <top>\n'
-        f'          V11\n'
+        f'          V21\n'
         f'        </top>\n'
         f'      (looped back)\n'
         f'\n'
         f'\n'
         f'Target Nodes:\n'
         f'\n'
-        f'{_short_hash(6)} (target, leaf)\n'
+        f'{_short_hash(16)} (target, leaf)\n'
         f' <top>\n'
-        f'   6\n'
+        f'   V16\n'
         f' </top>\n'
         f'\n'
         f'Remaining Nodes:\n'
         f'\n'
-        f'\033[1m{_short_hash(12)} (frontier, leaf)\033[0m\n'
+        f'\033[1m{_short_hash(10)} (frontier, leaf)\033[0m\n'
         f' <top>\n'
-        f'   V12\n'
+        f'   10\n'
+        f' </top>\n'
+        f'\n'
+        f'\033[1m{_short_hash(9)} (frontier, leaf)\033[0m\n'
+        f' <top>\n'
+        f'   9\n'
+        f' </top>\n'
+        f'\n'
+        f'\033[1m{_short_hash(23)} (frontier, leaf)\033[0m\n'
+        f' <top>\n'
+        f'   V23\n'
         f' </top>\n'
     )
 
