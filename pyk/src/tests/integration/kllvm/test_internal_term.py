96baaaae12ab6a6db55242216efc18f34c810e60
Scott Guest <scott.guest@runtimeverification.com>
2023-04-12 13:43:03 -0400

    Added integration test for lnternalTerm.__str__ binding (#346)
    As noted in runtimeverification/llvm-backend#724, there was previously a
    bug in the InternalTerm.\_\_str\_\_ binding which led to extraneous data
    being included in the string, causing a crash.
    
    This issue wasn't reproducible in the LLVM backend testing environment,
    so I am adding a test here on the Pyk side instead. I confirmed that the
    test fails if the LLVM backend fix is rolled back.
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

---
+++ pyk/src/tests/integration/kllvm/test_internal_term.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import pyk.kllvm.load  # noqa: F401
+from pyk.kllvm.parser import Parser
+
+from .utils import RuntimeTest
+
+if TYPE_CHECKING:
+    from types import ModuleType
+
+    from pyk.kllvm.ast import Pattern
+
+
+class TestInternalTerm(RuntimeTest):
+    KOMPILE_MAIN_FILE = 'k-files/imp.k'
+
+    def test_str_llvm_backend_issue_724(self, runtime: ModuleType) -> None:
+        for _ in range(10000):
+            term = runtime.InternalTerm(start_pattern())
+            term.step(-1)
+            # just checking that str doesn't crash
+            str(term)
+
+
+def start_pattern() -> Pattern:
+    """
+    <k> int x ; x = 1 </k>
+    """
+    text = r"""
+        LblinitGeneratedTopCell{}(
+            Lbl'Unds'Map'Unds'{}(
+                Lbl'Stop'Map{}(),
+                Lbl'UndsPipe'-'-GT-Unds'{}(
+                    inj{SortKConfigVar{}, SortKItem{}}(\dv{SortKConfigVar{}}("$PGM")),
+                    inj{SortPgm{}, SortKItem{}}(
+                        Lblint'UndsSClnUnds'{}(
+                            Lbl'UndsCommUnds'{}(
+                                \dv{SortId{}}("x"),
+                                Lbl'Stop'List'LBraQuotUndsCommUndsQuotRBraUnds'Ids{}()
+                            ),
+                            Lbl'UndsEqlsUndsSCln'{}(
+                                \dv{SortId{}}("x"),
+                                inj{SortInt{}, SortAExp{}}(\dv{SortInt{}}("1"))
+                            )
+                        )
+                    )
+                )
+            )
+        )
+
+    """
+    return Parser.from_string(text).pattern()
