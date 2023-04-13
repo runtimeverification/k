ed8b144de26fc69b471d721ef7561d963edcf963
Tamás Tóth <tothtamas28@users.noreply.github.com>
2023-04-13 13:37:17 +0200

    Remove string dequoting from `KToken` (https://github.com/runtimeverification/pyk/pull/333)
    ~Blocked on:
    https://github.com/runtimeverification/haskell-backend/issues/3563~
    **Edit**: skipping tests
    
    Fixes #140
    Fixes #314
    
    Removes string dequoting from `KToken`, which now occurs on KAST-to-KORE
    conversion.
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- pyk/src/pyk/prelude/bytes.py
+++ pyk/src/pyk/prelude/bytes.py
@@ -1,9 +1,18 @@
 from typing import Final
 
+from ..dequote import dequote_str, enquote_str
 from ..kast.inner import KSort, KToken
 
 BYTES: Final = KSort('Bytes')
 
 
-def bytesToken(s: str) -> KToken:  # noqa: N802
-    return KToken('b"' + s + '"', BYTES)
+def bytesToken(pretty: str) -> KToken:  # noqa: N802
+    return KToken(f'b"{enquote_str(pretty)}"', BYTES)
+
+
+def pretty_bytes(token: KToken) -> str:
+    if token.sort != BYTES:
+        raise ValueError(f'Expected Bytes token, got: {token}')
+    assert token.token[0:2] == 'b"'
+    assert token.token[-1] == '"'
+    return dequote_str(token.token[2:-1])
