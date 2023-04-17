70c193e5e4e944e15fbb7e1cc7b20514413bc5db
Tamás Tóth <tothtamas28@users.noreply.github.com>
2023-04-17 13:42:36 +0200

    Add optional parameter `target_dir` to `compile_runtime` (https://github.com/runtimeverification/pyk/pull/363)
    Makes it possible to output the binding to folders other than the
    kompiled directory.
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- pyk/src/pyk/kllvm/compiler.py
+++ pyk/src/pyk/kllvm/compiler.py
@@ -32,7 +32,7 @@ def compile_kllvm(target_dir: str | Path, *, verbose: bool = False) -> Path:
 
     args = ['llvm-kompile', 'pythonast', '--python', sys.executable, '--python-output-dir', str(target_dir)]
     if verbose:
-        args.append('--verbose')
+        args += ['--verbose']
 
     _LOGGER.info(f'Compiling pythonast extension: {module_file.name}')
     run_process(args, logger=_LOGGER)
@@ -49,21 +49,27 @@ RUNTIME_MODULE_NAME: Final = '_kllvm_runtime'
 RUNTIME_MODULE_FILE_NAME: Final = f'{RUNTIME_MODULE_NAME}{PYTHON_EXTENSION_SUFFIX}'
 
 
-def compile_runtime(kompiled_dir: str | Path, *, verbose: bool = False) -> Path:
-    kompiled_dir = Path(kompiled_dir).resolve()
-    check_dir_path(kompiled_dir)
+def compile_runtime(definition_dir: str | Path, target_dir: str | Path | None = None, *, verbose: bool = False) -> Path:
+    definition_dir = Path(definition_dir).resolve()
+    check_dir_path(definition_dir)
 
-    defn_file = kompiled_dir / 'definition.kore'
+    if target_dir is not None:
+        target_dir = Path(target_dir).resolve()
+        check_dir_path(target_dir)
+
+    defn_file = definition_dir / 'definition.kore'
     check_file_path(defn_file)
 
-    dt_dir = kompiled_dir / 'dt'
+    dt_dir = definition_dir / 'dt'
     check_dir_path(dt_dir)
 
-    module_file = kompiled_dir / RUNTIME_MODULE_FILE_NAME
+    module_file = definition_dir / RUNTIME_MODULE_FILE_NAME
 
     args = ['llvm-kompile', str(defn_file), str(dt_dir), 'python', '--python', sys.executable]
+    if target_dir:
+        args += ['--python-output-dir', str(target_dir)]
     if verbose:
-        args.append('--verbose')
+        args += ['--verbose']
 
     _LOGGER.info(f'Compiling python extension: {module_file.name}')
     run_process(args, logger=_LOGGER)
