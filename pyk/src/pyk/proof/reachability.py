c5788cf0156aeccc6883b8acbe184224be3bcd6b
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2023-04-17 16:43:16 -0600

    AGBMCProver: implement basic bounded-model-checker prover (https://github.com/runtimeverification/pyk/pull/347)
    Part of: https://github.com/runtimeverification/pyk/issues/310
    ~Blocked on: https://github.com/runtimeverification/pyk/pull/362~
    
    Most Ethereum verification tools implement Bounded Model Checking, which
    is where they unroll each loop a fixed number of times. This improves
    their stability to code changes because they don't need to provide loop
    invariants.
    
    This PR introduces bounded model checking as a proof technique to the
    pyk library:
    
    - Class `AGBMCProof` is introduced, which is an `AGProof`, but it stores
    also a list of states that are bounded due to reaching loop-unroll
    bound.
    - Class `AGBMCProver` is introduced, which is an `AGProver`, but every
    iteration of calling the `AGProver` it also checks if any of the new
    nodes have reached the depth bound. It accepts the as well a `same_loop`
    predicate which allows identifying loop heads for BMC purposes.
    - For IMP, the `_is_terminal` check is extended to also consider `<k>
    SOME_VAR </k>` as terminal, os that the backend doesn't start inventing
    program by narrowing on that case.
    - A test of the BMC prover is added which is not true in the unbounded
    case, but is true with bounded loop unrolling.
    - A test of the BMC prover is added which fails in the code following
    the unrolled loop.
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>
    Co-authored-by: rv-jenkins <admin@runtimeverification.com>

--- pyk/src/pyk/proof/reachability.py
+++ pyk/src/pyk/proof/reachability.py
@@ -68,6 +68,73 @@ class AGProof(Proof):
         ]
 
 
+class AGBMCProof(AGProof):
+    bmc_depth: int
+    _bounded_states: list[str]
+
+    def __init__(
+        self,
+        id: str,
+        kcfg: KCFG,
+        bmc_depth: int,
+        bounded_states: Iterable[str] | None = None,
+        proof_dir: Path | None = None,
+    ):
+        super().__init__(id, kcfg, proof_dir=proof_dir)
+        self.bmc_depth = bmc_depth
+        self._bounded_states = list(bounded_states) if bounded_states is not None else []
+
+    @staticmethod
+    def read_proof(id: str, proof_dir: Path) -> AGBMCProof:
+        proof_path = proof_dir / f'{hash_str(id)}.json'
+        if AGBMCProof.proof_exists(id, proof_dir):
+            proof_dict = json.loads(proof_path.read_text())
+            _LOGGER.info(f'Reading AGBMCProof from file {id}: {proof_path}')
+            return AGBMCProof.from_dict(proof_dict, proof_dir=proof_dir)
+        raise ValueError(f'Could not load AGBMCProof from file {id}: {proof_path}')
+
+    @property
+    def status(self) -> ProofStatus:
+        if any(nd.id not in self._bounded_states for nd in self.kcfg.stuck):
+            return ProofStatus.FAILED
+        elif len(self.kcfg.frontier) > 0:
+            return ProofStatus.PENDING
+        else:
+            return ProofStatus.PASSED
+
+    @classmethod
+    def from_dict(cls: type[AGBMCProof], dct: Mapping[str, Any], proof_dir: Path | None = None) -> AGBMCProof:
+        cfg = KCFG.from_dict(dct['cfg'])
+        id = dct['id']
+        bounded_states = dct['bounded_states']
+        bmc_depth = dct['bmc_depth']
+        return AGBMCProof(id, cfg, bmc_depth, bounded_states=bounded_states, proof_dir=proof_dir)
+
+    @property
+    def dict(self) -> dict[str, Any]:
+        return {
+            'type': 'AGBMCProof',
+            'id': self.id,
+            'cfg': self.kcfg.to_dict(),
+            'bmc_depth': self.bmc_depth,
+            'bounded_states': self._bounded_states,
+        }
+
+    def bound_state(self, nid: str) -> None:
+        self._bounded_states.append(nid)
+
+    @property
+    def summary(self) -> Iterable[str]:
+        return [
+            f'AGBMCProof(depth={self.bmc_depth}): {self.id}',
+            f'    status: {self.status}',
+            f'    nodes: {len(self.kcfg.nodes)}',
+            f'    frontier: {len(self.kcfg.frontier)}',
+            f'    stuck: {len([nd for nd in self.kcfg.stuck if nd.id not in self._bounded_states])}',
+            f'    bmc-depth-bounded: {len(self._bounded_states)}',
+        ]
+
+
 class AGProver:
     proof: AGProof
     _is_terminal: Callable[[CTerm], bool] | None
@@ -133,6 +200,66 @@ class AGProver:
                 execute_depth=execute_depth,
                 cut_point_rules=cut_point_rules,
                 terminal_rules=terminal_rules,
+            )
+
+        self.proof.write_proof()
+        return self.proof.kcfg
+
+
+class AGBMCProver(AGProver):
+    proof: AGBMCProof
+    _same_loop: Callable[[CTerm, CTerm], bool]
+    _checked_nodes: list[str]
+
+    def __init__(
+        self,
+        proof: AGBMCProof,
+        same_loop: Callable[[CTerm, CTerm], bool],
+        is_terminal: Callable[[CTerm], bool] | None = None,
+        extract_branches: Callable[[CTerm], Iterable[KInner]] | None = None,
+    ) -> None:
+        super().__init__(proof, is_terminal=is_terminal, extract_branches=extract_branches)
+        self._same_loop = same_loop
+        self._checked_nodes = []
+
+    def advance_proof(
+        self,
+        kcfg_explore: KCFGExplore,
+        max_iterations: int | None = None,
+        execute_depth: int | None = None,
+        cut_point_rules: Iterable[str] = (),
+        terminal_rules: Iterable[str] = (),
+        implication_every_block: bool = True,
+    ) -> KCFG:
+        iterations = 0
+
+        while self.proof.kcfg.frontier:
+            self.proof.write_proof()
+
+            if max_iterations is not None and max_iterations <= iterations:
+                _LOGGER.warning(f'Reached iteration bound {self.proof.id}: {max_iterations}')
+                break
+            iterations += 1
+
+            for f in self.proof.kcfg.frontier:
+                if f.id not in self._checked_nodes:
+                    self._checked_nodes.append(f.id)
+                    prior_loops = [
+                        nd.id
+                        for nd in self.proof.kcfg.reachable_nodes(f.id, reverse=True, traverse_covers=True)
+                        if nd.id != f.id and self._same_loop(nd.cterm, f.cterm)
+                    ]
+                    if len(prior_loops) >= self.proof.bmc_depth:
+                        self.proof.kcfg.add_expanded(f.id)
+                        self.proof.bound_state(f.id)
+
+            super().advance_proof(
+                kcfg_explore,
+                max_iterations=1,
+                execute_depth=execute_depth,
+                cut_point_rules=cut_point_rules,
+                terminal_rules=terminal_rules,
+                implication_every_block=implication_every_block,
             )
 
         self.proof.write_proof()
