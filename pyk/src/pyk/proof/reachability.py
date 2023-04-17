e17bf3d474fef689292c9773e0089cfcc5e4651b
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2023-04-17 09:25:36 -0600

    Overhaul prover/kcfg-explore architecture (https://github.com/runtimeverification/pyk/pull/362)
    Fixes: https://github.com/runtimeverification/pyk/issues/361
    
    Factored out of: https://github.com/runtimeverification/pyk/pull/347/
    
    Changes this induces on KEVM:
    https://github.com/runtimeverification/evm-semantics/pull/1733
    
    Refactors to the prover architecture:
    
    - Adds two new routines to `KCFGExplore`, which are semantics
    independent:
    - `KCFGExplore.target_subsume(kcfg, node)`: will attempt subsuming the
    given node into the target.
    - `KCFGExplore.extend(kcfg, node, ...)`: will do a chunk of symbolic
    execution from the given node, inserting either a (i) basic block, (ii)
    stuck marker, (iii) cut-rule, or (iv) branch, into the graph.
    - Changes `KCFGExplore`:
    - Accept the exploration id at constructor time, instead of for each
    individual routine.
    - `KCFGExplore.cterm_execute` no longer checks for bottom branches from
    the backend.
    - All methods in `KCFGExplore` no longer return a `KCFG`, just modify
    the passed in one in-place.
    - More informative `Proof.status` and `AGProof.status` output.
    - Refactor to `AGProver`:
    - Accept semantics specific heuristics as constructor arguments
    (`is_terminal` and `extract_branches`).
      - `AGProver.read_proof` returns an `AGProof`, not a `Proof`.
      - Refactor `AGProver.advance_proof` to use the new routines:
    - First `advance_proof` will check if the given node is subsumed into
    the target using `KCFGExplore.target_subsume`
        - Then it will check if it's a terminal (thus stuck) node.
    - Then it will use the user-supplied heuristic `extract_branches` to see
    if we can fast-extract branches.
    - Then it will call `KCFGExplore.extend` to extend the KCFG with
    symbolic execution.
    - Adjusts two tests that need one more iteration of `AGProver` to
    complete now, because subsumption check does not happen immediately on
    basic block insertion (but still happens before branch insertion).
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- pyk/src/pyk/proof/reachability.py
+++ pyk/src/pyk/proof/reachability.py
@@ -5,7 +5,6 @@ import logging
 from typing import TYPE_CHECKING
 
 from ..kcfg import KCFG
-from ..prelude.ml import mlAnd
 from ..utils import hash_str, shorten_hashes
 from .proof import Proof, ProofStatus
 
@@ -31,7 +30,7 @@ class AGProof(Proof):
         self.kcfg = kcfg
 
     @staticmethod
-    def read_proof(id: str, proof_dir: Path) -> Proof:
+    def read_proof(id: str, proof_dir: Path) -> AGProof:
         proof_path = proof_dir / f'{hash_str(id)}.json'
         if AGProof.proof_exists(id, proof_dir):
             proof_dict = json.loads(proof_path.read_text())
@@ -62,6 +61,7 @@ class AGProof(Proof):
     def summary(self) -> Iterable[str]:
         return [
             f'AGProof: {self.id}',
+            f'    status: {self.status}',
             f'    nodes: {len(self.kcfg.nodes)}',
             f'    frontier: {len(self.kcfg.frontier)}',
             f'    stuck: {len(self.kcfg.stuck)}',
@@ -70,35 +70,23 @@ class AGProof(Proof):
 
 class AGProver:
     proof: AGProof
+    _is_terminal: Callable[[CTerm], bool] | None
+    _extract_branches: Callable[[CTerm], Iterable[KInner]] | None
 
-    def __init__(self, proof: AGProof) -> None:
-        self.proof = proof
-
-    def _check_subsumption(
+    def __init__(
         self,
-        kcfg_explore: KCFGExplore,
-        curr_node: KCFG.Node,
+        proof: AGProof,
         is_terminal: Callable[[CTerm], bool] | None = None,
-        implication_every_block: bool = True,
-    ) -> bool:
-        if implication_every_block or (is_terminal is not None and is_terminal(curr_node.cterm)):
-            target_node = self.proof.kcfg.get_unique_target()
-            _LOGGER.info(
-                f'Checking subsumption into target state {self.proof.id}: {shorten_hashes((curr_node.id, target_node.id))}'
-            )
-            csubst = kcfg_explore.cterm_implies(curr_node.cterm, target_node.cterm)
-            if csubst is not None:
-                self.proof.kcfg.create_cover(curr_node.id, target_node.id, csubst=csubst)
-                _LOGGER.info(
-                    f'Subsumed into target node {self.proof.id}: {shorten_hashes((curr_node.id, target_node.id))}'
-                )
-                return True
-        return False
+        extract_branches: Callable[[CTerm], Iterable[KInner]] | None = None,
+    ) -> None:
+        self.proof = proof
+        self._is_terminal = is_terminal
+        self._extract_branches = extract_branches
 
-    def _check_terminal(self, curr_node: KCFG.Node, is_terminal: Callable[[CTerm], bool] | None = None) -> bool:
-        if is_terminal is not None:
+    def _check_terminal(self, curr_node: KCFG.Node) -> bool:
+        if self._is_terminal is not None:
             _LOGGER.info(f'Checking terminal {self.proof.id}: {shorten_hashes(curr_node.id)}')
-            if is_terminal(curr_node.cterm):
+            if self._is_terminal(curr_node.cterm):
                 _LOGGER.info(f'Terminal node {self.proof.id}: {shorten_hashes(curr_node.id)}.')
                 self.proof.kcfg.add_expanded(curr_node.id)
                 return True
@@ -107,8 +95,6 @@ class AGProver:
     def advance_proof(
         self,
         kcfg_explore: KCFGExplore,
-        is_terminal: Callable[[CTerm], bool] | None = None,
-        extract_branches: Callable[[CTerm], Iterable[KInner]] | None = None,
         max_iterations: int | None = None,
         execute_depth: int | None = None,
         cut_point_rules: Iterable[str] = (),
@@ -126,59 +112,28 @@ class AGProver:
             iterations += 1
             curr_node = self.proof.kcfg.frontier[0]
 
-            if self._check_subsumption(
-                kcfg_explore, curr_node, is_terminal=is_terminal, implication_every_block=implication_every_block
-            ):
+            if kcfg_explore.target_subsume(self.proof.kcfg, curr_node):
                 continue
 
-            if self._check_terminal(curr_node, is_terminal=is_terminal):
+            if self._check_terminal(curr_node):
                 continue
 
-            self.proof.kcfg.add_expanded(curr_node.id)
-
-            _LOGGER.info(f'Advancing proof from node {self.proof.id}: {shorten_hashes(curr_node.id)}')
-            depth, cterm, next_cterms = kcfg_explore.cterm_execute(
-                curr_node.cterm, depth=execute_depth, cut_point_rules=cut_point_rules, terminal_rules=terminal_rules
-            )
-
-            # Cut Rule
-            if depth == 0 and len(next_cterms) == 1:
-                new_execute_depth = execute_depth - 1 if execute_depth is not None else None
-                depth, cterm, next_cterms = kcfg_explore.cterm_execute(
-                    next_cterms[0],
-                    depth=new_execute_depth,
-                    cut_point_rules=cut_point_rules,
-                    terminal_rules=terminal_rules,
-                )
-                depth = depth + 1
-
-            if depth > 0:
-                next_node = self.proof.kcfg.get_or_create_node(cterm)
-                self.proof.kcfg.create_edge(curr_node.id, next_node.id, depth)
-                _LOGGER.info(
-                    f'Found basic block at depth {depth} for {self.proof.id}: {shorten_hashes((curr_node.id, next_node.id))}.'
-                )
-                curr_node = next_node
-
-                if self._check_subsumption(
-                    kcfg_explore, curr_node, is_terminal=is_terminal, implication_every_block=implication_every_block
-                ):
+            if self._extract_branches is not None:
+                branches = list(self._extract_branches(curr_node.cterm))
+                if len(branches) > 0:
+                    self.proof.kcfg.split_on_constraints(curr_node.id, branches)
+                    _LOGGER.info(
+                        f'Found {len(branches)} branches using heuristic for node {self.proof.id}: {shorten_hashes(curr_node.id)}: {[kcfg_explore.kprint.pretty_print(bc) for bc in branches]}'
+                    )
                     continue
 
-            if len(next_cterms) == 0:
-                _LOGGER.info(f'Found stuck node {self.proof.id}: {shorten_hashes(curr_node.id)}')
-
-            elif len(next_cterms) > 1:
-                branches = list(extract_branches(cterm)) if extract_branches is not None else []
-                if len(branches) != len(next_cterms):
-                    _LOGGER.warning(
-                        f'Falling back to manual branch extraction {self.proof.id}: {shorten_hashes(curr_node.id)}'
-                    )
-                    branches = [mlAnd(c for c in s.constraints if c not in cterm.constraints) for s in next_cterms]
-                _LOGGER.info(
-                    f'Found {len(branches)} branches for node {self.proof.id}: {shorten_hashes(curr_node.id)}: {[kcfg_explore.kprint.pretty_print(bc) for bc in branches]}'
-                )
-                self.proof.kcfg.split_on_constraints(curr_node.id, branches)
+            kcfg_explore.extend(
+                self.proof.kcfg,
+                curr_node,
+                execute_depth=execute_depth,
+                cut_point_rules=cut_point_rules,
+                terminal_rules=terminal_rules,
+            )
 
         self.proof.write_proof()
         return self.proof.kcfg
