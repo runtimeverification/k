8cb001dbcd320d2123b7a0e81ebe01a4adb399ea
Ana Pantilie <ana.pantilie95@gmail.com>
2023-04-20 20:44:22 +0300

    Constructor with total should error

--- kernel/src/main/java/org/kframework/compile/checks/CheckAtt.java
+++ kernel/src/main/java/org/kframework/compile/checks/CheckAtt.java
@@ -48,13 +48,18 @@ public class CheckAtt {
 
     private void check(Production prod) {
         if (!prod.sort().equals(Sorts.KItem())) {
-            Att sortAtt =  m.sortAttributesFor().getOrElse(prod.sort().head(), () -> Att.empty());
-            if (sortAtt.contains(Att.HOOK()) && !sortAtt.get(Att.HOOK()).equals("ARRAY.Array") && !(sortAtt.get(Att.HOOK()).equals("KVAR.KVar") && isSymbolicKast)) {
+            Att sortAtt = m.sortAttributesFor().getOrElse(prod.sort().head(), () -> Att.empty());
+            if (sortAtt.contains(Att.HOOK()) && !sortAtt.get(Att.HOOK()).equals("ARRAY.Array")
+                    && !(sortAtt.get(Att.HOOK()).equals("KVAR.KVar") && isSymbolicKast)) {
                 if (!prod.att().contains(Att.FUNCTION()) && !prod.att().contains(Att.BRACKET()) &&
-                    !prod.att().contains("token") && !prod.att().contains("macro") && !(prod.klabel().isDefined() && macros.contains(prod.klabel().get()))) {
-                    if (!(prod.sort().equals(Sorts.K()) && ((prod.klabel().isDefined() && (prod.klabel().get().name().equals("#EmptyK") || prod.klabel().get().name().equals("#KSequence"))) || prod.isSubsort()))) {
+                        !prod.att().contains("token") && !prod.att().contains("macro")
+                        && !(prod.klabel().isDefined() && macros.contains(prod.klabel().get()))) {
+                    if (!(prod.sort().equals(Sorts.K())
+                            && ((prod.klabel().isDefined() && (prod.klabel().get().name().equals("#EmptyK")
+                                    || prod.klabel().get().name().equals("#KSequence"))) || prod.isSubsort()))) {
                         if (!(sortAtt.contains("cellCollection") && prod.isSubsort())) {
-                            errors.add(KEMException.compilerError("Cannot add new constructors to hooked sort " + prod.sort(), prod));
+                            errors.add(KEMException
+                                    .compilerError("Cannot add new constructors to hooked sort " + prod.sort(), prod));
                         }
                     }
                 }
@@ -62,19 +67,22 @@ public class CheckAtt {
         }
         if (prod.att().contains("binder") && !isSymbolicKast) {
             if (!prod.att().get("binder").equals("")) {
-                errors.add(KEMException.compilerError("Attribute value for 'binder' attribute is not supported.", prod));
+                errors.add(
+                        KEMException.compilerError("Attribute value for 'binder' attribute is not supported.", prod));
             }
             if (prod.nonterminals().size() < 2) {
                 errors.add(KEMException.compilerError("Binder productions must have at least two nonterminals.", prod));
-            } else if (!m.sortAttributesFor().get(prod.nonterminals().apply(0).sort().head()).getOrElse(() -> Att.empty()).getOptional(Att.HOOK()).orElse("").equals("KVAR.KVar")) {
-                errors.add(KEMException.compilerError("First child of binder must have a sort with the 'KVAR.KVar' hook attribute.", prod));
+            } else if (!m.sortAttributesFor().get(prod.nonterminals().apply(0).sort().head())
+                    .getOrElse(() -> Att.empty()).getOptional(Att.HOOK()).orElse("").equals("KVAR.KVar")) {
+                errors.add(KEMException.compilerError(
+                        "First child of binder must have a sort with the 'KVAR.KVar' hook attribute.", prod));
             }
         }
         boolean hasColors = false;
         int ncolors = 0;
         if (prod.att().contains("colors")) {
-          hasColors = true;
-          ncolors = prod.att().get("colors").split(",").length;
+            hasColors = true;
+            ncolors = prod.att().get("colors").split(",").length;
         }
         int nterminals = prod.items().size() - prod.nonterminals().size();
         int nescapes = 0;
@@ -84,78 +92,93 @@ public class CheckAtt {
                 char c = format.charAt(i);
                 if (c == '%') {
                     if (i == format.length() - 1) {
-                        errors.add(KEMException.compilerError("Invalid format attribute: unfinished escape sequence.", prod));
+                        errors.add(KEMException.compilerError("Invalid format attribute: unfinished escape sequence.",
+                                prod));
                         break;
                     }
                     char c2 = format.charAt(i + 1);
                     i++;
-                    switch(c2) {
-                      case 'n':
-                      case 'i':
-                      case 'd':
-                      case 'r':
-                        break;
-                      case 'c':
-                        nescapes++;
-                        break;
-                      case '0':
-                      case '1':
-                      case '2':
-                      case '3':
-                      case '4':
-                      case '5':
-                      case '6':
-                      case '7':
-                      case '8':
-                      case '9':
-                        StringBuilder sb = new StringBuilder();
-                        for (; i < format.length() && format.charAt(i) >= '0' && format.charAt(i) <= '9'; i++) {
-                            sb.append(format.charAt(i));
-                        }
-                        i--;
-                        int idx = Integer.parseInt(sb.toString());
-                        if (idx == 0 || idx > prod.items().size()) {
-                            errors.add(KEMException.compilerError("Invalid format escape sequence '%" + sb.toString() + "'. Expected a number between 1 and " + prod.items().size(), prod));
-                        } else {
-                            ProductionItem pi = prod.items().apply(idx-1);
-                            if (pi instanceof RegexTerminal) {
-                                errors.add(KEMException.compilerError("Invalid format escape sequence referring to regular expression terminal '" + pi + "'.", prod));
+                    switch (c2) {
+                        case 'n':
+                        case 'i':
+                        case 'd':
+                        case 'r':
+                            break;
+                        case 'c':
+                            nescapes++;
+                            break;
+                        case '0':
+                        case '1':
+                        case '2':
+                        case '3':
+                        case '4':
+                        case '5':
+                        case '6':
+                        case '7':
+                        case '8':
+                        case '9':
+                            StringBuilder sb = new StringBuilder();
+                            for (; i < format.length() && format.charAt(i) >= '0' && format.charAt(i) <= '9'; i++) {
+                                sb.append(format.charAt(i));
                             }
-                        }
-                        break;
+                            i--;
+                            int idx = Integer.parseInt(sb.toString());
+                            if (idx == 0 || idx > prod.items().size()) {
+                                errors.add(
+                                        KEMException.compilerError(
+                                                "Invalid format escape sequence '%" + sb.toString()
+                                                        + "'. Expected a number between 1 and " + prod.items().size(),
+                                                prod));
+                            } else {
+                                ProductionItem pi = prod.items().apply(idx - 1);
+                                if (pi instanceof RegexTerminal) {
+                                    errors.add(KEMException.compilerError(
+                                            "Invalid format escape sequence referring to regular expression terminal '"
+                                                    + pi + "'.",
+                                            prod));
+                                }
+                            }
+                            break;
                     }
                 }
             }
-        } else if (!prod.att().contains("token") && !prod.sort().equals(Sorts.Layout()) && !prod.sort().equals(Sorts.LineMarker())) {
+        } else if (!prod.att().contains("token") && !prod.sort().equals(Sorts.Layout())
+                && !prod.sort().equals(Sorts.LineMarker())) {
             for (ProductionItem pi : iterable(prod.items())) {
                 if (pi instanceof RegexTerminal) {
-                    if (prod.items().size() == 1)  {
+                    if (prod.items().size() == 1) {
                         errors.add(KEMException.compilerError(
-                              "Expected format attribute on production with regular expression terminal. Did you forget the 'token' attribute?", prod));
+                                "Expected format attribute on production with regular expression terminal. Did you forget the 'token' attribute?",
+                                prod));
                     } else {
                         errors.add(KEMException.compilerError(
-                              "Expected format attribute on production with regular expression terminal.", prod));
+                                "Expected format attribute on production with regular expression terminal.", prod));
                     }
                 }
             }
         }
         if (hasColors && nescapes + nterminals != ncolors) {
-            errors.add(KEMException.compilerError("Invalid colors attribute: expected " + (nescapes + nterminals) + " colors, found " + ncolors + " colors instead.", prod));
+            errors.add(KEMException.compilerError("Invalid colors attribute: expected " + (nescapes + nterminals)
+                    + " colors, found " + ncolors + " colors instead.", prod));
         }
         if (prod.att().contains(Att.FUNCTIONAL())) {
             kem.registerCompilerWarning(ExceptionType.FUTURE_ERROR, errors,
-                "The attribute 'functional' has been deprecated on symbols. Use the combination of attributes 'function' and 'total' instead.", prod);
+                    "The attribute 'functional' has been deprecated on symbols. Use the combination of attributes 'function' and 'total' instead.",
+                    prod);
         }
         if (prod.att().contains(Att.TOTAL()) && !prod.att().contains(Att.FUNCTION())) {
-            kem.registerCompilerWarning(ExceptionType.IGNORED_ATTRIBUTE, errors,
-                "The attribute 'total' was applied to a production which does not have the 'function' attribute; the attribute was ignored.", prod);
+            errors.add(KEMException.compilerError(
+                    "The attribute 'total' was applied to a production which does not have the 'function' attribute; the attribute was ignored.",
+                    prod));
         }
     }
 
     private void check(Rule rule) {
         if (rule.isMacro()) {
             kem.registerCompilerWarning(ExceptionType.FUTURE_ERROR, errors,
-                    "The attribute [" + rule.att().getMacro().get() + "] has been deprecated on rules. Use this label on syntax declarations instead.", rule);
+                    "The attribute [" + rule.att().getMacro().get()
+                            + "] has been deprecated on rules. Use this label on syntax declarations instead.",
+                    rule);
         }
 
         checkNonExecutable(rule);
@@ -164,10 +187,10 @@ public class CheckAtt {
     private void checkNonExecutable(Rule rule) {
         boolean isNonExecutable = rule.att().contains(Att.NON_EXECUTABLE());
         boolean isFunction = m.attributesFor()
-                                .getOrElse(m.matchKLabel(rule), Att::empty)
-                                .contains(Att.FUNCTION());
+                .getOrElse(m.matchKLabel(rule), Att::empty)
+                .contains(Att.FUNCTION());
 
-        if(isNonExecutable && !isFunction) {
+        if (isNonExecutable && !isFunction) {
             errors.add(
                     KEMException.compilerError(
                             "non-executable attribute is only supported on function rules.",
@@ -177,15 +200,15 @@ public class CheckAtt {
 
     private void check(Att att, HasLocation loc) {
         if (att.contains(Att.OWISE()) && att.contains(Att.SIMPLIFICATION())) {
-          errors.add(KEMException.compilerError("owise attribute is not supported on simplification rules.", loc));
+            errors.add(KEMException.compilerError("owise attribute is not supported on simplification rules.", loc));
         }
         if (att.contains(Att.PRIORITY()) && att.contains(Att.SIMPLIFICATION())) {
-          errors.add(KEMException.compilerError("priority attribute is not supported on simplification rules.", loc));
+            errors.add(KEMException.compilerError("priority attribute is not supported on simplification rules.", loc));
         }
-        if(att.contains(Att.ANYWHERE()) && att.contains(Att.SIMPLIFICATION())) {
-          errors.add(KEMException.compilerError("anywhere attribute is not supported on simplification rules.", loc));
+        if (att.contains(Att.ANYWHERE()) && att.contains(Att.SIMPLIFICATION())) {
+            errors.add(KEMException.compilerError("anywhere attribute is not supported on simplification rules.", loc));
         }
-        if(att.contains(Att.ANYWHERE()) && att.contains(Att.SYMBOLIC())) {
+        if (att.contains(Att.ANYWHERE()) && att.contains(Att.SYMBOLIC())) {
             errors.add(KEMException.compilerError("anywhere attribute is not supported on symbolic rules.", loc));
         }
     }
