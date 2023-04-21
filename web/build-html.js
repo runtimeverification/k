4620a7300d3af8d27c22efb19824d4f31a3f56eb
Yiyi Wang <101185369+yiyi-wang-rv@users.noreply.github.com>
2023-04-22 00:27:34 +0800

    Display line numbers for all code blocks (#3349)
    * doc: Display line numbers for all code blocks
    
    * Reverted all markdown files changes
    
    * fix: Fixed the markdown file content
    
    * feat: Updated k-web-theme to the latest

--- web/build-html.js
+++ web/build-html.js
@@ -35,6 +35,7 @@ generatePagesFromMarkdownFiles({
     TOC: tocHTML,
   },
   displayCodeBlockSelectors: true,
+  displayCodeBlockLineNumbers: true,
 });
 generatePagesFromMarkdownFiles({
   globPattern: path.resolve(__dirname, "./pages/") + "/**/*.md",
@@ -50,6 +51,7 @@ generatePagesFromMarkdownFiles({
     TOC: tocHTML,
   },
   displayCodeBlockSelectors: true,
+  displayCodeBlockLineNumbers: true,
 });
 generatePagesFromMarkdownFiles({
   globPattern: path.resolve(__dirname, "../docs/") + "/*.md",
@@ -65,6 +67,7 @@ generatePagesFromMarkdownFiles({
     TOC: tocHTML,
   },
   displayCodeBlockSelectors: true,
+  displayCodeBlockLineNumbers: true,
 });
 
 fs.copyFileSync(
