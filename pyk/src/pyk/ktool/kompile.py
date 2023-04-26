c952e778500d52480ecc8bf0d5a3afcadcc475c9
Tamás Tóth <tothtamas28@users.noreply.github.com>
2023-04-26 21:03:20 +0200

    Rework the `ktool.kompile` module (https://github.com/runtimeverification/pyk/pull/397)
    * Use `**kwargs` in `kompile`
    * Factor out type declarations and logic into class hierarchy `Kompile`
    * Remove `llvm_kompile` and `haskell_kompile` (if a strict interface is
    needed classes `LLVMKompile` and `HaskellKompile` can be used)
    * Use the new classes to simplify integration test utilities
    
    The goal is to make adding new arguments easier. For each new flag, the
    following should be added to exactly one of `KompileArgs`,
    `LLVMKompile`, `HaskellKompile`:
    
    * A new attribute declaration
    * Logic to `__init__` to validate and set the attribute
    * Logic to `args` to convert the attribute value to command line
    arguments
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- pyk/src/pyk/ktool/kompile.py
+++ pyk/src/pyk/ktool/kompile.py
@@ -1,27 +1,158 @@
 from __future__ import annotations
 
-__all__ = ['KompileBackend', 'kompile', 'llvm_kompile', 'haskell_kompile']
+__all__ = ['KompileBackend', 'kompile']
 
+import dataclasses
 import logging
 import shlex
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
 from enum import Enum
+from functools import cached_property
 from pathlib import Path
 from subprocess import CalledProcessError
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, final
 
 from ..cli_utils import abs_or_rel_to, check_dir_path, check_file_path, run_process
 
 if TYPE_CHECKING:
-    from collections.abc import Iterable
-    from typing import Final
+    from collections.abc import Iterable, Mapping
+    from typing import Any, Final, Literal
 
 _LOGGER: Final = logging.getLogger(__name__)
+
+
+def kompile(
+    main_file: str | Path,
+    *,
+    command: Iterable[str] = ('kompile',),
+    output_dir: str | Path | None = None,
+    cwd: Path | None = None,
+    check: bool = True,
+    **kwargs: Any,
+) -> Path:
+    kwargs['main_file'] = main_file
+    kompiler = Kompile.from_dict(kwargs)
+    return kompiler(
+        command=command,
+        output_dir=output_dir,
+        cwd=cwd,
+        check=check,
+    )
 
 
 class KompileBackend(Enum):
     LLVM = 'llvm'
     HASKELL = 'haskell'
     KORE = 'kore'
+
+    @cached_property
+    def args(self) -> frozenset[str]:
+        match self:
+            case KompileBackend.LLVM:
+                return frozenset(field.name for field in dataclasses.fields(LLVMKompile) if field.name != 'base_args')
+            case KompileBackend.HASKELL:
+                return frozenset(
+                    field.name for field in dataclasses.fields(HaskellKompile) if field.name != 'base_args'
+                )
+            case _:
+                raise ValueError(f'Method not supported for backend: {self.value}')
+
+
+class Kompile(ABC):
+    base_args: KompileArgs
+
+    @staticmethod
+    def from_dict(dct: Mapping[str, Any]) -> Kompile:
+        backend = KompileBackend(dct.get('backend') or 'llvm')
+
+        common_args: dict[str, Any] = {}
+        backend_args: dict[str, Any] = {}
+        for key, value in dct.items():
+            if key == 'backend':
+                continue
+            elif key in COMMON_ARGS:
+                common_args[key] = value
+            elif key in backend.args:
+                backend_args[key] = value
+            else:
+                raise ValueError(f'Unexpected argument for backend: {backend.value}: {key}={value!r}')
+
+        base_args = KompileArgs(**common_args)
+        match backend:
+            case KompileBackend.HASKELL:
+                return HaskellKompile(base_args, **backend_args)
+            case KompileBackend.LLVM:
+                return LLVMKompile(base_args, **backend_args)
+            case _:
+                raise ValueError(f'Unsupported backend: {backend.value}')
+
+    @property
+    @abstractmethod
+    def backend(self) -> KompileBackend:
+        ...
+
+    def __call__(
+        self,
+        command: Iterable[str] | None = None,
+        *,
+        output_dir: str | Path | None = None,
+        cwd: Path | None = None,
+        check: bool = True,
+    ) -> Path:
+        check_file_path(abs_or_rel_to(self.base_args.main_file, cwd or Path()))
+        for include_dir in self.base_args.include_dirs:
+            check_dir_path(abs_or_rel_to(include_dir, cwd or Path()))
+
+        command = list(command) if command is not None else ['kompile']
+        args = command + self.args()
+
+        if output_dir is not None:
+            output_dir = Path(output_dir)
+            args += ['--output-definition', str(output_dir)]
+
+        try:
+            run_process(args, logger=_LOGGER, cwd=cwd, check=check)
+        except CalledProcessError as err:
+            raise RuntimeError(
+                f'Command kompile exited with code {err.returncode} for: {self.base_args.main_file}',
+                err.stdout,
+                err.stderr,
+                err.returncode,
+            ) from err
+
+        definition_dir = output_dir if output_dir else Path(self.base_args.main_file.stem + '-kompiled')
+        assert definition_dir.is_dir()
+        return definition_dir
+
+    @abstractmethod
+    def args(self) -> list[str]:
+        ...
+
+
+@final
+@dataclass(frozen=True)
+class HaskellKompile(Kompile):
+    base_args: KompileArgs
+    concrete_rules: tuple[str, ...]
+
+    def __init__(self, base_args: KompileArgs, *, concrete_rules: Iterable[str] = ()):
+        concrete_rules = tuple(concrete_rules)
+        object.__setattr__(self, 'base_args', base_args)
+        object.__setattr__(self, 'concrete_rules', concrete_rules)
+
+    @property
+    def backend(self) -> Literal[KompileBackend.HASKELL]:
+        return KompileBackend.HASKELL
+
+    def args(self) -> list[str]:
+        args = self.base_args.args()
+        args += ['--backend', 'haskell']
+
+        if self.concrete_rules:
+            args += ['--concrete-rules', ','.join(self.concrete_rules)]
+
+        return args
 
 
 class LLVMKompileType(Enum):
@@ -33,295 +164,165 @@ class LLVMKompileType(Enum):
     C = 'c'
 
 
-def kompile(
-    main_file: str | Path,
-    *,
-    command: Iterable[str] = ('kompile',),
-    output_dir: str | Path | None = None,
-    backend: str | KompileBackend | None = None,
-    main_module: str | None = None,
-    syntax_module: str | None = None,
-    include_dirs: Iterable[str | Path] = (),
-    md_selector: str | None = None,
-    hook_namespaces: Iterable[str] = (),
-    emit_json: bool = True,
-    gen_bison_parser: bool = False,
-    bison_parser_library: bool = False,
-    debug: bool = False,
-    post_process: str | None = None,
-    read_only: bool = False,
-    # LLVM backend
-    llvm_kompile_type: LLVMKompileType | None = None,
-    llvm_kompile_output: str | None = None,
-    opt_level: int | None = None,
-    ccopts: Iterable[str] = (),
-    no_llvm_kompile: bool = False,
-    enable_search: bool = False,
-    enable_llvm_debug: bool = False,
-    # Haskell backend
-    concrete_rules: Iterable[str] = (),
-    # ---
-    cwd: Path | None = None,
-    check: bool = True,
-) -> Path:
-    main_file = Path(main_file)
-    check_file_path(abs_or_rel_to(main_file, cwd or Path()))
+@final
+@dataclass(frozen=True)
+class LLVMKompile(Kompile):
+    base_args: KompileArgs
+    llvm_kompile_type: LLVMKompileType
+    llvm_kompile_output: str | None
+    opt_level: int
+    ccopts: tuple[str, ...]
+    no_llvm_kompile: bool
+    enable_search: bool
+    enable_llvm_debug: bool
 
-    _include_dirs = [Path(include_dir) for include_dir in include_dirs]
-    for include_dir in _include_dirs:
-        check_dir_path(abs_or_rel_to(include_dir, cwd or Path()))
+    def __init__(
+        self,
+        base_args: KompileArgs,
+        *,
+        llvm_kompile_type: str | LLVMKompileType | None = None,
+        llvm_kompile_output: str | None = None,
+        opt_level: int | None = None,
+        ccopts: Iterable[str] = (),
+        no_llvm_kompile: bool = False,
+        enable_search: bool = False,
+        enable_llvm_debug: bool = False,
+    ):
+        llvm_kompile_type = LLVMKompileType(llvm_kompile_type) if llvm_kompile_type is not None else None
 
-    backend = KompileBackend(backend) if backend is not None else None
+        opt_level = opt_level or 0
+        if not (0 <= opt_level <= 3):
+            raise ValueError('Invalid optimization level: {opt_level}')
 
-    if backend and backend != KompileBackend.LLVM:
-        _check_backend_param(llvm_kompile_type is None, 'llvm_kompile_type', backend)
-        _check_backend_param(llvm_kompile_output is None, 'llvm_kompile_output', backend)
-        _check_backend_param(opt_level is None, 'opt_level', backend)
-        _check_backend_param(not list(ccopts), 'ccopts', backend)
-        _check_backend_param(not no_llvm_kompile, 'no_llvm_kompile', backend)
-        _check_backend_param(not enable_search, 'enable_search', backend)
-        _check_backend_param(not enable_llvm_debug, 'enable_llvm_debug', backend)
+        ccopts = tuple(ccopts)
 
-    if backend != KompileBackend.HASKELL:
-        _check_backend_param(not list(concrete_rules), 'concrete_rules', backend)
+        object.__setattr__(self, 'base_args', base_args)
+        object.__setattr__(self, 'llvm_kompile_type', llvm_kompile_type)
+        object.__setattr__(self, 'llvm_kompile_output', llvm_kompile_output)
+        object.__setattr__(self, 'opt_level', opt_level)
+        object.__setattr__(self, 'ccopts', ccopts)
+        object.__setattr__(self, 'no_llvm_kompile', no_llvm_kompile)
+        object.__setattr__(self, 'enable_search', enable_search)
+        object.__setattr__(self, 'enable_llvm_debug', enable_llvm_debug)
 
-    if opt_level and not (0 <= opt_level <= 3):
-        raise ValueError('Invalid optimization level: {opt_level}')
+    @property
+    def backend(self) -> Literal[KompileBackend.LLVM]:
+        return KompileBackend.LLVM
 
-    output_dir = Path(output_dir) if output_dir is not None else None
+    def args(self) -> list[str]:
+        args = self.base_args.args()
+        args += ['--backend', 'llvm']
 
-    args = _build_arg_list(
-        command=command,
-        main_file=main_file,
-        output_dir=output_dir,
-        backend=backend,
-        main_module=main_module,
-        syntax_module=syntax_module,
-        include_dirs=_include_dirs,
-        md_selector=md_selector,
-        hook_namespaces=hook_namespaces,
-        emit_json=emit_json,
-        gen_bison_parser=gen_bison_parser,
-        bison_parser_library=bison_parser_library,
-        debug=debug,
-        post_process=post_process,
-        read_only=read_only,
-        llvm_kompile_type=llvm_kompile_type,
-        llvm_kompile_output=llvm_kompile_output,
-        enable_search=enable_search,
-        enable_llvm_debug=enable_llvm_debug,
-        opt_level=opt_level,
-        ccopts=ccopts,
-        no_llvm_kompile=no_llvm_kompile,
-        concrete_rules=concrete_rules,
-    )
+        if self.llvm_kompile_type:
+            args += ['--llvm-kompile-type', self.llvm_kompile_type.value]
 
-    try:
-        run_process(args, logger=_LOGGER, cwd=cwd, check=check)
-    except CalledProcessError as err:
-        raise RuntimeError(
-            f'Command kompile exited with code {err.returncode} for: {main_file}',
-            err.stdout,
-            err.stderr,
-            err.returncode,
-        ) from err
+        if self.llvm_kompile_output is not None:
+            args += ['--llvm-kompile-output', self.llvm_kompile_output]
 
-    kompiled_dir = output_dir if output_dir else Path(main_file.stem + '-kompiled')
-    assert kompiled_dir.is_dir()
-    return kompiled_dir
+        if self.opt_level:
+            args += [f'-O{self.opt_level}']
+
+        for ccopt in self.ccopts:
+            args += ['-ccopt', ccopt]
+
+        if self.no_llvm_kompile:
+            args += ['--no-llvm-kompile']
+
+        if self.enable_search:
+            args += ['--enable-search']
+
+        if self.enable_llvm_debug:
+            args += ['--enable-llvm-debug']
+
+        return args
 
 
-def llvm_kompile(
-    main_file: str | Path,
-    *,
-    command: Iterable[str] = ('kompile',),
-    output_dir: str | Path | None = None,
-    main_module: str | None = None,
-    syntax_module: str | None = None,
-    include_dirs: Iterable[str | Path] = (),
-    md_selector: str | None = None,
-    hook_namespaces: Iterable[str] = (),
-    emit_json: bool = True,
-    gen_bison_parser: bool = False,
-    bison_parser_library: bool = False,
-    debug: bool = False,
-    post_process: str | None = None,
-    read_only: bool = False,
-    llvm_kompile_type: LLVMKompileType | None = None,
-    opt_level: int | None = None,
-    ccopts: Iterable[str] = (),
-    no_llvm_kompile: bool = False,
-    enable_search: bool = False,
-    enable_llvm_debug: bool = False,
-    # ---
-    cwd: Path | None = None,
-    check: bool = True,
-) -> Path:
-    return kompile(
-        main_file=main_file,
-        backend=KompileBackend.LLVM,
-        command=command,
-        output_dir=output_dir,
-        main_module=main_module,
-        syntax_module=syntax_module,
-        include_dirs=include_dirs,
-        md_selector=md_selector,
-        hook_namespaces=hook_namespaces,
-        emit_json=emit_json,
-        gen_bison_parser=gen_bison_parser,
-        bison_parser_library=bison_parser_library,
-        debug=debug,
-        post_process=post_process,
-        read_only=read_only,
-        opt_level=opt_level,
-        ccopts=ccopts,
-        no_llvm_kompile=no_llvm_kompile,
-        enable_search=enable_search,
-        enable_llvm_debug=enable_llvm_debug,
-        llvm_kompile_type=llvm_kompile_type,
-        cwd=cwd,
-        check=check,
-    )
+@final
+@dataclass(frozen=True)
+class KompileArgs:
+    main_file: Path
+    main_module: str | None
+    syntax_module: str | None
+    include_dirs: tuple[Path, ...]
+    md_selector: str | None
+    hook_namespaces: tuple[str, ...]
+    emit_json: bool
+    gen_bison_parser: bool
+    bison_parser_library: bool
+    debug: bool
+    post_process: str | None
+    read_only: bool
+
+    def __init__(
+        self,
+        main_file: str | Path,
+        *,
+        main_module: str | None = None,
+        syntax_module: str | None = None,
+        include_dirs: Iterable[str | Path] = (),
+        md_selector: str | None = None,
+        hook_namespaces: Iterable[str] = (),
+        emit_json: bool = True,
+        gen_bison_parser: bool = False,
+        bison_parser_library: bool = False,
+        debug: bool = False,
+        post_process: str | None = None,
+        read_only: bool = False,
+    ):
+        main_file = Path(main_file)
+        include_dirs = tuple(sorted(Path(include_dir) for include_dir in include_dirs))
+        hook_namespaces = tuple(hook_namespaces)
+
+        object.__setattr__(self, 'main_file', main_file)
+        object.__setattr__(self, 'main_module', main_module)
+        object.__setattr__(self, 'syntax_module', syntax_module)
+        object.__setattr__(self, 'include_dirs', include_dirs)
+        object.__setattr__(self, 'md_selector', md_selector)
+        object.__setattr__(self, 'hook_namespaces', hook_namespaces)
+        object.__setattr__(self, 'emit_json', emit_json)
+        object.__setattr__(self, 'gen_bison_parser', gen_bison_parser)
+        object.__setattr__(self, 'bison_parser_library', bison_parser_library)
+        object.__setattr__(self, 'debug', debug)
+        object.__setattr__(self, 'post_process', post_process)
+        object.__setattr__(self, 'read_only', read_only)
+
+    def args(self) -> list[str]:
+        args = [str(self.main_file)]
+
+        if self.main_module:
+            args += ['--main-module', self.main_module]
+
+        if self.syntax_module:
+            args += ['--syntax-module', self.syntax_module]
+
+        for include_dir in self.include_dirs:
+            args += ['-I', str(include_dir)]
+
+        if self.md_selector:
+            args += ['--md-selector', self.md_selector]
+
+        if self.hook_namespaces:
+            args += ['--hook-namespaces', ' '.join(self.hook_namespaces)]
+
+        if self.emit_json:
+            args += ['--emit-json']
+
+        if self.gen_bison_parser:
+            args += ['--gen-bison-parser']
+
+        if self.bison_parser_library:
+            args += ['--bison-parser-library']
+
+        if self.debug:
+            args += ['--debug']
+
+        if self.post_process:
+            args += ['--post-process', shlex.quote(self.post_process)]
+
+        if self.read_only:
+            args += ['--read-only-kompiled-directory']
+
+        return args
 
 
-def haskell_kompile(
-    main_file: str | Path,
-    *,
-    command: Iterable[str] = ('kompile',),
-    output_dir: str | Path | None = None,
-    backend: KompileBackend | None = None,
-    main_module: str | None = None,
-    syntax_module: str | None = None,
-    include_dirs: Iterable[str | Path] = (),
-    md_selector: str | None = None,
-    hook_namespaces: Iterable[str] = (),
-    emit_json: bool = True,
-    gen_bison_parser: bool = False,
-    bison_parser_library: bool = False,
-    debug: bool = False,
-    post_process: str | None = None,
-    read_only: bool = False,
-    concrete_rules: Iterable[str] = (),
-    # ---
-    cwd: Path | None = None,
-    check: bool = True,
-) -> Path:
-    return kompile(
-        main_file=main_file,
-        backend=KompileBackend.HASKELL,
-        command=command,
-        output_dir=output_dir,
-        main_module=main_module,
-        syntax_module=syntax_module,
-        include_dirs=include_dirs,
-        md_selector=md_selector,
-        hook_namespaces=hook_namespaces,
-        emit_json=emit_json,
-        gen_bison_parser=gen_bison_parser,
-        bison_parser_library=bison_parser_library,
-        debug=debug,
-        post_process=post_process,
-        read_only=read_only,
-        concrete_rules=concrete_rules,
-        cwd=cwd,
-        check=check,
-    )
-
-
-def _check_backend_param(check: bool, param_name: str, backend: KompileBackend | None) -> None:
-    if not check:
-        raise ValueError(f'Parameter not supported by backend {backend}: {param_name}')
-
-
-def _build_arg_list(
-    *,
-    command: Iterable[str],
-    main_file: Path,
-    output_dir: Path | None,
-    backend: KompileBackend | None,
-    main_module: str | None,
-    syntax_module: str | None,
-    include_dirs: Iterable[Path],
-    md_selector: str | None,
-    hook_namespaces: Iterable[str],
-    emit_json: bool,
-    gen_bison_parser: bool,
-    bison_parser_library: bool,
-    debug: bool = False,
-    post_process: str | None,
-    read_only: bool = False,
-    llvm_kompile_type: LLVMKompileType | None = None,
-    llvm_kompile_output: str | None = None,
-    opt_level: int | None,
-    ccopts: Iterable[str],
-    no_llvm_kompile: bool,
-    enable_search: bool,
-    enable_llvm_debug: bool = False,
-    concrete_rules: Iterable[str],
-) -> list[str]:
-    args = list(command) + [str(main_file)]
-
-    if output_dir:
-        args += ['--output-definition', str(output_dir)]
-
-    if backend:
-        args += ['--backend', backend.value]
-
-    if main_module:
-        args.extend(['--main-module', main_module])
-
-    if syntax_module:
-        args.extend(['--syntax-module', syntax_module])
-
-    for include_dir in include_dirs:
-        args += ['-I', str(include_dir)]
-
-    if md_selector:
-        args.extend(['--md-selector', md_selector])
-
-    if hook_namespaces:
-        args.extend(['--hook-namespaces', ' '.join(hook_namespaces)])
-
-    if emit_json:
-        args.append('--emit-json')
-
-    if gen_bison_parser:
-        args.append('--gen-bison-parser')
-
-    if bison_parser_library:
-        args.append('--bison-parser-library')
-
-    if debug:
-        args.append('--debug')
-
-    if post_process:
-        args.extend(['--post-process', shlex.quote(post_process)])
-
-    if read_only:
-        args.append('--read-only-kompiled-directory')
-
-    if llvm_kompile_type is not None:
-        args.extend(['--llvm-kompile-type', llvm_kompile_type.value])
-
-    if llvm_kompile_output is not None:
-        args.extend(['--llvm-kompile-output', llvm_kompile_output])
-
-    if opt_level:
-        args.append(f'-O{opt_level}')
-
-    for ccopt in ccopts:
-        args += ['-ccopt', ccopt]
-
-    if no_llvm_kompile:
-        args.append('--no-llvm-kompile')
-
-    if enable_search:
-        args.append('--enable-search')
-
-    if enable_llvm_debug:
-        args.append('--enable-llvm-debug')
-
-    if concrete_rules:
-        args.extend(['--concrete-rules', ','.join(concrete_rules)])
-
-    return args
+COMMON_ARGS: Final = frozenset(field.name for field in dataclasses.fields(KompileArgs))
