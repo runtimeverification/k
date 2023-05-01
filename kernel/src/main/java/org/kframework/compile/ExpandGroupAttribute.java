9ff685264a00e7793982aa47be75e631325afdec
Scott Guest <scott.guest@runtimeverification.com>
2023-05-01 02:20:40 -0400

    Added missing ExpandGroupAttribute.java file

---
+++ kernel/src/main/java/org/kframework/compile/ExpandGroupAttribute.java
@@ -0,0 +1,47 @@
+package org.kframework.compile;
+
+import org.kframework.attributes.Att;
+import org.kframework.attributes.HasLocation;
+import org.kframework.kil.Definition;
+import org.kframework.kil.Module;
+import org.kframework.kil.Syntax;
+import org.kframework.utils.errorsystem.KEMException;
+
+/**
+ * Replace every attribute [group(att1,...,attN)] with the underlying attributes [att1,...,attN]
+ */
+public class ExpandGroupAttribute {
+
+    private static Att expandGroup(Att att, HasLocation node) {
+        if (!att.contains("group")) {
+            return att;
+        }
+        String groups = att.get("group").trim();
+        KEMException badCommaException =
+                KEMException.compilerError("Extraneous ',' in group(_) attribute.", node);
+        if (groups.startsWith(",") || groups.endsWith(",")) {
+            throw badCommaException;
+        }
+        for (String group : groups.split("\\s*,\\s*")) {
+            if (group.isEmpty()) {
+                throw badCommaException;
+            }
+            if (!group.matches("[a-z][a-zA-Z0-9-]*")) {
+                throw KEMException.compilerError("Invalid argument '" + group + "' in group(_) attribute. " +
+                        "Expected a lower case letter followed by any number of alphanumeric or '-' characters.", node);
+            }
+            att = att.addGroup(group);
+        }
+        return att.remove("group");
+    }
+
+    public static void apply(Definition d) {
+        d.getItems().stream()
+                .filter((item) -> item instanceof Module)
+                .flatMap((mod) -> ((Module) mod).getItems().stream())
+                .filter((modItem) -> modItem instanceof Syntax)
+                .flatMap((s) -> ((Syntax) s).getPriorityBlocks().stream())
+                .flatMap((pb) -> pb.getProductions().stream())
+                .forEach((p) -> p.setAttributes(expandGroup(p.getAttributes(), p)));
+    }
+}
