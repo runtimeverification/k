747621c454b97c1ff0d7161df36a6d69d7fe346e
Dwight Guth <dwight.guth@runtimeverification.com>
2023-05-03 13:42:25 -0500

    make field in KoreBackend protected instead of private (#3387)

--- kernel/src/main/java/org/kframework/backend/kore/KoreBackend.java
+++ kernel/src/main/java/org/kframework/backend/kore/KoreBackend.java
@@ -36,7 +36,7 @@ public class KoreBackend extends AbstractBackend {
     protected final FileUtil files;
     private final KExceptionManager kem;
     private final EnumSet<ResolveHeatCoolAttribute.Mode> heatCoolConditions;
-    private final boolean heatCoolEquations;
+    protected final boolean heatCoolEquations;
     private final Tool tool;
 
     @Inject
