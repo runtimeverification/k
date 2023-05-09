a8697558a93613296a15a4f802ca02dfe38252a9
Tamás Tóth <tothtamas28@users.noreply.github.com>
2023-05-09 11:43:47 +0200

    Update `Bytes` encoding (#412)
    * Restrict `Bytes` token syntax (in accordance with
    https://github.com/runtimeverification/k/issues/3396)
    * Add utilities to convert Python bytes values to KAST `Bytes` tokens
    and KORE `SortBytes` domain values
    * Add unit and integration tests
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

---
+++ pyk/src/tests/integration/test_bytes.py
@@ -0,0 +1,225 @@
+from __future__ import annotations
+
+import json
+from typing import TYPE_CHECKING
+
+import pytest
+
+from pyk.kast import KInner, kast_term
+from pyk.kllvm.compiler import compile_runtime
+from pyk.kllvm.importer import import_runtime
+from pyk.konvert import _kast_to_kore, _kore_to_kast
+from pyk.kore.parser import KoreParser
+from pyk.kore.prelude import BYTES, SORT_K_ITEM, bytes_dv, generated_counter, generated_top, inj, int_dv, k, kseq
+from pyk.kore.rpc import KoreClient, KoreServer, StuckResult
+from pyk.kore.syntax import App
+from pyk.ktool.kprint import _kast, pretty_print_kast
+from pyk.ktool.krun import KRun
+from pyk.prelude.bytes import bytesToken
+
+if TYPE_CHECKING:
+    from pathlib import Path
+    from types import ModuleType
+    from typing import Final
+
+    from pytest import FixtureRequest
+
+    from pyk.kore.syntax import Pattern
+
+    from .utils import Kompiler
+
+TEST_DATA: Final = (
+    b'hello',
+    b'\"' b'\\' b'\f',
+    b'\r',
+    b'\n',
+    b'\t',
+    b'\x00',
+    b'\x01',
+    b'\x80',
+    b'\xc2\x80',
+)
+
+
+KOMPILE_MAIN_FILE = 'k-files/bytes-rewrite.k'
+KOMPILE_MAIN_MODULE = 'BYTES-REWRITE'
+
+
+@pytest.fixture(scope='module')
+def llvm_dir(kompile: Kompiler) -> Path:
+    return kompile(
+        KOMPILE_MAIN_FILE,
+        main_module=KOMPILE_MAIN_MODULE,
+        backend='llvm',
+    )
+
+
+@pytest.fixture(scope='module')
+def haskell_dir(kompile: Kompiler) -> Path:
+    return kompile(
+        KOMPILE_MAIN_FILE,
+        main_module=KOMPILE_MAIN_MODULE,
+        backend='haskell',
+    )
+
+
+@pytest.fixture(scope='module', params=['llvm', 'haskell'])
+def backend(request: FixtureRequest) -> str:
+    return request.param
+
+
+@pytest.fixture(scope='module')
+def definition_dir(request: FixtureRequest, backend: str) -> Path:
+    return request.getfixturevalue(f'{backend}_dir')
+
+
+@pytest.fixture(scope='module')
+def runtime(llvm_dir: Path) -> ModuleType:
+    import pyk.kllvm.load  # noqa: F401
+
+    compile_runtime(llvm_dir)
+    return import_runtime(llvm_dir)
+
+
+def kore_config(kval: bytes | None, bval: bytes) -> Pattern:
+    def b(pattern: Pattern) -> App:
+        return App("Lbl'-LT-'b'-GT-'", (), (pattern,))
+
+    kitems = (inj(BYTES, SORT_K_ITEM, bytes_dv(kval)),) if kval is not None else ()
+    return generated_top(
+        (
+            k(kseq(kitems)),
+            b(bytes_dv(bval)),
+            generated_counter(int_dv(0)),
+        )
+    )
+
+
+@pytest.mark.parametrize('value', TEST_DATA)
+def test_kast_to_kore(value: bytes) -> None:  # TODO turn into unit test
+    # Given
+    kast = bytesToken(value)
+
+    # When
+    kore = _kast_to_kore(kast)
+
+    # Then
+    assert kore == bytes_dv(value)
+
+
+@pytest.mark.parametrize('value', TEST_DATA)
+def test_kore_to_kast(value: bytes) -> None:  # TODO turn into unit test
+    # Given
+    kore = bytes_dv(value)
+
+    # When
+    kast = _kore_to_kast(kore)
+
+    # Then
+    assert kast == bytesToken(value)
+
+
+@pytest.mark.parametrize('value', TEST_DATA)
+def test_cli_kast_to_kore(llvm_dir: Path, value: bytes) -> None:
+    # Given
+    kast = bytesToken(value)
+    kast_dict = {'format': 'KAST', 'version': 2, 'term': kast.to_dict()}  # TODO extract function
+    kast_json = json.dumps(kast_dict)
+
+    # When
+    proc_res = _kast(
+        definition_dir=llvm_dir,
+        expression=kast_json,
+        input='json',
+        output='kore',
+    )
+    kore_text = proc_res.stdout
+    kore = KoreParser(kore_text).dv()
+
+    # Then
+    assert kore == bytes_dv(value)
+
+
+@pytest.mark.parametrize('value', TEST_DATA)
+def test_cli_kore_to_kast(llvm_dir: Path, value: bytes) -> None:
+    # Given
+    kore = bytes_dv(value)
+    kore_text = kore.text
+
+    # When
+    proc_res = _kast(
+        definition_dir=llvm_dir,
+        expression=kore_text,
+        input='kore',
+        output='json',
+    )
+    kast_json = proc_res.stdout
+    kast = kast_term(json.loads(kast_json), KInner)  # type: ignore
+
+    # Then
+    assert kast == bytesToken(value)
+
+
+@pytest.mark.parametrize('value', TEST_DATA)
+def test_cli_rule_to_kast(llvm_dir: Path, value: bytes) -> None:
+    # Given
+    input_kast = bytesToken(value)
+    rule_text = pretty_print_kast(input_kast, {})
+
+    # When
+    proc_res = _kast(
+        definition_dir=llvm_dir,
+        expression=rule_text,
+        input='rule',
+        output='json',
+    )
+    kast_json = proc_res.stdout
+    output_kast = kast_term(json.loads(kast_json), KInner)  # type: ignore
+
+    # Then
+    assert input_kast == output_kast
+
+
+@pytest.mark.parametrize('value', TEST_DATA)
+def test_krun(backend: str, definition_dir: Path, value: bytes) -> None:
+    # Given
+    kore = kore_config(value, b'')
+    expected = kore_config(None, value)
+    krun = KRun(definition_dir)
+
+    # When
+    actual = krun.run_kore_term(kore)
+
+    # Then
+    assert actual == expected
+
+
+@pytest.mark.parametrize('value', TEST_DATA)
+def test_bindings(runtime: ModuleType, value: bytes) -> None:
+    from pyk.kllvm.convert import kore_to_llvm, llvm_to_kore
+
+    # Given
+    kore = kore_config(value, b'')
+    expected = kore_config(None, value)
+
+    # When
+    kore_llvm = runtime.interpret(kore_to_llvm(kore))
+    actual = llvm_to_kore(kore_llvm)
+
+    # Then
+    assert actual == expected
+
+
+@pytest.mark.parametrize('value', TEST_DATA)
+def test_kore_rpc(haskell_dir: Path, value: bytes) -> None:
+    # Given
+    kore = kore_config(value, b'')
+    expected = kore_config(None, value)
+
+    # When
+    with KoreServer(haskell_dir, KOMPILE_MAIN_MODULE) as server:
+        with KoreClient('localhost', server.port) as client:
+            result = client.execute(kore)
+
+    assert isinstance(result, StuckResult)
+    assert result.state.term == expected
