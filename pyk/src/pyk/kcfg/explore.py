5c07a6a2422819460d70faaec87fff6aa5dac466
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2023-05-31 07:43:29 -0600

    Cleanups and small bugfixes for EqualityProofs (https://github.com/runtimeverification/pyk/pull/469)
    Fixes: https://github.com/runtimeverification/pyk/issues/426
    
    - Adds a new method `KDefinition.sort_strict` which returns just a
    `KSort` or fails with `ValueError` (as a wrapper to `KDefinition.sort`
    which can erturn a `None`.
    - Adds a new method `KCGExplore.kast_simplify` (as counterpart to
    `KCFGExplore.cterm_simplify`), which allows passing in arbitrary kast
    terms.
    - Adds a new method `EqualityProof.constraint` which already calls
    `mlAnd` on `EqualityProof.constraints` for you.
    - Has `EqualityProof.from_claim` be simplified a bit to use
    `KDefinition.sort_strict` instead of the much weaker
    `KDefinition.return_sort` which only works for `KApply`.
    - Re-arranges the `EqualityProof.advance_proof` to use
    `KCFGExplore.kast_simplify` and `EqualityProof.constraint`, and
    simplifies its control flow a bit, improves logging messages.
    - Fixes tests in `imp-simple-spec.k` which had `False` instead of
    `false` (`False` is parsed as a variable of sort `Bool` by K, whereas
    `false` is the actual domain value we want).
    - Enables a test that was failing because of this typo.
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- pyk/src/pyk/kcfg/explore.py
+++ pyk/src/pyk/kcfg/explore.py
@@ -161,6 +161,14 @@ class KCFGExplore(ContextManager['KCFGExplore']):
         kast_simplified = self.kprint.kore_to_kast(kore_simplified)
         return kast_simplified, logs
 
+    def kast_simplify(self, kast: KInner) -> tuple[KInner, tuple[LogEntry, ...]]:
+        _LOGGER.debug(f'Simplifying: {kast}')
+        kore = self.kprint.kast_to_kore(kast, GENERATED_TOP_CELL)
+        _, kore_client = self._kore_rpc
+        kore_simplified, logs = kore_client.simplify(kore)
+        kast_simplified = self.kprint.kore_to_kast(kore_simplified)
+        return kast_simplified, logs
+
     def cterm_implies(
         self,
         antecedent: CTerm,
