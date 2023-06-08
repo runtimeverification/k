534e3608c831995bdb39fd1773bb34942cc4ccfb
Noah Watson <107630091+nwatson22@users.noreply.github.com>
2023-06-08 14:03:52 -0500

    Port proof methods and associated tests from #394 (#485)
    This PR takes some methods for the `Proof` abstract class from #394 into
    a separate pull request.
    
    Adds:
    - `Proof.digest()`
    - `Proof.up_to_date(check_method)`
    - `Proof.read_proof(id, proof_dir)`, Generic way to read a proof from a
    file, regardless of type
    - `hash_file(file)` to compute the hash of a file
    
    A test for `read_proof` with test cases for each of the 3 existing proof
    types
    
    This test should currently be failing due to #484.
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>
    Co-authored-by: Everett Hildenbrandt <everett.hildenbrandt@gmail.com>

--- pyk/src/pyk/proof/proof.py
+++ pyk/src/pyk/proof/proof.py
@@ -6,7 +6,7 @@ from abc import ABC, abstractmethod
 from enum import Enum
 from typing import TYPE_CHECKING
 
-from ..utils import hash_str
+from ..utils import hash_file, hash_str
 
 if TYPE_CHECKING:
     from collections.abc import Iterable, Mapping
@@ -25,6 +25,8 @@ class ProofStatus(Enum):
 
 
 class Proof(ABC):
+    _PROOF_TYPES: Final = {'APRProof', 'APRBMCProof', 'EqualityProof'}
+
     id: str
     proof_dir: Path | None
 
@@ -36,13 +38,37 @@ class Proof(ABC):
         if not self.proof_dir:
             return
         proof_path = self.proof_dir / f'{hash_str(self.id)}.json'
-        proof_path.write_text(json.dumps(self.dict))
-        _LOGGER.info(f'Updated proof file {self.id}: {proof_path}')
+        if not self.up_to_date():
+            proof_json = json.dumps(self.dict)
+            proof_path.write_text(proof_json)
+            self._last_modified = proof_path.stat().st_mtime_ns
+            _LOGGER.info(f'Updated proof file {self.id}: {proof_path}')
 
     @staticmethod
     def proof_exists(id: str, proof_dir: Path) -> bool:
         proof_path = proof_dir / f'{hash_str(id)}.json'
         return proof_path.exists() and proof_path.is_file()
+
+    @property
+    def digest(self) -> str:
+        return hash_str(json.dumps(self.dict))
+
+    def up_to_date(self, check_method: str = 'checksum') -> bool:
+        """
+        Check that the proof's representation on disk is up-to-date.
+        By default, compares the file timestamp to self._last_modified. Use check_method = 'checksum' to compare hashes instead.
+        """
+        if self.proof_dir is None:
+            raise ValueError(f'Cannot check if proof {self.id} with no proof_dir is up-to-date')
+        proof_path = self.proof_dir / f'{hash_str(id)}.json'
+        if proof_path.exists() and proof_path.is_file():
+            match check_method:
+                case 'checksum':
+                    return self.digest == hash_file(proof_path)
+                case _:  # timestamp
+                    return self._last_modified == proof_path.stat().st_mtime_ns
+        else:
+            return False
 
     @property
     @abstractmethod
@@ -58,6 +84,22 @@ class Proof(ABC):
     @abstractmethod
     def from_dict(cls: type[Proof], dct: Mapping[str, Any]) -> Proof:
         ...
+
+    @classmethod
+    def read_proof(cls: type[Proof], id: str, proof_dir: Path) -> Proof:
+        # these local imports allow us to call .to_dict() based on the proof type we read from JSON
+        from .equality import EqualityProof  # noqa
+        from .reachability import APRBMCProof, APRProof  # noqa
+
+        proof_path = proof_dir / f'{hash_str(id)}.json'
+        if Proof.proof_exists(id, proof_dir):
+            proof_dict = json.loads(proof_path.read_text())
+            proof_type = proof_dict['type']
+            _LOGGER.info(f'Reading {proof_type} from file {id}: {proof_path}')
+            if proof_type in Proof._PROOF_TYPES:
+                return locals()[proof_type].from_dict(proof_dict, proof_dir)
+
+        raise ValueError(f'Could not load Proof from file {id}: {proof_path}')
 
     @property
     def summary(self) -> Iterable[str]:
