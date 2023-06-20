a26e20b51ee4d19cd18a3de4ed27816ccc31a0df
Noah Watson <107630091+nwatson22@users.noreply.github.com>
2023-06-20 09:26:22 -0500

    Subproofs (https://github.com/runtimeverification/pyk/pull/500)
    This PR pulls the Subproof-related components out of #394.
    
    It also adds a default implementation of `dict` for the `Proof` class,
    which includes the proof ID and any subproofs. This can be built on by
    any implementing classes.
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>
    Co-authored-by: Everett Hildenbrandt <everett.hildenbrandt@gmail.com>

--- pyk/src/tests/unit/test_proof.py
+++ pyk/src/tests/unit/test_proof.py
@@ -24,6 +24,35 @@ def proof_dir(tmp_path_factory: TempPathFactory) -> Path:
     return tmp_path_factory.mktemp('proofs')
 
 
+def apr_proof(i: int, proof_dir: Path) -> APRProof:
+    return APRProof(
+        id=f'apr_proof_{i}',
+        init=node(1).id,
+        target=node(1).id,
+        kcfg=KCFG.from_dict({'nodes': node_dicts(i)}),
+        logs={},
+        proof_dir=proof_dir,
+    )
+
+
+def aprbmc_proof(i: int, proof_dir: Path) -> APRBMCProof:
+    return APRBMCProof(
+        id=f'aprbmc_proof_{i}',
+        init=node(1).id,
+        target=node(1).id,
+        bmc_depth=i,
+        kcfg=KCFG.from_dict({'nodes': node_dicts(i)}),
+        logs={},
+        proof_dir=proof_dir,
+    )
+
+
+def equality_proof(i: int, proof_dir: Path) -> EqualityProof:
+    return EqualityProof(
+        id=f'equality_proof_{i}', lhs_body=intToken(i), rhs_body=intToken(i), sort=BOOL, proof_dir=proof_dir
+    )
+
+
 PROOF_TEST_DATA: list[tuple[str, str, Proof]] = [
     (
         'apr-proof',
@@ -80,3 +109,127 @@ class TestProof:
         # Then
         assert type(proof_from_disk) is type(sample_proof)
         assert proof_from_disk.dict == sample_proof.dict
+
+
+#### APRProof
+
+
+def test_apr_proof_from_dict_no_subproofs(proof_dir: Path) -> None:
+    # Given
+    proof = apr_proof(1, proof_dir)
+
+    # When
+    proof.write_proof()
+    assert proof.proof_dir
+    proof_from_disk = Proof.read_proof(proof.id, proof_dir=proof.proof_dir)
+
+    # Then
+    assert proof.dict == proof_from_disk.dict
+
+
+def test_apr_proof_from_dict_one_subproofs(proof_dir: Path) -> None:
+    # Given
+    eq_proof = equality_proof(1, proof_dir)
+    proof = apr_proof(1, proof_dir)
+
+    # When
+    eq_proof.write_proof()
+    proof.add_subproof(eq_proof.id)
+    proof.write_proof()
+    assert proof.proof_dir
+    proof_from_disk = Proof.read_proof(proof.id, proof_dir=proof.proof_dir)
+
+    # Then
+    assert proof.dict == proof_from_disk.dict
+
+
+def test_apr_proof_from_dict_nested_subproofs(proof_dir: Path) -> None:
+    # Given
+    eq_proof = equality_proof(1, proof_dir)
+    subproof = apr_proof(2, proof_dir)
+    proof = apr_proof(1, proof_dir)
+
+    # When
+    eq_proof.write_proof()
+    subproof.add_subproof(eq_proof.id)
+    subproof.write_proof()
+    proof.add_subproof(subproof.id)
+    proof.write_proof()
+    assert proof.proof_dir
+    proof_from_disk = Proof.read_proof(proof.id, proof_dir=proof.proof_dir)
+
+    # Then
+    assert proof.dict == proof_from_disk.dict
+
+
+def test_apr_proof_from_dict_heterogeneous_subproofs(proof_dir: Path) -> None:
+    # Given
+    sub_proof_1 = equality_proof(1, proof_dir)
+    sub_proof_2 = apr_proof(2, proof_dir)
+    sub_proof_3 = aprbmc_proof(3, proof_dir)
+    proof = apr_proof(1, proof_dir)
+
+    # When
+    sub_proof_1.write_proof()
+    sub_proof_2.write_proof()
+    sub_proof_3.write_proof()
+    proof.add_subproof(sub_proof_1.id)
+    proof.add_subproof(sub_proof_2.id)
+    proof.add_subproof(sub_proof_3.id)
+    proof.write_proof()
+    assert proof.proof_dir
+    proof_from_disk = Proof.read_proof(proof.id, proof_dir=proof.proof_dir)
+
+    # Then
+    assert proof.dict == proof_from_disk.dict
+
+
+#### APRBMCProof
+
+
+def test_aprbmc_proof_from_dict_no_subproofs(proof_dir: Path) -> None:
+    # Given
+    proof = aprbmc_proof(1, proof_dir)
+
+    # When
+    proof.write_proof()
+    assert proof.proof_dir
+    proof_from_disk = Proof.read_proof(proof.id, proof_dir=proof.proof_dir)
+
+    # Then
+    assert proof.dict == proof_from_disk.dict
+
+
+def test_aprbmc_proof_from_dict_one_subproofs(proof_dir: Path) -> None:
+    # Given
+    eq_proof = equality_proof(1, proof_dir)
+    proof = aprbmc_proof(1, proof_dir)
+
+    # When
+    eq_proof.write_proof()
+    proof.add_subproof(eq_proof.id)
+    proof.write_proof()
+    assert proof.proof_dir
+    proof_from_disk = Proof.read_proof(proof.id, proof_dir=proof.proof_dir)
+
+    # Then
+    assert proof.dict == proof_from_disk.dict
+
+
+def test_aprbmc_proof_from_dict_heterogeneous_subproofs(proof_dir: Path) -> None:
+    # Given
+    eq_proof = equality_proof(1, proof_dir)
+    subproof = apr_proof(2, proof_dir)
+    proof = aprbmc_proof(1, proof_dir)
+
+    # When
+    eq_proof.write_proof()
+    subproof.add_subproof(eq_proof.id)
+    subproof.write_proof()
+    proof.add_subproof(subproof.id)
+    proof.write_proof()
+    assert proof.proof_dir
+    proof_from_disk = Proof.read_proof(proof.id, proof_dir=proof.proof_dir)
+
+    # Then
+    assert proof.dict == proof_from_disk.dict
