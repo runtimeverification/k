406558021b67dbf5b58cd3425746a7a72117d9b6
Virgil <25692529+virgil-serbanuta@users.noreply.github.com>
2023-06-23 13:28:07 +0300

    Fix LHS and RHS extraction from rules. (https://github.com/runtimeverification/pyk/pull/502)
    When normalizing a rewrite, pyk represents something like "(a => .K) ~>
    stuff" as "(a => #EmptyK) ~> stuff". When extracting the RHS of such a
    rewrite, pyk produces "#EmptyK ~> stuff" which breaks various tools.
    
    This PR replaces "#EmptyK" with an actual empty KSequence (i.e.,
    KSequence([])), which, for the example above, will produce just "stuff"
    as the RHS.
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- pyk/src/pyk/prelude/k.py
+++ pyk/src/pyk/prelude/k.py
@@ -1,10 +1,8 @@
 from typing import Final
 
-from ..kast.inner import KApply, KSort, KToken
+from ..kast.inner import KSort, KToken
 
 K: Final = KSort('K')
 GENERATED_TOP_CELL: Final = KSort('GeneratedTopCell')
 
 DOTS: Final = KToken('...', K)
-
-EMPTY_K: Final = KApply('#EmptyK')
