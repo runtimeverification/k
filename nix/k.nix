750078ee708a556fdbb6d27b3ed1ef0c4f1ca3fd
Roberto Rosmaninho <robertogomes@dcc.ufmg.br>
2023-07-06 13:10:08 -0300

    Initial commit for booster integration

--- nix/k.nix
+++ nix/k.nix
@@ -21,6 +21,7 @@ let
       "-DskipKTest=true"
       "-Dllvm.backend.skip=true"
       "-Dhaskell.backend.skip=true"
+      "-Dhaskell.backend.booster.skip=true"
     ];
     # Attributes are passed to the underlying `stdenv.mkDerivation`, so build
     # hooks can also be set here.
