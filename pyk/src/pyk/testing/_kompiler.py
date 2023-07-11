b70559869cf2a2bae2ca09de25b30bfd8eacecce
Palina Tolmach <polina.tolmach@gmail.com>
2023-07-11 21:26:21 +0800

    Remove unused `TestKoreClientWithSMTLemmas` class (https://github.com/runtimeverification/pyk/pull/530)
    Removed a redundant `TestKoreClientWithSMTLemmas` class definition,
    introduced in https://github.com/runtimeverification/pyk/pull/520 as
    part of adding the test for `chop` processing within the `get-model`
    request.
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- pyk/src/pyk/testing/_kompiler.py
+++ pyk/src/pyk/testing/_kompiler.py
@@ -150,21 +150,6 @@ class KoreClientTest(KompiledTest):
         server.close()
 
 
-class TestKoreClientWithSMTLemmas(KompiledTest):
-    KOMPILE_BACKEND = 'haskell'
-
-    KORE_MODULE_NAME: ClassVar[str]
-    KORE_CLIENT_TIMEOUT: ClassVar = 1000
-
-    @pytest.fixture
-    def kore_client(self, definition_dir: Path) -> Iterator[KoreClient]:
-        server = KoreServer(definition_dir, self.KORE_MODULE_NAME)
-        client = KoreClient('localhost', server.port, timeout=self.KORE_CLIENT_TIMEOUT)
-        yield client
-        client.close()
-        server.close()
-
-
 class RuntimeTest(KompiledTest):
     KOMPILE_BACKEND = 'llvm'
 
