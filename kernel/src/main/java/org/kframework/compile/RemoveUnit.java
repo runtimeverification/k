01eea458cbe5bac6e36e4288c7e578ca3a1d244a
Guy Repta <50716988+gtrepta@users.noreply.github.com>
2023-07-25 12:47:37 -0500

    Better error message

--- kernel/src/main/java/org/kframework/compile/RemoveUnit.java
+++ kernel/src/main/java/org/kframework/compile/RemoveUnit.java
@@ -4,14 +4,16 @@ package org.kframework.compile;
 import org.kframework.Collections;
 import org.kframework.attributes.Att;
 import org.kframework.definition.Module;
+import org.kframework.definition.Production;
 import org.kframework.definition.Rule;
 import org.kframework.definition.Sentence;
 import org.kframework.kore.*;
 import org.kframework.utils.errorsystem.KEMException;
 
 import java.util.HashSet;
-import java.util.Set;
 import java.util.stream.Stream;
+
+import scala.collection.Set;
 
 import static org.kframework.definition.Constructors.*;
 import static org.kframework.kore.KORE.*;
@@ -41,7 +43,15 @@ public class RemoveUnit {
     return new TransformK() {
       @Override
       public K apply(KApply k) {
-        Att att = m.attributesFor().getOrElse(k.klabel(), () -> Att.empty());
+        Production p;
+        if (m.productionsFor().contains(k.klabel())) {
+           Set<Production> s = m.productionsFor().get(k.klabel()).get();
+           assert s.size() == 1; // There should only be one production for this label
+           p = s.head();
+        } else {
+           return super.apply(k);
+        }
+        Att att = p.att();
 
         // Ignore optional cells, which have a unit attribute but no assoc
         if (   att.contains(Att.CELL())
@@ -52,7 +62,7 @@ public class RemoveUnit {
 
         if (att.contains(Att.UNIT())) {
           if (!att.contains(Att.ASSOC())) {
-            throw KEMException.internalError("unimplemented case in RemoveUnit: unit but no assoc");
+            throw KEMException.internalError("Unimplemented case when removing unit applications: unit attribute but no assoc", p);
           }
           return Assoc.flatten(k.klabel(), k.items(), m).stream().reduce((k1, k2) -> KApply(k.klabel(), k1, k2)).orElse(KApply(KLabel(att.get(Att.UNIT()))));
         }
