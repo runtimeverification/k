cbe988af9988e877b8912ebc63b41fb802ad3454
Petar Maksimović <PetarMax@users.noreply.github.com>
2023-07-26 09:45:37 +0100

    Factoring of semantics-specific functionalities (https://github.com/runtimeverification/pyk/pull/546)
    This PR creates a KCFGSemantics class, which holds semantics-specific
    functionalities, specifically:
    
    - the `is_terminal(c)` predicate, which checks whether or not the
    passed-in node is terminal;
    - the `extract_branches(c)` function, which understands whether the
    passed-in node can be split;
    - the `abstract_node(c)` function, which abstracts the passed-in node 
    - the `same_loop(c1, c2)` function, which identifies if the two
    passed-in nodes represent the same loop head;
    - the cut-point rules of the semantics; and
    - the terminal rules of the semantics;
    
    and refactors the code to use this class appropriately.
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- pyk/src/tests/integration/kcfg/test_goto.py
+++ pyk/src/tests/integration/kcfg/test_goto.py
@@ -7,6 +7,7 @@ from typing import TYPE_CHECKING
 import pytest
 
 from pyk.kast.inner import KApply, KSequence, KVariable
+from pyk.kcfg.semantics import KCFGSemantics
 from pyk.kcfg.show import KCFGShow
 from pyk.prelude.ml import mlEqualsTrue
 from pyk.prelude.utils import token
@@ -23,11 +24,40 @@ if TYPE_CHECKING:
 
     from pyk.cterm import CTerm
     from pyk.kast.inner import KInner
+    from pyk.kast.outer import KDefinition
     from pyk.kcfg import KCFGExplore
     from pyk.ktool.kprove import KProve
     from pyk.proof import APRProof
 
 _LOGGER: Final = logging.getLogger(__name__)
+
+
+class GotoSemantics(KCFGSemantics):
+    def is_terminal(self, c: CTerm) -> bool:
+        return False
+
+    def extract_branches(self, c: CTerm) -> list[KInner]:
+        k_cell_pattern = KSequence([KApply('jumpi', [KVariable('JD')])])
+        stack_cell_pattern = KApply('ws', [KVariable('S'), KVariable('SS')])
+        k_cell_match = k_cell_pattern.match(c.cell('K_CELL'))
+        stack_cell_match = stack_cell_pattern.match(c.cell('STACK_CELL'))
+        if k_cell_match is not None and stack_cell_match is not None:
+            return [
+                mlEqualsTrue(KApply('_==Int_', [token(0), stack_cell_match['S']])),
+                mlEqualsTrue(KApply('_=/=Int_', [token(0), stack_cell_match['S']])),
+            ]
+        return []
+
+    def abstract_node(self, c: CTerm) -> CTerm:
+        return c
+
+    def same_loop(self, c1: CTerm, c2: CTerm) -> bool:
+        k_cell = c1.cell('K_CELL')
+        pc_cell_1 = c1.cell('PC_CELL')
+        pc_cell_2 = c2.cell('PC_CELL')
+        if pc_cell_1 == pc_cell_2 and type(k_cell) is KSequence and len(k_cell) > 0 and type(k_cell[0]) is KApply:
+            return k_cell[0].label.name == 'jumpi'
+        return False
 
 
 APRBMC_PROVE_TEST_DATA: Iterable[
@@ -57,31 +87,8 @@ def leaf_number(proof: APRProof) -> int:
 class TestGoToProof(KCFGExploreTest):
     KOMPILE_MAIN_FILE = K_FILES / 'goto.k'
 
-    @staticmethod
-    def _is_terminal(cterm1: CTerm) -> bool:
-        return False
-
-    @staticmethod
-    def _extract_branches(cterm: CTerm) -> list[KInner]:
-        k_cell_pattern = KSequence([KApply('jumpi', [KVariable('JD')])])
-        stack_cell_pattern = KApply('ws', [KVariable('S'), KVariable('SS')])
-        k_cell_match = k_cell_pattern.match(cterm.cell('K_CELL'))
-        stack_cell_match = stack_cell_pattern.match(cterm.cell('STACK_CELL'))
-        if k_cell_match is not None and stack_cell_match is not None:
-            return [
-                mlEqualsTrue(KApply('_==Int_', [token(0), stack_cell_match['S']])),
-                mlEqualsTrue(KApply('_=/=Int_', [token(0), stack_cell_match['S']])),
-            ]
-        return []
-
-    @staticmethod
-    def _same_loop(cterm1: CTerm, cterm2: CTerm) -> bool:
-        k_cell = cterm1.cell('K_CELL')
-        pc_cell_1 = cterm1.cell('PC_CELL')
-        pc_cell_2 = cterm2.cell('PC_CELL')
-        if pc_cell_1 == pc_cell_2 and type(k_cell) is KSequence and len(k_cell) > 0 and type(k_cell[0]) is KApply:
-            return k_cell[0].label.name == 'jumpi'
-        return False
+    def semantics(self, definition: KDefinition) -> KCFGSemantics:
+        return GotoSemantics()
 
     @pytest.mark.parametrize(
         'test_id,spec_file,spec_module,claim_id,max_iterations,max_depth,bmc_depth,terminal_rules,cut_rules,proof_status,expected_leaf_number',
@@ -113,9 +120,6 @@ class TestGoToProof(KCFGExploreTest):
         prover = APRBMCProver(
             proof,
             kcfg_explore=kcfg_explore,
-            same_loop=TestGoToProof._same_loop,
-            is_terminal=TestGoToProof._is_terminal,
-            extract_branches=TestGoToProof._extract_branches,
         )
         prover.advance_proof(
             max_iterations=max_iterations,
