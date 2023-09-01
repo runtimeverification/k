34402a022adbf0efb8554733ce4556cc70624f7a
rv-jenkins <devops@runtimeverification.com>
2023-09-01 10:37:56 +0000

    flake.nix, hs-backend-booster/src/main/native/hs-backend-booster: update to version 393ed4b76d4cbcab9d49c89fc041327882e4f6c1

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     nixpkgs.url = "nixpkgs/nixos-23.05";
     haskell-backend.url = "github:runtimeverification/haskell-backend/1da6c43b8bb74a231b58978b1964a45d7ca753e4";
     booster-backend = {
-      url = "github:runtimeverification/hs-backend-booster/8f06341da63b5e3eabc9d353a4bdfe09b869a7d0";
+      url = "github:runtimeverification/hs-backend-booster/393ed4b76d4cbcab9d49c89fc041327882e4f6c1";
       # NB booster-backend will bring in another dependency on haskell-backend,
       # but the two are not necessarily the same (different more often than not).
       # We get two transitive dependencies on haskell-nix.
