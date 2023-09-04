11699a757d7414d4b5bb99e028a811ad000a2a60
Roberto Rosmaninho <robertogomes@dcc.ufmg.br>
2023-09-04 15:36:42 -0300

    Fixing Bencher version

--- k-distribution/tests/profiling/setup_profiling.sh
+++ k-distribution/tests/profiling/setup_profiling.sh
@@ -3,10 +3,11 @@
 set -euxo pipefail
 
 export DEBIAN_FRONTEND=noninteractive
+export BENCHER_VERSION="0.3.10"
 apt-get update
 apt-get upgrade --yes
 apt-get install --yes make time sudo wget
 apt-get install --yes ./kframework.deb
 
-wget https://github.com/bencherdev/bencher/releases/download/v0.3.9/bencher_0.3.9_amd64.deb
-sudo dpkg -i bencher_0.3.9_amd64.deb
+wget https://github.com/bencherdev/bencher/releases/download/v"${BENCHER_VERSION}"/bencher_"${BENCHER_VERSION}"_amd64.deb
+sudo dpkg -i bencher_"${BENCHER_VERSION}"_amd64.deb
