a46e44633a0287ae643aba220ded40e398839be2
Roberto Rosmaninho <robertogomes@dcc.ufmg.br>
2023-09-15 12:53:44 -0300

    Adding arguments to `kprove` flags

--- kernel/src/main/java/org/kframework/utils/options/OutputDirectoryOptions.java
+++ kernel/src/main/java/org/kframework/utils/options/OutputDirectoryOptions.java
@@ -13,9 +13,13 @@ public class OutputDirectoryOptions implements Serializable {
     @Inject
     public OutputDirectoryOptions(Void v) {}
 
-    @Parameter(names={"--directory", "-d"}, description="[DEPRECATED] Path to the directory in which the output resides. An output can be either a kompiled K definition or a document which depends on the type of backend. The default is the directory containing the main definition file.", descriptionKey = "path", hidden = true)
+    @Parameter(names={"--directory", "-d"},
+            description="[DEPRECATED] Path to the directory in which the output resides. An output can be either a " +
+                    "kompiled K definition or a document which depends on the type of backend. The default is the " +
+                    "directory containing the main definition file.", descriptionKey = "path", hidden = true)
     public String directory;
 
-    @Parameter(names={"--output-definition", "-o"}, description="Path to the exact directory in which the output resides.", descriptionKey = "path")
+    @Parameter(names={"--output-definition", "-o"},
+            description="Path to the exact directory in which the output resides.", descriptionKey = "path")
     public String outputDirectory;
 }
--- kernel/src/main/java/org/kframework/utils/options/SMTOptions.java
+++ kernel/src/main/java/org/kframework/utils/options/SMTOptions.java
@@ -14,7 +14,8 @@ public class SMTOptions implements Serializable {
     @Inject
     public SMTOptions(Void v) {}
 
-    @Parameter(names="--smt", converter=SMTSolverConverter.class, description="SMT solver to use for checking constraints. <executable> is one of [z3|none].", descriptionKey = "executable", hidden = true)
+    @Parameter(names="--smt", descriptionKey = "executable", converter=SMTSolverConverter.class,
+            description="SMT solver to use for checking constraints. <executable> is one of [z3|none].", hidden = true)
     public SMTSolver smt = SMTSolver.Z3;
 
     public static class SMTSolverConverter extends BaseEnumConverter<SMTSolver> {
@@ -29,25 +30,30 @@ public class SMTOptions implements Serializable {
         }
     }
 
-    @Parameter(names="--ignore-missing-smtlib-warning", description="Suppress warning when SMTLib translation fails.", hidden = true)
+    @Parameter(names="--ignore-missing-smtlib-warning",
+            description="Suppress warning when SMTLib translation fails.", hidden = true)
     public boolean ignoreMissingSMTLibWarning = false;
 
-    @Parameter(names="--floats-as-po", description="Abstracts floating-point values as a partial order relation.", hidden = true)
+    @Parameter(names="--floats-as-po",
+            description="Abstracts floating-point values as a partial order relation.", hidden = true)
     public boolean floatsAsPO = false;
 
     @Parameter(names="--maps-as-int-array", description="Abstracts map values as an array of ints.", hidden = true)
     public boolean mapAsIntArray = false;
 
-    @Parameter(names={"--smt-prelude", "--smt_prelude"}, description="Path to the SMT prelude file.", descriptionKey = "path", hidden = true)
+    @Parameter(names={"--smt-prelude", "--smt_prelude"},
+            description="Path to the SMT prelude file.", descriptionKey = "path", hidden = true)
     public String smtPrelude;
 
-    @Parameter(names="--smt-timeout", description="Timeout for calls to the SMT solver, in milliseconds.", hidden = true)
+    @Parameter(names="--smt-timeout", descriptionKey = "millisecond",
+            description="Timeout for calls to the SMT solver, in milliseconds.", hidden = true)
     public Integer smtTimeout = null;
 
     @Parameter(names="--z3-jni", description="Invokes Z3 as JNI library. Default is external process. " +
             "JNI is slightly faster, but can potentially lead to JVM crash.", hidden = true)
     public boolean z3JNI = false;
 
-    @Parameter(names="--z3-tactic", description="The path to solver tactic to use to check satisfiability in Z3.", descriptionKey = "solver", hidden = true)
+    @Parameter(names="--z3-tactic", descriptionKey = "solver",
+            description="The path to solver tactic to use to check satisfiability in Z3.", hidden = true)
     public String z3Tactic;
 }
