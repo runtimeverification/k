cf7ae22ede05c421ada06a8a8e2e6a3152ec325d
rv-jenkins <admin@runtimeverification.com>
2023-10-31 06:03:25 -0600

    Update dependency: deps/hs-backend-booster_release (#3769)
    Co-authored-by: devops <devops@runtimeverification.com>

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
   description = "K Framework";
   inputs = {
-    haskell-backend.url = "github:runtimeverification/haskell-backend/03a6228f78d7f4805fee4b9d9c45208dcbe0c9fb";
+    haskell-backend.url = "github:runtimeverification/haskell-backend/811e94fbcaef8550a6a709a9ce793f7166457911";
     booster-backend = {
-      url = "github:runtimeverification/hs-backend-booster/59bdfa3f5e3ee4d82351d8de44137029c47e23a8";
+      url = "github:runtimeverification/hs-backend-booster/d240f6e954bdbc16572259ac6fec70a66416e2d8";
       inputs.nixpkgs.follows = "haskell-backend/nixpkgs";
       inputs.haskell-backend.follows = "haskell-backend";
       inputs.stacklock2nix.follows = "haskell-backend/stacklock2nix";
