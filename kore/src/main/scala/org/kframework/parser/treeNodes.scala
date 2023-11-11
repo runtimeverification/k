a00435ad8bca51f64d1104cd0eae1c8f33798f10
Scott Guest <scott.guest@runtimeverification.com>
2023-11-10 19:24:06 -0500

    Remove unused strict parameter from ParseInModule.

--- kore/src/main/scala/org/kframework/parser/treeNodes.scala
+++ kore/src/main/scala/org/kframework/parser/treeNodes.scala
@@ -21,6 +21,7 @@ trait Term extends HasLocation {
 trait ProductionReference extends Term {
   val production: Production
   var id: Optional[Integer] = Optional.empty()
+
   def setId(id: Optional[Integer]) {
     this.id = id
   }
@@ -28,11 +29,13 @@ trait ProductionReference extends Term {
 
 trait HasChildren {
   def items: Iterable[Term]
+
   def replaceChildren(newChildren: Collection[Term]): Term
 }
 
 case class Constant private(value: String, production: Production) extends ProductionReference {
   override def toString = "#token(" + production.sort + "," + StringUtil.enquoteKString(value) + ")"
+
   override lazy val hashCode: Int = scala.runtime.ScalaRunTime._hashCode(Constant.this);
 }
 
@@ -40,10 +43,12 @@ case class Constant private(value: String, production: Production) extends Produ
 case class TermCons private(items: PStack[Term], production: Production)
   extends ProductionReference with HasChildren {
   def get(i: Int) = items.get(items.size() - 1 - i)
+
   def `with`(i: Int, e: Term) = TermCons(items.`with`(items.size() - 1 - i, e), production, location, source, id)
 
   def replaceChildren(newChildren: Collection[Term]) = TermCons(ConsPStack.from(newChildren), production, location, source, id)
-  override def toString() = new TreeNodesToKORE(s => Sort(s), false).apply(this).toString()
+
+  override def toString() = new TreeNodesToKORE(s => Sort(s)).apply(this).toString()
 
   override lazy val hashCode: Int = scala.runtime.ScalaRunTime._hashCode(TermCons.this);
 }
@@ -51,12 +56,14 @@ case class TermCons private(items: PStack[Term], production: Production)
 case class Ambiguity(items: Set[Term])
   extends Term with HasChildren {
   def replaceChildren(newChildren: Collection[Term]) = Ambiguity(new HashSet[Term](newChildren), location, source)
+
   override def toString() = "amb(" + (items.asScala mkString ",") + ")"
+
   override lazy val hashCode: Int = scala.runtime.ScalaRunTime._hashCode(Ambiguity.this);
 }
 
 object Constant {
-  def apply(value: String, production: Production, location: Optional[Location], source: Optional[Source]):Constant = {
+  def apply(value: String, production: Production, location: Optional[Location], source: Optional[Source]): Constant = {
     val res = Constant(value, production)
     res.location = location
     res.source = source
@@ -65,7 +72,7 @@ object Constant {
 }
 
 object TermCons {
-  def apply(items: PStack[Term], production: Production, location: Optional[Location], source: Optional[Source], id: Optional[Integer]):TermCons = {
+  def apply(items: PStack[Term], production: Production, location: Optional[Location], source: Optional[Source], id: Optional[Integer]): TermCons = {
     val res = TermCons(items, production)
     res.location = location
     res.source = source
@@ -73,16 +80,17 @@ object TermCons {
     res
   }
 
-  def apply(items: PStack[Term], production: Production, location: Optional[Location], source: Optional[Source]):TermCons = {
+  def apply(items: PStack[Term], production: Production, location: Optional[Location], source: Optional[Source]): TermCons = {
     TermCons(items, production, location, source, Optional.empty())
   }
 
-  def apply(items: PStack[Term], production: Production, location: Location, source: Source):TermCons = TermCons(items, production, Optional.of(location), Optional.of(source), Optional.empty())
+  def apply(items: PStack[Term], production: Production, location: Location, source: Source): TermCons = TermCons(items, production, Optional.of(location), Optional.of(source), Optional.empty())
 }
 
 object Ambiguity {
   @annotation.varargs def apply(items: Term*): Ambiguity = Ambiguity(items.to[mutable.Set].asJava)
-  def apply(items: Set[Term], location: Optional[Location], source: Optional[Source]):Ambiguity = {
+
+  def apply(items: Set[Term], location: Optional[Location], source: Optional[Source]): Ambiguity = {
     val res = Ambiguity(items)
     res.location = location
     res.source = source
