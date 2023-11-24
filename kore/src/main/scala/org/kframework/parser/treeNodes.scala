04812a8b86ada9d63830b8bc570d30537d6ed7e4
Scott Guest <scott.guest@runtimeverification.com>
2023-11-24 16:04:51 -0500

    Apply scalafmt with 100 column width.

--- kore/src/main/scala/org/kframework/parser/treeNodes.scala
+++ kore/src/main/scala/org/kframework/parser/treeNodes.scala
@@ -11,7 +11,7 @@ import org.pcollections.{ConsPStack, PStack}
 import collection.JavaConverters._
 import org.kframework.utils.StringUtil
 
-import scala.collection.mutable;
+import scala.collection.mutable
 
 trait Term extends HasLocation {
   var location: Optional[Location] = Optional.empty()
@@ -33,37 +33,45 @@ trait HasChildren {
   def replaceChildren(newChildren: Collection[Term]): Term
 }
 
-case class Constant private(value: String, production: Production) extends ProductionReference {
+case class Constant private (value: String, production: Production) extends ProductionReference {
   override def toString = "#token(" + production.sort + "," + StringUtil.enquoteKString(value) + ")"
 
   override lazy val hashCode: Int = scala.runtime.ScalaRunTime._hashCode(Constant.this);
 }
 
 // note that items is reversed because it is more efficient to generate it this way during parsing
-case class TermCons private(items: PStack[Term], production: Production)
-  extends ProductionReference with HasChildren {
+case class TermCons private (items: PStack[Term], production: Production)
+    extends ProductionReference
+    with HasChildren {
   def get(i: Int) = items.get(items.size() - 1 - i)
 
-  def `with`(i: Int, e: Term) = TermCons(items.`with`(items.size() - 1 - i, e), production, location, source, id)
+  def `with`(i: Int, e: Term) =
+    TermCons(items.`with`(items.size() - 1 - i, e), production, location, source, id)
 
-  def replaceChildren(newChildren: Collection[Term]) = TermCons(ConsPStack.from(newChildren), production, location, source, id)
+  def replaceChildren(newChildren: Collection[Term]) =
+    TermCons(ConsPStack.from(newChildren), production, location, source, id)
 
   override def toString() = new TreeNodesToKORE(s => Sort(s)).apply(this).toString()
 
   override lazy val hashCode: Int = scala.runtime.ScalaRunTime._hashCode(TermCons.this);
 }
 
-case class Ambiguity(items: Set[Term])
-  extends Term with HasChildren {
-  def replaceChildren(newChildren: Collection[Term]) = Ambiguity(new HashSet[Term](newChildren), location, source)
+case class Ambiguity(items: Set[Term]) extends Term with HasChildren {
+  def replaceChildren(newChildren: java.util.Collection[Term]) =
+    Ambiguity(new java.util.HashSet[Term](newChildren), location, source)
 
-  override def toString() = "amb(" + (items.asScala mkString ",") + ")"
+  override def toString: String = "amb(" + (items.asScala mkString ",") + ")"
 
-  override lazy val hashCode: Int = scala.runtime.ScalaRunTime._hashCode(Ambiguity.this);
+  override lazy val hashCode: Int = scala.runtime.ScalaRunTime._hashCode(Ambiguity.this)
 }
 
 object Constant {
-  def apply(value: String, production: Production, location: Optional[Location], source: Optional[Source]): Constant = {
+  def apply(
+      value: String,
+      production: Production,
+      location: Optional[Location],
+      source: Optional[Source]
+  ): Constant = {
     val res = Constant(value, production)
     res.location = location
     res.source = source
@@ -72,7 +80,13 @@ object Constant {
 }
 
 object TermCons {
-  def apply(items: PStack[Term], production: Production, location: Optional[Location], source: Optional[Source], id: Optional[Integer]): TermCons = {
+  def apply(
+      items: PStack[Term],
+      production: Production,
+      location: Optional[Location],
+      source: Optional[Source],
+      id: Optional[Integer]
+  ): TermCons = {
     val res = TermCons(items, production)
     res.location = location
     res.source = source
@@ -80,17 +94,39 @@ object TermCons {
     res
   }
 
-  def apply(items: PStack[Term], production: Production, location: Optional[Location], source: Optional[Source]): TermCons = {
+  def apply(
+      items: PStack[Term],
+      production: Production,
+      location: Optional[Location],
+      source: Optional[Source]
+  ): TermCons = {
     TermCons(items, production, location, source, Optional.empty())
   }
 
-  def apply(items: PStack[Term], production: Production, location: Location, source: Source): TermCons = TermCons(items, production, Optional.of(location), Optional.of(source), Optional.empty())
+  def apply(
+      items: PStack[Term],
+      production: Production,
+      location: Location,
+      source: Source
+  ): TermCons = TermCons(
+    items,
+    production,
+    Optional.of(location),
+    Optional.of(source),
+    Optional
+      .empty()
+  )
 }
 
 object Ambiguity {
-  @annotation.varargs def apply(items: Term*): Ambiguity = Ambiguity(items.to[mutable.Set].asJava)
+  @annotation.varargs
+  def apply(items: Term*): Ambiguity = Ambiguity(items.to[mutable.Set].asJava)
 
-  def apply(items: Set[Term], location: Optional[Location], source: Optional[Source]): Ambiguity = {
+  def apply(
+      items: java.util.Set[Term],
+      location: Optional[Location],
+      source: Optional[Source]
+  ): Ambiguity = {
     val res = Ambiguity(items)
     res.location = location
     res.source = source
