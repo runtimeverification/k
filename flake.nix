d83f98f022e7ce16f21e1f851965ac67c3dc134a
rv-jenkins <devops@runtimeverification.com>
2023-12-12 09:58:31 +0000

    flake.nix, hs-backend-booster/src/main/native/hs-backend-booster: update to version 83c33a579af27bb5183d733dd4adc85b1db304ec

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs = {
     haskell-backend.url = "github:runtimeverification/haskell-backend/a5847301404583e16d55cd4d051b8e605d704fbc";
     booster-backend = {
-      url = "github:runtimeverification/hs-backend-booster/0f4af137399adc30c6d083d717ae96a4c02a8076";
+      url = "github:runtimeverification/hs-backend-booster/83c33a579af27bb5183d733dd4adc85b1db304ec";
       inputs.nixpkgs.follows = "haskell-backend/nixpkgs";
       inputs.haskell-backend.follows = "haskell-backend";
       inputs.stacklock2nix.follows = "haskell-backend/stacklock2nix";
