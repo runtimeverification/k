776df3fcc6ff3e2d2bc845b1d639e347ba0c8f4f
Scott Guest <scott.guest@runtimeverification.com>
2023-12-17 19:46:22 -0500

    Disable CHECKED for merge

--- kernel/src/main/java/org/kframework/parser/inner/ParseInModule.java
+++ kernel/src/main/java/org/kframework/parser/inner/ParseInModule.java
@@ -142,7 +142,7 @@ public class ParseInModule implements Serializable, AutoCloseable {
     this.typeInferenceDebug = typeInferenceDebug;
     this.typeInferenceMode =
         typeInferenceMode == InnerParsingOptions.TypeInferenceMode.DEFAULT
-            ? InnerParsingOptions.TypeInferenceMode.CHECKED
+            ? InnerParsingOptions.TypeInferenceMode.Z3
             : typeInferenceMode;
     this.partialParseDebug = partialParseDebug;
   }
