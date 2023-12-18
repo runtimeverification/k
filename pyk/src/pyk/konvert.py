runtimeverification/k

commit b42e8b812a3c56fd8b520b4c54bf87d2d7b1b3fb

Replace recursion by iteration (#775)
Eliminates recursion in the following functions:
* `Pattern.bottom_up`
* `Pattern.top_down`
* `Pattern.from_dict`
* `Pattern.dict`
* `konvert._kore_to_kast`
* `KompiledKore.add_injections`
* `kast.inner.bottom_up`
* `kast.inner.top_down`
* `kast.inner.bottom_up_with_summary`
* `KInner.to_dict`
* `KInner.from_dict`
* `konvert._kast_to_kore`

---------

Co-authored-by: devops <devops@runtimeverification.com>

pyk/package/version
@@ -1,1 +1,1 @@
-0.1.548
+0.1.549

pyk/pyproject.toml
@@ -4,7 +4,7 @@ build-backend = "poetry.core.masonry.api"
 
 [tool.poetry]
 name = "pyk"
-version = "0.1.548"
+version = "0.1.549"
 description = ""
 authors = [
     "Runtime Verification, Inc. <contact@runtimeverification.com>",

pyk/src/pyk/konvert.py
@@ -8,6 +8,7 @@ from .cterm import CTerm
 from .kast.inner import KApply, KLabel, KSequence, KSort, KToken, KVariable
 from .kast.manip import bool_to_ml_pred, extract_lhs, extract_rhs
 from .kore.prelude import BYTES as KORE_BYTES
+from .kore.prelude import DOTK, SORT_K
 from .kore.prelude import STRING as KORE_STRING
 from .kore.syntax import (
     DV,
@@ -137,17 +138,42 @@ def krule_to_kore(kast_defn: KDefinition, kompiled_kore: KompiledKore, krule: KR
     return axiom
 
 
-def _kast_to_kore(kast: KInner) -> Pattern:
-    if type(kast) is KToken:
-        return _ktoken_to_kore(kast)
-    elif type(kast) is KVariable:
-        return _kvariable_to_kore(kast)
-    elif type(kast) is KSequence:
-        return _ksequence_to_kore(kast)
-    elif type(kast) is KApply:
-        return _kapply_to_kore(kast)
+def _ksort_to_kore(ksort: KSort) -> SortApp:
+    return SortApp('Sort' + ksort.name)
 
-    raise ValueError(f'Unsupported KInner: {kast}')
+
+def _kast_to_kore(term: KInner) -> Pattern:
+    stack: list = [term, []]
+    while True:
+        patterns = stack[-1]
+        term = stack[-2]
+        idx = len(patterns) - len(term.terms)
+        if not idx:
+            stack.pop()
+            stack.pop()
+            pattern = _kinner_to_kore(term, patterns)
+            if not stack:
+                return pattern
+            stack[-1].append(pattern)
+        else:
+            stack.append(term.terms[idx])
+            stack.append([])
+
+
+def _kinner_to_kore(kinner: KInner, patterns: list[Pattern]) -> Pattern:
+    match kinner:
+        case KToken():
+            assert not patterns
+            return _ktoken_to_kore(kinner)
+        case KVariable():
+            assert not patterns
+            return _kvariable_to_kore(kinner)
+        case KSequence():
+            return _ksequence_to_kore(kinner, patterns)
+        case KApply():
+            return _kapply_to_kore(kinner, patterns)
+        case _:
+            raise ValueError(f'Unsupported KInner: {kinner}')
 
 
 def _ktoken_to_kore(ktoken: KToken) -> DV:
@@ -164,61 +190,52 @@ def _ktoken_to_kore(ktoken: KToken) -> DV:
     return DV(sort, value)
 
 
-def _ksort_to_kore(ksort: KSort) -> SortApp:
-    return SortApp('Sort' + ksort.name)
-
-
 def _kvariable_to_kore(kvar: KVariable) -> EVar:
     sort: Sort
     if kvar.sort:
         sort = _ksort_to_kore(kvar.sort)
     else:
-        sort = SortApp('SortK')
+        sort = SORT_K
     return EVar('Var' + munge(kvar.name), sort)
 
 
-def _ksequence_to_kore(kseq: KSequence) -> Pattern:
-    if not kseq:
-        return App('dotk')
+def _ksequence_to_kore(kseq: KSequence, patterns: list[Pattern]) -> Pattern:
+    if not patterns:
+        return DOTK
 
     unit: Pattern
-    items: tuple[KInner, ...]
+    args: list[Pattern]
 
-    last = kseq[-1]
-    if type(last) is KVariable and (not last.sort or last.sort == KSort('K')):
-        unit = _kvariable_to_kore(last)
-        items = kseq[:-1]
+    last = patterns[-1]
+    if type(last) is EVar and last.sort == SORT_K:
+        unit = last
+        args = patterns[:-1]
     else:
-        unit = App('dotk')
-        items = kseq.items
+        unit = DOTK
+        args = patterns
 
-    patterns = tuple(_kast_to_kore(item) for item in items)
-    return reduce(lambda x, y: App('kseq', (), (y, x)), reversed(patterns), unit)
+    args.reverse()
+    return reduce(lambda x, y: App('kseq', (), (y, x)), args, unit)
 
 
-def _kapply_to_kore(kapply: KApply) -> Pattern:
+def _kapply_to_kore(kapply: KApply, patterns: list[Pattern]) -> Pattern:
     if kapply.label.name in ML_QUANT_LABELS:
-        return _kapply_to_ml_quant(kapply)
+        return _kapply_to_ml_quant(kapply, patterns)
 
-    return _kapply_to_pattern(kapply)
+    return _kapply_to_pattern(kapply, patterns)
 
 
-def _kapply_to_ml_quant(kapply: KApply) -> MLQuant:
+def _kapply_to_ml_quant(kapply: KApply, patterns: list[Pattern]) -> MLQuant:
     label = kapply.label
     symbol = ML_QUANT_LABELS[label.name]
     sorts = tuple(_ksort_to_kore(ksort) for ksort in label.params)
-    kvar, kast = kapply.args
-    var = _kast_to_kore(kvar)
-    pattern = _kast_to_kore(kast)
-    patterns = (var, pattern)
     return MLQuant.of(symbol, sorts, patterns)
 
 
-def _kapply_to_pattern(kapply: KApply) -> Pattern:
+def _kapply_to_pattern(kapply: KApply, patterns: list[Pattern]) -> Pattern:
     label = kapply.label
     symbol = _label_to_kore(label.name)
     sorts = tuple(_ksort_to_kore(ksort) for ksort in label.params)
-    patterns = tuple(_kast_to_kore(kast) for kast in kapply.args)
 
     if label.name in ML_PATTERN_LABELS:
         return MLPattern.of(symbol, sorts, patterns)
@@ -243,94 +260,133 @@ def kore_to_kast(kast_defn: KDefinition, kore: Pattern) -> KInner:
     return kast_defn.remove_cell_map_items(kast)
 
 
-def _kore_to_kast(kore: Pattern) -> KInner:
-    if type(kore) is DV and kore.sort.name.startswith('Sort'):
-        if kore.sort == KORE_STRING:
-            return stringToken(kore.value.value)
-        if kore.sort == KORE_BYTES:
-            return bytesToken_from_str(kore.value.value)  # noqa: N802(kore.value.value)
-        return KToken(kore.value.value, KSort(kore.sort.name[4:]))
+def _kore_to_kast(pattern: Pattern) -> KInner:
+    stack: list = [
+        pattern,
+        (pattern.app,) if isinstance(pattern, Assoc) else pattern.patterns,
+        [],
+    ]
 
-    elif type(kore) is EVar:
-        vname = unmunge(kore.name[3:])
-        return KVariable(vname, sort=KSort(kore.sort.name[4:]))
+    while True:
+        terms = stack[-1]
+        patterns = stack[-2]
+        pattern = stack[-3]
+        idx = len(terms) - len(patterns)
+        if not idx:
+            stack.pop()
+            stack.pop()
+            stack.pop()
+            term = _pattern_to_kast(pattern, terms)
+            if not stack:
+                return term
+            stack[-1].append(term)
+        else:
+            pattern = patterns[idx]
+            stack.append(pattern)
+            stack.append((pattern.app,) if isinstance(pattern, Assoc) else pattern.patterns)
+            stack.append([])
 
-    elif type(kore) is App:
-        if kore.symbol == 'inj' and len(kore.sorts) == 2 and len(kore.args) == 1:
-            return _kore_to_kast(kore.args[0])
 
-        elif len(kore.sorts) == 0:
-            if kore.symbol == 'dotk' and len(kore.args) == 0:
-                return KSequence(())
+def _pattern_to_kast(pattern: Pattern, terms: list[KInner]) -> KInner:
+    match pattern:
+        case DV(sort, String(value)):
+            assert not terms
+            if sort == KORE_STRING:
+                return stringToken(value)
+            if sort == KORE_BYTES:
+                return bytesToken_from_str(value)
+            return KToken(value, _sort_to_kast(sort))
 
-            elif kore.symbol == 'kseq' and len(kore.args) == 2:
-                p0 = _kore_to_kast(kore.args[0])
-                p1 = _kore_to_kast(kore.args[1])
-                return KSequence((p0, p1))
+        case EVar(name, sort):
+            assert not terms
+            return KVariable(name=unmunge(name[3:]), sort=_sort_to_kast(sort))
+
+        case App(symbol, sorts, _):
+            if symbol == 'inj':
+                _, _ = sorts
+                (term,) = terms
+                return term
+
+            elif not sorts:
+                if symbol == 'dotk':
+                    () = terms
+                    return KSequence(())
+
+                elif symbol == 'kseq':
+                    _, _ = terms
+                    return KSequence(terms)
+
+                else:
+                    klabel = KLabel(unmunge(symbol[3:]))
+                    return KApply(klabel, terms)
+
+            # hardcoded polymorphic operators
+            elif (
+                symbol
+                == "Lbl'Hash'if'UndsHash'then'UndsHash'else'UndsHash'fi'Unds'K-EQUAL-SYNTAX'Unds'Sort'Unds'Bool'Unds'Sort'Unds'Sort"
+            ):
+                (sort,) = sorts
+                klabel = KLabel(unmunge(symbol[3:]), (_sort_to_kast(sort),))
+                return KApply(klabel, terms)
 
             else:
-                _label_name = unmunge(kore.symbol[3:])
-                klabel = KLabel(_label_name, [KSort(k.name[4:]) for k in kore.sorts])
-                args = tuple(_kore_to_kast(_a) for _a in kore.args)
-                return KApply(klabel, args)
+                raise ValueError(f'Unsupported polymorphic operator: {symbol}')
 
-        # hardcoded polymorphic operators
-        elif (
-            len(kore.sorts) == 1
-            and kore.symbol
-            == "Lbl'Hash'if'UndsHash'then'UndsHash'else'UndsHash'fi'Unds'K-EQUAL-SYNTAX'Unds'Sort'Unds'Bool'Unds'Sort'Unds'Sort"
-        ):
-            _label_name = unmunge(kore.symbol[3:])
-            klabel = KLabel(_label_name, [KSort(kore.sorts[0].name[4:])])
-            args = tuple(_kore_to_kast(_a) for _a in kore.args)
-            return KApply(klabel, args)
+        case Top(sort):
+            assert not terms
+            return mlTop(sort=_sort_to_kast(sort))
 
-    elif type(kore) is Top:
-        return mlTop(sort=KSort(kore.sort.name[4:]))
+        case Bottom(sort):
+            assert not terms
+            return mlBottom(sort=_sort_to_kast(sort))
 
-    elif type(kore) is Bottom:
-        return mlBottom(sort=KSort(kore.sort.name[4:]))
+        case And(sort, _):
+            return mlAnd(terms, sort=_sort_to_kast(sort))
 
-    elif type(kore) is And:
-        psort = KSort(kore.sort.name[4:])
-        args = tuple(_kore_to_kast(op) for op in kore.ops)
-        return mlAnd(args, sort=psort)
+        case Implies(sort, left, right):
+            larg = _kore_to_kast(left)
+            rarg = _kore_to_kast(right)
+            return mlImplies(larg, rarg, sort=_sort_to_kast(sort))
 
-    elif type(kore) is Implies:
-        psort = KSort(kore.sort.name[4:])
-        larg = _kore_to_kast(kore.left)
-        rarg = _kore_to_kast(kore.right)
-        return mlImplies(larg, rarg, sort=psort)
+        case Not(sort, pattern):
+            karg = _kore_to_kast(pattern)
+            return mlNot(karg, sort=_sort_to_kast(sort))
 
-    elif type(kore) is Not:
-        psort = KSort(kore.sort.name[4:])
-        arg = _kore_to_kast(kore.pattern)
-        return mlNot(arg, sort=psort)
+        case Exists(sort, var, pattern):
+            kvar = _kore_to_kast(var)
+            body = _kore_to_kast(pattern)
+            assert isinstance(kvar, KVariable)
+            return mlExists(kvar, body, sort=_sort_to_kast(sort))
 
-    elif type(kore) is Exists:
-        psort = KSort(kore.sort.name[4:])
-        var = _kore_to_kast(kore.var)
-        assert type(var) is KVariable
-        body = _kore_to_kast(kore.pattern)
-        return mlExists(var, body, sort=psort)
+        case Equals(op_sort, sort, left, right):
+            larg = _kore_to_kast(left)
+            rarg = _kore_to_kast(right)
+            return mlEquals(
+                larg,
+                rarg,
+                arg_sort=_sort_to_kast(op_sort),
+                sort=_sort_to_kast(sort),
+            )
 
-    elif type(kore) is Equals:
-        osort = KSort(kore.op_sort.name[4:])
-        psort = KSort(kore.sort.name[4:])
-        larg = _kore_to_kast(kore.left)
-        rarg = _kore_to_kast(kore.right)
-        return mlEquals(larg, rarg, arg_sort=osort, sort=psort)
+        case Ceil(op_sort, sort, pattern):
+            karg = _kore_to_kast(pattern)
+            return mlCeil(
+                karg,
+                arg_sort=_sort_to_kast(op_sort),
+                sort=_sort_to_kast(sort),
+            )
 
-    elif type(kore) is Ceil:
-        osort = KSort(kore.op_sort.name[4:])
-        psort = KSort(kore.sort.name[4:])
-        arg = _kore_to_kast(kore.pattern)
-        return mlCeil(arg, arg_sort=osort, sort=psort)
+        case Assoc():
+            return _kore_to_kast(pattern.pattern)
 
-    elif isinstance(kore, Assoc):
-        return _kore_to_kast(kore.pattern)
+        case _:
+            raise ValueError(f'Unsupported Pattern: {pattern}')
 
-    raise ValueError(f'Unsupported Pattern: {kore}')
+
+def _sort_to_kast(sort: Sort) -> KSort:
+    if not isinstance(sort, SortApp) or not sort.name.startswith('Sort'):
+        raise ValueError(f'Unsupported Sort: {sort}')
+    return KSort(sort.name[4:])
 
 
 # --------------

pyk/src/pyk/__main__.py
@@ -117,7 +117,7 @@ def exec_rpc_print(args: Namespace) -> None:
         non_state_keys = set(request_params.keys()).difference(['state'])
         for key in non_state_keys:
             output_buffer.append(f'{key}: {request_params[key]}')
-        state = CTerm.from_kast(printer.kore_to_kast(kore_term(request_params['state'])))  # type: ignore
+        state = CTerm.from_kast(printer.kore_to_kast(kore_term(request_params['state'])))
         output_buffer.append('State:')
         output_buffer.append(printer.pretty_print(state.kast, sort_collections=True))
         return output_buffer
@@ -163,7 +163,7 @@ def exec_rpc_print(args: Namespace) -> None:
             output_buffer.append(f'id: {input_dict["id"]}')
             if list(input_dict['result'].keys()) == ['state']:  # this is a "simplify" response
                 output_buffer.append('Method: simplify')
-                state = CTerm.from_kast(printer.kore_to_kast(kore_term(input_dict['result']['state'])))  # type: ignore
+                state = CTerm.from_kast(printer.kore_to_kast(kore_term(input_dict['result']['state'])))
                 output_buffer.append('State:')
                 output_buffer.append(printer.pretty_print(state.kast, sort_collections=True))
             elif list(input_dict['result'].keys()) == ['module']:  # this is an "add-module" response

pyk/src/pyk/kast/kast.py
@@ -25,30 +25,9 @@ class KAst(ABC):
     def version() -> int:
         return 3
 
-    @classmethod
-    def from_dict(cls: type[T], d: Mapping[str, Any]) -> T:
-        from . import inner, outer
-
-        node = d['node']
-
-        if node == 'KAtt':
-            return KAtt.from_dict(d)  # type: ignore
-
-        if node in inner.KInner._INNER_NODES:
-            return getattr(inner, node).from_dict(d)
-
-        if node in outer.KOuter._OUTER_NODES:
-            return getattr(outer, node).from_dict(d)
-
-        raise ValueError(f'Expected KAst label as "node" value, found: {node}')
-
     @abstractmethod
     def to_dict(self) -> dict[str, Any]:
         ...
-
-    @classmethod
-    def from_json(cls: type[T], s: str) -> T:
-        return cls.from_dict(json.loads(s))
 
     @final
     def to_json(self) -> str:
@@ -59,23 +38,16 @@ class KAst(ABC):
     def hash(self) -> str:
         return hash_str(self.to_json())
 
-    @classmethod
-    def _check_node(cls: type[T], d: Mapping[str, Any], expected: str | None = None) -> None:
-        expected = expected if expected is not None else cls.__name__
-        actual = d['node']
-        if actual != expected:
-            raise ValueError(f'Expected "node" value: {expected}, got: {actual}')
-
-    def _as_shallow_tuple(self) -> tuple[Any, ...]:
-        # shallow copy version of dataclass.astuple.
-        return tuple(self.__dict__[field.name] for field in fields(type(self)))  # type: ignore
-
     def __lt__(self, other: Any) -> bool:
         if not isinstance(other, KAst):
             return NotImplemented
         if type(self) == type(other):
             return self._as_shallow_tuple() < other._as_shallow_tuple()
         return type(self).__name__ < type(other).__name__
+
+    def _as_shallow_tuple(self) -> tuple[Any, ...]:
+        # shallow copy version of dataclass.astuple.
+        return tuple(self.__dict__[field.name] for field in fields(type(self)))  # type: ignore
 
 
 @final
@@ -116,16 +88,16 @@ class KAtt(KAst, Mapping[str, Any]):
 
     @classmethod
     def from_dict(cls: type[KAtt], d: Mapping[str, Any]) -> KAtt:
-        cls._check_node(d)
         return KAtt(atts=d['att'])
 
     def to_dict(self) -> dict[str, Any]:
-        def _to_dict(m: Any) -> Any:
-            if isinstance(m, FrozenDict):
-                return {k: _to_dict(v) for (k, v) in m.items()}
-            return m
+        return {'node': 'KAtt', 'att': KAtt._unfreeze(self.atts)}
 
-        return {'node': 'KAtt', 'att': _to_dict(self.atts)}
+    @staticmethod
+    def _unfreeze(x: Any) -> Any:
+        if isinstance(x, FrozenDict):
+            return {k: KAtt._unfreeze(v) for (k, v) in x.items()}
+        return x
 
     def let(self, *, atts: Mapping[str, Any] | None = None) -> KAtt:
         atts = atts if atts is not None else self.atts
@@ -174,12 +146,11 @@ class WithKAtt(ABC):
         return self.let_att(att=self.att.update(atts))
 
 
-# TODO fix typing (maybe just return Mapping)
-def kast_term(dct: Mapping[str, Any], cls: type[T] = KAst) -> T:  # type: ignore
+def kast_term(dct: Mapping[str, Any]) -> Mapping[str, Any]:
     if dct['format'] != 'KAST':
         raise ValueError(f"Invalid format: {dct['format']}")
 
     if dct['version'] != KAst.version():
         raise ValueError(f"Invalid version: {dct['version']}")
 
-    return cls.from_dict(dct['term'])
+    return dct['term']

commit 0d25e72213b7336687afca4e6225458bcf82ca18

Fix `_kore_to_kast` (#789)
Fixes a bug introduced in #775

---------

Co-authored-by: devops <devops@runtimeverification.com>

pyk/package/version
@@ -1,1 +1,1 @@
-0.1.551
+0.1.552

pyk/pyproject.toml
@@ -4,7 +4,7 @@ build-backend = "poetry.core.masonry.api"
 
 [tool.poetry]
 name = "pyk"
-version = "0.1.551"
+version = "0.1.552"
 description = ""
 authors = [
     "Runtime Verification, Inc. <contact@runtimeverification.com>",

pyk/src/pyk/konvert.py
@@ -261,19 +261,13 @@ def kore_to_kast(kast_defn: KDefinition, kore: Pattern) -> KInner:
 
 
 def _kore_to_kast(pattern: Pattern) -> KInner:
-    stack: list = [
-        pattern,
-        (pattern.app,) if isinstance(pattern, Assoc) else pattern.patterns,
-        [],
-    ]
-
+    stack: list = [pattern.pattern if isinstance(pattern, Assoc) else pattern, []]
     while True:
         terms = stack[-1]
-        patterns = stack[-2]
-        pattern = stack[-3]
+        pattern = stack[-2]
+        patterns = pattern.patterns
         idx = len(terms) - len(patterns)
         if not idx:
-            stack.pop()
             stack.pop()
             stack.pop()
             term = _pattern_to_kast(pattern, terms)
@@ -282,8 +276,7 @@ def _kore_to_kast(pattern: Pattern) -> KInner:
             stack[-1].append(term)
         else:
             pattern = patterns[idx]
-            stack.append(pattern)
-            stack.append((pattern.app,) if isinstance(pattern, Assoc) else pattern.patterns)
+            stack.append(pattern.pattern if isinstance(pattern, Assoc) else pattern)
             stack.append([])
 
 
@@ -343,24 +336,21 @@ def _pattern_to_kast(pattern: Pattern, terms: list[KInner]) -> KInner:
         case And(sort, _):
             return mlAnd(terms, sort=_sort_to_kast(sort))
 
-        case Implies(sort, left, right):
-            larg = _kore_to_kast(left)
-            rarg = _kore_to_kast(right)
+        case Implies(sort, _, _):
+            larg, rarg = terms
             return mlImplies(larg, rarg, sort=_sort_to_kast(sort))
 
-        case Not(sort, pattern):
-            karg = _kore_to_kast(pattern)
+        case Not(sort, _):
+            (karg,) = terms
             return mlNot(karg, sort=_sort_to_kast(sort))
 
-        case Exists(sort, var, pattern):
-            kvar = _kore_to_kast(var)
-            body = _kore_to_kast(pattern)
-            assert isinstance(kvar, KVariable)
+        case Exists(sort, EVar(vname, vsort), _):
+            kvar = KVariable(name=unmunge(vname[3:]), sort=_sort_to_kast(vsort))
+            (body,) = terms
             return mlExists(kvar, body, sort=_sort_to_kast(sort))
 
-        case Equals(op_sort, sort, left, right):
-            larg = _kore_to_kast(left)
-            rarg = _kore_to_kast(right)
+        case Equals(op_sort, sort, _, _):
+            larg, rarg = terms
             return mlEquals(
                 larg,
                 rarg,
@@ -368,16 +358,13 @@ def _pattern_to_kast(pattern: Pattern, terms: list[KInner]) -> KInner:
                 sort=_sort_to_kast(sort),
             )
 
-        case Ceil(op_sort, sort, pattern):
-            karg = _kore_to_kast(pattern)
+        case Ceil(op_sort, sort, _):
+            (karg,) = terms
             return mlCeil(
                 karg,
                 arg_sort=_sort_to_kast(op_sort),
                 sort=_sort_to_kast(sort),
             )
-
-        case Assoc():
-            return _kore_to_kast(pattern.pattern)
 
         case _:
             raise ValueError(f'Unsupported Pattern: {pattern}')

pyk/src/tests/profiling/profile_kore_to_kast.py
@@ -1,0 +1,23 @@
+from __future__ import annotations
+
+import json
+from typing import TYPE_CHECKING
+
+from pyk.konvert import _kore_to_kast
+from pyk.kore.syntax import Pattern
+
+from .utils import TEST_DATA_DIR
+
+if TYPE_CHECKING:
+    from pyk.testing import Profiler
+
+
+def test_kore_to_kast(profile: Profiler) -> None:
+    json_file = TEST_DATA_DIR / 'kore-to-kast' / 'mir-semantics-295.json'
+    json_data = json.loads(json_file.read_text())
+
+    with profile('json-to-kore.prof', sort_keys=('cumtime',), limit=50):
+        pattern = Pattern.from_dict(json_data)
+
+    with profile('kore-to-kast.prof', sort_keys=('cumtime',), limit=50):
+        _kore_to_kast(pattern)