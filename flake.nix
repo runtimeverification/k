dea3bde0e99695336d7d2b9a31447033d19c84f2
rv-jenkins <devops@runtimeverification.com>
2024-01-23 14:30:40 +0000

    flake.nix, haskell-backend/src/main/native/haskell-backend: update to version d9fa6aeb36407193b701059a54307b6368841632

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "K Framework";
   inputs = {
-    haskell-backend.url = "github:runtimeverification/haskell-backend/25f07c8b37b85bf19c8ec0894b60a51cb5e73842";
+    haskell-backend.url = "github:runtimeverification/haskell-backend/d9fa6aeb36407193b701059a54307b6368841632";
     booster-backend = {
       url = "github:runtimeverification/hs-backend-booster/f295a6a99b9cf6e8ebc33f553373fa41c1270f90";
       inputs.nixpkgs.follows = "haskell-backend/nixpkgs";
