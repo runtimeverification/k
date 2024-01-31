b96bdb08e36a1f9a9a93ccf9a270c189e7f17d1a
Dwight Guth <dwight.guth@runtimeverification.com>
2024-01-31 12:08:08 -0600

    new module to load bindings from llvm backend distro (https://github.com/runtimeverification/pyk/pull/850)
    I spoke with Bruce and he suggested this approach for dealing with the
    `_kllvm` bindings module in Pi^2. Essentially, the LLVM backend builds
    the bindings module with a particular pre-configured version of python,
    and then this code makes the assumption that that version of python is
    ABI-compatible with the version of python used by the code importing
    this module. It may not be true in general, but in cases where we can
    control both the version of K and the version of pyk that are installed,
    it is possible to guarantee this. This then becomes part of the
    requirements for a successful installation of the tool in question. I
    tested that locally, this seems to cause the unit tests to pass. I can
    also confirm that this causes a significant improvement in the time to
    run a short-lived python invocation that imports this module, on the
    order of about 6 seconds.
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- pyk/docs/conf.py
+++ pyk/docs/conf.py
@@ -9,8 +9,8 @@
 project = 'pyk'
 author = 'Runtime Verification, Inc'
 copyright = '2024, Runtime Verification, Inc'
-version = '0.1.606'
-release = '0.1.606'
+version = '0.1.607'
+release = '0.1.607'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
