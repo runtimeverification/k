28db5517174d720bb94101266f02d76018e0636e
Scott Guest <scott.guest@runtimeverification.com>
2024-02-02 01:43:55 -0800

    Remove usages of `kore` attribute (https://github.com/runtimeverification/pyk/pull/855)
    Paired with runtimeverification/k#3942
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>
    Co-authored-by: Bruce Collie <brucecollie82@gmail.com>

--- pyk/docs/conf.py
+++ pyk/docs/conf.py
@@ -9,8 +9,8 @@
 project = 'pyk'
 author = 'Runtime Verification, Inc'
 copyright = '2024, Runtime Verification, Inc'
-version = '0.1.611'
-release = '0.1.611'
+version = '0.1.612'
+release = '0.1.612'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
