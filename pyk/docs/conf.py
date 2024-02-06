cd4b3ae38e9a3698e53b2d2ce260005b0de638e0
Tamás Tóth <tothtamas28@users.noreply.github.com>
2024-02-06 11:44:30 +0100

    Modify outer lexer to admit dots in attribute keys (https://github.com/runtimeverification/pyk/pull/864)
    Related: https://github.com/runtimeverification/k/pull/3953
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- pyk/docs/conf.py
+++ pyk/docs/conf.py
@@ -9,8 +9,8 @@
 project = 'pyk'
 author = 'Runtime Verification, Inc'
 copyright = '2024, Runtime Verification, Inc'
-version = '0.1.614'
-release = '0.1.614'
+version = '0.1.615'
+release = '0.1.615'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
