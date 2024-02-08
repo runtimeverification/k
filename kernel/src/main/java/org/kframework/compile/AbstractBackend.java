65e1f69a37065c7769329e1cb4c33d3f48a3c759
Scott Guest <scott.guest@runtimeverification.com>
2024-02-08 01:27:18 -0500

    Delete AbstractBackend as it is now empty

--- kernel/src/main/java/org/kframework/compile/AbstractBackend.java
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Runtime Verification, Inc. All Rights Reserved.
-package org.kframework.compile;
-
-/**
- * @author Denis Bogdanas Created on 09-Jan-20.
- */
-public abstract class AbstractBackend implements Backend {}
