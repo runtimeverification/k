a6bc4d81bd0980220fafb1650f95eee418279e9b
Tamás Tóth <tothtamas28@users.noreply.github.com>
2024-02-16 13:47:22 +0100

    Add test for `KProduction.label` (https://github.com/runtimeverification/pyk/pull/880)
    ~Blocked on #860~
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- pyk/src/tests/integration/test_kompile.py
+++ pyk/src/tests/integration/test_kompile.py
@@ -4,12 +4,15 @@ from typing import TYPE_CHECKING
 
 import pytest
 
+from pyk.kast import KAtt
 from pyk.ktool.kompile import KompileBackend, KompileNotFoundError, kompile
 from pyk.testing import KompiledTest
 
 from .utils import K_FILES
 
 if TYPE_CHECKING:
+    from pyk.kast.inner import KLabel
+    from pyk.kast.outer import KDefinition
     from pyk.ktool.kompile import DefinitionInfo
 
 
@@ -42,3 +45,28 @@ def test_kompile_not_found(monkeypatch: pytest.MonkeyPatch) -> None:
 
     with pytest.raises(KompileNotFoundError):
         kompile(k_file, command=[bad_kompile])
+
+
+class TestKLabel(KompiledTest):
+    KOMPILE_MAIN_FILE = K_FILES / 'klabel.k'
+
+    def test(self, definition: KDefinition) -> None:
+        # Given
+        module = definition.module('KLABEL')
+
+        def klabel_defined_at_line(line: int) -> KLabel:
+            (prod,) = (prod for prod in module.productions if prod.att.get(KAtt.LOCATION, [None])[0] == line)
+            res = prod.klabel
+            assert res is not None
+            return res
+
+        foo = klabel_defined_at_line(2)
+        bar = klabel_defined_at_line(3)
+        baz = klabel_defined_at_line(4)
+        qux = klabel_defined_at_line(5)
+
+        # Then
+        assert foo.name == 'foo'
+        assert bar.name == 'bar'
+        assert baz.name == 'baz_KLABEL_Foo'
+        assert qux.name == 'qux_KLABEL_Foo'
