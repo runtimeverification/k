72aecb596e7cfab55410d7e22ab63b6080049784
Lisandra <lisandrasilva18@hotmail.com>
2024-02-19 09:26:06 +0000

    Added proof execution time to APRProof (https://github.com/runtimeverification/pyk/pull/881)
    This PR records how long a proof takes to execute in the `APRProof`
    class so it can be accessed in Kontrol to display in the output message
    and in the `xml` report.
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- pyk/docs/conf.py
+++ pyk/docs/conf.py
@@ -9,8 +9,8 @@
 project = 'pyk'
 author = 'Runtime Verification, Inc'
 copyright = '2024, Runtime Verification, Inc'
-version = '0.1.631'
-release = '0.1.631'
+version = '0.1.632'
+release = '0.1.632'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
