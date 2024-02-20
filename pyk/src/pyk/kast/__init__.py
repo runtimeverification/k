40d8b3b8973cdc0261ebb77b1e94a0842f5003f2
Tamás Tóth <tothtamas28@users.noreply.github.com>
2024-02-20 19:04:13 +0100

    Extract module `att` (https://github.com/runtimeverification/pyk/pull/897)
    Code movement.
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- pyk/src/pyk/kast/__init__.py
+++ pyk/src/pyk/kast/__init__.py
@@ -1,3 +1,4 @@
+from .att import EMPTY_ATT, AttKey, Atts, KAtt, WithKAtt
 from .inner import KInner
-from .kast import EMPTY_ATT, AttKey, Atts, KAst, KAtt, WithKAtt, kast_term
+from .kast import KAst, kast_term
 from .outer import KOuter
