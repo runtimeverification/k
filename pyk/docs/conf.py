f98462b443848b4a19d137bdf5288093778124c6
Tamás Tóth <tothtamas28@users.noreply.github.com>
2024-02-20 14:43:21 +0100

    Remove special casing of `ARRAY` hooks (https://github.com/runtimeverification/pyk/pull/896)
    In accordance with runtimeverification/k#4002.
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- pyk/docs/conf.py
+++ pyk/docs/conf.py
@@ -9,8 +9,8 @@
 project = 'pyk'
 author = 'Runtime Verification, Inc'
 copyright = '2024, Runtime Verification, Inc'
-version = '0.1.638'
-release = '0.1.638'
+version = '0.1.639'
+release = '0.1.639'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
