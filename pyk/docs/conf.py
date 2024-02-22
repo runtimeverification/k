2709178c205b19dd88caaa5e47c689db8c468b54
Lisandra <lisandrasilva18@hotmail.com>
2024-02-22 16:43:39 +0000

    Added execution time to APRSummary (https://github.com/runtimeverification/pyk/pull/908)
    Needed to implement [Issue
    389](https://github.com/runtimeverification/kontrol/issues/389) in
    Kontrol.
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- pyk/docs/conf.py
+++ pyk/docs/conf.py
@@ -9,8 +9,8 @@
 project = 'pyk'
 author = 'Runtime Verification, Inc'
 copyright = '2024, Runtime Verification, Inc'
-version = '0.1.645'
-release = '0.1.645'
+version = '0.1.646'
+release = '0.1.646'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
