0faec02643267855ee1d17d0ff2768b9100cb422
Noah Watson <107630091+nwatson22@users.noreply.github.com>
2024-02-22 15:26:52 -0600

    Move kcfg commit logic to KCFG class (https://github.com/runtimeverification/pyk/pull/909)
    - `KCFGExplore.extend_kcfg()` replaced by `KCFG.extend()`
    - Logging of `ExtendResult`s moved earlier to when they are created
    (before, they were logged when applied, but this is no longer possible
    with application logic (`extend()`) living in `KCFG`.
    - `KCFGExplore.extend()` removed and its logic moved up into
    `advance_pending_node()`
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- pyk/docs/conf.py
+++ pyk/docs/conf.py
@@ -9,8 +9,8 @@
 project = 'pyk'
 author = 'Runtime Verification, Inc'
 copyright = '2024, Runtime Verification, Inc'
-version = '0.1.646'
-release = '0.1.646'
+version = '0.1.647'
+release = '0.1.647'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
