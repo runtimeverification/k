d1494295b9073f925c3dd1b2ceddb18ce77a9bf2
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2024-02-27 10:43:43 -0700

    Fix KCFG.replace_node functionality (https://github.com/runtimeverification/pyk/pull/920)
    Fixes: https://github.com/runtimeverification/pyk/issues/686
    
    Currently, `KCFG.replace_node` is broken because `KCFG.Successor` stores
    directly a `KCFG.Node` for the sources and targets. So when we replace a
    node in the store currently, we are only replacing the node itself, but
    not iterating through the successors and replacing occurances of the
    same node there.
    
    This PR uses a pretty brute-force way to do that (to be able to make
    progress on storing Node attributes on nodes directly instead of as
    separate lists), and later we can make a better way of storing this
    information: https://github.com/runtimeverification/pyk/issues/919.
    
    - A test is added, that is failing on master but passing on this PR,
    that retrieving a node that is part of an edge happens correctly after
    erplacement.
    - Helpers `KCFG.Succesor.{replace_source,replace_target}` are added for
    producing new copies of the successors with th eappropriate node on them
    updated.
    - The routine `KCFG.replace_node` is updated to use these replacer
    methods and update the actual edge dictionaries.
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- pyk/docs/conf.py
+++ pyk/docs/conf.py
@@ -9,8 +9,8 @@
 project = 'pyk'
 author = 'Runtime Verification, Inc'
 copyright = '2024, Runtime Verification, Inc'
-version = '0.1.656'
-release = '0.1.656'
+version = '0.1.657'
+release = '0.1.657'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
