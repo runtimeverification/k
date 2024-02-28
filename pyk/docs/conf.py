01b90b7def5001de419d8579553d21c878102788
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2024-02-28 07:34:22 -0700

    konvert/_kast_to_kore: remove dependency on CTerm (#927)
    Another circular import caused by the change to the `cterm` module is
    removed.
    
    This was a light dependency on `CTerm` by the `konvert` module, which is
    not actually needed.
    
    - Logic from `CTerm` that was used in `konvert` is now inlined.
    - The per-test timeout is increased to 500s (the switch to normal
    runners instead of ephemeral ones was causing this to be an issue).
    - The `fail-fast` is disabled, so that even if one run of integration
    tests is flaky, the other goes through. We are seeing more flakiness in
    starting RPC servers on the non-ephemeral machines.
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>
    Co-authored-by: rv-jenkins <admin@runtimeverification.com>

--- pyk/docs/conf.py
+++ pyk/docs/conf.py
@@ -9,8 +9,8 @@
 project = 'pyk'
 author = 'Runtime Verification, Inc'
 copyright = '2024, Runtime Verification, Inc'
-version = '0.1.661'
-release = '0.1.661'
+version = '0.1.662'
+release = '0.1.662'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
