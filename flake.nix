c08409ceb03638797ee6f25ec0acfa50dcbd0f7c
Scott Guest <scott.guest@runtimeverification.com>
2024-02-27 21:51:08 -0500

    Update mvnHash in flake.nix

--- flake.nix
+++ flake.nix
@@ -61,7 +61,7 @@
 
             k-framework = { haskell-backend-bins, llvm-kompile-libs }:
               prev.callPackage ./nix/k.nix {
-                mvnHash = "sha256-AMxXqu1bbpnmsmLTizNw1n2llSdvx6AuNZRGUHqPn14=";
+                mvnHash = "sha256-KGVrEM+JD2bvCzFaiU3mepcMgGLy7JsWToVK02p/8KY=";
                 manualMvnArtifacts = [
                   "org.scala-lang:scala-compiler:2.12.18"
                   "ant-contrib:ant-contrib:1.0b3"
