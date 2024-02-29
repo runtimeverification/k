35ff80215b45692cadfd58afd838bf54dc8b95a9
Tamás Tóth <tothtamas28@users.noreply.github.com>
2024-02-29 06:03:40 +0100

    Add tests for `add-module` with hash as module name (https://github.com/runtimeverification/pyk/pull/871)
    Automerge https://github.com/runtimeverification/pyk/pull/871: Add tests for `add-module` with hash as module name

--- pyk/docs/conf.py
+++ pyk/docs/conf.py
@@ -9,8 +9,8 @@
 project = 'pyk'
 author = 'Runtime Verification, Inc'
 copyright = '2024, Runtime Verification, Inc'
-version = '0.1.666'
-release = '0.1.666'
+version = '0.1.667'
+release = '0.1.667'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
