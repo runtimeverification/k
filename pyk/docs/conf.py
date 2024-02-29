966ba6cb0c8e1fb2650418c09d69472c94365c2a
Noah Watson <107630091+nwatson22@users.noreply.github.com>
2024-02-29 11:04:21 -0600

    Subsumption checking fix (https://github.com/runtimeverification/pyk/pull/936)
    Automerge https://github.com/runtimeverification/pyk/pull/936: Subsumption checking fix

--- pyk/docs/conf.py
+++ pyk/docs/conf.py
@@ -9,8 +9,8 @@
 project = 'pyk'
 author = 'Runtime Verification, Inc'
 copyright = '2024, Runtime Verification, Inc'
-version = '0.1.671'
-release = '0.1.671'
+version = '0.1.672'
+release = '0.1.672'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
