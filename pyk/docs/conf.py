eb221bcdcffff5eea6a5820c66dea0b4e36a1f1e
rv-jenkins <admin@runtimeverification.com>
2024-02-29 04:05:55 -0700

    Update dependency: deps/k_release (https://github.com/runtimeverification/pyk/pull/931)
    Automerge https://github.com/runtimeverification/pyk/pull/931: Update dependency: deps/k_release

--- pyk/docs/conf.py
+++ pyk/docs/conf.py
@@ -9,8 +9,8 @@
 project = 'pyk'
 author = 'Runtime Verification, Inc'
 copyright = '2024, Runtime Verification, Inc'
-version = '0.1.668'
-release = '0.1.668'
+version = '0.1.669'
+release = '0.1.669'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
