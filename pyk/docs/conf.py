d095fa5c7eca0982d0659cf0e04444478dcedb27
Jost Berthold <jost.berthold@gmail.com>
2024-03-06 11:48:50 +1100

    Fix username for dockerhub push (https://github.com/runtimeverification/pyk/pull/949)
    Co-authored-by: devops <devops@runtimeverification.com>

--- pyk/docs/conf.py
+++ pyk/docs/conf.py
@@ -9,8 +9,8 @@
 project = 'pyk'
 author = 'Runtime Verification, Inc'
 copyright = '2024, Runtime Verification, Inc'
-version = '0.1.683'
-release = '0.1.683'
+version = '0.1.684'
+release = '0.1.684'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
