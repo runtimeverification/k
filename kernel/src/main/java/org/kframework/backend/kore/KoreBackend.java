19572d8d3d7d4763ff6064e424d42bb51698a5ad
Scott Guest <scott.guest@runtimeverification.com>
2024-03-08 19:04:37 -0500

    Emit warning if config var is in cell collection without initial attribute

--- kernel/src/main/java/org/kframework/backend/kore/KoreBackend.java
+++ kernel/src/main/java/org/kframework/backend/kore/KoreBackend.java
@@ -200,6 +200,7 @@ public class KoreBackend implements Backend {
             DefinitionTransformer.from(
                     m ->
                         new ResolveFreshConstants(
+                                kem,
                                 d,
                                 kompileOptions.topCell,
                                 files,
@@ -315,7 +316,7 @@ public class KoreBackend implements Backend {
     Function1<Module, Module> resolveFreshConstants =
         d ->
             ModuleTransformer.from(
-                    new ResolveFreshConstants(def, kompileOptions.topCell, files)::resolve,
+                    new ResolveFreshConstants(kem, def, kompileOptions.topCell, files)::resolve,
                     "resolving !Var variables")
                 .apply(d);
     Function1<Module, Module> addImplicitCounterCell =
