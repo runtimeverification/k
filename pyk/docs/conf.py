36ecb58289166166c236f9ce4a459bc690f4534d
Tolga Ovatman <ovatman@gmail.com>
2024-03-11 23:43:41 +0300

    cache prior loops discovered in bmc (https://github.com/runtimeverification/pyk/pull/922)
    This pr adds a `prior_loops_cache` that holds a dictionary of {loop :
    prior same loops} that has been discovered so far.
    During `bmc_depth` bound checking the new loop node's cache is populated
    with the previously discovered same nodes.
    The cache is also written/read as proof data. 
    Closes: https://github.com/runtimeverification/kontrol/issues/283
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>
    Co-authored-by: Tolga Ovatman <tolga.ovatman@runtimeverification.com>

--- pyk/docs/conf.py
+++ pyk/docs/conf.py
@@ -9,8 +9,8 @@
 project = 'pyk'
 author = 'Runtime Verification, Inc'
 copyright = '2024, Runtime Verification, Inc'
-version = '0.1.694'
-release = '0.1.694'
+version = '0.1.695'
+release = '0.1.695'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
