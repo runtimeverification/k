993805e184ce667704ebef11cccd0e0996c39230
Samuel Balco <goodlyrottenapple@gmail.com>
2024-03-11 13:27:01 +0100

    KCFG viewer tweaks (https://github.com/runtimeverification/pyk/pull/963)
    Hide custom view when none is supplied and show constraints in the tree
    view as booleans not ML.
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>
    Co-authored-by: Palina Tolmach <polina.tolmach@gmail.com>

--- pyk/docs/conf.py
+++ pyk/docs/conf.py
@@ -9,8 +9,8 @@
 project = 'pyk'
 author = 'Runtime Verification, Inc'
 copyright = '2024, Runtime Verification, Inc'
-version = '0.1.693'
-release = '0.1.693'
+version = '0.1.694'
+release = '0.1.694'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
