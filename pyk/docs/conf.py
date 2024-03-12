14cca602799b7fe3057e25146a2eea65f8c9d902
lucasmt <36549752+lucasmt@users.noreply.github.com>
2024-03-12 11:43:48 -0500

    Refactor `Proof.status` (https://github.com/runtimeverification/pyk/pull/969)
    - Replaces the original `Proof.status` abstract method with a new
    abstract method `own_status` that returns only the status of the current
    proof, not accounting for any subproofs.
    - Adds a general implementation of `Proof.status` that considers both
    `own_status` and `subproofs_status`.
    - Updates `APRProof` and `ImpliesProof` to implement `own_status`
    instead of `status`.
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>
    Co-authored-by: Everett Hildenbrandt <everett.hildenbrandt@gmail.com>

--- pyk/docs/conf.py
+++ pyk/docs/conf.py
@@ -9,8 +9,8 @@
 project = 'pyk'
 author = 'Runtime Verification, Inc'
 copyright = '2024, Runtime Verification, Inc'
-version = '0.1.699'
-release = '0.1.699'
+version = '0.1.700'
+release = '0.1.700'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
