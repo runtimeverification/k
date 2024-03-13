48679611ce1b6b9a073b3da93454d667c2e8b626
Bruce Collie <brucecollie82@gmail.com>
2024-03-13 14:35:22 +0000

    Fix version parsing script

--- kernel/src/main/java/org/kframework/main/JavaVersion.java
+++ kernel/src/main/java/org/kframework/main/JavaVersion.java
@@ -4,8 +4,15 @@ package org.kframework.main;
 /** Created by dwightguth on 9/23/15. */
 public class JavaVersion {
 
+  /**
+   * Prints a version string that can be parsed by checkJava.
+   *
+   * For more context on what format is expected here, see the checkJava script.
+   * If the output of `java --version` ever changes, this class should do so as
+   * well.
+   */
   public static void main(String[] args) {
-    System.err.println("java version \"" + System.getProperty("java.version") + "\"");
+    System.err.println("java " + System.getProperty("java.version"));
     System.err.println(System.getProperty("sun.arch.data.model") + "-Bit");
   }
 }
