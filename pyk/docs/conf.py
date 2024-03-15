a93f2c791deb8ac5abd5a7dd946a90811b1ba4e4
Tamás Tóth <tothtamas28@users.noreply.github.com>
2024-03-15 14:21:05 +0100

    Generate no-confusion axioms (https://github.com/runtimeverification/pyk/pull/978)
    Co-authored-by: devops <devops@runtimeverification.com>

--- pyk/docs/conf.py
+++ pyk/docs/conf.py
@@ -9,8 +9,8 @@
 project = 'pyk'
 author = 'Runtime Verification, Inc'
 copyright = '2024, Runtime Verification, Inc'
-version = '0.1.712'
-release = '0.1.712'
+version = '0.1.713'
+release = '0.1.713'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
