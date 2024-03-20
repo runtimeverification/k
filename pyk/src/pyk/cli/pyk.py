1377824d75af9851638c128b6fe73530ba1b5823
Tolga Ovatman <tolga.ovatman@runtimeverification.com>
2024-03-20 21:43:59 +0300

    format fixes

--- pyk/src/pyk/cli/pyk.py
+++ pyk/src/pyk/cli/pyk.py
@@ -93,8 +93,8 @@ def get_option_string_destination(command: str, option_string: str) -> str:
 
     if option_string in option_string_destinations:
         return option_string_destinations[option_string]
-    else: 
-        return option_string.replace('-','_')
+    else:
+        return option_string.replace('-', '_')
 
 
 class PrintInput(Enum):
@@ -110,15 +110,20 @@ class KoreToJsonOptions(LoggingOptions): ...
 
 class CoverageOptions(DefinitionOptions, OutputFileOptions, LoggingOptions):
     coverage_file: IO[Any]
-    
+
     @staticmethod
     def from_option_string() -> dict[str, str]:
-        return DefinitionOptions.from_option_string() | OutputFileOptions.from_option_string() | LoggingOptions.from_option_string()
+        return (
+            DefinitionOptions.from_option_string()
+            | OutputFileOptions.from_option_string()
+            | LoggingOptions.from_option_string()
+        )
+
 
 class GraphImportsOptions(DefinitionOptions, LoggingOptions):
     @staticmethod
     def from_option_string() -> dict[str, str]:
-        return DefinitionOptions.from_option_string() | LoggingOptions.from_option_string() 
+        return DefinitionOptions.from_option_string() | LoggingOptions.from_option_string()
 
 
 class RPCKastOptions(OutputFileOptions, LoggingOptions):
@@ -127,14 +132,20 @@ class RPCKastOptions(OutputFileOptions, LoggingOptions):
 
     @staticmethod
     def from_option_string() -> dict[str, str]:
-        return OutputFileOptions.from_option_string() | LoggingOptions.from_option_string() 
+        return OutputFileOptions.from_option_string() | LoggingOptions.from_option_string()
+
 
 class RPCPrintOptions(DefinitionOptions, OutputFileOptions, LoggingOptions):
     input_file: IO[Any]
 
     @staticmethod
     def from_option_string() -> dict[str, str]:
-        return DefinitionOptions.from_option_string() | OutputFileOptions.from_option_string() | LoggingOptions.from_option_string() 
+        return (
+            DefinitionOptions.from_option_string()
+            | OutputFileOptions.from_option_string()
+            | LoggingOptions.from_option_string()
+        )
+
 
 class PrintOptions(DefinitionOptions, OutputFileOptions, DisplayOptions, LoggingOptions):
     term: IO[Any]
@@ -153,7 +164,13 @@ class PrintOptions(DefinitionOptions, OutputFileOptions, DisplayOptions, Logging
 
     @staticmethod
     def from_option_string() -> dict[str, str]:
-        return DefinitionOptions.from_option_string() | OutputFileOptions.from_option_string() | DisplayOptions.from_option_string() | LoggingOptions.from_option_string() 
+        return (
+            DefinitionOptions.from_option_string()
+            | OutputFileOptions.from_option_string()
+            | DisplayOptions.from_option_string()
+            | LoggingOptions.from_option_string()
+        )
+
 
 class ProveLegacyOptions(DefinitionOptions, OutputFileOptions, LoggingOptions):
     main_file: Path
@@ -166,10 +183,15 @@ class ProveLegacyOptions(DefinitionOptions, OutputFileOptions, LoggingOptions):
         return {
             'k_args': [],
         }
-    
+
     @staticmethod
     def from_option_string() -> dict[str, str]:
-        return DefinitionOptions.from_option_string() | OutputFileOptions.from_option_string() | LoggingOptions.from_option_string() | {'kArgs': 'k_args'}
+        return (
+            DefinitionOptions.from_option_string()
+            | OutputFileOptions.from_option_string()
+            | LoggingOptions.from_option_string()
+            | {'kArgs': 'k_args'}
+        )
 
 
 
@@ -189,7 +211,13 @@ class KompileCommandOptions(LoggingOptions, WarningOptions, KDefinitionOptions, 
 
     @staticmethod
     def from_option_string() -> dict[str, str]:
-        return KDefinitionOptions.from_option_string() | KompileOptions.from_option_string() | LoggingOptions.from_option_string() | {'definition': 'definition_dir'}
+        return (
+            KDefinitionOptions.from_option_string()
+            | KompileOptions.from_option_string()
+            | LoggingOptions.from_option_string()
+            | {'definition': 'definition_dir'}
+        )
+
 
 class ProveOptions(LoggingOptions, SpecOptions, SaveDirOptions):
     definition_dir: Path | None
@@ -212,7 +240,12 @@ class ProveOptions(LoggingOptions, SpecOptions, SaveDirOptions):
 
     @staticmethod
     def from_option_string() -> dict[str, str]:
-        return KDefinitionOptions.from_option_string() | KompileOptions.from_option_string() | LoggingOptions.from_option_string() | {'definition': 'definition_dir'}
+        return (
+            KDefinitionOptions.from_option_string()
+            | KompileOptions.from_option_string()
+            | LoggingOptions.from_option_string()
+            | {'definition': 'definition_dir'}
+        )
 
 
 class RunOptions(LoggingOptions):
