d85ed2ed365ca28ecded067fe59ff79787b5bdcd
Tamás Tóth <tothtamas28@users.noreply.github.com>
2024-03-20 17:45:32 +0100

    Generate overload axioms  (https://github.com/runtimeverification/pyk/pull/1000)
    Co-authored-by: devops <devops@runtimeverification.com>

--- pyk/docs/conf.py
+++ pyk/docs/conf.py
@@ -9,8 +9,8 @@
 project = 'pyk'
 author = 'Runtime Verification, Inc'
 copyright = '2024, Runtime Verification, Inc'
-version = '0.1.726'
-release = '0.1.726'
+version = '0.1.727'
+release = '0.1.727'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
