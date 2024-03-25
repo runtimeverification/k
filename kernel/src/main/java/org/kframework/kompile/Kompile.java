2cde8936e0f9d59f32f2fbc9f73bc1445bef5f96
Bruce Collie <brucecollie82@gmail.com>
2024-03-25 17:16:07 +0000

    Also production sort

--- kernel/src/main/java/org/kframework/kompile/Kompile.java
+++ kernel/src/main/java/org/kframework/kompile/Kompile.java
@@ -643,15 +643,19 @@ public class Kompile {
     stream(withOverload)
         .forEach(
             p -> {
-              // When disambiguating, an extra production `Es ::= E` is added for every user list sort `Es`.
-              // This means that productions that reference a user list sort (or the child sort of one) can
-              // behave as overloads at disambiguation, even if they look like singletons here. To avoid
-              // reconstructing the entire disambiguation module for this check, we simply silence this warning
-              // when a singleton production references any sort mentioned in a `List{S, ...}` production.
+              // When disambiguating, an extra production `Es ::= E` is added for every user list
+              // sort `Es`.
+              //
+              // This means that productions that reference a user list sort (or the child sort of
+              // one) can behave as overloads at disambiguation, even if they look like singletons
+              // here. To avoid reconstructing the entire disambiguation module for this check, we
+              // simply silence this warning when a singleton production references any sort
+              // mentioned in a `List{S, ...}` production.
               //
               // See `RuleGrammarGenerator::getCombinedGrammarImpl`.
               var couldBeUserList =
-                  p.nonterminals().toStream().exists(nt -> listSorts.contains(nt.sort())) || listSorts.contains(p.sort());
+                  p.nonterminals().toStream().exists(nt -> listSorts.contains(nt.sort()))
+                      || listSorts.contains(p.sort());
               if (!module.overloads().elements().contains(p) && !couldBeUserList) {
                 kem.registerCompilerWarning(
                     KException.ExceptionType.SINGLETON_OVERLOAD,
