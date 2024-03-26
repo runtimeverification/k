c8a6b676d8358b629614ec26ea6b4ff31428114f
Virgil <25692529+virgil-serbanuta@users.noreply.github.com>
2024-03-26 20:04:22 +0200

    Allow loading single kcfg nodes (#1018)
    This is useful when, e.g., binary searching for the first node where a
    certain value occurred.
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>
    Co-authored-by: Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
    Co-authored-by: rv-jenkins <admin@runtimeverification.com>

--- pyk/docs/conf.py
+++ pyk/docs/conf.py
@@ -9,8 +9,8 @@
 project = 'pyk'
 author = 'Runtime Verification, Inc'
 copyright = '2024, Runtime Verification, Inc'
-version = '0.1.743'
-release = '0.1.743'
+version = '0.1.744'
+release = '0.1.744'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- pyk/src/pyk/kcfg/kcfg.py
+++ pyk/src/pyk/kcfg/kcfg.py
@@ -78,9 +78,12 @@ class KCFGStore:
 
     def read_cfg_data(self) -> dict[str, Any]:
         dct = json.loads(self.kcfg_json_path.read_text())
-        nodes = [json.loads(self.kcfg_node_path(node_id).read_text()) for node_id in dct.get('nodes') or []]
+        nodes = [self.read_node_data(node_id) for node_id in dct.get('nodes') or []]
         dct['nodes'] = nodes
         return dct
+
+    def read_node_data(self, node_id: int) -> dict[str, Any]:
+        return json.loads(self.kcfg_node_path(node_id).read_text())
 
 
 class KCFG(Container[Union['KCFG.Node', 'KCFG.Successor']]):
@@ -1161,6 +1164,11 @@ class KCFG(Container[Union['KCFG.Node', 'KCFG.Successor']]):
         cfg = KCFG.from_dict(store.read_cfg_data())
         cfg._kcfg_store = store
         return cfg
+
+    @staticmethod
+    def read_node_data(cfg_dir: Path, node_id: int) -> KCFG.Node:
+        store = KCFGStore(cfg_dir)
+        return KCFG.Node.from_dict(store.read_node_data(node_id))
 
 
 class KCFGExtendResult(ABC): ...
