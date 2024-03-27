315d60e855ec7e067748673ac3f791613d96fd5c
Petar Maksimović <PetarMax@users.noreply.github.com>
2024-03-27 08:27:37 +0000

    Minimizing substitution printing in `Split` nodes (#1033)
    This PR fixes the unnecessary prints of identity parts of substitutions
    in `Split` nodes.
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- pyk/docs/conf.py
+++ pyk/docs/conf.py
@@ -9,8 +9,8 @@
 project = 'pyk'
 author = 'Runtime Verification, Inc'
 copyright = '2024, Runtime Verification, Inc'
-version = '0.1.746'
-release = '0.1.746'
+version = '0.1.747'
+release = '0.1.747'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
