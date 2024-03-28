6b049f4fc9b7357e70810d1fd9a5f0173f3685be
rv-jenkins <admin@runtimeverification.com>
2024-03-28 14:25:26 -0600

    Update dependency: deps/k_release (https://github.com/runtimeverification/pyk/pull/1048)
    Co-authored-by: devops <devops@runtimeverification.com>

--- pyk/docs/conf.py
+++ pyk/docs/conf.py
@@ -9,8 +9,8 @@
 project = 'pyk'
 author = 'Runtime Verification, Inc'
 copyright = '2024, Runtime Verification, Inc'
-version = '0.1.755'
-release = '0.1.755'
+version = '0.1.756'
+release = '0.1.756'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
