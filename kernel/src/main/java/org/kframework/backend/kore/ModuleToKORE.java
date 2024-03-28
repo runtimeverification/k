f770709b75805e5683d4af24174c330ebf00ce14
Tamás Tóth <tothtamas28@users.noreply.github.com>
2024-03-28 10:22:41 +0000

    Extract method `syntaxAttributes`

--- kernel/src/main/java/org/kframework/backend/kore/ModuleToKORE.java
+++ kernel/src/main/java/org/kframework/backend/kore/ModuleToKORE.java
@@ -1686,8 +1686,16 @@ public class ModuleToKORE {
     if (isMacro) {
       att = att.add(Att.MACRO());
     }
+
+    att = att.remove(Att.FORMAT());
+    att = att.addAll(syntaxAttributes(prod));
+
+    return att;
+  }
+
+  private Att syntaxAttributes(Production prod) {
     // update format attribute with structure expected by backend
-    String format = att.getOptional(Att.FORMAT()).orElse(prod.defaultFormat());
+    String format = prod.att().getOptional(Att.FORMAT()).orElse(prod.defaultFormat());
     int nt = 1;
     boolean hasFormat = true;
 
@@ -1710,15 +1718,18 @@ public class ModuleToKORE {
         hasFormat = false;
       }
     }
+
     if (hasFormat) {
+      Att att = Att.empty();
       att = att.add(Att.FORMAT(), format);
-      if (att.contains(Att.COLOR())) {
+      if (prod.att().contains(Att.COLOR())) {
+        String color = prod.att().get(Att.COLOR());
         boolean escape = false;
         StringBuilder colors = new StringBuilder();
         String conn = "";
         for (int i = 0; i < format.length(); i++) {
           if (escape && format.charAt(i) == 'c') {
-            colors.append(conn).append(att.get(Att.COLOR()));
+            colors.append(conn).append(color);
             conn = ",";
           }
           escape = format.charAt(i) == '%';
@@ -1747,10 +1758,9 @@ public class ModuleToKORE {
                 KList.class,
                 getAssoc(module.rightAssoc(), prod.klabel().get()));
       }
-    } else {
-      att = att.remove(Att.FORMAT());
+      return att;
     }
-    return att;
+    return Att.empty();
   }
 
   private KList getPriorities(KLabel klabel) {
