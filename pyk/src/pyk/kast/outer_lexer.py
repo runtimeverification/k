5e4bfe7e650d24713de1e86e6c0f3e93921046b5
Guy Repta <50716988+gtrepta@users.noreply.github.com>
2024-04-03 13:32:10 -0500

    Don't compare locations in Token.__eq__ (for unit tests)

--- pyk/src/pyk/kast/outer_lexer.py
+++ pyk/src/pyk/kast/outer_lexer.py
@@ -96,6 +96,9 @@ class Token(NamedTuple):
     type: TokenType
     loc: Location = Location()
 
+    def __eq__(self, t: Token) -> bool:
+        return self.text == t.text and self.type == t.type
+
     def with_loc(self, loc: Location) -> Token:
         return Token(self.text, self.type, loc)
 
