b6bd18f51dc90da16ee9dbc0e8b1a2c71c832391
rv-jenkins <admin@runtimeverification.com>
2024-04-09 03:07:37 -0600

    Update dependency: deps/k_release (https://github.com/runtimeverification/pyk/pull/1070)
    Related:
    * runtimeverification/k#4146
    * runtimeverification/haskell-backend#3761
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>
    Co-authored-by: Tamás Tóth <tothtamas28@users.noreply.github.com>

--- pyk/src/tests/integration/proof/test_mini_kevm.py
+++ pyk/src/tests/integration/proof/test_mini_kevm.py
@@ -50,6 +50,8 @@ def leaf_number(proof: APRProof) -> int:
 
 class TestMiniKEVM(KCFGExploreTest, KProveTest):
     KOMPILE_MAIN_FILE = K_FILES / 'mini-kevm.k'
+    # Disabled until resolved: https://github.com/runtimeverification/haskell-backend/issues/3761
+    DISABLE_LEGACY = True
 
     @pytest.mark.parametrize(
         'test_id,spec_file,spec_module,claim_id,max_iterations,max_depth,cut_rules,proof_status,expected_leaf_number',
