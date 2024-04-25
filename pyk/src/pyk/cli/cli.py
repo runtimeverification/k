6cb5d64a0f5bd0cd3b5cdbe30e663df4aecf306e
Noah Watson <noah@nwatson.xyz>
2024-04-25 17:32:49 -0500

    Add Option and OptionsHandler classes

--- pyk/src/pyk/cli/cli.py
+++ pyk/src/pyk/cli/cli.py
@@ -1,6 +1,60 @@
 from __future__ import annotations
 
-from typing import Any
+from typing import Any, Generic, Iterable
+from abc import ABC, abstractmethod
+
+T = TypeVar('T')
+O = TypeVar('O', bound=Options)
+
+class CLI:
+    _command_handlers: list[OptionsHandler]
+
+    def __init__(self, command_handlers: Iterable[OptionsHandler] = ()) -> None:
+        self._command_handlers = list(command_handlers)
+
+    def add_command(self, command_handler: OptionsHandler) -> None:
+        self._command_handlers.append(command_handler)
+
+
+class Option(Generic[T]):
+    _name: str
+    _cmd_line_name: str
+    _toml_name: str
+    _optional: bool
+    _help_str: str | None
+    _default: T
+
+    def __init__(
+        self,
+        name: str,
+        cmd_line_name: str | None = None,
+        toml_name: str | None = None,
+        optional: bool = False,
+        help_str: str | None = None,
+        default: T | str = 'NoDefault',
+    ) -> None:
+        self._name = name,
+        self._cmd_line_name = cmd_line_name or name
+        self._toml_name = cmd_line_name or name
+        self._optional = optional
+        self._help_str = help_str
+
+        if default == 'NoDefault' and optional:
+            raise ValueError(f'Optional option {name} must define a default value.')
+        if default != 'NoDefault' and not optional:
+            raise ValueError(f'Required option {name} cannot take a default value.')
+
+        self._default = default
+
+
+class OptionsHandler(Generic[O], ABC):
+
+    def add_option(self, ) -> dict[str, Any]:
+        ...
+
+    @abstractmethod
+    def default(self) -> dict[str, Any]:
+        ...
 
 
 class Options:
