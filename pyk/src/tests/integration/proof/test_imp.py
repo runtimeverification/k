b9cc6b2583be19c2a9c6a6e898d05166b1282b7a
Petar Maksimovic <petar.maksimovic@runtimeverification.com>
2024-04-25 11:22:11 +0100

    correcting terminal checks and adding tests

--- pyk/src/tests/integration/proof/test_imp.py
+++ pyk/src/tests/integration/proof/test_imp.py
@@ -902,10 +902,10 @@ class TestImpProof(KCFGExploreTest, KProveTest):
         kcfg_explore: KCFGExplore,
         tmp_path_factory: TempPathFactory,
     ) -> None:
-        test_id: str = 'imp-simple-addition-1'
+        test_id: str = 'imp-terminal-node-subsumption'
         spec_file: Path = K_FILES / 'imp-simple-spec.k'
         spec_module: str = 'IMP-SIMPLE-SPEC'
-        claim_id: str = 'addition-1'
+        claim_id: str = 'terminal-node-subsumption'
         cut_rules: Iterable[str] = []
         with tmp_path_factory.mktemp(f'apr_tmp_proofs-{test_id}') as proof_dir:
             spec_modules = kprove.get_claim_modules(Path(spec_file), spec_module_name=spec_module)
@@ -918,10 +918,18 @@ class TestImpProof(KCFGExploreTest, KProveTest):
                 proof_dir=proof_dir,
             )
             proof = single([p for p in proofs if p.id == spec_label])
-            prover = APRProver(kcfg_explore=kcfg_explore, execute_depth=1, cut_point_rules=cut_rules)
+            prover = APRProver(kcfg_explore=kcfg_explore, execute_depth=7, cut_point_rules=cut_rules)
             prover.advance_proof(proof, max_iterations=1)
-
+            # We have reached a terminal node, but not yet checked subsumption
             assert proof.status != ProofStatus.PASSED
+
+            kcfg_show = KCFGShow(kprove, node_printer=APRProofNodePrinter(proof, kprove, full_printer=True))
+            cfg_lines = kcfg_show.show(proof.kcfg)
+            print('\n'.join(cfg_lines))
+
+            # The next advance only checks subsumption
+            prover.advance_proof(proof, max_iterations=1)
+            assert proof.status == ProofStatus.PASSED
 
     @pytest.mark.parametrize(
         'test_id,spec_file,spec_module,claim_id,max_iterations,max_depth,terminal_rules,cut_rules,expected_constraint',
