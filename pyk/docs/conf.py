c885ed22e2357cf9855f9a8df0b64683d601b980
devops <devops@runtimeverification.com>
2024-05-01 10:03:02 +0000

    Set Version: 7.0.32

--- pyk/docs/conf.py
+++ pyk/docs/conf.py
@@ -9,8 +9,8 @@
 project = 'pyk'
 author = 'Runtime Verification, Inc'
 copyright = '2024, Runtime Verification, Inc'
-version = '7.0.31'
-release = '7.0.31'
+version = '7.0.32'
+release = '7.0.32'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
