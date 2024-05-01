32167b5fd25562f375b29d34eae8632fd0fbe1fe
Bruce Collie <brucecollie82@gmail.com>
2024-05-01 14:09:23 +0100

    Update haskell-backend/src/main/java/org/kframework/backend/haskell/HaskellKompileOptions.java
    Co-authored-by: Samuel Balco <goodlyrottenapple@gmail.com>

--- haskell-backend/src/main/java/org/kframework/backend/haskell/HaskellKompileOptions.java
+++ haskell-backend/src/main/java/org/kframework/backend/haskell/HaskellKompileOptions.java
@@ -22,7 +22,7 @@ public class HaskellKompileOptions {
       names = "--no-haskell-binary",
       description =
           "Use the textual KORE format in the haskell backend instead of the binary KORE format."
-              + " This flag is enabled by default and cannot be disabled when running on macOS because of a bug in the underlying Haskell libraries.",
+              + " This flag is disabled by default, except when running on macOS, where it is enabled and cannot be disabled because of a bug in the underlying Haskell libraries.",
       hidden = true)
   public boolean noHaskellBinary = false;
 }
