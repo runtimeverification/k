8fc285bda7b2f058c4cc7488292e1f1feaf9f3e0
Bruce Collie <brucecollie82@gmail.com>
2024-05-02 09:22:06 +0100

    Use @contextmanager

--- pyk/src/pyk/kllvm/importer.py
+++ pyk/src/pyk/kllvm/importer.py
@@ -2,9 +2,10 @@ from __future__ import annotations
 
 import os
 import sys
+from contextlib import contextmanager
 from importlib.util import module_from_spec, spec_from_file_location
 from pathlib import Path
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING, Iterator
 
 from ..cli.utils import check_dir_path, check_file_path
 from .compiler import KLLVM_MODULE_FILE_NAME, KLLVM_MODULE_NAME, RUNTIME_MODULE_FILE_NAME, RUNTIME_MODULE_NAME
@@ -14,15 +15,12 @@ if TYPE_CHECKING:
     from types import ModuleType
 
 
-class RTLDLocal:
-    def __init__(self) -> None:
-        self.old_flags = sys.getdlopenflags()
-
-    def __enter__(self) -> None:
-        sys.setdlopenflags(self.old_flags | os.RTLD_LOCAL)
-
-    def __exit__(self, *args: Any) -> None:
-        sys.setdlopenflags(self.old_flags)
+@contextmanager
+def rtld_local() -> Iterator[None]:
+    old_flags = sys.getdlopenflags()
+    sys.setdlopenflags(old_flags | os.RTLD_LOCAL)
+    yield
+    sys.setdlopenflags(old_flags)
 
 
 def import_from_file(module_name: str, module_file: str | Path) -> ModuleType:
@@ -57,7 +55,7 @@ def import_runtime(target_dir: str | Path) -> Runtime:
     check_dir_path(target_dir)
     module_file = target_dir / RUNTIME_MODULE_FILE_NAME
 
-    with RTLDLocal():
+    with rtld_local():
         module = import_from_file(RUNTIME_MODULE_NAME, module_file)
 
     return Runtime(module)
