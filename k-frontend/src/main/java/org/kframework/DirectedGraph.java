412c0ee7514d7b4c20a65b804bcc2e84d0a1e801
Scott Guest <scott.guest@runtimeverification.com>
2024-05-16 15:49:40 -0400

    Implement cycle-finding utilities in DirectedGraph

--- k-frontend/src/main/java/org/kframework/DirectedGraph.java
+++ k-frontend/src/main/java/org/kframework/DirectedGraph.java
@@ -1,6 +1,9 @@
 // Copyright (c) Runtime Verification, Inc. All Rights Reserved.
 package org.kframework;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -46,5 +49,80 @@ public class DirectedGraph {
       entry.getValue().removeAll(found);
     }
     return topologicalSort(hasPreds, Stream.concat(done, found.stream()));
+  }
+
+  /**
+   * Find an inclusion-maximal set of mutually vertex-disjoint cycles. Note that the particular
+   * cycles found depends on the iteration order of the input adjacency matrix.
+   */
+  public static <T> Stream<List<T>> findDisjointCycles(Map<T, Set<T>> deps) {
+    Optional<List<T>> cycle = findCycle(deps);
+    if (cycle.isEmpty()) {
+      return Stream.empty();
+    }
+    Set<T> inCycle = new HashSet<>(cycle.get());
+    Map<T, Set<T>> depsMinusCycle =
+        deps.entrySet().stream()
+            .filter(e -> !inCycle.contains(e.getKey()))
+            .collect(
+                Collectors.toMap(
+                    Map.Entry::getKey,
+                    e ->
+                        e.getValue().stream()
+                            .filter(t -> !inCycle.contains(t))
+                            .collect(Collectors.toSet())));
+    return Stream.concat(Stream.of(cycle.get()), findDisjointCycles(depsMinusCycle));
+  }
+
+  /**
+   * Find a cycle in the graph if one exists. Note that the particular cycle found depends on the
+   * iteration order of the provided adjacency matrix.
+   */
+  public static <T> Optional<List<T>> findCycle(Map<T, Set<T>> adjacency) {
+    Set<T> visited = new HashSet<>();
+    Set<T> onStack = new HashSet<>();
+    Map<T, T> edgeTo = new HashMap<>();
+    for (T elem : adjacency.keySet()) {
+      if (!visited.contains(elem)) {
+        Optional<List<T>> cycle = findCycle(adjacency, visited, onStack, edgeTo, elem);
+        if (cycle.isPresent()) {
+          return cycle;
+        }
+      }
+    }
+    return Optional.empty();
+  }
+
+  private static <T> Optional<List<T>> findCycle(
+      Map<T, Set<T>> deps, Set<T> visited, Set<T> onStack, Map<T, T> edgeTo, T start) {
+    visited.add(start);
+    onStack.add(start);
+    for (T next : deps.getOrDefault(start, Set.of())) {
+      if (!visited.contains(next)) {
+        edgeTo.put(next, start);
+        Optional<List<T>> cycle = findCycle(deps, visited, onStack, edgeTo, next);
+        if (cycle.isPresent()) {
+          return cycle;
+        }
+        continue;
+      }
+      if (onStack.contains(next)) {
+        List<T> cycle = new ArrayList<>();
+        for (T elem = start; !elem.equals(next); elem = edgeTo.get(elem)) {
+          cycle.add(elem);
+        }
+        cycle.add(next);
+        Collections.reverse(cycle);
+        return Optional.of(cycle);
+      }
+    }
+    onStack.remove(start);
+    return Optional.empty();
+  }
+
+  /** Rotate a cycle so that the least element is first. */
+  public static <T> List<T> sortCycle(List<T> cycle, Comparator<T> comp) {
+    Collections.rotate(cycle, -(cycle.indexOf(Collections.min(cycle, comp))));
+    return cycle;
   }
 }
