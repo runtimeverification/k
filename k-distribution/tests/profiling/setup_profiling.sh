6e5f03c76dbca885ecfbf3610efa1a91da87d843
Bruce Collie <brucecollie82@gmail.com>
2024-06-05 11:10:15 +0100

    Move docker run step to separate stage

--- k-distribution/tests/profiling/setup_profiling.sh
+++ k-distribution/tests/profiling/setup_profiling.sh
@@ -7,10 +7,6 @@ export BENCHER_VERSION="0.3.23"
 apt-get update
 apt-get upgrade --yes
 apt-get install --yes python3 python3-pip git curl make time sudo wget
-# Build K if no flag was given and don't execute if flag was given
-if [ $# -eq 0 ]; then
-    apt-get install --yes ./kframework.deb
-fi
 python3 -m pip install python-slugify
 
 wget https://github.com/bencherdev/bencher/releases/download/v"${BENCHER_VERSION}"/bencher_"${BENCHER_VERSION}"_amd64.deb
