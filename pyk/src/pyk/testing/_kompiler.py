4997f90377d308331f91cf9df31d52d3aad81648
Roberto Rosmaninho <robertogomes@dcc.ufmg.br>
2024-06-06 17:42:44 -0300

    Fixing import issue

--- pyk/src/pyk/testing/_kompiler.py
+++ pyk/src/pyk/testing/_kompiler.py
@@ -14,7 +14,6 @@ from ..cterm import CTermSymbolic
 from ..kast.outer import read_kast_definition
 from ..kcfg import KCFGExplore
 from ..kllvm.compiler import compile_runtime, generate_hints
-from ..kllvm.hints.prooftrace import KoreHeader
 from ..kllvm.importer import import_runtime
 from ..kore.kompiled import KompiledKore
 from ..kore.pool import KoreServerPool
@@ -34,6 +33,7 @@ if TYPE_CHECKING:
 
     from ..kast.outer import KDefinition
     from ..kcfg.semantics import KCFGSemantics
+    from ..kllvm.hints.prooftrace import KoreHeader
     from ..kllvm.runtime import Runtime
     from ..ktool.kprint import SymbolTable
     from ..utils import BugReport
