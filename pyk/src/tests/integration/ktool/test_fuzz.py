fa43d4431d23824a52c583d95080e0b6c1a88283
Guy Repta <50716988+gtrepta@users.noreply.github.com>
2024-06-18 18:59:00 -0500

    Test the fuzzing

--- pyk/src/tests/integration/ktool/test_fuzz.py
+++ pyk/src/tests/integration/ktool/test_fuzz.py
@@ -1,37 +1,81 @@
 from __future__ import annotations
 
 import logging
-from contextlib import nullcontext
 from pathlib import Path
 from typing import TYPE_CHECKING
 
 import pytest
 
-from pyk.cli.pyk import ProveOptions
-from pyk.kast.inner import KApply, KSequence, KVariable
-from pyk.kcfg.semantics import KCFGSemantics
-from pyk.ktool.kprove import ProveRpc
-from pyk.proof import ProofStatus
-from pyk.testing import KCFGExploreTest, KompiledTest, KProveTest
-from pyk.utils import single
+from pyk.kast.inner import KSort
+from pyk.kore.parser import KoreParser
+from pyk.kore.syntax import App, EVar, SortApp
+from pyk.ktool.krun import fuzz, kintegers
+from pyk.testing import KompiledTest
 
-from ..utils import K_FILES
+from ..utils import K_FILES, TEST_DATA_DIR
 
 if TYPE_CHECKING:
-    from collections.abc import Iterable
+    from collections.abc import Callable
     from typing import Final
 
-    from pyk.cterm import CTerm
-    from pyk.kast.outer import KDefinition
-    from pyk.kcfg import KCFGExplore
-    from pyk.kcfg.kcfg import KCFGExtendResult
-    from pyk.ktool.kprove import KProve
+    from hypothesis.strategies import SearchStrategy
+
+    from pyk.kore.syntax import Pattern
 
 _LOGGER: Final = logging.getLogger(__name__)
+
+FUZZ_FILES: Path = TEST_DATA_DIR / 'fuzzing'
+
 
 class TestImpFuzz(KompiledTest):
     KOMPILE_MAIN_FILE = K_FILES / 'imp.k'
     KOMPILE_BACKEND = 'llvm'
 
-    def test_fuzz(definition_dir: Path):
-        print(str(definition_dir))
+    @pytest.fixture
+    def template_config(self, request) -> Pattern:
+        if request.function.__name__ == 'test_fuzz':
+            return KoreParser(Path(FUZZ_FILES / 'imp_comm_config.kore').read_text()).pattern()
+        elif request.function.__name__ == 'test_fuzz_fail':
+            return KoreParser(Path(FUZZ_FILES / 'imp_lte_config.kore').read_text()).pattern()
+
+    @pytest.fixture
+    def substs(self) -> dict[EVar, SearchStrategy[Pattern]]:
+        var_x = EVar(name='VarX', sort=SortApp('SortInt'))
+        var_y = EVar(name='VarY', sort=SortApp('SortInt'))
+        return {var_x: kintegers(with_inj=KSort('AExp')), var_y: kintegers(with_inj=KSort('AExp'))}
+
+    @pytest.fixture
+    def check_func(self) -> Callable[[Pattern], None]:
+
+        lbl = "Lbl'UndsPipe'-'-GT-Unds'"
+
+        def checkres(p: Pattern) -> Pattern:
+            if isinstance(p, App):
+                if p.symbol == lbl:
+                    left = p.args[0]
+                    right = p.args[1]
+                    if left.args[0].value.value == 'res':  # type: ignore[attr-defined]
+                        val = int(right.args[0].value.value)  # type: ignore[attr-defined]
+                        assert val == 0
+            return p
+
+        return lambda pattern: pattern.args[0].args[1].args[0].pattern.top_down(checkres)  # type: ignore[attr-defined]
+
+    def test_fuzz(
+        self,
+        definition_dir: Path,
+        template_config: Pattern,
+        substs: dict[EVar, SearchStrategy[Pattern]],
+        check_func: Callable[[Pattern], None],
+    ) -> None:
+        fuzz(definition_dir, template_config, substs, check_func)
+
+    def test_fuzz_fail(
+        self,
+        definition_dir: Path,
+        template_config: Pattern,
+        substs: dict[EVar, SearchStrategy[Pattern]],
+        check_func: Callable[[Pattern], None],
+    ) -> None:
+        with pytest.raises(AssertionError):
+            fuzz(definition_dir, template_config, substs, check_func)
