fd388977a597b9c87a1e683e8142d7d137c37295
rv-jenkins <devops@runtimeverification.com>
2024-07-09 12:36:13 +0000

    flake.nix, haskell-backend/src/main/native/haskell-backend: update to version v0.1.31

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "K Framework";
   inputs = {
-    haskell-backend.url = "github:runtimeverification/haskell-backend/v0.1.30";
+    haskell-backend.url = "github:runtimeverification/haskell-backend/v0.1.31";
     nixpkgs.follows = "llvm-backend/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
     llvm-backend = {
