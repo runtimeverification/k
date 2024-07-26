f96a1dcaf9380b018119634a837d059c45567100
rv-jenkins <devops@runtimeverification.com>
2024-07-26 18:00:56 +0000

    flake.nix, haskell-backend/src/main/native/haskell-backend: update to version v0.1.50

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs = {
     llvm-backend.url = "github:runtimeverification/llvm-backend/v0.1.64";
     haskell-backend = {
-      url = "github:runtimeverification/haskell-backend/v0.1.49";
+      url = "github:runtimeverification/haskell-backend/v0.1.50";
       inputs.rv-utils.follows = "llvm-backend/rv-utils";
       inputs.nixpkgs.follows = "llvm-backend/nixpkgs";
     };
