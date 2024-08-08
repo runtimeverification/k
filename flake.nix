2e97030646138ff3c13f79b0699d242007af82a5
rv-jenkins <devops@runtimeverification.com>
2024-08-08 01:47:58 +0000

    flake.nix, llvm-backend/src/main/native/llvm-backend: update to version v0.1.76

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "K Framework";
   inputs = {
-    llvm-backend.url = "github:runtimeverification/llvm-backend/v0.1.75";
+    llvm-backend.url = "github:runtimeverification/llvm-backend/v0.1.76";
     haskell-backend = {
       url = "github:runtimeverification/haskell-backend/v0.1.58";
       inputs.rv-utils.follows = "llvm-backend/rv-utils";
