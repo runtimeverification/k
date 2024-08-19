c8738d6694434c6adcbce898629deacc47fc14b5
Petar Maksimović <PetarMax@users.noreply.github.com>
2024-08-19 15:57:47 +0200

    Reusing backend information to construct KCFGs more efficiently (#4590)
    This PR leverages information provided by the backend as part of an
    `execute` request to construct more than one KCFG element: specifically,
    vacuous nodes, stuck nodes, edges resulting from cut-nodes, branches,
    and non-deterministic branches.
    
    In addition, it moves the associated printing to the KCFG class,
    allowing for more informative prints: for example, we can now print the
    target nodes of edges and branches, the absence of which (personally)
    was making debugging of large proofs slightly more difficult.
    
    The PR also removes the `always_check_subsumption` flag, which was not
    used and was made obsolete by the subsumption logic in `step_proof`.
    
    ---------
    
    Co-authored-by: Tamás Tóth <tothtamas28@users.noreply.github.com>

--- pyk/src/pyk/kcfg/semantics.py
+++ pyk/src/pyk/kcfg/semantics.py
@@ -12,14 +12,27 @@ class KCFGSemantics(ABC):
     @abstractmethod
     def is_terminal(self, c: CTerm) -> bool: ...
 
+    """Check whether or not a given ``CTerm`` is terminal."""
+
     @abstractmethod
     def abstract_node(self, c: CTerm) -> CTerm: ...
+
+    """Implement an abstraction mechanism."""
 
     @abstractmethod
     def same_loop(self, c1: CTerm, c2: CTerm) -> bool: ...
 
+    """Check whether or not the two given ``CTerm``s represent the same loop head."""
+
+    @abstractmethod
+    def can_make_custom_step(self, c: CTerm) -> bool: ...
+
+    """Check whether or not the semantics can make a custom step from a given ``CTerm``."""
+
     @abstractmethod
     def custom_step(self, c: CTerm) -> KCFGExtendResult | None: ...
+
+    """Implement a custom semantic step."""
 
 
 class DefaultSemantics(KCFGSemantics):
@@ -30,6 +43,9 @@ class DefaultSemantics(KCFGSemantics):
         return c
 
     def same_loop(self, c1: CTerm, c2: CTerm) -> bool:
+        return False
+
+    def can_make_custom_step(self, c: CTerm) -> bool:
         return False
 
     def custom_step(self, c: CTerm) -> KCFGExtendResult | None:
