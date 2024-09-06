931c800e2779f84b0fb91f4ffb873e0070ae501a
Tamás Tóth <tothtamas28@users.noreply.github.com>
2024-09-06 09:35:38 +0000

    Add LLVM tests

--- pyk/src/tests/integration/test_division_hooks.py
+++ pyk/src/tests/integration/test_division_hooks.py
@@ -4,15 +4,31 @@ from typing import TYPE_CHECKING
 
 import pytest
 
-from pyk.kore.prelude import BOOL, INT, TRUE, eq_int, int_dv
+from pyk.kore.prelude import (
+    BOOL,
+    INT,
+    SORT_K_ITEM,
+    TRUE,
+    eq_int,
+    generated_counter,
+    generated_top,
+    inj,
+    int_dv,
+    k,
+    kseq,
+)
 from pyk.kore.rpc import SatResult
 from pyk.kore.syntax import App, Equals, EVar
-from pyk.testing import KoreClientTest
+from pyk.ktool.krun import llvm_interpret
+from pyk.testing import KompiledTest, KoreClientTest
 
 if TYPE_CHECKING:
+    from pathlib import Path
     from typing import Final
 
     from pyk.kore.rpc import KoreClient
+    from pyk.kore.syntax import Pattern
+
 
 T_DIVISION_TEST_DATA: Final = (
     # Op   ,  a,  b, a Op b
@@ -45,6 +61,44 @@ T_DIVISION_TEST_DATA: Final = (
 )
 
 kore_for = {'/Int': "Lbl'UndsSlsh'Int'Unds'", '%Int': "Lbl'UndsPerc'Int'Unds'"}
+
+
+class TestDivisionHooksLlvm(KompiledTest):
+    KOMPILE_DEFINITION = """
+        module SMT
+            imports INT
+        endmodule
+    """
+    KOMPILE_MAIN_MODULE = 'SMT'
+    KOMPILE_ARGS = {'syntax_module': 'SMT'}
+
+    @pytest.mark.parametrize(
+        'op, a, b, c',
+        T_DIVISION_TEST_DATA,
+        ids=[f'{a} {op} {b} == {c}' for op, a, b, c in T_DIVISION_TEST_DATA],
+    )
+    def test(self, definition_dir: Path, op: str, a: int, b: int, c: int) -> None:
+        # Given
+        def config(pgm: Pattern) -> Pattern:
+            return generated_top(
+                (
+                    k(
+                        kseq(
+                            (inj(INT, SORT_K_ITEM, pgm),),
+                        )
+                    ),
+                    generated_counter(int_dv(0)),
+                ),
+            )
+
+        pattern = config(App(kore_for[op], (), (int_dv(a), int_dv(b))))
+        expected = config(int_dv(c))
+
+        # When
+        actual = llvm_interpret(definition_dir, pattern)
+
+        # Then
+        assert actual == expected
 
 
 class TestDivisionHooksHs(KoreClientTest):
