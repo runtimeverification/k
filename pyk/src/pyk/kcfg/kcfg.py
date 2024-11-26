efef04d6867a1c80bb906edabe390de190071539
Stevengre <zhaojianhong96@gmail.com>
2024-11-26 11:35:43 +0800

    provide a silent way to remove nodes in KCFG

--- pyk/src/pyk/kcfg/kcfg.py
+++ pyk/src/pyk/kcfg/kcfg.py
@@ -758,11 +758,15 @@ class KCFG(Container[Union['KCFG.Node', 'KCFG.Successor']]):
         return node
 
     def remove_node(self, node_id: NodeIdLike) -> None:
+        self.remove_node_silent(node_id)
+        node_id = self._resolve(node_id)
+        self._nodes.pop(node_id)
+
+    def remove_node_silent(self, node_id: NodeIdLike) -> None:
         node_id = self._resolve(node_id)
 
-        node = self._nodes.pop(node_id)
         self._created_nodes.discard(node_id)
-        self._deleted_nodes.add(node.id)
+        self._deleted_nodes.add(node_id)
 
         self._edges = {k: s for k, s in self._edges.items() if k != node_id and node_id not in s.target_ids}
         self._merged_edges = {
