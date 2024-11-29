e6fc73bb31865b4d3fc28945dc2abb8265064b10
Tamás Tóth <tothtamas28@users.noreply.github.com>
2024-11-29 15:26:37 +0000

    Fix pattern order

--- pyk/src/pyk/kore/rule.py
+++ pyk/src/pyk/kore/rule.py
@@ -246,12 +246,12 @@ class SimpliRule(Rule):
         # Cases 11-12 of get_left_hand_side
         # Case 0 of get_right_hand_side
         match axiom.pattern:
+            case Implies(right=Equals(left=Ceil())):
+                raise ValueError(f'Axiom is a ceil rule: {axiom.text}')
             case Implies(left=Top(), right=Equals(left=lhs, right=And(ops=(rhs, Top() | Equals() as _ens)))):
                 pass
             case Implies(left=Equals(left=req), right=Equals(left=lhs, right=And(ops=(rhs, Top() | Equals() as _ens)))):
                 pass
-            case Implies(right=Equals(left=Ceil())):
-                raise ValueError(f'Axiom is a ceil rule: {axiom.text}')
             case _:
                 raise ValueError(f'Cannot extract simplification rule from axiom: {axiom.text}')
         ens = _extract_ensures(_ens)
