d71f6f6c5524bb0b0dae8a419e5049601e6f7a0e
Tamás Tóth <tothtamas28@users.noreply.github.com>
2024-12-02 13:07:26 +0000

    Adjust test

--- pyk/src/tests/integration/kore/test_rule.py
+++ pyk/src/tests/integration/kore/test_rule.py
@@ -18,7 +18,7 @@ if TYPE_CHECKING:
 @pytest.fixture(scope='module')
 def definition(kompile: Kompiler) -> Definition:
     main_file = K_FILES / 'imp.k'
-    definition_dir = kompile(main_file=main_file)
+    definition_dir = kompile(main_file=main_file, backend='haskell')
     kore_file = definition_dir / 'definition.kore'
     kore_text = kore_file.read_text()
     definition = KoreParser(kore_text).definition()
@@ -33,4 +33,6 @@ def test_extract_all(definition: Definition) -> None:
     cnt = Counter(type(rule).__name__ for rule in rules)
     assert cnt['RewriteRule']
     assert cnt['FunctionRule']
-    assert cnt['SimpliRule']
+    assert cnt['AppRule']
+    assert cnt['CeilRule']
+    assert cnt['EqualsRule']
