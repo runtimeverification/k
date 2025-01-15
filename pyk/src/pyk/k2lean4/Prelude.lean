efc52fa5e8d789e1a5ca714c6a39dc2a59440bc4
Tamás Tóth <tothtamas28@users.noreply.github.com>
2025-01-15 13:21:11 +0100

    Add type class `Inj` to the prelude
    Co-authored-by: Juan C. <38925412+JuanCoRo@users.noreply.github.com>

--- pyk/src/pyk/k2lean4/Prelude.lean
+++ pyk/src/pyk/k2lean4/Prelude.lean
@@ -4,3 +4,8 @@ abbrev SortId : Type := String
 abbrev SortInt : Type := Int
 abbrev SortString : Type := String
 abbrev SortStringBuffer : Type := String
+
+class Inj (From To : Type) : Type where
+  inj (x : From) : To
+
+def inj {From To : Type} [inst : Inj From To] := inst.inj
