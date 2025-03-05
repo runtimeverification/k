da5a5dc87fd64e05d735eb74d66e40adc6e28ef6
rv-jenkins <admin@runtimeverification.com>
2025-03-05 02:27:38 -0700

    Update dependency: deps/haskell-backend_release (#4763)
    Co-authored-by: devops <devops@runtimeverification.com>

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs = {
     llvm-backend.url = "github:runtimeverification/llvm-backend/v0.1.126";
     haskell-backend = {
-      url = "github:runtimeverification/haskell-backend/v0.1.113";
+      url = "github:runtimeverification/haskell-backend/v0.1.115";
       inputs.rv-utils.follows = "llvm-backend/rv-utils";
       inputs.nixpkgs.follows = "llvm-backend/nixpkgs";
     };
