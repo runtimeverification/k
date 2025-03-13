248a49f1e20f9f8e4604debb8b38898e695587b0
Juan C <juanconrod@protonmail.com>
2025-03-13 16:22:15 +0100

    `Prelude.lean`: embed implementation into the hooks

--- pyk/src/pyk/klean/template/{{ cookiecutter.package_name }}/{{ cookiecutter.library_name }}/Prelude.lean
+++ pyk/src/pyk/klean/template/{{ cookiecutter.package_name }}/{{ cookiecutter.library_name }}/Prelude.lean
@@ -36,52 +36,49 @@ Note that both keys and values will always be KItems.
 
 -- Signature to be instantiated by map implementations
 structure MapHookSig (K V : Type) where
-  map        : Type -- Carrier, such as List (KItem × KItem)
-  unit       : map
-  cons       : K → V → map → map
-  lookup     : map → K → Option V
-  lookup?    : map → K → V -- lookup with default
-  update     : K → V → map → map
-  delete     : map → K → map
-  concat     : map → map → Option map
-  difference : map → map → map
-  updateMap  : map → map → map
-  removeAll  : map → List K → map
-  keys       : map → List K
-  in_keys    : map → K → Bool
-  values     : map → List V
-  size       : map → Nat
-  includes   : map → map → Bool -- map inclusion
-  choice     : map → K -- arbitrary key from a map
-  nodup      : forall al : map, List.Nodup (keys al)
+  unit       : List (K × V)
+  cons       : K → V → List (K × V) → List (K × V)
+  lookup     : List (K × V) → K → Option V
+  lookup?    : List (K × V) → K → V -- lookup with default
+  update     : K → V → List (K × V) → List (K × V)
+  delete     : List (K × V) → K → List (K × V)
+  concat     : List (K × V) → List (K × V) → Option (List (K × V))
+  difference : List (K × V) → List (K × V) → List (K × V)
+  updateMap  : List (K × V) → List (K × V) → List (K × V)
+  removeAll  : List (K × V) → List K → List (K × V)
+  keys       : List (K × V) → List K
+  in_keys    : List (K × V) → K → Bool
+  values     : List (K × V) → List V
+  size       : List (K × V) → Nat
+  includes   : List (K × V) → List (K × V) → Bool -- List (K × V) inclusion
+  choice     : List (K × V) → K -- arbitrary key from a List (K × V)
+  nodup      : forall al : List (K × V), List.Nodup (keys al)
 
 -- We use axioms to have uninterpreted functions
 namespace MapHookDef
   variable (K V : Type)
-  def mapCAx        := List (K × V) -- Map Carrier
-  axiom unitAx       : mapCAx K V
-  axiom consAx       : K → V → mapCAx K V → mapCAx K V
-  axiom lookupAx     : mapCAx K V → K → Option V
-  axiom lookupAx?    : mapCAx K V → K → V -- lookup with default
-  axiom updateAx     : K → V → mapCAx K V → mapCAx K V
-  axiom deleteAx     : mapCAx K V → K → mapCAx K V
-  axiom concatAx     : mapCAx K V → mapCAx K V → Option (mapCAx K V)
-  axiom differenceAx : mapCAx K V → mapCAx K V → mapCAx K V
-  axiom updateMapAx  : mapCAx K V → mapCAx K V → mapCAx K V
-  axiom removeAllAx  : mapCAx K V → List K → mapCAx K V
-  axiom keysAx       : mapCAx K V → List K
-  axiom in_keysAx    : mapCAx K V → K → Bool
-  axiom valuesAx     : mapCAx K V → List V
-  axiom sizeAx       : mapCAx K V → Nat
-  axiom includesAx   : mapCAx K V → mapCAx K V → Bool -- map inclusion
-  axiom choiceAx     : mapCAx K V → K -- arbitrary key from a map
+  axiom unitAx       : List (K × V)
+  axiom consAx       : K → V → List (K × V) → List (K × V)
+  axiom lookupAx     : List (K × V) → K → Option V
+  axiom lookupAx?    : List (K × V) → K → V -- lookup with default
+  axiom updateAx     : K → V → List (K × V) → List (K × V)
+  axiom deleteAx     : List (K × V) → K → List (K × V)
+  axiom concatAx     : List (K × V) → List (K × V) → Option (List (K × V))
+  axiom differenceAx : List (K × V) → List (K × V) → List (K × V)
+  axiom updateMapAx  : List (K × V) → List (K × V) → List (K × V)
+  axiom removeAllAx  : List (K × V) → List K → List (K × V)
+  axiom keysAx       : List (K × V) → List K
+  axiom in_keysAx    : List (K × V) → K → Bool
+  axiom valuesAx     : List (K × V) → List V
+  axiom sizeAx       : List (K × V) → Nat
+  axiom includesAx   : List (K × V) → List (K × V) → Bool -- map inclusion
+  axiom choiceAx     : List (K × V) → K -- arbitrary key from a map
   axiom nodupAx      : forall m, List.Nodup (keysAx K V m)
 end MapHookDef
 
 -- Uninterpreted Map implementation
 noncomputable def MapHook (K V : Type) : MapHookSig K V :=
-  { map        := MapHookDef.mapCAx K V,
-    unit       := MapHookDef.unitAx K V,
+  { unit       := MapHookDef.unitAx K V,
     cons       := MapHookDef.consAx K V,
     lookup     := MapHookDef.lookupAx K V,
     lookup?    := MapHookDef.lookupAx? K V,
@@ -98,10 +95,10 @@ noncomputable def MapHook (K V : Type) : MapHookSig K V :=
     includes   := MapHookDef.includesAx K V,
     choice     := MapHookDef.choiceAx K V,
     nodup      := MapHookDef.nodupAx K V}
-
-instance {K V : Type} [BEq K] [BEq V]: BEq (MapHook K V).map where
+/-
+instance {K V : Type} [BEq K] [BEq V]: BEq (List (K × V)) where
   beq := List.beq
-
+ -/
 /-
 Implementation of immutable, associative, commutative sets of `KItem`.
 The sets are nilpotent, i.e., the concatenation of two sets containing elements in common is `#False` (note however, this may be silently allowed during concrete execution).
@@ -109,36 +106,33 @@ If you intend to add an element to a set that might already be present in the se
  -/
 
 structure SetHookSig (T : Type) where
-  set          : Type -- Carrier, such as `T → Prop`
-  unit         : set
-  concat       : set → set → Option set
-  element      : T → set
-  union        : set → set → set
-  intersection : set → set → set
-  difference   : set → set → set
-  inSet        : T → set → Bool
-  inclusion    : set → set → Bool
-  size         : set → Int
-  choice       : set → T
+  unit         : List T
+  concat       : List T → List T → Option (List T)
+  element      : T → List T
+  union        : List T → List T → List T
+  intersection : List T → List T → List T
+  difference   : List T → List T → List T
+  inSet        : T → List T → Bool
+  inclusion    : List T → List T → Bool
+  size         : List T → Int
+  choice       : List T → T
 
 namespace SetHookDef
   variable (T : Type)
-  def setCAx          := List T
-  axiom unitAx         : setCAx T
-  axiom concatAx       : setCAx T → setCAx T → Option (setCAx T)
-  axiom elementAx      : T → setCAx T
-  axiom unionAx        : setCAx T → setCAx T → setCAx T
-  axiom intersectionAx : setCAx T → setCAx T → setCAx T
-  axiom differenceAx   : setCAx T → setCAx T → setCAx T
-  axiom inSetAx        : T → setCAx T → Bool
-  axiom inclusionAx    : setCAx T → setCAx T → Bool
-  axiom sizeAx         : setCAx T → Int
-  axiom choiceAx       : setCAx T → T
+  axiom unitAx         : List T
+  axiom concatAx       : List T → List T → Option (List T)
+  axiom elementAx      : T → List T
+  axiom unionAx        : List T → List T → List T
+  axiom intersectionAx : List T → List T → List T
+  axiom differenceAx   : List T → List T → List T
+  axiom inSetAx        : T → List T → Bool
+  axiom inclusionAx    : List T → List T → Bool
+  axiom sizeAx         : List T → Int
+  axiom choiceAx       : List T → T
 end SetHookDef
 
 noncomputable def SetHook (T : Type) : SetHookSig T :=
-  { set          := SetHookDef.setCAx T,
-    unit         := SetHookDef.unitAx T,
+  { unit         := SetHookDef.unitAx T,
     concat       := SetHookDef.concatAx T,
     element      := SetHookDef.elementAx T,
     union        := SetHookDef.unionAx T,
@@ -154,49 +148,46 @@ The `List` sort is an ordered collection that may contain duplicate elements.
  -/
 
 structure listHookSig (T : Type) where
-  list      : Type -- Carrier, such as `T → Prop`
-  unit      : list
-  concat    : list → list → Option list
-  element   : T → list
-  push      : T → list → list
-  get       : Int → list → Option T
-  updte     : Int → T → list → Option list
-  -- create a list with `length` elements, each containing  `value`. `Option` return type from `Int` parameter
-  make      : Int → T → Option list
-  -- create a new `List` which is equal to `dest` except the  `N` elements starting at `index` are replaced with the   contents of `src`
+  unit      : List T
+  concat    : List T → List T → Option (List T)
+  element   : T → List T
+  push      : T → List T → List T
+  get       : Int → List T → Option T
+  updte     : Int → T → List T → Option (List T)
+  -- create a List T with `length` elements, each containing  `value`. `Option` return type from `Int` parameter
+  make      : Int → T → Option (List T)
+  -- create a new `List T` which is equal to `dest` except the  `N` elements starting at `index` are replaced with the   contents of `src`
   -- Having `index + size(src) > size(dest)` is undefined
-  -- updateList(dest: List, index: Int, src: List)
-  updateAll : list → Int → list → Option list
-  -- create a new `List` where the `length` elements starting   at `index` are replaced with `value`
-  fill      : list → Int → T → Option list
-  -- compute a new `List` by removing `fromFront` elements from   the front of the list and `fromBack` elements from the back   of the list
-  -- range(List, fromFront: Int, fromBack: Int)
-  range     : list → Int → Int → Option list
-  -- compute whether an element is in a list
+  -- updateList(dest: List T, index: Int, src: List T)
+  updateAll : List T → Int → List T → Option (List T)
+  -- create a new `List T` where the `length` elements starting   at `index` are replaced with `value`
+  fill      : List T → Int → T → Option (List T)
+  -- compute a new `List T` by removing `fromFront` elements from   the front of the list and `fromBack` elements from the back   of the List T
+  -- range(List T, fromFront: Int, fromBack: Int)
+  range     : List T → Int → Int → Option (List T)
+  -- compute whether an element is in a List T
   -- the hook is `in`, but clashes with Lean syntax
-  inList    : T → list → Bool
-  size      : list → Int
+  inList    : T → List T → Bool
+  size      : List T → Int
 
 namespace ListHookDef
   variable (T : Type)
-  def listCAx       := List T
-  axiom unitAx      : listCAx T
-  axiom concatAx    : listCAx T → listCAx T → Option (listCAx T)
-  axiom elementAx   : T → listCAx T
-  axiom pushAx      : T → listCAx T → listCAx T
-  axiom getAx       : Int → listCAx T → Option T
-  axiom updteAx     : Int → T → listCAx T → Option (listCAx T)
-  axiom makeAx      : Int → T → Option (listCAx T)
-  axiom updateAllAx : listCAx T → Int → listCAx T → Option (listCAx T)
-  axiom fillAx      : listCAx T → Int → T → Option (listCAx T)
-  axiom rangeAx     : listCAx T → Int → Int → Option (listCAx T)
-  axiom inListAx    : T → listCAx T → Bool
-  axiom sizeAx      : listCAx T → Int
+  axiom unitAx      : List T
+  axiom concatAx    : List T → List T → Option (List T)
+  axiom elementAx   : T → List T
+  axiom pushAx      : T → List T → List T
+  axiom getAx       : Int → List T → Option T
+  axiom updteAx     : Int → T → List T → Option (List T)
+  axiom makeAx      : Int → T → Option (List T)
+  axiom updateAllAx : List T → Int → List T → Option (List T)
+  axiom fillAx      : List T → Int → T → Option (List T)
+  axiom rangeAx     : List T → Int → Int → Option (List T)
+  axiom inListAx    : T → List T → Bool
+  axiom sizeAx      : List T → Int
 end ListHookDef
 
 noncomputable def ListHook (T : Type) : listHookSig T :=
-  { list      := ListHookDef.listCAx T,
-    unit      := ListHookDef.unitAx T,
+  { unit      := ListHookDef.unitAx T,
     concat    := ListHookDef.concatAx T,
     element   := ListHookDef.elementAx T,
     push      := ListHookDef.pushAx T,
