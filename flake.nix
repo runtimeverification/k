f30f0e36f17f4849eb1325c925dfb5bcb5f0aa2a
rv-jenkins <admin@runtimeverification.com>
2025-04-16 16:44:07 -0600

    Update dependency: deps/llvm-backend_release (#4793)
    Co-authored-by: devops <devops@runtimeverification.com>
    Co-authored-by: Roberto Rosmaninho <robertogomes@dcc.ufmg.br>

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "K Framework";
   inputs = {
-    llvm-backend.url = "github:runtimeverification/llvm-backend/v0.1.129";
+    llvm-backend.url = "github:runtimeverification/llvm-backend/v0.1.130";
     haskell-backend = {
       url = "github:runtimeverification/haskell-backend/v0.1.122";
       inputs.rv-utils.follows = "llvm-backend/rv-utils";
